388bcea7a0202f27557b8da792433f10e14fa4f4
hugomrdias <hugomrdias@gmail.com>
2020-04-25 22:44:01 +0000

    Merge "cpuidle: lpm-levels: Fix WFI mode to work on Non-PSCI targets"

---
+++ scripts/deploy-node.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+
+HOST=$1
+
+# upload binaries
+# TODO: destroy
+
+FILES_TO_SEND=(
+	./lotus
+	./lotus-miner
+	scripts/lotus-daemon.service
+	scripts/louts-miner.service
+)
+
+rsync -P "${FILES_TO_SEND[@]}" "$HOST:~/lotus-stage/"
+
+ssh "$HOST" 'bash -s' << 'EOF'
+set -euo pipefail
+
+systemctl stop lotus-miner
+systemctl stop lotus-daemon
+mkdir -p .lotus .lotusminer
+
+cd "$HOME/lotus-stage/"
+cp -f lotus lotus-miner /usr/local/bin
+cp -f lotus-daemon.service /etc/systemd/system/lotus-daemon.service
+cp -f lotus-miner.service /etc/systemd/system/lotus-miner.service
+
+systemctl daemon-reload
+systemctl start lotus-daemon
+EOF
+
+
+# setup miner actor
