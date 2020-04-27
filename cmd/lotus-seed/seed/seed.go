64b83adb5d5811fbe3a4d87b07d46f56e785a9d3
aeongrp <aeongrp@outlook.com>
2020-04-27 11:52:10 +0000

    Brew formula update for git-publish version v1.0.5

---
+++ cmd/lotus-seed/seed/seed.go
@@ -0,0 +1,272 @@
+package seed
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	logging "github.com/ipfs/go-log/v2"
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/minio/blake2b-simd"
+	"golang.org/x/xerrors"
+
+	ffi "github.com/filecoin-project/filecoin-ffi"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-commp-utils/zerocomm"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/specs-storage/storage"
+
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/genesis"
+)
+
+var log = logging.Logger("preseal")
+
+func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.SectorNumber, sectors int, sbroot string, preimage []byte, key *types.KeyInfo, fakeSectors bool) (*genesis.Miner, *types.KeyInfo, error) {
+	mid, err := address.IDFromAddress(maddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := os.MkdirAll(sbroot, 0775); err != nil { //nolint:gosec
+		return nil, nil, err
+	}
+
+	next := offset
+
+	sbfs := &basicfs.Provider{
+		Root: sbroot,
+	}
+
+	sb, err := ffiwrapper.New(sbfs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ssize, err := spt.SectorSize()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var sealedSectors []*genesis.PreSeal
+	for i := 0; i < sectors; i++ {
+		sid := abi.SectorID{Miner: abi.ActorID(mid), Number: next}
+		ref := storage.SectorRef{ID: sid, ProofType: spt}
+		next++
+
+		var preseal *genesis.PreSeal
+		if !fakeSectors {
+			preseal, err = presealSector(sb, sbfs, ref, ssize, preimage)
+			if err != nil {
+				return nil, nil, err
+			}
+		} else {
+			preseal, err = presealSectorFake(sbfs, ref, ssize)
+			if err != nil {
+				return nil, nil, err
+			}
+		}
+
+		sealedSectors = append(sealedSectors, preseal)
+	}
+
+	var minerAddr *wallet.Key
+	if key != nil {
+		minerAddr, err = wallet.NewKey(*key)
+		if err != nil {
+			return nil, nil, err
+		}
+	} else {
+		minerAddr, err = wallet.GenerateKey(types.KTBLS)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	var pid peer.ID
+	{
+		log.Warn("PeerID not specified, generating dummy")
+		p, _, err := ic.GenerateEd25519Key(rand.Reader)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		pid, err = peer.IDFromPrivateKey(p)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	miner := &genesis.Miner{
+		ID:            maddr,
+		Owner:         minerAddr.Address,
+		Worker:        minerAddr.Address,
+		MarketBalance: big.Zero(),
+		PowerBalance:  big.Zero(),
+		SectorSize:    ssize,
+		Sectors:       sealedSectors,
+		PeerId:        pid,
+	}
+
+	if err := createDeals(miner, minerAddr, maddr, ssize); err != nil {
+		return nil, nil, xerrors.Errorf("creating deals: %w", err)
+	}
+
+	{
+		b, err := json.MarshalIndent(&stores.LocalStorageMeta{
+			ID:       stores.ID(uuid.New().String()),
+			Weight:   0, // read-only
+			CanSeal:  false,
+			CanStore: false,
+		}, "", "  ")
+		if err != nil {
+			return nil, nil, xerrors.Errorf("marshaling storage config: %w", err)
+		}
+
+		if err := ioutil.WriteFile(filepath.Join(sbroot, "sectorstore.json"), b, 0644); err != nil {
+			return nil, nil, xerrors.Errorf("persisting storage metadata (%s): %w", filepath.Join(sbroot, "storage.json"), err)
+		}
+	}
+
+	return miner, &minerAddr.KeyInfo, nil
+}
+
+func presealSector(sb *ffiwrapper.Sealer, sbfs *basicfs.Provider, sid storage.SectorRef, ssize abi.SectorSize, preimage []byte) (*genesis.PreSeal, error) {
+	pi, err := sb.AddPiece(context.TODO(), sid, nil, abi.PaddedPieceSize(ssize).Unpadded(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	trand := blake2b.Sum256(preimage)
+	ticket := abi.SealRandomness(trand[:])
+
+	fmt.Printf("sector-id: %d, piece info: %v\n", sid, pi)
+
+	in2, err := sb.SealPreCommit1(context.TODO(), sid, ticket, []abi.PieceInfo{pi})
+	if err != nil {
+		return nil, xerrors.Errorf("commit: %w", err)
+	}
+
+	cids, err := sb.SealPreCommit2(context.TODO(), sid, in2)
+	if err != nil {
+		return nil, xerrors.Errorf("commit: %w", err)
+	}
+
+	if err := sb.FinalizeSector(context.TODO(), sid, nil); err != nil {
+		return nil, xerrors.Errorf("trim cache: %w", err)
+	}
+
+	if err := cleanupUnsealed(sbfs, sid); err != nil {
+		return nil, xerrors.Errorf("remove unsealed file: %w", err)
+	}
+
+	log.Warn("PreCommitOutput: ", sid, cids.Sealed, cids.Unsealed)
+
+	return &genesis.PreSeal{
+		CommR:     cids.Sealed,
+		CommD:     cids.Unsealed,
+		SectorID:  sid.ID.Number,
+		ProofType: sid.ProofType,
+	}, nil
+}
+
+func presealSectorFake(sbfs *basicfs.Provider, sid storage.SectorRef, ssize abi.SectorSize) (*genesis.PreSeal, error) {
+	paths, done, err := sbfs.AcquireSector(context.TODO(), sid, 0, storiface.FTSealed|storiface.FTCache, storiface.PathSealing)
+	if err != nil {
+		return nil, xerrors.Errorf("acquire unsealed sector: %w", err)
+	}
+	defer done()
+
+	if err := os.Mkdir(paths.Cache, 0755); err != nil {
+		return nil, xerrors.Errorf("mkdir cache: %w", err)
+	}
+
+	commr, err := ffi.FauxRep(sid.ProofType, paths.Cache, paths.Sealed)
+	if err != nil {
+		return nil, xerrors.Errorf("fauxrep: %w", err)
+	}
+
+	return &genesis.PreSeal{
+		CommR:     commr,
+		CommD:     zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded()),
+		SectorID:  sid.ID.Number,
+		ProofType: sid.ProofType,
+	}, nil
+}
+
+func cleanupUnsealed(sbfs *basicfs.Provider, ref storage.SectorRef) error {
+	paths, done, err := sbfs.AcquireSector(context.TODO(), ref, storiface.FTUnsealed, storiface.FTNone, storiface.PathSealing)
+	if err != nil {
+		return err
+	}
+	defer done()
+
+	return os.Remove(paths.Unsealed)
+}
+
+func WriteGenesisMiner(maddr address.Address, sbroot string, gm *genesis.Miner, key *types.KeyInfo) error {
+	output := map[string]genesis.Miner{
+		maddr.String(): *gm,
+	}
+
+	out, err := json.MarshalIndent(output, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	log.Infof("Writing preseal manifest to %s", filepath.Join(sbroot, "pre-seal-"+maddr.String()+".json"))
+
+	if err := ioutil.WriteFile(filepath.Join(sbroot, "pre-seal-"+maddr.String()+".json"), out, 0664); err != nil {
+		return err
+	}
+
+	if key != nil {
+		b, err := json.Marshal(key)
+		if err != nil {
+			return err
+		}
+
+		// TODO: allow providing key
+		if err := ioutil.WriteFile(filepath.Join(sbroot, "pre-seal-"+maddr.String()+".key"), []byte(hex.EncodeToString(b)), 0664); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func createDeals(m *genesis.Miner, k *wallet.Key, maddr address.Address, ssize abi.SectorSize) error {
+	for i, sector := range m.Sectors {
+		proposal := &market2.DealProposal{
+			PieceCID:             sector.CommD,
+			PieceSize:            abi.PaddedPieceSize(ssize),
+			Client:               k.Address,
+			Provider:             maddr,
+			Label:                fmt.Sprintf("%d", i),
+			StartEpoch:           0,
+			EndEpoch:             9001,
+			StoragePricePerEpoch: big.Zero(),
+			ProviderCollateral:   big.Zero(),
+			ClientCollateral:     big.Zero(),
+		}
+
+		sector.Deal = *proposal
+	}
+
+	return nil
+}
---
+++ metrics/metrics.go
@@ -0,0 +1,358 @@
+package metrics
+
+import (
+	"context"
+	"time"
+
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
+
+	rpcmetrics "github.com/filecoin-project/go-jsonrpc/metrics"
+
+	"github.com/filecoin-project/lotus/blockstore"
+)
+
+// Distribution
+var defaultMillisecondsDistribution = view.Distribution(0.01, 0.05, 0.1, 0.3, 0.6, 0.8, 1, 2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30, 40, 50, 65, 80, 100, 130, 160, 200, 250, 300, 400, 500, 650, 800, 1000, 2000, 3000, 4000, 5000, 7500, 10000, 20000, 50000, 100000)
+var workMillisecondsDistribution = view.Distribution(
+	250, 500, 1000, 2000, 5000, 10_000, 30_000, 60_000, 2*60_000, 5*60_000, 10*60_000, 15*60_000, 30*60_000, // short sealing tasks
+	40*60_000, 45*60_000, 50*60_000, 55*60_000, 60*60_000, 65*60_000, 70*60_000, 75*60_000, 80*60_000, 85*60_000, 100*60_000, 120*60_000, // PC2 / C2 range
+	130*60_000, 140*60_000, 150*60_000, 160*60_000, 180*60_000, 200*60_000, 220*60_000, 260*60_000, 300*60_000, // PC1 range
+	350*60_000, 400*60_000, 600*60_000, 800*60_000, 1000*60_000, 1300*60_000, 1800*60_000, 4000*60_000, 10000*60_000, // intel PC1 range
+)
+
+// Global Tags
+var (
+	// common
+	Version, _     = tag.NewKey("version")
+	Commit, _      = tag.NewKey("commit")
+	NodeType, _    = tag.NewKey("node_type")
+	PeerID, _      = tag.NewKey("peer_id")
+	MinerID, _     = tag.NewKey("miner_id")
+	FailureType, _ = tag.NewKey("failure_type")
+
+	// chain
+	Local, _        = tag.NewKey("local")
+	MessageFrom, _  = tag.NewKey("message_from")
+	MessageTo, _    = tag.NewKey("message_to")
+	MessageNonce, _ = tag.NewKey("message_nonce")
+	ReceivedFrom, _ = tag.NewKey("received_from")
+	Endpoint, _     = tag.NewKey("endpoint")
+	APIInterface, _ = tag.NewKey("api") // to distinguish between gateway api and full node api endpoint calls
+
+	// miner
+	TaskType, _       = tag.NewKey("task_type")
+	WorkerHostname, _ = tag.NewKey("worker_hostname")
+)
+
+// Measures
+var (
+	// common
+	LotusInfo          = stats.Int64("info", "Arbitrary counter to tag lotus info to", stats.UnitDimensionless)
+	PeerCount          = stats.Int64("peer/count", "Current number of FIL peers", stats.UnitDimensionless)
+	APIRequestDuration = stats.Float64("api/request_duration_ms", "Duration of API requests", stats.UnitMilliseconds)
+
+	// chain
+	ChainNodeHeight                     = stats.Int64("chain/node_height", "Current Height of the node", stats.UnitDimensionless)
+	ChainNodeHeightExpected             = stats.Int64("chain/node_height_expected", "Expected Height of the node", stats.UnitDimensionless)
+	ChainNodeWorkerHeight               = stats.Int64("chain/node_worker_height", "Current Height of workers on the node", stats.UnitDimensionless)
+	MessagePublished                    = stats.Int64("message/published", "Counter for total locally published messages", stats.UnitDimensionless)
+	MessageReceived                     = stats.Int64("message/received", "Counter for total received messages", stats.UnitDimensionless)
+	MessageValidationFailure            = stats.Int64("message/failure", "Counter for message validation failures", stats.UnitDimensionless)
+	MessageValidationSuccess            = stats.Int64("message/success", "Counter for message validation successes", stats.UnitDimensionless)
+	BlockPublished                      = stats.Int64("block/published", "Counter for total locally published blocks", stats.UnitDimensionless)
+	BlockReceived                       = stats.Int64("block/received", "Counter for total received blocks", stats.UnitDimensionless)
+	BlockValidationFailure              = stats.Int64("block/failure", "Counter for block validation failures", stats.UnitDimensionless)
+	BlockValidationSuccess              = stats.Int64("block/success", "Counter for block validation successes", stats.UnitDimensionless)
+	BlockValidationDurationMilliseconds = stats.Float64("block/validation_ms", "Duration for Block Validation in ms", stats.UnitMilliseconds)
+	BlockDelay                          = stats.Int64("block/delay", "Delay of accepted blocks, where delay is >5s", stats.UnitMilliseconds)
+	PubsubPublishMessage                = stats.Int64("pubsub/published", "Counter for total published messages", stats.UnitDimensionless)
+	PubsubDeliverMessage                = stats.Int64("pubsub/delivered", "Counter for total delivered messages", stats.UnitDimensionless)
+	PubsubRejectMessage                 = stats.Int64("pubsub/rejected", "Counter for total rejected messages", stats.UnitDimensionless)
+	PubsubDuplicateMessage              = stats.Int64("pubsub/duplicate", "Counter for total duplicate messages", stats.UnitDimensionless)
+	PubsubRecvRPC                       = stats.Int64("pubsub/recv_rpc", "Counter for total received RPCs", stats.UnitDimensionless)
+	PubsubSendRPC                       = stats.Int64("pubsub/send_rpc", "Counter for total sent RPCs", stats.UnitDimensionless)
+	PubsubDropRPC                       = stats.Int64("pubsub/drop_rpc", "Counter for total dropped RPCs", stats.UnitDimensionless)
+	VMFlushCopyDuration                 = stats.Float64("vm/flush_copy_ms", "Time spent in VM Flush Copy", stats.UnitMilliseconds)
+	VMFlushCopyCount                    = stats.Int64("vm/flush_copy_count", "Number of copied objects", stats.UnitDimensionless)
+	VMApplyBlocksTotal                  = stats.Float64("vm/applyblocks_total_ms", "Time spent applying block state", stats.UnitMilliseconds)
+	VMApplyMessages                     = stats.Float64("vm/applyblocks_messages", "Time spent applying block messages", stats.UnitMilliseconds)
+	VMApplyEarly                        = stats.Float64("vm/applyblocks_early", "Time spent in early apply-blocks (null cron, upgrades)", stats.UnitMilliseconds)
+	VMApplyCron                         = stats.Float64("vm/applyblocks_cron", "Time spent in cron", stats.UnitMilliseconds)
+	VMApplyFlush                        = stats.Float64("vm/applyblocks_flush", "Time spent flushing vm state", stats.UnitMilliseconds)
+	VMSends                             = stats.Int64("vm/sends", "Counter for sends processed by the VM", stats.UnitDimensionless)
+	VMApplied                           = stats.Int64("vm/applied", "Counter for messages (including internal messages) processed by the VM", stats.UnitDimensionless)
+
+	// miner
+	WorkerCallsStarted           = stats.Int64("sealing/worker_calls_started", "Counter of started worker tasks", stats.UnitDimensionless)
+	WorkerCallsReturnedCount     = stats.Int64("sealing/worker_calls_returned_count", "Counter of returned worker tasks", stats.UnitDimensionless)
+	WorkerCallsReturnedDuration  = stats.Float64("sealing/worker_calls_returned_ms", "Counter of returned worker tasks", stats.UnitMilliseconds)
+	WorkerUntrackedCallsReturned = stats.Int64("sealing/worker_untracked_calls_returned", "Counter of returned untracked worker tasks", stats.UnitDimensionless)
+
+	// splitstore
+	SplitstoreMiss                  = stats.Int64("splitstore/miss", "Number of misses in hotstre access", stats.UnitDimensionless)
+	SplitstoreCompactionTimeSeconds = stats.Float64("splitstore/compaction_time", "Compaction time in seconds", stats.UnitSeconds)
+	SplitstoreCompactionHot         = stats.Int64("splitstore/hot", "Number of hot blocks in last compaction", stats.UnitDimensionless)
+	SplitstoreCompactionCold        = stats.Int64("splitstore/cold", "Number of cold blocks in last compaction", stats.UnitDimensionless)
+	SplitstoreCompactionDead        = stats.Int64("splitstore/dead", "Number of dead blocks in last compaction", stats.UnitDimensionless)
+)
+
+var (
+	InfoView = &view.View{
+		Name:        "info",
+		Description: "Lotus node information",
+		Measure:     LotusInfo,
+		Aggregation: view.LastValue(),
+		TagKeys:     []tag.Key{Version, Commit},
+	}
+	ChainNodeHeightView = &view.View{
+		Measure:     ChainNodeHeight,
+		Aggregation: view.LastValue(),
+	}
+	ChainNodeHeightExpectedView = &view.View{
+		Measure:     ChainNodeHeightExpected,
+		Aggregation: view.LastValue(),
+	}
+	ChainNodeWorkerHeightView = &view.View{
+		Measure:     ChainNodeWorkerHeight,
+		Aggregation: view.LastValue(),
+	}
+	BlockReceivedView = &view.View{
+		Measure:     BlockReceived,
+		Aggregation: view.Count(),
+	}
+	BlockValidationFailureView = &view.View{
+		Measure:     BlockValidationFailure,
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{FailureType},
+	}
+	BlockValidationSuccessView = &view.View{
+		Measure:     BlockValidationSuccess,
+		Aggregation: view.Count(),
+	}
+	BlockValidationDurationView = &view.View{
+		Measure:     BlockValidationDurationMilliseconds,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	BlockDelayView = &view.View{
+		Measure: BlockDelay,
+		TagKeys: []tag.Key{MinerID},
+		Aggregation: func() *view.Aggregation {
+			var bounds []float64
+			for i := 5; i < 29; i++ { // 5-29s, step 1s
+				bounds = append(bounds, float64(i*1000))
+			}
+			for i := 30; i < 60; i += 2 { // 30-58s, step 2s
+				bounds = append(bounds, float64(i*1000))
+			}
+			for i := 60; i <= 300; i += 10 { // 60-300s, step 10s
+				bounds = append(bounds, float64(i*1000))
+			}
+			bounds = append(bounds, 600*1000) // final cutoff at 10m
+			return view.Distribution(bounds...)
+		}(),
+	}
+	MessagePublishedView = &view.View{
+		Measure:     MessagePublished,
+		Aggregation: view.Count(),
+	}
+	MessageReceivedView = &view.View{
+		Measure:     MessageReceived,
+		Aggregation: view.Count(),
+	}
+	MessageValidationFailureView = &view.View{
+		Measure:     MessageValidationFailure,
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{FailureType, Local},
+	}
+	MessageValidationSuccessView = &view.View{
+		Measure:     MessageValidationSuccess,
+		Aggregation: view.Count(),
+	}
+	PeerCountView = &view.View{
+		Measure:     PeerCount,
+		Aggregation: view.LastValue(),
+	}
+	PubsubPublishMessageView = &view.View{
+		Measure:     PubsubPublishMessage,
+		Aggregation: view.Count(),
+	}
+	PubsubDeliverMessageView = &view.View{
+		Measure:     PubsubDeliverMessage,
+		Aggregation: view.Count(),
+	}
+	PubsubRejectMessageView = &view.View{
+		Measure:     PubsubRejectMessage,
+		Aggregation: view.Count(),
+	}
+	PubsubDuplicateMessageView = &view.View{
+		Measure:     PubsubDuplicateMessage,
+		Aggregation: view.Count(),
+	}
+	PubsubRecvRPCView = &view.View{
+		Measure:     PubsubRecvRPC,
+		Aggregation: view.Count(),
+	}
+	PubsubSendRPCView = &view.View{
+		Measure:     PubsubSendRPC,
+		Aggregation: view.Count(),
+	}
+	PubsubDropRPCView = &view.View{
+		Measure:     PubsubDropRPC,
+		Aggregation: view.Count(),
+	}
+	APIRequestDurationView = &view.View{
+		Measure:     APIRequestDuration,
+		Aggregation: defaultMillisecondsDistribution,
+		TagKeys:     []tag.Key{APIInterface, Endpoint},
+	}
+	VMFlushCopyDurationView = &view.View{
+		Measure:     VMFlushCopyDuration,
+		Aggregation: view.Sum(),
+	}
+	VMFlushCopyCountView = &view.View{
+		Measure:     VMFlushCopyCount,
+		Aggregation: view.Sum(),
+	}
+	VMApplyBlocksTotalView = &view.View{
+		Measure:     VMApplyBlocksTotal,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	VMApplyMessagesView = &view.View{
+		Measure:     VMApplyMessages,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	VMApplyEarlyView = &view.View{
+		Measure:     VMApplyEarly,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	VMApplyCronView = &view.View{
+		Measure:     VMApplyCron,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	VMApplyFlushView = &view.View{
+		Measure:     VMApplyFlush,
+		Aggregation: defaultMillisecondsDistribution,
+	}
+	VMSendsView = &view.View{
+		Measure:     VMSends,
+		Aggregation: view.LastValue(),
+	}
+	VMAppliedView = &view.View{
+		Measure:     VMApplied,
+		Aggregation: view.LastValue(),
+	}
+
+	// miner
+	WorkerCallsStartedView = &view.View{
+		Measure:     WorkerCallsStarted,
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{TaskType, WorkerHostname},
+	}
+	WorkerCallsReturnedCountView = &view.View{
+		Measure:     WorkerCallsReturnedCount,
+		Aggregation: view.Count(),
+		TagKeys:     []tag.Key{TaskType, WorkerHostname},
+	}
+	WorkerUntrackedCallsReturnedView = &view.View{
+		Measure:     WorkerUntrackedCallsReturned,
+		Aggregation: view.Count(),
+	}
+	WorkerCallsReturnedDurationView = &view.View{
+		Measure:     WorkerCallsReturnedDuration,
+		Aggregation: workMillisecondsDistribution,
+		TagKeys:     []tag.Key{TaskType, WorkerHostname},
+	}
+
+	// splitstore
+	SplitstoreMissView = &view.View{
+		Measure:     SplitstoreMiss,
+		Aggregation: view.Count(),
+	}
+	SplitstoreCompactionTimeSecondsView = &view.View{
+		Measure:     SplitstoreCompactionTimeSeconds,
+		Aggregation: view.LastValue(),
+	}
+	SplitstoreCompactionHotView = &view.View{
+		Measure:     SplitstoreCompactionHot,
+		Aggregation: view.LastValue(),
+	}
+	SplitstoreCompactionColdView = &view.View{
+		Measure:     SplitstoreCompactionCold,
+		Aggregation: view.Sum(),
+	}
+	SplitstoreCompactionDeadView = &view.View{
+		Measure:     SplitstoreCompactionDead,
+		Aggregation: view.Sum(),
+	}
+)
+
+// DefaultViews is an array of OpenCensus views for metric gathering purposes
+var DefaultViews = func() []*view.View {
+	views := []*view.View{
+		InfoView,
+		PeerCountView,
+		APIRequestDurationView,
+	}
+	views = append(views, blockstore.DefaultViews...)
+	views = append(views, rpcmetrics.DefaultViews...)
+	return views
+}()
+
+var ChainNodeViews = append([]*view.View{
+	ChainNodeHeightView,
+	ChainNodeHeightExpectedView,
+	ChainNodeWorkerHeightView,
+	BlockReceivedView,
+	BlockValidationFailureView,
+	BlockValidationSuccessView,
+	BlockValidationDurationView,
+	BlockDelayView,
+	MessagePublishedView,
+	MessageReceivedView,
+	MessageValidationFailureView,
+	MessageValidationSuccessView,
+	PubsubPublishMessageView,
+	PubsubDeliverMessageView,
+	PubsubRejectMessageView,
+	PubsubDuplicateMessageView,
+	PubsubRecvRPCView,
+	PubsubSendRPCView,
+	PubsubDropRPCView,
+	VMFlushCopyCountView,
+	VMFlushCopyDurationView,
+	SplitstoreMissView,
+	SplitstoreCompactionTimeSecondsView,
+	SplitstoreCompactionHotView,
+	SplitstoreCompactionColdView,
+	SplitstoreCompactionDeadView,
+	VMApplyBlocksTotalView,
+	VMApplyMessagesView,
+	VMApplyEarlyView,
+	VMApplyCronView,
+	VMApplyFlushView,
+	VMSendsView,
+	VMAppliedView,
+}, DefaultViews...)
+
+var MinerNodeViews = append([]*view.View{
+	WorkerCallsStartedView,
+	WorkerCallsReturnedCountView,
+	WorkerUntrackedCallsReturnedView,
+	WorkerCallsReturnedDurationView,
+}, DefaultViews...)
+
+// SinceInMilliseconds returns the duration of time since the provide time as a float64.
+func SinceInMilliseconds(startTime time.Time) float64 {
+	return float64(time.Since(startTime).Nanoseconds()) / 1e6
+}
+
+// Timer is a function stopwatch, calling it starts the timer,
+// calling the returned function will record the duration.
+func Timer(ctx context.Context, m *stats.Float64Measure) func() {
+	start := time.Now()
+	return func() {
+		stats.Record(ctx, m.M(SinceInMilliseconds(start)))
+	}
+}
