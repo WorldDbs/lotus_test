ffb7cb0e6a6c3c29a6d1046e7bbe8baee07d3f65
jon <jon@atack.com>
2020-05-17 05:14:17 +0000

    Fixed argument order when delegating to QUnit.equal in test_helper.

---
+++ cmd/lotus-shed/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/urfave/cli/v2"
+
+	"github.com/filecoin-project/lotus/build"
+)
+
+var log = logging.Logger("lotus-shed")
+
+func main() {
+	logging.SetLogLevel("*", "INFO")
+
+	local := []*cli.Command{
+		base64Cmd,
+		base32Cmd,
+		base16Cmd,
+		bitFieldCmd,
+		cronWcCmd,
+		frozenMinersCmd,
+		keyinfoCmd,
+		jwtCmd,
+		noncefix,
+		bigIntParseCmd,
+		staterootCmd,
+		auditsCmd,
+		importCarCmd,
+		importObjectCmd,
+		commpToCidCmd,
+		fetchParamCmd,
+		postFindCmd,
+		proofsCmd,
+		verifRegCmd,
+		marketCmd,
+		miscCmd,
+		mpoolCmd,
+		genesisVerifyCmd,
+		mathCmd,
+		minerCmd,
+		mpoolStatsCmd,
+		exportChainCmd,
+		consensusCmd,
+		storageStatsCmd,
+		syncCmd,
+		stateTreePruneCmd,
+		datastoreCmd,
+		ledgerCmd,
+		sectorsCmd,
+		msgCmd,
+		electionCmd,
+		rpcCmd,
+		cidCmd,
+		blockmsgidCmd,
+		signaturesCmd,
+		actorCmd,
+		minerTypesCmd,
+	}
+
+	app := &cli.App{
+		Name:     "lotus-shed",
+		Usage:    "A place for all the lotus tools",
+		Version:  build.BuildVersion,
+		Commands: local,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "repo",
+				EnvVars: []string{"LOTUS_PATH"},
+				Hidden:  true,
+				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
+			},
+			&cli.StringFlag{
+				Name:    "miner-repo",
+				Aliases: []string{"storagerepo"},
+				EnvVars: []string{"LOTUS_MINER_PATH", "LOTUS_STORAGE_PATH"},
+				Value:   "~/.lotusminer", // TODO: Consider XDG_DATA_HOME
+				Usage:   fmt.Sprintf("Specify miner repo path. flag storagerepo and env LOTUS_STORAGE_PATH are DEPRECATION, will REMOVE SOON"),
+			},
+			&cli.StringFlag{
+				Name:  "log-level",
+				Value: "info",
+			},
+		},
+		Before: func(cctx *cli.Context) error {
+			return logging.SetLogLevel("lotus-shed", cctx.String("log-level"))
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Warnf("%+v", err)
+		os.Exit(1)
+		return
+	}
+}
