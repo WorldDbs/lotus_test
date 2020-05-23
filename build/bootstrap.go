afad4236ae51cab70c47c30995e6d38c80949f7b
juan <juan@benet.ai>
2020-05-23 14:04:46 +0000

    570486f6-2e49-11e5-9284-b827eb9e62be

---
+++ build/bootstrap.go
@@ -0,0 +1,30 @@
+package build
+
+import (
+	"context"
+	"strings"
+
+	"github.com/filecoin-project/lotus/lib/addrutil"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+func BuiltinBootstrap() ([]peer.AddrInfo, error) {
+	if DisableBuiltinAssets {
+		return nil, nil
+	}
+
+	b := rice.MustFindBox("bootstrap")
+
+	if BootstrappersFile != "" {
+		spi := b.MustString(BootstrappersFile)
+		if spi == "" {
+			return nil, nil
+		}
+
+		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
+	}
+
+	return nil, nil
+}
