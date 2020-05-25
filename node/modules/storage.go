2bf3b3279e2f991650f4a2a5f6b5bd2a281e9170
arajasek94 <arajasek94@gmail.com>
2020-05-25 15:07:08 +0000

    Create divide-two-integers.cpp

---
+++ node/modules/storage.go
@@ -0,0 +1,59 @@
+package modules
+
+import (
+	"context"
+	"path/filepath"
+
+	"go.uber.org/fx"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/lib/backupds"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/repo"
+)
+
+func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
+	return func(lc fx.Lifecycle) repo.LockedRepo {
+		lc.Append(fx.Hook{
+			OnStop: func(_ context.Context) error {
+				return lr.Close()
+			},
+		})
+
+		return lr
+	}
+}
+
+func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
+	return lr.KeyStore()
+}
+
+func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
+	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
+		ctx := helpers.LifecycleCtx(mctx, lc)
+		mds, err := r.Datastore(ctx, "/metadata")
+		if err != nil {
+			return nil, err
+		}
+
+		var logdir string
+		if !disableLog {
+			logdir = filepath.Join(r.Path(), "kvlog/metadata")
+		}
+
+		bds, err := backupds.Wrap(mds, logdir)
+		if err != nil {
+			return nil, xerrors.Errorf("opening backupds: %w", err)
+		}
+
+		lc.Append(fx.Hook{
+			OnStop: func(_ context.Context) error {
+				return bds.CloseLog()
+			},
+		})
+
+		return bds, nil
+	}
+}
