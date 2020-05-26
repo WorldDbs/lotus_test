9a41a75df6414ba88bbc78e902d4d315f32e0d42
magik6k <magik6k@gmail.com>
2020-05-26 13:28:01 +0000

    Remove maxsplit named parameter for py2 compat

---
+++ chain/events/utils.go
@@ -0,0 +1,50 @@
+package events
+
+import (
+	"context"
+
+	"github.com/filecoin-project/lotus/chain/stmgr"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
+	msg := smsg.VMMessage()
+
+	return func(ts *types.TipSet) (done bool, more bool, err error) {
+		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
+		if err != nil {
+			return false, true, err
+		}
+
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain
+		if msg.Nonce >= fa.Nonce {
+			return false, true, nil
+		}
+
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
+		if err != nil {
+			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
+		}
+
+		if ml == nil {
+			more, err = hnd(msg, nil, ts, ts.Height())
+		} else {
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
+		}
+
+		return true, more, err
+	}
+}
+
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
+	return func(msg *types.Message) (matched bool, err error) {
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
+		}
+
+		return inmsg.Equals(msg), nil
+	}
+}
