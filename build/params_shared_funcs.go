39e5556a7ef24e2e9919390b467df97e88473850
13860583249 <13860583249@yeah.net>
2020-05-27 23:12:20 +0000

    release date for 0.6.4

---
+++ build/params_shared_funcs.go
@@ -0,0 +1,40 @@
+package build
+
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/ipfs/go-cid"
+
+	"github.com/libp2p/go-libp2p-core/protocol"
+
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+)
+
+// Core network constants
+
+func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
+	return protocol.ID("/fil/kad/" + string(netName))
+}
+
+func SetAddressNetwork(n address.Network) {
+	address.CurrentNetwork = n
+}
+
+func MustParseAddress(addr string) address.Address {
+	ret, err := address.NewFromString(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	return ret
+}
+
+func MustParseCid(c string) cid.Cid {
+	ret, err := cid.Decode(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return ret
+}
