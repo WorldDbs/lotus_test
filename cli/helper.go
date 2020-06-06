e28e717d97cc503da03fc6bf53d5dab079c5ac73
aeongrp <aeongrp@outlook.com>
2020-06-06 18:13:45 +0000

    Create demo1.js

--- cli/helper.go
+++ cli/helper.go
@@ -2,16 +2,15 @@ package cli
 
 import (
 	"fmt"
-	"io"
 	"os"
 
-	ufcli "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 type PrintHelpErr struct {
 	Err error
-	Ctx *ufcli.Context
+	Ctx *cli.Context
 }
 
 func (e *PrintHelpErr) Error() string {
@@ -27,11 +26,11 @@ func (e *PrintHelpErr) Is(o error) bool {
 	return ok
 }
 
-func ShowHelp(cctx *ufcli.Context, err error) error {
+func ShowHelp(cctx *cli.Context, err error) error {
 	return &PrintHelpErr{Err: err, Ctx: cctx}
 }
 
-func RunApp(app *ufcli.App) {
+func RunApp(app *cli.App) {
 	if err := app.Run(os.Args); err != nil {
 		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
@@ -40,40 +39,8 @@ func RunApp(app *ufcli.App) {
 		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
-			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
+			_ = cli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
 		}
 		os.Exit(1)
 	}
-}
-
-type AppFmt struct {
-	app   *ufcli.App
-	Stdin io.Reader
-}
-
-func NewAppFmt(a *ufcli.App) *AppFmt {
-	var stdin io.Reader
-	istdin, ok := a.Metadata["stdin"]
-	if ok {
-		stdin = istdin.(io.Reader)
-	} else {
-		stdin = os.Stdin
-	}
-	return &AppFmt{app: a, Stdin: stdin}
-}
-
-func (a *AppFmt) Print(args ...interface{}) {
-	fmt.Fprint(a.app.Writer, args...)
-}
-
-func (a *AppFmt) Println(args ...interface{}) {
-	fmt.Fprintln(a.app.Writer, args...)
-}
-
-func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
-	fmt.Fprintf(a.app.Writer, fmtstr, args...)
-}
-
-func (a *AppFmt) Scan(args ...interface{}) (int, error) {
-	return fmt.Fscan(a.Stdin, args...)
 }
