5e70c278818f6413390865f71f295880f56c3664
vyzo <vyzo@hackzen.org>
2020-06-06 15:50:21 +0000

    Small speedup in slow integration test

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -11,8 +11,8 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/test"
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/node/repo"
@@ -55,7 +55,7 @@ func TestMinerAllInfo(t *testing.T) {
 			"testnode-full":    n[0],
 			"testnode-storage": sn[0],
 		}
-		api.RunningNodeType = api.NodeMiner
+		build.RunningNodeType = build.NodeMiner
 
 		cctx := cli.NewContext(app, flag.NewFlagSet("", flag.ContinueOnError), nil)
 
@@ -70,7 +70,7 @@ func TestMinerAllInfo(t *testing.T) {
 		return n, sn
 	}
 
-	test.TestDealFlow(t, bp, time.Second, false, false, 0)
+	test.TestDealFlow(t, bp, time.Second, false, false)
 
 	t.Run("post-info-all", run)
 }
