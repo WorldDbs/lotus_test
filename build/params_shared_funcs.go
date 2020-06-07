74ba65586e4f5a2b4023a982441b78589beb7e36
sjors <sjors@sprovoost.nl>
2020-06-07 13:07:04 +0000

    Remove State Econ Variables

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,13 +1,35 @@
 package build
 
 import (
+	"sort"
+
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 
+	"github.com/filecoin-project/go-state-types/abi"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
+
+func DefaultSectorSize() abi.SectorSize {
+	szs := make([]abi.SectorSize, 0, len(miner0.SupportedProofTypes))
+	for spt := range miner0.SupportedProofTypes {
+		ss, err := spt.SectorSize()
+		if err != nil {
+			panic(err)
+		}
+
+		szs = append(szs, ss)
+	}
+
+	sort.Slice(szs, func(i, j int) bool {
+		return szs[i] < szs[j]
+	})
+
+	return szs[0]
+}
 
 // Core network constants
 
@@ -17,24 +39,14 @@ func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
 
+func UseNewestNetwork() bool {
+	// TODO: Put these in a container we can iterate over
+	if UpgradeBreezeHeight <= 0 && UpgradeSmokeHeight <= 0 && UpgradeActorsV2Height <= 0 {
+		return true
+	}
+	return false
+}
+
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}
-
-func MustParseAddress(addr string) address.Address {
-	ret, err := address.NewFromString(addr)
-	if err != nil {
-		panic(err)
-	}
-
-	return ret
-}
-
-func MustParseCid(c string) cid.Cid {
-	ret, err := cid.Decode(c)
-	if err != nil {
-		panic(err)
-	}
-
-	return ret
 }
