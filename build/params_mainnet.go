d2e594b1892203ddc069b62329408f3a448f99a7
hugomrdias <hugomrdias@gmail.com>
2020-06-09 16:24:09 +0000

    bc8bb876-2e5a-11e5-9284-b827eb9e62be

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -1,9 +1,6 @@
 // +build !debug
 // +build !2k
 // +build !testground
-// +build !calibnet
-// +build !nerpanet
-// +build !butterflynet
 
 package build
 
@@ -14,7 +11,8 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
@@ -22,11 +20,7 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	UpgradeSmokeHeight: DrandMainnet,
 }
 
-const BootstrappersFile = "mainnet.pi"
-const GenesisFile = "mainnet.car"
-
 const UpgradeBreezeHeight = 41280
-
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = 51000
@@ -34,7 +28,7 @@ const UpgradeSmokeHeight = 51000
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
 
-const UpgradeActorsV2Height = 138720
+var UpgradeActorsV2Height = abi.ChainEpoch(138720)
 
 const UpgradeTapeHeight = 140760
 
@@ -43,51 +37,24 @@ const UpgradeTapeHeight = 140760
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
-const UpgradeKumquatHeight = 170000
-
-const UpgradeCalicoHeight = 265200
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
-
-const UpgradeOrangeHeight = 336458
-
-// 2020-12-22T02:00:00Z
-const UpgradeClausHeight = 343200
-
-// 2021-03-04T00:00:30Z
-var UpgradeActorsV3Height = abi.ChainEpoch(550321)
-
-// 2021-04-12T22:00:00Z
-const UpgradeNorwegianHeight = 665280
-
-// 2021-04-29T06:00:00Z
-var UpgradeActorsV4Height = abi.ChainEpoch(712320)
-
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
+	policy.SetSupportedProofTypes(
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
+		abi.RegisteredSealProof_StackedDrg64GiBV1,
+	)
 
 	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
 		SetAddressNetwork(address.Mainnet)
 	}
 
-	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
-		UpgradeActorsV3Height = math.MaxInt64
-	}
-
-	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
-		UpgradeActorsV4Height = math.MaxInt64
+	if os.Getenv("LOTUS_DISABLE_V2_ACTOR_MIGRATION") == "1" {
+		UpgradeActorsV2Height = math.MaxInt64
 	}
 
 	Devnet = false
-
-	BuildType = BuildMainnet
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+const BlockDelaySecs = uint64(builtin0.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
-
-// we skip checks on message validity in this block to sidestep the zero-bls signature
-var WhitelistedBlock = MustParseCid("bafy2bzaceapyg2uyzk7vueh3xccxkuwbz3nxewjyguoxvhx77malc2lzn2ybi")
