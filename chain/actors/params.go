13dd2ba94d96d288b2b724ca3adca1057e7b8580
ligi <ligi@ligi.de>
2020-06-13 12:27:38 +0000

    Added group permissions.

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -3,8 +3,6 @@ package actors
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"
-
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -13,7 +11,7 @@ func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
+		return nil, aerrors.Absorb(err, 1, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
 }
