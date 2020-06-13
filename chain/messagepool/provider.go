85424e0d14ca6004dac0440fa1a73c57a463e95f
hello <hello@brooklynzelenka.com>
2020-06-13 18:11:59 +0000

    Fix URL in table of contents.

--- chain/messagepool/provider.go
+++ chain/messagepool/provider.go
@@ -2,23 +2,14 @@ package messagepool
 
 import (
 	"context"
-	"time"
 
 	"github.com/ipfs/go-cid"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/messagesigner"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
-var (
-	HeadChangeCoalesceMinDelay      = 2 * time.Second
-	HeadChangeCoalesceMaxDelay      = 6 * time.Second
-	HeadChangeCoalesceMergeInterval = time.Second
 )
 
 type Provider interface {
@@ -31,36 +22,19 @@ type Provider interface {
 	MessagesForTipset(*types.TipSet) ([]types.ChainMsg, error)
 	LoadTipSet(tsk types.TipSetKey) (*types.TipSet, error)
 	ChainComputeBaseFee(ctx context.Context, ts *types.TipSet) (types.BigInt, error)
-	IsLite() bool
 }
 
 type mpoolProvider struct {
 	sm *stmgr.StateManager
 	ps *pubsub.PubSub
-
-	lite messagesigner.MpoolNonceAPI
 }
 
 func NewProvider(sm *stmgr.StateManager, ps *pubsub.PubSub) Provider {
 	return &mpoolProvider{sm: sm, ps: ps}
 }
 
-func NewProviderLite(sm *stmgr.StateManager, ps *pubsub.PubSub, noncer messagesigner.MpoolNonceAPI) Provider {
-	return &mpoolProvider{sm: sm, ps: ps, lite: noncer}
-}
-
-func (mpp *mpoolProvider) IsLite() bool {
-	return mpp.lite != nil
-}
-
 func (mpp *mpoolProvider) SubscribeHeadChanges(cb func(rev, app []*types.TipSet) error) *types.TipSet {
-	mpp.sm.ChainStore().SubscribeHeadChanges(
-		store.WrapHeadChangeCoalescer(
-			cb,
-			HeadChangeCoalesceMinDelay,
-			HeadChangeCoalesceMaxDelay,
-			HeadChangeCoalesceMergeInterval,
-		))
+	mpp.sm.ChainStore().SubscribeHeadChanges(cb)
 	return mpp.sm.ChainStore().GetHeaviestTipSet()
 }
 
@@ -73,19 +47,6 @@ func (mpp *mpoolProvider) PubSubPublish(k string, v []byte) error {
 }
 
 func (mpp *mpoolProvider) GetActorAfter(addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	if mpp.IsLite() {
-		n, err := mpp.lite.GetNonce(context.TODO(), addr, ts.Key())
-		if err != nil {
-			return nil, xerrors.Errorf("getting nonce over lite: %w", err)
-		}
-		a, err := mpp.lite.GetActor(context.TODO(), addr, ts.Key())
-		if err != nil {
-			return nil, xerrors.Errorf("getting actor over lite: %w", err)
-		}
-		a.Nonce = n
-		return a, nil
-	}
-
 	stcid, _, err := mpp.sm.TipSetState(context.TODO(), ts)
 	if err != nil {
 		return nil, xerrors.Errorf("computing tipset state for GetActor: %w", err)
