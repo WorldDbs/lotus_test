ddc31245f813f13eff52cd9a4044481035e716fc
mail <mail@bitpshr.net>
2020-06-17 12:11:40 +0000

    Update _solo.scss

--- cmd/chain-noise/main.go
+++ cmd/chain-noise/main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -26,16 +26,6 @@ func main() {
 				EnvVars: []string{"LOTUS_PATH"},
 				Hidden:  true,
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
-			},
-			&cli.IntFlag{
-				Name:  "limit",
-				Usage: "spam transaction count limit, <= 0 is no limit",
-				Value: 0,
-			},
-			&cli.IntFlag{
-				Name:  "rate",
-				Usage: "spam transaction rate, count per second",
-				Value: 5,
 			},
 		},
 		Commands: []*cli.Command{runCmd},
@@ -62,17 +52,11 @@ var runCmd = &cli.Command{
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
-		rate := cctx.Int("rate")
-		if rate <= 0 {
-			rate = 5
-		}
-		limit := cctx.Int("limit")
-
-		return sendSmallFundsTxs(ctx, api, addr, rate, limit)
+		return sendSmallFundsTxs(ctx, api, addr, 5)
 	},
 }
 
-func sendSmallFundsTxs(ctx context.Context, api v0api.FullNode, from address.Address, rate, limit int) error {
+func sendSmallFundsTxs(ctx context.Context, api api.FullNode, from address.Address, rate int) error {
 	var sendSet []address.Address
 	for i := 0; i < 20; i++ {
 		naddr, err := api.WalletNew(ctx, types.KTSecp256k1)
@@ -82,14 +66,9 @@ func sendSmallFundsTxs(ctx context.Context, api v0api.FullNode, from address.Add
 
 		sendSet = append(sendSet, naddr)
 	}
-	count := limit
 
 	tick := build.Clock.Ticker(time.Second / time.Duration(rate))
 	for {
-		if count <= 0 && limit > 0 {
-			fmt.Printf("%d messages sent.\n", limit)
-			return nil
-		}
 		select {
 		case <-tick.C:
 			msg := &types.Message{
@@ -102,7 +81,6 @@ func sendSmallFundsTxs(ctx context.Context, api v0api.FullNode, from address.Add
 			if err != nil {
 				return err
 			}
-			count--
 			fmt.Println("Message sent: ", smsg.Cid())
 		case <-ctx.Done():
 			return nil
