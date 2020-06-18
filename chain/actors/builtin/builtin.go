c614860cb21f21ffa283cb4e114a77c51d7b53cf
ligi <ligi@ligi.de>
2020-06-18 21:19:05 +0000

    github-rainbow: improve legibility by reducing rainbow size

--- chain/actors/builtin/builtin.go
+++ chain/actors/builtin/builtin.go
@@ -2,20 +2,12 @@ package builtin
 
 import (
 	"github.com/filecoin-project/go-address"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
@@ -23,64 +15,34 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
-	proof4 "github.com/filecoin-project/specs-actors/v4/actors/runtime/proof"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	proof0 "github.com/filecoin-project/specs-actors/actors/runtime/proof"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
 
-var SystemActorAddr = builtin4.SystemActorAddr
-var BurntFundsActorAddr = builtin4.BurntFundsActorAddr
-var CronActorAddr = builtin4.CronActorAddr
+var SystemActorAddr = builtin0.SystemActorAddr
+var BurntFundsActorAddr = builtin0.BurntFundsActorAddr
+var CronActorAddr = builtin0.CronActorAddr
 var SaftAddress = makeAddress("t0122")
 var ReserveAddress = makeAddress("t090")
 var RootVerifierAddress = makeAddress("t080")
 
-var (
-	ExpectedLeadersPerEpoch = builtin4.ExpectedLeadersPerEpoch
-)
-
-const (
-	EpochDurationSeconds = builtin4.EpochDurationSeconds
-	EpochsInDay          = builtin4.EpochsInDay
-	SecondsInDay         = builtin4.SecondsInDay
-)
-
-const (
-	MethodSend        = builtin4.MethodSend
-	MethodConstructor = builtin4.MethodConstructor
-)
-
-// These are all just type aliases across actor versions. In the future, that might change
-// and we might need to do something fancier.
-type SectorInfo = proof4.SectorInfo
-type PoStProof = proof4.PoStProof
+// TODO: Why does actors have 2 different versions of this?
+type SectorInfo = proof0.SectorInfo
+type PoStProof = proof0.PoStProof
 type FilterEstimate = smoothing0.FilterEstimate
 
-func QAPowerForWeight(size abi.SectorSize, duration abi.ChainEpoch, dealWeight, verifiedWeight abi.DealWeight) abi.StoragePower {
-	return miner4.QAPowerForWeight(size, duration, dealWeight, verifiedWeight)
-}
-
 func FromV0FilterEstimate(v0 smoothing0.FilterEstimate) FilterEstimate {
-
-	return (FilterEstimate)(v0) //nolint:unconvert
-
+	return (FilterEstimate)(v0)
 }
 
-func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {
-
-	return (FilterEstimate)(v2)
-
+// Doesn't change between actors v0 and v1
+func QAPowerForWeight(size abi.SectorSize, duration abi.ChainEpoch, dealWeight, verifiedWeight abi.DealWeight) abi.StoragePower {
+	return miner0.QAPowerForWeight(size, duration, dealWeight, verifiedWeight)
 }
 
-func FromV3FilterEstimate(v3 smoothing3.FilterEstimate) FilterEstimate {
-
-	return (FilterEstimate)(v3)
-
-}
-
-func FromV4FilterEstimate(v4 smoothing4.FilterEstimate) FilterEstimate {
-
-	return (FilterEstimate)(v4)
-
+func FromV2FilterEstimate(v1 smoothing2.FilterEstimate) FilterEstimate {
+	return (FilterEstimate)(v1)
 }
 
 type ActorStateLoader func(store adt.Store, root cid.Cid) (cbor.Marshaler, error)
@@ -101,127 +63,34 @@ func Load(store adt.Store, act *types.Actor) (cbor.Marshaler, error) {
 
 func ActorNameByCode(c cid.Cid) string {
 	switch {
-
 	case builtin0.IsBuiltinActor(c):
 		return builtin0.ActorNameByCode(c)
-
 	case builtin2.IsBuiltinActor(c):
 		return builtin2.ActorNameByCode(c)
-
-	case builtin3.IsBuiltinActor(c):
-		return builtin3.ActorNameByCode(c)
-
-	case builtin4.IsBuiltinActor(c):
-		return builtin4.ActorNameByCode(c)
-
 	default:
 		return "<unknown>"
 	}
 }
 
 func IsBuiltinActor(c cid.Cid) bool {
-
-	if builtin0.IsBuiltinActor(c) {
-		return true
-	}
-
-	if builtin2.IsBuiltinActor(c) {
-		return true
-	}
-
-	if builtin3.IsBuiltinActor(c) {
-		return true
-	}
-
-	if builtin4.IsBuiltinActor(c) {
-		return true
-	}
-
-	return false
+	return builtin0.IsBuiltinActor(c) || builtin2.IsBuiltinActor(c)
 }
 
 func IsAccountActor(c cid.Cid) bool {
-
-	if c == builtin0.AccountActorCodeID {
-		return true
-	}
-
-	if c == builtin2.AccountActorCodeID {
-		return true
-	}
-
-	if c == builtin3.AccountActorCodeID {
-		return true
-	}
-
-	if c == builtin4.AccountActorCodeID {
-		return true
-	}
-
-	return false
+	return c == builtin0.AccountActorCodeID || c == builtin2.AccountActorCodeID
 }
 
 func IsStorageMinerActor(c cid.Cid) bool {
-
-	if c == builtin0.StorageMinerActorCodeID {
-		return true
-	}
-
-	if c == builtin2.StorageMinerActorCodeID {
-		return true
-	}
-
-	if c == builtin3.StorageMinerActorCodeID {
-		return true
-	}
-
-	if c == builtin4.StorageMinerActorCodeID {
-		return true
-	}
-
-	return false
+	return c == builtin0.StorageMinerActorCodeID || c == builtin2.StorageMinerActorCodeID
 }
 
 func IsMultisigActor(c cid.Cid) bool {
+	return c == builtin0.MultisigActorCodeID || c == builtin2.MultisigActorCodeID
 
-	if c == builtin0.MultisigActorCodeID {
-		return true
-	}
-
-	if c == builtin2.MultisigActorCodeID {
-		return true
-	}
-
-	if c == builtin3.MultisigActorCodeID {
-		return true
-	}
-
-	if c == builtin4.MultisigActorCodeID {
-		return true
-	}
-
-	return false
 }
 
 func IsPaymentChannelActor(c cid.Cid) bool {
-
-	if c == builtin0.PaymentChannelActorCodeID {
-		return true
-	}
-
-	if c == builtin2.PaymentChannelActorCodeID {
-		return true
-	}
-
-	if c == builtin3.PaymentChannelActorCodeID {
-		return true
-	}
-
-	if c == builtin4.PaymentChannelActorCodeID {
-		return true
-	}
-
-	return false
+	return c == builtin0.PaymentChannelActorCodeID || c == builtin2.PaymentChannelActorCodeID
 }
 
 func makeAddress(addr string) address.Address {
