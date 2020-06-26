34ecef7083347dab2836d2878dc8b2b3b0e6f251
joshua <joshua@yottadb.com>
2020-06-26 13:47:47 +0000

    Revert license

--- cmd/lotus-seal-worker/rpc.go
+++ cmd/lotus-seal-worker/rpc.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"context"
-	"sync/atomic"
 
-	"github.com/google/uuid"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	"github.com/filecoin-project/specs-storage/storage"
+
 	"github.com/filecoin-project/lotus/build"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type worker struct {
@@ -21,12 +18,10 @@ type worker struct {
 
 	localStore *stores.Local
 	ls         stores.LocalStorage
-
-	disabled int64
 }
 
-func (w *worker) Version(context.Context) (api.Version, error) {
-	return api.WorkerAPIVersion0, nil
+func (w *worker) Version(context.Context) (build.Version, error) {
+	return build.WorkerAPIVersion, nil
 }
 
 func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
@@ -48,38 +43,4 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
 	return nil
 }
 
-func (w *worker) SetEnabled(ctx context.Context, enabled bool) error {
-	disabled := int64(1)
-	if enabled {
-		disabled = 0
-	}
-	atomic.StoreInt64(&w.disabled, disabled)
-	return nil
-}
-
-func (w *worker) Enabled(ctx context.Context) (bool, error) {
-	return atomic.LoadInt64(&w.disabled) == 0, nil
-}
-
-func (w *worker) WaitQuiet(ctx context.Context) error {
-	w.LocalWorker.WaitQuiet() // uses WaitGroup under the hood so no ctx :/
-	return nil
-}
-
-func (w *worker) ProcessSession(ctx context.Context) (uuid.UUID, error) {
-	return w.LocalWorker.Session(ctx)
-}
-
-func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {
-	if atomic.LoadInt64(&w.disabled) == 1 {
-		return uuid.UUID{}, xerrors.Errorf("worker disabled")
-	}
-
-	return w.LocalWorker.Session(ctx)
-}
-
-func (w *worker) Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) {
-	return build.OpenRPCDiscoverJSON_Worker(), nil
-}
-
-var _ storiface.WorkerCalls = &worker{}
+var _ storage.Sealer = &worker{}
