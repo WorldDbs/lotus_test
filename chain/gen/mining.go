63ff26870a854520e099b6e07e63f2fc78a3c220
nicksavers <nicksavers@gmail.com>
2020-06-28 06:39:53 +0000

    Comments on sftpconnection class

--- chain/gen/mining.go
+++ chain/gen/mining.go
@@ -4,18 +4,21 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/crypto"
-	blockadt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cid "github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/vm"
+	"github.com/filecoin-project/lotus/lib/sigs/bls"
 )
 
-func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet, bt *api.BlockTemplate) (*types.FullBlock, error) {
+func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.WalletAPI, bt *api.BlockTemplate) (*types.FullBlock, error) {
 
 	pts, err := sm.ChainStore().LoadTipSet(bt.Parents)
 	if err != nil {
@@ -27,12 +30,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		return nil, xerrors.Errorf("failed to load tipset state: %w", err)
 	}
 
-	_, lbst, err := stmgr.GetLookbackTipSetForRound(ctx, sm, pts, bt.Epoch)
-	if err != nil {
-		return nil, xerrors.Errorf("getting lookback miner actor state: %w", err)
-	}
-
-	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, lbst, bt.Miner)
+	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, st, bt.Miner)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to get miner worker: %w", err)
 	}
@@ -79,7 +77,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		}
 	}
 
-	store := sm.ChainStore().ActorStore(ctx)
+	store := sm.ChainStore().Store(ctx)
 	blsmsgroot, err := toArray(store, blsMsgCids)
 	if err != nil {
 		return nil, xerrors.Errorf("building bls amt: %w", err)
@@ -116,12 +114,23 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 	}
 	next.ParentBaseFee = baseFee
 
+	cst := cbor.NewCborStore(sm.ChainStore().Blockstore())
+	tree, err := state.LoadStateTree(cst, st)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to load state tree: %w", err)
+	}
+
+	waddr, err := vm.ResolveToKeyAddr(tree, cst, worker)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to resolve miner address to key address: %w", err)
+	}
+
 	nosigbytes, err := next.SigningBytes()
 	if err != nil {
 		return nil, xerrors.Errorf("failed to get signing bytes for block: %w", err)
 	}
 
-	sig, err := w.WalletSign(ctx, worker, nosigbytes, api.MsgMeta{
+	sig, err := w.WalletSign(ctx, waddr, nosigbytes, api.MsgMeta{
 		Type: api.MTBlock,
 	})
 	if err != nil {
@@ -140,34 +149,40 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 }
 
 func aggregateSignatures(sigs []crypto.Signature) (*crypto.Signature, error) {
-	sigsS := make([]ffi.Signature, len(sigs))
+	sigsS := make([][]byte, len(sigs))
 	for i := 0; i < len(sigs); i++ {
-		copy(sigsS[i][:], sigs[i].Data[:ffi.SignatureBytes])
+		sigsS[i] = sigs[i].Data
 	}
 
-	aggSig := ffi.Aggregate(sigsS)
-	if aggSig == nil {
+	aggregator := new(bls.AggregateSignature).AggregateCompressed(sigsS)
+	if aggregator == nil {
 		if len(sigs) > 0 {
 			return nil, xerrors.Errorf("bls.Aggregate returned nil with %d signatures", len(sigs))
 		}
-
-		zeroSig := ffi.CreateZeroSignature()
 
 		// Note: for blst this condition should not happen - nil should not
 		// be returned
 		return &crypto.Signature{
 			Type: crypto.SigTypeBLS,
-			Data: zeroSig[:],
+			Data: new(bls.Signature).Compress(),
 		}, nil
 	}
+	aggSigAff := aggregator.ToAffine()
+	if aggSigAff == nil {
+		return &crypto.Signature{
+			Type: crypto.SigTypeBLS,
+			Data: new(bls.Signature).Compress(),
+		}, nil
+	}
+	aggSig := aggSigAff.Compress()
 	return &crypto.Signature{
 		Type: crypto.SigTypeBLS,
-		Data: aggSig[:],
+		Data: aggSig,
 	}, nil
 }
 
-func toArray(store blockadt.Store, cids []cid.Cid) (cid.Cid, error) {
-	arr := blockadt.MakeEmptyArray(store)
+func toArray(store adt.Store, cids []cid.Cid) (cid.Cid, error) {
+	arr := adt.MakeEmptyArray(store)
 	for i, c := range cids {
 		oc := cbg.CborCid(c)
 		if err := arr.Set(uint64(i), &oc); err != nil {
