4df6a10e73d477f5e0912a2d723a7e4c14ca7e5f
fjl <fjl@ethereum.org>
2020-06-30 16:31:15 +0000

    Added specs for PostGIS geography types

--- cmd/lotus-seal-worker/storage.go
+++ cmd/lotus-seal-worker/storage.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/docker/go-units"
 	"github.com/google/uuid"
 	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
@@ -47,10 +46,6 @@ var storageAttachCmd = &cli.Command{
 			Name:  "store",
 			Usage: "(for init) use path for long-term storage",
 		},
-		&cli.StringFlag{
-			Name:  "max-storage",
-			Usage: "(for init) limit storage space for sectors (expensive for very large paths!)",
-		},
 	},
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetWorkerAPI(cctx)
@@ -84,20 +79,11 @@ var storageAttachCmd = &cli.Command{
 				return err
 			}
 
-			var maxStor int64
-			if cctx.IsSet("max-storage") {
-				maxStor, err = units.RAMInBytes(cctx.String("max-storage"))
-				if err != nil {
-					return xerrors.Errorf("parsing max-storage: %w", err)
-				}
-			}
-
 			cfg := &stores.LocalStorageMeta{
-				ID:         stores.ID(uuid.New().String()),
-				Weight:     cctx.Uint64("weight"),
-				CanSeal:    cctx.Bool("seal"),
-				CanStore:   cctx.Bool("store"),
-				MaxStorage: uint64(maxStor),
+				ID:       stores.ID(uuid.New().String()),
+				Weight:   cctx.Uint64("weight"),
+				CanSeal:  cctx.Bool("seal"),
+				CanStore: cctx.Bool("store"),
 			}
 
 			if !(cfg.CanStore || cfg.CanSeal) {
