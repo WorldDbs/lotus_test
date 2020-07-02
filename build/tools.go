f1e6c7a73bab20ab3a2aef0e3b03857ab485a70a
why <why@ipfs.io>
2020-07-02 04:38:34 +0000

    Licensing Update

--- build/tools.go
+++ build/tools.go
@@ -3,8 +3,5 @@
 package build
 
 import (
-	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"
 )
