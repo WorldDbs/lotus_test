c52392623409c05d063d4fd9bc684f492dbdb8ab
greg <greg@colvin.org>
2020-07-02 12:59:50 +0000

    Reduced output for some tx in reorg test

--- genesis/types.go
+++ genesis/types.go
@@ -5,10 +5,9 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
-
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 )
 
 type ActorType string
@@ -22,7 +21,7 @@ type PreSeal struct {
 	CommR     cid.Cid
 	CommD     cid.Cid
 	SectorID  abi.SectorNumber
-	Deal      market2.DealProposal
+	Deal      market.DealProposal
 	ProofType abi.RegisteredSealProof
 }
 
