4d1fda8cdd5a31e107beafaebe5b088f77f3adba
yuvalalaluf <yuvalalaluf@gmail.com>
2020-07-02 20:51:57 +0000

    Create panel_customization

--- build/genesis.go
+++ build/genesis.go
@@ -14,7 +14,7 @@ func MaybeGenesis() []byte {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
-	genBytes, err := builtinGen.Bytes(GenesisFile)
+	genBytes, err := builtinGen.Bytes("devnet.car")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
