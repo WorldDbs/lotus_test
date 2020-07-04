1f8fb27e349482a030c63b0d61c9dd65913a82bc
nagydani <nagydani@epointsystem.org>
2020-07-04 05:33:48 +0000

    Merge "Add IPA CoreOS Imagebuilder to check + gate"

--- chain/actors/builtin/miner/miner.go
+++ chain/actors/builtin/miner/miner.go
@@ -2,7 +2,6 @@ package miner
 
 import (
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/network"
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -18,42 +17,21 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
-	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 func init() {
-
 	builtin.RegisterActorState(builtin0.StorageMinerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
 	builtin.RegisterActorState(builtin2.StorageMinerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-
-	builtin.RegisterActorState(builtin3.StorageMinerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)
-	})
-
-	builtin.RegisterActorState(builtin4.StorageMinerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)
-	})
-
 }
 
-var Methods = builtin4.MethodsMiner
-
-// Unchanged between v0, v2, v3, and v4 actors
+// Unchanged between v0 and v2 actors
 var WPoStProvingPeriod = miner0.WPoStProvingPeriod
 var WPoStPeriodDeadlines = miner0.WPoStPeriodDeadlines
 var WPoStChallengeWindow = miner0.WPoStChallengeWindow
@@ -62,26 +40,12 @@ var FaultDeclarationCutoff = miner0.FaultDeclarationCutoff
 
 const MinSectorExpiration = miner0.MinSectorExpiration
 
-// Not used / checked in v0
-// TODO: Abstract over network versions
-var DeclarationsMax = miner2.DeclarationsMax
-var AddressedSectorsMax = miner2.AddressedSectorsMax
-
-func Load(store adt.Store, act *types.Actor) (State, error) {
+func Load(store adt.Store, act *types.Actor) (st State, err error) {
 	switch act.Code {
-
 	case builtin0.StorageMinerActorCodeID:
 		return load0(store, act.Head)
-
 	case builtin2.StorageMinerActorCodeID:
 		return load2(store, act.Head)
-
-	case builtin3.StorageMinerActorCodeID:
-		return load3(store, act.Head)
-
-	case builtin4.StorageMinerActorCodeID:
-		return load4(store, act.Head)
-
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
@@ -111,10 +75,8 @@ type State interface {
 	DeadlinesChanged(State) (bool, error)
 
 	Info() (MinerInfo, error)
-	MinerInfoChanged(State) (bool, error)
 
 	DeadlineInfo(epoch abi.ChainEpoch) (*dline.Info, error)
-	DeadlineCronActive() (bool, error)
 
 	// Diff helpers. Used by Diff* functions internally.
 	sectors() (adt.Array, error)
@@ -126,10 +88,9 @@ type State interface {
 type Deadline interface {
 	LoadPartition(idx uint64) (Partition, error)
 	ForEachPartition(cb func(idx uint64, part Partition) error) error
-	PartitionsPoSted() (bitfield.BitField, error)
+	PostSubmissions() (bitfield.BitField, error)
 
 	PartitionsChanged(Deadline) (bool, error)
-	DisputableProofCount() (uint64, error)
 }
 
 type Partition interface {
@@ -173,60 +134,6 @@ type DeclareFaultsParams = miner0.DeclareFaultsParams
 type DeclareFaultsRecoveredParams = miner0.DeclareFaultsRecoveredParams
 type SubmitWindowedPoStParams = miner0.SubmitWindowedPoStParams
 type ProveCommitSectorParams = miner0.ProveCommitSectorParams
-type DisputeWindowedPoStParams = miner3.DisputeWindowedPoStParams
-
-func PreferredSealProofTypeFromWindowPoStType(nver network.Version, proof abi.RegisteredPoStProof) (abi.RegisteredSealProof, error) {
-	// We added support for the new proofs in network version 7, and removed support for the old
-	// ones in network version 8.
-	if nver < network.Version7 {
-		switch proof {
-		case abi.RegisteredPoStProof_StackedDrgWindow2KiBV1:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
-		case abi.RegisteredPoStProof_StackedDrgWindow8MiBV1:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
-		case abi.RegisteredPoStProof_StackedDrgWindow512MiBV1:
-			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
-		case abi.RegisteredPoStProof_StackedDrgWindow32GiBV1:
-			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
-		case abi.RegisteredPoStProof_StackedDrgWindow64GiBV1:
-			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
-		default:
-			return -1, xerrors.Errorf("unrecognized window post type: %d", proof)
-		}
-	}
-
-	switch proof {
-	case abi.RegisteredPoStProof_StackedDrgWindow2KiBV1:
-		return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow8MiBV1:
-		return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow512MiBV1:
-		return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow32GiBV1:
-		return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow64GiBV1:
-		return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
-	default:
-		return -1, xerrors.Errorf("unrecognized window post type: %d", proof)
-	}
-}
-
-func WinningPoStProofTypeFromWindowPoStProofType(nver network.Version, proof abi.RegisteredPoStProof) (abi.RegisteredPoStProof, error) {
-	switch proof {
-	case abi.RegisteredPoStProof_StackedDrgWindow2KiBV1:
-		return abi.RegisteredPoStProof_StackedDrgWinning2KiBV1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow8MiBV1:
-		return abi.RegisteredPoStProof_StackedDrgWinning8MiBV1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow512MiBV1:
-		return abi.RegisteredPoStProof_StackedDrgWinning512MiBV1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow32GiBV1:
-		return abi.RegisteredPoStProof_StackedDrgWinning32GiBV1, nil
-	case abi.RegisteredPoStProof_StackedDrgWindow64GiBV1:
-		return abi.RegisteredPoStProof_StackedDrgWinning64GiBV1, nil
-	default:
-		return -1, xerrors.Errorf("unknown proof type %d", proof)
-	}
-}
 
 type MinerInfo struct {
 	Owner                      address.Address   // Must be an ID-address.
@@ -236,7 +143,7 @@ type MinerInfo struct {
 	WorkerChangeEpoch          abi.ChainEpoch
 	PeerId                     *peer.ID
 	Multiaddrs                 []abi.Multiaddrs
-	WindowPoStProofType        abi.RegisteredPoStProof
+	SealProofType              abi.RegisteredSealProof
 	SectorSize                 abi.SectorSize
 	WindowPoStPartitionSectors uint64
 	ConsensusFaultElapsed      abi.ChainEpoch
