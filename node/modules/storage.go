022c685c374aaa3a3ee22eeb2a26a309a96e05ec
greg <greg@colvin.org>
2020-07-06 21:24:14 +0000

    Removed overflow rule

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -2,15 +2,12 @@ package modules
 
 import (
 	"context"
-	"path/filepath"
 
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
@@ -30,30 +27,11 @@ func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 	return lr.KeyStore()
 }
 
-func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)
-		mds, err := r.Datastore(ctx, "/metadata")
-		if err != nil {
-			return nil, err
-		}
-
-		var logdir string
-		if !disableLog {
-			logdir = filepath.Join(r.Path(), "kvlog/metadata")
-		}
-
-		bds, err := backupds.Wrap(mds, logdir)
-		if err != nil {
-			return nil, xerrors.Errorf("opening backupds: %w", err)
-		}
-
-		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {
-				return bds.CloseLog()
-			},
-		})
-
-		return bds, nil
+func Datastore(r repo.LockedRepo) (dtypes.MetadataDS, error) {
+	mds, err := r.Datastore("/metadata")
+	if err != nil {
+		return nil, err
 	}
+
+	return backupds.Wrap(mds), nil
 }
