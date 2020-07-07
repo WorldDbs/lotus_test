1c126bd1246aaad5b58ee25a52a0d5fcae4df5af
alessio <alessio@tendermint.com>
2020-07-07 15:32:04 +0000

    fix(version): update runtime

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -12,27 +12,27 @@ import (
 )
 
 func TestMedian(t *testing.T) {
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(5), medianGasPremium([]gasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]gasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]gasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(25), medianGasPremium([]gasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]gasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
