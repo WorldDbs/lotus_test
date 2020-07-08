2b059aba25cab1f1a76d5ae9fcd2ddbf8e19a75d
julia <julia@jvns.ca>
2020-07-08 10:38:30 +0000

    Merge branch 'master' into create-start-page

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -5,37 +5,79 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
-	m.inputLk.Lock()
-	defer m.inputLk.Unlock()
+func (m *Sealing) pledgeSector(ctx context.Context, sectorID abi.SectorID, existingPieceSizes []abi.UnpaddedPieceSize, sizes ...abi.UnpaddedPieceSize) ([]abi.PieceInfo, error) {
+	if len(sizes) == 0 {
+		return nil, nil
+	}
 
+	log.Infof("Pledge %d, contains %+v", sectorID, existingPieceSizes)
+
+	out := make([]abi.PieceInfo, len(sizes))
+	for i, size := range sizes {
+		ppi, err := m.sealer.AddPiece(ctx, sectorID, existingPieceSizes, size, NewNullReader(size))
+		if err != nil {
+			return nil, xerrors.Errorf("add piece: %w", err)
+		}
+
+		existingPieceSizes = append(existingPieceSizes, size)
+
+		out[i] = ppi
+	}
+
+	return out, nil
+}
+
+func (m *Sealing) PledgeSector() error {
 	cfg, err := m.getConfig()
 	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
+		return xerrors.Errorf("getting config: %w", err)
 	}
 
 	if cfg.MaxSealingSectors > 0 {
-		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+		if m.stats.curSealing() > cfg.MaxSealingSectors {
+			return xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
 	}
 
-	spt, err := m.currentSealProof(ctx)
-	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
-	}
+	go func() {
+		ctx := context.TODO() // we can't use the context from command which invokes
+		// this, as we run everything here async, and it's cancelled when the
+		// command exits
 
-	sid, err := m.createSector(ctx, cfg, spt)
-	if err != nil {
-		return storage.SectorRef{}, err
-	}
+		size := abi.PaddedPieceSize(m.sealer.SectorSize()).Unpadded()
 
-	log.Infof("Creating CC sector %d", sid)
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
-		ID:         sid,
-		SectorType: spt,
-	})
+		sid, err := m.sc.Next()
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+		err = m.sealer.NewSector(ctx, m.minerSector(sid))
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+
+		pieces, err := m.pledgeSector(ctx, m.minerSector(sid), []abi.UnpaddedPieceSize{}, size)
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+
+		ps := make([]Piece, len(pieces))
+		for idx := range ps {
+			ps[idx] = Piece{
+				Piece:    pieces[idx],
+				DealInfo: nil,
+			}
+		}
+
+		if err := m.newSectorCC(sid, ps); err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+	}()
+	return nil
 }
