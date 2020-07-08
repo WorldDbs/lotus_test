bf83709b7ac1535f725c4fb5cd5239be2507483b
alan.shaw <alan.shaw@protocol.ai>
2020-07-08 12:36:14 +0000

    Create skripsi.sebarin.com-1455556205074

--- cmd/lotus-stats/setup.bash
+++ cmd/lotus-stats/setup.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-GRAFANA_HOST="http://localhost:13000"
+GRAFANA_HOST="localhost:3000"
 
 curl -s -XPOST http://admin:admin@$GRAFANA_HOST/api/datasources -H 'Content-Type: text/json' --data-binary @- > /dev/null << EOF
 {
-  "name":"filecoin-ntwk-localstats",
+  "name":"InfluxDB",
   "type":"influxdb",
   "database":"lotus",
   "url": "http://influxdb:8086",
