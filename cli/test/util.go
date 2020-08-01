c3d43975461a61c85e31052b2748619982b77c86
zaq1tomo <zaq1tomo@gmail.com>
2020-08-01 09:50:09 +0000

    create test workflow

--- cli/test/util.go
+++ cli/test/util.go
@@ -9,6 +9,4 @@ func QuietMiningLogs() {
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
