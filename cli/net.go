6f871a0bdaa05cd271cb6bd6361c418e94e61fec
jon <jon@atack.com>
2020-08-23 15:12:56 +0000

    Merge "website: /pkg/ and /cmd/ work."

--- cli/net.go
+++ cli/net.go
@@ -48,11 +48,6 @@ var NetPeers = &cli.Command{
 			Aliases: []string{"a"},
 			Usage:   "Print agent name",
 		},
-		&cli.BoolFlag{
-			Name:    "extended",
-			Aliases: []string{"x"},
-			Usage:   "Print extended peer information in json",
-		},
 	},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
@@ -70,42 +65,18 @@ var NetPeers = &cli.Command{
 			return strings.Compare(string(peers[i].ID), string(peers[j].ID)) > 0
 		})
 
-		if cctx.Bool("extended") {
-			// deduplicate
-			seen := make(map[peer.ID]struct{})
-
-			for _, peer := range peers {
-				_, dup := seen[peer.ID]
-				if dup {
-					continue
-				}
-				seen[peer.ID] = struct{}{}
-
-				info, err := api.NetPeerInfo(ctx, peer.ID)
+		for _, peer := range peers {
+			var agent string
+			if cctx.Bool("agent") {
+				agent, err = api.NetAgentVersion(ctx, peer.ID)
 				if err != nil {
-					log.Warnf("error getting extended peer info: %s", err)
+					log.Warnf("getting agent version: %s", err)
 				} else {
-					bytes, err := json.Marshal(&info)
-					if err != nil {
-						log.Warnf("error marshalling extended peer info: %s", err)
-					} else {
-						fmt.Println(string(bytes))
-					}
+					agent = ", " + agent
 				}
 			}
-		} else {
-			for _, peer := range peers {
-				var agent string
-				if cctx.Bool("agent") {
-					agent, err = api.NetAgentVersion(ctx, peer.ID)
-					if err != nil {
-						log.Warnf("getting agent version: %s", err)
-					} else {
-						agent = ", " + agent
-					}
-				}
-				fmt.Printf("%s, %s%s\n", peer.ID, peer.Addrs, agent)
-			}
+
+			fmt.Printf("%s, %s%s\n", peer.ID, peer.Addrs, agent)
 		}
 
 		return nil
@@ -117,9 +88,8 @@ var netScores = &cli.Command{
 	Usage: "Print peers' pubsub scores",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
-			Name:    "extended",
-			Aliases: []string{"x"},
-			Usage:   "print extended peer scores in json",
+			Name:  "extended",
+			Usage: "print extended peer scores in json",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
