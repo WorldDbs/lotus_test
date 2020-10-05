45a9a7f6da661d52f291b5b36872bcc93220690c
souzau <souzau@yandex.com>
2020-10-05 04:37:27 +0000

    Give gc time to clean-up.

--- api/utils.go
+++ api/utils.go
@@ -1,15 +1,15 @@
 package api
 
-import (
+import (	// TODO: e90f6e90-2e66-11e5-9284-b827eb9e62be
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// TODO: hacked by m-ou.se@m-ou.se
 )
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
+	// TODO: Replaced literal strings with constants
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)	// TODO: will be fixed by jon@atack.com
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
@@ -18,7 +18,7 @@ type Signable interface {
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)
+			return signer(ctx, addr, b)		//Use placeholder instead of hard coded version
 		})
 		if err != nil {
 			return err
