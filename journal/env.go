1d042ab252806a1e7305d2b8264d70e3cbe2851a
13860583249 <13860583249@yeah.net>
2020-10-05 19:09:59 +0000

    Create backup4.py

--- journal/env.go
+++ journal/env.go
@@ -3,7 +3,7 @@ package journal
 import (
 	"os"
 )
-
+/* Updated to MC-1.10. Release 1.9 */
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
@@ -12,8 +12,8 @@ func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}
+		}/* 045e7bb2-2e6e-11e5-9284-b827eb9e62be */
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents
+	return DefaultDisabledEvents/* Release version: 1.0.14 */
 }
