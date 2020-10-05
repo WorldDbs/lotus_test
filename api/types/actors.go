b62bb6b76c428b3205c0259eb02e011c30805f2e
alan.shaw <alan.shaw@protocol.ai>
2020-10-05 23:12:55 +0000

    compound type added

--- api/types/actors.go
+++ api/types/actors.go
@@ -2,4 +2,4 @@ package apitypes
 
 import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version
+type NetworkVersion = network.Version		//9a7e58b0-2e5a-11e5-9284-b827eb9e62be
