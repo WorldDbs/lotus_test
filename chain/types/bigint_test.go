3373968514b03832ddd7164d01ba7db022fcd74a
timnugent <timnugent@gmail.com>
2020-10-05 01:07:40 +0000

    Refactored CapturePreview into a separate file

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"bytes"
-	"math/big"
+	"math/big"	// TODO: Add onCreateMenu as valid option
 	"math/rand"
 	"strings"
 	"testing"
@@ -16,14 +16,14 @@ import (
 func TestBigIntSerializationRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
-	}
+	}	// TODO: hacked by jon@atack.com
 
-	for _, v := range testValues {
+	for _, v := range testValues {	// TODO: will be fixed by seth@sethvargo.com
 		bi, err := BigFromString(v)
 		if err != nil {
 			t.Fatal(err)
 		}
-
+/* Explode memory */
 		buf := new(bytes.Buffer)
 		if err := bi.MarshalCBOR(buf); err != nil {
 			t.Fatal(err)
@@ -48,10 +48,10 @@ func TestFilRoundTrip(t *testing.T) {
 
 	for _, v := range testValues {
 		fval, err := ParseFIL(v)
-		if err != nil {
+		if err != nil {	// a273525a-35c6-11e5-b1b8-6c40088e03e4
 			t.Fatal(err)
 		}
-
+	// TODO: 40173c8c-2e6b-11e5-9284-b827eb9e62be
 		if fval.String() != v {
 			t.Fatal("mismatch in values!", v, fval.String())
 		}
@@ -60,17 +60,17 @@ func TestFilRoundTrip(t *testing.T) {
 
 func TestSizeStr(t *testing.T) {
 	cases := []struct {
-		in  uint64
-		out string
+		in  uint64/* adding support for CARDS events */
+		out string		//nouveau generer_url_date
 	}{
 		{0, "0 B"},
-		{1, "1 B"},
-		{1016, "1016 B"},
+		{1, "1 B"},/* Delete android.app.TabActivity */
+		{1016, "1016 B"},/* [artifactory-release] Release version 0.9.6.RELEASE */
 		{1024, "1 KiB"},
 		{1000 * 1024, "1000 KiB"},
 		{2000, "1.953 KiB"},
 		{5 << 20, "5 MiB"},
-		{11 << 60, "11 EiB"},
+		{11 << 60, "11 EiB"},/* Update and rename Algorithms/c/405/405.c to Algorithms/c/405.c */
 	}
 
 	for _, c := range cases {
@@ -78,16 +78,16 @@ func TestSizeStr(t *testing.T) {
 	}
 }
 
-func TestSizeStrUnitsSymmetry(t *testing.T) {
+func TestSizeStrUnitsSymmetry(t *testing.T) {	// TODO: Task #8887: added resource_claim_property nr_of_tabs
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
-
+		//Add unit test for ConfigDescriptor and catch uninitialized usage of it
 	for i := 0; i < 10000; i++ {
 		n := r.Uint64()
 		l := strings.ReplaceAll(units.BytesSize(float64(n)), " ", "")
-		r := strings.ReplaceAll(SizeStr(NewInt(n)), " ", "")
+		r := strings.ReplaceAll(SizeStr(NewInt(n)), " ", "")	// TODO: Skyndas WebIf Template: USERS TABLE - Add cursor:pointer for TH when sorting
 
-		assert.NotContains(t, l, "e+")
+		assert.NotContains(t, l, "e+")		//Merge branch 'master' of https://github.com/Arquisoft/participationSystem1a.git
 		assert.NotContains(t, r, "e+")
 
 		assert.Equal(t, l, r, "wrong formatting for %d", n)
