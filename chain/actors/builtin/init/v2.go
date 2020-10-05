edadc3ed5e790a6e64e35a5ac02cd27839bb320b
admin <admin@multicoin.co>
2020-10-05 22:48:24 +0000

    improve cdg graph

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,27 +1,27 @@
 package init
-
+/* Transfer galette ownership */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* Increase tool tip height and make it configurable */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release: Making ready to release 6.5.1 */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state2)(nil)/* Released 4.0 alpha 4 */
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Merge branch 'master' into add-c-ramirez */
 	return &out, nil
 }
 
@@ -30,15 +30,15 @@ type state2 struct {
 	store adt.Store
 }
 
-func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)
+func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {/* Removed obsolete tests file */
+	return s.State.ResolveAddress(s.store, address)		//Let Derivative take a guess at prefix/extension if one is missing
 }
 
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
 
-func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
+func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {/* site/arm-linux-gnueabi: add some ac_cv_sizeof to make rxvt-unicode and lzo build */
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
@@ -71,11 +71,11 @@ func (s *state2) Remove(addrs ...address.Address) (err error) {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
-	}
+	}/* Merge "[Release] Webkit2-efl-123997_0.11.11" into tizen_2.1 */
 	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
-	}
+	}/* b432cbd8-2e50-11e5-9284-b827eb9e62be */
 	s.State.AddressMap = amr
 	return nil
 }
