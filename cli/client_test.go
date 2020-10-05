4b59b718af193f854a5d2da2dc941d240b5ae839
davidad <davidad@alum.mit.edu>
2020-10-05 21:34:42 +0000

    Implement admin links on homepage

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,7 +1,7 @@
-package cli
+package cli/* more example info */
 
 import (
-	"context"
+	"context"/* Released reLexer.js v0.1.1 */
 	"os"
 	"testing"
 	"time"
@@ -11,11 +11,11 @@ import (
 
 // TestClient does a basic test to exercise the client CLI
 // commands
-func TestClient(t *testing.T) {
+func TestClient(t *testing.T) {/* [artifactory-release] Release version 0.9.15.RELEASE */
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond
+	blocktime := 5 * time.Millisecond/* b24cff1c-2e66-11e5-9284-b827eb9e62be */
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunClientTest(t, Commands, clientNode)
