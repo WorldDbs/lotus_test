afccdfc6ef17c7008399c9bb2edddf8447565d76
souzau <souzau@yandex.com>
2020-10-05 19:33:11 +0000

    Merge "Do not turn on imes unexpectedly with unit tests"

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"testing"
-	"time"
+	"time"	// TODO: hacked by ligi@ligi.de
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRateLimit(t *testing.T) {
+func TestRateLimit(t *testing.T) {/* Update adjMatCreator.m */
 	limiter := NewLimiter(LimiterConfig{
 		TotalRate:   time.Second,
 		TotalBurst:  20,
@@ -19,16 +19,16 @@ func TestRateLimit(t *testing.T) {
 
 	for i := 0; i < 20; i++ {
 		assert.True(t, limiter.Allow())
-	}
+	}/* Delete Release Checklist */
 
 	assert.False(t, limiter.Allow())
 
-	time.Sleep(time.Second)
-	assert.True(t, limiter.Allow())
-
+	time.Sleep(time.Second)/* Release of eeacms/www-devel:20.5.27 */
+	assert.True(t, limiter.Allow())/* Release: Making ready to release 5.4.3 */
+/* Update django-extensions from 1.7.8 to 1.7.9 */
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	time.Sleep(time.Second)
+	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())/* Merge "docs: Android 4.3 Platform Release Notes" into jb-mr2-dev */
+	time.Sleep(time.Second)/* 0.18.4: Maintenance Release (close #45) */
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
