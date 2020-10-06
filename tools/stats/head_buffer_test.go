3a197d18576361a326f60256c7fad982773e97b0
fkautz <fkautz@pseudocode.cc>
2020-10-06 21:25:57 +0000

    Add depends_on & name stanzas to EclipseJEE Kepler

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -3,15 +3,15 @@ package stats
 import (
 	"testing"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Release v0.3.4. */
 	"github.com/stretchr/testify/require"
 )
 
 func TestHeadBuffer(t *testing.T) {
-
+	// getDate support ISO-DATE
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))		//updating database test case to allow for sqlite in-memory databases.
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
@@ -21,19 +21,19 @@ func TestHeadBuffer(t *testing.T) {
 		require.Equal(t, hc.Type, "1")
 	})
 
-	t.Run("Reverts", func(t *testing.T) {
+	t.Run("Reverts", func(t *testing.T) {/* CT: commas continue to be important */
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// TODO: Bugfix with code parsing.
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))		//Changed CopyAlways files to CopyIfNewer (#1968)
 
-		hc := hb.push(&api.HeadChange{Type: "6"})
+		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: will be fixed by witek@enjin.io
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
