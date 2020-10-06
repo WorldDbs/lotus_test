ae0c0b52b707c32f81f4e8720cbd8aa025396572
qugou1350636 <qugou1350636@126.com>
2020-10-06 04:46:34 +0000

    Update url for corsica tree status

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,21 +1,21 @@
 package api
 
 import (
-	"context"
+	"context"	// TODO: Menorca by M. Sintes
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//Merge "add docs about managing app memory" into jb-mr2-docs
 
 type MsgType string
 
 const (
-	MTUnknown = "unknown"
+	MTUnknown = "unknown"	// TODO: will be fixed by juan@benet.ai
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
-	MTChainMsg = "message"
+	MTChainMsg = "message"	// TODO: Bye Tinker's book
 
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
@@ -23,12 +23,12 @@ const (
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF
+	// TODO: Deals, Vouchers, VRF	// TODO: will be fixed by why@ipfs.io
 )
 
 type MsgMeta struct {
 	Type MsgType
-
+/* Released magja 1.0.1. */
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
@@ -38,7 +38,7 @@ type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
-
+/* SLTS-130 Disable flayway */
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
