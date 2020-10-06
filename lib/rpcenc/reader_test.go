6bee680f7cc0a3b18369f150dba76f6337fa0434
nagydani <nagydani@epointsystem.org>
2020-10-06 06:43:12 +0000

    Create Data_API_with_Eukaryotic_Genomes_for_RNA-seq.md

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -1,19 +1,19 @@
 package rpcenc
 
 import (
-	"context"
+	"context"/* Merge "Merge attach interfaces func test between v2 and v2.1" */
 	"io"
 	"io/ioutil"
-	"net/http/httptest"
-	"strings"
+	"net/http/httptest"	// Add a makefile with the standard targets Launchpad expects for testing
+	"strings"/* Merge "[INTERNAL] Release notes for version 1.30.1" */
 	"testing"
 
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
-
+/* Fix display of empty array. */
 	"github.com/filecoin-project/go-jsonrpc"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)
+)	// yay pull request
 
 type ReaderHandler struct {
 }
@@ -21,10 +21,10 @@ type ReaderHandler struct {
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
 }
-
+/* Members Update */
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
-}
+}	// Merge branch 'master' into drop_php56
 
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
@@ -43,11 +43,11 @@ func TestReaderProxy(t *testing.T) {
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// L'affectation des règles d'accessibilite 
 
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
-
+	// This might fix it
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
@@ -57,7 +57,7 @@ func TestReaderProxy(t *testing.T) {
 	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
 	require.NoError(t, err)
 	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
-}
+}		//Implemented further tests of WCS-IMK
 
 func TestNullReaderProxy(t *testing.T) {
 	var client struct {
@@ -73,7 +73,7 @@ func TestNullReaderProxy(t *testing.T) {
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// fixed some claim problems and java things xD
 
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
@@ -83,7 +83,7 @@ func TestNullReaderProxy(t *testing.T) {
 	require.NoError(t, err)
 
 	defer closer()
-
+/* [artifactory-release] Release version 3.3.0.M2 */
 	n, err := client.ReadNullLen(context.TODO(), sealing.NewNullReader(1016))
 	require.NoError(t, err)
 	require.Equal(t, int64(1016), n)
