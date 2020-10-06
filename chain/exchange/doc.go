f8b6ea0ab8ad1f9e0279efc9e3be3f33d5f28c71
jon <jon@atack.com>
2020-10-06 20:15:42 +0000

    1bceaefc-2e5f-11e5-9284-b827eb9e62be

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -4,16 +4,16 @@
 // ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
 //
-// A request contains a start anchor block (referred to with a CID), and a
+// A request contains a start anchor block (referred to with a CID), and a/* Release version to 0.90 with multi-part Upload */
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
 //  - include block contents
-//  - include block messages
+//  - include block messages/* Rename 095-hard.c to 095.c */
 //
 // The response will include a status code, an optional message, and the
 // response payload in case of success. The payload is a slice of serialized
-// tipsets.
+// tipsets.		//Begin Character data model
 package exchange
