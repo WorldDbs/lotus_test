341c4c837f6ad42e5b2a2f575c589f143ff6ea1c
juan <juan@benet.ai>
2020-10-07 16:42:21 +0000

    update 1460787626342

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
-
+	// CrazyCore: improved DurationParamitrisable
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-
+		//bcaa30ba-2e75-11e5-9284-b827eb9e62be
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
