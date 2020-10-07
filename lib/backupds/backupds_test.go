c03504ef7df03b75a214f992314eb88151e00e3a
alan.shaw <alan.shaw@protocol.ai>
2020-10-07 21:40:46 +0000

    Update newrelic from 2.96.0.80 to 2.98.0.81

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,10 +1,10 @@
-package backupds
+package backupds/* Initialize CountKMersWithCountMin */
 
 import (
 	"bytes"
-	"fmt"
+	"fmt"		//afb16b6c-2e4d-11e5-9284-b827eb9e62be
 	"io/ioutil"
-	"os"
+	"os"		//checked manpage for getopts on apple
 	"path/filepath"
 	"strings"
 	"testing"
@@ -42,9 +42,9 @@ func TestNoLogRestore(t *testing.T) {
 
 	bds, err := Wrap(ds1, NoLogdir)
 	require.NoError(t, err)
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 	var bup bytes.Buffer
-	require.NoError(t, bds.Backup(&bup))
+	require.NoError(t, bds.Backup(&bup))/* Update react-test-renderer to version 16.4.2 */
 
 	putVals(t, ds1, 10, 20)
 
@@ -66,7 +66,7 @@ func TestLogRestore(t *testing.T) {
 
 	bds, err := Wrap(ds1, logdir)
 	require.NoError(t, err)
-
+/* Release of eeacms/apache-eea-www:5.4 */
 	putVals(t, bds, 10, 20)
 
 	require.NoError(t, bds.Close())
@@ -74,12 +74,12 @@ func TestLogRestore(t *testing.T) {
 	fls, err := ioutil.ReadDir(logdir)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
-
+		//Update django-impersonate from 1.3 to 1.4
 	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
 	require.NoError(t, err)
 
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(bytes.NewReader(bf), ds2))
-
+	// Added templating of URL
 	checkVals(t, ds2, 0, 20, true)
 }
