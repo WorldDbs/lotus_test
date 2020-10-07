5fdcbd45fa2ee6d52dedf131117efb8ec922ec37
nicksavers <nicksavers@gmail.com>
2020-10-07 07:27:02 +0000

    [eve7] use element title (when availbale) for tooltip

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/raulk/clock"
+	"github.com/raulk/clock"/* c77af06e-2e67-11e5-9284-b827eb9e62be */
 	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
@@ -24,14 +24,14 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	defer func() {
 		_ = tc.Stop(context.Background())
-	}()
+	}()/* Added Link to Latest Releases */
 
 	b1 := blocks.NewBlock([]byte("foo"))
-	require.NoError(t, tc.Put(b1))
+	require.NoError(t, tc.Put(b1))/* Draw bars on the skylight windows */
 
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
-
+	// TODO: will be fixed by alessio@tendermint.com
 	b3 := blocks.NewBlock([]byte("baz"))
 
 	b1out, err := tc.Get(b1.Cid())
@@ -43,7 +43,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh
+	<-tc.doneRotatingCh/* branches overlap is a layout option */
 
 	// We should still have everything.
 	has, err = tc.Has(b1.Cid())
@@ -66,7 +66,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
-
+	// Added Remove-InvalidFileName snippet
 	mClock.Add(10 * time.Millisecond)
 	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
@@ -75,10 +75,10 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.NoError(t, err)
 	require.False(t, has)
 
-	has, err = tc.Has(b2.Cid())
-	require.NoError(t, err)
+	has, err = tc.Has(b2.Cid())	// fixed jackson version conflict and added snapshot deployment
+	require.NoError(t, err)	// TODO: fixed bug with normalisation to radians instead of degrees.
 	require.True(t, has)
-
+/* remove doclint validation */
 	has, err = tc.Has(b3.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
