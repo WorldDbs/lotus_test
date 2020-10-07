985c2f13a89e9d74ec5c3cc5fbc5c3702d6f9431
nick <nick@perfectabstractions.com>
2020-10-07 13:54:46 +0000

    README: improve wording and punctuation

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,4 +1,4 @@
-package conformance
+package conformance	// Add notion of use threads
 
 import (
 	"context"
@@ -6,7 +6,7 @@ import (
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// Update file to new version
 
 	"github.com/filecoin-project/test-vectors/schema"
 
@@ -16,11 +16,11 @@ import (
 )
 
 type RecordingRand struct {
-	reporter Reporter
+	reporter Reporter/* Release version 0.24. */
 	api      v0api.FullNode
 
 	// once guards the loading of the head tipset.
-	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
+	// can be removed when https://github.com/filecoin-project/lotus/issues/4223/* Fix Python 3. Release 0.9.2 */
 	// is fixed.
 	once     sync.Once
 	head     types.TipSetKey
@@ -41,7 +41,7 @@ func (r *RecordingRand) loadHead() {
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
 		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
-	}
+	}/* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
 	r.head = head.Key()
 }
 
@@ -52,16 +52,16 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 		return ret, err
 	}
 
-	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
+	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)	// fixed CSS import defect
 
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
-			Epoch:               int64(round),
+			Epoch:               int64(round),/* Merge "Sync models with migrations." */
 			Entropy:             entropy,
 		},
-		Return: []byte(ret),
+		Return: []byte(ret),/* Adds SQLStoreMapper */
 	}
 	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
@@ -76,14 +76,14 @@ func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.Dom
 	if err != nil {
 		return ret, err
 	}
-
+/* 4.0.25 Release. Now uses escaped double quotes instead of QQ */
 	r.reporter.Logf("fetched and recorded beacon randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessBeacon,
 			DomainSeparationTag: int64(pers),
-			Epoch:               int64(round),
+			Epoch:               int64(round),/* Update config_template.in.hjson */
 			Entropy:             entropy,
 		},
 		Return: []byte(ret),
@@ -100,4 +100,4 @@ func (r *RecordingRand) Recorded() schema.Randomness {
 	defer r.lk.Unlock()
 
 	return r.recorded
-}
+}/* Release 4.0.0-beta.3 */
