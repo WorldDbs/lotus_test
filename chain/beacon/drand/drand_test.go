3fe1b4ca5cc6c5e41d5df8e87e3d26f4efd7bb12
boringland <boringland@protonmail.ch>
2020-10-07 16:18:07 +0000

    Merge "Revert "Pass environment variables of proxy to tox""

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -3,23 +3,23 @@ package drand
 import (
 	"os"
 	"testing"
-
+	// TODO: Added more communities
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* fix названия файла с лицензией, публичный ключ перенесён в библиотеки */
 )
 
-func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
+func TestPrintGroupInfo(t *testing.T) {/* add links to server implemetations and demos */
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* Release version 0.3.3 for the Grails 1.0 version. */
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// Create rnd12.p
 	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
 }
