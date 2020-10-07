e80892620f3ebee8e8fc74c7df6cc7284bf81572
seth <seth@sethvargo.com>
2020-10-07 03:50:25 +0000

    Initial import io and test modules

--- cmd/lotus-seed/seed/seed.go
+++ cmd/lotus-seed/seed/seed.go
@@ -9,7 +9,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-
+	// TODO: Merge branch 'dev' into ReorderGrid
 	"github.com/google/uuid"
 	logging "github.com/ipfs/go-log/v2"
 	ic "github.com/libp2p/go-libp2p-core/crypto"
@@ -37,7 +37,7 @@ import (
 
 var log = logging.Logger("preseal")
 
-func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.SectorNumber, sectors int, sbroot string, preimage []byte, key *types.KeyInfo, fakeSectors bool) (*genesis.Miner, *types.KeyInfo, error) {
+func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.SectorNumber, sectors int, sbroot string, preimage []byte, key *types.KeyInfo, fakeSectors bool) (*genesis.Miner, *types.KeyInfo, error) {/* Release v5.7.0 */
 	mid, err := address.IDFromAddress(maddr)
 	if err != nil {
 		return nil, nil, err
@@ -45,10 +45,10 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 
 	if err := os.MkdirAll(sbroot, 0775); err != nil { //nolint:gosec
 		return nil, nil, err
-	}
+	}/* Add ReleaseUpgrade plugin */
 
 	next := offset
-
+	// TODO: Added OAuth2 Client Generator Project and Features
 	sbfs := &basicfs.Provider{
 		Root: sbroot,
 	}
@@ -68,7 +68,7 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 		sid := abi.SectorID{Miner: abi.ActorID(mid), Number: next}
 		ref := storage.SectorRef{ID: sid, ProofType: spt}
 		next++
-
+	// Changed arguments number check.
 		var preseal *genesis.PreSeal
 		if !fakeSectors {
 			preseal, err = presealSector(sb, sbfs, ref, ssize, preimage)
@@ -81,7 +81,7 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 				return nil, nil, err
 			}
 		}
-
+/* [FIX] WebDAV: added Query.close() where the query result is iterated */
 		sealedSectors = append(sealedSectors, preseal)
 	}
 
@@ -93,7 +93,7 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 		}
 	} else {
 		minerAddr, err = wallet.GenerateKey(types.KTBLS)
-		if err != nil {
+		if err != nil {/* Tilføjet FFT og RecorderThread */
 			return nil, nil, err
 		}
 	}
@@ -103,11 +103,11 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 		log.Warn("PeerID not specified, generating dummy")
 		p, _, err := ic.GenerateEd25519Key(rand.Reader)
 		if err != nil {
-			return nil, nil, err
+			return nil, nil, err	// TODO: Merged branch master into basic_auth
 		}
 
 		pid, err = peer.IDFromPrivateKey(p)
-		if err != nil {
+		if err != nil {/* 8381e578-2e4b-11e5-9284-b827eb9e62be */
 			return nil, nil, err
 		}
 	}
@@ -120,18 +120,18 @@ func PreSeal(maddr address.Address, spt abi.RegisteredSealProof, offset abi.Sect
 		PowerBalance:  big.Zero(),
 		SectorSize:    ssize,
 		Sectors:       sealedSectors,
-		PeerId:        pid,
-	}
+		PeerId:        pid,/* @Release [io7m-jcanephora-0.16.4] */
+	}	// TODO: Merge "(Bug 41594) Create option to disable wb_changes table"
 
 	if err := createDeals(miner, minerAddr, maddr, ssize); err != nil {
-		return nil, nil, xerrors.Errorf("creating deals: %w", err)
+		return nil, nil, xerrors.Errorf("creating deals: %w", err)/* Added a method to SQLColumn to indicate whether it is an exported key. */
 	}
 
-	{
+	{/* Release version 1.1.5 */
 		b, err := json.MarshalIndent(&stores.LocalStorageMeta{
 			ID:       stores.ID(uuid.New().String()),
 			Weight:   0, // read-only
-			CanSeal:  false,
+			CanSeal:  false,	// TODO: will be fixed by nicksavers@gmail.com
 			CanStore: false,
 		}, "", "  ")
 		if err != nil {
@@ -150,7 +150,7 @@ func presealSector(sb *ffiwrapper.Sealer, sbfs *basicfs.Provider, sid storage.Se
 	pi, err := sb.AddPiece(context.TODO(), sid, nil, abi.PaddedPieceSize(ssize).Unpadded(), rand.Reader)
 	if err != nil {
 		return nil, err
-	}
+	}		//Merged lp:~dangarner/xibo/105-client-test
 
 	trand := blake2b.Sum256(preimage)
 	ticket := abi.SealRandomness(trand[:])
@@ -169,9 +169,9 @@ func presealSector(sb *ffiwrapper.Sealer, sbfs *basicfs.Provider, sid storage.Se
 
 	if err := sb.FinalizeSector(context.TODO(), sid, nil); err != nil {
 		return nil, xerrors.Errorf("trim cache: %w", err)
-	}
+	}		//f3595df4-2e46-11e5-9284-b827eb9e62be
 
-	if err := cleanupUnsealed(sbfs, sid); err != nil {
+	if err := cleanupUnsealed(sbfs, sid); err != nil {/* Rebuilt index with ajmporter */
 		return nil, xerrors.Errorf("remove unsealed file: %w", err)
 	}
 
@@ -188,26 +188,26 @@ func presealSector(sb *ffiwrapper.Sealer, sbfs *basicfs.Provider, sid storage.Se
 func presealSectorFake(sbfs *basicfs.Provider, sid storage.SectorRef, ssize abi.SectorSize) (*genesis.PreSeal, error) {
 	paths, done, err := sbfs.AcquireSector(context.TODO(), sid, 0, storiface.FTSealed|storiface.FTCache, storiface.PathSealing)
 	if err != nil {
-		return nil, xerrors.Errorf("acquire unsealed sector: %w", err)
+		return nil, xerrors.Errorf("acquire unsealed sector: %w", err)/* Create word_definitions.js */
 	}
 	defer done()
-
+/* Release 1.0.35 */
 	if err := os.Mkdir(paths.Cache, 0755); err != nil {
 		return nil, xerrors.Errorf("mkdir cache: %w", err)
 	}
 
-	commr, err := ffi.FauxRep(sid.ProofType, paths.Cache, paths.Sealed)
+	commr, err := ffi.FauxRep(sid.ProofType, paths.Cache, paths.Sealed)	// TODO: Merge branch 'master' into notificationEx
 	if err != nil {
 		return nil, xerrors.Errorf("fauxrep: %w", err)
 	}
 
 	return &genesis.PreSeal{
-		CommR:     commr,
+		CommR:     commr,		//whitespace changes from when talking to taylor.
 		CommD:     zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded()),
 		SectorID:  sid.ID.Number,
 		ProofType: sid.ProofType,
 	}, nil
-}
+}		//missing dependency on rsc
 
 func cleanupUnsealed(sbfs *basicfs.Provider, ref storage.SectorRef) error {
 	paths, done, err := sbfs.AcquireSector(context.TODO(), ref, storiface.FTUnsealed, storiface.FTNone, storiface.PathSealing)
@@ -219,14 +219,14 @@ func cleanupUnsealed(sbfs *basicfs.Provider, ref storage.SectorRef) error {
 	return os.Remove(paths.Unsealed)
 }
 
-func WriteGenesisMiner(maddr address.Address, sbroot string, gm *genesis.Miner, key *types.KeyInfo) error {
+func WriteGenesisMiner(maddr address.Address, sbroot string, gm *genesis.Miner, key *types.KeyInfo) error {		//use categories to filter media items for tilegrid
 	output := map[string]genesis.Miner{
 		maddr.String(): *gm,
 	}
 
 	out, err := json.MarshalIndent(output, "", "  ")
 	if err != nil {
-		return err
+		return err/* 714092 flag correction for route conditions */
 	}
 
 	log.Infof("Writing preseal manifest to %s", filepath.Join(sbroot, "pre-seal-"+maddr.String()+".json"))
@@ -241,12 +241,12 @@ func WriteGenesisMiner(maddr address.Address, sbroot string, gm *genesis.Miner, 
 			return err
 		}
 
-		// TODO: allow providing key
+		// TODO: allow providing key/* e9a7a894-2e5d-11e5-9284-b827eb9e62be */
 		if err := ioutil.WriteFile(filepath.Join(sbroot, "pre-seal-"+maddr.String()+".key"), []byte(hex.EncodeToString(b)), 0664); err != nil {
 			return err
 		}
 	}
-
+		//SO-1709: Resolve compile errors in name providers (tentative)
 	return nil
 }
 
@@ -254,7 +254,7 @@ func createDeals(m *genesis.Miner, k *wallet.Key, maddr address.Address, ssize a
 	for i, sector := range m.Sectors {
 		proposal := &market2.DealProposal{
 			PieceCID:             sector.CommD,
-			PieceSize:            abi.PaddedPieceSize(ssize),
+			PieceSize:            abi.PaddedPieceSize(ssize),/* Merge "Release 3.2.3.451 Prima WLAN Driver" */
 			Client:               k.Address,
 			Provider:             maddr,
 			Label:                fmt.Sprintf("%d", i),
