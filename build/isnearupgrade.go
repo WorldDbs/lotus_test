bcc6c533fa3ac15c5dfc446edaf328a6dfd60632
vyzo <vyzo@hackzen.org>
2020-10-08 11:49:17 +0000

    Merge branch 'feature/v2.1.1' into hotfix/add-notices-field-to-all-post-types

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -2,7 +2,7 @@ package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* Updated Release with the latest code changes. */
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
