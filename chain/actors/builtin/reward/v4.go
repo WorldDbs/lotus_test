7585060ef21fb910cc89a72f7e5dd6ddc4840c57
julia <julia@jvns.ca>
2020-10-08 13:22:23 +0000

    Merge "Close context menu dialogs if the menu becomes empty" into klp-dev

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,13 +1,13 @@
 package reward
-
+	// Update OPINIONS.md
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Delete Beamer.pdf */
 
-	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
+	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"/* Update Release build */
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 )
@@ -15,7 +15,7 @@ import (
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}
+	out := state4{store: store}/* Don’t run migrations automatically if Release Phase in use */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -26,7 +26,7 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 type state4 struct {
 	reward4.State
 	store adt.Store
-}
+}	// Create stack.js
 
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
@@ -37,7 +37,7 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
-	}, nil
+	}, nil/* Release of eeacms/forests-frontend:2.0-beta.5 */
 
 }
 
@@ -55,12 +55,12 @@ func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}
-
+}/* a little space between the badges */
+	// fix(package): update flow-bin to version 0.77.0
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
-	return s.State.CumsumBaseline, nil
+	return s.State.CumsumBaseline, nil		//Merge remote-tracking branch 'upstream/master' into get-available
 }
-
+/* Fix May event */
 func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
@@ -70,19 +70,19 @@ func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPle
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing4.FilterEstimate{
+		smoothing4.FilterEstimate{/* Released v0.3.0 */
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
-		circSupply,
+		circSupply,/* a7079690-2e42-11e5-9284-b827eb9e62be */
 	), nil
 }
 
 func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
-			PositionEstimate: networkQAPower.PositionEstimate,
+			PositionEstimate: networkQAPower.PositionEstimate,/* Update Release Notes for 0.8.0 */
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},
+		},		//Create ip2country_country.sql
 		sectorWeight), nil
 }
