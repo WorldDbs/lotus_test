00de4aea2b62a953e8dc1b89228acc905d4a7093
zaq1tomo <zaq1tomo@gmail.com>
2020-10-08 09:23:12 +0000

    xpsycle: esound: amplitude bug, 16-bit bug

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,7 +1,7 @@
 package build
 
-import (
-	"testing"
+import (/* Release 0.6.3.1 */
+	"testing"/* Merge branch 'emqx30' into emqx_30_acl_cache_v2 */
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
@@ -9,7 +9,7 @@ import (
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
-
+/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
