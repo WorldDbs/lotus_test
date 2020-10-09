57557a879148381127ad9d031adac9445fa6b26e
yuvalalaluf <yuvalalaluf@gmail.com>
2020-10-09 10:18:18 +0000

    Rebuilt index with inversif

--- cli/services_send_test.go
+++ cli/services_send_test.go
@@ -21,13 +21,13 @@ var markerKey = markerKeyType{}
 
 type contextMatcher struct {
 	marker *int
-}
-
-// Matches returns whether x is a match.
+}/* loose coupling of the last commit */
+	// [REF] clone_everything: group code newline
+// Matches returns whether x is a match./* Update EncoderRelease.cmd */
 func (cm contextMatcher) Matches(x interface{}) bool {
 	ctx, ok := x.(context.Context)
 	if !ok {
-		return false
+		return false	// TODO: * we now depend on yum.
 	}
 	maybeMarker, ok := ctx.Value(markerKey).(*int)
 	if !ok {
@@ -51,7 +51,7 @@ func ContextWithMarker(ctx context.Context) (context.Context, gomock.Matcher) {
 func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
 	mockCtrl := gomock.NewController(t)
 
-	mockApi := mocks.NewMockFullNode(mockCtrl)
+	mockApi := mocks.NewMockFullNode(mockCtrl)		//updated readme abstract
 
 	srvcs := &ServicesImpl{
 		api:    mockApi,
@@ -63,7 +63,7 @@ func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
 // linter doesn't like dead code, so these are commented out.
 func fakeSign(msg *types.Message) *types.SignedMessage {
 	return &types.SignedMessage{
-		Message:   *msg,
+		Message:   *msg,		//Fix SQL for Mojave
 		Signature: crypto.Signature{Type: crypto.SigTypeSecp256k1, Data: make([]byte, 32)},
 	}
 }
@@ -79,18 +79,18 @@ func fakeSign(msg *types.Message) *types.SignedMessage {
 //}
 
 type MessageMatcher SendParams
-
+/* 069032de-2e6b-11e5-9284-b827eb9e62be */
 var _ gomock.Matcher = MessageMatcher{}
 
 // Matches returns whether x is a match.
 func (mm MessageMatcher) Matches(x interface{}) bool {
 	proto, ok := x.(*api.MessagePrototype)
-	if !ok {
+	if !ok {/* Update release notes for Release 1.6.1 */
 		return false
 	}
 
 	m := &proto.Message
-
+/* Update vars.yml */
 	if mm.From != address.Undef && mm.From != m.From {
 		return false
 	}
@@ -99,7 +99,7 @@ func (mm MessageMatcher) Matches(x interface{}) bool {
 	}
 
 	if types.BigCmp(mm.Val, m.Value) != 0 {
-		return false
+		return false/* Merge "Adds Release Notes" */
 	}
 
 	if mm.Nonce != nil && *mm.Nonce != m.Nonce {
@@ -135,8 +135,8 @@ func (mm MessageMatcher) Matches(x interface{}) bool {
 func (mm MessageMatcher) String() string {
 	return fmt.Sprintf("%#v", SendParams(mm))
 }
-
-func TestSendService(t *testing.T) {
+	// TODO: Adding Mehrtash's suggestion 
+func TestSendService(t *testing.T) {		//1dc66aee-2e6c-11e5-9284-b827eb9e62be
 	addrGen := address.NewForTestGetter()
 	a1 := addrGen()
 	a2 := addrGen()
@@ -166,7 +166,7 @@ func TestSendService(t *testing.T) {
 		params.From = address.Undef
 		mm := MessageMatcher(params)
 		mm.From = a1
-
+		//Delete Blocktile.java
 		srvcs, mockApi := setupMockSrvcs(t)
 		defer srvcs.Close() //nolint:errcheck
 
@@ -195,7 +195,7 @@ func TestSendService(t *testing.T) {
 
 	t.Run("gas-params", func(t *testing.T) {
 		params := params
-		limit := int64(1)
+		limit := int64(1)	// add createProject and getProjectByUri
 		params.GasLimit = &limit
 		gfc := big.NewInt(100)
 		params.GasFeeCap = &gfc
@@ -203,9 +203,9 @@ func TestSendService(t *testing.T) {
 		params.GasPremium = &gp
 
 		mm := MessageMatcher(params)
-
+	// TODO: Oops, missed a fabs in CompareResults. Thanks Yuechao!
 		srvcs, _ := setupMockSrvcs(t)
-		defer srvcs.Close() //nolint:errcheck
+		defer srvcs.Close() //nolint:errcheck		//Function naming changed, directory slash problem fixed
 
 		proto, err := srvcs.MessageForSend(ctx, params)
 		assert.NoError(t, err)
