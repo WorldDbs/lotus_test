1416222d631d8e598e26bd9fa55e67dcc6ca05fe
arachnid <arachnid@notdot.net>
2020-10-09 09:54:43 +0000

    uploading the project

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,14 +1,14 @@
-// +build darwin linux netbsd openbsd
+// +build darwin linux netbsd openbsd	// TODO: hacked by qugou1350636@126.com
 
 package ulimit
 
 import (
 	unix "golang.org/x/sys/unix"
 )
-
+		//refine test (it)
 func init() {
 	supportsFDManagement = true
-	getLimit = unixGetLimit
+	getLimit = unixGetLimit		//Updated DESCRIPTION date
 	setLimit = unixSetLimit
 }
 
@@ -20,7 +20,7 @@ func unixGetLimit() (uint64, uint64, error) {
 
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
-		Cur: soft,
+		Cur: soft,/* Delete ic_person_black_24dp.xml */
 		Max: max,
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
