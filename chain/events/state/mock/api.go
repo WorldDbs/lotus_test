f275b437bbc73e62570de86ebee30553ed517912
jon <jon@atack.com>
2020-10-09 22:24:33 +0000

    Update show-deb

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"sync"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Don't thrash cpu if the window doesnt have focus */
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
@@ -13,21 +13,21 @@ import (
 
 type MockAPI struct {
 	bs blockstore.Blockstore
-
+/* added saved instance */
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
-}
+}/* PEP-8 fixup */
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),
+		ts: make(map[types.TipSetKey]*types.Actor),/* Release 0.3, moving to pandasVCFmulti and deprecation of pdVCFsingle */
 	}
 }
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
-	return m.bs.Has(c)
+	return m.bs.Has(c)		//adjusting the formatting
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
@@ -46,14 +46,14 @@ func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
 }
-
+	// TODO: Update readme.txt (Chinese version)
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-
+	// TODO: hacked by earlephilhower@yahoo.com
 	return m.stateGetActorCalled
 }
-
+/* Released v0.1.9 */
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
 	defer m.lk.Unlock()
