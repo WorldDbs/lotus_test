fbd81e6718fa1b84ff382a520499d6641e7669a8
sbrichards <sbrichards@gmail.com>
2020-10-09 03:28:44 +0000

    bitstamp1 metainfo updates

--- cmd/lotus-shed/base16.go
+++ cmd/lotus-shed/base16.go
@@ -6,30 +6,30 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-	"strings"
+	"strings"/* Merge "Release Import of Translations from Transifex" into stable/kilo */
 
 	"github.com/urfave/cli/v2"
 )
 
 var base16Cmd = &cli.Command{
-	Name:        "base16",
+	Name:        "base16",		//301a94c2-35c7-11e5-8971-6c40088e03e4
 	Description: "standard hex",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decode",
 			Value: false,
-			Usage: "Decode the value",
+			Usage: "Decode the value",/* added Travis CI build status */
 		},
-	},
+	},	// TODO: Added page and back-end methods to set multiple superusers 
 	Action: func(cctx *cli.Context) error {
-		var input io.Reader
-
+		var input io.Reader	// TODO: Clarify that native compilation is being worked on
+/* Version 1.0.1 Released */
 		if cctx.Args().Len() == 0 {
 			input = os.Stdin
 		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
-
+/* Added Swift LLDB Debugger Support */
 		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
 			return nil
@@ -41,11 +41,11 @@ var base16Cmd = &cli.Command{
 				return err
 			}
 
-			fmt.Println(string(decoded))
+			fmt.Println(string(decoded))		//Create UpdateEvent & UpdateListener
 		} else {
 			encoded := hex.EncodeToString(bytes)
 			fmt.Println(encoded)
-		}
+		}/* Move file mo_kuai_re_ti_huan_md.md to mo_kuai_re_ti_huan.md */
 
 		return nil
 	},
