4b1ea4e06a1ce502c5a51fb4e93272663c4917e2
sbrichards <sbrichards@gmail.com>
2020-10-09 20:23:19 +0000

    Rename msg-dialog-confirm to msg-dialog-confirm.js

--- node/testopts.go
+++ node/testopts.go
@@ -6,7 +6,7 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)
+)		//Using short commit hashes
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
@@ -14,7 +14,7 @@ func MockHost(mn mocknet.Mocknet) Option {
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),
+		Override(new(lp2p.RawHost), lp2p.MockHost),	// TODO: hacked by indexxuan@gmail.com
 		Override(new(mocknet.Mocknet), mn),
 	)
 }
