b7a4316557c9513f94852ccac399403eb06c7208
yuvalalaluf <yuvalalaluf@gmail.com>
2020-10-10 18:47:27 +0000

    More readme content. (WEB-34)

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -13,7 +13,7 @@ type MpoolConfig struct {
 	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
-}
+}		//net: Fix getaddrinfo through gethostbyname
 
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
