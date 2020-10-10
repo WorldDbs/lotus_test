6a7346d10e08730977c0c8c8884b3ac82f6a4332
cory <cory@protocol.ai>
2020-10-10 01:37:12 +0000

    18a35392-2e5d-11e5-9284-b827eb9e62be

--- api/permissioned.go
+++ api/permissioned.go
@@ -5,8 +5,8 @@ import (
 )
 
 const (
-	// When changing these, update docs/API.md too
-
+	// When changing these, update docs/API.md too	// TODO: Create print_2_vcf.pl
+		//Merge autosize branch changes.
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
@@ -17,27 +17,27 @@ var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
+	var out StorageMinerStruct/* Fix-up half-written paragraph in the docs */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
-
+	// TODO: small code improvement for getting leaves
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out
-}
+	return &out/* Delete fakeindex.txt */
+}/* Merge "Audio preprocessing wrapper for webrtc." */
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-
-func PermissionedWalletAPI(a Wallet) Wallet {
+	// TODO: add start of nlp stuff
+func PermissionedWalletAPI(a Wallet) Wallet {	// TODO: will be fixed by mowrain@yandex.com
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
-}
+}	// TODO: hacked by witek@enjin.io
