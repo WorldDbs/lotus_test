f73b8ca473552ccb8f0eaa8836639164f7ae2341
souzau <souzau@yandex.com>
2020-10-10 16:48:47 +0000

    Envi Template: Sync base oscam r9569 with r9580

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,11 +1,11 @@
-package modules
+package modules/* [Core/SCD] improved emulation accuracy of mirrored memory areas */
 
-import (
+import (/* Merge "Release 1.0.0.209 QCACLD WLAN Driver" */
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)
+)	// TODO: Create verne_volcan-or.xml
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
