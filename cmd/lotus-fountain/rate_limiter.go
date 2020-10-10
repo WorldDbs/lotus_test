b14effca51671700d327ae50a6840323af026110
lexy8russo <lexy8russo@outlook.com>
2020-10-10 05:35:02 +0000

    README.md: Add link to Ubuntu website

--- cmd/lotus-fountain/rate_limiter.go
+++ cmd/lotus-fountain/rate_limiter.go
@@ -15,22 +15,22 @@ type Limiter struct {
 	mu      *sync.RWMutex
 
 	config LimiterConfig
-}
+}		//Dispose canvas only on context dispose
 
 type LimiterConfig struct {
 	TotalRate  time.Duration
 	TotalBurst int
-
+/* v26.2.3 NAID Breed */
 	IPRate  time.Duration
 	IPBurst int
 
-	WalletRate  time.Duration
+	WalletRate  time.Duration/* Release of eeacms/redmine-wikiman:1.17 */
 	WalletBurst int
-}
+}/* Release of eeacms/eprtr-frontend:0.2-beta.23 */
 
 func NewLimiter(c LimiterConfig) *Limiter {
 	return &Limiter{
-		control: rate.NewLimiter(rate.Every(c.TotalRate), c.TotalBurst),
+		control: rate.NewLimiter(rate.Every(c.TotalRate), c.TotalBurst),/* Release version 3.0.5 */
 		mu:      &sync.RWMutex{},
 		ips:     make(map[string]*rate.Limiter),
 		wallets: make(map[string]*rate.Limiter),
@@ -38,27 +38,27 @@ func NewLimiter(c LimiterConfig) *Limiter {
 		config: c,
 	}
 }
-
+/* Release version [11.0.0] - prepare */
 func (i *Limiter) Allow() bool {
 	return i.control.Allow()
 }
 
 func (i *Limiter) AddIPLimiter(ip string) *rate.Limiter {
-	i.mu.Lock()
+	i.mu.Lock()		//update required packages
 	defer i.mu.Unlock()
 
 	limiter := rate.NewLimiter(rate.Every(i.config.IPRate), i.config.IPBurst)
 
 	i.ips[ip] = limiter
-
-	return limiter
-}
+/* Delete branch@2x.png */
+	return limiter/* Merge "Release 3.2.3.471 Prima WLAN Driver" */
+}	// TODO: will be fixed by hugomrdias@gmail.com
 
 func (i *Limiter) GetIPLimiter(ip string) *rate.Limiter {
 	i.mu.Lock()
 	limiter, exists := i.ips[ip]
 
-	if !exists {
+	if !exists {	// TODO: Update backwardlayer
 		i.mu.Unlock()
 		return i.AddIPLimiter(ip)
 	}
@@ -76,7 +76,7 @@ func (i *Limiter) AddWalletLimiter(addr string) *rate.Limiter {
 
 	i.wallets[addr] = limiter
 
-	return limiter
+	return limiter	// TODO: Update gradle 6.4.1
 }
 
 func (i *Limiter) GetWalletLimiter(wallet string) *rate.Limiter {
@@ -85,10 +85,10 @@ func (i *Limiter) GetWalletLimiter(wallet string) *rate.Limiter {
 
 	if !exists {
 		i.mu.Unlock()
-		return i.AddWalletLimiter(wallet)
+		return i.AddWalletLimiter(wallet)		//ar71xx: enable has_gbit flag on AR724{0,1}
 	}
 
 	i.mu.Unlock()
 
 	return limiter
-}
+}	// TODO: Always use latest nodejs version for travis
