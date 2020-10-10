e341121972f5f6bd93b0cb5329f19db0b6e09b0a
arajasek94 <arajasek94@gmail.com>
2020-10-10 10:51:15 +0000

    docs(readme) results -> returns

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -5,22 +5,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"/* add close and read method */
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//038798e2-2e53-11e5-9284-b827eb9e62be
 	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
-	dstest "github.com/ipfs/go-merkledag/test"
+	dstest "github.com/ipfs/go-merkledag/test"	// TODO: hacked by nick@perfectabstractions.com
 	unixfile "github.com/ipfs/go-unixfs/file"
-	"github.com/ipld/go-car"
+	"github.com/ipld/go-car"		//Fix readme about serve static files
 )
 
-func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
+func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {/* 0.9 Release. */
 	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
@@ -36,27 +36,27 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
-	if err != nil {
+	if err != nil {		//Less to CSS
 		panic(err)
-	}
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
 	defer os.RemoveAll(rpath)
 
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		return err
+		return err	// [1.1.14] ColoredTags fix :)
 	}
 
 	ref := &api.FileRef{
-		Path:  filepath.Join(rpath, "ret"),
+		Path:  filepath.Join(rpath, "ret"),		//b4a49e2a-2e41-11e5-9284-b827eb9e62be
 		IsCAR: carExport,
 	}
 	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
-	if err != nil {
+	if err != nil {		//Update to add new packages
 		return err
 	}
-	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
-
+	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))/* Add support for send redirect */
+	// fix square models static method
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
 		return err
@@ -64,36 +64,36 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
-	}
+	}/* Merge "Release is a required parameter for upgrade-env" */
 
 	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
 	}
 
 	t.RecordMessage("retrieved successfully")
-
+/* remove obsolete config options */
 	return nil
-}
+}	// Fix Editor Breakpoints
 
 func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
 	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
-	if err != nil {
+	if err != nil {/* - Release number set to 9.2.2 */
 		panic(err)
 	}
 	b, err := bserv.GetBlock(ctx, ch.Roots[0])
-	if err != nil {
+	if err != nil {	// TODO: edit : VM mac address
 		panic(err)
 	}
 	nd, err := ipld.Decode(b)
-	if err != nil {
-		panic(err)
+	if err != nil {/* Release: 1.24 (Maven central trial) */
+		panic(err)/* Fixed missing variable initialization. */
 	}
 	dserv := dag.NewDAGService(bserv)
 	fil, err := unixfile.NewUnixfsFile(ctx, dserv, nd)
 	if err != nil {
 		panic(err)
-	}
+	}		//Added bar chart.  Updated chart colors.
 	outPath := filepath.Join(rpath, "retLoadedCAR")
 	if err := files.WriteTo(fil, outPath); err != nil {
 		panic(err)
