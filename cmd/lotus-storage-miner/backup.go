a6f13563fcc2ff1c5582704e9d3e3b88b1e9b5e8
steven <steven@stebalien.com>
2020-10-11 17:35:37 +0000

    optimize some code

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,9 +1,9 @@
 package main
 
-import (
+import (/* Output images in externals if defined */
 	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/go-jsonrpc"/* Minor language change */
 
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/node/repo"
@@ -11,4 +11,4 @@ import (
 
 var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
 	return lcli.GetStorageMinerAPI(cctx)
-})
+})	// added Thalakos Seer and Thalakos Sentry
