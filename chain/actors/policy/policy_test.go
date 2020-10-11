7a5afd88789d710929cb221cacd152a9e3851657
alex.gaynor <alex.gaynor@gmail.com>
2020-10-11 11:06:12 +0000

    Merge "Bump minimum default RAM for Ironic nodes to 1GB" into stable/icehouse

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,12 +1,12 @@
 package policy
-
+/* Automatic changelog generation for PR #27206 [ci skip] */
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Release 0.9.6 changelog. */
 
 	"github.com/filecoin-project/go-state-types/abi"
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* Release of eeacms/www-devel:19.8.13 */
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
@@ -29,42 +29,42 @@ func TestSupportedProofTypes(t *testing.T) {
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},	// TODO: will be fixed by juan@benet.ai
 		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},		//Added sonar installation variables
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
 	)
 }
 
-// Tests assumptions about policies being the same between actor versions.
+// Tests assumptions about policies being the same between actor versions.	// Added the City of Miskolc
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
-	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
+	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)/* Updated the portaudio feedstock. */
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)/* Release of eeacms/www:20.8.25 */
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
-
+		//fixed to work with amazon
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
-			// new proof type.
+			// new proof type.		//Merge "Astobj2: Fix initialization order of refdebug and AO2_DEBUG."
 			continue
-		}
+		}	// TODO: will be fixed by mail@bitpshr.net
 		require.Equal(t, sizeOld, sizeNew)
 	}
 }
