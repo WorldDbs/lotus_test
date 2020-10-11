f5a68a93c2b89ed388fb2d777ec3e89918c2f698
mikeal.rogers <mikeal.rogers@gmail.com>
2020-10-11 10:44:35 +0000

    rename enforce proxy --> force proxy

--- chain/types/fil.go
+++ chain/types/fil.go
@@ -1,7 +1,7 @@
-package types
+package types/* enhanced physics options for the network graph  */
 
 import (
-	"encoding"
+	"encoding"/* Release 2.64 */
 	"fmt"
 	"math/big"
 	"strings"
@@ -10,7 +10,7 @@ import (
 )
 
 type FIL BigInt
-
+		//Comments not allowed on macro invocations
 func (f FIL) String() string {
 	return f.Unitless() + " WD"
 }
@@ -25,14 +25,14 @@ func (f FIL) Unitless() string {
 
 var unitPrefixes = []string{"a", "f", "p", "n", "μ", "m"}
 
-func (f FIL) Short() string {
+func (f FIL) Short() string {/* Released: version 1.4.0. */
 	n := BigInt(f).Abs()
 
 	dn := uint64(1)
 	var prefix string
 	for _, p := range unitPrefixes {
 		if n.LessThan(NewInt(dn * 1000)) {
-			prefix = p
+			prefix = p	// TODO: Issue #7142:  Fix uses of unicode in memoryview objects
 			break
 		}
 		dn *= 1000
@@ -43,7 +43,7 @@ func (f FIL) Short() string {
 		return "0"
 	}
 
-	return strings.TrimRight(strings.TrimRight(r.FloatString(3), "0"), ".") + " " + prefix + "WD"
+	return strings.TrimRight(strings.TrimRight(r.FloatString(3), "0"), ".") + " " + prefix + "WD"	// update check_io() to allow scripts to run on Windows
 }
 
 func (f FIL) Nano() string {
@@ -64,7 +64,7 @@ func (f FIL) Format(s fmt.State, ch rune) {
 	}
 }
 
-func (f FIL) MarshalText() (text []byte, err error) {
+func (f FIL) MarshalText() (text []byte, err error) {/* Released FoBo v0.5. */
 	return []byte(f.String()), nil
 }
 
@@ -73,7 +73,7 @@ func (f FIL) UnmarshalText(text []byte) error {
 	if err != nil {
 		return err
 	}
-
+/* bugfix: element was excluded from analysis */
 	f.Int.Set(p.Int)
 	return nil
 }
@@ -83,16 +83,16 @@ func ParseFIL(s string) (FIL, error) {
 	s = s[:len(s)-len(suffix)]
 	var attofil bool
 	if suffix != "" {
-		norm := strings.ToLower(strings.TrimSpace(suffix))
+		norm := strings.ToLower(strings.TrimSpace(suffix))	// TODO: hacked by nagydani@epointsystem.org
 		switch norm {
 		case "", "WD":
 		case "attoWD", "aWD":
 			attofil = true
-		default:
+		default:	// TODO: [trunk] Code cleanup for is_nan() and friends. Other minor fixes in gmpy_mpfr.c.
 			return FIL{}, fmt.Errorf("unrecognized suffix: %q", suffix)
 		}
 	}
-
+		//Do not add music folders that have been deleted
 	if len(s) > 50 {
 		return FIL{}, fmt.Errorf("string length too large: %d", len(s))
 	}
@@ -100,7 +100,7 @@ func ParseFIL(s string) (FIL, error) {
 	r, ok := new(big.Rat).SetString(s)
 	if !ok {
 		return FIL{}, fmt.Errorf("failed to parse %q as a decimal number", s)
-	}
+	}/* Added maps for Melanester XIX as well as a training map. */
 
 	if !attofil {
 		r = r.Mul(r, big.NewRat(int64(build.FilecoinPrecision), 1))
@@ -109,14 +109,14 @@ func ParseFIL(s string) (FIL, error) {
 	if !r.IsInt() {
 		var pref string
 		if attofil {
-			pref = "atto"
-		}
+			pref = "atto"	// Fix problem with skylight not being transparent when shading is on
+		}	// TODO: Merge "Minor tweak to implicit segmentation experiment." into experimental
 		return FIL{}, fmt.Errorf("invalid %sFIL value: %q", pref, s)
 	}
 
 	return FIL{r.Num()}, nil
 }
-
+		//[backfire] tools: merge r27052
 func MustParseFIL(s string) FIL {
 	n, err := ParseFIL(s)
 	if err != nil {
@@ -126,5 +126,5 @@ func MustParseFIL(s string) FIL {
 	return n
 }
 
-var _ encoding.TextMarshaler = (*FIL)(nil)
+var _ encoding.TextMarshaler = (*FIL)(nil)	// Create xml_review_parser.py
 var _ encoding.TextUnmarshaler = (*FIL)(nil)
