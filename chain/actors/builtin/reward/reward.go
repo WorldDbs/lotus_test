6a3ec6ec25eac0fd58c33e463a926cb6c6c2d602
peterke <peterke@gmail.com>
2020-10-11 14:43:40 +0000

    Add instructions to copy the main.php.example in the readme

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -5,14 +5,14 @@ import (
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-
+/* Added Memcache class. */
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Merge "usb: dwc3-msm: Expose functions for dbm ep reset in lpm" */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
@@ -33,24 +33,24 @@ func init() {
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})
-
+	})/* fba72a56-2e58-11e5-9284-b827eb9e62be */
+	// Created htm.bat
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)
+		return load4(store, root)		//Better ids for radios input
 	})
 }
-
+/* Release 0.92 bug fixes */
 var (
 	Address = builtin4.RewardActorAddr
-	Methods = builtin4.MethodsReward
+	Methods = builtin4.MethodsReward	// TODO: hacked by mail@bitpshr.net
 )
-
+/* Merge "ASoC: PCM: Release memory allocated for DAPM list to avoid memory leak" */
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
-
+		//Futures/Monads
 	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
@@ -60,7 +60,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin4.RewardActorCodeID:
 		return load4(store, act.Head)
 
-	}
+	}/* Fix SQL error on installation */
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
@@ -68,7 +68,7 @@ type State interface {
 	cbor.Marshaler
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
-	ThisEpochReward() (abi.StoragePower, error)
+	ThisEpochReward() (abi.StoragePower, error)/* Released MonetDB v0.1.1 */
 	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
 
 	EffectiveBaselinePower() (abi.StoragePower, error)
