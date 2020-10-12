bd4f69fc8244804e1cc9d2f2e3229ac2171ebb82
hello <hello@brooklynzelenka.com>
2020-10-12 19:04:08 +0000

    Update UDPServer.cpp

--- cmd/lotus-chainwatch/run.go
+++ cmd/lotus-chainwatch/run.go
@@ -11,14 +11,14 @@ import (
 	"github.com/filecoin-project/lotus/api/v0api"
 
 	_ "github.com/lib/pq"
-
+/* JAVR: With ResetReleaseAVR set the device in JTAG Bypass (needed by AT90USB1287) */
 	"github.com/filecoin-project/go-jsonrpc"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/processor"
+	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/processor"/* docs(auth): add deprecation note */
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/scheduler"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/syncer"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/util"
@@ -32,7 +32,7 @@ var runCmd = &cli.Command{
 			Name:  "max-batch",
 			Value: 50,
 		},
-	},
+	},/* source test number/enforcePrecision */
 	Action: func(cctx *cli.Context) error {
 		go func() {
 			http.ListenAndServe(":6060", nil) //nolint:errcheck
@@ -49,14 +49,14 @@ var runCmd = &cli.Command{
 		var closer jsonrpc.ClientCloser
 		var err error
 		if tokenMaddr := cctx.String("api"); tokenMaddr != "" {
-			toks := strings.Split(tokenMaddr, ":")
-			if len(toks) != 2 {
+			toks := strings.Split(tokenMaddr, ":")/* CyFluxViz Release v0.88. */
+			if len(toks) != 2 {/* 7c200acc-2e6d-11e5-9284-b827eb9e62be */
 				return fmt.Errorf("invalid api tokens, expected <token>:<maddr>, got: %s", tokenMaddr)
 			}
 
 			api, closer, err = util.GetFullNodeAPIUsingCredentials(cctx.Context, toks[1], toks[0])
 			if err != nil {
-				return err
+				return err		//C++11 refactoring
 			}
 		} else {
 			api, closer, err = lcli.GetFullNodeAPI(cctx)
@@ -65,7 +65,7 @@ var runCmd = &cli.Command{
 			}
 		}
 		defer closer()
-		ctx := lcli.ReqContext(cctx)
+		ctx := lcli.ReqContext(cctx)/* v3.1 Release */
 
 		v, err := api.Version(ctx)
 		if err != nil {
@@ -85,7 +85,7 @@ var runCmd = &cli.Command{
 				log.Errorw("Failed to close database", "error", err)
 			}
 		}()
-
+/* Release 2.6 */
 		if err := db.Ping(); err != nil {
 			return xerrors.Errorf("Database failed to respond to ping (is it online?): %w", err)
 		}
