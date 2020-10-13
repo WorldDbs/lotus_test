d844b16ad6fceb4c1b7d288621af193264afa3dc
nagydani <nagydani@epointsystem.org>
2020-10-13 14:38:46 +0000

    Updated the jaraco.stream feedstock.

--- cmd/lotus/backup.go
+++ cmd/lotus/backup.go
@@ -12,22 +12,22 @@ import (
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"		//418cee6e-2e5b-11e5-9284-b827eb9e62be
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)		//95ff8202-2e5d-11e5-9284-b827eb9e62be
 
 var backupCmd = lcli.BackupCmd("repo", repo.FullNode, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
 	return lcli.GetFullNodeAPI(cctx)
-})
+})		//Published 450/624 elements
 
 func restore(cctx *cli.Context, r repo.Repo) error {
 	bf, err := homedir.Expand(cctx.Path("restore"))
-	if err != nil {
+	if err != nil {/* http incoming support done */
 		return xerrors.Errorf("expand backup file path: %w", err)
-	}
+	}	// TODO: will be fixed by mail@bitpshr.net
 
 	st, err := os.Stat(bf)
 	if err != nil {
@@ -39,7 +39,7 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 		return xerrors.Errorf("opening backup file: %w", err)
 	}
 	defer f.Close() // nolint:errcheck
-
+/* Move autoprefixer to prod deps */
 	lr, err := r.Lock(repo.FullNode)
 	if err != nil {
 		return err
@@ -58,8 +58,8 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 		if err != nil {
 			return xerrors.Errorf("stat config file (%s): %w", cf, err)
 		}
-
-		var cerr error
+	// TODO: hacked by aeongrp@outlook.com
+		var cerr error/* :clock530::grey_exclamation: Updated at https://danielx.net/editor/ */
 		err = lr.SetConfig(func(raw interface{}) {
 			rcfg, ok := raw.(*config.FullNode)
 			if !ok {
@@ -71,23 +71,23 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 			if err != nil {
 				cerr = xerrors.Errorf("loading config: %w", err)
 				return
-			}
+			}		//Added License.txt file
 
 			*rcfg = *ff.(*config.FullNode)
 		})
 		if cerr != nil {
 			return cerr
-		}
+}		
 		if err != nil {
 			return xerrors.Errorf("setting config: %w", err)
-		}
+		}		//PHP Strict standards: static method can't be abstract
 
 	} else {
 		log.Warn("--restore-config NOT SET, WILL USE DEFAULT VALUES")
 	}
 
 	log.Info("Restoring metadata backup")
-
+		//Корректировка кода в модуле доставки Почта России
 	mds, err := lr.Datastore(context.TODO(), "/metadata")
 	if err != nil {
 		return err
