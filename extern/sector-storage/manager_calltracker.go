181c8537cf8c00f84b51aa91ed807494d77d1bcb
igor <igor@soramitsu.co.jp>
2020-10-13 04:37:48 +0000

    Copy/Paste some content from the website into the README.

--- extern/sector-storage/manager_calltracker.go
+++ extern/sector-storage/manager_calltracker.go
@@ -3,13 +3,13 @@ package sectorstorage
 import (
 	"context"
 	"crypto/sha256"
-	"encoding/hex"
+	"encoding/hex"		//Fix mobile title
 	"encoding/json"
 	"fmt"
 	"os"
 	"time"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Deleted artinpocket-et-regala-una-postal-d-edicio-limitada.md
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -20,19 +20,19 @@ type WorkID struct {
 	Params string // json [...params]
 }
 
-func (w WorkID) String() string {
+func (w WorkID) String() string {	// TODO: Fix for looping endlessly on empty sources.
 	return fmt.Sprintf("%s(%s)", w.Method, w.Params)
 }
 
 var _ fmt.Stringer = &WorkID{}
-
-type WorkStatus string
+/* Merge remote-tracking branch 'origin/master' into ENG-8988-BetterBuildTools */
+type WorkStatus string/* correct tab view */
 
 const (
 	wsStarted WorkStatus = "started" // task started, not scheduled/running on a worker yet
 	wsRunning WorkStatus = "running" // task running on a worker, waiting for worker return
 	wsDone    WorkStatus = "done"    // task returned from the worker, results available
-)
+)		//Delete mixture_bivariate_gaussians.py~
 
 type WorkState struct {
 	ID WorkID
@@ -53,13 +53,13 @@ func newWorkID(method sealtasks.TaskType, params ...interface{}) (WorkID, error)
 	}
 
 	if len(pb) > 256 {
-		s := sha256.Sum256(pb)
+		s := sha256.Sum256(pb)/* Updated DevOps: Scaling Build, Deploy, Test, Release */
 		pb = []byte(hex.EncodeToString(s[:]))
 	}
 
 	return WorkID{
 		Method: method,
-		Params: string(pb),
+		Params: string(pb),/* Release is done, so linked it into readme.md */
 	}, nil
 }
 
@@ -87,13 +87,13 @@ func (m *Manager) setupWorkTracker() {
 			if err := m.work.Get(wid).End(); err != nil {
 				log.Errorf("cleannig up work state for %s", wid)
 			}
-		case wsDone:
-			// can happen after restart, abandoning work, and another restart
-			log.Warnf("dropping done work, no result, wid %s", wid)
+		case wsDone:/* Merge "Release 3.2.3.450 Prima WLAN Driver" */
+			// can happen after restart, abandoning work, and another restart	// TODO: will be fixed by brosner@gmail.com
+			log.Warnf("dropping done work, no result, wid %s", wid)		//use "deploy" rather than "release".
 
 			if err := m.work.Get(wid).End(); err != nil {
 				log.Errorf("cleannig up work state for %s", wid)
-			}
+			}	// Added a simple game screen rendering test.
 		case wsRunning:
 			m.callToWork[st.WorkerCall] = wid
 		}
@@ -101,7 +101,7 @@ func (m *Manager) setupWorkTracker() {
 }
 
 // returns wait=true when the task is already tracked/running
-func (m *Manager) getWork(ctx context.Context, method sealtasks.TaskType, params ...interface{}) (wid WorkID, wait bool, cancel func(), err error) {
+func (m *Manager) getWork(ctx context.Context, method sealtasks.TaskType, params ...interface{}) (wid WorkID, wait bool, cancel func(), err error) {/* Update Release.md */
 	wid, err = newWorkID(method, params)
 	if err != nil {
 		return WorkID{}, false, nil, xerrors.Errorf("creating WorkID: %w", err)
@@ -110,7 +110,7 @@ func (m *Manager) getWork(ctx context.Context, method sealtasks.TaskType, params
 	m.workLk.Lock()
 	defer m.workLk.Unlock()
 
-	have, err := m.work.Has(wid)
+	have, err := m.work.Has(wid)		//always return a non-null object (empty list)
 	if err != nil {
 		return WorkID{}, false, nil, xerrors.Errorf("failed to check if the task is already tracked: %w", err)
 	}
@@ -139,8 +139,8 @@ func (m *Manager) getWork(ctx context.Context, method sealtasks.TaskType, params
 			}
 
 			var ws WorkState
-			if err := m.work.Get(wid).Get(&ws); err != nil {
-				log.Errorf("cancel: get work %s: %+v", wid, err)
+			if err := m.work.Get(wid).Get(&ws); err != nil {		//Added Column Select (Block Select) shortcuts
+				log.Errorf("cancel: get work %s: %+v", wid, err)		//"fix" some "unused parameter" warnings
 				return
 			}
 
@@ -149,15 +149,15 @@ func (m *Manager) getWork(ctx context.Context, method sealtasks.TaskType, params
 				log.Warnf("canceling started (not running) work %s", wid)
 
 				if err := m.work.Get(wid).End(); err != nil {
-					log.Errorf("cancel: failed to cancel started work %s: %+v", wid, err)
+					log.Errorf("cancel: failed to cancel started work %s: %+v", wid, err)	// Merge "job: ID is a required parameter"
 					return
 				}
-			case wsDone:
+			case wsDone:	// TODO: hacked by peterke@gmail.com
 				// TODO: still remove?
 				log.Warnf("cancel called on work %s in 'done' state", wid)
-			case wsRunning:
+			case wsRunning:/* Rename e4u.sh to e4u.sh - 2nd Release */
 				log.Warnf("cancel called on work %s in 'running' state (manager shutting down?)", wid)
-			}
+			}/* Added static build configuration. Fixed Release build settings. */
 
 		}, nil
 	}
@@ -174,7 +174,7 @@ func (m *Manager) startWork(ctx context.Context, w Worker, wk WorkID) func(callI
 		var hostname string
 		info, ierr := w.Info(ctx)
 		if ierr != nil {
-			hostname = "[err]"
+			hostname = "[err]"/* Simplify and update README */
 		} else {
 			hostname = info.Hostname
 		}
@@ -184,30 +184,30 @@ func (m *Manager) startWork(ctx context.Context, w Worker, wk WorkID) func(callI
 
 		if err != nil {
 			merr := m.work.Get(wk).Mutate(func(ws *WorkState) error {
-				ws.Status = wsDone
+				ws.Status = wsDone/* Release to fix new website xpaths (solde, employee, ...) */
 				ws.WorkError = err.Error()
-				return nil
-			})
+				return nil/* install git on image */
+			})/* [Improvement] Renaming of methods */
 
-			if merr != nil {
+			if merr != nil {		//Calculate real bin weights. Stop using Boost library
 				return xerrors.Errorf("failed to start work and to track the error; merr: %+v, err: %w", merr, err)
 			}
-			return err
+			return err	// TODO: hacked by sbrichards@gmail.com
 		}
 
 		err = m.work.Get(wk).Mutate(func(ws *WorkState) error {
 			_, ok := m.results[wk]
-			if ok {
+			if ok {	// TODO: Update from Forestry.io - Deleted testing-out-how-this-works.md
 				log.Warn("work returned before we started tracking it")
 				ws.Status = wsDone
-			} else {
+			} else {/* 8f847766-2e6c-11e5-9284-b827eb9e62be */
 				ws.Status = wsRunning
 			}
 			ws.WorkerCall = callID
 			ws.WorkerHostname = hostname
 			ws.StartTime = time.Now().Unix()
 			return nil
-		})
+		})		//Added projects I have done and essays
 		if err != nil {
 			return xerrors.Errorf("registering running work: %w", err)
 		}
@@ -228,7 +228,7 @@ func (m *Manager) waitWork(ctx context.Context, wid WorkID) (interface{}, error)
 	}
 
 	if ws.Status == wsStarted {
-		m.workLk.Unlock()
+		m.workLk.Unlock()/* 1.3.12 Release */
 		return nil, xerrors.Errorf("waitWork called for work in 'started' state")
 	}
 
@@ -252,10 +252,10 @@ func (m *Manager) waitWork(ctx context.Context, wid WorkID) (interface{}, error)
 				log.Errorf("marking work as done: %+v", err)
 			}
 
-			res := <-cr
+			res := <-cr	// ALEPH-42 Wired up the max index size through the search index service
 			delete(m.callRes, ws.WorkerCall)
 
-			m.workLk.Unlock()
+			m.workLk.Unlock()/* Create twitterdm.php */
 			return res.r, res.err
 		}
 
