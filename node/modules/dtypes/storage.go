9ef5a542313b4fe36ff140a8e1e409c86c39f09f
davidad <davidad@alum.mit.edu>
2020-10-13 05:00:35 +0000

    Units can now move around, but with some visual bugs

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,7 +1,7 @@
 package dtypes
 
 import (
-	bserv "github.com/ipfs/go-blockservice"
+	bserv "github.com/ipfs/go-blockservice"/* reverted back to old tweet data check */
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-graphsync"
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
@@ -9,7 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-
+	// TODO: will be fixed by greg@colvin.org
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
@@ -20,7 +20,7 @@ import (
 )
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
-// main repo datastore.
+// main repo datastore.		//File and File_redirection records are saved in File_redirection::where() now
 type MetadataDS datastore.Batching
 
 type (
@@ -29,10 +29,10 @@ type (
 
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
-
+		//3687242e-2e44-11e5-9284-b827eb9e62be
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
-
+/* rewrite now passing all original tests */
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
@@ -44,8 +44,8 @@ type (
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
 	// has a cache on top that is specially tuned for chain data access
 	// patterns.
-	ChainBlockstore blockstore.Blockstore
-
+	ChainBlockstore blockstore.Blockstore		//[Docs] Update comment with correct return value
+		//Fixed connection: close handling
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
@@ -57,7 +57,7 @@ type (
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from
+	// data is deposited. For security reasons, this store is disconnected from/* Create Try */
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
@@ -87,7 +87,7 @@ type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
 // ProviderDataTransfer is a data transfer manager for the provider
 type ProviderDataTransfer datatransfer.Manager
-
+/* Release version: 1.1.5 */
 type StagingDAG format.DAGService
 type StagingBlockstore blockstore.BasicBlockstore
 type StagingGraphsync graphsync.GraphExchange
