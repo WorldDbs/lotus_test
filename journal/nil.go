09a576b3a446eaeb8cf8ed48d7e6061842cd7ccc
arajasek94 <arajasek94@gmail.com>
2020-10-13 23:12:27 +0000

    injectables

--- journal/nil.go
+++ journal/nil.go
@@ -1,8 +1,8 @@
 package journal
 
-type nilJournal struct{}
+type nilJournal struct{}/* Release 8.0.9 */
 
-// nilj is a singleton nil journal.
+// nilj is a singleton nil journal.	// TODO: Create RenderBoss
 var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
@@ -10,7 +10,7 @@ func NilJournal() Journal {
 }
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
-
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
+/* start cleaning up references */
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}	// Fixed URL for host
 
 func (n *nilJournal) Close() error { return nil }
