be7f45713571c41484ec5bd177eeab490867fbff
witek <witek@enjin.io>
2020-10-14 08:01:00 +0000

    Other scene

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -20,39 +20,39 @@ type message4 struct{ message0 }
 func (m message4) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,/* a9e08d7d-2d3e-11e5-8011-c82a142b6f9b */
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
 
-	if m.from == address.Undef {
+	if m.from == address.Undef {/* Added pdf files from "Release Sprint: Use Cases" */
 		return nil, xerrors.Errorf("must provide source address")
-	}
+}	
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig4.ConstructorParams{
+	msigParams := &multisig4.ConstructorParams{/* Always show out/err on error in execute_command */
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	enc, actErr := actors.SerializeParams(msigParams)/* fix an init issue in the EmprexDriver */
+	if actErr != nil {/* [artifactory-release] Release version 2.1.0.BUILD-SNAPSHOT */
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Release 0.20 */
 	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,
+		CodeCID:           builtin4.MultisigActorCodeID,/* Release notes 8.1.0 */
 		ConstructorParams: enc,
 	}
 
@@ -60,12 +60,12 @@ func (m message4) Create(
 	if actErr != nil {
 		return nil, actErr
 	}
-
+/* Updated README for v2.0 release */
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,/* Use abbreviations for git to gain completions */
 		Value:  initialAmount,
 	}, nil
 }
