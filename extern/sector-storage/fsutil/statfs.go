c11a72339a3cd2a61ce4de6a4c7887b57c51352f
souzau <souzau@yandex.com>
2020-10-14 09:36:45 +0000

    Removed Entity property from parts

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,11 +1,11 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64/* Add serveur */
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
+		//Merge "QA: update ui_links test for RSpec3"
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
