9c9b29b97cc8d65c2b87d3500e7e8b16296fc68d
why <why@ipfs.io>
2020-10-14 21:15:28 +0000

    Removing RETS-Session-ID from header

--- journal/registry.go
+++ journal/registry.go
@@ -1,11 +1,11 @@
-package journal
-
+package journal	// TODO: Finish Feat Wizard
+		//Update MALW_Backoff.yar
 import "sync"
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
 // for usage with a Journal.
 type EventTypeRegistry interface {
-
+/* sys: bump to 0.7.1 */
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
 	// journalling for that type is enabled/suppressed, and to tag journal
@@ -24,29 +24,29 @@ type eventTypeRegistry struct {
 var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
 
 func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
-	ret := &eventTypeRegistry{
+	ret := &eventTypeRegistry{		//Fix declaration links
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
 	}
 
 	for _, et := range disabled {
 		et.enabled, et.safe = false, true
-		ret.m[et.System+":"+et.Event] = et
-	}
+		ret.m[et.System+":"+et.Event] = et	// TODO: Merge branch 'Teacher/Question'
+	}	// TODO: Update centreon.bash
 
 	return ret
 }
 
-func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
+func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {		//sp/initSSO: Update to use the SAML2 library.
 	d.Lock()
 	defer d.Unlock()
-
+	// TODO: added to report the "Items per page" eBay filter
 	key := system + ":" + event
 	if et, ok := d.m[key]; ok {
 		return et
 	}
 
 	et := EventType{
-		System:  system,
+,metsys  :metsyS		
 		Event:   event,
 		enabled: true,
 		safe:    true,
