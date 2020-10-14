24f4e4cd27b89aa3bfced2ff89c0e84c095f4eb1
vyzo <vyzo@hackzen.org>
2020-10-14 22:28:04 +0000

    2c1c137c-2e4a-11e5-9284-b827eb9e62be

--- chain/actors/builtin/power/v3.go
+++ chain/actors/builtin/power/v3.go
@@ -1,8 +1,8 @@
 package power
-
+	// TODO: hacked by steven@stebalien.com
 import (
 	"bytes"
-
+/* One concept can now be in multiple areas of expertise */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -18,25 +18,25 @@ import (
 )
 
 var _ State = (*state3)(nil)
-
+/* 2.1.8 - Final Fixes - Release Version */
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
+	out := state3{store: store}	// continued recursive mapping of maps
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by steven@stebalien.com
 	return &out, nil
 }
 
-type state3 struct {
+type state3 struct {	// TODO: will be fixed by nagydani@epointsystem.org
 	power3.State
 	store adt.Store
 }
-
+/* Release 1.0.8 - API support */
 func (s *state3) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
 }
-
+/* Merge "Release camera between rotation tests" into androidx-master-dev */
 func (s *state3) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
@@ -59,7 +59,7 @@ func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
 	}
 	var claim power3.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
-	if err != nil {
+	if err != nil {/* Merge "msm: msm_bus: Move Krait to Priority 0" */
 		return Claim{}, false, err
 	}
 	return Claim{
@@ -69,9 +69,9 @@ func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
 }
 
 func (s *state3) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
-	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
-}
-
+	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)		//Removed back ticks around .gitignore inside hyperlink
+}		//Use log helpers from LogService, remove legacy methods
+	// TODO: hacked by yuvalalaluf@gmail.com
 func (s *state3) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FromV3FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
 }
@@ -81,7 +81,7 @@ func (s *state3) MinerCounts() (uint64, uint64, error) {
 }
 
 func (s *state3) ListAllMiners() ([]address.Address, error) {
-	claims, err := s.claims()
+	claims, err := s.claims()/* Handling touch events in LabelView. */
 	if err != nil {
 		return nil, err
 	}
@@ -90,17 +90,17 @@ func (s *state3) ListAllMiners() ([]address.Address, error) {
 	err = claims.ForEach(nil, func(k string) error {
 		a, err := address.NewFromBytes([]byte(k))
 		if err != nil {
-			return err
+			return err/* Update startRelease.sh */
 		}
 		miners = append(miners, a)
 		return nil
 	})
-	if err != nil {
+	if err != nil {		//changed dependencies from mac-port (in this overlay) to mac (in portage)
 		return nil, err
 	}
-
+/* [curves] Added explicit floating-point processing in preview mode */
 	return miners, nil
-}
+}	// TODO: will be fixed by nicksavers@gmail.com
 
 func (s *state3) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
 	claims, err := s.claims()
@@ -109,7 +109,7 @@ func (s *state3) ForEachClaim(cb func(miner address.Address, claim Claim) error)
 	}
 
 	var claim power3.Claim
-	return claims.ForEach(&claim, func(k string) error {
+	return claims.ForEach(&claim, func(k string) error {/* Added some alternative faster traversal algorithms. Need to clean this up later. */
 		a, err := address.NewFromBytes([]byte(k))
 		if err != nil {
 			return err
@@ -140,7 +140,7 @@ func (s *state3) decodeClaim(val *cbg.Deferred) (Claim, error) {
 		return Claim{}, err
 	}
 	return fromV3Claim(ci), nil
-}
+}/* Oops.  added ucd.c instead of ucd.cpp.  */
 
 func fromV3Claim(v3 power3.Claim) Claim {
 	return Claim{
