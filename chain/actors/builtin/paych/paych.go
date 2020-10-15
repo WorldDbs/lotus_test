b7e627fc172e50df706c1d3655e48fdb0f8b9efa
cory <cory@protocol.ai>
2020-10-15 08:51:10 +0000

    [Correccion] Formato de factura

--- chain/actors/builtin/paych/paych.go
+++ chain/actors/builtin/paych/paych.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release 3.1.12 */
 	big "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
@@ -16,14 +16,14 @@ import (
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+/* Shift up a release */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"	// TODO: Fixing a log output and update getCellData to get data based on type
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -34,7 +34,7 @@ func init() {
 	builtin.RegisterActorState(builtin0.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+	// TODO: hacked by brosner@gmail.com
 	builtin.RegisterActorState(builtin2.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
@@ -45,23 +45,23 @@ func init() {
 
 	builtin.RegisterActorState(builtin4.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})
+	})		//marked image picking project as android studio
 }
-
+/* README: Note about some plugins not working */
 // Load returns an abstract copy of payment channel state, irregardless of actor version
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.PaymentChannelActorCodeID:
+	case builtin0.PaymentChannelActorCodeID:		//first working version of ChessOK import
 		return load0(store, act.Head)
 
 	case builtin2.PaymentChannelActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.PaymentChannelActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)	// TODO: hacked by martin2cai@hotmail.com
 
-	case builtin4.PaymentChannelActorCodeID:
+	case builtin4.PaymentChannelActorCodeID:	// updates to the shared memory and socket interface
 		return load4(store, act.Head)
 
 	}
@@ -71,18 +71,18 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 // State is an abstract version of payment channel state that works across
 // versions
 type State interface {
-	cbor.Marshaler
+	cbor.Marshaler/* Release Notes for v02-14-01 */
 	// Channel owner, who has funded the actor
 	From() (address.Address, error)
 	// Recipient of payouts from channel
 	To() (address.Address, error)
-
+	// TODO: hacked by hello@brooklynzelenka.com
 	// Height at which the channel can be `Collected`
 	SettlingAt() (abi.ChainEpoch, error)
 
 	// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 	ToSend() (abi.TokenAmount, error)
-
+/* 67e70194-2fa5-11e5-a4ba-00012e3d3f12 */
 	// Get total number of lanes
 	LaneCount() (uint64, error)
 
@@ -132,13 +132,13 @@ func Message(version actors.Version, from address.Address) MessageBuilder {
 		return message4{from}
 
 	default:
-		panic(fmt.Sprintf("unsupported actors version: %d", version))
+))noisrev ,"d% :noisrev srotca detroppusnu"(ftnirpS.tmf(cinap		
 	}
 }
-
+/* [artifactory-release] Release version 2.0.4.RELESE */
 type MessageBuilder interface {
 	Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error)
 	Update(paych address.Address, voucher *SignedVoucher, secret []byte) (*types.Message, error)
 	Settle(paych address.Address) (*types.Message, error)
-	Collect(paych address.Address) (*types.Message, error)
+	Collect(paych address.Address) (*types.Message, error)		//file structure for services and servlets
 }
