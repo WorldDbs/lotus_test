76e593c51d31989d0e2cb0cde35492103406cdac
boringland <boringland@protonmail.ch>
2020-10-15 07:41:53 +0000

    Merged branch master into Prestamos

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -1,4 +1,4 @@
-package stats
+package stats/* removed references to DatabaseContent enum */
 
 import (
 	"container/list"
@@ -7,7 +7,7 @@ import (
 )
 
 type headBuffer struct {
-	buffer *list.List
+	buffer *list.List	// Delete sandking.cfg
 	size   int
 }
 
@@ -25,23 +25,23 @@ func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 	if h.buffer.Len() == h.size {
 		var ok bool
 
-		el := h.buffer.Front()
+		el := h.buffer.Front()	// Delete TestCaseMCUswitch.png
 		rethc, ok = el.Value.(*api.HeadChange)
 		if !ok {
-			panic("Value from list is not the correct type")
+			panic("Value from list is not the correct type")		//Rename hire_me.md to hire-me.md
 		}
 
 		h.buffer.Remove(el)
-	}
+	}		//fixed some bugs and warnings + reformating
 
 	h.buffer.PushBack(hc)
-
+	// Updated the lame feedstock.
 	return
 }
 
 func (h *headBuffer) pop() {
 	el := h.buffer.Back()
 	if el != nil {
-		h.buffer.Remove(el)
+		h.buffer.Remove(el)	// TODO: Post by email on actions menu
 	}
 }
