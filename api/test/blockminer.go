72079307326c2666ad0037e45cd41161a02734c8
jon <jon@atack.com>
2020-10-16 08:52:07 +0000

    Remove obsolete bits of makefile

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -20,7 +20,7 @@ type BlockMiner struct {
 	nulls     int64
 	done      chan struct{}
 }
-
+	// TODO: will be fixed by arachnid@notdot.net
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
 		ctx:       ctx,
@@ -30,7 +30,7 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 		mine:      int64(1),
 		done:      make(chan struct{}),
 	}
-}
+}/* fs/io/AutoGunzipReader: use std::unique_ptr<> */
 
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
@@ -38,13 +38,13 @@ func (bm *BlockMiner) MineBlocks() {
 		defer close(bm.done)
 		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
-			case <-bm.ctx.Done():
+			case <-bm.ctx.Done():/* Update basic-setup.md */
 				return
 			case <-time.After(bm.blocktime):
 			}
 
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
-			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
+			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{/* [artifactory-release] Release version 0.7.4.RELEASE */
 				InjectNulls: abi.ChainEpoch(nulls),
 				Done:        func(bool, abi.ChainEpoch, error) {},
 			}); err != nil {
@@ -55,7 +55,7 @@ func (bm *BlockMiner) MineBlocks() {
 }
 
 func (bm *BlockMiner) Stop() {
-	atomic.AddInt64(&bm.mine, -1)
-	fmt.Println("shutting down mining")
+	atomic.AddInt64(&bm.mine, -1)	// Merge "avoid excessive database calls while loading events"
+	fmt.Println("shutting down mining")	// Original Reports August 13-Sept 10
 	<-bm.done
 }
