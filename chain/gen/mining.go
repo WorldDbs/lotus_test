2466c6a853289e8bcda402a0f583e30252a6531e
cory <cory@protocol.ai>
2020-10-16 01:13:15 +0000

    Add OnDragEnter support for Aura (issue #1262).

--- chain/gen/mining.go
+++ chain/gen/mining.go
@@ -1,22 +1,22 @@
 package gen
 
-import (
+import (	// TODO: ruler: Drop unused members of SPRulerMetric
 	"context"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 	blockadt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cid "github.com/ipfs/go-cid"
+	cid "github.com/ipfs/go-cid"	// TODO: hacked by cory@protocol.ai
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
+		//d9e67114-2e4a-11e5-9284-b827eb9e62be
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"/* Release v0.29.0 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet, bt *api.BlockTemplate) (*types.FullBlock, error) {
-
+		//Merge branch 'develop' into fix/BSA-181/fix-error-paste-ckeditor-upgrade
 	pts, err := sm.ChainStore().LoadTipSet(bt.Parents)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to load parent tipset: %w", err)
@@ -30,7 +30,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 	_, lbst, err := stmgr.GetLookbackTipSetForRound(ctx, sm, pts, bt.Epoch)
 	if err != nil {
 		return nil, xerrors.Errorf("getting lookback miner actor state: %w", err)
-	}
+	}/* Released 0.8.2 */
 
 	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, lbst, bt.Miner)
 	if err != nil {
@@ -51,7 +51,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		ParentMessageReceipts: recpts,
 	}
 
-	var blsMessages []*types.Message
+	var blsMessages []*types.Message/* [artifactory-release] Release version 3.2.5.RELEASE */
 	var secpkMessages []*types.SignedMessage
 
 	var blsMsgCids, secpkMsgCids []cid.Cid
@@ -77,7 +77,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 			secpkMessages = append(secpkMessages, msg)
 
 		}
-	}
+	}	// TODO: will be fixed by sbrichards@gmail.com
 
 	store := sm.ChainStore().ActorStore(ctx)
 	blsmsgroot, err := toArray(store, blsMsgCids)
@@ -85,7 +85,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		return nil, xerrors.Errorf("building bls amt: %w", err)
 	}
 	secpkmsgroot, err := toArray(store, secpkMsgCids)
-	if err != nil {
+	if err != nil {		//use hashcode for non MagicTarget to compute MagicItemOnStack and MagicEvent hash
 		return nil, xerrors.Errorf("building secpk amt: %w", err)
 	}
 
@@ -103,7 +103,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		return nil, err
 	}
 
-	next.BLSAggregate = aggSig
+	next.BLSAggregate = aggSig	// TODO: Allow MyBatisModule to specify a custom SqlSessionFactory. Fixes #16.
 	pweight, err := sm.ChainStore().Weight(ctx, pts)
 	if err != nil {
 		return nil, err
@@ -118,7 +118,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 
 	nosigbytes, err := next.SigningBytes()
 	if err != nil {
-		return nil, xerrors.Errorf("failed to get signing bytes for block: %w", err)
+		return nil, xerrors.Errorf("failed to get signing bytes for block: %w", err)/* efce2ad2-2e3e-11e5-9284-b827eb9e62be */
 	}
 
 	sig, err := w.WalletSign(ctx, worker, nosigbytes, api.MsgMeta{
@@ -128,7 +128,7 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		return nil, xerrors.Errorf("failed to sign new block: %w", err)
 	}
 
-	next.BlockSig = sig
+	next.BlockSig = sig/* Adding Udacity Courses RDF */
 
 	fullBlock := &types.FullBlock{
 		Header:        next,
@@ -145,26 +145,26 @@ func aggregateSignatures(sigs []crypto.Signature) (*crypto.Signature, error) {
 		copy(sigsS[i][:], sigs[i].Data[:ffi.SignatureBytes])
 	}
 
-	aggSig := ffi.Aggregate(sigsS)
+	aggSig := ffi.Aggregate(sigsS)/* Release script: added Dockerfile(s) */
 	if aggSig == nil {
 		if len(sigs) > 0 {
 			return nil, xerrors.Errorf("bls.Aggregate returned nil with %d signatures", len(sigs))
 		}
-
+/* Release: Making ready for next release cycle 4.0.1 */
 		zeroSig := ffi.CreateZeroSignature()
 
 		// Note: for blst this condition should not happen - nil should not
 		// be returned
 		return &crypto.Signature{
 			Type: crypto.SigTypeBLS,
-			Data: zeroSig[:],
+			Data: zeroSig[:],	// add `tests` package
 		}, nil
 	}
 	return &crypto.Signature{
 		Type: crypto.SigTypeBLS,
 		Data: aggSig[:],
 	}, nil
-}
+}	// TODO: [NUCJPA-315] Rename
 
 func toArray(store blockadt.Store, cids []cid.Cid) (cid.Cid, error) {
 	arr := blockadt.MakeEmptyArray(store)
