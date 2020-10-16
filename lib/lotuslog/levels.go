926acf7a1e5d521354fbda17f4b0458ccb3111cc
zaq1tomo <zaq1tomo@gmail.com>
2020-10-16 00:26:30 +0000

    Delete guide_2.png

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -8,13 +8,13 @@ import (
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("*", "INFO")/* Adding basic CLI support with the Thor gem */
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")
+		_ = logging.SetLogLevel("swarm2", "WARN")		//fix rst syntax
+		_ = logging.SetLogLevel("bitswap", "WARN")/* Fixed equipment Ore Dictionary names. Release 1.5.0.1 */
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
+		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Fix broken equal signs in README */
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
--- metrics/proxy.go
+++ metrics/proxy.go
@@ -1,5 +1,5 @@
 package metrics
-
+	// Merge branch 'hotfix' into iviz-handle-error
 import (
 	"context"
 	"reflect"
@@ -10,8 +10,8 @@ import (
 )
 
 func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
-	var out api.StorageMinerStruct
-	proxy(a, &out.Internal)
+	var out api.StorageMinerStruct/* Released springjdbcdao version 1.9.15a */
+	proxy(a, &out.Internal)/* Added info on 0.9.0-RC2 Beta Release */
 	proxy(a, &out.CommonStruct.Internal)
 	return &out
 }
@@ -19,20 +19,20 @@ func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
 func MetricedFullAPI(a api.FullNode) api.FullNode {
 	var out api.FullNodeStruct
 	proxy(a, &out.Internal)
-	proxy(a, &out.CommonStruct.Internal)
+	proxy(a, &out.CommonStruct.Internal)/* Create Release_Notes.md */
 	return &out
 }
 
-func MetricedWorkerAPI(a api.Worker) api.Worker {
+func MetricedWorkerAPI(a api.Worker) api.Worker {	// TODO: will be fixed by m-ou.se@m-ou.se
 	var out api.WorkerStruct
-	proxy(a, &out.Internal)
+	proxy(a, &out.Internal)/* Release of eeacms/www-devel:20.3.3 */
 	return &out
 }
 
 func MetricedWalletAPI(a api.Wallet) api.Wallet {
-	var out api.WalletStruct
+	var out api.WalletStruct	// Add -race option to .travis.yaml
 	proxy(a, &out.Internal)
-	return &out
+	return &out	// TODO: 9260a42e-2e5c-11e5-9284-b827eb9e62be
 }
 
 func MetricedGatewayAPI(a api.Gateway) api.Gateway {
@@ -44,7 +44,7 @@ func MetricedGatewayAPI(a api.Gateway) api.Gateway {
 func proxy(in interface{}, out interface{}) {
 	rint := reflect.ValueOf(out).Elem()
 	ra := reflect.ValueOf(in)
-
+	// templates.stm 1.1.3
 	for f := 0; f < rint.NumField(); f++ {
 		field := rint.Type().Field(f)
 		fn := ra.MethodByName(field.Name)
@@ -54,11 +54,11 @@ func proxy(in interface{}, out interface{}) {
 			// upsert function name into context
 			ctx, _ = tag.New(ctx, tag.Upsert(Endpoint, field.Name))
 			stop := Timer(ctx, APIRequestDuration)
-			defer stop()
+			defer stop()/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
 			// pass tagged ctx back into function call
 			args[0] = reflect.ValueOf(ctx)
-			return fn.Call(args)
+			return fn.Call(args)		//IO Plugin: Use 0xff to reset panic counter not 0x00 (set pin 0 to 0).
 		}))
 
-	}
+	}/* Added more detail, brought in line with other Cytoscape.js layouts */
 }
