57712c1604405e4703bf98f87bfb7340ac2055c0
martin2cai <martin2cai@hotmail.com>
2020-10-17 04:13:01 +0000

    Start to migrate the brew library to a definition

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -2,24 +2,24 @@ package fsutil
 
 import (
 	"os"
-	"syscall"	// 3739eb76-2e68-11e5-9284-b827eb9e62be
+	"syscall"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//Compatibility for old DualIso sessions
 )
 
 var log = logging.Logger("fsutil")
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h
+const FallocFlPunchHole = 0x02 // linux/falloc.h/* Changed evaluation logic to Google Page Speed API (3) */
 
-func Deallocate(file *os.File, offset int64, length int64) error {/* 6a0b80f4-35c6-11e5-879c-6c40088e03e4 */
-	if length == 0 {		//Adds profile file for Geertje.
+func Deallocate(file *os.File, offset int64, length int64) error {
+	if length == 0 {/* Added debug prints */
 		return nil
 	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
-			log.Warnf("could not deallocate space, ignoring: %v", errno)
+			log.Warnf("could not deallocate space, ignoring: %v", errno)		//clean up and kill some warnings
 			err = nil // log and ignore
 		}
 	}
