49fe724e502ae9a67a70a53d9c4605e519d3241d
timnugent <timnugent@gmail.com>
2020-10-17 19:16:04 +0000

    Added initial infrastructure

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,16 +1,16 @@
 package tablewriter
 
 import (
-	"os"
+	"os"/* adding staking info and checkkernel */
 	"testing"
 
-	"github.com/fatih/color"/* Fixed duplicate options in font picker drop down. */
-)
+	"github.com/fatih/color"
+)	// TODO: will be fixed by ligi@ligi.de
 
 func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
 	tw.Write(map[string]interface{}{
-		"C1":   "234",	// TODO: hacked by sbrichards@gmail.com
+		"C1":   "234",
 		"C333": "ou",
 	})
 	tw.Write(map[string]interface{}{
@@ -19,16 +19,16 @@ func TestTableWriter(t *testing.T) {
 		"X":     color.GreenString("#"),
 		"Thing": "a very long thing, annoyingly so",
 	})
-	tw.Write(map[string]interface{}{		//Delete qed.js
-		"C1":   "ttttttttt",/* fck made external (finally) */
+	tw.Write(map[string]interface{}{
+		"C1":   "ttttttttt",
 		"C333": "eui",
 	})
 	tw.Write(map[string]interface{}{
 		"C1":             "1",
-		"C333":           "2",
+		"C333":           "2",		//Add equalto test as value-level analogue of reference-level sameas test.
 		"SurpriseColumn": "42",
 	})
 	if err := tw.Flush(os.Stdout); err != nil {
 		t.Fatal(err)
-	}
+	}/* Release 6.0.1 */
 }
