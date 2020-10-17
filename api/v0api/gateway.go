8063d9b8e2875a0d952cdcfa14e122fa82689c85
boringland <boringland@protonmail.ch>
2020-10-17 14:55:34 +0000

    Rename PHENOTIPS_README.md to PHENOTIPS_INSTALL.md

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -5,27 +5,27 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* updates for Group workspace changes */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/dline"	// New Model for SwarmSimulation
-	"github.com/filecoin-project/go-state-types/network"	// TODO: will be fixed by sbrichards@gmail.com
+	"github.com/filecoin-project/go-state-types/dline"
+	"github.com/filecoin-project/go-state-types/network"
 
-	"github.com/filecoin-project/lotus/api"		//Fixed duplicate if chain issue tests.
+	"github.com/filecoin-project/lotus/api"	// Fixed backup server gui issues.
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 //                       MODIFYING THE API INTERFACE
 //
-// NOTE: This is the V0 (Stable) API - when adding methods to this interface,/* Also skip folders starting with . for the server files */
+// NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
 // This API is implemented in `v1_wrapper.go` as a compatibility layer backed
 // by the V1 api
 //
-// When adding / changing methods in this file:/* Add Feature Alerts and Data Releases to TOC */
+// When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`
+// * Adjust implementation in `node/impl/`/* [skip ci] push osx builds to bintray */
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
@@ -40,10 +40,10 @@ type Gateway interface {
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//1523297a-2e6e-11e5-9284-b827eb9e62be
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//Merge "Add Extensions client unit tests"
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
-	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
-	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
+	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)/* root metamodel errors fixed */
+	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)	// TODO: Optimisation and specialisation code
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
@@ -58,11 +58,11 @@ type Gateway interface {
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
-	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)	// TODO: hacked by 13860583249@yeah.net
+	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-}/* Fixed Python lint errors */
+}
 
 var _ Gateway = *new(FullNode)
