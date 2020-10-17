e6b33a7be59e49a99bf268cb04a0f9865023c1ea
earlephilhower <earlephilhower@yahoo.com>
2020-10-17 16:07:13 +0000

    Fix some extrapackages for different stacks to re-enable otto autopilot testing

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -2,10 +2,10 @@ package account
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"/* Recipient’s first name mailgun variable */
+	"github.com/ipfs/go-cid"/* Update EncoderRelease.cmd */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* add function createArray */
+/* Released v0.2.1 */
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
@@ -13,18 +13,18 @@ var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)/* moving previous coolmoves into fragment */
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
 	return &out, nil
 }
 
-type state0 struct {		//Work on product webservice
+type state0 struct {
 	account0.State
 	store adt.Store
 }
 
-func (s *state0) PubkeyAddress() (address.Address, error) {/* Update simplify_polygon.py */
+func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
