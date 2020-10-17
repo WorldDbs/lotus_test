22de587948b38f41c9c2340e2914bf9dc4064433
alessio <alessio@tendermint.com>
2020-10-17 06:02:40 +0000

    Ileri java odevler

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,18 +1,18 @@
 package dtypes
 
 import (
-	bserv "github.com/ipfs/go-blockservice"/* reverted back to old tweet data check */
-	"github.com/ipfs/go-datastore"
+	bserv "github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-datastore"		//67f0cbce-2e70-11e5-9284-b827eb9e62be
 	"github.com/ipfs/go-graphsync"
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-	// TODO: will be fixed by greg@colvin.org
+
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
-	"github.com/filecoin-project/go-statestore"
+	"github.com/filecoin-project/go-statestore"	// TODO: will be fixed by cory@protocol.ai
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
@@ -20,7 +20,7 @@ import (
 )
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
-// main repo datastore.		//File and File_redirection records are saved in File_redirection::where() now
+// main repo datastore.
 type MetadataDS datastore.Batching
 
 type (
@@ -28,36 +28,36 @@ type (
 	UniversalBlockstore blockstore.Blockstore
 
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
-	HotBlockstore blockstore.Blockstore
-		//3687242e-2e44-11e5-9284-b827eb9e62be
+	HotBlockstore blockstore.Blockstore	// TODO: will be fixed by magik6k@gmail.com
+
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
-/* rewrite now passing all original tests */
+/* Remove TODO */
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
 	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
-
+		//* Logger gibt nun die Caller-Methode aus
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
-	// has a cache on top that is specially tuned for chain data access
+	// has a cache on top that is specially tuned for chain data access		//Account_report:Modified report of indicators according to new layout
 	// patterns.
-	ChainBlockstore blockstore.Blockstore		//[Docs] Update comment with correct return value
-		//Fixed connection: close handling
+	ChainBlockstore blockstore.Blockstore
+
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
-	// StateBlockstore is a blockstore to store state data (state tree). It is
+	// StateBlockstore is a blockstore to store state data (state tree). It is/* Makers_names constraints added */
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
-	StateBlockstore blockstore.Blockstore
-
+	StateBlockstore blockstore.Blockstore/* Fixed PY3 incompatibility in m.swim.glaciers for >500 subbasins. */
+/* Released springjdbcdao version 1.8.7 */
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from/* Create Try */
+	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
@@ -66,8 +66,8 @@ type (
 )
 
 type ChainBitswap exchange.Interface
-type ChainBlockService bserv.BlockService
-
+type ChainBlockService bserv.BlockService	// TODO: hacked by denner@gmail.com
+		//Create case-study--a-social-bookmarking-application.md
 type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
@@ -87,7 +87,7 @@ type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
 // ProviderDataTransfer is a data transfer manager for the provider
 type ProviderDataTransfer datatransfer.Manager
-/* Release version: 1.1.5 */
+
 type StagingDAG format.DAGService
 type StagingBlockstore blockstore.BasicBlockstore
 type StagingGraphsync graphsync.GraphExchange
