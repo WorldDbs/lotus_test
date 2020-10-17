01b6fa6dbb1961e96b3feafedd2b5cb6da04de4b
13860583249 <13860583249@yeah.net>
2020-10-17 21:37:48 +0000

    connection always verified before use

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -10,8 +10,8 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-/* focus on drag&drop #342 */
-type taskSelector struct {	// TODO: will be fixed by aeongrp@outlook.com
+
+type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
 }
 
@@ -23,19 +23,19 @@ func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}/* Released jujiboutils 2.0 */
+	}
 	_, supported := tasks[task]
 
-	return supported, nil/* Released Beta 0.9.0.1 */
+	return supported, nil
 }
 
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	atasks, err := a.workerRpc.TaskTypes(ctx)	// TODO: Tidy up and tighten up css
+	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}		//5d28654f-2d16-11e5-af21-0401358ea401
+	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {		//705a2c78-2e49-11e5-9284-b827eb9e62be
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
