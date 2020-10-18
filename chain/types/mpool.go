8bf1379909b935f139d4cecf9e9998a9e0b88727
caojiaoyue <caojiaoyue@protonmail.com>
2020-10-18 22:50:24 +0000

    Delete resconfig_comet.json

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,5 +1,5 @@
 package types
-
+	// TODO: will be fixed by ligi@ligi.de
 import (
 	"time"
 
@@ -11,12 +11,12 @@ type MpoolConfig struct {
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration
+	PruneCooldown          time.Duration/* Merge "Release notes for newton RC2" */
 	GasLimitOverestimation float64
-}		//net: Fix getaddrinfo through gethostbyname
+}
 
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc
+	*r = *mc	// Added <module>../org.yakindu.sct.model.stext.test</module> to base pom
 	return r
 }
