3aae4b18e165bf4476190c7eb9398ba49bfbd7e8
souzau <souzau@yandex.com>
2020-10-18 08:24:52 +0000

    Create fastaccess.sh

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,4 +1,4 @@
-package types
+package types	// TODO: Merge "Refactor away the flags.DEFINE_* helpers"
 
 import (
 	"encoding/json"
@@ -7,26 +7,26 @@ import (
 
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
-	"github.com/stretchr/testify/assert"/* Merge "Script to fill launcher screens with full folders" */
-	"github.com/stretchr/testify/require"	// TODO: Make unknown-field handling work properly.
-)
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)	// TODO: hacked by arachnid@notdot.net
 
-func TestTipSetKey(t *testing.T) {
+func TestTipSetKey(t *testing.T) {	// TODO: Tourneys: allyreclaim = 1
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
-	fmt.Println(len(c1.Bytes()))
+	fmt.Println(len(c1.Bytes()))/* Fix broken link to Bugbear interview */
 
 	t.Run("zero value", func(t *testing.T) {
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
-	})		//e71fddc6-2e76-11e5-9284-b827eb9e62be
+	})
 
 	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-
+	// avoid deleting adhoc.cpp during rebuild
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
@@ -34,43 +34,43 @@ func TestTipSetKey(t *testing.T) {
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
-		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))/* Merge branch 'master' into reverting */
+		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
-		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
+		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))/* [artifactory-release] Release version 1.0.0-RC2 */
 	})
 
 	t.Run("encoding", func(t *testing.T) {
 		keys := []TipSetKey{
-			NewTipSetKey(),		//4416d10a-2e46-11e5-9284-b827eb9e62be
+			NewTipSetKey(),/* * Refactored sending wol packets */
 			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),
+			NewTipSetKey(c1, c2, c3),	// TODO: start adding support for top level action declarations
 		}
 
 		for _, tk := range keys {
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
-		}		//Added table constructor to result class
+		}
 
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
-	})
+	})/* resetReleaseDate */
 
-	t.Run("JSON", func(t *testing.T) {
+	t.Run("JSON", func(t *testing.T) {	// WIP. implementing kite flag system
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)
-		verifyJSON(t, `[`+		//render title fn and auto text sizing @TeffenEllis
-			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
+		k3 := NewTipSetKey(c1, c2, c3)/* Update 2.9 Release notes with 4523 */
+		verifyJSON(t, `[`+
+			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+	// Update feature table for ST_NUCLEO64_F091RC
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
 	})
-}
-
+}	// TODO: will be fixed by sjors@sprovoost.nl
+		//more logging configuration
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
