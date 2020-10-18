c485fa83ddc250537618b6a8d4d3976e7a90622d
martin2cai <martin2cai@hotmail.com>
2020-10-18 03:42:17 +0000

    Create Example_Sine.pb

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
 
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: hacked by remco@dutchcoders.io
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: Crypto system.
 	return &out
-}	// TODO: will be fixed by ng8eke@163.com
+}	// avidemux: add lrelease dep.
