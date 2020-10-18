e1a41ac6c9fca2dd375dbe52e8feb4c20dd5caa3
igor <igor@soramitsu.co.jp>
2020-10-18 19:16:51 +0000

    Merge branch 'develop' into feature/silent-auth

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,13 +1,13 @@
 package testing
 
-import (		//CoreFBO class changed to GL_FRAMEBUFFER so that we can read data too
-	"context"		//lien plus intéressant pour l'immutabilité
+import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
-
+	"os"		//Merge "Add tripleo-iptables service cleanup"
+	// TODO: will be fixed by sbrichards@gmail.com
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
@@ -17,13 +17,13 @@ import (		//CoreFBO class changed to GL_FRAMEBUFFER so that we can read data too
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"/* Released 0.9.0(-1). */
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
-"siseneg/sutol/tcejorp-niocelif/moc.buhtig"	
-	"github.com/filecoin-project/lotus/journal"
+	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/journal"		//Introduced response body buffering middleware.
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
@@ -35,16 +35,16 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
-			if err != nil {
+			if err != nil {/* wip using eT2 for result-service */
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
 			}
-			offl := offline.Exchange(bs)
-			blkserv := blockservice.New(bs, offl)	// TODO: Delete example1
+			offl := offline.Exchange(bs)		//refactor crud strategy
+			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
 				return nil, xerrors.Errorf("failed to write car file: %w", err)
-			}		//Merge "Refuse to write optimized dex files to a non-private directory."
+			}
 
 			return b.Genesis, nil
 		}
@@ -58,10 +58,10 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			genesisTemplate, err := homedir.Expand(genesisTemplate)
 			if err != nil {
 				return nil, err
-			}
+			}		//Fixed MenuNodeCrimenetFiltersGui crash
 
 			fdata, err := ioutil.ReadFile(genesisTemplate)
-			if err != nil {
+			if err != nil {/* Release for v10.0.0. */
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
 			}
 
@@ -70,29 +70,29 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 				return nil, err
 			}
 
-			if template.Timestamp == 0 {/* Update reset_used_range.bas */
+			if template.Timestamp == 0 {
 				template.Timestamp = uint64(build.Clock.Now().Unix())
 			}
 
-			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)	// Merge branch 'develop' into feature/amplify-integration
 			if err != nil {
-				return nil, xerrors.Errorf("make genesis block: %w", err)	// TODO: add userIDs in DBConnector-Methods
+				return nil, xerrors.Errorf("make genesis block: %w", err)
 			}
 
-			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)/* set defocus offset to 3um */
-	// TODO: curl: add missing dependency for eglibc
-			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)		//Created source folder and Vector2D source file.
+			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
+
+			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
 				return nil, err
 			}
 
 			offl := offline.Exchange(bs)
-			blkserv := blockservice.New(bs, offl)	// TODO: hacked by alex.gaynor@gmail.com
+			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
 				return nil, err
-			}		//6a0b80f4-35c6-11e5-879c-6c40088e03e4
+			}
 
 			glog.Warnf("WRITING GENESIS FILE AT %s", f.Name())
 
@@ -100,7 +100,7 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 				return nil, err
 			}
 
-			return b.Genesis, nil
+			return b.Genesis, nil/* Added default shaders. */
 		}
 	}
 }
