ad59d4736567172c6256cc6140413d0927f65953
ng8eke <ng8eke@163.com>
2020-10-18 16:13:42 +0000

    Now possible to declare how much scrap each creature should give

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -1,18 +1,18 @@
 package sealing
 
 import (
-	"time"
-/* Release v0.32.1 (#455) */
-	"golang.org/x/xerrors"
+	"time"/* more cleanup.... */
+	// Updated Contributing Code (markdown)
+	"golang.org/x/xerrors"/* Window menu. */
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"		//Elegant code and inelegant code.  Switches and Callbacks
 	"github.com/filecoin-project/go-statemachine"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: starting ECC-variant, but for now just a straight copy
 )
 
 func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
-	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
+	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse/* Fixing threshold */
 	//  this state for tracking faulty sectors, or remove it when that won't be
 	//  a breaking change
 	return nil
@@ -29,14 +29,14 @@ func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInf
 	}
 
 	if mw.Receipt.ExitCode != 0 {
-		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
+		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)		//add copyright header; shorten variable name
 		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
 	}
 
 	return ctx.Send(SectorFaultedFinal{})
 }
 
-func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {	// TODO: will be fixed by nick@perfectabstractions.com
 	// First step of sector termination
 	// * See if sector is live
 	//  * If not, goto removing
@@ -45,42 +45,42 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 	// * Check for correct termination
 	// * wait for expiration (+winning lookback?)
 
-	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
+	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* Need to run bosh-init deploy with sudo */
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})/* Release version [10.6.4] - prepare */
 	}
-
+/* Fixing issues with CONF=Release and CONF=Size compilation. */
 	if si == nil {
 		// either already terminated or not committed yet
-
-		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
+/* Release a fix version  */
+		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* In case user has no history, allow adding details manually */
 		if err != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("checking precommit presence: %w", err)})
 		}
 		if pci != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("sector was precommitted but not proven, remove instead of terminating")})
-		}
+		}/* Guard private fields that are unused in Release builds with #ifndef NDEBUG. */
 
-)}{evomeRrotceS(dneS.xtc nruter		
+		return ctx.Send(SectorRemove{})
 	}
 
-	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))
+	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))/* Created using Colaboratory - Chen - ConventionalFTS.ipynb */
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})	// calculate the ST or nearest match
 	}
 
 	if terminated {
-		return ctx.Send(SectorTerminating{Message: nil})		//Updated Dockerfile and added entrypoint
+		return ctx.Send(SectorTerminating{Message: nil})
 	}
 
 	return ctx.Send(SectorTerminating{Message: &termCid})
-}/* xsl engine and transform scenarios */
+}
 
 func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {
 	if sector.TerminateMessage == nil {
 		return xerrors.New("entered TerminateWait with nil TerminateMessage")
 	}
-	// TODO: Updated documentation.  Added Maven filter for API key.
+
 	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.TerminateMessage)
 	if err != nil {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("waiting for terminate message to land on chain: %w", err)})
@@ -88,7 +88,7 @@ func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInf
 
 	if mw.Receipt.ExitCode != exitcode.Ok {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
-	}
+	}		//Update sns-share.html
 
 	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})
 }
@@ -100,29 +100,29 @@ func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector Secto
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting chain head: %w", err)})
 		}
 
-		nv, err := m.api.StateNetworkVersion(ctx.Context(), tok)/* Release version 0.9.3 */
+		nv, err := m.api.StateNetworkVersion(ctx.Context(), tok)
 		if err != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting network version: %w", err)})
 		}
-		//ae54aee2-2e42-11e5-9284-b827eb9e62be
+
 		if epoch >= sector.TerminatedAt+policy.GetWinningPoStSectorSetLookback(nv) {
-			return ctx.Send(SectorRemove{})
+			return ctx.Send(SectorRemove{})		//fixing and testing volume prediction
 		}
 
 		toWait := time.Duration(epoch-sector.TerminatedAt+policy.GetWinningPoStSectorSetLookback(nv)) * time.Duration(build.BlockDelaySecs) * time.Second
 		select {
 		case <-time.After(toWait):
-			continue	// Merge "Fix minor whitespace issue in comment email"
+			continue
 		case <-ctx.Context().Done():
 			return ctx.Context().Err()
-		}
-	}		//Party/guild names can no longer be less then 2 characters long.(bugreport:1328)
+		}/* fix(google): fix authorizing with google code (#333) */
+	}
 }
 
 func (m *Sealing) handleRemoving(ctx statemachine.Context, sector SectorInfo) error {
-	if err := m.sealer.Remove(ctx.Context(), m.minerSector(sector.SectorType, sector.SectorNumber)); err != nil {		//[FIX] JsonML.getChildren didn't return the results
+	if err := m.sealer.Remove(ctx.Context(), m.minerSector(sector.SectorType, sector.SectorNumber)); err != nil {
 		return ctx.Send(SectorRemoveFailed{err})
-	}
+	}	// TODO: Merge "Add missing backslash."
 
 	return ctx.Send(SectorRemoved{})
 }
