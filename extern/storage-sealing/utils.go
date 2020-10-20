2e00900a9ab334ecb5f5cdd00eddc66998895976
arajasek94 <arajasek94@gmail.com>
2020-10-20 23:19:58 +0000

    e15e8d32-2e40-11e5-9284-b827eb9e62be

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,21 +1,21 @@
 package sealing
-		//cdb1c9b6-2fbc-11e5-b64f-64700227155b
+		//Delete Nexion.txt
 import (
 	"math/bits"
-		//83000da1-2d15-11e5-af21-0401358ea401
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Added ROTATESHAPE */
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
-	// of user-usable data.
+	// of user-usable data.	// TODO: Merge "Change auto caps mode argument to int type" into jb-dev
 	//
-	// (1024/1016 = 128/127)/* Release 2.0.5. */
+	// (1024/1016 = 128/127)
 	//
 	// Given that we can get sector size by simply adding 1/127 of the user
-	// bytes	// TODO: will be fixed by souzau@yandex.com
+	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
@@ -31,8 +31,8 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		//d0f367fa-2e5a-11e5-9284-b827eb9e62be
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// remove ig link
 		// next bit
 		toFill ^= psize
 
@@ -44,14 +44,14 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {
+	if err := m.sectors.List(&sectors); err != nil {/* Add a work in progress import from mongo. */
 		return nil, err
-	}
+	}/* - Version 0.23 Release.  Minor features */
 	return sectors, nil
 }
 
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo
-)tuo&(teG.))dis(46tniu(teG.srotces.m =: rre	
+	var out SectorInfo/* Release of eeacms/eprtr-frontend:0.2-beta.24 */
+	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
