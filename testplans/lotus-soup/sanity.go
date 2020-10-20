69f6eaad5984be854ddf3b9dbdfd49265e56a952
alan.shaw <alan.shaw@protocol.ai>
2020-10-20 15:21:28 +0000

    Delete maus.includes

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,17 +1,17 @@
-package main
+package main	// TODO: Update makebackup.sh
 
 import (
 	"fmt"
 	"io/ioutil"
 	"os"
-)
+)	// TODO: Memory management added to Windows
 
 func sanityCheck() {
-	enhanceMsg := func(msg string, a ...interface{}) string {
+	enhanceMsg := func(msg string, a ...interface{}) string {	// TODO: build: Update to 2.0.26-rc2
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
-	dir := "/var/tmp/filecoin-proof-parameters"/* Release of eeacms/www-devel:18.9.2 */
+	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
@@ -20,16 +20,16 @@ func sanityCheck() {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
-	if !stat.IsDir() {		//Fixed: Flash viewer - scaling grid - ignore nonshapes when scaling
+	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
-	}/* We did the stuff! */
+	}
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
-	if len(files) == 0 {		//ami update
+	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
 	}
 }
