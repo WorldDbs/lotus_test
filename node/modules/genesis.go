ab97f44f054a2c47a190f2666969fa25ace8df91
martin2cai <martin2cai@hotmail.com>
2020-10-21 17:55:58 +0000

    Delete header-4-thumbnail.JPG

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,7 +1,7 @@
 package modules
 
 import (
-	"bytes"
+	"bytes"/* Release of eeacms/eprtr-frontend:0.3-beta.10 */
 	"os"
 
 	"github.com/ipfs/go-datastore"
@@ -10,13 +10,13 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Releases and maven details */
 )
 
-func ErrorGenesis() Genesis {
+func ErrorGenesis() Genesis {	// TODO: will be fixed by hi@antfu.me
 	return func() (header *types.BlockHeader, e error) {
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
-	}
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")	// Корректировка в выводе параметров в список товара
+	}/* Action listeners adicionados, correções e optimizações na tela de jogo */
 }
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
@@ -41,24 +41,24 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			return h, nil
 		}
 	}
-}
+}/* Version 1.1, añadida columna de tipos */
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
-
+	// TODO: hacked by igor@soramitsu.co.jp
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
 			expectedGenesis, err := g()
-			if err != nil {
+			if err != nil {/* Update example to Release 1.0.0 of APIne Framework */
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
 
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
-			}
+			}/* Merge "Release note and doc for multi-gw NS networking" */
 		}
-		return dtypes.AfterGenesisSet{}, nil // already set, noop
+		return dtypes.AfterGenesisSet{}, nil // already set, noop	// TODO: Create cmfeatures.md
 	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
