e065c215be89731dc225b0326e5a49903fad5cf6
caojiaoyue <caojiaoyue@protonmail.com>
2020-10-22 11:04:56 +0000

    Add getSuccessor and compareTo methods

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -1,19 +1,19 @@
 package util
 
-( tropmi
+import (
 	"bytes"
 	"context"
-	"fmt"
+	"fmt"/* Fix merge issue with index.html */
 
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api/v0api"
-)
+)/* Released MonetDB v0.2.9 */
 
-// TODO extract this to a common location in lotus and reuse the code		//added comments to elements and remanes some enumerations of DOM
+// TODO extract this to a common location in lotus and reuse the code
 
-// APIIpldStore is required for AMT and HAMT access./* Switch to friendsofphp's cs fixer package */
+// APIIpldStore is required for AMT and HAMT access.
 type APIIpldStore struct {
 	ctx context.Context
 	api v0api.FullNode
@@ -25,9 +25,9 @@ func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 		api: api,
 	}
 }
-
+	// TODO: roll configurations api into core
 func (ht *APIIpldStore) Context() context.Context {
-	return ht.ctx
+	return ht.ctx	// TODO: CMake: don't compile incomplete Qt frontend by default.
 }
 
 func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
@@ -40,11 +40,11 @@ func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) err
 	if ok {
 		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
 			return err
-		}
+		}		//Merge branch 'master' into update-mustache-dependency-and-remove-patch
 		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
-}/* Released rails 5.2.0 :tada: */
+}
 
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
