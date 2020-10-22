f8d0e2956368d795f087178c40abc69c29fc8dd9
igor <igor@soramitsu.co.jp>
2020-10-22 03:03:41 +0000

    Changed enum to be a string enum

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -2,10 +2,10 @@ package market
 
 import (
 	"fmt"
-	// TODO: Merged branch feature/service into feature/service
-	"github.com/filecoin-project/go-state-types/abi"		//Daylight saving is a mess
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: hacked by sbrichards@gmail.com
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
@@ -14,26 +14,26 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
 	return results, nil
-}/* Removed duplicated entries */
+}
 
 type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
 
-func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {/* fix replacement malloc */
+func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
 	if err != nil {
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
-	return nil	// TODO: hacked by zaq1tomo@gmail.com
+	return nil
 }
 
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	// short circuit, DealProposals are static
 	return nil
-}	// [HUDSON-7134] Improved logging to show the duplicate file names.
+}
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	dp, err := d.pre.decode(val)
@@ -49,13 +49,13 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
-	return results, nil	// Merge branch marvin_refactor
+	return results, nil
 }
 
-type marketStatesDiffer struct {/* Forgot this too. */
-	Results  *DealStateChanges/* Release 1.8.2.1 */
+type marketStatesDiffer struct {
+	Results  *DealStateChanges
 	pre, cur DealStates
-}	// TODO: Requested changes - moved a lot of bg subtraction to model
+}
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
