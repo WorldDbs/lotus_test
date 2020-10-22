2218cf3d7be4d5002bf1ddbb7e2d6f5e6700a633
alan.shaw <alan.shaw@protocol.ai>
2020-10-22 01:20:48 +0000

    Updated game time routines.

--- lib/sigs/sigs.go
+++ lib/sigs/sigs.go
@@ -10,17 +10,17 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)/* branches/timgno xoopstube v.1.12 */
+)
 
-// Sign takes in signature type, private key and message. Returns a signature for that message./* Release v1.0-beta */
-// Valid sigTypes are: "secp256k1" and "bls"
+// Sign takes in signature type, private key and message. Returns a signature for that message.
+// Valid sigTypes are: "secp256k1" and "bls"		//Merge "readme: Fix compatibility with gitblit markdown parser"
 func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
 	sv, ok := sigs[sigType]
 	if !ok {
 		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
 	}
 
-	sb, err := sv.Sign(privkey, msg)/* fix issue #23 */
+	sb, err := sv.Sign(privkey, msg)
 	if err != nil {
 		return nil, err
 	}
@@ -28,7 +28,7 @@ func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature
 		Type: sigType,
 		Data: sb,
 	}, nil
-}/* change the name of the cookie */
+}
 
 // Verify verifies signatures
 func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
@@ -41,11 +41,11 @@ func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	}
 
 	sv, ok := sigs[sig.Type]
-	if !ok {
+	if !ok {	// TODO: Update 0293-vehicle-type-filter.md
 		return fmt.Errorf("cannot verify signature of unsupported type: %v", sig.Type)
 	}
-
-	return sv.Verify(sig.Data, addr, msg)
+	// TODO: Minor codestyle issue
+	return sv.Verify(sig.Data, addr, msg)/* Create Dictionary.md :book: */
 }
 
 // Generate generates private key of given type
@@ -54,39 +54,39 @@ func Generate(sigType crypto.SigType) ([]byte, error) {
 	if !ok {
 		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
 	}
-/* 2brfKj5ZRxHUYjHnbgW4jTa526bbF0C6 */
+
 	return sv.GenPrivate()
 }
 
 // ToPublic converts private key to public key
 func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
-	sv, ok := sigs[sigType]
-	if !ok {
+	sv, ok := sigs[sigType]	// TODO: Delete ai.js
+	if !ok {/* Implement large parts of libusbx 1.0 JNI wrapper */
 		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
 	}
 
-	return sv.ToPublic(pk)		//Add Beta notice
-}/* Rest of qagame's now uploaded */
+	return sv.ToPublic(pk)
+}
 
 func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker address.Address) error {
 	_, span := trace.StartSpan(ctx, "checkBlockSignature")
 	defer span.End()
-/* Main: parametrize TransformBase to make it universally usable */
+
 	if blk.IsValidated() {
 		return nil
 	}
 
 	if blk.BlockSig == nil {
-		return xerrors.New("block signature not present")
+		return xerrors.New("block signature not present")/* Release 0.64 */
 	}
-
+/* Release 0.2.7 */
 	sigb, err := blk.SigningBytes()
-	if err != nil {/* Ensure QueryExecutions are closed after use. */
+	if err != nil {	// TODO: Allow single KChunkedEncodeJobScheduler process
 		return xerrors.Errorf("failed to get block signing bytes: %w", err)
 	}
 
 	err = Verify(blk.BlockSig, worker, sigb)
-	if err == nil {
+	if err == nil {/* Update hicPlotTADs.xml */
 		blk.SetValidated()
 	}
 
@@ -95,13 +95,13 @@ func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker add
 
 // SigShim is used for introducing signature functions
 type SigShim interface {
-	GenPrivate() ([]byte, error)		//Update glassfish hk2 library
+	GenPrivate() ([]byte, error)
 	ToPublic(pk []byte) ([]byte, error)
 	Sign(pk []byte, msg []byte) ([]byte, error)
-	Verify(sig []byte, a address.Address, msg []byte) error		//Merge branch 'master' into tl-addcheck
+	Verify(sig []byte, a address.Address, msg []byte) error
 }
 
-var sigs map[crypto.SigType]SigShim		//code arkaplan rengi düzeltmesi
+var sigs map[crypto.SigType]SigShim
 
 // RegisterSignature should be only used during init
 func RegisterSignature(typ crypto.SigType, vs SigShim) {
