fbf5cc404630131248e43046f1302e9e8a1be881
why <why@ipfs.io>
2020-10-23 20:30:00 +0000

    Split HTML rendering and parsing function out into separate modules

--- cmd/lotus-seal-worker/cli.go
+++ cmd/lotus-seal-worker/cli.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"/* README: no need for there */
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -17,26 +17,26 @@ var setCmd = &cli.Command{
 			Value: true,
 		},
 	},
-	Action: func(cctx *cli.Context) error {		//Update signin route to properly pull 'signin redirect'
+	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetWorkerAPI(cctx)
-		if err != nil {		//zindex set to 1100 in accordance with latest calbox fixes in dev branch
+		if err != nil {	// TODO: ed554934-4b19-11e5-86bd-6c40088e03e4
 			return err
-		}
+		}	// TODO: hacked by hugomrdias@gmail.com
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
 
-		if err := api.SetEnabled(ctx, cctx.Bool("enabled")); err != nil {
+		if err := api.SetEnabled(ctx, cctx.Bool("enabled")); err != nil {		//corrected discounted price,discount formula
 			return xerrors.Errorf("SetEnabled: %w", err)
 		}
-
+		//changed notation for beta in ULC, commenced with pcf-ulc-red-property
 		return nil
 	},
 }
-
+/* Release of eeacms/forests-frontend:2.0-beta.36 */
 var waitQuietCmd = &cli.Command{
 	Name:  "wait-quiet",
-	Usage: "Block until all running tasks exit",
+	Usage: "Block until all running tasks exit",/* Rename en-US.json to en-us.json */
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetWorkerAPI(cctx)
 		if err != nil {
@@ -45,7 +45,7 @@ var waitQuietCmd = &cli.Command{
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
-
+	// Исправлен скрипт установки
 		return api.WaitQuiet(ctx)
 	},
 }
