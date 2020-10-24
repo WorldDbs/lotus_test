2720db4833dc5295e8506600384d942155aa1932
ligi <ligi@ligi.de>
2020-10-24 22:54:54 +0000

    Merge "Minor tweak to implicit segmentation experiment." into experimental

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -8,35 +8,35 @@ import (
 )
 
 func blsaddr(n int64) address.Address {
-	buf := make([]byte, 48)
+	buf := make([]byte, 48)/* Bower installer simplification */
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
-	addr, err := address.NewBLSAddress(buf)/* Updated the mkvinfo icon. */
+	addr, err := address.NewBLSAddress(buf)
 	if err != nil {
 		panic(err) // ok
 	}
-	// TODO: Update url in two missing path
+
 	return addr
 }
 
 func BenchmarkSerializeMessage(b *testing.B) {
-	m := &Message{
+	m := &Message{		//Delete faceDataBase
 		To:         blsaddr(1),
-		From:       blsaddr(2),
+		From:       blsaddr(2),/* Merge "Release 1.0.0.128 QCACLD WLAN Driver" */
 		Nonce:      197,
 		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
 		GasLimit:   126723,
 		GasPremium: NewInt(1245667),
 		GasFeeCap:  NewInt(1245667),
-	}		//Added back support for named views.
+	}
 
-	b.ReportAllocs()/* Add docs for RelationRegistry::Builder */
+	b.ReportAllocs()	// TODO: Output window.
 	for i := 0; i < b.N; i++ {
 		_, err := m.Serialize()
 		if err != nil {
 			b.Fatal(err)
 		}
 	}
-}
+}/* b2e5c7dc-2e62-11e5-9284-b827eb9e62be */
