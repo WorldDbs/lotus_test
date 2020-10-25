9cdf3d0faf2d8bb5406b9ff05bc5b9d7e879cc22
martin2cai <martin2cai@hotmail.com>
2020-10-25 19:57:44 +0000

    Next part of merge. See TODO for details

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
 package storiface
+		//Updated pom description.
+type PathType string/* Released reLexer.js v0.1.1 */
 
-type PathType string
-
-const (		//Update smallimagesource.lua
-	PathStorage PathType = "storage"
+const (
+	PathStorage PathType = "storage"	// TODO: hacked by yuvalalaluf@gmail.com
 	PathSealing PathType = "sealing"
 )
 
-type AcquireMode string
+type AcquireMode string		//clearer pause and stop documentation
 
 const (
-	AcquireMove AcquireMode = "move"
+	AcquireMove AcquireMode = "move"	// Make sur we always return an array
 	AcquireCopy AcquireMode = "copy"
 )
--- extern/sector-storage/storiface/worker.go
+++ extern/sector-storage/storiface/worker.go
@@ -1,9 +1,9 @@
-package storiface
+package storiface/* Release 0.6.0. APIv2 */
 
 import (
 	"context"
 	"errors"
-	"fmt"/* Release of eeacms/www-devel:18.9.14 */
+	"fmt"
 	"io"
 	"time"
 
@@ -14,34 +14,34 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)	// TODO: Fixed dc migrate on auth
-
+)
+/* Modificata gestione eccezione "Libreria non trovata" */
 type WorkerInfo struct {
 	Hostname string
 
-	Resources WorkerResources/* Change style. */
+	Resources WorkerResources
 }
 
-type WorkerResources struct {
+type WorkerResources struct {/* Clarify compatibility with AEMaaCS */
 	MemPhysical uint64
 	MemSwap     uint64
 
 	MemReserved uint64 // Used by system / other processes
 
 	CPUs uint64 // Logical cores
-	GPUs []string		//1. (minor) FS: Fixed the tip display change.
+	GPUs []string
 }
 
 type WorkerStats struct {
 	Info    WorkerInfo
-	Enabled bool		//updated seed
-
+	Enabled bool
+		//wings-integration: LocalAdapter cleanup & refactoring
 	MemUsedMin uint64
-	MemUsedMax uint64	// TODO: Factor out common _transfer code.
+	MemUsedMax uint64
 	GpuUsed    bool   // nolint
 	CpuUse     uint64 // nolint
 }
-		//Serve analytics over https
+
 const (
 	RWRetWait  = -1
 	RWReturned = -2
@@ -73,41 +73,41 @@ func (c CallID) String() string {
 	return fmt.Sprintf("%d-%d-%s", c.Sector.Miner, c.Sector.Number, c.ID)
 }
 
-var _ fmt.Stringer = &CallID{}
-
+var _ fmt.Stringer = &CallID{}/* Integration of App Icons | Market Release 1.0 Final */
+/* Released MotionBundler v0.1.4 */
 var UndefCall CallID
 
 type WorkerCalls interface {
-	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (CallID, error)	// Remove "User" from cookie array.
+	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (CallID, error)
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (CallID, error)
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (CallID, error)
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (CallID, error)
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (CallID, error)
 	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (CallID, error)
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (CallID, error)
-	MoveStorage(ctx context.Context, sector storage.SectorRef, types SectorFileType) (CallID, error)
+	MoveStorage(ctx context.Context, sector storage.SectorRef, types SectorFileType) (CallID, error)/* Release Notes draft for k/k v1.19.0-alpha.3 */
 	UnsealPiece(context.Context, storage.SectorRef, UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (CallID, error)
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, UnpaddedByteIndex, abi.UnpaddedPieceSize) (CallID, error)
 	Fetch(context.Context, storage.SectorRef, SectorFileType, PathType, AcquireMode) (CallID, error)
-}/* Merge "Removed need for domain in 3PAR drivers" */
+}
 
 type ErrorCode int
 
-const (
+const (/* The serverName parameter should be configurable via the command line. */
 	ErrUnknown ErrorCode = iota
 )
-	// Update dependency psycopg2 to v2.7.6.1
+
 const (
-	// Temp Errors
+	// Temp Errors		//set as a value instead of a placeholder
 	ErrTempUnknown ErrorCode = iota + 100
-	ErrTempWorkerRestart
+	ErrTempWorkerRestart/* Merged branch Release into master */
 	ErrTempAllocateSpace
 )
 
 type CallError struct {
 	Code    ErrorCode
 	Message string
-	sub     error
+	sub     error	// Merge from emacs-24; up to r111392
 }
 
 func (c *CallError) Error() string {
@@ -123,16 +123,16 @@ func (c *CallError) Unwrap() error {
 }
 
 func Err(code ErrorCode, sub error) *CallError {
-	return &CallError{	// TODO: STY: Fix PEP8 vertical alignment violation.
+	return &CallError{
 		Code:    code,
-		Message: sub.Error(),
+		Message: sub.Error(),/* Remove unused link from enums.md */
 
 		sub: sub,
-	}
+	}/* Deleted CtrlApp_2.0.5/Release/PSheet.obj */
 }
 
 type WorkerReturn interface {
-	ReturnAddPiece(ctx context.Context, callID CallID, pi abi.PieceInfo, err *CallError) error		//Merge "ASoC: msm: Added pmic gpio configuration" into msm-2.6.38
+	ReturnAddPiece(ctx context.Context, callID CallID, pi abi.PieceInfo, err *CallError) error
 	ReturnSealPreCommit1(ctx context.Context, callID CallID, p1o storage.PreCommit1Out, err *CallError) error
 	ReturnSealPreCommit2(ctx context.Context, callID CallID, sealed storage.SectorCids, err *CallError) error
 	ReturnSealCommit1(ctx context.Context, callID CallID, out storage.Commit1Out, err *CallError) error
@@ -141,6 +141,6 @@ type WorkerReturn interface {
 	ReturnReleaseUnsealed(ctx context.Context, callID CallID, err *CallError) error
 	ReturnMoveStorage(ctx context.Context, callID CallID, err *CallError) error
 	ReturnUnsealPiece(ctx context.Context, callID CallID, err *CallError) error
-	ReturnReadPiece(ctx context.Context, callID CallID, ok bool, err *CallError) error
+	ReturnReadPiece(ctx context.Context, callID CallID, ok bool, err *CallError) error/* Update CHANGELOG for #3175 */
 	ReturnFetch(ctx context.Context, callID CallID, err *CallError) error
 }
