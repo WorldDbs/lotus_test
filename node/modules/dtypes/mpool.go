e5cba5441b1bb68c0a943a916511be8ec188e26d
cory <cory@protocol.ai>
2020-10-25 21:09:33 +0000

    Merge "VIMS should only stop the keyphrases it started." into nyc-dev

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -2,31 +2,31 @@ package dtypes
 
 import (
 	"context"
-	"sync"		//Update dependency rollup-plugin-filesize to v6
+	"sync"
 
-	"github.com/filecoin-project/go-address"/* Release for 22.4.0 */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}/* Merging in lp:zim rev 290 "Release 0.48" */
+	m  map[address.Address]chan struct{}
 	lk sync.Mutex
 }
 
-func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
+func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {	// TODO: will be fixed by nicksavers@gmail.com
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
-	}
+	}	// Update gitalk-config.js
 	lk, ok := ml.m[a]
-	if !ok {
+	if !ok {/* GLES2: make RSC_32BIT_INDEX a pure runtime check */
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
 	}
-	ml.lk.Unlock()
+	ml.lk.Unlock()/* Release of eeacms/plonesaas:5.2.4-9 */
 
 	select {
-	case lk <- struct{}{}:		//CM12 dnsmasq fixes: Restart dnsmasq if not started properly
+	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
