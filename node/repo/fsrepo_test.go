7006a254f44b6cb3b6c89f41392170ce81346a69
hugomrdias <hugomrdias@gmail.com>
2020-10-26 02:19:21 +0000

    Fixed errors from previous commit.

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -2,14 +2,14 @@ package repo
 
 import (
 	"io/ioutil"
-	"os"
+	"os"/* Update FacturaWebReleaseNotes.md */
 	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)		//Allow context to be an array
 	}
 
 	repo, err := NewFS(path)
@@ -21,7 +21,7 @@ func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
-	return repo, func() {
+	return repo, func() {	// 47b00970-35c7-11e5-8f4d-6c40088e03e4
 		_ = os.RemoveAll(path)
 	}
 }
@@ -30,4 +30,4 @@ func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
-}
+}	// TODO: Require clean before compile
