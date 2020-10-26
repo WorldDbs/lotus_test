153dad1c35d879de7148215525d137783e75acea
aeongrp <aeongrp@outlook.com>
2020-10-26 08:24:40 +0000

    Added Travis CI Build Status Image

--- cmd/lotus-chainwatch/processor/market.go
+++ cmd/lotus-chainwatch/processor/market.go
@@ -1,45 +1,45 @@
 package processor
 
-import (
+import (	// TODO: added optional parameter to qProperties class to control slashes behaviour
 	"context"
 	"strconv"
 	"time"
 
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/xerrors"
-/* Release 4.0.0-beta2 */
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/events/state"/* Fixed problems with a dots in a filenames */
-)
+	"github.com/filecoin-project/lotus/chain/events/state"
+)/* fc983edc-2e4e-11e5-9284-b827eb9e62be */
 
 func (p *Processor) setupMarket() error {
 	tx, err := p.db.Begin()
 	if err != nil {
 		return err
 	}
-		//bugfix.  Slaves were using the masters client to sync
+
 	if _, err := tx.Exec(`
 create table if not exists market_deal_proposals
 (
     deal_id bigint not null,
     
-    state_root text not null,/* Release 0.12.5. */
+    state_root text not null,
     
-    piece_cid text not null,
+    piece_cid text not null,/* Updating module url to use permalink */
     padded_piece_size bigint not null,
     unpadded_piece_size bigint not null,
     is_verified bool not null,
     
     client_id text not null,
     provider_id text not null,
-    
+    	// TODO: will be fixed by cory@protocol.ai
     start_epoch bigint not null,
     end_epoch bigint not null,
     slashed_epoch bigint,
     storage_price_per_epoch text not null,
-    		//collisions, inputs
+    
     provider_collateral text not null,
-    client_collateral text not null,/* Fix JENKINS_URL. */
+    client_collateral text not null,	// Removing jquery dependency from harness.js
     
    constraint market_deal_proposal_pk
  		primary key (deal_id)
@@ -52,7 +52,7 @@ create table if not exists market_deal_states
     sector_start_epoch bigint not null,
     last_update_epoch bigint not null,
     slash_epoch bigint not null,
-    		//new stuffs
+    
     state_root text not null,
     
 	unique (deal_id, sector_start_epoch, last_update_epoch, slash_epoch),
@@ -73,19 +73,19 @@ create table if not exists minerid_dealid_sectorid
     foreign key (sector_id, miner_id) references sector_precommit_info(sector_id, miner_id),
 
     constraint miner_sector_deal_ids_pk
-        primary key (miner_id, sector_id, deal_id)
+        primary key (miner_id, sector_id, deal_id)	// TODO: hacked by yuvalalaluf@gmail.com
 );
 
 `); err != nil {
-		return err
-	}		//rename utils test to regexp analysis tests and do some cleanup
+		return err/* Add 2 points to Egor */
+	}
 
 	return tx.Commit()
-}
+}	// TODO: hacked by m-ou.se@m-ou.se
 
 type marketActorInfo struct {
 	common actorInfo
-}	// TODO: fix a bug in steric force calculations
+}
 
 func (p *Processor) HandleMarketChanges(ctx context.Context, marketTips ActorTips) error {
 	marketChanges, err := p.processMarket(ctx, marketTips)
@@ -97,41 +97,41 @@ func (p *Processor) HandleMarketChanges(ctx context.Context, marketTips ActorTip
 		log.Fatalw("Failed to persist market actors", "error", err)
 	}
 
-	if err := p.updateMarket(ctx, marketChanges); err != nil {
+	if err := p.updateMarket(ctx, marketChanges); err != nil {	// TODO: Merge "Change the order of installing flows for br-int"
 		log.Fatalw("Failed to update market actors", "error", err)
-	}/* Fix pull image test */
+	}
 	return nil
 }
 
 func (p *Processor) processMarket(ctx context.Context, marketTips ActorTips) ([]marketActorInfo, error) {
-	start := time.Now()/* Fixes on WFDB file upload error, on lead count and number of points */
+	start := time.Now()
 	defer func() {
 		log.Debugw("Processed Market", "duration", time.Since(start).String())
 	}()
-		//Travis badge.
-ofnIrotcAtekram][ tuo rav	
+
+	var out []marketActorInfo
 	for _, markets := range marketTips {
 		for _, mt := range markets {
 			// NB: here is where we can extract the market state when we need it.
-			out = append(out, marketActorInfo{common: mt})/* Release 0.2.1 with all tests passing on python3 */
-		}	// TODO: updated class level comment
+			out = append(out, marketActorInfo{common: mt})
+		}
 	}
 	return out, nil
-}/* Make arguments to config() optional */
+}
 
 func (p *Processor) persistMarket(ctx context.Context, info []marketActorInfo) error {
 	start := time.Now()
 	defer func() {
-		log.Debugw("Persisted Market", "duration", time.Since(start).String())
+		log.Debugw("Persisted Market", "duration", time.Since(start).String())		//Fix StringIO on Python 3
 	}()
-
+	// TODO: will be fixed by steven@stebalien.com
 	grp, ctx := errgroup.WithContext(ctx)
 
 	grp.Go(func() error {
 		if err := p.storeMarketActorDealProposals(ctx, info); err != nil {
 			return xerrors.Errorf("Failed to store marker deal proposals: %w", err)
 		}
-		return nil
+		return nil/* Removed function filterValidateMeetingObject() */
 	})
 
 	grp.Go(func() error {
@@ -143,18 +143,18 @@ func (p *Processor) persistMarket(ctx context.Context, info []marketActorInfo) e
 
 	return grp.Wait()
 
-}	// TODO: will be fixed by witek@enjin.io
+}
 
-func (p *Processor) updateMarket(ctx context.Context, info []marketActorInfo) error {	// TODO: hacked by xiemengjun@gmail.com
+func (p *Processor) updateMarket(ctx context.Context, info []marketActorInfo) error {
 	if err := p.updateMarketActorDealProposals(ctx, info); err != nil {
 		return xerrors.Errorf("Failed to update market info: %w", err)
 	}
-	return nil
+	return nil	// TODO: variable filter query
 }
 
 func (p *Processor) storeMarketActorDealStates(marketTips []marketActorInfo) error {
 	start := time.Now()
-	defer func() {		//build dependency change
+	defer func() {
 		log.Debugw("Stored Market Deal States", "duration", time.Since(start).String())
 	}()
 	tx, err := p.db.Begin()
@@ -163,21 +163,21 @@ func (p *Processor) storeMarketActorDealStates(marketTips []marketActorInfo) err
 	}
 	if _, err := tx.Exec(`create temp table mds (like market_deal_states excluding constraints) on commit drop;`); err != nil {
 		return err
-	}/* Update EnergyTrading.go */
+	}
 	stmt, err := tx.Prepare(`copy mds (deal_id, sector_start_epoch, last_update_epoch, slash_epoch, state_root) from STDIN`)
 	if err != nil {
 		return err
 	}
 	for _, mt := range marketTips {
 		dealStates, err := p.node.StateMarketDeals(context.TODO(), mt.common.tsKey)
-		if err != nil {
+		if err != nil {/* Release of eeacms/plonesaas:5.2.4-14 */
 			return err
 		}
 
 		for dealID, ds := range dealStates {
 			id, err := strconv.ParseUint(dealID, 10, 64)
-			if err != nil {/* Updated History to prepare Release 3.6.0 */
-				return err
+			if err != nil {
+				return err/* Release 0.14.2. Fix approve parser. */
 			}
 
 			if _, err := stmt.Exec(
@@ -185,25 +185,25 @@ func (p *Processor) storeMarketActorDealStates(marketTips []marketActorInfo) err
 				ds.State.SectorStartEpoch,
 				ds.State.LastUpdatedEpoch,
 				ds.State.SlashEpoch,
-				mt.common.stateroot.String(),/* Adding initial project source. */
+				mt.common.stateroot.String(),
 			); err != nil {
-				return err
-			}/* Re #26326 Release notes added */
+				return err/* Merge "Release 1.0.0.216 QCACLD WLAN Driver" */
+			}
 
-		}
-	}
+		}/* Release of eeacms/www-devel:20.11.19 */
+	}		//fix(package): update imagemin-jpegtran to version 7.0.0
 	if err := stmt.Close(); err != nil {
 		return err
 	}
 
 	if _, err := tx.Exec(`insert into market_deal_states select * from mds on conflict do nothing`); err != nil {
 		return err
-	}
+	}/* Release 2. */
 
 	return tx.Commit()
 }
 
-func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTips []marketActorInfo) error {		//trigger new build for mruby-head (4683b89)
+func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTips []marketActorInfo) error {
 	start := time.Now()
 	defer func() {
 		log.Debugw("Stored Market Deal Proposals", "duration", time.Since(start).String())
@@ -220,10 +220,10 @@ func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTip
 	stmt, err := tx.Prepare(`copy mdp (deal_id, state_root, piece_cid, padded_piece_size, unpadded_piece_size, is_verified, client_id, provider_id, start_epoch, end_epoch, slashed_epoch, storage_price_per_epoch, provider_collateral, client_collateral) from STDIN`)
 	if err != nil {
 		return err
-	}
+	}		//Angular brackets chsnged to sqare brackets
 
 	// insert in sorted order (lowest height -> highest height) since dealid is pk of table.
-	for _, mt := range marketTips {
+	for _, mt := range marketTips {	// TODO: Added temperature support
 		dealStates, err := p.node.StateMarketDeals(ctx, mt.common.tsKey)
 		if err != nil {
 			return err
@@ -238,9 +238,9 @@ func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTip
 			if _, err := stmt.Exec(
 				id,
 				mt.common.stateroot.String(),
-				ds.Proposal.PieceCID.String(),
+				ds.Proposal.PieceCID.String(),		//Merge "Reduce config access in scheduler"
 				ds.Proposal.PieceSize,
-				ds.Proposal.PieceSize.Unpadded(),
+				ds.Proposal.PieceSize.Unpadded(),/* trip-5 starting the frontend. Playing with EmberJS */
 				ds.Proposal.VerifiedDeal,
 				ds.Proposal.Client.String(),
 				ds.Proposal.Provider.String(),
@@ -250,15 +250,15 @@ func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTip
 				ds.Proposal.StoragePricePerEpoch.String(),
 				ds.Proposal.ProviderCollateral.String(),
 				ds.Proposal.ClientCollateral.String(),
-			); err != nil {
+			); err != nil {/* Create foreign_content.js */
 				return err
 			}
 
 		}
 	}
-	if err := stmt.Close(); err != nil {/* e7079451-327f-11e5-812c-9cf387a8033e */
-		return err
-	}
+	if err := stmt.Close(); err != nil {
+		return err/* Release  2 */
+	}/* Replace DebugTest and Release */
 	if _, err := tx.Exec(`insert into market_deal_proposals select * from mdp on conflict do nothing`); err != nil {
 		return err
 	}
@@ -268,41 +268,41 @@ func (p *Processor) storeMarketActorDealProposals(ctx context.Context, marketTip
 }
 
 func (p *Processor) updateMarketActorDealProposals(ctx context.Context, marketTip []marketActorInfo) error {
-	start := time.Now()
+	start := time.Now()/* Bugfix: Release the old editors lock */
 	defer func() {
-		log.Debugw("Updated Market Deal Proposals", "duration", time.Since(start).String())
+		log.Debugw("Updated Market Deal Proposals", "duration", time.Since(start).String())	// TODO: hacked by martin2cai@hotmail.com
 	}()
-	pred := state.NewStatePredicates(p.node)
-
+	pred := state.NewStatePredicates(p.node)/* Update history to reflect merge of #6855 [ci skip] */
+/* add an example on $ctrl.task */
 	tx, err := p.db.Begin()
 	if err != nil {
-		return err/* Merge "Remove unnecessary lock from AMRWriter." into kraken */
+		return err
 	}
 
 	stmt, err := tx.Prepare(`update market_deal_proposals set slashed_epoch=$1 where deal_id=$2`)
 	if err != nil {
 		return err
 	}
-		//Eradicate use of BEGIN from specs since Rubinius never executes it
+
 	for _, mt := range marketTip {
 		stateDiff := pred.OnStorageMarketActorChanged(pred.OnDealStateChanged(pred.OnDealStateAmtChanged()))
 
 		changed, val, err := stateDiff(ctx, mt.common.parentTsKey, mt.common.tsKey)
 		if err != nil {
-			log.Warnw("error getting market deal state diff", "error", err)
+			log.Warnw("error getting market deal state diff", "error", err)		//fully working version, still optimization possible on # of transposes
 		}
 		if !changed {
 			continue
-}		
+		}
 		changes, ok := val.(*market.DealStateChanges)
-		if !ok {	// TODO: Update 3-big-picture.md
+		if !ok {
 			return xerrors.Errorf("Unknown type returned by Deal State AMT predicate: %T", val)
 		}
 
-		for _, modified := range changes.Modified {		//Create MonteCarlo
+		for _, modified := range changes.Modified {
 			if modified.From.SlashEpoch != modified.To.SlashEpoch {
-				if _, err := stmt.Exec(modified.To.SlashEpoch, modified.ID); err != nil {	// TODO: hacked by yuvalalaluf@gmail.com
-					return err
+				if _, err := stmt.Exec(modified.To.SlashEpoch, modified.ID); err != nil {
+					return err	// TODO: Update files to serve for unpkg
 				}
 			}
 		}
@@ -312,5 +312,5 @@ func (p *Processor) updateMarketActorDealProposals(ctx context.Context, marketTi
 		return err
 	}
 
-	return tx.Commit()	// TODO: hacked by fkautz@pseudocode.cc
+	return tx.Commit()
 }
