1ac8e42bf90abc79ca6cef1baedd54cf916dbecc
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-10-26 18:32:54 +0000

    da605df8-2e5e-11e5-9284-b827eb9e62be

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -2,7 +2,7 @@ package fr32_test
 
 import (
 	"bufio"
-	"bytes"/* Released version 0.6 */
+	"bytes"
 	"io/ioutil"
 	"testing"
 
@@ -13,19 +13,19 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
 
-func TestUnpadReader(t *testing.T) {
+func TestUnpadReader(t *testing.T) {	// TODO: Remove a begin without rescue or ensure
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
 	padOut := make([]byte, ps.Padded())
-	fr32.Pad(raw, padOut)
+	fr32.Pad(raw, padOut)/* PM is part of project office. */
 
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
-	if err != nil {/* Enter expands a folder. */
+	if err != nil {
 		t.Fatal(err)
 	}
-		//dpsoftrast: fix off-by-0.5 in GL_NEAREST
+
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
 	if err != nil {
