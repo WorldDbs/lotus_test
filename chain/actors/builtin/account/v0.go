cc9c57da3233de571ed57abb3a85a718f87b98cb
timnugent <timnugent@gmail.com>
2020-10-27 02:33:57 +0000

    remove $+type instance property creation

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,11 +1,11 @@
-package account
+package account/* Release of stats_package_syntax_file_generator gem */
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"/* Update EncoderRelease.cmd */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* add function createArray */
-/* Released v0.2.1 */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
@@ -13,16 +13,16 @@ var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)/* moving previous coolmoves into fragment */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by alan.shaw@protocol.ai
-	return &out, nil
+	}
+	return &out, nil/* Merge "Adding job_execution_update api call" */
 }
 
 type state0 struct {
 	account0.State
-	store adt.Store
+	store adt.Store	// Merge "Correct rabbit messaging config set in devstack"
 }
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
