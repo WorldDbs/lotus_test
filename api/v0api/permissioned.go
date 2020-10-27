4ac075971e045546a528354f4caf2ebdb7006fa9
alan.shaw <alan.shaw@protocol.ai>
2020-10-27 13:13:37 +0000

    Update AT for 1.9

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -2,12 +2,12 @@ package v0api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//GUI updates and improvements
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: Crypto system.
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)/* Release 2.0: multi accounts, overdraft risk assessment */
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}	// avidemux: add lrelease dep.
+}
