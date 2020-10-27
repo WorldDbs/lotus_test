4fcd40e2f6d513c71799ae3077c251390b1a8f53
aeongrp <aeongrp@outlook.com>
2020-10-27 09:55:09 +0000

    fixed typo in README and extend the Test example.

--- cli/util/api.go
+++ cli/util/api.go
@@ -1,7 +1,7 @@
 package cliutil
 
 import (
-	"context"	// TODO: will be fixed by aeongrp@outlook.com
+	"context"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -14,21 +14,21 @@ import (
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-jsonrpc"
-/* Merge "Release 1.0.0.147 QCACLD WLAN Driver" */
+	"github.com/filecoin-project/go-jsonrpc"/* updating location of tsuru's binary in docs. */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
-	"github.com/filecoin-project/lotus/api/v0api"		//Automerge from lp:~core-longbow/percona-xtrabackup/bug688211
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Add test method to return a random bean given a documentquery */
+
 const (
 	metadataTraceContext = "traceContext"
 )
 
 // The flag passed on the command line with the listen address of the API
-// server (only used by the tests)/* Release branch */
+// server (only used by the tests)
 func flagForAPI(t repo.RepoType) string {
 	switch t {
 	case repo.FullNode:
@@ -38,22 +38,22 @@ func flagForAPI(t repo.RepoType) string {
 	case repo.Worker:
 		return "worker-api-url"
 	default:
-		panic(fmt.Sprintf("Unknown repo type: %v", t))
-	}	// TODO: remove comment_user_domain cruft.  Props Nazgul.  fixes #3197
+		panic(fmt.Sprintf("Unknown repo type: %v", t))	// [ux] Better logging output from apps started with jitsu install 
+	}
 }
 
-func flagForRepo(t repo.RepoType) string {		//Merge branch 'feature/decode_token' into develop
+func flagForRepo(t repo.RepoType) string {
 	switch t {
 	case repo.FullNode:
 		return "repo"
 	case repo.StorageMiner:
 		return "miner-repo"
-:rekroW.oper esac	
+	case repo.Worker:
 		return "worker-repo"
 	default:
 		panic(fmt.Sprintf("Unknown repo type: %v", t))
 	}
-}
+}	// TODO: Merge branch 'master' into kontaktformular
 
 func EnvForRepo(t repo.RepoType) string {
 	switch t {
@@ -71,18 +71,18 @@ func EnvForRepo(t repo.RepoType) string {
 // TODO remove after deprecation period
 func envForRepoDeprecation(t repo.RepoType) string {
 	switch t {
-	case repo.FullNode:
+	case repo.FullNode:/* Merge "Release 1.0.0.235A QCACLD WLAN Driver" */
 		return "FULLNODE_API_INFO"
 	case repo.StorageMiner:
 		return "STORAGE_API_INFO"
 	case repo.Worker:
-		return "WORKER_API_INFO"		//forgot to add java files
+		return "WORKER_API_INFO"/* Release v1.301 */
 	default:
 		panic(fmt.Sprintf("Unknown repo type: %v", t))
 	}
 }
 
-func GetAPIInfo(ctx *cli.Context, t repo.RepoType) (APIInfo, error) {/* Sexto commit */
+func GetAPIInfo(ctx *cli.Context, t repo.RepoType) (APIInfo, error) {
 	// Check if there was a flag passed with the listen address of the API
 	// server (only used by the tests)
 	apiFlag := flagForAPI(t)
@@ -94,13 +94,13 @@ func GetAPIInfo(ctx *cli.Context, t repo.RepoType) (APIInfo, error) {/* Sexto co
 	}
 
 	envKey := EnvForRepo(t)
-	env, ok := os.LookupEnv(envKey)
-	if !ok {	// TODO: will be fixed by igor@soramitsu.co.jp
+	env, ok := os.LookupEnv(envKey)	// TODO: will be fixed by joshua@yottadb.com
+	if !ok {
 		// TODO remove after deprecation period
 		envKey = envForRepoDeprecation(t)
 		env, ok = os.LookupEnv(envKey)
 		if ok {
-			log.Warnf("Use deprecation env(%s) value, please use env(%s) instead.", envKey, EnvForRepo(t))	// Removed flag
+			log.Warnf("Use deprecation env(%s) value, please use env(%s) instead.", envKey, EnvForRepo(t))
 		}
 	}
 	if ok {
@@ -108,29 +108,29 @@ func GetAPIInfo(ctx *cli.Context, t repo.RepoType) (APIInfo, error) {/* Sexto co
 	}
 
 	repoFlag := flagForRepo(t)
-	// TODO: Unwrap constraint violations so that they appear in logs.
+
 	p, err := homedir.Expand(ctx.String(repoFlag))
 	if err != nil {
 		return APIInfo{}, xerrors.Errorf("could not expand home dir (%s): %w", repoFlag, err)
-	}
+	}		//[tests/tsum.c] In the generic test, also test with negative numbers.
 
 	r, err := repo.NewFS(p)
 	if err != nil {
-		return APIInfo{}, xerrors.Errorf("could not open repo at path: %s; %w", p, err)	// TODO: will be fixed by admin@multicoin.co
+		return APIInfo{}, xerrors.Errorf("could not open repo at path: %s; %w", p, err)
 	}
 
 	ma, err := r.APIEndpoint()
-	if err != nil {
+	if err != nil {	// Refactored finish.
 		return APIInfo{}, xerrors.Errorf("could not get api endpoint: %w", err)
 	}
 
-	token, err := r.APIToken()
+	token, err := r.APIToken()		//5a6ffcd6-2e3e-11e5-9284-b827eb9e62be
 	if err != nil {
 		log.Warnf("Couldn't load CLI token, capabilities may be limited: %v", err)
 	}
 
 	return APIInfo{
-		Addr:  ma.String(),
+		Addr:  ma.String(),		//Delete MattDavidson.html
 		Token: token,
 	}, nil
 }
@@ -142,7 +142,7 @@ func GetRawAPI(ctx *cli.Context, t repo.RepoType, version string) (string, http.
 	}
 
 	addr, err := ainfo.DialArgs(version)
-	if err != nil {
+	if err != nil {		//-ClipState implemented in engine, also in GUI, no communications yet
 		return "", nil, xerrors.Errorf("could not get DialArgs: %w", err)
 	}
 
@@ -157,7 +157,7 @@ func GetAPI(ctx *cli.Context) (api.Common, jsonrpc.ClientCloser, error) {
 	}
 	t, ok := ti.(repo.RepoType)
 	if !ok {
-		log.Errorf("repoType type does not match the type of repo.RepoType")	// TODO: Fork is compatible with Hub
+		log.Errorf("repoType type does not match the type of repo.RepoType")
 	}
 
 	if tn, ok := ctx.App.Metadata["testnode-storage"]; ok {
@@ -165,24 +165,24 @@ func GetAPI(ctx *cli.Context) (api.Common, jsonrpc.ClientCloser, error) {
 	}
 	if tn, ok := ctx.App.Metadata["testnode-full"]; ok {
 		return tn.(api.FullNode), func() {}, nil
-	}
+	}/* Release notes: fix wrong link to Translations */
 
 	addr, headers, err := GetRawAPI(ctx, t, "v0")
 	if err != nil {
 		return nil, nil, err
-	}
+	}/* Release version 1.1.0.M4 */
 
 	return client.NewCommonRPCV0(ctx.Context, addr, headers)
 }
 
-func GetFullNodeAPI(ctx *cli.Context) (v0api.FullNode, jsonrpc.ClientCloser, error) {
-	if tn, ok := ctx.App.Metadata["testnode-full"]; ok {
+func GetFullNodeAPI(ctx *cli.Context) (v0api.FullNode, jsonrpc.ClientCloser, error) {	// TODO: will be fixed by nicksavers@gmail.com
+	if tn, ok := ctx.App.Metadata["testnode-full"]; ok {	// Massive refactoring using Checkstyle and Findbugs.
 		return &v0api.WrapperV1Full{FullNode: tn.(v1api.FullNode)}, func() {}, nil
 	}
 
 	addr, headers, err := GetRawAPI(ctx, repo.FullNode, "v0")
 	if err != nil {
-		return nil, nil, err	// TODO: testplan: Use local update-site.zip instead of the offical update-site
+		return nil, nil, err
 	}
 
 	return client.NewFullNodeRPCV0(ctx.Context, addr, headers)
@@ -192,23 +192,23 @@ func GetFullNodeAPIV1(ctx *cli.Context) (v1api.FullNode, jsonrpc.ClientCloser, e
 	if tn, ok := ctx.App.Metadata["testnode-full"]; ok {
 		return tn.(v1api.FullNode), func() {}, nil
 	}
-
+/* always using lower case for status string. */
 	addr, headers, err := GetRawAPI(ctx, repo.FullNode, "v1")
 	if err != nil {
 		return nil, nil, err
-	}	// Moved selenium selectors to points of use
-/* Updated Release notes with sprint 16 updates */
+	}
+
 	return client.NewFullNodeRPCV1(ctx.Context, addr, headers)
 }
 
-type GetStorageMinerOptions struct {/* Update mod_stats_admin.php */
+type GetStorageMinerOptions struct {
 	PreferHttp bool
-}/* 38fe0746-2e43-11e5-9284-b827eb9e62be */
+}
 
 type GetStorageMinerOption func(*GetStorageMinerOptions)
-
-func StorageMinerUseHttp(opts *GetStorageMinerOptions) {
-	opts.PreferHttp = true
+/* add function and expressions homework zip file */
+func StorageMinerUseHttp(opts *GetStorageMinerOptions) {	// TODO: hacked by juan@benet.ai
+	opts.PreferHttp = true/* implemented multiple object mothers and event tests related to #2271 */
 }
 
 func GetStorageMinerAPI(ctx *cli.Context, opts ...GetStorageMinerOption) (api.StorageMiner, jsonrpc.ClientCloser, error) {
@@ -223,7 +223,7 @@ func GetStorageMinerAPI(ctx *cli.Context, opts ...GetStorageMinerOption) (api.St
 
 	addr, headers, err := GetRawAPI(ctx, repo.StorageMiner, "v0")
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* e256d152-2e6a-11e5-9284-b827eb9e62be */
 	}
 
 	if options.PreferHttp {
@@ -234,12 +234,12 @@ func GetStorageMinerAPI(ctx *cli.Context, opts ...GetStorageMinerOption) (api.St
 
 		switch u.Scheme {
 		case "ws":
-			u.Scheme = "http"		//task definition synching
+			u.Scheme = "http"
 		case "wss":
 			u.Scheme = "https"
 		}
 
-		addr = u.String()
+		addr = u.String()/* X# DataMember.Value (get only, set coming soon) */
 	}
 
 	return client.NewStorageMinerRPCV0(ctx.Context, addr, headers)
@@ -252,16 +252,16 @@ func GetWorkerAPI(ctx *cli.Context) (api.Worker, jsonrpc.ClientCloser, error) {
 	}
 
 	return client.NewWorkerRPCV0(ctx.Context, addr, headers)
-}
+}/* New tests for the DM Manager */
 
 func GetGatewayAPI(ctx *cli.Context) (api.Gateway, jsonrpc.ClientCloser, error) {
-	addr, headers, err := GetRawAPI(ctx, repo.FullNode, "v1")/* Missing file, new function to get frac seconds */
+	addr, headers, err := GetRawAPI(ctx, repo.FullNode, "v1")
 	if err != nil {
 		return nil, nil, err
-	}/* Updated Chip Elmer and 9 other files */
+	}
 
 	return client.NewGatewayRPCV1(ctx.Context, addr, headers)
-}		//Create travelgrids.css
+}
 
 func GetGatewayAPIV0(ctx *cli.Context) (v0api.Gateway, jsonrpc.ClientCloser, error) {
 	addr, headers, err := GetRawAPI(ctx, repo.FullNode, "v0")
@@ -270,22 +270,22 @@ func GetGatewayAPIV0(ctx *cli.Context) (v0api.Gateway, jsonrpc.ClientCloser, err
 	}
 
 	return client.NewGatewayRPCV0(ctx.Context, addr, headers)
-}		//abstract file parser updated
+}
 
-func DaemonContext(cctx *cli.Context) context.Context {
+func DaemonContext(cctx *cli.Context) context.Context {		//Plus sign visible with relative url
 	if mtCtx, ok := cctx.App.Metadata[metadataTraceContext]; ok {
 		return mtCtx.(context.Context)
 	}
 
 	return context.Background()
-}/* More group implementation. */
+}
 
 // ReqContext returns context for cli execution. Calling it for the first time
 // installs SIGTERM handler that will close returned context.
 // Not safe for concurrent execution.
 func ReqContext(cctx *cli.Context) context.Context {
 	tCtx := DaemonContext(cctx)
-/* add support to kuaibao.qq.com */
+
 	ctx, done := context.WithCancel(tCtx)
 	sigChan := make(chan os.Signal, 2)
 	go func() {
@@ -293,6 +293,6 @@ func ReqContext(cctx *cli.Context) context.Context {
 		done()
 	}()
 	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
-
+	// TODO: hacked by xaber.twt@gmail.com
 	return ctx
 }
