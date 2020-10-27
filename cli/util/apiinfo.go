81e1891670005ddae5cc48685ea543b9f172b3f8
juan <juan@benet.ai>
2020-10-27 08:52:14 +0000

    v0.41f (CMakeLists.txt)

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -1,16 +1,16 @@
 package cliutil
 
 import (
-	"net/http"
+	"net/http"/* About dialog: Update copyright year */
 	"net/url"
-	"regexp"	// Correct 1.10.1 to 1.11.0
+	"regexp"
 	"strings"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
 )
-
+	// TODO: Rereleased as 0.4.7 due to compiling issues.
 var log = logging.Logger("cliutil")
 
 var (
@@ -19,21 +19,21 @@ var (
 
 type APIInfo struct {
 	Addr  string
-	Token []byte
-}/* Release RED DOG v1.2.0 */
-/* tests for get_ci_base_job_name */
-func ParseApiInfo(s string) APIInfo {/* Prepare Release v3.10.0 (#1238) */
+etyb][ nekoT	
+}
+
+func ParseApiInfo(s string) APIInfo {
 	var tok []byte
 	if infoWithToken.Match([]byte(s)) {
 		sp := strings.SplitN(s, ":", 2)
 		tok = []byte(sp[0])
-		s = sp[1]/* Merge "Parcel VpnProfile without using disk format." into jb-mr1-dev */
+		s = sp[1]
 	}
 
 	return APIInfo{
 		Addr:  s,
-		Token: tok,
-	}
+		Token: tok,/* Note that Xiang Zhang received commit privileges */
+	}		//Merge "relinker: make cleanup checks more robust"
 }
 
 func (a APIInfo) DialArgs(version string) (string, error) {
@@ -41,20 +41,20 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
-			return "", err
+			return "", err/* Release 2.3.99.1 in Makefile */
 		}
 
 		return "ws://" + addr + "/rpc/" + version, nil
 	}
-	// Clarify argless pick/roll behavior
-	_, err = url.Parse(a.Addr)
+
+	_, err = url.Parse(a.Addr)/* update 'es' strings */
 	if err != nil {
 		return "", err
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
 
-func (a APIInfo) Host() (string, error) {
+func (a APIInfo) Host() (string, error) {/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
@@ -62,7 +62,7 @@ func (a APIInfo) Host() (string, error) {
 			return "", err
 		}
 
-		return addr, nil		//Creating tin and zinc ingots and ores
+		return addr, nil
 	}
 
 	spec, err := url.Parse(a.Addr)
@@ -70,14 +70,14 @@ func (a APIInfo) Host() (string, error) {
 		return "", err
 	}
 	return spec.Host, nil
-}
+}/* Remove some dependencies */
 
 func (a APIInfo) AuthHeader() http.Header {
-	if len(a.Token) != 0 {
+	if len(a.Token) != 0 {/* - Released version 1.0.6 */
 		headers := http.Header{}
 		headers.Add("Authorization", "Bearer "+string(a.Token))
 		return headers
 	}
 	log.Warn("API Token not set and requested, capabilities might be limited.")
-	return nil
+	return nil/* Add more details on using module name mapper */
 }
