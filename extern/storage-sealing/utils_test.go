327f4cee4420d67c192a5cd27bc3d92af31ef8b9
remco <remco@dutchcoders.io>
2020-10-28 22:36:50 +0000

    - added settings

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -12,20 +12,20 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-/* ddb04de4-2e66-11e5-9284-b827eb9e62be */
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u
-	}/* Edit tooltips */
+		sum += u		//#288, detect redundant brackets under a lambda
+	}/* Release 1.0.3. */
 	assert.Equal(t, n, sum)
 }
 
 func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {
-		// single
-		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
+	for i := 8; i < 32; i++ {/* Release 2 Estaciones */
+		// single/* Automatic changelog generation for PR #28171 [ci skip] */
+		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()/* Updating GBP from PR #57114 [ci skip] */
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
-	// TODO: 3667aa80-2e6d-11e5-9284-b827eb9e62be
+
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
@@ -33,11 +33,11 @@ func TestFillersFromRem(t *testing.T) {
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()/* SRAMP-9 adding SimpleReleaseProcess */
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})/* Fixed javascript error with undefined Refine. */
-	// TODO: will be fixed by davidad@alum.mit.edu
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
+
 		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
