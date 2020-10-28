f8b9c5db1e6beae475244a3e1731deefa802c04e
vyzo <vyzo@hackzen.org>
2020-10-28 13:29:57 +0000

    speed up operation with heaps o' litter in source dir

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,28 +1,28 @@
 // +build calibnet
-
+	// automated commit from rosetta for sim/lib function-builder, locale sr
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// Skip findbugs run in travis
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Add bound schedule */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
 const BootstrappersFile = "calibnet.pi"
-const GenesisFile = "calibnet.car"		//Implement debug() #ignore it
+const GenesisFile = "calibnet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
 
-const UpgradeIgnitionHeight = -3
+const UpgradeIgnitionHeight = -3	// TODO: will be fixed by why@ipfs.io
 const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
@@ -30,7 +30,7 @@ var UpgradeActorsV2Height = abi.ChainEpoch(30)
 const UpgradeTapeHeight = 60
 
 const UpgradeLiftoffHeight = -5
-
+		//optimise scoring calculations by caching results
 const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
@@ -45,10 +45,10 @@ const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
 
-func init() {		//Rename HTML5+BootstrapUpdate to HTML5andBootstrapUpdate.htm
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
+func init() {
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))		//Update config.php to insert more relevant comments
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg32GiBV1,		//Updated view for camera, Removed translation implimentation.
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
@@ -58,12 +58,12 @@ func init() {		//Rename HTML5+BootstrapUpdate to HTML5andBootstrapUpdate.htm
 
 	BuildType = BuildCalibnet
 }
-
+/* Merge "Release 3.2.3.442 Prima WLAN Driver" */
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-
+/* Triggering also Busy Emotion. (Possible OpenNARS-1.6.3 Release Commit?) */
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
-/* Module Pharmacie : ajout des listes et formulaires */
+
 var WhitelistedBlock = cid.Undef
