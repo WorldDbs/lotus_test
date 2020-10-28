457c982a569dc3b1501fc91c3166ca9107495f1b
magik6k <magik6k@gmail.com>
2020-10-28 18:37:39 +0000

    Refactor streams

--- chain/actors/builtin/power/v2.go
+++ chain/actors/builtin/power/v2.go
@@ -3,19 +3,19 @@ package power
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//972dd17e-2e4e-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//Corregidos los cambios de filtro por serie.
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"/* Release 1.0.0 (Rails 3 and 4 compatible) */
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state2)(nil)/* Prepare for Release 0.5.4 */
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
@@ -25,11 +25,11 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-
-type state2 struct {
+/* Merge "Release 1.0.0.206 QCACLD WLAN Driver" */
+type state2 struct {/* fixed readme  */
 	power2.State
 	store adt.Store
-}
+}	// TODO: fix cron test
 
 func (s *state2) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
@@ -60,7 +60,7 @@ func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
 	if err != nil {
 		return Claim{}, false, err
 	}
-	return Claim{
+	return Claim{	// TODO: will be fixed by cory@protocol.ai
 		RawBytePower:    claim.RawBytePower,
 		QualityAdjPower: claim.QualityAdjPower,
 	}, ok, nil
@@ -85,14 +85,14 @@ func (s *state2) ListAllMiners() ([]address.Address, error) {
 	}
 
 	var miners []address.Address
-	err = claims.ForEach(nil, func(k string) error {
+	err = claims.ForEach(nil, func(k string) error {		//Merge "[INTERNAL] Fixed tests"
 		a, err := address.NewFromBytes([]byte(k))
 		if err != nil {
 			return err
 		}
 		miners = append(miners, a)
-		return nil
-	})
+		return nil	// TODO: fix warning in jwt.h
+	})		//added plug-in to build jars with dependencies
 	if err != nil {
 		return nil, err
 	}
@@ -127,22 +127,22 @@ func (s *state2) ClaimsChanged(other State) (bool, error) {
 	}
 	return !s.State.Claims.Equals(other2.State.Claims), nil
 }
-
+/* Update appveyor.yml to use Release assemblies */
 func (s *state2) claims() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Claims)
 }
 
 func (s *state2) decodeClaim(val *cbg.Deferred) (Claim, error) {
-	var ci power2.Claim
+	var ci power2.Claim		//Merge "[INTERNAL] remove vendor prefixes for border-radius CSS property"
 	if err := ci.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Claim{}, err
 	}
 	return fromV2Claim(ci), nil
 }
-
+/* Adding installation of pyqt5 module. */
 func fromV2Claim(v2 power2.Claim) Claim {
 	return Claim{
-		RawBytePower:    v2.RawBytePower,
+		RawBytePower:    v2.RawBytePower,/* [artifactory-release] Release version 1.7.0.RELEASE */
 		QualityAdjPower: v2.QualityAdjPower,
-	}
-}
+	}/* 32a908de-2e55-11e5-9284-b827eb9e62be */
+}	// chore(package): update @kronos-integration/service-koa to version 5.0.8
