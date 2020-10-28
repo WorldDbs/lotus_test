fdfa5bf7977af15b8b09dde60a8b2871f27ff453
witek <witek@enjin.io>
2020-10-28 20:52:40 +0000

    Added upload timing info

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -7,36 +7,36 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"/* Renamed the 'threading' sub library to 'thread'. */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"		//Update rrd_export.py
+	"github.com/ipfs/go-cid"
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 )
 
-func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {		//pre-req checks function creation
+func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		panic(err)
-	}
+	}/* @Release [io7m-jcanephora-0.34.2] */
 
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
-			Root:         fcid,
+			Root:         fcid,/* ValueCached is now preserved when saved. */
 		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,
-	})/* added missing root element specification */
+		FastRetrieval:     fastRetrieval,	// TODO: chore(deps): update dependency remap-istanbul to v0.10.0
+	})
 	if err != nil {
 		panic(err)
 	}
-	return deal/* Merge "Fixes for multiple notifications recevied by node drivers." */
+	return deal	// set the merged data into output document. 
 }
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
@@ -45,20 +45,20 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
+	// update lines program
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
-	if err != nil {
+	if err != nil {/* Testing eclipse project integration */
 		panic(err)
-	}		//Add parameters for probability distribution to NameConstraintBuilder
+	}
 
-	for tipset := range tipsetsCh {
+	for tipset := range tipsetsCh {/* Add --subunit to 'make check'. */
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
 		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
 			panic(err)
 		}
-		switch di.State {
+		switch di.State {/* welcomeDM property */
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
 		case storagemarket.StorageDealFailing:
@@ -67,8 +67,8 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
 			t.RecordMessage("completed deal: %s", di)
-			return
-		}
+			return/* Automatic changelog generation for PR #52531 [ci skip] */
+		}/* 7db2673c-2e3f-11e5-9284-b827eb9e62be */
 
 		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
 	}
