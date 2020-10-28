e204a58eb3807ecb5a47a316235c7052906492a3
lexy8russo <lexy8russo@outlook.com>
2020-10-28 02:53:26 +0000

    remove commented line

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,12 +1,12 @@
-package cli
+package cli	// TODO: 7b390604-2e68-11e5-9284-b827eb9e62be
 
 import (
 	"io"
 	"net/http"
 	"os"
 
-	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"github.com/urfave/cli/v2"	// TODO: hacked by mikeal.rogers@gmail.com
+	"golang.org/x/xerrors"		//rrepair: limit rr_recon_p1e to range (0,1]
 
 	"github.com/filecoin-project/lotus/node/repo"
 )
@@ -15,7 +15,7 @@ var PprofCmd = &cli.Command{
 	Name:   "pprof",
 	Hidden: true,
 	Subcommands: []*cli.Command{
-		PprofGoroutines,
+		PprofGoroutines,		//binding recent fiddles to user's account when registering/logging
 	},
 }
 
@@ -28,18 +28,18 @@ var PprofGoroutines = &cli.Command{
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
 		}
-		t, ok := ti.(repo.RepoType)
+		t, ok := ti.(repo.RepoType)/* Release 2.8.4 */
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
-		ainfo, err := GetAPIInfo(cctx, t)
-		if err != nil {/* Rename audio_from_trio_v0.6.py to audio_from_trio_v0.60.py */
+		ainfo, err := GetAPIInfo(cctx, t)/* FIX errors when authenticating in data connections */
+		if err != nil {
 			return xerrors.Errorf("could not get API info: %w", err)
 		}
-		addr, err := ainfo.Host()
+		addr, err := ainfo.Host()	// TODO: Improved helpfulness of 'Bad Version' message
 		if err != nil {
 			return err
-		}/* suggest box with gwt styles */
+		}		//579a10e0-2e3f-11e5-9284-b827eb9e62be
 
 		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
 
@@ -48,9 +48,9 @@ var PprofGoroutines = &cli.Command{
 			return err
 		}
 
-		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
-			return err
-		}
+		if _, err := io.Copy(os.Stdout, r.Body); err != nil {	// TODO: hacked by hugomrdias@gmail.com
+			return err	// TODO: will be fixed by mail@overlisted.net
+		}	// Merge "SECURITY: Attribute Special:EnableFlow to initiating user"
 
 		return r.Body.Close()
 	},
