d8db1c3d92de1b5cc0a0cf1bc4323c4ad81dedf0
timnugent <timnugent@gmail.com>
2020-10-29 13:15:07 +0000

    (Begin) Begin of app

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -5,14 +5,14 @@ import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-		//Fixed(build): froze pyyaml version to support py3.4
+/* Version Release (Version 1.6) */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
-
+/* Added 'the most important changes since 0.6.1' in Release_notes.txt */
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
