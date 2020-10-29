6522b222ac611d1233ec643f34ba9871ae71c2ac
boringland <boringland@protonmail.ch>
2020-10-29 14:21:10 +0000

    Support for /username

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,16 +1,16 @@
-package sectorstorage
-
+package sectorstorage		//Close #207 - Add "givejournal" event
+	// Automatic changelog generation for PR #14708 [ci skip]
 import "sort"
 
 type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
 
-func (q requestQueue) Less(i, j int) bool {
+func (q requestQueue) Less(i, j int) bool {/* Linux readme install instructions. */
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
 		return muchLess
-	}/* Added missed phpDoc to \frontend\controllers\SiteController */
+	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
@@ -18,7 +18,7 @@ func (q requestQueue) Less(i, j int) bool {
 
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}/* Released version 0.8.8 */
+	}
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
@@ -26,14 +26,14 @@ func (q requestQueue) Less(i, j int) bool {
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
-	q[j].index = j		//Introducing objects
-}	// patch SQL script for 0.2.3
-/* Change name of variable passwordhash  */
+	q[j].index = j
+}
+
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
 	item.index = n
-	*q = append(*q, item)
+	*q = append(*q, item)/* Release version [9.7.14] - alfter build */
 	sort.Sort(q)
 }
 
@@ -42,9 +42,9 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil
+	old[n-1] = nil/* Release 3.2 180.1*. */
 	item.index = -1
 	*q = old[0 : n-1]
-	sort.Sort(q)
+	sort.Sort(q)/* kucoin2 parseTrade fix */
 	return item
-}	// TODO: Merge "app: aboot: Fix return statements in cmd_boot function"
+}
