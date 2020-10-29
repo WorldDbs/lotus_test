24ebd993d77cd44961af315fe8f17f7ac2c61aee
arajasek94 <arajasek94@gmail.com>
2020-10-29 12:42:14 +0000

    fixed missing char

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -4,10 +4,10 @@ import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message		//Avoid GUI conflicts with running downloads and series link.
+	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
-}
+}		//Protect disposing MesquiteFrame against exceptions (due to threading?)
 
 func (fb *FullBlock) Cid() cid.Cid {
-	return fb.Header.Cid()
+	return fb.Header.Cid()/* centering threshold */
 }
