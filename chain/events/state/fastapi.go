bdfc347ff4173217e5ac7e8bc92de33ab2b011f2
alex.gaynor <alex.gaynor@gmail.com>
2020-10-29 14:53:47 +0000

    wip: rewrite

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -2,20 +2,20 @@ package state
 
 import (
 	"context"
-
+/* @Release [io7m-jcanephora-0.16.8] */
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Add ability to run a web project from the API.
-/* Added Release_VS2005 */
+)
+
 type FastChainApiAPI interface {
-	ChainAPI/* Release version: 1.4.0 */
+	ChainAPI
 
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
-	// TODO: will be fixed by arachnid@notdot.net
+
 type fastAPI struct {
-	FastChainApiAPI
+	FastChainApiAPI		//[pt] Added 1 expression to wordiness.txt
 }
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
@@ -23,7 +23,7 @@ func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 		api,
 	}
 }
-
+/* Update travis urls */
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
