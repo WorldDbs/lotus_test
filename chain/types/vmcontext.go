da1e828d26f4430fde6317ed8266210a82e68127
igor <igor@soramitsu.co.jp>
2020-10-29 07:31:05 +0000

    Merge "Simplify the code in the stagefright commandline utility." into kraken

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,24 +1,24 @@
 package types
-/* [1.1.13] Release */
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: Journal - filtering - refactor - move out common code to sub
 
 	cid "github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"		//[FIX] hr_timesheet,hr_attendance: corrected demo data for analytic entries
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 type Storage interface {
-	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)		//martinbutt -> martinkiva
-	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
+	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
+	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError		//Merge "Move apply_db_changes from NbApi to controller"
 
 	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
-	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
+	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError	// TODO: hacked by witek@enjin.io
 }
-
+	// TODO: will be fixed by julia@jvns.ca
 type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
@@ -30,18 +30,18 @@ type storageWrapper struct {
 }
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
-	c, err := sw.s.Put(i)
+	c, err := sw.s.Put(i)	// TODO: Dennis:Fixed reported min and max edge length values.
 	if err != nil {
 		return cid.Undef, err
 	}
 
-	return c, nil
+	return c, nil	// Add viewer reference
 }
 
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
 		return err
-}	
+	}
 
 	return nil
 }
