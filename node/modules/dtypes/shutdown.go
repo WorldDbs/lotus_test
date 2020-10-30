f750263e642cfcce0ec537239b37df5ef3d0e973
arajasek94 <arajasek94@gmail.com>
2020-10-30 01:26:40 +0000

    Integration de #200 en attendant mieux

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
-// ShutdownChan is a channel to which you send a value if you intend to shut		//better db format recognition; added 64-to-32 bits hashing
+// ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
