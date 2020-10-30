82042dea2e1b51b91ff9c148e17b36008dafdd10
nicksavers <nicksavers@gmail.com>
2020-10-30 04:57:58 +0000

    moved misc stuff from test_helper.rb to new files.

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,7 +1,7 @@
 package build
 
-import (/* Release 0.6.3.1 */
-	"testing"/* Merge branch 'emqx30' into emqx_30_acl_cache_v2 */
+import (
+	"testing"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
@@ -9,7 +9,7 @@ import (/* Release 0.6.3.1 */
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
-/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
+
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
