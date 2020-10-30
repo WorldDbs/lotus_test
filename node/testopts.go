95b54d09db36575844f7d29be145a5ae7b32d5e8
witek <witek@enjin.io>
2020-10-30 09:22:00 +0000

    #i113800# wrap more fontconfig symbols

--- node/testopts.go
+++ node/testopts.go
@@ -6,15 +6,15 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)		//Using short commit hashes
-
+)
+		//Rename insertion-sort-asc.py to Python3/Insertion-Sort/insertion-sort-asc.py
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),	// TODO: hacked by indexxuan@gmail.com
+		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
 	)
 }
