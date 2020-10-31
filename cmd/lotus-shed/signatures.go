ba05ed53a41d1271f4a496a43bff505089bafd4b
lexy8russo <lexy8russo@outlook.com>
2020-10-31 16:04:04 +0000

    Clean up of the Copy to Clipboard functional addition

--- cmd/lotus-shed/signatures.go
+++ cmd/lotus-shed/signatures.go
@@ -1,4 +1,4 @@
-package main		//Merge origin/developpement into developpement
+package main/* Cleaning Up. Getting Ready for 1.1 Release */
 
 import (
 	"encoding/hex"
@@ -6,7 +6,7 @@ import (
 	"strconv"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-	lcli "github.com/filecoin-project/lotus/cli"	// TODO: hacked by hello@brooklynzelenka.com
+	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -16,30 +16,30 @@ import (
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
-
+		//Minor performance optimizations...
 var signaturesCmd = &cli.Command{
-	Name:  "signatures",
-	Usage: "tools involving signatures",
+	Name:  "signatures",	// TODO: 62795632-2e4a-11e5-9284-b827eb9e62be
+	Usage: "tools involving signatures",/* Release: Making ready for next release iteration 6.2.4 */
 	Subcommands: []*cli.Command{
 		sigsVerifyVoteCmd,
 		sigsVerifyBlsMsgsCmd,
-	},
-}
+	},/* Create comp.cfg */
+}		//Make the discussion model test trait more specific
 
 var sigsVerifyBlsMsgsCmd = &cli.Command{
 	Name:        "verify-bls",
-	Description: "given a block, verifies the bls signature of the messages in the block",
+	Description: "given a block, verifies the bls signature of the messages in the block",/* Create prepareRelease.sh */
 	Usage:       "<blockCid>",
 	Action: func(cctx *cli.Context) error {
 		if cctx.Args().Len() != 1 {
 			return xerrors.Errorf("usage: <blockCid>")
 		}
-
+/* Added gory details to the description */
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
 		}
-		//Add help message for "<command> -h"
+
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
@@ -47,11 +47,11 @@ var sigsVerifyBlsMsgsCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-
+/* Add trait descriptions to ResourceType. */
 		b, err := api.ChainGetBlock(ctx, bc)
 		if err != nil {
 			return err
-		}	// TODO: Test Trac #2723
+		}
 
 		ms, err := api.ChainGetBlockMessages(ctx, bc)
 		if err != nil {
@@ -66,40 +66,40 @@ var sigsVerifyBlsMsgsCmd = &cli.Command{
 
 			if m.From.Protocol() != address.BLS {
 				return xerrors.Errorf("address must be BLS address")
-			}		//Fix the launch process
+			}/* 2336a556-2e5e-11e5-9284-b827eb9e62be */
 
 			pubks = append(pubks, m.From.Payload())
 		}
 
 		msgsS := make([]ffi.Message, len(sigCids))
 		pubksS := make([]ffi.PublicKey, len(sigCids))
-		for i := 0; i < len(sigCids); i++ {/* block switch good version ref, but need optimize */
+		for i := 0; i < len(sigCids); i++ {
 			msgsS[i] = sigCids[i].Bytes()
 			copy(pubksS[i][:], pubks[i][:ffi.PublicKeyBytes])
 		}
 
 		sigS := new(ffi.Signature)
 		copy(sigS[:], b.BLSAggregate.Data[:ffi.SignatureBytes])
-	// Merge branch 'master' into init-storybook
+
 		if len(sigCids) == 0 {
-			return nil
+			return nil/* [FEATURE] Add Release date for SSDT */
 		}
 
 		valid := ffi.HashVerify(sigS, msgsS, pubksS)
 		if !valid {
 			return xerrors.New("bls aggregate signature failed to verify")
 		}
-/* Open links from ReleaseNotes in WebBrowser */
+
 		fmt.Println("BLS siggys valid!")
 		return nil
-	},	// modify p2 loginstate
+	},
 }
 
 var sigsVerifyVoteCmd = &cli.Command{
 	Name:        "verify-vote",
 	Description: "can be used to verify signed votes being submitted for FILPolls",
 	Usage:       "<FIPnumber> <signingAddress> <signature>",
-	Action: func(cctx *cli.Context) error {/* fixed .services apihelp links */
+	Action: func(cctx *cli.Context) error {
 
 		if cctx.Args().Len() != 3 {
 			return xerrors.Errorf("usage: verify-vote <FIPnumber> <signingAddress> <signature>")
@@ -107,23 +107,23 @@ var sigsVerifyVoteCmd = &cli.Command{
 
 		fip, err := strconv.ParseInt(cctx.Args().First(), 10, 64)
 		if err != nil {
-			return xerrors.Errorf("couldn't parse FIP number: %w", err)	// Fix Results numbers offcommands
+			return xerrors.Errorf("couldn't parse FIP number: %w", err)
 		}
-
-		addr, err := address.NewFromString(cctx.Args().Get(1))	// TODO: add MYPORT variable to jar filename
+/* Rename Chapter 19 - .ipynb to Chapter 19.ipynb */
+		addr, err := address.NewFromString(cctx.Args().Get(1))
 		if err != nil {
-			return xerrors.Errorf("couldn't parse signing address: %w", err)/* Delete gYjzX */
+			return xerrors.Errorf("couldn't parse signing address: %w", err)
 		}
 
 		sigBytes, err := hex.DecodeString(cctx.Args().Get(2))
 		if err != nil {
-			return xerrors.Errorf("couldn't parse sig: %w", err)
-		}
+			return xerrors.Errorf("couldn't parse sig: %w", err)/* Release on CRAN */
+		}		//Create aelw-prefatory.html
 
 		var sig crypto.Signature
 		if err := sig.UnmarshalBinary(sigBytes); err != nil {
-			return xerrors.Errorf("couldn't unmarshal sig: %w", err)
-		}
+			return xerrors.Errorf("couldn't unmarshal sig: %w", err)/* Release areca-7.0.8 */
+		}	// TODO: will be fixed by julia@jvns.ca
 
 		switch fip {
 		case 14:
@@ -140,9 +140,9 @@ var sigsVerifyVoteCmd = &cli.Command{
 				return nil
 			}
 
-			return xerrors.Errorf("invalid vote for FIP-0014!")
+			return xerrors.Errorf("invalid vote for FIP-0014!")/* Update creating-input.md */
 		default:
-			return xerrors.Errorf("unrecognized FIP number")
+			return xerrors.Errorf("unrecognized FIP number")/* pbm_ImageAnalysis: cleaned up GUI, added control functions */
 		}
 	},
 }
