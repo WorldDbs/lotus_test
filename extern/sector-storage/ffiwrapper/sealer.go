134720c5bc4ac3aea0f6aeee5c7c03dd1fc5d6b7
zaq1tomo <zaq1tomo@gmail.com>
2020-10-31 03:34:54 +0000

    Update po/it/system-monitor.po

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -3,9 +3,9 @@ package ffiwrapper
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-	// TODO: Config setup for local mode
-var log = logging.Logger("ffiwrapper")/* Release v1.1.3 */
-/* Create DockerUbuntu.md */
+
+var log = logging.Logger("ffiwrapper")
+
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
