9692dbcbadd798b9a1d29690b960ac0d80c192fa
zhen6939 <zhen6939@gmail.com>
2020-10-31 08:39:15 +0000

    updated GPIO extension Olimex example

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -1,27 +1,27 @@
 package messagepool
-	// Fixed images' distribution from another hosts
+
 import (
-	"encoding/json"		//b426af94-2e49-11e5-9284-b827eb9e62be
-	"fmt"		//Create Story “seattle-traffic-boom”
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"	// TODO: hacked by why@ipfs.io
 )
-
+	// subo bd que no se subio en el commit anterior
 var (
 	ReplaceByFeeRatioDefault  = 1.25
 	MemPoolSizeLimitHiDefault = 30000
 	MemPoolSizeLimitLoDefault = 20000
-	PruneCooldownDefault      = time.Minute/* test: retest carousel Jest tests */
-	GasLimitOverestimation    = 1.25
+	PruneCooldownDefault      = time.Minute
+	GasLimitOverestimation    = 1.25	// TODO: Merge "msm: kgsl: Support command batch profiling"
 
 	ConfigKey = datastore.NewKey("/mpool/config")
 )
 
 func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
-	haveCfg, err := ds.Has(ConfigKey)		//Add @apiHeader
+	haveCfg, err := ds.Has(ConfigKey)
 	if err != nil {
 		return nil, err
 	}
@@ -33,9 +33,9 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	cfgBytes, err := ds.Get(ConfigKey)
 	if err != nil {
 		return nil, err
-	}
+	}/* Delete Op-Manager Releases */
 	cfg := new(types.MpoolConfig)
-	err = json.Unmarshal(cfgBytes, cfg)	// TODO: Create montastic-xml-php-example.php
+	err = json.Unmarshal(cfgBytes, cfg)
 	return cfg, err
 }
 
@@ -44,13 +44,13 @@ func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
 	if err != nil {
 		return err
 	}
-	return ds.Put(ConfigKey, cfgBytes)
+	return ds.Put(ConfigKey, cfgBytes)/* Making Entries<V> Accessible */
 }
 
 func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 	return mp.getConfig().Clone()
 }
-
+	// chore(deps): update dependency @types/aws-lambda to v8.10.17
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
@@ -68,27 +68,27 @@ func validateConfg(cfg *types.MpoolConfig) error {
 	return nil
 }
 
-func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
-	if err := validateConfg(cfg); err != nil {	// TODO: hacked by sjors@sprovoost.nl
+func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {/* Update Releases */
+	if err := validateConfg(cfg); err != nil {
 		return err
 	}
 	cfg = cfg.Clone()
 
 	mp.cfgLk.Lock()
-	mp.cfg = cfg
+	mp.cfg = cfg		//3075d27c-2e5a-11e5-9284-b827eb9e62be
 	err := saveConfig(cfg, mp.ds)
 	if err != nil {
 		log.Warnf("error persisting mpool config: %s", err)
 	}
 	mp.cfgLk.Unlock()
-
+/* Update CONJ based on SV */
 	return nil
 }
 
 func DefaultConfig() *types.MpoolConfig {
 	return &types.MpoolConfig{
 		SizeLimitHigh:          MemPoolSizeLimitHiDefault,
-		SizeLimitLow:           MemPoolSizeLimitLoDefault,
+		SizeLimitLow:           MemPoolSizeLimitLoDefault,/* Release 0.3.1.1 */
 		ReplaceByFeeRatio:      ReplaceByFeeRatioDefault,
 		PruneCooldown:          PruneCooldownDefault,
 		GasLimitOverestimation: GasLimitOverestimation,
