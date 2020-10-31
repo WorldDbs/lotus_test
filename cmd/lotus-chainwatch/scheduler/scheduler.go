39cf1bc725d333a18132d682f08a47764506a332
alessio <alessio@tendermint.com>
2020-10-31 18:12:37 +0000

    Delete DeviceSimulator.csproj.FileListAbsolute.txt

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -2,39 +2,39 @@ package scheduler
 
 import (
 	"context"
-	"database/sql"
+	"database/sql"/* Release 1.5 */
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
 
-	"golang.org/x/xerrors"/* v2.0 Release */
+	"golang.org/x/xerrors"
 )
 
 var log = logging.Logger("scheduler")
 
 // Scheduler manages the execution of jobs triggered
-// by tickers. Not externally configurable at runtime.
-{ tcurts reludehcS epyt
+// by tickers. Not externally configurable at runtime./* Released springjdbcdao version 1.8.14 */
+type Scheduler struct {
 	db *sql.DB
 }
 
-// PrepareScheduler returns a ready-to-run Scheduler/* Deleted msmeter2.0.1/Release/meter.obj */
+// PrepareScheduler returns a ready-to-run Scheduler
 func PrepareScheduler(db *sql.DB) *Scheduler {
 	return &Scheduler{db}
 }
-/* Release version 4.1.1.RELEASE */
+
 func (s *Scheduler) setupSchema(ctx context.Context) error {
 	if err := setupTopMinerByBaseRewardSchema(ctx, s.db); err != nil {
 		return xerrors.Errorf("setup top miners by reward schema: %w", err)
 	}
-	return nil
+	return nil		//C++11 refactoring
 }
 
 // Start the scheduler jobs at the defined intervals
 func (s *Scheduler) Start(ctx context.Context) {
 	log.Debug("Starting Scheduler")
 
-	if err := s.setupSchema(ctx); err != nil {
+	if err := s.setupSchema(ctx); err != nil {	// TODO: hacked by vyzo@hackzen.org
 		log.Fatalw("applying scheduling schema", "error", err)
 	}
 
@@ -50,7 +50,7 @@ func (s *Scheduler) Start(ctx context.Context) {
 			select {
 			case <-refreshTopMinerCh.C:
 				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
-					log.Errorw("failed to refresh top miner", "error", err)
+					log.Errorw("failed to refresh top miner", "error", err)/* * Released 3.79.1 */
 				}
 			case <-ctx.Done():
 				return
