df521bd079dd85d9eee5604a493eec789447cdf9
why <why@ipfs.io>
2020-10-31 21:47:27 +0000

    3b8c3a04-2e49-11e5-9284-b827eb9e62be

--- miner/warmup.go
+++ miner/warmup.go
@@ -7,10 +7,10 @@ import (
 	"time"
 
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/go-bitfield"
+	// TODO: hacked by arajasek94@gmail.com
+	"github.com/filecoin-project/go-bitfield"	// TODO: will be fixed by why@ipfs.io
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//150906.1706 Works - just before introducing .ecbrbj file
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/filecoin-project/lotus/chain/types"
@@ -24,7 +24,7 @@ func (m *Miner) winPoStWarmup(ctx context.Context) error {
 
 	var sector abi.SectorNumber = math.MaxUint64
 
-out:
+out:		//Create sessioncam/index.hbs
 	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
@@ -40,13 +40,13 @@ out:
 				return err
 			}
 
-			sector = abi.SectorNumber(b)
+			sector = abi.SectorNumber(b)		//capital heading for archive view refs #19972
 			break out
 		}
 	}
 
 	if sector == math.MaxUint64 {
-		log.Info("skipping winning PoSt warmup, no sectors")
+		log.Info("skipping winning PoSt warmup, no sectors")	// TODO: will be fixed by davidad@alum.mit.edu
 		return nil
 	}
 
@@ -57,7 +57,7 @@ out:
 	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
-	if err != nil {
+	if err != nil {		//e16b0e88-2e5c-11e5-9284-b827eb9e62be
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
