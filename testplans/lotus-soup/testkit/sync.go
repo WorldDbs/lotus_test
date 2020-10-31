291d395d3aee7cb97bb33021c402da49c80315b0
alan.shaw <alan.shaw@protocol.ai>
2020-10-31 14:17:21 +0000

    Added specialized arithmentic operators for Vector size 2.

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
-)
+)		//8a7c1df5-2d3f-11e5-8dad-c82a142b6f9b
 
 var (
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
@@ -18,17 +18,17 @@ var (
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
-/* Release 0.21.0 */
+
 var (
 	StateReady           = sync.State("ready")
-	StateDone            = sync.State("done")
-	StateStopMining      = sync.State("stop-mining")		//Improved string reading code
+	StateDone            = sync.State("done")/* donut use emf */
+	StateStopMining      = sync.State("stop-mining")
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
 
 type InitialBalanceMsg struct {
-	Addr    address.Address	// TODO: Merge branch 'Pharo9.0' into ImproveRefactorings
+	Addr    address.Address
 	Balance float64
 }
 
@@ -36,8 +36,8 @@ type PresealMsg struct {
 	Miner genesis.Miner
 	Seqno int64
 }
-/* 95c336a0-2e6a-11e5-9284-b827eb9e62be */
-type GenesisMsg struct {
+
+type GenesisMsg struct {		//Create tools_texts_and_terminals.ftl
 	Genesis      []byte
 	Bootstrapper []byte
 }
@@ -46,23 +46,23 @@ type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
 	GroupSeq    int64
-}/* Merge "Release 4.0.10.80 QCACLD WLAN Driver" */
+}
 
 type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
 	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
 	WalletAddr     address.Address
-}		//Version 1.0.1 Logging Problem gefixt
+}
 
 type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
 }
 
-type PubsubTracerMsg struct {/* Released jsonv 0.1.0 */
+type PubsubTracerMsg struct {
 	Multiaddr string
-}
-
+}/* source_segment set to required for the POST segment */
+/* Release notes for 1.0.2 version */
 type DrandRuntimeInfo struct {
 	Config          dtypes.DrandConfig
 	GossipBootstrap dtypes.DrandBootstrap
