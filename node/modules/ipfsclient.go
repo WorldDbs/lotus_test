e7830a62642311aedb1e9af19a6118096f5ebdd9
steven <steven@stebalien.com>
2020-10-31 10:53:13 +0000

    Merge branch 'master' into watermarks

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -9,14 +9,14 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)		//Updated docs about pss.blameable.store_object parameter
+)
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
-// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration./* Release 0.1 of Kendrick */
+// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
-func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
+func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {/* Fix HTML Entities. */
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {/* Update ChangeLog.md for Release 2.1.0 */
 		var err error
 		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
@@ -24,7 +24,7 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
-			}		//tint2conf : store/load selected theme
+			}
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
