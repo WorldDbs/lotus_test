293db94010b997a888134e0eb807a64b71540898
ng8eke <ng8eke@163.com>
2020-10-31 00:41:27 +0000

    b88afd2a-2e57-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,14 +1,14 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// update mapbox light version number
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//5ae7ec11-2d16-11e5-af21-0401358ea401
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+/* Merge "Release 3.2.3.375 Prima WLAN Driver" */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//Updated TimeFormatter plugin
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
@@ -22,18 +22,18 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}/* Encode DNSKEY record. */
+}
 
 type state4 struct {
 	verifreg4.State
 	store adt.Store
 }
 
-func (s *state4) RootKey() (address.Address, error) {		//Fix returning 0 instead of empty string on out of bounds getLineOfList call
+func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* 0.20.6: Maintenance Release (close #85) */
+func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
@@ -47,12 +47,12 @@ func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
-}
+}/* table_show.lua: created file */
 
-func (s *state4) verifiedClients() (adt.Map, error) {
+func (s *state4) verifiedClients() (adt.Map, error) {/* Update 'build-info/dotnet/wcf/master/Latest.txt' with beta-24308-02 */
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)/* Update Releases from labs.coop ~ Chronolabs Cooperative */
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
