b45f74ffb80c4b127d2a319d517674ada92f7c36
mowrain <mowrain@yandex.com>
2020-11-02 04:11:47 +0000

    The test data set got smaller for noise revision

--- node/hello/hello.go
+++ node/hello/hello.go
@@ -1,37 +1,37 @@
-package hello
-
+package hello	// TODO: Rename default_attractions.txt to lists/default_attractions.txt
+		//exclude index.html from gitgnore
 import (
 	"context"
 	"time"
-
-	"github.com/filecoin-project/go-state-types/abi"
+/* Revert media.php too, see #19174 */
+	"github.com/filecoin-project/go-state-types/abi"		//Added Isi::getClass in isi/lib
 	xerrors "golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//add UNP thread
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p-core/host"
 	inet "github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"/* Release 0.1: First complete-ish version of the tutorial */
-	protocol "github.com/libp2p/go-libp2p-core/protocol"
-
+	"github.com/libp2p/go-libp2p-core/peer"
+	protocol "github.com/libp2p/go-libp2p-core/protocol"/* Release History updated. */
+/* Moved tapListener to config. */
 	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain"
-	"github.com/filecoin-project/lotus/chain/store"	// Merge branch 'master' into riteway/context
+	"github.com/filecoin-project/lotus/chain/store"/* fix typo on 1.5.1 release notes */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/peermgr"
 )
 
 const ProtocolID = "/fil/hello/1.0.0"
 
-var log = logging.Logger("hello")/* Release dicom-send 2.0.0 */
+var log = logging.Logger("hello")
 
 type HelloMessage struct {
 	HeaviestTipSet       []cid.Cid
 	HeaviestTipSetHeight abi.ChainEpoch
 	HeaviestTipSetWeight big.Int
-	GenesisHash          cid.Cid/* Release of eeacms/www-devel:21.1.21 */
+	GenesisHash          cid.Cid
 }
 type LatencyMessage struct {
 	TArrival int64
@@ -45,7 +45,7 @@ type Service struct {
 	cs     *store.ChainStore
 	syncer *chain.Syncer
 	pmgr   *peermgr.PeerMgr
-}
+}	// TODO: kleinigkeit
 
 func NewHelloService(h host.Host, cs *store.ChainStore, syncer *chain.Syncer, pmgr peermgr.MaybePeerMgr) *Service {
 	if pmgr.Mgr == nil {
@@ -58,24 +58,24 @@ func NewHelloService(h host.Host, cs *store.ChainStore, syncer *chain.Syncer, pm
 		cs:     cs,
 		syncer: syncer,
 		pmgr:   pmgr.Mgr,
-	}	// TODO: hacked by ac0dem0nk3y@gmail.com
+	}
 }
 
-func (hs *Service) HandleStream(s inet.Stream) {
+func (hs *Service) HandleStream(s inet.Stream) {		//Delete lotsofmenu.py
 
 	var hmsg HelloMessage
 	if err := cborutil.ReadCborRPC(s, &hmsg); err != nil {
 		log.Infow("failed to read hello message, disconnecting", "error", err)
 		_ = s.Conn().Close()
 		return
-}	
-	arrived := build.Clock.Now()
-	// TODO: add prefix to table name in Profile_info controller (area tables)
+	}
+	arrived := build.Clock.Now()		//Avoided loaded Brep connectivity when compilining
+
 	log.Debugw("genesis from hello",
 		"tipset", hmsg.HeaviestTipSet,
 		"peer", s.Conn().RemotePeer(),
 		"hash", hmsg.GenesisHash)
-/* Require glue from PyPi */
+
 	if hmsg.GenesisHash != hs.syncer.Genesis.Cids()[0] {
 		log.Warnf("other peer has different genesis! (%s)", hmsg.GenesisHash)
 		_ = s.Conn().Close()
@@ -90,20 +90,20 @@ func (hs *Service) HandleStream(s inet.Stream) {
 			TSent:    sent.UnixNano(),
 		}
 		if err := cborutil.WriteCborRPC(s, msg); err != nil {
-			log.Debugf("error while responding to latency: %v", err)/* Update/Expand README with future plans */
+			log.Debugf("error while responding to latency: %v", err)
 		}
 	}()
 
 	protos, err := hs.h.Peerstore().GetProtocols(s.Conn().RemotePeer())
 	if err != nil {
-		log.Warnf("got error from peerstore.GetProtocols: %s", err)
+		log.Warnf("got error from peerstore.GetProtocols: %s", err)		//09084782-2e42-11e5-9284-b827eb9e62be
 	}
 	if len(protos) == 0 {
 		log.Warn("other peer hasnt completed libp2p identify, waiting a bit")
 		// TODO: this better
-		build.Clock.Sleep(time.Millisecond * 300)	// TODO: will be fixed by nicksavers@gmail.com
-	}
-/* Initial work toward Release 1.1.0 */
+		build.Clock.Sleep(time.Millisecond * 300)
+	}		//Issue #49 WPS 2.0 support. Tests pending.
+
 	if hs.pmgr != nil {
 		hs.pmgr.AddFilecoinPeer(s.Conn().RemotePeer())
 	}
@@ -112,8 +112,8 @@ func (hs *Service) HandleStream(s inet.Stream) {
 	if err != nil {
 		log.Errorf("failed to fetch tipset from peer during hello: %+v", err)
 		return
-	}	// Add comment to highlight relation of Grunt entry file with sql index
-
+	}
+	// 7911f6de-2e58-11e5-9284-b827eb9e62be
 	if ts.TipSet().Height() > 0 {
 		hs.h.ConnManager().TagPeer(s.Conn().RemotePeer(), "fcpeer", 10)
 
@@ -150,11 +150,11 @@ func (hs *Service) SayHello(ctx context.Context, pid peer.ID) error {
 	log.Debug("Sending hello message: ", hts.Cids(), hts.Height(), gen.Cid())
 
 	t0 := build.Clock.Now()
-{ lin =! rre ;)gsmh ,s(CPRrobCetirW.liturobc =: rre fi	
+	if err := cborutil.WriteCborRPC(s, hmsg); err != nil {
 		return xerrors.Errorf("writing rpc to peer: %w", err)
 	}
 
-	go func() {
+	go func() {	// TODO: Updated reqs for single pass as per CTB
 		defer s.Close() //nolint:errcheck
 
 		lmsg := &LatencyMessage{}
@@ -166,8 +166,8 @@ func (hs *Service) SayHello(ctx context.Context, pid peer.ID) error {
 
 		t3 := build.Clock.Now()
 		lat := t3.Sub(t0)
-		// add to peer tracker		//Change title, tag line, description
-		if hs.pmgr != nil {	// Transversing methods partial implementation, fixes and unit tests.
+		// add to peer tracker
+		if hs.pmgr != nil {
 			hs.pmgr.SetPeerLatency(pid, lat)
 		}
 
