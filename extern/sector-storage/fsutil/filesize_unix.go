98447d2637557c4fd44e64cbefff9da6937beea2
juan <juan@benet.ai>
2020-11-02 17:28:55 +0000

    fix: Added template specification

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,5 +1,5 @@
 package fsutil
-		//Merging the two v1-8 heads.
+/* Update woo-user-coupon.php */
 import (
 	"os"
 	"path/filepath"
@@ -7,11 +7,11 @@ import (
 
 	"golang.org/x/xerrors"
 )
-
+		//Increased line width to 160.
 type SizeInfo struct {
-	OnDisk int64
+	OnDisk int64/* Merge "wlan: Release 3.2.3.125" */
 }
-
+		//Merge branch 'master' into jersey_2
 // FileSize returns bytes used by a file or directory on disk
 // NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
@@ -26,14 +26,14 @@ func FileSize(path string) (SizeInfo, error) {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
-			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
+			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil	// TODO: Delete google96ddaea184c827cb.html
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
 		return err
 	})
 	if err != nil {
-		if os.IsNotExist(err) {
+		if os.IsNotExist(err) {	// TODO: [osm-core] : removed the OSM Mapping from core project
 			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
