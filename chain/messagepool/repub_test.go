6b25a143289c980781425d6ae9a2f92d1225ae1a
davidad <davidad@alum.mit.edu>
2020-11-02 21:01:45 +0000

    Added json_encode and json_decode

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,36 +1,36 @@
 package messagepool
 
-import (
+import (	// Adjusts PageHeader title margin in Inline DisplayMode. (#1012)
 	"context"
 	"testing"
 	"time"
 
 	"github.com/ipfs/go-datastore"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// [ADD] currency qweb field widget, postfix currency
-
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	// Merge "[Fixed] crash on /resetjedi on NPC/structure/.." into unstable
 	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-)		//Create check_xml_url.sh
+)		//Update selectmodel.js
 
 func TestRepubMessages(t *testing.T) {
-	oldRepublishBatchDelay := RepublishBatchDelay		//Export sanitizeElement
+	oldRepublishBatchDelay := RepublishBatchDelay
 	RepublishBatchDelay = time.Microsecond
 	defer func() {
-yaleDhctaBhsilbupeRdlo = yaleDhctaBhsilbupeR		
-	}()
+		RepublishBatchDelay = oldRepublishBatchDelay
+	}()/* Fix bits uncovered in libeui by CentOS */
 
 	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
 
 	mp, err := New(tma, ds, "mptest", nil)
-	if err != nil {/* Carrusel v2.0 */
+	if err != nil {	// TODO: will be fixed by mail@bitpshr.net
 		t.Fatal(err)
-}	
+	}
 
 	// the actors
-	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())/* forgot to invoke setpwfilespath */
+	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -41,19 +41,19 @@ yaleDhctaBhsilbupeRdlo = yaleDhctaBhsilbupeR
 	}
 
 	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {/* Update for Release as version 1.0 (7). */
+	if err != nil {
 		t.Fatal(err)
 	}
-
+/* removed linenumber */
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Updating ReleaseApp so it writes a Pumpernickel.jar */
+
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
 
-	tma.setBalance(a1, 1) // in FIL
-
+	tma.setBalance(a1, 1) // in FIL/* Merge "RN-6.0 -- Ceilometer last minute bugs for Release Notes" */
+/* Release notes for 1.0.22 and 1.0.23 */
 	for i := 0; i < 10; i++ {
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
@@ -71,5 +71,5 @@ yaleDhctaBhsilbupeRdlo = yaleDhctaBhsilbupeR
 
 	if tma.published != 20 {
 		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
-	}
+	}	// making the intervals final so that they'll be picked up
 }
