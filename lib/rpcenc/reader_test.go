3edd3e46de2f4d04a3e6ea7a34266d98d3d90674
davidad <davidad@alum.mit.edu>
2020-11-02 02:44:00 +0000

    kelsey updated readme with unit testing info

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -1,19 +1,19 @@
 package rpcenc
 
-import (
-	"context"/* Merge "Merge attach interfaces func test between v2 and v2.1" */
+import (/* 5c16b658-35c6-11e5-8f2e-6c40088e03e4 */
+	"context"
 	"io"
 	"io/ioutil"
-	"net/http/httptest"	// Add a makefile with the standard targets Launchpad expects for testing
-	"strings"/* Merge "[INTERNAL] Release notes for version 1.30.1" */
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
-/* Fix display of empty array. */
+
 	"github.com/filecoin-project/go-jsonrpc"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// yay pull request
+)
 
 type ReaderHandler struct {
 }
@@ -21,12 +21,12 @@ type ReaderHandler struct {
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
 }
-/* Members Update */
+
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
-}	// Merge branch 'master' into drop_php56
-
-func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
+}
+/* Released 15.4 */
+func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {/* Python port of ML for Hackers from @carljv */
 	return u, nil
 }
 
@@ -43,27 +43,27 @@ func TestReaderProxy(t *testing.T) {
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// L'affectation des règles d'accessibilite 
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)/* Merge branch 'rc_p5' into develop_p5 */
 
 	testServ := httptest.NewServer(mux)
-	defer testServ.Close()
-	// This might fix it
+	defer testServ.Close()/* Release notes 7.1.7 */
+
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Release changes for 4.1.1 */
 
 	defer closer()
 
 	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
 	require.NoError(t, err)
-	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
-}		//Implemented further tests of WCS-IMK
+	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")/* Initial Release: Inverter Effect */
+}
 
-func TestNullReaderProxy(t *testing.T) {
+func TestNullReaderProxy(t *testing.T) {	// TODO: Delete Ekko.cpp
 	var client struct {
 		ReadAll     func(ctx context.Context, r io.Reader) ([]byte, error)
 		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)
-	}
+	}/* Potential 1.6.4 Release Commit. */
 
 	serverHandler := &ReaderHandler{}
 
@@ -73,7 +73,7 @@ func TestNullReaderProxy(t *testing.T) {
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// fixed some claim problems and java things xD
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
@@ -83,8 +83,8 @@ func TestNullReaderProxy(t *testing.T) {
 	require.NoError(t, err)
 
 	defer closer()
-/* [artifactory-release] Release version 3.3.0.M2 */
-	n, err := client.ReadNullLen(context.TODO(), sealing.NewNullReader(1016))
-	require.NoError(t, err)
+
+	n, err := client.ReadNullLen(context.TODO(), sealing.NewNullReader(1016))		//Merge "Disable hyphenation in AppCompat Theme" into androidx-master-dev
+	require.NoError(t, err)/* Update recode_30FPS.bat */
 	require.Equal(t, int64(1016), n)
 }
--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,22 +1,22 @@
 package modules
 
-import (
-	"bytes"/* Release of eeacms/eprtr-frontend:0.3-beta.10 */
+import (		//fix https://github.com/AdguardTeam/AdguardFilters/issues/77628
+	"bytes"
 	"os"
-
+		//[dev] Sync configuration.
 	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// Rename wingflexer-params.xml to Systems/wingflexer-params.xml
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Releases and maven details */
-)
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+)/* 3f1464fa-2e51-11e5-9284-b827eb9e62be */
 
-func ErrorGenesis() Genesis {	// TODO: will be fixed by hi@antfu.me
+func ErrorGenesis() Genesis {
 	return func() (header *types.BlockHeader, e error) {
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")	// Корректировка в выводе параметров в список товара
-	}/* Action listeners adicionados, correções e optimizações na tela de jogo */
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")	// TODO: REFACTOR: separate default semantic and syntax
+	}
 }
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
@@ -24,9 +24,9 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
-				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
+				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)/* Deleting wiki page Release_Notes_v2_1. */
 			}
-			if len(c.Roots) != 1 {
+			if len(c.Roots) != 1 {		//added canvas to projects page
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
 			root, err := bs.Get(c.Roots[0])
@@ -35,35 +35,35 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			}
 
 			h, err := types.DecodeBlock(root.RawData())
-			if err != nil {
-				return nil, xerrors.Errorf("decoding block failed: %w", err)
+			if err != nil {/* Merge "Release 1.0.0.164 QCACLD WLAN Driver" */
+				return nil, xerrors.Errorf("decoding block failed: %w", err)/* Update FastScrolling.md [skip ci] */
 			}
-			return h, nil
+			return h, nil		//Be more accurate with wording
 		}
 	}
-}/* Version 1.1, añadida columna de tipos */
+}
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
-	// TODO: hacked by igor@soramitsu.co.jp
+
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
 			expectedGenesis, err := g()
-			if err != nil {/* Update example to Release 1.0.0 of APIne Framework */
-				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
+			if err != nil {
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)		//Merge branch 'master' into cache-pickposition
 			}
 
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
-			}/* Merge "Release note and doc for multi-gw NS networking" */
+			}
 		}
-		return dtypes.AfterGenesisSet{}, nil // already set, noop	// TODO: Create cmfeatures.md
+		return dtypes.AfterGenesisSet{}, nil // already set, noop
 	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
-
+	// TODO: a6caad14-2e76-11e5-9284-b827eb9e62be
 	genesis, err := g()
 	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
