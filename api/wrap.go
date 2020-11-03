f63121c7c90984b5dea0be9762ec727a264059aa
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-11-03 09:03:49 +0000

    Shorter, clearer README

--- api/wrap.go
+++ api/wrap.go
@@ -1,9 +1,9 @@
-package api
+package api	// TODO: hacked by why@ipfs.io
 
 import (
 	"reflect"
 )
-
+/* Release jedipus-2.6.2 */
 // Wrap adapts partial api impl to another version
 // proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
@@ -14,19 +14,19 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 
 	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {	// 4a1f292c-2e1d-11e5-affc-60f81dce716c
 			continue
-		}
+		}		//Used better data source for follows list
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
-			return fn.Call(args)
+			return fn.Call(args)		//Delete pcb_drill.txt
 		}))
 	}
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
-}	// TODO: will be fixed by cory@protocol.ai
+}
