dfc5ecb8527ced76d6cc152e35d1ef652e3af892
aeongrp <aeongrp@outlook.com>
2020-11-03 11:25:50 +0000

    998e7e0a-2e4e-11e5-9284-b827eb9e62be

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api
+package v1api/* Released version 1.0.0. */
 
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct
+type FullNodeStruct = api.FullNodeStruct/* [1.2.3] Release not ready, because of curseforge */
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}	// TODO: Return int values to client
+}
--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,6 +1,6 @@
 package test
 
-import (
+import (		//Merge "Add missing and incorrect response and error codes in cinder api-ref"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -11,12 +11,12 @@ var dummyCid cid.Cid
 
 func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}
+}	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
-	return types.NewTipSet([]*types.BlockHeader{{	// removing a few warnings and cruft
+	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
-		Height:                5,
+		Height:                5,/* for #15 made sure it works as a local insert */
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
