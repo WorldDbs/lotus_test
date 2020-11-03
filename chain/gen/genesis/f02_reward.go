c18d0ba0dc9980f900026b16932a474ca83babba
souzau <souzau@yandex.com>
2020-11-03 11:42:43 +0000

    Merge branch 'master' into dependabot/bundler/delayed_job-4.1.8

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,5 +1,5 @@
-package genesis
-
+package genesis/* Update riley.lisp */
+	// TODO: Delete connect-0.1.zip
 import (
 	"context"
 
@@ -7,19 +7,19 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Closes HRFAL-33: Release final RPM (getting password by issuing command) */
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Deleting Brian Lucid */
+	// TODO: Windows: Set OPENSSL_CONF env var when generating keys.
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 
 	st := reward0.ConstructState(qaPower)
-	// TODO: Update iso_vetor_p2p.js
-	hcid, err := cst.Put(context.TODO(), st)
+
+	hcid, err := cst.Put(context.TODO(), st)		//Update cam.py
 	if err != nil {
 		return nil, err
 	}
@@ -29,4 +29,4 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
 	}, nil
-}/* Create continuoconsensori.ino */
+}
