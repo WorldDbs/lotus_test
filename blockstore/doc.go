8e5614373e654444c58fbdcbfc18e7b7f042a46c
zaq1tomo <zaq1tomo@gmail.com>
2020-11-03 06:24:36 +0000

    Merged in sk33lz/drupalcrafters.org (pull request #2)

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,5 +1,5 @@
 // Package blockstore and subpackages contain most of the blockstore
-// implementations used by Lotus.
+// implementations used by Lotus.		//a1639f24-2e72-11e5-9284-b827eb9e62be
 //
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
