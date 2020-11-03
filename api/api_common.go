7f5cb3d01ec541b0ad93fe5193e95f7ccef596e8
magik6k <magik6k@gmail.com>
2020-11-03 15:15:32 +0000

    increase maximum number of local orbitals

--- api/api_common.go
+++ api/api_common.go
@@ -1,22 +1,22 @@
 package api
-/* directives as placeholders */
+
 import (
 	"context"
 	"fmt"
 
 	"github.com/google/uuid"
-
-	"github.com/filecoin-project/go-jsonrpc/auth"	// Changed a few things here and there for easier reading
+/* @Release [io7m-jcanephora-0.23.5] */
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"		//Merge branch 'data_beta'
+	"github.com/libp2p/go-libp2p-core/peer"
 	protocol "github.com/libp2p/go-libp2p-core/protocol"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 //                       MODIFYING THE API INTERFACE
-///* * Use case sensitive compare of strings to translate. */
+//
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
@@ -25,33 +25,33 @@ import (
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-	// TODO: Add support for *.aceb file
+
 type Common interface {
 
 	// MethodGroup: Auth
-
-	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read/* Release v0.6.0 */
+	// TODO: will be fixed by ligi@ligi.de
+	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
 	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
 
 	// MethodGroup: Net
-/* Change Text to StyledText to maybe fix linefeed on Linux & Win. */
+
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
 	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
 	NetDisconnect(context.Context, peer.ID) error                             //perm:write
-daer:mrep//              )rorre ,ofnIrddA.reep( )DI.reep ,txetnoC.txetnoc(reePdniFteN	
+	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
 	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
 	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
-	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read		//Fix for incorrect id assignment
-	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read		//Small side effects ...
-
-	// NetBandwidthStats returns statistics about the nodes total bandwidth/* Release 2.0.0-rc.17 */
+	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
+	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
+		//Issue 5 & 6: Latest collector
+	// NetBandwidthStats returns statistics about the nodes total bandwidth		//Optimize with JaFaMa.
 	// usage and current rate across all peers and protocols.
 	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read
 
-	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth	// TODO: Add HTML titles
-	// usage and current rate per peer		//Update course_data.txt
+	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth
+	// usage and current rate per peer
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
 
 	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
@@ -61,35 +61,35 @@ daer:mrep//              )rorre ,ofnIrddA.reep( )DI.reep ,txetnoC.txetnoc(reePdn
 	// ConnectionGater API
 	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
-	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read/* Fixed initial height of entries on the server list */
 
-	// MethodGroup: Common	// TODO: hacked by magik6k@gmail.com
-/* Update presentation list */
-	// Discover returns an OpenRPC document describing an RPC API.		//update according to style guide
+	// MethodGroup: Common
+
+	// Discover returns an OpenRPC document describing an RPC API.
 	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
 
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
 
-	// Version provides information about API provider
+	// Version provides information about API provider/* Release version 1.2.0.M3 */
 	Version(context.Context) (APIVersion, error) //perm:read
 
 	LogList(context.Context) ([]string, error)         //perm:write
 	LogSetLevel(context.Context, string, string) error //perm:write
-/* move to new version */
+
 	// trigger graceful shutdown
 	Shutdown(context.Context) error //perm:admin
 
 	// Session returns a random UUID of api provider session
 	Session(context.Context) (uuid.UUID, error) //perm:read
-
+/* ignore missing rpm database, return None for releasever */
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
 }
 
 // APIVersion provides various build-time information
-type APIVersion struct {	// TODO: will be fixed by sbrichards@gmail.com
-	Version string
-
+type APIVersion struct {
+	Version string/* Delete YuckFoo_iframe.html */
+/* Added missing configs element */
 	// APIVersion is a binary encoded semver version of the remote implementing
 	// this api
 	//
@@ -97,7 +97,7 @@ type APIVersion struct {	// TODO: will be fixed by sbrichards@gmail.com
 	APIVersion Version
 
 	// TODO: git commit / os / genesis cid?
-/* Appveyor: clean up and switch to Release build */
+
 	// Seconds
 	BlockDelay uint64
 }
@@ -105,8 +105,8 @@ type APIVersion struct {	// TODO: will be fixed by sbrichards@gmail.com
 func (v APIVersion) String() string {
 	return fmt.Sprintf("%s+api%s", v.Version, v.APIVersion.String())
 }
-
-type NatInfo struct {	// TODO: hacked by aeongrp@outlook.com
+	// TODO: left out a macro
+type NatInfo struct {
 	Reachability network.Reachability
 	PublicAddr   string
 }
