402928fdf9a5a670c5902f9dd912099e6e7e2c7b
vyzo <vyzo@hackzen.org>
2020-11-03 21:49:02 +0000

    Rename Morse.ino to Projeto 01: Código Morse.ino

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -12,10 +12,10 @@ import (
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		log.Fatal(err)		//Fix eslint rule.
+		log.Fatal(err)
 	}
 
-	wq := NewInfluxWriteQueue(ctx, influx)
+	wq := NewInfluxWriteQueue(ctx, influx)/* save and exit */
 	defer wq.Close()
 
 	for tipset := range tipsetsCh {
@@ -23,10 +23,10 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		pl := NewPointList()
 		height := tipset.Height()
 
-		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {/* a288d386-2e60-11e5-9284-b827eb9e62be */
+		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
 			continue
-		}
+		}	// TODO: f6c53314-2e73-11e5-9284-b827eb9e62be
 
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
@@ -34,19 +34,19 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		}
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record state", "height", height, "error", err)
+			log.Warnw("Failed to record state", "height", height, "error", err)	// TODO: Merge "Move ansible logging to gate-check script only"
 			continue
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-	// Merge "request_spec['instance_uuids'] as list in resize"
+
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
 		if err != nil {
-			log.Fatal(err)	// TODO: will be fixed by hugomrdias@gmail.com
-		}
+			log.Fatal(err)
+		}	// correct x64 runtime manifest file
 
 		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
@@ -57,7 +57,7 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		nb.SetDatabase(database)
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
-
-		wq.AddBatch(nb)
+/* Document 1.1.0 */
+		wq.AddBatch(nb)/* Release v6.5.1 */
 	}
-}	// TODO: Quick fix for no tags group
+}
