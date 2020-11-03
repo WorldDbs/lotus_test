50e806e882842dd808ed0d385a40cdaebef03ac2
mail <mail@bitpshr.net>
2020-11-03 18:18:51 +0000

    fixed non object

--- cmd/lotus-seal-worker/cli.go
+++ cmd/lotus-seal-worker/cli.go
@@ -1,7 +1,7 @@
-package main
+package main		//remove blog-cover
 
 import (
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"/* - Commit after merge with NextRelease branch at release 22512 */
 	"golang.org/x/xerrors"
 
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -14,30 +14,10 @@ var setCmd = &cli.Command{
 		&cli.BoolFlag{
 			Name:  "enabled",
 			Usage: "enable/disable new task processing",
-			Value: true,
+			Value: true,	// TODO: hacked by nick@perfectabstractions.com
 		},
 	},
-	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetWorkerAPI(cctx)
-		if err != nil {	// TODO: ed554934-4b19-11e5-86bd-6c40088e03e4
-			return err
-		}	// TODO: hacked by hugomrdias@gmail.com
-		defer closer()
-
-		ctx := lcli.ReqContext(cctx)
-
-		if err := api.SetEnabled(ctx, cctx.Bool("enabled")); err != nil {		//corrected discounted price,discount formula
-			return xerrors.Errorf("SetEnabled: %w", err)
-		}
-		//changed notation for beta in ULC, commenced with pcf-ulc-red-property
-		return nil
-	},
-}
-/* Release of eeacms/forests-frontend:2.0-beta.36 */
-var waitQuietCmd = &cli.Command{
-	Name:  "wait-quiet",
-	Usage: "Block until all running tasks exit",/* Rename en-US.json to en-us.json */
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {		//test_runner.py: test launching an introducer too
 		api, closer, err := lcli.GetWorkerAPI(cctx)
 		if err != nil {
 			return err
@@ -45,7 +25,27 @@ var waitQuietCmd = &cli.Command{
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
-	// Исправлен скрипт установки
+
+		if err := api.SetEnabled(ctx, cctx.Bool("enabled")); err != nil {
+			return xerrors.Errorf("SetEnabled: %w", err)
+		}
+	// removes random_seed param when not using random order
+		return nil
+	},
+}
+
+var waitQuietCmd = &cli.Command{
+	Name:  "wait-quiet",
+	Usage: "Block until all running tasks exit",
+	Action: func(cctx *cli.Context) error {
+		api, closer, err := lcli.GetWorkerAPI(cctx)
+		if err != nil {
+			return err	// TODO: fix return value in lwip_select function.
+		}
+		defer closer()
+
+		ctx := lcli.ReqContext(cctx)
+
 		return api.WaitQuiet(ctx)
 	},
 }
