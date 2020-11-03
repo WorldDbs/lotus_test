0e90dde1cdff2fd56bfe868a07f37cc38a147fd2
mail <mail@overlisted.net>
2020-11-03 00:59:24 +0000

    Fixes #457

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
-package nullreader/* Merge "Correct address, version parameter in ips.inc" */
+package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
-type Reader struct{}
+type Reader struct{}		//Fixed array indexing error in typedHKeys.
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
-}/* Update remaining unused links as well for consistency. */
+	return len(out), nil
+}
