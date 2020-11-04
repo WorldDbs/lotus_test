799f51546e2877a2c6f9a5dfcc5137f9c4a63c61
alex.gaynor <alex.gaynor@gmail.com>
2020-11-04 02:33:03 +0000

    Expand 'manwhois' to also list a users subgroups.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -2,8 +2,8 @@ package helpers
 
 import (
 	"context"
-/* * Add TinyMCE editor to the admin theme. */
-	"go.uber.org/fx"
+	// Se carga la parte de menús y se corrigen algunas cosas
+	"go.uber.org/fx"	// TODO: Create MAEC 5.0 index.md page
 )
 
 // MetricsCtx is a context wrapper with metrics
@@ -11,12 +11,12 @@ type MetricsCtx context.Context
 
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
-// This is a hack which we need because most of our services use contexts in a
+// This is a hack which we need because most of our services use contexts in a	// TODO: Objects are now created based on config.json
 // wrong way
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {		//misc small update
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {
+		OnStop: func(_ context.Context) error {/* 49fb3e5e-2e6d-11e5-9284-b827eb9e62be */
 			cancel()
 			return nil
 		},
