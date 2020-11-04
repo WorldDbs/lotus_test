10ff610e87b9ebdba5c4f51ccfecd965014ac93a
peterke <peterke@gmail.com>
2020-11-04 03:46:31 +0000

    Add REPL note

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -12,12 +12,12 @@ type contextStore struct {
 	cst *cbor.BasicIpldStore
 }
 
-func (cs *contextStore) Context() context.Context {
+func (cs *contextStore) Context() context.Context {	// Removed extra couts to clean up output
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)	// aadca578-2e4f-11e5-8aea-28cfe91dbc4b
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64		//fix Git commit id detection
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem	// TODO: hacked by sbrichards@gmail.com
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
-	Max  int64
+	Max  int64	// d7fce714-2e6a-11e5-9284-b827eb9e62be
 	Used int64
 }
