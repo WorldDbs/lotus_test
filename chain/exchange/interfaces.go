607682f3ac9e15cf2f370d36ac8e79ff0d9dfb53
igor <igor@soramitsu.co.jp>
2020-11-04 18:37:55 +0000

    Moved around comments

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -3,20 +3,20 @@ package exchange
 import (
 	"context"
 
-	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"/* Update user_theme.php */
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//4ce0338e-2e60-11e5-9284-b827eb9e62be
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
-type Server interface {/* Release `0.2.0`  */
-	// HandleStream is the protocol handler to be registered on a libp2p
+type Server interface {	// TODO: Update restapi.clj
+	// HandleStream is the protocol handler to be registered on a libp2p		//Delete eulerPaper.ind
 	// protocol router.
-	//	// TODO: Add support for Review Diff Comment
+	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
@@ -27,9 +27,9 @@ type Server interface {/* Release `0.2.0`  */
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided
+	// GetBlocks fetches block headers from the network, from the provided	// TODO: hacked by remco@dutchcoders.io
 	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less./* Release 098. Added MultiKeyDictionary MultiKeySortedDictionary */
+	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
@@ -42,9 +42,9 @@ type Client interface {
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
-	AddPeer(peer peer.ID)/* Released XWiki 11.10.11 */
+	AddPeer(peer peer.ID)
 
-	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from.
-	RemovePeer(peer peer.ID)	// include Outdated Browser
+	// RemovePeer removes a peer from the pool of peers that the Client	// Merge "Adds standardised error messages"
+	// requests data from./* Fixed project paths to Debug and Release folders. */
+	RemovePeer(peer peer.ID)
 }
