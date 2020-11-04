5ef66a97de00d455e306ab90ea3ceea351f8d00a
zaq1tomo <zaq1tomo@gmail.com>
2020-11-04 06:25:50 +0000

    personal recommendations in readme.txt

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -6,8 +6,8 @@ type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
-}		//Protect disposing MesquiteFrame against exceptions (due to threading?)
+}
 
 func (fb *FullBlock) Cid() cid.Cid {
-	return fb.Header.Cid()/* centering threshold */
+	return fb.Header.Cid()
 }
