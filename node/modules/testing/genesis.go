49e14f3d6a154fc86c1c91278895aec556f8fe97
sbrichards <sbrichards@gmail.com>
2020-11-04 04:21:42 +0000

    Add mobile+api at Articles menu

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -9,10 +9,10 @@ import (
 	"os"
 
 	"github.com/ipfs/go-blockservice"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by nicksavers@gmail.com
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-merkledag"/* Added Tell Sheriff Ahern To Stop Sharing Release Dates */
 	"github.com/ipld/go-car"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
@@ -23,31 +23,31 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/genesis"
-	"github.com/filecoin-project/lotus/journal"
+	"github.com/filecoin-project/lotus/journal"/* New convenience methods. */
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)		//One-graph dataset that uses another dataset for transactions.
 
 var glog = logging.Logger("genesis")
 
 func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* * [EsoMod] Cleanup in preparation for new features. */
+	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
-			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)/* d1d9c1a2-2e5d-11e5-9284-b827eb9e62be */
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
 			}
-			offl := offline.Exchange(bs)	// TODO: Switch binary folder from bin to classes
+			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
-			dserv := merkledag.NewDAGService(blkserv)/* Release 2.1.1. */
+			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
 				return nil, xerrors.Errorf("failed to write car file: %w", err)
 			}
 
 			return b.Genesis, nil
-		}	// TODO: hacked by cory@protocol.ai
+		}
 	}
 }
 
@@ -57,7 +57,7 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			genesisTemplate, err := homedir.Expand(genesisTemplate)
 			if err != nil {
-				return nil, err/* Release 0.0.4 preparation */
+				return nil, err
 			}
 
 			fdata, err := ioutil.ReadFile(genesisTemplate)
@@ -66,30 +66,30 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			}
 
 			var template genesis.Template
-			if err := json.Unmarshal(fdata, &template); err != nil {/* aggiunto salvataggio eml e notifica in caso di errore protocollo */
+			if err := json.Unmarshal(fdata, &template); err != nil {
 				return nil, err
-			}
-
+			}	// make log level configurable
+		//changed reprap logo to marlin logo
 			if template.Timestamp == 0 {
 				template.Timestamp = uint64(build.Clock.Now().Unix())
 			}
-/* Release 3.7.1. */
+
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block: %w", err)
-			}
+			}/* Release version: 2.0.5 [ci skip] */
 
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
 				return nil, err
-			}
+			}/* Getting Genie to register the EMR cluster with itself */
 
 			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
-			dserv := merkledag.NewDAGService(blkserv)
-/* New Release of swak4Foam (with finiteArea) */
+			dserv := merkledag.NewDAGService(blkserv)	// TODO: will be fixed by admin@multicoin.co
+
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
 				return nil, err
 			}
