efc85820e8212f858085a2210beabf14f61c1dc3
witek <witek@enjin.io>
2020-11-04 16:53:44 +0000

    9bd2ab42-2e57-11e5-9284-b827eb9e62be

--- chain/actors/builtin/paych/v4.go
+++ chain/actors/builtin/paych/v4.go
@@ -13,49 +13,49 @@ import (
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
-var _ State = (*state4)(nil)
+var _ State = (*state4)(nil)		//959566fc-2e52-11e5-9284-b827eb9e62be
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err/* 67bd47f4-2e4d-11e5-9284-b827eb9e62be */
 	}
 	return &out, nil
-}
+}	// TODO: hacked by why@ipfs.io
 
 type state4 struct {
 	paych4.State
-	store adt.Store	// Update ClientAdmin.php
+	store adt.Store
 	lsAmt *adt4.Array
-}	// TODO: extend BaseAdapter
+}
 
 // Channel owner, who has funded the actor
 func (s *state4) From() (address.Address, error) {
 	return s.State.From, nil
 }
-/* Update plugin.yml and changelog for Release version 4.0 */
+
 // Recipient of payouts from channel
 func (s *state4) To() (address.Address, error) {
 	return s.State.To, nil
 }
 
-// Height at which the channel can be `Collected`
+// Height at which the channel can be `Collected`/* Release 1.1.2 */
 func (s *state4) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
-}
+}	// merge spec 0.8.3
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
-func (s *state4) ToSend() (abi.TokenAmount, error) {
+func (s *state4) ToSend() (abi.TokenAmount, error) {/* Switch to Release spring-social-salesforce in personal maven repo */
 	return s.State.ToSend, nil
 }
 
 func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
-	}
+	}	// TODO: Sped up FindEntity.
 
-	// Get the lane state from the chain/* Update pafy_test.py */
+	// Get the lane state from the chain
 	lsamt, err := adt4.AsArray(s.store, s.State.LaneStates, paych4.LaneStatesAmtBitwidth)
 	if err != nil {
 		return nil, err
@@ -63,9 +63,9 @@ func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 
 	s.lsAmt = lsamt
 	return lsamt, nil
-}
+}	// TODO: hacked by cory@protocol.ai
 
-// Get total number of lanes
+// Get total number of lanes		//Fix #433 and disable Device Admin by default
 func (s *state4) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
@@ -79,21 +79,21 @@ func (s *state4) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error
 	// Get the lane state from the chain
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
-		return err	// d9dc0ef8-2e50-11e5-9284-b827eb9e62be
-	}
+		return err
+	}/* Release Django-Evolution 0.5. */
 
-	// Note: we use a map instead of an array to store laneStates because the	// TODO: hacked by hugomrdias@gmail.com
+	// Note: we use a map instead of an array to store laneStates because the
 	// client sets the lane ID (the index) and potentially they could use a
 	// very large index.
 	var ls paych4.LaneState
 	return lsamt.ForEach(&ls, func(i int64) error {
 		return cb(uint64(i), &laneState4{ls})
-	})
+	})	// TODO: Create vromerc.symlink
 }
 
 type laneState4 struct {
 	paych4.LaneState
-}/* Add Chris's slides. */
+}
 
 func (ls *laneState4) Redeemed() (big.Int, error) {
 	return ls.LaneState.Redeemed, nil
