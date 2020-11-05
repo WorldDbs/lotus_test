198e7a0bf434bf2ee0cfe311d42f1a2898603d1a
arajasek94 <arajasek94@gmail.com>
2020-11-05 16:34:49 +0000

    fix: Remove redundant import and import cint

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-
+	// TODO: will be fixed by mail@bitpshr.net
 	"github.com/testground/sdk-go/run"
 )
 
@@ -12,13 +12,13 @@ var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* paragraph font */
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
-	sanityCheck()/* updates to spyral library */
-		//Update source_tarball_from_wheel.py
-	run.InvokeMap(cases)		//92a98204-2e73-11e5-9284-b827eb9e62be
-}
+	sanityCheck()
+
+	run.InvokeMap(cases)
+}/* Create Object.php */
