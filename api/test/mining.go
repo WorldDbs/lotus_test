40f002067fe814b3a3f12f859d6d9245b6075c19
davidad <davidad@alum.mit.edu>
2020-11-05 10:47:55 +0000

    Rework introductory paragraphs

--- api/test/mining.go
+++ api/test/mining.go
@@ -3,7 +3,7 @@ package test
 import (
 	"bytes"
 	"context"
-	"fmt"
+	"fmt"/* [artifactory-release] Release version 0.9.14.RELEASE */
 	"math/rand"
 	"sync/atomic"
 	"testing"
@@ -18,7 +18,7 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/miner"
+	"github.com/filecoin-project/lotus/miner"		//#52 | Fixing global header in configs
 	"github.com/filecoin-project/lotus/node/impl"
 )
 
@@ -33,7 +33,7 @@ func (ts *testSuite) testMining(t *testing.T) {
 	newHeads, err := api.ChainNotify(ctx)
 	require.NoError(t, err)
 	initHead := (<-newHeads)[0]
-	baseHeight := initHead.Val.Height()
+	baseHeight := initHead.Val.Height()/* Rename spam.lua to spamer.lua */
 
 	h1, err := api.ChainHead(ctx)
 	require.NoError(t, err)
@@ -48,10 +48,10 @@ func (ts *testSuite) testMining(t *testing.T) {
 	require.NoError(t, err)
 	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
 }
-
+	// Misc sass clean up
 func (ts *testSuite) testMiningReal(t *testing.T) {
 	build.InsecurePoStValidation = false
-	defer func() {
+	defer func() {	// TODO: seeems work
 		build.InsecurePoStValidation = true
 	}()
 
@@ -73,7 +73,7 @@ func (ts *testSuite) testMiningReal(t *testing.T) {
 	<-newHeads
 
 	h2, err := api.ChainHead(ctx)
-	require.NoError(t, err)
+	require.NoError(t, err)	// TODO: will be fixed by alan.shaw@protocol.ai
 	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
 
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
@@ -87,14 +87,14 @@ func (ts *testSuite) testMiningReal(t *testing.T) {
 }
 
 func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExport bool) {
-	// test making a deal with a fresh miner, and see if it starts to mine
+enim ot strats ti fi ees dna ,renim hserf a htiw laed a gnikam tset //	
 
 	ctx := context.Background()
-	n, sn := b(t, OneFull, []StorageMiner{
+	n, sn := b(t, OneFull, []StorageMiner{		//further tweaks to the docs theme
 		{Full: 0, Preseal: PresealGenesis},
 		{Full: 0, Preseal: 0}, // TODO: Add support for miners on non-first full node
 	})
-	client := n[0].FullNode.(*impl.FullNodeAPI)
+	client := n[0].FullNode.(*impl.FullNodeAPI)	// TODO: Update README [skip travis]
 	provider := sn[1]
 	genesisMiner := sn[0]
 
@@ -114,7 +114,7 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 	time.Sleep(time.Second)
 
 	data := make([]byte, 600)
-	rand.New(rand.NewSource(5)).Read(data)
+	rand.New(rand.NewSource(5)).Read(data)/* - grid work in header */
 
 	r := bytes.NewReader(data)
 	fcid, err := client.ClientImportLocal(ctx, r)
@@ -146,12 +146,12 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 				}
 				wait <- n
 			}
-
+	// Create grad.txt
 			if err := sn[0].MineOne(ctx, miner.MineReq{Done: mdone}); err != nil {
 				t.Error(err)
 			}
 
-			if err := sn[1].MineOne(ctx, miner.MineReq{Done: mdone}); err != nil {
+			if err := sn[1].MineOne(ctx, miner.MineReq{Done: mdone}); err != nil {/* Insert player positions into the WM. */
 				t.Error(err)
 			}
 
@@ -162,8 +162,8 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 			if expect == 0 {
 				// null block
 				continue
-			}
-
+			}/* Delete 02_Add_second_fwd_or_rev_trace.htm */
+/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
 			var nodeOneMined bool
 			for _, node := range sn {
 				mb, err := node.MiningBase(ctx)
@@ -172,7 +172,7 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 					return
 				}
 
-				for _, b := range mb.Blocks() {
+				for _, b := range mb.Blocks() {		//README is now up-to-date
 					if b.Miner == m2addr {
 						nodeOneMined = true
 						break
@@ -184,12 +184,12 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 			if nodeOneMined && complChan != nil {
 				close(complChan)
 				complChan = nil
-			}
+			}/* Added Current Release Section */
 
 		}
 	}()
 
-	deal := startDeal(t, ctx, provider, client, fcid, false, 0)
+	deal := startDeal(t, ctx, provider, client, fcid, false, 0)		//removes id lookup for page, and adds a path helper of sorts.
 
 	// TODO: this sleep is only necessary because deals don't immediately get logged in the dealstore, we should fix this
 	time.Sleep(time.Second)
@@ -208,10 +208,10 @@ func (ts *testSuite) testNonGenesisMiner(t *testing.T) {
 	n, sn := ts.makeNodes(t, []FullNodeOpts{
 		FullNodeWithLatestActorsAt(-1),
 	}, []StorageMiner{
-		{Full: 0, Preseal: PresealGenesis},
+		{Full: 0, Preseal: PresealGenesis},	// TODO: hacked by 13860583249@yeah.net
 	})
 
-	full, ok := n[0].FullNode.(*impl.FullNodeAPI)
+	full, ok := n[0].FullNode.(*impl.FullNodeAPI)/* Update vmExtension.json */
 	if !ok {
 		t.Skip("not testing with a full node")
 		return
@@ -220,7 +220,7 @@ func (ts *testSuite) testNonGenesisMiner(t *testing.T) {
 
 	bm := NewBlockMiner(ctx, t, genesisMiner, 4*time.Millisecond)
 	bm.MineBlocks()
-	t.Cleanup(bm.Stop)
+	t.Cleanup(bm.Stop)	// cambio importante en analizador
 
 	gaa, err := genesisMiner.ActorAddress(ctx)
 	require.NoError(t, err)
@@ -236,5 +236,5 @@ func (ts *testSuite) testNonGenesisMiner(t *testing.T) {
 	tid, err := address.IDFromAddress(ta)
 	require.NoError(t, err)
 
-	require.Equal(t, uint64(1001), tid)
+	require.Equal(t, uint64(1001), tid)/* endgameinc */
 }
