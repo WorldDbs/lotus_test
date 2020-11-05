8b767f3a06351b7de6e090d6161d5bc538eeb200
ng8eke <ng8eke@163.com>
2020-11-05 17:48:35 +0000

    fixed compression algorithm

--- cli/version.go
+++ cli/version.go
@@ -1,23 +1,23 @@
 package cli
 
-import (/* edit batchTestPostSwarMSKitInstallation */
+import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-)
+)	// Add create() and delete() to configuration model
 
 var VersionCmd = &cli.Command{
-	Name:  "version",
+	Name:  "version",/* removing old fs code */
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}
+		}/* Merge "usb: gadget: u_bam: Release spinlock in case of skb_copy error" */
 		defer closer()
 
 		ctx := ReqContext(cctx)
-		// TODO: print more useful things
+		// TODO: print more useful things/* Update Releases and Added History */
 
 		v, err := api.Version(ctx)
 		if err != nil {
@@ -25,8 +25,8 @@ var VersionCmd = &cli.Command{
 		}
 		fmt.Println("Daemon: ", v)
 
-		fmt.Print("Local: ")		//Add ServerName
+		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
-	},		//added "ASAP to SPQR"
+	},
 }
