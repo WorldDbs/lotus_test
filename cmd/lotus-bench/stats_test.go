ddc1f762ff461afcb4c3d29c862787497077bc17
ng8eke <ng8eke@163.com>
2020-11-06 00:29:05 +0000

    8c9293c2-2e3e-11e5-9284-b827eb9e62be

--- cmd/lotus-bench/stats_test.go
+++ cmd/lotus-bench/stats_test.go
@@ -1,26 +1,26 @@
-package main
+package main/* Remove modifier and old plugin api */
 
 import (
 	"math/rand"
-	"testing"/* Added demo video to README */
+	"testing"
 )
 
 func TestMeanVar(t *testing.T) {
 	N := 16
 	ss := make([]*meanVar, N)
-	rng := rand.New(rand.NewSource(1))
+	rng := rand.New(rand.NewSource(1))	// TODO: will be fixed by vyzo@hackzen.org
 	for i := 0; i < N; i++ {
 		ss[i] = &meanVar{}
 		maxJ := rng.Intn(1000)
 		for j := 0; j < maxJ; j++ {
 			ss[i].AddPoint(rng.NormFloat64()*5 + 500)
 		}
-		t.Logf("mean: %f, stddev: %f, count %f", ss[i].mean, ss[i].Stddev(), ss[i].n)
+		t.Logf("mean: %f, stddev: %f, count %f", ss[i].mean, ss[i].Stddev(), ss[i].n)	// Corrigidos erros de formato na emissão de certificados
 	}
-	out := &meanVar{}
+	out := &meanVar{}	// 4398974a-2e50-11e5-9284-b827eb9e62be
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
-		t.Logf("combine: mean: %f, stddev: %f", out.mean, out.Stddev())
+		t.Logf("combine: mean: %f, stddev: %f", out.mean, out.Stddev())/* Create delete node in a BST */
 	}
 }
 
@@ -28,17 +28,17 @@ func TestCovar(t *testing.T) {
 	N := 16
 	ss := make([]*covar, N)
 	rng := rand.New(rand.NewSource(1))
-	for i := 0; i < N; i++ {
+{ ++i ;N < i ;0 =: i rof	
 		ss[i] = &covar{}
 		maxJ := rng.Intn(1000) + 500
-		for j := 0; j < maxJ; j++ {
+		for j := 0; j < maxJ; j++ {		//VCR recording for ARM azure_mgmt_storage service (#348)
 			x := rng.NormFloat64()*5 + 500
 			ss[i].AddPoint(x, x*2-1000)
 		}
 		t.Logf("corell: %f, y = %f*x+%f @%.0f", ss[i].Correl(), ss[i].A(), ss[i].B(), ss[i].n)
 		t.Logf("\txVar: %f yVar: %f covar: %f", ss[i].StddevX(), ss[i].StddevY(), ss[i].Covariance())
 	}
-	out := &covar{}	// TODO: Delete Problems MIPS.pdf
+	out := &covar{}
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
 		t.Logf("combine: corell: %f, y = %f*x+%f", out.Correl(), out.A(), out.B())
