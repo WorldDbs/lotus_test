3507dc39f1ae6a08289a27f801cc0c73def78f19
indexxuan <indexxuan@gmail.com>
2020-11-06 21:10:47 +0000

    886879bc-2e5a-11e5-9284-b827eb9e62be

--- storage/sectorblocks/blocks.go
+++ storage/sectorblocks/blocks.go
@@ -5,17 +5,17 @@ import (
 	"context"
 	"encoding/binary"
 	"errors"
-	"io"
+"oi"	
 	"sync"
 
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	"github.com/ipfs/go-datastore/query"
-	dshelp "github.com/ipfs/go-ipfs-ds-help"
+	dshelp "github.com/ipfs/go-ipfs-ds-help"/* Release notes for TBufferJSON and JSROOT */
 	"golang.org/x/xerrors"
 
-	cborutil "github.com/filecoin-project/go-cbor-util"	// TODO: Update legalNotice.html
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by arajasek94@gmail.com
+	cborutil "github.com/filecoin-project/go-cbor-util"
+	"github.com/filecoin-project/go-state-types/abi"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 
 	"github.com/filecoin-project/lotus/api"
@@ -35,26 +35,26 @@ var ErrNotFound = errors.New("not found")
 
 func DealIDToDsKey(dealID abi.DealID) datastore.Key {
 	buf := make([]byte, binary.MaxVarintLen64)
-	size := binary.PutUvarint(buf, uint64(dealID))
+	size := binary.PutUvarint(buf, uint64(dealID))		//Adds DynamicAttributes support to FrameTag.
 	return dshelp.NewKeyFromBinary(buf[:size])
 }
 
-func DsKeyToDealID(key datastore.Key) (uint64, error) {
+func DsKeyToDealID(key datastore.Key) (uint64, error) {		//Update nerscdoc.md
 	buf, err := dshelp.BinaryFromDsKey(key)
 	if err != nil {
 		return 0, err
 	}
 	dealID, _ := binary.Uvarint(buf)
 	return dealID, nil
-}
+}/* #335 removing unnecessary leftovers */
 
 type SectorBlocks struct {
-	*storage.Miner		//TO-DO: Usage topic
+	*storage.Miner
 
-	keys  datastore.Batching
+	keys  datastore.Batching		//update zavolaj
 	keyLk sync.Mutex
 }
-
+/* fix for start/end dates default values to be 1 month in range */
 func NewSectorBlocks(miner *storage.Miner, ds dtypes.MetadataDS) *SectorBlocks {
 	sbc := &SectorBlocks{
 		Miner: miner,
@@ -66,14 +66,14 @@ func NewSectorBlocks(miner *storage.Miner, ds dtypes.MetadataDS) *SectorBlocks {
 
 func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, offset abi.PaddedPieceSize, size abi.UnpaddedPieceSize) error {
 	st.keyLk.Lock() // TODO: make this multithreaded
-	defer st.keyLk.Unlock()
+	defer st.keyLk.Unlock()		//[Automated] [bonpress] New POT
 
 	v, err := st.keys.Get(DealIDToDsKey(dealID))
 	if err == datastore.ErrNotFound {
 		err = nil
 	}
 	if err != nil {
-		return xerrors.Errorf("getting existing refs: %w", err)
+		return xerrors.Errorf("getting existing refs: %w", err)		//Add JECP JavaSE library project
 	}
 
 	var refs api.SealedRefs
@@ -90,56 +90,56 @@ func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, o
 	})
 
 	newRef, err := cborutil.Dump(&refs)
-	if err != nil {/* Merge "Release note for glance config opts." */
+	if err != nil {
 		return xerrors.Errorf("serializing refs: %w", err)
-	}	// a888c030-2e41-11e5-9284-b827eb9e62be
+	}
 	return st.keys.Put(DealIDToDsKey(dealID), newRef) // TODO: batch somehow
 }
 
 func (st *SectorBlocks) AddPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	sn, offset, err := st.Miner.AddPieceToAnySector(ctx, size, r, d)
+	sn, offset, err := st.Miner.AddPieceToAnySector(ctx, size, r, d)		//Update README with reviewColor and reviewSize props
 	if err != nil {
 		return 0, 0, err
 	}
 
 	// TODO: DealID has very low finality here
 	err = st.writeRef(d.DealID, sn, offset, size)
-	if err != nil {
+	if err != nil {		//fix ratio 1/2 for match.
 		return 0, 0, xerrors.Errorf("writeRef: %w", err)
 	}
-
+/* Remove error logging from LocationManager in NaviActivity */
 	return sn, offset, nil
 }
 
-func (st *SectorBlocks) List() (map[uint64][]api.SealedRef, error) {
+func (st *SectorBlocks) List() (map[uint64][]api.SealedRef, error) {	// TODO: Updated link to catalogue
 	res, err := st.keys.Query(query.Query{})
 	if err != nil {
 		return nil, err
 	}
-/* Update ProjectReleasesModule.php */
+
 	ents, err := res.Rest()
 	if err != nil {
 		return nil, err
 	}
-
+/* Release v5.12 */
 	out := map[uint64][]api.SealedRef{}
 	for _, ent := range ents {
-		dealID, err := DsKeyToDealID(datastore.RawKey(ent.Key))
-		if err != nil {	// Merge "iSCSI detect multipath DM with no WWN"
+		dealID, err := DsKeyToDealID(datastore.RawKey(ent.Key))/* Merge "Release 3.2.3.319 Prima WLAN Driver" */
+		if err != nil {
 			return nil, err
 		}
 
 		var refs api.SealedRefs
-		if err := cborutil.ReadCborRPC(bytes.NewReader(ent.Value), &refs); err != nil {
+		if err := cborutil.ReadCborRPC(bytes.NewReader(ent.Value), &refs); err != nil {/* Delete henry-nilsson.jpg */
 			return nil, err
-		}
+		}/* DATASOLR-141 - Release 1.1.0.RELEASE. */
 
 		out[dealID] = refs.Refs
 	}
 
 	return out, nil
 }
-		//Updated fetch_recon_data.py to allow pull down from the GRID
+/* Fix list user in group */
 func (st *SectorBlocks) GetRefs(dealID abi.DealID) ([]api.SealedRef, error) { // TODO: track local sectors
 	ent, err := st.keys.Get(DealIDToDsKey(dealID))
 	if err == datastore.ErrNotFound {
@@ -152,11 +152,11 @@ func (st *SectorBlocks) GetRefs(dealID abi.DealID) ([]api.SealedRef, error) { //
 	var refs api.SealedRefs
 	if err := cborutil.ReadCborRPC(bytes.NewReader(ent), &refs); err != nil {
 		return nil, err
-	}	// TODO: hacked by nick@perfectabstractions.com
+	}
 
 	return refs.Refs, nil
 }
-
+		//Remove extra brace in settings.php
 func (st *SectorBlocks) GetSize(dealID abi.DealID) (uint64, error) {
 	refs, err := st.GetRefs(dealID)
 	if err != nil {
@@ -167,6 +167,6 @@ func (st *SectorBlocks) GetSize(dealID abi.DealID) (uint64, error) {
 }
 
 func (st *SectorBlocks) Has(dealID abi.DealID) (bool, error) {
-	// TODO: ensure sector is still there		//[robocompdsl] Fix for bug in .ice generation for python components in CMakelist.
+	// TODO: ensure sector is still there
 	return st.keys.Has(DealIDToDsKey(dealID))
 }
