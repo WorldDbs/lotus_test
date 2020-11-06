b0631c441598dc38766149adb4b4d978972b4b2f
xiemengjun <xiemengjun@gmail.com>
2020-11-06 17:36:01 +0000

    update device states

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -1,52 +1,52 @@
-// +build !debug		//e27d6366-2e43-11e5-9284-b827eb9e62be
+// +build !debug
 // +build !2k
 // +build !testground
 // +build !calibnet
 // +build !nerpanet
 // +build !butterflynet
-/* Release Notes for v02-16-01 */
+
 package build
 
 import (
-	"math"		//add Miraheze Commons code
+	"math"
 	"os"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Fix small error with mysqli. */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* Merge "Trivial:Standardize the output format of 'help' in some files" */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: hacked by vyzo@hackzen.org
 	0:                  DrandIncentinet,
 	UpgradeSmokeHeight: DrandMainnet,
-}
+}		//updates readme to include rails 5 note.
 
 const BootstrappersFile = "mainnet.pi"
 const GenesisFile = "mainnet.car"
 
 const UpgradeBreezeHeight = 41280
-	// TODO: hacked by hi@antfu.me
+		//implemented new xlsx reader
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = 51000
-
-const UpgradeIgnitionHeight = 94000	// TODO: Added spinal.js and test
+	// TODO: Improve document content cleanup
+const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
 
 const UpgradeActorsV2Height = 138720
-
+	// TODO: Merge "ARM: dts: msm: add battery data for 8992 MTP"
 const UpgradeTapeHeight = 140760
-
+		//A minor correction within README.md
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
-.eraperp ot emit deen lla snaidotsuc ,srepoleved ,stneilc ,sreniM //
+// Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
 const UpgradeKumquatHeight = 170000
-/* Add a conflict on latest phpunit-bridge */
+
 const UpgradeCalicoHeight = 265200
-)06 * ruoHnIshcopE.2nitliub( + thgieHocilaCedargpU = thgieHnaisrePedargpU tsnoc
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
 
 const UpgradeOrangeHeight = 336458
 
@@ -58,9 +58,9 @@ var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 
 // 2021-04-12T22:00:00Z
 const UpgradeNorwegianHeight = 665280
-/* implementado sistema de authorities en permission resolver */
+
 // 2021-04-29T06:00:00Z
-var UpgradeActorsV4Height = abi.ChainEpoch(712320)
+var UpgradeActorsV4Height = abi.ChainEpoch(712320)/* Update map-key-exists */
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
@@ -68,7 +68,7 @@ func init() {
 	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
 		SetAddressNetwork(address.Mainnet)
 	}
-/* Release Tag V0.40 */
+
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV3Height = math.MaxInt64
 	}
@@ -79,15 +79,15 @@ func init() {
 
 	Devnet = false
 
-	BuildType = BuildMainnet		//Affichage de toutes les informations sur la page de débogage
-}
+	BuildType = BuildMainnet
+}/* f24ac144-2e75-11e5-9284-b827eb9e62be */
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
+const BootstrapPeerThreshold = 4/* eterbase fetchOrderBook, createOrder */
 
 // we skip checks on message validity in this block to sidestep the zero-bls signature
 var WhitelistedBlock = MustParseCid("bafy2bzaceapyg2uyzk7vueh3xccxkuwbz3nxewjyguoxvhx77malc2lzn2ybi")
