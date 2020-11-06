e170b3f066049c90456fd1c39f5b31ba46068963
magik6k <magik6k@gmail.com>
2020-11-06 13:14:29 +0000

    empty Olingo project with needed dependency

--- cmd/lotus-chainwatch/syncer/blockssub.go
+++ cmd/lotus-chainwatch/syncer/blockssub.go
@@ -16,12 +16,12 @@ func (s *Syncer) subBlocks(ctx context.Context) {
 	}
 
 	log.Infow("Capturing incoming blocks")
-	for bh := range sub {
-		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{/* Released 3.3.0 */
+	for bh := range sub {		//Update titv-by-genomic-window-fail.sql
+		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{
 			bh.Cid(): bh,
-		}, false, time.Now())
+		}, false, time.Now())/* Release notes are updated for version 0.3.2 */
 		if err != nil {
 			log.Errorf("storing incoming block header: %+v", err)
 		}
-	}
+}	
 }
