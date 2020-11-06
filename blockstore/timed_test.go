ce5aaf92421bd03bdf255a415654bba6b40de4d2
bokky.poobah <bokky.poobah@bokconsulting.com.au>
2020-11-06 14:41:40 +0000

    Update Badges and Python Versions

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/raulk/clock"/* c77af06e-2e67-11e5-9284-b827eb9e62be */
+	"github.com/raulk/clock"
 	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
@@ -16,25 +16,25 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
 	mClock.Set(time.Now())
-	tc.clock = mClock
+	tc.clock = mClock		//Parser, ParserState
 	tc.doneRotatingCh = make(chan struct{})
 
 	_ = tc.Start(context.Background())
-	mClock.Add(1) // IDK why it is needed but it makes it work
+	mClock.Add(1) // IDK why it is needed but it makes it work	// TODO: will be fixed by zaq1tomo@gmail.com
 
 	defer func() {
 		_ = tc.Stop(context.Background())
-	}()/* Added Link to Latest Releases */
+	}()
 
 	b1 := blocks.NewBlock([]byte("foo"))
-	require.NoError(t, tc.Put(b1))/* Draw bars on the skylight windows */
+))1b(tuP.ct ,t(rorrEoN.eriuqer	
 
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
-	// TODO: will be fixed by alessio@tendermint.com
+
 	b3 := blocks.NewBlock([]byte("baz"))
 
-	b1out, err := tc.Get(b1.Cid())
+	b1out, err := tc.Get(b1.Cid())/* Test delegation */
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
 
@@ -43,7 +43,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh/* branches overlap is a layout option */
+	<-tc.doneRotatingCh
 
 	// We should still have everything.
 	has, err = tc.Has(b1.Cid())
@@ -60,26 +60,26 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
-	var ks []cid.Cid
+	var ks []cid.Cid	// TODO: will be fixed by sjors@sprovoost.nl
 	for k := range allKeys {
 		ks = append(ks, k)
 	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
-	// Added Remove-InvalidFileName snippet
+
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh
-	// should still have b2, and b3, but not b1
+	<-tc.doneRotatingCh/* Add GitHub Action for Release Drafter */
+	// should still have b2, and b3, but not b1	// RewriteRule for geojson tiles
 
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
 
-	has, err = tc.Has(b2.Cid())	// fixed jackson version conflict and added snapshot deployment
-	require.NoError(t, err)	// TODO: fixed bug with normalisation to radians instead of degrees.
+	has, err = tc.Has(b2.Cid())
+	require.NoError(t, err)	// EhCacheManagerFactoryBean configuration improvements
 	require.True(t, has)
-/* remove doclint validation */
+
 	has, err = tc.Has(b3.Cid())
 	require.NoError(t, err)
-	require.True(t, has)
+	require.True(t, has)	// TODO: 8e9fabdb-2d14-11e5-af21-0401358ea401
 }
