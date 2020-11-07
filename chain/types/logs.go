c6ce2303a6e0ba2cf7b40d5db4310fab95760631
hugomrdias <hugomrdias@gmail.com>
2020-11-07 06:48:28 +0000

    Fixed NullPointerExceptions being thrown from ABCActorTest

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,15 +1,15 @@
 package types
-/* Release 1.9.4 */
+
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
 )
 
 type LogCids []cid.Cid
-/* Release 0.7.11 */
+
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
-func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {		//optimizing Scheduler
+func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
