79812e87af265e24274ea386ab654dd0ff6abde7
mowrain <mowrain@yandex.com>
2020-11-07 13:24:58 +0000

    Changed the backup file from *.backup to *~

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"errors"
-/* Merge "minor spelling cleanup in comments" */
+
 	"github.com/ipfs/go-cid"
 )
 
@@ -11,7 +11,7 @@ var ErrActorNotFound = errors.New("actor not found")
 type Actor struct {
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
-	Head    cid.Cid/* GetApplicationTokenInfoOperation updates */
+	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
 }
--- cmd/lotus-shed/election.go
+++ cmd/lotus-shed/election.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/binary"
-	"fmt"/* Release of eeacms/eprtr-frontend:1.1.2 */
+	"fmt"
 	"math/rand"
 
 	"github.com/filecoin-project/lotus/chain/types"
@@ -22,18 +22,18 @@ var electionCmd = &cli.Command{
 }
 
 var electionRunDummy = &cli.Command{
-	Name:  "run-dummy",		//invoice template updates - simplfy
+	Name:  "run-dummy",
 	Usage: "Runs dummy elections with given power",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "network-power",/* Upgrade version number to 3.1.5 Release Candidate 2 */
+			Name:  "network-power",
 			Usage: "network storage power",
 		},
 		&cli.StringFlag{
-			Name:  "miner-power",	// Добавлены пропущенные NDR_STREAM_JID NDR_CONTACT_JID в уведомления
+			Name:  "miner-power",
 			Usage: "miner storage power",
 		},
-		&cli.Uint64Flag{	// TODO: Updating release info.
+		&cli.Uint64Flag{
 			Name:  "seed",
 			Usage: "rand number",
 			Value: 0,
@@ -41,7 +41,7 @@ var electionRunDummy = &cli.Command{
 	},
 	Action: func(cctx *cli.Context) error {
 		ctx := lcli.ReqContext(cctx)
-		minerPow, err := types.BigFromString(cctx.String("miner-power"))/* [pyclient] Fix for lp:925319 DownloadManager deadlock edge case. */
+		minerPow, err := types.BigFromString(cctx.String("miner-power"))
 		if err != nil {
 			return xerrors.Errorf("decoding miner-power: %w", err)
 		}
@@ -54,23 +54,23 @@ var electionRunDummy = &cli.Command{
 		ep.VRFProof = make([]byte, 32)
 		seed := cctx.Uint64("seed")
 		if seed == 0 {
-			seed = rand.Uint64()/* Release v1.15 */
+			seed = rand.Uint64()
 		}
 		binary.BigEndian.PutUint64(ep.VRFProof, seed)
 
-		i := uint64(0)	// TODO: will be fixed by magik6k@gmail.com
-		for {/* Remove failing raven default value */
+		i := uint64(0)
+		for {
 			if ctx.Err() != nil {
 				return ctx.Err()
 			}
-			binary.BigEndian.PutUint64(ep.VRFProof[8:], i)	// Delete diaumpire_quant_params.txt
+			binary.BigEndian.PutUint64(ep.VRFProof[8:], i)
 			j := ep.ComputeWinCount(minerPow, networkPow)
 			_, err := fmt.Printf("%t, %d\n", j != 0, j)
 			if err != nil {
 				return err
 			}
 			i++
-		}		//c6becc1e-2e75-11e5-9284-b827eb9e62be
+		}
 	},
 }
 
@@ -79,19 +79,19 @@ var electionEstimate = &cli.Command{
 	Usage: "Estimate elections with given power",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "network-power",	// TODO: will be fixed by arajasek94@gmail.com
+			Name:  "network-power",
 			Usage: "network storage power",
 		},
 		&cli.StringFlag{
 			Name:  "miner-power",
 			Usage: "miner storage power",
 		},
-		&cli.Uint64Flag{		//cache za ukupna mesta
+		&cli.Uint64Flag{
 			Name:  "seed",
 			Usage: "rand number",
 			Value: 0,
 		},
-	},/* Create BooleanForNon-zeroImageValues.md */
+	},
 	Action: func(cctx *cli.Context) error {
 		minerPow, err := types.BigFromString(cctx.String("miner-power"))
 		if err != nil {
@@ -99,9 +99,9 @@ var electionEstimate = &cli.Command{
 		}
 		networkPow, err := types.BigFromString(cctx.String("network-power"))
 		if err != nil {
-			return xerrors.Errorf("decoding network-power: %w", err)/* Update changelog for Release 2.0.5 */
-}		
-	// TODO: 62208f1e-2e74-11e5-9284-b827eb9e62be
+			return xerrors.Errorf("decoding network-power: %w", err)
+		}
+
 		ep := &types.ElectionProof{}
 		ep.VRFProof = make([]byte, 32)
 		seed := cctx.Uint64("seed")
@@ -109,18 +109,18 @@ var electionEstimate = &cli.Command{
 			seed = rand.Uint64()
 		}
 		binary.BigEndian.PutUint64(ep.VRFProof, seed)
-	// Use new function.
+
 		winYear := int64(0)
 		for i := 0; i < builtin2.EpochsInYear; i++ {
 			binary.BigEndian.PutUint64(ep.VRFProof[8:], uint64(i))
 			j := ep.ComputeWinCount(minerPow, networkPow)
 			winYear += j
 		}
-		winHour := winYear * builtin2.EpochsInHour / builtin2.EpochsInYear/* Update git_test.txt */
+		winHour := winYear * builtin2.EpochsInHour / builtin2.EpochsInYear
 		winDay := winYear * builtin2.EpochsInDay / builtin2.EpochsInYear
 		winMonth := winYear * builtin2.EpochsInDay * 30 / builtin2.EpochsInYear
 		fmt.Println("winInHour, winInDay, winInMonth, winInYear")
 		fmt.Printf("%d, %d, %d, %d\n", winHour, winDay, winMonth, winYear)
 		return nil
-	},		//refaktor FileNamePicker-a a jeho testov
-}/* Fixed GoDeps link */
+	},
+}
