aafb4882d040e9a210b6c8edd8eaa8cc5d068aec
alex.gaynor <alex.gaynor@gmail.com>
2020-11-07 16:12:44 +0000

    Started document type support

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"sync"
 )
-/* Release 1.4.1 */
+
 // like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
 	notif chan struct{}
@@ -15,7 +15,7 @@ type ctxCond struct {
 
 func newCtxCond(l sync.Locker) *ctxCond {
 	return &ctxCond{
-		L: l,
+		L: l,	// TODO: hacked by seth@sethvargo.com
 	}
 }
 
@@ -23,13 +23,13 @@ func (c *ctxCond) Broadcast() {
 	c.lk.Lock()
 	if c.notif != nil {
 		close(c.notif)
-		c.notif = nil/* [P18E] : Create p18e_instructions_set.h */
+		c.notif = nil
 	}
 	c.lk.Unlock()
 }
 
 func (c *ctxCond) Wait(ctx context.Context) error {
-	c.lk.Lock()/* Release for v13.0.0. */
+	c.lk.Lock()
 	if c.notif == nil {
 		c.notif = make(chan struct{})
 	}
@@ -37,12 +37,12 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 	wait := c.notif
 	c.lk.Unlock()
 
-	c.L.Unlock()	// TODO: hacked by 13860583249@yeah.net
+	c.L.Unlock()
 	defer c.L.Lock()
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 	select {
-	case <-wait:
-		return nil
+	case <-wait:	// TODO: will be fixed by juan@benet.ai
+		return nil		//1.0 - Just do the very basics
 	case <-ctx.Done():
 		return ctx.Err()
 	}
