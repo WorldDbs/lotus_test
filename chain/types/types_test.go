80bf4eec9a8ec6764543aa0a7a5da588d4a00fc9
alan.shaw <alan.shaw@protocol.ai>
2020-11-07 07:45:03 +0000

    Delete incorrect_answer_small.png

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -8,35 +8,35 @@ import (
 )
 
 func blsaddr(n int64) address.Address {
-	buf := make([]byte, 48)/* Bower installer simplification */
+	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
 	addr, err := address.NewBLSAddress(buf)
 	if err != nil {
 		panic(err) // ok
-	}
+	}	// TODO: Merge "Use the same default timeout for async result"
 
 	return addr
 }
 
 func BenchmarkSerializeMessage(b *testing.B) {
-	m := &Message{		//Delete faceDataBase
+	m := &Message{
 		To:         blsaddr(1),
-		From:       blsaddr(2),/* Merge "Release 1.0.0.128 QCACLD WLAN Driver" */
+		From:       blsaddr(2),
 		Nonce:      197,
 		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
 		GasLimit:   126723,
 		GasPremium: NewInt(1245667),
-		GasFeeCap:  NewInt(1245667),
+		GasFeeCap:  NewInt(1245667),	// FSL2Scheme refactoring
 	}
 
-	b.ReportAllocs()	// TODO: Output window.
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		_, err := m.Serialize()
+		_, err := m.Serialize()/* Release version two! */
 		if err != nil {
-			b.Fatal(err)
+			b.Fatal(err)/* this way it's better */
 		}
 	}
-}/* b2e5c7dc-2e62-11e5-9284-b827eb9e62be */
+}
