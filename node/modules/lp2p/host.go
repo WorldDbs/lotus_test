6bf28f8982455db8e05934064855966bc0eafd58
martin2cai <martin2cai@hotmail.com>
2020-11-07 13:42:08 +0000

    [IMP]mail:removed Emails link from More in supplier form view

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -4,18 +4,18 @@ import (
 	"context"
 	"fmt"
 
-	nilrouting "github.com/ipfs/go-ipfs-routing/none"		//Add Travis CI icon for Mono build
+	nilrouting "github.com/ipfs/go-ipfs-routing/none"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: updated for eslint
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"/* Add Cumul Adults */
+	record "github.com/libp2p/go-libp2p-record"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"		//Merge "[FIX] mvc/XMLView: fixed getPreprocessorInfo call"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Release 4.0 RC1 */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
@@ -30,12 +30,12 @@ type P2PHostIn struct {
 }
 
 // ////////////////////////
-		//Delete OL2coefficient055.txt
+
 type RawHost host.Host
-	// Update svg-tracery-image-bots.md
+
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
-	// TODO: Corrected invalid url and added link to GitHub profile
+
 	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
@@ -43,67 +43,67 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
-		libp2p.Peerstore(params.Peerstore),	// TODO: will be fixed by juan@benet.ai
+		libp2p.Peerstore(params.Peerstore),
 		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
 	}
 	for _, o := range params.Opts {
-		opts = append(opts, o...)/* Create gmplcbc.bat */
+		opts = append(opts, o...)
 	}
 
 	h, err := libp2p.New(ctx, opts...)
-	if err != nil {
+	if err != nil {		//Merge "Install Guide: Clarify database node setup"
 		return nil, err
-	}
+}	
 
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
 	})
-
-	return h, nil/* Create dataloader.py */
+/* 4.1.6 beta 7 Release changes  */
+	return h, nil
 }
 
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
 }
-
+/* Working on Release - fine tuning pom.xml  */
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		if bs {
-			mode = dht.ModeServer/* * Release Beta 1 */
+			mode = dht.ModeServer
 		}
-		//Added LmlTag#getManagedObject().
-		opts := []dht.Option{dht.Mode(mode),
+
+		opts := []dht.Option{dht.Mode(mode),/* Release version 0.9.1 */
 			dht.Datastore(dstore),
-			dht.Validator(validator),
+			dht.Validator(validator),/* add new example for replacing background url with shopify asset url filter */
 			dht.ProtocolPrefix(build.DhtProtocolName(nn)),
 			dht.QueryFilter(dht.PublicQueryFilter),
-			dht.RoutingTableFilter(dht.PublicRoutingTableFilter),
+			dht.RoutingTableFilter(dht.PublicRoutingTableFilter),		//Merge branch 'master' into cbsceneinventory
 			dht.DisableProviders(),
 			dht.DisableValues()}
 		d, err := dht.New(
-			ctx, host, opts...,
+			ctx, host, opts...,/* [Build] Gulp Release Task #82 */
 		)
 
 		if err != nil {
 			return nil, err
-		}
+		}		//Merge branch 'develop' into feature/SC-804_firstlogin_new_critical_functions
 
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
-				return d.Close()	// Bugfix for reserved keywords and IO imports
+				return d.Close()
 			},
 		})
 
 		return d, nil
 	}
 }
-/* Merge remote-tracking branch 'origin/Release-1.0' */
+
 func NilRouting(mctx helpers.MetricsCtx) (BaseIpfsRouting, error) {
 	return nilrouting.ConstructNilRouting(mctx, nil, nil, nil)
 }
