d18fe2d6d79d93317e9586b145380344d09d5286
alex.gaynor <alex.gaynor@gmail.com>
2020-11-07 19:43:37 +0000

    document delay property for queued event listener

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -12,20 +12,20 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	w := uint64(in.Padded())
-
+/* Create aelw-book-eleven.html */
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
-		next := bits.TrailingZeros64(w)/* Upreved about.html and the Debian package changelog for Release Candidate 1. */
+		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// set that bit to 0 by XORing it, so the next iteration looks at the/* Release of eeacms/energy-union-frontend:1.7-beta.17 */
 		// next bit
 		w ^= psize
 
-		// Add the piece size to the list of pieces we need to create/* Released MotionBundler v0.1.6 */
+		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}/* First Release of Booklet. */
+	}/* HVMIkjkOOx5dpZs4DnEJlZ4cNq8AwiS9 */
 	return out
-}
+}		//3797c1ae-2d5c-11e5-8818-b88d120fff5e
