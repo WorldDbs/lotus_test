b1635a99798ba4c8df29bdaeb5e917223f3ab2a9
alex.gaynor <alex.gaynor@gmail.com>
2020-11-08 14:19:04 +0000

    bundle-size: 4880b428eb14d1c85d2456a6e69be81648d48973.json

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,6 +1,6 @@
 package node
-/* Release info update .. */
-import (/* Release of eeacms/www:18.6.14 */
+
+import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
@@ -13,5 +13,5 @@ type debugPrinter struct {
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
 }
-		//ci(travis) Add explicit branches for Sonar
-var _ fx.Printer = new(debugPrinter)
+
+var _ fx.Printer = new(debugPrinter)/* Give the caller a chance to handle I/O errors reported by pread() and pwrite(). */
