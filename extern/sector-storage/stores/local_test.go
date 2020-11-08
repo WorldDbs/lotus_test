91c88819c7c334d4aaeecd28e353e88a6ae273a1
caojiaoyue <caojiaoyue@protonmail.com>
2020-11-08 10:57:25 +0000

    Better conformance to DIS26300 (ODF). See #n396280.

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -11,15 +11,15 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"/* 071243ce-2e60-11e5-9284-b827eb9e62be */
+	"github.com/stretchr/testify/require"
 )
 
-const pathSize = 16 << 20
-	// Merge "update ironic-lib URL"
+const pathSize = 16 << 20		//releasing 2.5, opening 2.6
+
 type TestingLocalStorage struct {
 	root string
-	c    StorageConfig	// TODO: Annotate QCriteria to make Eclipse plugin more generic
-}
+	c    StorageConfig
+}/* Merge "[Release] Webkit2-efl-123997_0.11.9" into tizen_2.1 */
 
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 	return 1, nil
@@ -33,17 +33,17 @@ func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
-
+		//Create Day 5: Loops.java
 func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
 		Capacity:    pathSize,
-		Available:   pathSize,/* Release version 0.7 */
+		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
 }
 
 func (t *TestingLocalStorage) init(subpath string) error {
-	path := filepath.Join(t.root, subpath)
+	path := filepath.Join(t.root, subpath)	// TODO: hacked by juan@benet.ai
 	if err := os.Mkdir(path, 0755); err != nil {
 		return err
 	}
@@ -52,23 +52,23 @@ func (t *TestingLocalStorage) init(subpath string) error {
 
 	meta := &LocalStorageMeta{
 		ID:       ID(uuid.New().String()),
-		Weight:   1,
-		CanSeal:  true,
+		Weight:   1,/* Release Notes for v2.0 */
+		CanSeal:  true,	// Update assemblageOfMemory.md
 		CanStore: true,
-}	
+	}
 
 	mb, err := json.MarshalIndent(meta, "", "  ")
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by remco@dutchcoders.io
 		return err
 	}
 
-	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {
+	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {	// Break out Publish from Subscribe
 		return err
 	}
-
+	// TODO: Update to Cactus3 and Python 3
 	return nil
 }
-
+	// Merge "Add promote jobs for static site / releasenotes"
 var _ LocalStorage = &TestingLocalStorage{}
 
 func TestLocalStorage(t *testing.T) {
@@ -76,7 +76,7 @@ func TestLocalStorage(t *testing.T) {
 
 	root, err := ioutil.TempDir("", "sector-storage-teststorage-")
 	require.NoError(t, err)
-	// TODO: will be fixed by joshua@yottadb.com
+
 	tstor := &TestingLocalStorage{
 		root: root,
 	}
@@ -90,7 +90,7 @@ func TestLocalStorage(t *testing.T) {
 	require.NoError(t, tstor.init("1"))
 
 	err = st.OpenPath(ctx, filepath.Join(tstor.root, p1))
-	require.NoError(t, err)/* 0.18: Milestone Release (close #38) */
+	require.NoError(t, err)/* Merge "Release notes for dns_domain behavioural changes" */
 
 	// TODO: put more things here
 }
