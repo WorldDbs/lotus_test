cd96e969851781acaec07ceb384ca4d12e56cdd7
arajasek94 <arajasek94@gmail.com>
2020-11-08 07:04:29 +0000

    Throw exception if the profile entry is not found

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,21 +1,21 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"	// TODO: hacked by sebastian.tharakan97@gmail.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//0.1.20_6-ALPHA
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	// TODO: Merge Toolbar/Menu from gtk/eagle.py
+
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)
+)/* Bug fix, remove parameter, and server format check */
 
-var _ State = (*state0)(nil)/* Release 0.95.161 */
+var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -29,29 +29,29 @@ type state0 struct {
 }
 
 func (s *state0) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil/*   * TODO: uses usr/lib/jvm/java-gcj  while openjdk-6-jdk installed */
+	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//Add export of data to LDM
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
-	// TODO: hacked by aeongrp@outlook.com
+
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}/* added xeon phi sysmem benchmark in sysmem manager.  */
-
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// gerbview: refresh screen when active layer is selecte from the layer mamager.
+}
+/* use only one SealerAES for TX/RX */
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}
-	// TODO: Fix destroyEntity so it rms all colls entity has
+}/* don't make more calls to the disk thread when aborting */
+
 func (s *state0) verifiers() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.Verifiers)/* AniImageList.java Fix. Ability WSYSTEMFC removed. ANISMILES in InfoWindow.java */
+	return adt0.AsMap(s.store, s.Verifiers)
 }
