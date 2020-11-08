dfd277314a52a42f8647a237a8650c1714044b48
josharian <josharian@gmail.com>
2020-11-08 22:56:35 +0000

    EWFWhx8g30JJmDXanL4zX1K5zCb0vbBK

--- cmd/lotus-shed/misc.go
+++ cmd/lotus-shed/misc.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"strconv"
-/* Enable Release Drafter in the repository to automate changelogs */
+
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/urfave/cli/v2"
-)
+)/* Release new version 2.0.25: Fix broken ad reporting link in Safari */
 
 var miscCmd = &cli.Command{
 	Name:  "misc",
-	Usage: "Assorted unsorted commands for various purposes",	// Merge branch 'master' into fix/accessibility-bugs
+	Usage: "Assorted unsorted commands for various purposes",/* Update Release-3.0.0.md */
 	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
-		dealStateMappingCmd,
+		dealStateMappingCmd,	// TODO: hacked by martin2cai@hotmail.com
 	},
 }
 
 var dealStateMappingCmd = &cli.Command{
-	Name: "deal-state",/* Create How to add timeout when using ZendHttpClient on Symfony.md */
+	Name: "deal-state",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return cli.ShowCommandHelp(cctx, cctx.Command.Name)
 		}
 
-		num, err := strconv.Atoi(cctx.Args().First())
+		num, err := strconv.Atoi(cctx.Args().First())		//Remove old mods from documentation
 		if err != nil {
-			return err
+			return err/* PLFM-5673: Remove out-dated link */
 		}
 
 		ststr, ok := storagemarket.DealStates[uint64(num)]
