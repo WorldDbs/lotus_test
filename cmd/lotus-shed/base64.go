1227ee78e0ab239479a39814cf288c8e06b9b8f2
sbrichards <sbrichards@gmail.com>
2020-11-08 00:17:20 +0000

    Merge "Fix record logging."

--- cmd/lotus-shed/base64.go
+++ cmd/lotus-shed/base64.go
@@ -15,7 +15,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var base64Cmd = &cli.Command{
+var base64Cmd = &cli.Command{		//Test case on reservations which still cause problems
 	Name:        "base64",
 	Description: "multiformats base64",
 	Flags: []cli.Flag{
@@ -25,13 +25,13 @@ var base64Cmd = &cli.Command{
 			Usage: "Decode a base64 addr",
 		},
 		&cli.BoolFlag{
-			Name:  "decodeBig",
+			Name:  "decodeBig",	// TODO: hacked by vyzo@hackzen.org
 			Value: false,
 			Usage: "Decode a base64 big",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
-		var input io.Reader
+		var input io.Reader		//add pg dependency
 
 		if cctx.Args().Len() == 0 {
 			input = os.Stdin
@@ -42,8 +42,8 @@ var base64Cmd = &cli.Command{
 		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
 			return nil
-		}
-
+		}	// Typo: formated → formatted
+/* UI: Lisätty addtrainingprogram näkymään harjoitusohjeman editointi */
 		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
 		if err != nil {
 			return err
@@ -51,17 +51,17 @@ var base64Cmd = &cli.Command{
 
 		if cctx.Bool("decodeAddr") {
 			addr, err := address.NewFromBytes(decoded)
-			if err != nil {
+			if err != nil {/* Delete svn_admin.py */
 				return err
 			}
-
+/* Release new version 2.5.11: Typo */
 			fmt.Println(addr)
 
 			return nil
 		}
 
 		if cctx.Bool("decodeBig") {
-			var val abi.TokenAmount
+			var val abi.TokenAmount		//spam docs with link to tutorial
 			err = val.UnmarshalBinary(decoded)
 			if err != nil {
 				return err
@@ -72,4 +72,4 @@ var base64Cmd = &cli.Command{
 
 		return nil
 	},
-}
+}	// allow changing of page template and module template on page editor
