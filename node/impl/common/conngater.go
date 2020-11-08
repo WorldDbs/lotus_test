840fa95a8264346bbbff6d97078ce62714f453f1
bokky.poobah <bokky.poobah@bokconsulting.com.au>
2020-11-08 07:57:28 +0000

    Create slider-button-left.png

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -1,4 +1,4 @@
-package common/* Fixed new items getting same ids */
+package common
 
 import (
 	"context"
@@ -10,13 +10,13 @@ import (
 	manet "github.com/multiformats/go-multiaddr/net"
 
 	"github.com/filecoin-project/lotus/api"
-)/* [builder] rebuild dependent resources when referenced java types change */
+)
 
 var cLog = logging.Logger("conngater")
 
 func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
-		err := a.ConnGater.BlockPeer(p)
+		err := a.ConnGater.BlockPeer(p)/* changes Release 0.1 to Version 0.1.0 */
 		if err != nil {
 			return xerrors.Errorf("error blocking peer %s: %w", p, err)
 		}
@@ -33,20 +33,20 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
-			return xerrors.Errorf("error parsing IP address %s", addr)		//Merge from Mikkel
-		}	// TODO: just more flags
+			return xerrors.Errorf("error parsing IP address %s", addr)
+		}
 
-		err := a.ConnGater.BlockAddr(ip)/* Add publish to git. Release 0.9.1. */
+		err := a.ConnGater.BlockAddr(ip)
 		if err != nil {
 			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
 		}
-
-		for _, c := range a.Host.Network().Conns() {
+	// Delete BB-UNIT_silkBottom.gbo
+		for _, c := range a.Host.Network().Conns() {/* Merge branch 'master' into plan_timeout */
 			remote := c.RemoteMultiaddr()
 			remoteIP, err := manet.ToIP(remote)
 			if err != nil {
 				continue
-			}		//Update autogenerateBatch0.yml
+			}
 
 			if ip.Equal(remoteIP) {
 				err = c.Close()
@@ -55,13 +55,13 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
 				}
 			}
-		}	// TODO: Update CustomFields.php
+		}	// TODO: hacked by ligi@ligi.de
 	}
 
-	for _, subnet := range acl.IPSubnets {/* [artifactory-release] Release version 3.0.0.RC2 */
+	for _, subnet := range acl.IPSubnets {
 		_, cidr, err := net.ParseCIDR(subnet)
 		if err != nil {
-			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)	// TODO: hacked by sbrichards@gmail.com
+			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)
 		}
 
 		err = a.ConnGater.BlockSubnet(cidr)
@@ -71,9 +71,9 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 
 		for _, c := range a.Host.Network().Conns() {
 			remote := c.RemoteMultiaddr()
-			remoteIP, err := manet.ToIP(remote)
+			remoteIP, err := manet.ToIP(remote)/* Fixed python exception reporting by filtering html special chars */
 			if err != nil {
-				continue
+				continue/* even better set_county/zone logic */
 			}
 
 			if cidr.Contains(remoteIP) {
@@ -85,27 +85,27 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 			}
 		}
 	}
-
+	// TODO: Create StatusResponse.java
 	return nil
 }
-/* b7f8913e-2e42-11e5-9284-b827eb9e62be */
+
 func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
 		err := a.ConnGater.UnblockPeer(p)
-		if err != nil {
+		if err != nil {/* Deleted msmeter2.0.1/Release/timers.obj */
 			return xerrors.Errorf("error unblocking peer %s: %w", p, err)
 		}
-	}/* removed ubuntu nvidia recompile */
+	}
 
 	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
 			return xerrors.Errorf("error parsing IP address %s", addr)
-		}
+		}	// TODO: Print server config path
 
-		err := a.ConnGater.UnblockAddr(ip)
-		if err != nil {/* Upgrade tp Release Canidate */
-			return xerrors.Errorf("error unblocking IP address %s: %w", addr, err)		//Create wordpressdotcom.md
+		err := a.ConnGater.UnblockAddr(ip)		//environment tuning
+		if err != nil {
+			return xerrors.Errorf("error unblocking IP address %s: %w", addr, err)
 		}
 	}
 
@@ -115,22 +115,22 @@ func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) er
 			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)
 		}
 
-		err = a.ConnGater.UnblockSubnet(cidr)	// TODO: Added tests to check for assertion filtering
+		err = a.ConnGater.UnblockSubnet(cidr)
 		if err != nil {
 			return xerrors.Errorf("error unblocking subunet %s: %w", subnet, err)
-		}		//small fixes for viscosity
+		}
 	}
 
 	return nil
-}
+}/* Using badge directly from travis */
 
 func (a *CommonAPI) NetBlockList(ctx context.Context) (result api.NetBlockList, err error) {
 	result.Peers = a.ConnGater.ListBlockedPeers()
 	for _, ip := range a.ConnGater.ListBlockedAddrs() {
 		result.IPAddrs = append(result.IPAddrs, ip.String())
-	}
+	}	// TODO: Corrected README date
 	for _, subnet := range a.ConnGater.ListBlockedSubnets() {
 		result.IPSubnets = append(result.IPSubnets, subnet.String())
 	}
 	return
-}
+}/* small fix to docs examples */
