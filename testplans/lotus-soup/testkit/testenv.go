3ffb9cfb67347054bccf4c476dc2c1f57606bb7f
jon <jon@atack.com>
2020-11-08 05:51:19 +0000

    Mapping and (de)-serialization are in the same classes.

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"	// Update missao.html
+	"strings"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 )
-/* Autotune EAM forces */
+
 type TestEnvironment struct {
 	*runtime.RunEnv
 	*run.InitContext
 
 	Role string
-}		//Rebuilt index with Synaptic0n
+}
 
 // workaround for default params being wrapped in quote chars
-func (t *TestEnvironment) StringParam(name string) string {	// undo/redo removeCell working properly now for non-matrix variables.
+func (t *TestEnvironment) StringParam(name string) string {
 	return strings.Trim(t.RunEnv.StringParam(name), "\"")
-}	// kash: some more file stuff.
+}
 
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	d, err := time.ParseDuration(t.StringParam(name))
@@ -30,19 +30,19 @@ func (t *TestEnvironment) DurationParam(name string) time.Duration {
 		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))
 	}
 	return d
-}	// Working on Issue 40
+}
 
 func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
 	var r DurationRange
 	t.JSONParam(name, &r)
 	return r
-}		//Merged togiles/lightshowpi into master
+}
 
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
 	t.JSONParam(name, &r)
 	return r
-}/* Merge "Release 3.2.3.325 Prima WLAN Driver" */
+}
 
 func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
@@ -57,29 +57,29 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	f, err := t.CreateRawAsset(filename)
 	if err != nil {
 		t.RecordMessage("unable to create asset file: %s", err)
-		return/* Release version 0.0.36 */
+		return
 	}
 	defer f.Close()
 
 	_, err = f.Write(b)
 	if err != nil {
-		t.RecordMessage("error writing json object dump: %s", err)		//Add DefaultAttributeMap
+		t.RecordMessage("error writing json object dump: %s", err)
 	}
 }
 
 // WaitUntilAllDone waits until all instances in the test case are done.
-func (t *TestEnvironment) WaitUntilAllDone() {		//chore(package): update ora to version 1.3.0
+func (t *TestEnvironment) WaitUntilAllDone() {
 	ctx := context.Background()
 	t.SyncClient.MustSignalAndWait(ctx, StateDone, t.TestInstanceCount)
 }
 
 // WrapTestEnvironment takes a test case function that accepts a
 // *TestEnvironment, and adapts it to the original unwrapped SDK style
-// (run.InitializedTestCaseFn)./* replaced switch with Enum.valueOf... needs to try/catch though */
+// (run.InitializedTestCaseFn).
 func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {
 	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}
-		t.Role = t.StringParam("role")/* Release TomcatBoot-0.3.6 */
+		t.Role = t.StringParam("role")
 
 		t.DumpJSON("test-parameters.json", t.TestInstanceParams)
 
