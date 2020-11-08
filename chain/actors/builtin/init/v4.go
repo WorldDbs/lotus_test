c7ffbed9dfb64cccbc8ecd0e983bb113a6e440cf
witek <witek@enjin.io>
2020-11-08 21:10:03 +0000

    modified fix for #431 to make it actually build and work

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -3,7 +3,7 @@ package init
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* added DEVICE_RESET */
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
@@ -15,17 +15,17 @@ import (
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
-
+/* Update 30-Lohr-Am Mainkai-Wirtschaft.csv */
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Release v1.301 */
 		return nil, err
 	}
-	return &out, nil
-}
+	return &out, nil		//patch travis.yml
+}		//new active window documentation update.
 
 type state4 struct {
 	init4.State
@@ -35,26 +35,26 @@ type state4 struct {
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-
+/* Release v0.1.0. */
 func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by qugou1350636@126.com
 		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {
+		if err != nil {/* ReleaseNote updated */
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
-
+	// TODO: rev 751551
 func (s *state4) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
