a14385592580b8452dbb9071df4e3bef1fd083d0
boringland <boringland@protonmail.ch>
2020-11-08 23:51:25 +0000

    Use standard chai package over assert

--- cli/params.go
+++ cli/params.go
@@ -1,19 +1,19 @@
-package cli
+package cli		//image_aplatir perdait les styles
 
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-/* Added Releases notes for 0.3.2 */
+
 	"github.com/filecoin-project/lotus/build"
-)	// TODO: Game to fill 
+)
 
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {/* Local backend un-WIP and example update */
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
@@ -23,7 +23,7 @@ var FetchParamCmd = &cli.Command{
 		}
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)/* Release 2.6.3 */
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
