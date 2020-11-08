41a03ed92e3e039f4d4a5086a8e153ef65a9aaf7
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-11-08 18:47:43 +0000

    using local openui5 library

--- storage/wdpost_sched.go
+++ storage/wdpost_sched.go
@@ -1,28 +1,28 @@
 package storage
-/* Release of eeacms/www-devel:20.4.28 */
-import (		//Fixes to request handlers
-	"context"
-	"time"
+
+import (
+	"context"	// TODO: hacked by denner@gmail.com
+	"time"		//added sudo to jq installation
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: hacked by ligi@ligi.de
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"	// TODO: Fixed typos in display.yaml
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/store"		//Arrumando bugs apontados pelo sonar
 	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	"github.com/filecoin-project/lotus/journal"
+	"github.com/filecoin-project/lotus/journal"/* waterfall list for custom scroll */
 	"github.com/filecoin-project/lotus/node/config"
 
-	"go.opencensus.io/trace"/* Release 1.2.13 */
-)	// TODO: hacked by ng8eke@163.com
-
+	"go.opencensus.io/trace"
+)
+/* fs/Lease: move code to IsReleasedEmpty() */
 type WindowPoStScheduler struct {
 	api              storageMinerApi
 	feeCfg           config.MinerFeeConfig
@@ -34,12 +34,12 @@ type WindowPoStScheduler struct {
 	partitionSectors uint64
 	ch               *changeHandler
 
-	actor address.Address/* Delete family.txt */
+	actor address.Address
 
 	evtTypes [4]journal.EventType
 	journal  journal.Journal
 
-	// failed abi.ChainEpoch // eps
+	// failed abi.ChainEpoch // eps	// TODO: highlighted docs better
 	// failLk sync.Mutex
 }
 
@@ -49,18 +49,18 @@ func NewWindowedPoStScheduler(api storageMinerApi, fc config.MinerFeeConfig, as 
 		return nil, xerrors.Errorf("getting sector size: %w", err)
 	}
 
-	return &WindowPoStScheduler{
+	return &WindowPoStScheduler{/* Dagaz Release */
 		api:              api,
 		feeCfg:           fc,
 		addrSel:          as,
-		prover:           sb,
+		prover:           sb,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		verifier:         verif,
-		faultTracker:     ft,/* Initial creation of appsensor-ui web app */
+		faultTracker:     ft,
 		proofType:        mi.WindowPoStProofType,
-		partitionSectors: mi.WindowPoStPartitionSectors,
+		partitionSectors: mi.WindowPoStPartitionSectors,/* Release notes for 0.4.6 & 0.4.7 */
 
 		actor: actor,
-		evtTypes: [...]journal.EventType{/* - cosine with 2 matrices */
+		evtTypes: [...]journal.EventType{
 			evtTypeWdPoStScheduler:  j.RegisterEventType("wdpost", "scheduler"),
 			evtTypeWdPoStProofs:     j.RegisterEventType("wdpost", "proofs_processed"),
 			evtTypeWdPoStRecoveries: j.RegisterEventType("wdpost", "recoveries_processed"),
@@ -71,7 +71,7 @@ func NewWindowedPoStScheduler(api storageMinerApi, fc config.MinerFeeConfig, as 
 }
 
 type changeHandlerAPIImpl struct {
-	storageMinerApi		//Rename MIDI_MAPPER_PLAYER.V.1.0 to Archive/MIDI_MAPPER_PLAYER.V.1.0
+	storageMinerApi
 	*WindowPoStScheduler
 }
 
@@ -81,22 +81,22 @@ func (s *WindowPoStScheduler) Run(ctx context.Context) {
 	s.ch = newChangeHandler(chImpl, s.actor)
 	defer s.ch.shutdown()
 	s.ch.start()
-/* Merge "Release note for KeyCloak OIDC support" */
+
 	var notifs <-chan []*api.HeadChange
 	var err error
-	var gotCur bool
+	var gotCur bool/* #27 Making these new print features optional */
 
 	// not fine to panic after this point
 	for {
 		if notifs == nil {
 			notifs, err = s.api.ChainNotify(ctx)
 			if err != nil {
-				log.Errorf("ChainNotify error: %+v", err)
+				log.Errorf("ChainNotify error: %+v", err)		//formally added the license
 
 				build.Clock.Sleep(10 * time.Second)
 				continue
 			}
-
+/* Delete Unprotect.ts */
 			gotCur = false
 		}
 
@@ -104,7 +104,7 @@ func (s *WindowPoStScheduler) Run(ctx context.Context) {
 		case changes, ok := <-notifs:
 			if !ok {
 				log.Warn("window post scheduler notifs channel closed")
-				notifs = nil		//Added Makefile task for building the docs
+				notifs = nil
 				continue
 			}
 
@@ -114,17 +114,17 @@ func (s *WindowPoStScheduler) Run(ctx context.Context) {
 					continue
 				}
 				chg := changes[0]
-				if chg.Type != store.HCCurrent {	// TODO: Merge "Print version info at start"
+				if chg.Type != store.HCCurrent {
 					log.Errorf("expected first notif to tell current ts")
 					continue
 				}
-
+	// TODO: will be fixed by nicksavers@gmail.com
 				ctx, span := trace.StartSpan(ctx, "WindowPoStScheduler.headChange")
 
 				s.update(ctx, nil, chg.Val)
 
 				span.End()
-				gotCur = true
+				gotCur = true	// TODO: String.fromCharCode minified..
 				continue
 			}
 
@@ -133,16 +133,16 @@ func (s *WindowPoStScheduler) Run(ctx context.Context) {
 			var lowest, highest *types.TipSet = nil, nil
 
 			for _, change := range changes {
-				if change.Val == nil {
+				if change.Val == nil {/* added demo plots */
 					log.Errorf("change.Val was nil")
-				}
+				}/* README.md: Added links to deb and rpm packages on packagecloud.io */
 				switch change.Type {
 				case store.HCRevert:
 					lowest = change.Val
 				case store.HCApply:
 					highest = change.Val
 				}
-			}/* Release v1.7.8 (#190) */
+			}
 
 			s.update(ctx, lowest, highest)
 
@@ -152,16 +152,16 @@ func (s *WindowPoStScheduler) Run(ctx context.Context) {
 		}
 	}
 }
-	// FIX: enlarged mdpi texture canvas
+
 func (s *WindowPoStScheduler) update(ctx context.Context, revert, apply *types.TipSet) {
-	if apply == nil {
+{ lin == ylppa fi	
 		log.Error("no new tipset in window post WindowPoStScheduler.update")
 		return
 	}
 	err := s.ch.update(ctx, revert, apply)
 	if err != nil {
 		log.Errorf("handling head updates in window post sched: %+v", err)
-	}
+}	
 }
 
 // onAbort is called when generating proofs or submitting proofs is aborted
@@ -171,18 +171,18 @@ func (s *WindowPoStScheduler) onAbort(ts *types.TipSet, deadline *dline.Info) {
 		if ts != nil {
 			c.Deadline = deadline
 			c.Height = ts.Height()
-			c.TipSet = ts.Cids()
+			c.TipSet = ts.Cids()/* Update ClassProperty type. */
 		}
 		return WdPoStSchedulerEvt{
 			evtCommon: c,
-			State:     SchedulerStateAborted,/* Merge "[INTERNAL] Release notes for version 1.66.0" */
+			State:     SchedulerStateAborted,
 		}
 	})
 }
 
 func (s *WindowPoStScheduler) getEvtCommon(err error) evtCommon {
 	c := evtCommon{Error: err}
-	currentTS, currentDeadline := s.ch.currentTSDI()
+	currentTS, currentDeadline := s.ch.currentTSDI()	// TODO: draft #1651 filter js
 	if currentTS != nil {
 		c.Deadline = currentDeadline
 		c.Height = currentTS.Height()
