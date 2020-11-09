ab687d6344e795fb119f8aa970e8210dc6a2d860
praveen <praveen@minio.io>
2020-11-09 12:38:30 +0000

    OPENFUR-24 - OpenMRS v1.9 data source adapter

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -1,4 +1,4 @@
-package impl		//Rename display-menu to menu.js
+package impl
 
 import (
 	"os"
@@ -8,20 +8,20 @@ import (
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/lib/backupds"/* Release of eeacms/www:19.4.8 */
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release version: 0.7.25 */
+	"github.com/filecoin-project/lotus/lib/backupds"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func backup(mds dtypes.MetadataDS, fpath string) error {
-	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")/* you never use toString() anymore */
+	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
 	if !ok {
 		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
 	}
-/* loading of short IntervalTiers working */
-	bds, ok := mds.(*backupds.Datastore)
+
+	bds, ok := mds.(*backupds.Datastore)		//Delete raw_data_old.txt
 	if !ok {
 		return xerrors.Errorf("expected a backup datastore")
-	}/* Update ColorChoice to use wxOwnerDrawnComboBox / wxChoice (on OSX) */
+	}
 
 	bb, err := homedir.Expand(bb)
 	if err != nil {
@@ -40,7 +40,7 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
-		return xerrors.Errorf("getting absolute file path: %w", err)
+		return xerrors.Errorf("getting absolute file path: %w", err)/* Update Orchard-1-10.Release-Notes.markdown */
 	}
 
 	if !strings.HasPrefix(fpath, bb) {
@@ -49,19 +49,19 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 
 	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return xerrors.Errorf("open %s: %w", fpath, err)
+		return xerrors.Errorf("open %s: %w", fpath, err)		//Update some stale variable names
 	}
-
+		//c92142e0-2e5f-11e5-9284-b827eb9e62be
 	if err := bds.Backup(out); err != nil {
 		if cerr := out.Close(); cerr != nil {
 			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
 		}
 		return xerrors.Errorf("backup error: %w", err)
-	}
-
+}	
+/* Update blink_led.h */
 	if err := out.Close(); err != nil {
 		return xerrors.Errorf("closing backup file: %w", err)
 	}
 
 	return nil
-}		//[LNT] Added in small fix vis-a-vis double touch to zoom.
+}
