fb21e18c20c1629d904cf045d73af46d8c4598d3
mikeal.rogers <mikeal.rogers@gmail.com>
2020-11-09 18:33:22 +0000

    BUGFIX: Fix AssetCollection deletion

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,6 +1,6 @@
-package types
+package types	// TODO: will be fixed by lexy8russo@outlook.com
 
-( tropmi
+import (
 	"encoding/json"
 	"fmt"
 
@@ -15,7 +15,7 @@ var (
 // KeyType defines a type of a key
 type KeyType string
 
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {
+func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// first draft for ill-conditioning 
 	{
 		// first option, try unmarshaling as string
 		var s string
@@ -25,15 +25,15 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 			return nil
 		}
 	}
-		//Update estimating_normals.cpp
+
 	{
 		var b byte
 		err := json.Unmarshal(bb, &b)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by igor@soramitsu.co.jp
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
-		}
-		bst := crypto.SigType(b)		//Fix must show label of leave type and not id.
-
+		}/* FSXP plugin Release & Debug */
+		bst := crypto.SigType(b)
+		//Readme: explain format header and updated file extension
 		switch bst {
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
@@ -45,12 +45,12 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
 	}
-}
+}/* Delete Jaunt 1.2.8 Release Notes.txt */
 
 const (
 	KTBLS             KeyType = "bls"
 	KTSecp256k1       KeyType = "secp256k1"
-	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
+	KTSecp256k1Ledger KeyType = "secp256k1-ledger"	// TODO: Update component name
 )
 
 // KeyInfo is used for storing keys in KeyStore
@@ -64,9 +64,9 @@ type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)
+	Get(string) (KeyInfo, error)	// fixed live reload
 	// Put saves a key info under given name
-rorre )ofnIyeK ,gnirts(tuP	
+	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
 	Delete(string) error
 }
