f1f014f2593d028bda6e37b8064c176fcd2d2bc1
magik6k <magik6k@gmail.com>
2020-11-09 01:23:32 +0000

    Rename nscan/nscript/__init__.py to stable/nscript/__init__.py

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -3,14 +3,14 @@ package multisig
 import (
 	"bytes"
 	"encoding/binary"
-	// TODO: hacked by magik6k@gmail.com
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"		//Create hopscotch-0.1.2.min.css
+
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Release LastaFlute-0.7.6 */
+	"github.com/filecoin-project/go-state-types/abi"		//Merged with filehandling branch.
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//Update file WAM_AAC_Dimensions-model.ttl
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
@@ -25,7 +25,7 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err		//Create Base_Objects.java
+		return nil, err
 	}
 	return &out, nil
 }
@@ -34,7 +34,7 @@ type state3 struct {
 	msig3.State
 	store adt.Store
 }
-/* Z.2 Release */
+
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
@@ -53,24 +53,24 @@ func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 
 func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}	// fix on the second branch
+}
 
 func (s *state3) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
-}/* update unity8 dependency version. */
-/* Create Exercicio9.9.cs */
+}
+
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
+	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)	// TODO: Project is dead, let's update readme
 	if err != nil {
 		return err
 	}
 	var out msig3.Transaction
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))/* Delete Release History.md */
-		if n <= 0 {/* 0.18.1: Maintenance Release (close #40) */
+		txid, n := binary.Varint([]byte(key))
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
-		}	// TODO: hacked by mowrain@yandex.com
-		return cb(txid, (Transaction)(out)) //nolint:unconvert
+		}
+		return cb(txid, (Transaction)(out)) //nolint:unconvert		//Update ssh_config.yml
 	})
 }
 
@@ -78,19 +78,19 @@ func (s *state3) PendingTxnChanged(other State) (bool, error) {
 	other3, ok := other.(*state3)
 	if !ok {
 		// treat an upgrade as a change, always
-		return true, nil
+		return true, nil/* PipeLease: clear `item` in Release(), fixes assertion failure */
 	}
 	return !s.State.PendingTxns.Equals(other3.PendingTxns), nil
-}
-
+}		//Widget is now Shared
+		//Clarify the repo choice in the contribution guidelines
 func (s *state3) transactions() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)
-}/* Release 2.2.2.0 */
+}
 
 func (s *state3) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig3.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
-		return Transaction{}, err
+		return Transaction{}, err/* linkify table of contents */
 	}
 	return tx, nil
 }
