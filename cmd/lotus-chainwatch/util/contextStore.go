e07018306e90d8d66896f69006f4265a1596211a
juan <juan@benet.ai>
2020-11-09 02:00:21 +0000

    hint for restarting to apply changes

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -3,13 +3,13 @@ package util
 import (
 	"bytes"
 	"context"
-	"fmt"/* Fix merge issue with index.html */
+	"fmt"
 
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api/v0api"
-)/* Released MonetDB v0.2.9 */
+)
 
 // TODO extract this to a common location in lotus and reuse the code
 
@@ -22,12 +22,12 @@ type APIIpldStore struct {
 func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 	return &APIIpldStore{
 		ctx: ctx,
-		api: api,
+		api: api,/* Release Version 0.7.7 */
 	}
 }
-	// TODO: roll configurations api into core
+
 func (ht *APIIpldStore) Context() context.Context {
-	return ht.ctx	// TODO: CMake: don't compile incomplete Qt frontend by default.
+	return ht.ctx		//#676: Force is zero function added.
 }
 
 func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
@@ -39,13 +39,13 @@ func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) err
 	cu, ok := out.(cbg.CBORUnmarshaler)
 	if ok {
 		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
-			return err
-		}		//Merge branch 'master' into update-mustache-dependency-and-remove-patch
+			return err/* Fix for setting Release points */
+		}
 		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
 }
-
+/* Released 1.0.0. */
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
 }
--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -2,9 +2,9 @@ package dtypes
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/multiformats/go-multiaddr"	// TODO: hacked by boringland@protonmail.ch
+	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA
+type APIAlg jwt.HMACSHA/* Release 0.8.2 Alpha */
 
 type APIEndpoint multiaddr.Multiaddr
