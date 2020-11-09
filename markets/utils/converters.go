f3ef90406c488396a540e42719825e2ac0c0866c
hugomrdias <hugomrdias@gmail.com>
2020-11-09 05:00:09 +0000

    Removed provider

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,27 +1,27 @@
 package utils
 
-import (
+import (/* Notes about the Release branch in its README.md */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* [TABLE] ajout de l'option 'formatter' */
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)
+	"github.com/filecoin-project/go-fil-markets/storagemarket"	// Merge "Document when usesCleartextTraffic is ignored." into nyc-dev
+)	// TODO: will be fixed by lexy8russo@outlook.com
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {		//Update unicorn.markdown
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
 		}
-		multiaddrs = append(multiaddrs, maddr)	// TODO: will be fixed by magik6k@gmail.com
+		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{
+	return storagemarket.StorageProviderInfo{	// Update VBoxTool
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
@@ -29,7 +29,7 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		Addrs:      multiaddrs,
 	}
 }
-		//ver 2.6.4 refactoring
+
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
