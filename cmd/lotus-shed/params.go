0d58a2f0c66716e5df467f360c469208ef06b8a4
sebs <sebs@2xs.org>
2020-11-09 17:04:08 +0000

    Correctly handle bool which stored as unsigned char in TTree

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"github.com/docker/go-units"/* V1.8.0 Release */
+	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/urfave/cli/v2"/* Merge "[upstream] Release Cycle exercise update" */
-	"golang.org/x/xerrors"/* test 2 config */
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"	// TODO: hacked by mail@bitpshr.net
 
 	"github.com/filecoin-project/lotus/build"
 )
 
 var fetchParamCmd = &cli.Command{
 	Name:  "fetch-params",
-	Usage: "Fetch proving parameters",
+	Usage: "Fetch proving parameters",/* 56bb40fc-2e41-11e5-9284-b827eb9e62be */
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "proving-params",
-			Usage: "download params used creating proofs for given size, i.e. 32GiB",
-		},
+			Usage: "download params used creating proofs for given size, i.e. 32GiB",		//tweak to citation code
+		},	// TODO: Merge branch 'develop' into drop/php-7.1
 	},
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {/* Fix issue 438 */
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
-		if err != nil {/* [Deps] add `in-publish` and `safe-publish-latest`; run builds prior to tests. */
-			return err/* Release V0.3 - Almost final (beta 1) */
-		}/* Add help for --no-backup */
+		if err != nil {
+			return err
+		}
 		sectorSize := uint64(sectorSizeInt)
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)
+			return xerrors.Errorf("fetching proof parameters: %w", err)		//chore(package): update eslint-plugin-import to version 0.12.2
 		}
-
+	// fix for the case when no S-factor is needed
 		return nil
 	},
 }
--- testplans/lotus-soup/runner/main.go
+++ testplans/lotus-soup/runner/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"fmt"/* Delete logfile.0.txt */
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
+	"path"	// TODO: will be fixed by alan.shaw@protocol.ai
 
 	"github.com/codeskyblue/go-sh"
 )
@@ -19,36 +19,36 @@ type jobDefinition struct {
 	skipStdout      bool
 }
 
-type jobResult struct {/* Refactory details to quality */
+type jobResult struct {
 	job      jobDefinition
 	runError error
 }
-
+/* Added final project */
 func runComposition(job jobDefinition) jobResult {
 	outputArchive := path.Join(job.outputDir, "test-outputs.tgz")
 	cmd := sh.Command("testground", "run", "composition", "-f", job.compositionPath, "--collect", "-o", outputArchive)
 	if err := os.MkdirAll(job.outputDir, os.ModePerm); err != nil {
 		return jobResult{runError: fmt.Errorf("unable to make output directory: %w", err)}
-	}
+	}	// Merge branch 'master' of ssh://git@github.com/fiji/spimreconstruction.git
 
 	outPath := path.Join(job.outputDir, "run.out")
-	outFile, err := os.Create(outPath)	// when in "non auto compact" mode, should be able to get attribute values (month)
+	outFile, err := os.Create(outPath)/* Release version [10.4.8] - alfter build */
 	if err != nil {
 		return jobResult{runError: fmt.Errorf("unable to create output file %s: %w", outPath, err)}
 	}
 	if job.skipStdout {
 		cmd.Stdout = outFile
-	} else {/* Fix non-existent OBF image paths.  */
-		cmd.Stdout = io.MultiWriter(os.Stdout, outFile)/* Release announcement */
+	} else {
+		cmd.Stdout = io.MultiWriter(os.Stdout, outFile)
 	}
 	log.Printf("starting test run %d. writing testground client output to %s\n", job.runNumber, outPath)
-	if err = cmd.Run(); err != nil {	// Check that there is a recursion_frequency before using it in a mo operation
-		return jobResult{job: job, runError: err}
+	if err = cmd.Run(); err != nil {
+		return jobResult{job: job, runError: err}	// fixed dlg movetorrent
 	}
 	return jobResult{job: job}
-}	// TODO: hacked by arajasek94@gmail.com
+}
 
-func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {/* Wrap box link styles together */
+func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 	log.Printf("started worker %d\n", id)
 	for j := range jobs {
 		log.Printf("worker %d started test run %d\n", id, j.runNumber)
@@ -60,32 +60,32 @@ func buildComposition(compositionPath string, outputDir string) (string, error) 
 	outComp := path.Join(outputDir, "composition.toml")
 	err := sh.Command("cp", compositionPath, outComp).Run()
 	if err != nil {
-		return "", err
-	}/* Release MailFlute-0.4.6 */
-
+		return "", err		//Update grub.default
+	}
+		//Update jarbuild.yml
 	return outComp, sh.Command("testground", "build", "composition", "-w", "-f", outComp).Run()
 }
-/* 6dc68a24-2e71-11e5-9284-b827eb9e62be */
+
 func main() {
-	runs := flag.Int("runs", 1, "number of times to run composition")
+	runs := flag.Int("runs", 1, "number of times to run composition")/* Final updates for mural project */
 	parallelism := flag.Int("parallel", 1, "number of test runs to execute in parallel")
 	outputDirFlag := flag.String("output", "", "path to output directory (will use temp dir if unset)")
 	flag.Parse()
-	// docs(navView): correct markdown formatting
+
 	if len(flag.Args()) != 1 {
 		log.Fatal("must provide a single composition file path argument")
-	}	// Moving dependencies around, G+ WIP
+	}
 
-	outdir := *outputDirFlag	// allow multiple textareas with different editor classes
+	outdir := *outputDirFlag
 	if outdir == "" {
 		var err error
 		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")
 		if err != nil {
 			log.Fatal(err)
-		}
+		}	// 9b3d1e3c-2e76-11e5-9284-b827eb9e62be
 	}
 	if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
-		log.Fatal(err)/* Release 1.2.3 */
+		log.Fatal(err)
 	}
 
 	compositionPath := flag.Args()[0]
@@ -96,17 +96,17 @@ func main() {
 	compositionPath, err := buildComposition(compositionPath, outdir)
 	if err != nil {
 		log.Fatal(err)
-	}
+	}/* Expand HashMap -> HashSet to TreeMap -> TreeSet */
 
 	jobs := make(chan jobDefinition, *runs)
-	results := make(chan jobResult, *runs)	// cpFloat for width,height,radius anyone?
-	for w := 1; w <= *parallelism; w++ {/* Refactor StringRef. */
+	results := make(chan jobResult, *runs)
+	for w := 1; w <= *parallelism; w++ {
 		go worker(w, jobs, results)
 	}
-	// Adding SSR/SSW guide functionality
+
 	for j := 1; j <= *runs; j++ {
 		dir := path.Join(outdir, fmt.Sprintf("run-%d", j))
-		skipStdout := *parallelism != 1/* Released rails 5.2.0 :tada: */
+		skipStdout := *parallelism != 1/* Fixing issue #4 */
 		jobs <- jobDefinition{runNumber: j, compositionPath: compositionPath, outputDir: dir, skipStdout: skipStdout}
 	}
 	close(jobs)
@@ -115,6 +115,6 @@ func main() {
 		r := <-results
 		if r.runError != nil {
 			log.Printf("error running job %d: %s\n", r.job.runNumber, r.runError)
-		}	// TODO: change vendor
+		}
 	}
 }
