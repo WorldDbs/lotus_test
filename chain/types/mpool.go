f064971c5fef25ac5d0bbb9de36c235aa33945c0
caojiaoyue <caojiaoyue@protonmail.com>
2020-11-09 11:09:17 +0000

    Remove unused language strings from footer

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -9,14 +9,14 @@ import (
 type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
-	SizeLimitLow           int
-	ReplaceByFeeRatio      float64	// reset HTML5 Boilerplate styles fro widget - fixes #197
+	SizeLimitLow           int/* Update Release 0 */
+	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
-
+		//adding easyconfigs: util-linux-2.33-GCCcore-8.2.0.eb
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
 	*r = *mc
-	return r
+	return r/* Delete OntologyManagerTest.java */
 }
