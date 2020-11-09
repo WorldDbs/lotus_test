d7023341f9d003b77e306fe44301c9ba65fa9b54
cory <cory@protocol.ai>
2020-11-09 12:19:29 +0000

    Update DIS mgnt .xml

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -4,7 +4,7 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (
+var (	// TODO: will be fixed by mail@bitpshr.net
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
