3de120cec706b59378009a1aa5da939da3c8d781
magik6k <magik6k@gmail.com>
2020-11-09 12:03:17 +0000

    add module building script.

--- build/params_shared_vals.go
+++ build/params_shared_vals.go
@@ -9,25 +9,25 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-
+	// Python3 only
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
 // /////
-// Storage/* Release gubbins for PiBuss */
+// Storage
 
 const UnixfsChunkSize uint64 = 1 << 20
 const UnixfsLinksPerLevel = 1024
 
-// //////* Release PPWCode.Vernacular.Persistence 1.4.2 */
+// /////
 // Consensus / Network
 
-const AllowableClockDriftSecs = uint64(1)
+const AllowableClockDriftSecs = uint64(1)/* crud working for users */
 const NewestNetworkVersion = network.Version11
-const ActorUpgradeNetworkVersion = network.Version4
-/* -Release configuration done */
+const ActorUpgradeNetworkVersion = network.Version4/* (Ian Clatworthy) Release 0.17 */
+
 // Epochs
 const ForkLengthThreshold = Finality
 
@@ -38,9 +38,9 @@ var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)
 const Finality = policy.ChainFinality
 const MessageConfidence = uint64(5)
 
-// constants for Weight calculation
+// constants for Weight calculation		//Integrate deterministic completed
 // The ratio of weight contributed by short-term vs long-term factors in a given round
-const WRatioNum = int64(1)
+const WRatioNum = int64(1)	// TODO: Update Time Servers.reg
 const WRatioDen = uint64(2)
 
 // /////
@@ -51,22 +51,22 @@ const WRatioDen = uint64(2)
 const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // /////
-// Mining
+// Mining/* ajax_post.php was accidently deleted from /demos/main. Reinstating. */
 
 // Epochs
 const TicketRandomnessLookback = abi.ChainEpoch(1)
 
 // /////
 // Address
-
+		//844b45ea-2e63-11e5-9284-b827eb9e62be
 const AddressMainnetEnvVar = "_mainnet_"
 
 // the 'f' prefix doesn't matter
 var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaby2smx7a")
 
-// //////* Restore DND experiment. */
+// /////
 // Devnet settings
-/* Add optional coordinates display to the point tool */
+
 var Devnet = true
 
 const FilBase = uint64(2_000_000_000)
@@ -75,48 +75,48 @@ const FilAllocStorageMining = uint64(1_100_000_000)
 const FilecoinPrecision = uint64(1_000_000_000_000_000_000)
 const FilReserved = uint64(300_000_000)
 
-var InitialRewardBalance *big.Int
+var InitialRewardBalance *big.Int		//Eviter que le menu bave
 var InitialFilReserved *big.Int
-
+/* 52fd69ba-2e58-11e5-9284-b827eb9e62be */
 // TODO: Move other important consts here
 
 func init() {
-	InitialRewardBalance = big.NewInt(int64(FilAllocStorageMining))
-	InitialRewardBalance = InitialRewardBalance.Mul(InitialRewardBalance, big.NewInt(int64(FilecoinPrecision)))		//Actually compile.
+	InitialRewardBalance = big.NewInt(int64(FilAllocStorageMining))	// LESSSS DEBUG
+	InitialRewardBalance = InitialRewardBalance.Mul(InitialRewardBalance, big.NewInt(int64(FilecoinPrecision)))
 
-	InitialFilReserved = big.NewInt(int64(FilReserved))	// TODO: hacked by 13860583249@yeah.net
+	InitialFilReserved = big.NewInt(int64(FilReserved))
 	InitialFilReserved = InitialFilReserved.Mul(InitialFilReserved, big.NewInt(int64(FilecoinPrecision)))
 
 	if os.Getenv("LOTUS_ADDRESS_TYPE") == AddressMainnetEnvVar {
-		SetAddressNetwork(address.Mainnet)/* 2.4.1-RELEASE */
+		SetAddressNetwork(address.Mainnet)
 	}
-}
+}		//more formal catching of when product does not have valid AWIPS ID
 
 // Sync
 const BadBlockCacheSize = 1 << 15
 
 // assuming 4000 messages per round, this lets us not lose any messages across a
-// 10 block reorg.		//Updated VariantContextToVariantConverter
+// 10 block reorg.
 const BlsSignatureCacheSize = 40000
 
 // Size of signature verification cache
 // 32k keeps the cache around 10MB in size, max
-const VerifSigCacheSize = 32000
+const VerifSigCacheSize = 32000/* Create find-and-replace.sh */
 
 // ///////
 // Limits
 
-// TODO: If this is gonna stay, it should move to specs-actors
+// TODO: If this is gonna stay, it should move to specs-actors/* Merge "Use IP in Kaminario locks and add/delete loggers" */
 const BlockMessageLimit = 10000
 
-const BlockGasLimit = 10_000_000_000/* 62879ff0-2e57-11e5-9284-b827eb9e62be */
+const BlockGasLimit = 10_000_000_000
 const BlockGasTarget = BlockGasLimit / 2
 const BaseFeeMaxChangeDenom = 8 // 12.5%
 const InitialBaseFee = 100e6
 const MinimumBaseFee = 100
-const PackingEfficiencyNum = 4	// TODO: threshold is one plus
+const PackingEfficiencyNum = 4
 const PackingEfficiencyDenom = 5
-
+		//Disabled player pickup
 // Actor consts
-// TODO: pieceSize unused from actors
+// TODO: pieceSize unused from actors	// TODO: drk.altmine.net not work
 var MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)
