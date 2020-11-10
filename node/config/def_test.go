594824c4f23356e8162b887b5e86ca7b140067e6
steven <steven@stebalien.com>
2020-11-10 02:26:05 +0000

    [list] retrieve selection from the list

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -1,12 +1,12 @@
 package config
 
-import (
+import (	// TODO: will be fixed by magik6k@gmail.com
 	"bytes"
 	"fmt"
 	"reflect"
 	"strings"
 	"testing"
-	// TODO: now including all possible ranks from NCBI taxonomy
+
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
 )
@@ -15,8 +15,8 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {
 	c := DefaultFullNode()
 
 	var s string
-	{/* Adding parallel-testing.png to docs */
-		buf := new(bytes.Buffer)
+	{
+		buf := new(bytes.Buffer)/* Release 0.10 */
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
@@ -28,7 +28,7 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println(s)
-
+		//Updating worker dequeuing to send callbacks
 	require.True(t, reflect.DeepEqual(c, c2))
 }
 
@@ -39,7 +39,7 @@ func TestDefaultMinerRoundtrip(t *testing.T) {
 	{
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
-		e := toml.NewEncoder(buf)	// TODO: [Fix] hr_expense : fixed error in report yml of expense report
+		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
 
 		s = buf.String()
