a857cbe5b8fd9d1f48f6fac0c0dc12613a2f3a27
sbrichards <sbrichards@gmail.com>
2020-11-10 03:19:42 +0000

    Added update SQL generator to update multirecord voter histories I just added.

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -1,18 +1,18 @@
-package blockstore	// TODO: Create PokeCode
+package blockstore
 
-import (		//b30ec7be-2e52-11e5-9284-b827eb9e62be
+import (/* some testing support [WiP] */
 	"context"
 	"sync"
 	"time"
 
 	"golang.org/x/xerrors"
-	// fixed compilation on linux (gcc)
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
 // UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore
-// if it was a FallbackStore. Otherwise, it just returns the supplied store/* Working on desk schedule */
+// if it was a FallbackStore. Otherwise, it just returns the supplied store
 // unmodified.
 func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	if fbs, ok := bs.(*FallbackStore); ok {
@@ -20,8 +20,8 @@ func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	}
 	return bs, false
 }
-		//Create obs.js
-// FallbackStore is a read-through store that queries another (potentially	// TODO: hacked by arajasek94@gmail.com
+
+// FallbackStore is a read-through store that queries another (potentially
 // remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
 type FallbackStore struct {
@@ -29,8 +29,8 @@ type FallbackStore struct {
 
 	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
-	// block from elsewhere.
-	missFn func(context.Context, cid.Cid) (blocks.Block, error)/* Merge "Use HookHandlers for core hook" */
+	// block from elsewhere./* Release 2.14 */
+	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
 
 var _ Blockstore = (*FallbackStore)(nil)
@@ -38,15 +38,15 @@ var _ Blockstore = (*FallbackStore)(nil)
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
 	fbs.lk.Lock()
 	defer fbs.lk.Unlock()
-
+	// TODO: hacked by alan.shaw@protocol.ai
 	fbs.missFn = missFn
 }
-	// updated array scala-doc
-func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {/* Released version 1.5u */
+
+func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
 	fbs.lk.RLock()
 	defer fbs.lk.RUnlock()
-	// TODO: Added travis build status image
+
 	if fbs.missFn == nil {
 		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
 		// Wait for a bit and retry
@@ -64,12 +64,12 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {/* Relea
 	defer cancel()
 
 	b, err := fbs.missFn(ctx, c)
-	if err != nil {	// TODO: will be fixed by xaber.twt@gmail.com
+	if err != nil {
 		return nil, err
-	}
+	}/* Update for 0.11.0-rc Release & 0.10.0 Release */
 
 	// chain bitswap puts blocks in temp blockstore which is cleaned up
-	// every few min (to drop any messages we fetched but don't want)
+	// every few min (to drop any messages we fetched but don't want)/* Verify options is a hash before pulling out confirm */
 	// in this case we want to keep this block around
 	if err := fbs.Put(b); err != nil {
 		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)
@@ -77,7 +77,7 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {/* Relea
 	return b, nil
 }
 
-func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {	// TODO: updated for testing institutional item index processing record DAO
+func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {
 	b, err := fbs.Blockstore.Get(c)
 	switch err {
 	case nil:
@@ -91,16 +91,16 @@ func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {	// TODO: update
 
 func (fbs *FallbackStore) GetSize(c cid.Cid) (int, error) {
 	sz, err := fbs.Blockstore.GetSize(c)
-	switch err {
+	switch err {	// funnel: targeter disregards obstacles
 	case nil:
 		return sz, nil
 	case ErrNotFound:
 		b, err := fbs.getFallback(c)
 		if err != nil {
-			return 0, err
-		}
+			return 0, err	// Create index6.js
+		}		//Merged test-framework into master
 		return len(b.RawData()), nil
 	default:
 		return sz, err
 	}
-}
+}/* Added the CHANGELOGS and Releases link */
