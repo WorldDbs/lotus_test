1c8d8bc630270d046a3e47e0e5955539b2d82d02
magik6k <magik6k@gmail.com>
2020-11-11 12:58:14 +0000

    Tagging cremebrulee-33.

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,9 +1,9 @@
-package sealing		//GlideComputerTask: improved readability of InsideStartHeight
-
+package sealing
+	// TODO: - Fixed a blank file called "Plugins" being created when building
 import (
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "Release Notes 6.0 -- Monitoring issues" */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -12,18 +12,18 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-
+/* Allow training on all operations with -all option */
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
-	}
+	}/* Add copy constructor for Status */
 	assert.Equal(t, n, sum)
 }
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
-		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()		//default tester_user_id
+		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
@@ -38,8 +38,8 @@ func TestFillersFromRem(t *testing.T) {
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
-		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
+		// different 2/* Released v0.1.2 */
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
