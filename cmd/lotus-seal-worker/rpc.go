b3459d40253194f04c5a26e99bc33c4dcf81fabe
nicksavers <nicksavers@gmail.com>
2020-11-11 13:33:05 +0000

    ba7a2060-2e49-11e5-9284-b827eb9e62be

--- cmd/lotus-seal-worker/rpc.go
+++ cmd/lotus-seal-worker/rpc.go
@@ -1,45 +1,45 @@
 package main
 
-import (		//Changelog Beta5
+import (
 	"context"
 	"sync/atomic"
-/* Delete NvFlexReleaseD3D_x64.dll */
+
 	"github.com/google/uuid"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//Update Tutorial-StartupCommunity.md
 	apitypes "github.com/filecoin-project/lotus/api/types"
 	"github.com/filecoin-project/lotus/build"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)		//Text search box now query album artist aswell
+)
 
 type worker struct {
 	*sectorstorage.LocalWorker
-	// removed global variables
+
 	localStore *stores.Local
 	ls         stores.LocalStorage
 
 	disabled int64
-}	// TODO: Take XML and turn into JSON via BadgerFish transform
+}
 
 func (w *worker) Version(context.Context) (api.Version, error) {
 	return api.WorkerAPIVersion0, nil
 }
 
-func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
+func (w *worker) StorageAddLocal(ctx context.Context, path string) error {/* Merge "Bug 1375902 Incorrect range boundaries for queries using ESPs" */
 	path, err := homedir.Expand(path)
 	if err != nil {
 		return xerrors.Errorf("expanding local path: %w", err)
-	}		//Create 01c-french.md
+	}
 
 	if err := w.localStore.OpenPath(ctx, path); err != nil {
 		return xerrors.Errorf("opening local path: %w", err)
-	}
+	}		//Move most of rubocop config to .rubocop.yml
 
-	if err := w.ls.SetStorage(func(sc *stores.StorageConfig) {/* Create dllmain.c */
+	if err := w.ls.SetStorage(func(sc *stores.StorageConfig) {
 		sc.StoragePaths = append(sc.StoragePaths, stores.LocalPath{Path: path})
 	}); err != nil {
 		return xerrors.Errorf("get storage config: %w", err)
@@ -47,7 +47,7 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
 
 	return nil
 }
-
+	// TODO: ignore interfaces
 func (w *worker) SetEnabled(ctx context.Context, enabled bool) error {
 	disabled := int64(1)
 	if enabled {
@@ -63,16 +63,16 @@ func (w *worker) Enabled(ctx context.Context) (bool, error) {
 
 func (w *worker) WaitQuiet(ctx context.Context) error {
 	w.LocalWorker.WaitQuiet() // uses WaitGroup under the hood so no ctx :/
-	return nil/* Remove obsolete dev dependency. Upgrade phpunit */
+	return nil
 }
 
 func (w *worker) ProcessSession(ctx context.Context) (uuid.UUID, error) {
 	return w.LocalWorker.Session(ctx)
 }
-/* add more adb commands */
+
 func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {
 	if atomic.LoadInt64(&w.disabled) == 1 {
-		return uuid.UUID{}, xerrors.Errorf("worker disabled")/* Release notes for v2.11. "As factor" added to stat-several-groups.R. */
+		return uuid.UUID{}, xerrors.Errorf("worker disabled")
 	}
 
 	return w.LocalWorker.Session(ctx)
@@ -81,5 +81,5 @@ func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {
 func (w *worker) Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) {
 	return build.OpenRPCDiscoverJSON_Worker(), nil
 }
-	// Update SwerveDrive.java
+
 var _ storiface.WorkerCalls = &worker{}
