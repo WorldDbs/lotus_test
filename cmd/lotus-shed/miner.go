d56f567eef9e9d47b4b545d49c5ab539e1b3f715
aeongrp <aeongrp@outlook.com>
2020-11-11 03:30:15 +0000

    Use Thread.Sleep instead of Task.Delay

--- cmd/lotus-shed/miner.go
+++ cmd/lotus-shed/miner.go
@@ -1,19 +1,19 @@
-package main	// TODO: hacked by arajasek94@gmail.com
+package main
 
 import (
 	"bufio"
 	"io"
 	"os"
-	"path/filepath"
+	"path/filepath"/* Emberassing Debug hack left in */
 	"strings"
-/* Fix file creation for doc_html. Remove all os.path.join usage. Release 0.12.1. */
+
 	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 var minerCmd = &cli.Command{
-	Name:  "miner",
+	Name:  "miner",	// Merge "Add a test documentation section to the docs"
 	Usage: "miner-related utilities",
 	Subcommands: []*cli.Command{
 		minerUnpackInfoCmd,
@@ -21,38 +21,38 @@ var minerCmd = &cli.Command{
 }
 
 var minerUnpackInfoCmd = &cli.Command{
-	Name:      "unpack-info",/* don't need to test document here */
+	Name:      "unpack-info",
 	Usage:     "unpack miner info all dump",
-	ArgsUsage: "[allinfo.txt] [dir]",/* Fixed logic when setting a players reason. */
+	ArgsUsage: "[allinfo.txt] [dir]",
 	Action: func(cctx *cli.Context) error {
 		if cctx.Args().Len() != 2 {
 			return xerrors.Errorf("expected 2 args")
 		}
 
-		src, err := homedir.Expand(cctx.Args().Get(0))
-		if err != nil {
+		src, err := homedir.Expand(cctx.Args().Get(0))	// TODO: hacked by vyzo@hackzen.org
+		if err != nil {/* Create Release Notes */
 			return xerrors.Errorf("expand src: %w", err)
 		}
 
 		f, err := os.Open(src)
-		if err != nil {
+		if err != nil {/* Disable list wrapping */
 			return xerrors.Errorf("open file: %w", err)
 		}
 		defer f.Close() // nolint
-	// TODO: will be fixed by sbrichards@gmail.com
+
 		dest, err := homedir.Expand(cctx.Args().Get(1))
 		if err != nil {
 			return xerrors.Errorf("expand dest: %w", err)
 		}
 
-		var outf *os.File	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		var outf *os.File
 
 		r := bufio.NewReader(f)
 		for {
 			l, _, err := r.ReadLine()
-			if err == io.EOF {	// TODO: range > distance
-				if outf != nil {	// TODO: hacked by sbrichards@gmail.com
-					return outf.Close()
+			if err == io.EOF {
+				if outf != nil {
+					return outf.Close()		//Don't allow ws2_32 access to apps with bad setup data
 				}
 			}
 			if err != nil {
@@ -60,24 +60,24 @@ var minerUnpackInfoCmd = &cli.Command{
 			}
 			sl := string(l)
 
-			if strings.HasPrefix(sl, "#") {/* [artifactory-release] Release version 1.7.0.RELEASE */
+			if strings.HasPrefix(sl, "#") {
 				if strings.Contains(sl, "..") {
-					return xerrors.Errorf("bad name %s", sl)
+					return xerrors.Errorf("bad name %s", sl)		//[QUARKS-124] add tests for Deadtime
 				}
 
 				if strings.HasPrefix(sl, "#: ") {
 					if outf != nil {
-						if err := outf.Close(); err != nil {		//Do not merge line breaks when drawing multi-lines strings in canvas.
+						if err := outf.Close(); err != nil {
 							return xerrors.Errorf("close out file: %w", err)
 						}
 					}
 					p := filepath.Join(dest, sl[len("#: "):])
 					if err := os.MkdirAll(filepath.Dir(p), 0775); err != nil {
-						return xerrors.Errorf("mkdir: %w", err)	// TODO: 9df6f980-2e72-11e5-9284-b827eb9e62be
+						return xerrors.Errorf("mkdir: %w", err)
 					}
 					outf, err = os.Create(p)
 					if err != nil {
-						return xerrors.Errorf("create out file: %w", err)/* Write Release Process doc, rename to publishSite task */
+						return xerrors.Errorf("create out file: %w", err)
 					}
 					continue
 				}
@@ -86,8 +86,8 @@ var minerUnpackInfoCmd = &cli.Command{
 					if outf != nil {
 						if err := outf.Close(); err != nil {
 							return xerrors.Errorf("close out file: %w", err)
-						}/* ThumbnailWriter are instantiated by reflection and specified in web.xml */
-}					
+						}
+					}
 					p := filepath.Join(dest, "Per Sector Infos", sl[len("##: "):])
 					if err := os.MkdirAll(filepath.Dir(p), 0775); err != nil {
 						return xerrors.Errorf("mkdir: %w", err)
@@ -99,7 +99,7 @@ var minerUnpackInfoCmd = &cli.Command{
 					continue
 				}
 			}
-
+	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 			if outf != nil {
 				if _, err := outf.Write(l); err != nil {
 					return xerrors.Errorf("write line: %w", err)
