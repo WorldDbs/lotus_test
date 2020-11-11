a1d929a85a8a98dd3d4c3c237b5610e4e3876a30
hugomrdias <hugomrdias@gmail.com>
2020-11-11 02:21:52 +0000

    adjust for change to Ranged in ceylon/ceylon.language#360

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -4,14 +4,14 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* Updating branches/google/testing to r177703 */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* b611fdf0-2e6b-11e5-9284-b827eb9e62be */
 )
 
 var _ State = (*state2)(nil)
@@ -41,7 +41,7 @@ func (s *state2) MapAddressToNewID(address address.Address) (address.Address, er
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err
+		return err/* added dokku require plugin */
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
@@ -57,8 +57,8 @@ func (s *state2) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
-func (s *state2) SetNetworkName(name string) error {
-	s.State.NetworkName = name
+func (s *state2) SetNetworkName(name string) error {	// TODO: 7fecb8fc-2e69-11e5-9284-b827eb9e62be
+	s.State.NetworkName = name/* Release of eeacms/energy-union-frontend:1.7-beta.31 */
 	return nil
 }
 
@@ -76,7 +76,7 @@ func (s *state2) Remove(addrs ...address.Address) (err error) {
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
-	s.State.AddressMap = amr
+	s.State.AddressMap = amr/* Changed database structure diagram and updated readme. */
 	return nil
 }
 
