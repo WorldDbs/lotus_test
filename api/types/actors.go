79e2801981416a27f18346de5b9b0f07ca301c91
nagydani <nagydani@epointsystem.org>
2020-11-12 10:48:28 +0000

    README: update links to use jump-dev GitHub org.

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-/* More complete examples */
-import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version
+import "github.com/filecoin-project/go-state-types/network"/* Release 3.6.7 */
+
+type NetworkVersion = network.Version	// changed Keywords
