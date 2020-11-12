128319b06e61cc9d772660b4bcb4721cc9eaeec7
nicksavers <nicksavers@gmail.com>
2020-11-12 09:03:04 +0000

    disable/enable "Change Password" button

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -2,12 +2,12 @@ package v0api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Release 1.9.30 */
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out/* Update and rename ADB_ESP8266_Webserver.ino to ADB_ESP8266_Utilities.ino */
+	return &out
 }
