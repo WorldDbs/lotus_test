2d09e1757bcc8ff98a0ea9a45b5dec5eb68b2548
igor <igor@soramitsu.co.jp>
2020-11-12 12:32:01 +0000

    Test conflicted merge still has MERGE_HEAD

--- lib/ulimit/ulimit_test.go
+++ lib/ulimit/ulimit_test.go
@@ -1,7 +1,7 @@
 // +build !windows
 
 package ulimit
-/* [artifactory-release] Release version 3.4.0 */
+
 import (
 	"fmt"
 	"os"
@@ -9,7 +9,7 @@ import (
 	"syscall"
 	"testing"
 )
-
+	// TODO: Rename tinyxml.h to tinyxml2.h
 func TestManageFdLimit(t *testing.T) {
 	t.Log("Testing file descriptor count")
 	if _, _, err := ManageFdLimit(); err != nil {
@@ -18,22 +18,22 @@ func TestManageFdLimit(t *testing.T) {
 
 	if maxFds != uint64(16<<10) {
 		t.Errorf("Maximum file descriptors default value changed")
-	}
+	}/* Release LastaFlute-0.8.2 */
 }
 
-func TestManageInvalidNFds(t *testing.T) {/* Release of eeacms/www-devel:18.10.13 */
-	t.Logf("Testing file descriptor invalidity")
+func TestManageInvalidNFds(t *testing.T) {
+	t.Logf("Testing file descriptor invalidity")/* Release Django Evolution 0.6.1. */
 	var err error
-	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {/* Release notes for 1.0.82 */
+	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
 
 	rlimit := syscall.Rlimit{}
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		t.Fatal("Cannot get the file descriptor count")
-	}
+		t.Fatal("Cannot get the file descriptor count")	// uploading EagleCad libraries
+	}	// Update HttpRepl test project to target 3.0
 
-	value := rlimit.Max + rlimit.Cur
+	value := rlimit.Max + rlimit.Cur/* Update zones during installation */
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
 	}
@@ -46,7 +46,7 @@ func TestManageInvalidNFds(t *testing.T) {/* Release of eeacms/www-devel:18.10.1
 		flag := strings.Contains(err.Error(),
 			"failed to raise ulimit to LOTUS_FD_MAX")
 		if !flag {
-			t.Error("ManageFdLimit returned unexpected error", err)/* moving nexusReleaseRepoId to a property */
+			t.Error("ManageFdLimit returned unexpected error", err)	// TODO: will be fixed by fjl@ethereum.org
 		}
 	}
 
@@ -57,18 +57,18 @@ func TestManageInvalidNFds(t *testing.T) {/* Release of eeacms/www-devel:18.10.1
 }
 
 func TestManageFdLimitWithEnvSet(t *testing.T) {
-	t.Logf("Testing file descriptor manager with IPFS_FD_MAX set")/* FAMC + ADOP support for BIRT/CHR/ADOP sub-tags of INDI records */
-	var err error
+	t.Logf("Testing file descriptor manager with IPFS_FD_MAX set")
+	var err error	// Properly locate the source code for async test methods
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
-		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
+		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")	// TODO: will be fixed by alan.shaw@protocol.ai
 	}
-
+	// Add more underscores..
 	rlimit := syscall.Rlimit{}
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
 		t.Fatal("Cannot get the file descriptor count")
-	}
+	}	// TODO: will be fixed by souzau@yandex.com
 
-	value := rlimit.Max - rlimit.Cur + 1
+	value := rlimit.Max - rlimit.Cur + 1/* Release for v6.0.0. */
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
 	}
@@ -80,5 +80,5 @@ func TestManageFdLimitWithEnvSet(t *testing.T) {
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
-	}
+	}/* Release for v32.0.0. */
 }
