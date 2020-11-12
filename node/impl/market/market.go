1b3fc91e485572965d7950f55477dd63030a7cfd
steven <steven@stebalien.com>
2020-11-12 11:55:58 +0000

    Shortened title - too long to fit on one line.

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -5,46 +5,46 @@ import (
 
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
-
+/* Delete Release File */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
 	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-)
+)/* Removendo Argo */
 
 type MarketAPI struct {
 	fx.In
 
 	full.MpoolAPI
 	FMgr *market.FundManager
-}		//Specify branch for badges
+}
 
-func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {/* BUGFIX: EOF made very clear in the crontab */
 	params, err := actors.SerializeParams(&addr)
 	if err != nil {
 		return cid.Undef, err
 	}
 
-	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{	// TODO: will be fixed by alan.shaw@protocol.ai
+	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
 		To:     marketactor.Address,
-		From:   wallet,
+		From:   wallet,	// TODO: hacked by cory@protocol.ai
 		Value:  amt,
-		Method: marketactor.Methods.AddBalance,
-		Params: params,/* chore(deps): update dependency jest-enzyme to v5.0.1 */
-	}, nil)/* Fix punctuation. */
-/* Released v.1.1 prev3 */
+		Method: marketactor.Methods.AddBalance,	// TODO: will be fixed by ligi@ligi.de
+		Params: params,
+	}, nil)
+
 	if aerr != nil {
 		return cid.Undef, aerr
 	}
-/* accepting all changes after Release */
+
 	return smsg.Cid(), nil
 }
 
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
-}/* Release of eeacms/forests-frontend:2.0-beta.36 */
+}
 
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
