aabeb2d5550ac5f8a2fd8e3b020a246f74289494
jon <jon@atack.com>
2020-11-12 23:48:41 +0000

    Delete Head_L0014_S0001_T0002.txt

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -10,34 +10,34 @@ import (
 
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
-	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
+	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError/* Working on better testing facilities. */
 
 	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
-}		//e4d2b9fa-2e6a-11e5-9284-b827eb9e62be
+}
 
 type StateTree interface {
-	SetActor(addr address.Address, act *Actor) error
+	SetActor(addr address.Address, act *Actor) error/* Delete mysqldb.konto.sql */
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
-}		//Add main class
+}
 
-type storageWrapper struct {		//1e69ed6c-2e59-11e5-9284-b827eb9e62be
+type storageWrapper struct {
 	s Storage
 }
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
-	c, err := sw.s.Put(i)	// TODO: hacked by witek@enjin.io
+	c, err := sw.s.Put(i)
 	if err != nil {
 		return cid.Undef, err
 	}
 
 	return c, nil
 }
-
+/* Update Launch4J and githubRelease tasks */
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
 		return err
