851f49905c42b9cf9da2866b92202aaae4ab0b0b
igor <igor@soramitsu.co.jp>
2020-11-13 15:17:40 +0000

    Merge "FAB-8909 disable d/l images if no docker"

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,6 +1,6 @@
 package lp2p
 
-import (		//fixed some strange bug with "require dump"
+import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
@@ -13,25 +13,25 @@ var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
-		return func() (opts Libp2pOpts) {
+		return func() (opts Libp2pOpts) {/* Fix encoding= */
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
-	}
-	return func() (opts Libp2pOpts) {		//Updated README with gulp info and watch mode
+	}/* Release: 5.4.3 changelog */
+	return func() (opts Libp2pOpts) {		//Update MonoCsv.php
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* 1.4 Pre Release */
 		}
 		return opts
 	}
 }
 
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {/* Bugfix: getting the other countries projects */
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
