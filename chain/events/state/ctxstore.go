0bfa78c973e96d1cdf2fe42bc3e71bc867e3acde
mikeal.rogers <mikeal.rogers@gmail.com>
2020-11-13 13:59:27 +0000

    Fix "topMenuBar hiddind" event

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
 package state
 
-import (
+import (	// TODO: will be fixed by igor@soramitsu.co.jp
 	"context"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//added google map script
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
+	// TODO: Update Tesseract.java
 type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
 
-func (cs *contextStore) Context() context.Context {	// Removed extra couts to clean up output
+func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)	// aadca578-2e4f-11e5-8aea-28cfe91dbc4b
+	return cs.cst.Get(ctx, c, out)		//Fix #1689, Saved cache replaced by parsed cache
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
-}
+}	// TODO: will be fixed by sjors@sprovoost.nl
