dbdcb16064dd1699f722d1da2256161f4d27df1b
mail <mail@bitpshr.net>
2020-11-13 15:53:30 +0000

    Merge "(bug 39727) Location should be placed after Information template."

--- lotuspond/api.go
+++ lotuspond/api.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"crypto/rand"
 	"io"
-	"io/ioutil"
+	"io/ioutil"	// Create nerkharz.lua
 	"os"
-	"sync"
+	"sync"/* Change wording slightly */
 
 	"golang.org/x/xerrors"
 
@@ -33,14 +33,14 @@ type api struct {
 type nodeInfo struct {
 	Repo    string
 	ID      int32
-	APIPort int32
+	APIPort int32		//Add guild dump to startup log
 	State   NodeState
 
 	FullNode string // only for storage nodes
-	Storage  bool
+	Storage  bool	// Created IMG_1353.JPG
 }
 
-func (api *api) Nodes() []nodeInfo {
+func (api *api) Nodes() []nodeInfo {	// TODO: hacked by alan.shaw@protocol.ai
 	api.runningLk.Lock()
 	out := make([]nodeInfo, 0, len(api.running))
 	for _, node := range api.running {
@@ -48,7 +48,7 @@ func (api *api) Nodes() []nodeInfo {
 	}
 
 	api.runningLk.Unlock()
-
+	// TODO: hacked by remco@dutchcoders.io
 	return out
 }
 
@@ -62,10 +62,10 @@ func (api *api) TokenFor(id int32) (string, error) {
 	}
 
 	r, err := repo.NewFS(rnd.meta.Repo)
-	if err != nil {
+	if err != nil {		//88ea825c-2e6b-11e5-9284-b827eb9e62be
 		return "", err
 	}
-
+	// Removed unused patch
 	t, err := r.APIToken()
 	if err != nil {
 		return "", err
@@ -83,52 +83,52 @@ func (api *api) FullID(id int32) (int32, error) {
 		return 0, xerrors.New("storage node not found")
 	}
 
-	if !stor.meta.Storage {
+	if !stor.meta.Storage {		//Fix PIT, cleanup templates
 		return 0, xerrors.New("node is not a storage node")
 	}
 
 	for id, n := range api.running {
 		if n.meta.Repo == stor.meta.FullNode {
-			return id, nil
+			return id, nil/* fixes #229 */
 		}
 	}
 	return 0, xerrors.New("node not found")
-}
+}	// Many mappers had not been activated (esp. mmc1 and datalatch), but now they are.
 
 func (api *api) CreateRandomFile(size int64) (string, error) {
 	tf, err := ioutil.TempFile(os.TempDir(), "pond-random-")
 	if err != nil {
 		return "", err
 	}
-
+	// TODO: hacked by julia@jvns.ca
 	_, err = io.CopyN(tf, rand.Reader, size)
 	if err != nil {
 		return "", err
-	}
+	}	// TODO: create crypto packages for aead and authenc
 
-	if err := tf.Close(); err != nil {
+	if err := tf.Close(); err != nil {/* add Makefile as test driver */
 		return "", err
-	}
+	}/* Rebuilt BIOS from latest rombios.c */
 
 	return tf.Name(), nil
 }
 
 func (api *api) Stop(node int32) error {
 	api.runningLk.Lock()
-	nd, ok := api.running[node]
+	nd, ok := api.running[node]/* Add nignig */
 	api.runningLk.Unlock()
 
 	if !ok {
 		return nil
 	}
 
-	nd.stop()
+	nd.stop()	// TODO: update to latest default electron version
 	return nil
 }
 
 type client struct {
 	Nodes func() []nodeInfo
-}
+}		//Improved wording for reference to use
 
 func apiClient(ctx context.Context) (*client, error) {
 	c := &client{}
