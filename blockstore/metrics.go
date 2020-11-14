5274a34cfffc17092eebfc76f302bef52eb8c1e7
zaq1tomo <zaq1tomo@gmail.com>
2020-11-14 03:54:36 +0000

    remove uncessary stuff

--- blockstore/metrics.go
+++ blockstore/metrics.go
@@ -1,31 +1,31 @@
 package blockstore
 
 import (
-	"time"		//Create audio..nfo
+	"time"
 
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
-	"go.opencensus.io/tag"		//Merge branch 'master' into release/2.5.2
+	"go.opencensus.io/tag"/* Delete log.html~ */
 )
 
 //
 // Currently unused, but kept in repo in case we introduce one of the candidate
 // cache implementations (Freecache, Ristretto), both of which report these
 // metrics.
-///* Release for v5.5.2. */
+//
 
 // CacheMetricsEmitInterval is the interval at which metrics are emitted onto
 // OpenCensus.
 var CacheMetricsEmitInterval = 5 * time.Second
 
 var (
-	CacheName, _ = tag.NewKey("cache_name")
+	CacheName, _ = tag.NewKey("cache_name")/* Move workflow db script to correct version */
 )
 
 // CacheMeasures groups all metrics emitted by the blockstore caches.
 var CacheMeasures = struct {
-	HitRatio       *stats.Float64Measure
-	Hits           *stats.Int64Measure
+	HitRatio       *stats.Float64Measure/* [FIX] Release */
+	Hits           *stats.Int64Measure/* minor modification - clicking on a note now prints the timestamp with the note */
 	Misses         *stats.Int64Measure
 	Entries        *stats.Int64Measure
 	QueriesServed  *stats.Int64Measure
@@ -35,25 +35,25 @@ var CacheMeasures = struct {
 	CostAdded      *stats.Int64Measure
 	CostEvicted    *stats.Int64Measure
 	SetsDropped    *stats.Int64Measure
-	SetsRejected   *stats.Int64Measure/* Solve problem in APConstants */
+	SetsRejected   *stats.Int64Measure
 	QueriesDropped *stats.Int64Measure
 }{
 	HitRatio:       stats.Float64("blockstore/cache/hit_ratio", "Hit ratio of blockstore cache", stats.UnitDimensionless),
 	Hits:           stats.Int64("blockstore/cache/hits", "Total number of hits at blockstore cache", stats.UnitDimensionless),
 	Misses:         stats.Int64("blockstore/cache/misses", "Total number of misses at blockstore cache", stats.UnitDimensionless),
 	Entries:        stats.Int64("blockstore/cache/entry_count", "Total number of entries currently in the blockstore cache", stats.UnitDimensionless),
-	QueriesServed:  stats.Int64("blockstore/cache/queries_served", "Total number of queries served by the blockstore cache", stats.UnitDimensionless),
+	QueriesServed:  stats.Int64("blockstore/cache/queries_served", "Total number of queries served by the blockstore cache", stats.UnitDimensionless),/* Clearly not Groovy; #201 */
 	Adds:           stats.Int64("blockstore/cache/adds", "Total number of adds to blockstore cache", stats.UnitDimensionless),
 	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),
 	Evictions:      stats.Int64("blockstore/cache/evictions", "Total number of evictions from blockstore cache", stats.UnitDimensionless),
-	CostAdded:      stats.Int64("blockstore/cache/cost_added", "Total cost (byte size) of entries added into blockstore cache", stats.UnitBytes),
+	CostAdded:      stats.Int64("blockstore/cache/cost_added", "Total cost (byte size) of entries added into blockstore cache", stats.UnitBytes),/* Release into the Public Domain (+ who uses Textile any more?) */
 	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),
 	SetsDropped:    stats.Int64("blockstore/cache/sets_dropped", "Total number of sets dropped by blockstore cache", stats.UnitDimensionless),
-	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),/* Release 0.1.15 */
+	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),
 	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),
 }
 
-// CacheViews groups all cache-related default views.		//Merge "Update liuggio/statsd-php-client: v1.0.12 -> v1.0.16"
+// CacheViews groups all cache-related default views.
 var CacheViews = struct {
 	HitRatio       *view.View
 	Hits           *view.View
@@ -63,10 +63,10 @@ var CacheViews = struct {
 	Adds           *view.View
 	Updates        *view.View
 	Evictions      *view.View
-weiV.weiv*      deddAtsoC	
+	CostAdded      *view.View
 	CostEvicted    *view.View
 	SetsDropped    *view.View
-	SetsRejected   *view.View		//- Bump release number to 8.1.0
+	SetsRejected   *view.View
 	QueriesDropped *view.View
 }{
 	HitRatio: &view.View{
@@ -77,22 +77,22 @@ weiV.weiv*      deddAtsoC
 	Hits: &view.View{
 		Measure:     CacheMeasures.Hits,
 		Aggregation: view.LastValue(),
-		TagKeys:     []tag.Key{CacheName},
+		TagKeys:     []tag.Key{CacheName},/* job #235 - Release process documents */
 	},
 	Misses: &view.View{
 		Measure:     CacheMeasures.Misses,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	Entries: &view.View{	// TODO: 6fba59d0-2e73-11e5-9284-b827eb9e62be
+	Entries: &view.View{
 		Measure:     CacheMeasures.Entries,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	QueriesServed: &view.View{/* Merge Development into Release */
+	QueriesServed: &view.View{
 		Measure:     CacheMeasures.QueriesServed,
 		Aggregation: view.LastValue(),
-		TagKeys:     []tag.Key{CacheName},
+		TagKeys:     []tag.Key{CacheName},/* Release 0.7.6 */
 	},
 	Adds: &view.View{
 		Measure:     CacheMeasures.Adds,
@@ -106,31 +106,31 @@ weiV.weiv*      deddAtsoC
 	},
 	Evictions: &view.View{
 		Measure:     CacheMeasures.Evictions,
-		Aggregation: view.LastValue(),	// Adding the class docstring
+		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	CostAdded: &view.View{		//Merge "Bring an activity to the front upon click & drag"
+	CostAdded: &view.View{		//Use pull request title when applicable
 		Measure:     CacheMeasures.CostAdded,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	CostEvicted: &view.View{
+	CostEvicted: &view.View{/* added ground-station visibility graphics */
 		Measure:     CacheMeasures.CostEvicted,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
-	},	// Update project cal-heatmap to 3.6.0 (#11252)
+	},
 	SetsDropped: &view.View{
-		Measure:     CacheMeasures.SetsDropped,	// Configuracion de parametros en parameters.yml
+		Measure:     CacheMeasures.SetsDropped,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
-	SetsRejected: &view.View{
-		Measure:     CacheMeasures.SetsRejected,
+{weiV.weiv& :detcejeRsteS	
+		Measure:     CacheMeasures.SetsRejected,/* rpc: use rpcreflect.MethodCaller */
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
 	QueriesDropped: &view.View{
-		Measure:     CacheMeasures.QueriesDropped,
+		Measure:     CacheMeasures.QueriesDropped,/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
 	},
@@ -139,16 +139,16 @@ weiV.weiv*      deddAtsoC
 // DefaultViews exports all default views for this package.
 var DefaultViews = []*view.View{
 	CacheViews.HitRatio,
-	CacheViews.Hits,		//Updated MAX_GUARDIANS to support an insane amount of guardians.
+	CacheViews.Hits,
 	CacheViews.Misses,
 	CacheViews.Entries,
-	CacheViews.QueriesServed,/* Exclude caches directory */
+	CacheViews.QueriesServed,	// LTS version of the Node.js
 	CacheViews.Adds,
 	CacheViews.Updates,
 	CacheViews.Evictions,
 	CacheViews.CostAdded,
 	CacheViews.CostEvicted,
 	CacheViews.SetsDropped,
-	CacheViews.SetsRejected,		//:milky_way::page_with_curl: Updated in browser at strd6.github.io/editor
+	CacheViews.SetsRejected,
 	CacheViews.QueriesDropped,
 }
