c215743fc140c04efeb032cfef45431a1f842eac
bokky.poobah <bokky.poobah@bokconsulting.com.au>
2020-11-14 18:51:33 +0000

    Merge "Introduce upgrade testing with Grenade"

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -5,26 +5,26 @@ import (
 	"io"
 )
 
-// State is the state for the chaos actor used by some methods to invoke
+// State is the state for the chaos actor used by some methods to invoke	// TODO: Changed message list errors.
 // behaviours in the vm or runtime.
-type State struct {/* DATASOLR-47 - Release version 1.0.0.RC1. */
+type State struct {
 	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
 	Value string
-	// Unmarshallable is a sentinel value. If the slice contains no values, the
+	// Unmarshallable is a sentinel value. If the slice contains no values, the/* Merge "Doc FIX" */
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
-}		//Added smart scaler
+}
 
 // UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
-// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.	// TODO: will be fixed by steven@stebalien.com
+// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
 // UnmarshalCBOR will fail to unmarshal the value from CBOR.
 func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 	return fmt.Errorf("failed to unmarshal cbor")
-}
+}	// added reference to https://github.com/taketime/hood-model
 
 // MarshalCBOR will fail to marshal the value to CBOR.
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
