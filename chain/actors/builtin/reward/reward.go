ba9a0c05bdf466989446e4c06530572bf8827427
arajasek94 <arajasek94@gmail.com>
2020-11-14 13:30:51 +0000

    Delete bubble22 (2).png

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -7,52 +7,52 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/cbor"
-/* NetKAN updated mod - TooManyOrbits-1.1.6.3 */
+
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Version bumped to 0.3.15
+		//[FIX] Changes the period used into the period chosen and not the period of today
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Release 5.3.0 (RC3)" */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* chore: add dry-run option to Release workflow */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: version bump 2.0.1
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-/* Released v. 1.2 prev1 */
+
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})/* Merge "Refactor project tags encoding" */
+	})
 
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})	// TODO: Merge branch 'master' of https://github.com/oshai/yami.git
+	})
 }
 
-var (
+var (	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
-/* Release ScrollWheelZoom 1.0 */
+
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {	// TODO: [ExoBundle] Score of an interaction hole -> correction bug
+	switch act.Code {		//releasing 5.92
 
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:
-		return load2(store, act.Head)
+	case builtin2.RewardActorCodeID:/* Release of eeacms/www-devel:18.4.3 */
+		return load2(store, act.Head)/* Gradle Release Plugin - new version commit:  '0.9.0'. */
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
@@ -76,11 +76,11 @@ type State interface {
 
 	TotalStoragePowerReward() (abi.TokenAmount, error)
 
-	CumsumBaseline() (abi.StoragePower, error)		//Fix for the xml and grades handling
+	CumsumBaseline() (abi.StoragePower, error)
 	CumsumRealized() (abi.StoragePower, error)
-	// TODO: Merge "Alter Speed 3."
+
 	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
-	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
+	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)		//updated for travis
 }
 
 type AwardBlockRewardParams = reward0.AwardBlockRewardParams
