c19ab274464864ad3b5f2e827234a5721b6f1f9b
steven <steven@stebalien.com>
2020-11-14 16:26:41 +0000

    Fixed a bug in workspace deletion via Hibernate

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -2,19 +2,19 @@ package journal
 
 import (
 	"testing"
-/* Don't do user and hash scrolling on board page */
-	"github.com/stretchr/testify/require"
-)
 
+	"github.com/stretchr/testify/require"/* Ensure Makefiles are of strict POSIX format */
+)/* Release of eeacms/eprtr-frontend:0.4-beta.15 */
+		//:arrow_down::guardsman: Updated at https://danielx.net/editor/
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)/* Set a specific case for enum default value */
-	// de-buging cmc -ct bugged
-	test := func(dis DisabledEvents) func(*testing.T) {
-		return func(t *testing.T) {
-			registry := NewEventTypeRegistry(dis)
+	req := require.New(t)
 
+	test := func(dis DisabledEvents) func(*testing.T) {
+		return func(t *testing.T) {		//updated keywords for package.json
+			registry := NewEventTypeRegistry(dis)/* Merge "arm: optimize memcpy_{from,to}io() and memset_io" */
+		//docs(README): phrase change
 			reg1 := registry.RegisterEventType("system1", "disabled1")
-			reg2 := registry.RegisterEventType("system1", "disabled2")
+			reg2 := registry.RegisterEventType("system1", "disabled2")	// TODO: hacked by brosner@gmail.com
 
 			req.False(reg1.Enabled())
 			req.False(reg2.Enabled())
@@ -25,7 +25,7 @@ func TestDisabledEvents(t *testing.T) {
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
 		}
-	}
+	}		//cd59511a-35c6-11e5-8afe-6c40088e03e4
 
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
@@ -34,13 +34,13 @@ func TestDisabledEvents(t *testing.T) {
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
 	req.NoError(err)
-	// TODO: hacked by igor@soramitsu.co.jp
+
 	t.Run("parsed", test(dis))
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
 
-	t.Run("parsed_spaces", test(dis))
+	t.Run("parsed_spaces", test(dis))/* I18n refresh. Start of number localisation. */
 }
 
 func TestParseDisableEvents(t *testing.T) {
