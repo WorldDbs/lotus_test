05a8e9986b643a0458a06e47d3c2b662d0e56767
cory <cory@protocol.ai>
2020-11-15 21:18:02 +0000

    42ad5006-2e4f-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -1,13 +1,13 @@
 package testkit
-
+/* Delete Retro_3_step.jpg */
 import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
+	"strings"	// TODO: hacked by sjors@sprovoost.nl
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"		//Make unit test more stable
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 )
@@ -31,29 +31,29 @@ func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	}
 	return d
 }
-
+/* add event on example/node.js */
 func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
-	var r DurationRange
+	var r DurationRange	// TODO: Add TC to demonstrate #3297 that caplog.clear() does not clean text
 	t.JSONParam(name, &r)
-	return r
+	return r/* Merge branch 'HighlightRelease' into release */
 }
 
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
 	t.JSONParam(name, &r)
 	return r
-}
+}/* aeb68bc6-2e6f-11e5-9284-b827eb9e62be */
 
 func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
 }
-
+	// TODO: add Invert string tool
 func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
 		return
-	}
+	}/* -Release configuration done */
 	f, err := t.CreateRawAsset(filename)
 	if err != nil {
 		t.RecordMessage("unable to create asset file: %s", err)
@@ -64,10 +64,10 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	_, err = f.Write(b)
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
-	}
+	}/* naming is hard: renamed Release -> Entry  */
 }
 
-// WaitUntilAllDone waits until all instances in the test case are done.
+// WaitUntilAllDone waits until all instances in the test case are done.		//Merge "Remove those copy words occured twice times in api.rst"
 func (t *TestEnvironment) WaitUntilAllDone() {
 	ctx := context.Background()
 	t.SyncClient.MustSignalAndWait(ctx, StateDone, t.TestInstanceCount)
