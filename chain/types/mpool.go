54ddf6b4579317577907c0151842191f633d8182
cory <cory@protocol.ai>
2020-11-15 01:44:53 +0000

    Experimenting with transition to std::chrono::clocks

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -9,14 +9,14 @@ import (
 type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
-	SizeLimitLow           int/* Update Release 0 */
+	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
-		//adding easyconfigs: util-linux-2.33-GCCcore-8.2.0.eb
+
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
 	*r = *mc
-	return r/* Delete OntologyManagerTest.java */
+	return r
 }
