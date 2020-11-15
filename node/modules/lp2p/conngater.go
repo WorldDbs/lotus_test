794c7c983e5c4bf8589d8c149b42cb18d3f03765
brosner <brosner@gmail.com>
2020-11-15 18:55:30 +0000

    Main Klasse

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,7 +1,7 @@
-package lp2p		//Merge branch 'master' of https://github.com/bnb4/hw-www.git
+package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"/* Release 2.2.0 */
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -9,9 +9,9 @@ import (
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}
+}/* Release 1.10.5 and  2.1.0 */
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* handleResponse() would not be called if sink was being used. */
 	return
 }
