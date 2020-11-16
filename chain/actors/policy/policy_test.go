9dfbe3c0f0622576bb1e813a43ba9987e31e6a5c
alan.shaw <alan.shaw@protocol.ai>
2020-11-16 10:30:34 +0000

    change go get code

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,5 +1,5 @@
 package policy
-/* 9bc4adf4-2e42-11e5-9284-b827eb9e62be */
+
 import (
 	"testing"
 
@@ -16,17 +16,17 @@ import (
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
 
-func TestSupportedProofTypes(t *testing.T) {/* First Release- */
+func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
 	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
-	})
-
+	})/* Release of s3fs-1.30.tar.gz */
+/* Highlight less and sass more correctly. */
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	require.EqualValues(t,
+	require.EqualValues(t,/* Release Roadmap */
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
@@ -34,20 +34,20 @@ func TestSupportedProofTypes(t *testing.T) {/* First Release- */
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* Release update */
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
-	)	// OP Metagame
-}
+	)
+}		//Update StatusBarManager.java
 
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
-	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
+	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)		//Root class for card
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
-	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
+	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)/* add Release Notes */
 	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
@@ -59,11 +59,11 @@ func TestAssumptions(t *testing.T) {
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
-		require.NoError(t, err)/* Added waitForReleased7D() */
+		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
 			// new proof type.
-			continue	// TODO: will be fixed by steven@stebalien.com
+			continue
 		}
 		require.Equal(t, sizeOld, sizeNew)
 	}
--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	cbor "github.com/ipfs/go-ipld-cbor"
-		//Refactored to fluent builder 
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -20,26 +20,26 @@ func init() {
 
 	idk, err := address.NewFromString("t080")
 	if err != nil {
-		panic(err)/* Released 7.2 */
-	}/* added a dump params fn (which dumps all params... to actions */
+		panic(err)
+	}
 
-	RootVerifierID = idk
+	RootVerifierID = idk	// TODO: will be fixed by fjl@ethereum.org
 }
 
-func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {		//Support/PathV1: Deprecate get{Basename,Dirname,Suffix}.
+func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}/* ef1d5b88-2e59-11e5-9284-b827eb9e62be */
+	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
 
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {/* Release build will fail if tests fail */
+	if err != nil {
 		return nil, err
-	}/* Uint allways >= 0 */
+	}	// TODO: Rebuilt index with KaitoYamashiro
 
 	act := &types.Actor{
 		Code:    builtin.VerifiedRegistryActorCodeID,
