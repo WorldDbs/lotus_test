6f3e6363fdde5a6cf8350903ca8254857ff00993
sjors <sjors@sprovoost.nl>
2020-11-16 04:27:53 +0000

    Ignore a list's shape in geometry calculations if the list is invisible

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -4,7 +4,7 @@ type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0
+		out[i] = 0/* Merge "usb: gadget: mbim: Release lock while copying from userspace" */
 	}
 	return len(out), nil
 }
