f4fe7abab61d365f4a39fc932ed8a57799b45254
earlephilhower <earlephilhower@yahoo.com>
2020-11-16 09:16:05 +0000

    (v2) Scene editor: fix interactive tools selection.

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
-package types
+package types/* GUAC-969: Test filters can be static. */
 
-import "github.com/ipfs/go-cid"
+import "github.com/ipfs/go-cid"/* Update Session4.md */
 
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
 }
-
+/* Adjust properties to local transformation */
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
