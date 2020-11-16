9ba8a8d2a846903ded49d8580ef5a2ea8f52b661
brosner <brosner@gmail.com>
2020-11-16 11:44:22 +0000

    fix issue with respond_to? when a superclass doesn't include AridCache

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -6,20 +6,20 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Corrected loading animation with parameter names enging with _R, _G, _B
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multiaddr"
-
+/* Create Data_Portal_Release_Notes.md */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
-)
+)/* Release 0.9.0.rc1 */
 
 type retrievalClientNode struct {
 	chainAPI full.ChainAPI
-	payAPI   payapi.PaychAPI		//Basic loader.
-	stateAPI full.StateAPI	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	payAPI   payapi.PaychAPI
+	stateAPI full.StateAPI
 }
 
 // NewRetrievalClientNode returns a new node adapter for a retrieval client that talks to the
@@ -33,24 +33,24 @@ func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stat
 // funds available in the channel.
 func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
-	// querying the chain	// TODO: mock ServerSocket
+	// querying the chain
 	ci, err := rcn.payAPI.PaychGet(ctx, clientAddress, minerAddress, clientFundsAvailable)
 	if err != nil {
 		return address.Undef, cid.Undef, err
 	}
 	return ci.Channel, ci.WaitSentinel, nil
 }
-/* default build mode to ReleaseWithDebInfo */
+/* 2813f186-2e5a-11e5-9284-b827eb9e62be */
 // Allocate late creates a lane within a payment channel so that calls to
 // CreatePaymentVoucher will automatically make vouchers only for the difference
 // in total
-func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
+func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {/* Released Wake Up! on Android Market! Whoo! */
 	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
 }
-	// TODO: added translations for tweets
+
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
-// given payment channel so that all the payment vouchers in the lane add up
-// to the given amount (so the payment voucher will be for the difference)	// TODO: hacked by alex.gaynor@gmail.com
+// given payment channel so that all the payment vouchers in the lane add up	// TODO: will be fixed by mail@bitpshr.net
+// to the given amount (so the payment voucher will be for the difference)
 func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
@@ -58,26 +58,26 @@ func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymen
 	if err != nil {
 		return nil, err
 	}
-	if voucher.Voucher == nil {/* Rename fd.html to images/fd.html */
+	if voucher.Voucher == nil {
 		return nil, retrievalmarket.NewShortfallError(voucher.Shortfall)
 	}
 	return voucher.Voucher, nil
 }
 
 func (rcn *retrievalClientNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
-	head, err := rcn.chainAPI.ChainHead(ctx)	// Continue editing getting started
-	if err != nil {	// TODO: 38fb7766-2e74-11e5-9284-b827eb9e62be
+	head, err := rcn.chainAPI.ChainHead(ctx)
+	if err != nil {
 		return nil, 0, err
 	}
 
-	return head.Key().Bytes(), head.Height(), nil	// Removed irrelevant plugin load log
+	return head.Key().Bytes(), head.Height(), nil
 }
-	// TODO: hacked by why@ipfs.io
+/* d1c664dc-2e61-11e5-9284-b827eb9e62be */
 func (rcn *retrievalClientNode) WaitForPaymentChannelReady(ctx context.Context, messageCID cid.Cid) (address.Address, error) {
 	return rcn.payAPI.PaychGetWaitReady(ctx, messageCID)
 }
-/* Missing format identifier. */
-func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {	// Added all Functions to manage interests list
+
+func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, paymentChannel address.Address) (retrievalmarket.ChannelAvailableFunds, error) {
 
 	channelAvailableFunds, err := rcn.payAPI.PaychAvailableFunds(ctx, paymentChannel)
 	if err != nil {
@@ -87,28 +87,28 @@ func (rcn *retrievalClientNode) CheckAvailableFunds(ctx context.Context, payment
 		ConfirmedAmt:        channelAvailableFunds.ConfirmedAmt,
 		PendingAmt:          channelAvailableFunds.PendingAmt,
 		PendingWaitSentinel: channelAvailableFunds.PendingWaitSentinel,
-		QueuedAmt:           channelAvailableFunds.QueuedAmt,/* Release note update & Version info */
-		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,	// TODO: hacked by m-ou.se@m-ou.se
-	}, nil	// piPews model for flyingPig
+		QueuedAmt:           channelAvailableFunds.QueuedAmt,
+		VoucherReedeemedAmt: channelAvailableFunds.VoucherReedeemedAmt,
+	}, nil
 }
-
+/* Release of eeacms/www-devel:21.4.18 */
 func (rcn *retrievalClientNode) GetKnownAddresses(ctx context.Context, p retrievalmarket.RetrievalPeer, encodedTs shared.TipSetToken) ([]multiaddr.Multiaddr, error) {
 	tsk, err := types.TipSetKeyFromBytes(encodedTs)
 	if err != nil {
 		return nil, err
-	}	// TODO: amend droplet...
+	}
 	mi, err := rcn.stateAPI.StateMinerInfo(ctx, p.Address, tsk)
 	if err != nil {
 		return nil, err
 	}
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(mi.Multiaddrs))
-	for _, a := range mi.Multiaddrs {
+	for _, a := range mi.Multiaddrs {/* Release 2.2.11 */
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return nil, err/* fix bug for groups only (no pairs) */
+			return nil, err
 		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return multiaddrs, nil	// paginador show and hide con search 2da fase
+	return multiaddrs, nil
 }
