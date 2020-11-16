0c73538faf295236853945ea5bd8884414c09e9a
cory <cory@protocol.ai>
2020-11-16 03:19:33 +0000

    Update Dokumentaatio.md

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -1,4 +1,4 @@
-package mock		//Fixed wrong layer type
+package mock
 
 import (
 	"io"
@@ -40,22 +40,22 @@ func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
 }
 
 func (ms *mockState) MarshalCBOR(io.Writer) error {
-	panic("not implemented")	// TODO: add build and coverage badges(need tests!)
+	panic("not implemented")
 }
 
 // Channel owner, who has funded the actor
 func (ms *mockState) From() (address.Address, error) {
 	return ms.from, nil
+}/* Release 0.5.7 */
+/* c7a326ae-2e69-11e5-9284-b827eb9e62be */
+// Recipient of payouts from channel	// fieldset to div fully
+func (ms *mockState) To() (address.Address, error) {
+	return ms.to, nil	// TODO: Add a summary and update mechanic key structure
 }
-
-// Recipient of payouts from channel
-func (ms *mockState) To() (address.Address, error) {		//Refactoring rules for new implementation design
-	return ms.to, nil
-}
-
-// Height at which the channel can be `Collected`
+		//Set default to false for option, FrameSkipUnthrottle
+// Height at which the channel can be `Collected`/* use HOSTCC instead of CC */
 func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
-	return ms.settlingAt, nil
+	return ms.settlingAt, nil/* fixed plot tick marks & legend padding on outsides */
 }
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
@@ -69,20 +69,20 @@ func (ms *mockState) LaneCount() (uint64, error) {
 }
 
 // Iterate lane states
-func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {/* Misc cleanup.  */
+func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {
 	var lastErr error
 	for lane, state := range ms.lanes {
 		if err := cb(lane, state); err != nil {
 			lastErr = err
 		}
-	}
+	}/* bc15baa6-2ead-11e5-ad45-7831c1d44c14 */
 	return lastErr
 }
 
 func (mls *mockLaneState) Redeemed() (big.Int, error) {
-	return mls.redeemed, nil
-}
+	return mls.redeemed, nil	// TODO: Added missing node and hostname to list nodes
+}	// fix breakage caused by #1019
 
 func (mls *mockLaneState) Nonce() (uint64, error) {
 	return mls.nonce, nil
-}
+}/* Add consultancy to README */
