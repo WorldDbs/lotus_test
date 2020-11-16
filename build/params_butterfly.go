386463f4566559d08d0951895d6884381be1444f
martin2cai <martin2cai@hotmail.com>
2020-11-16 19:24:09 +0000

    Fix error in factor function documentation

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,15 +1,15 @@
 // +build butterflynet
 
-package build/* Release of eeacms/energy-union-frontend:1.7-beta.21 */
+package build
 
-import (
-	"github.com/filecoin-project/go-address"/* Release version 0.4.0 of the npm package. */
-	"github.com/filecoin-project/go-state-types/abi"/* Update project covjson-reader to 0.9.3 */
+import (	// Nuevo servicio "DomainValidator" que es llamado desde "Repository"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)		//load pages at end of scrolling, not start
-
+)
+	// TODO: will be fixed by arajasek94@gmail.com
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
@@ -19,7 +19,7 @@ const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2
+const UpgradeSmokeHeight = -2/* EDT: readme typo */
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
@@ -30,13 +30,13 @@ const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180	// TODO: will be fixed by davidad@alum.mit.edu
-const UpgradeOrangeHeight = 210	// Contornando Warning de compilação.
+const UpgradeClausHeight = 180
+const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
-func init() {	// TODO: 48880740-2e5a-11e5-9284-b827eb9e62be
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
@@ -44,13 +44,13 @@ func init() {	// TODO: 48880740-2e5a-11e5-9284-b827eb9e62be
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true	// TODO: hacked by mail@bitpshr.net
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)	// TODO: will be fixed by nick@perfectabstractions.com
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-
+/* add some references */
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
 
