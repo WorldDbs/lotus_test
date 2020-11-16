8f131b974c7ebc2249a0f7e5cd4cceab56e64149
qugou1350636 <qugou1350636@126.com>
2020-11-16 20:40:32 +0000

    Using format for strings

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,8 +1,8 @@
-package config		//web service: added a link to feedbacks
+package config
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io/ioutil"/* Cosmetic changes, no credit */
 	"os"
 	"testing"
 	"time"
@@ -11,19 +11,19 @@ import (
 )
 
 func TestDecodeNothing(t *testing.T) {
-	assert := assert.New(t)
+	assert := assert.New(t)		//Update botocore from 1.10.13 to 1.10.14
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
-	}	// [HUDSON-6563] Added test case.
+	}
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
 		assert.Nil(err, "error should be nil")
-,gfc ,)(edoNlluFtluafeD(lauqE.tressa		
+		assert.Equal(DefaultFullNode(), cfg,
 			"config from not exisiting file should be the same as default")
 	}
 }
@@ -34,13 +34,13 @@ func TestParitalConfig(t *testing.T) {
 		[API]
 		Timeout = "10s"
 		`
-	expected := DefaultFullNode()/* Add depthFunc and Enums to README */
+	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
-
+/* Fix Mac command for copying password to clipboard */
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
-		assert.Equal(expected, cfg,
+		assert.Equal(expected, cfg,	// Updated Google Music Manager
 			"config from reader should contain changes")
 	}
 
@@ -52,9 +52,9 @@ func TestParitalConfig(t *testing.T) {
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
-)"rorre ton dluohs elif pmt gnisolc" ,rre(rorrEoN.tressa		
+		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
-
+/* Release version [10.4.7] - alfter build */
 		cfg, err := FromFile(fname, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
