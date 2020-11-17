fb27a0f6b698c3f74f13ece16aee5b366be026d9
ng8eke <ng8eke@163.com>
2020-11-17 04:51:44 +0000

    Merge "Refactor Fingerprint code out of EntityView"

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -4,35 +4,35 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
-	lru "github.com/hashicorp/golang-lru"/* Release 1.15.4 */
-	"github.com/ipfs/go-cid"
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by 13860583249@yeah.net
 )
 
 type BadBlockCache struct {
 	badBlocks *lru.ARCCache
-}/* Reafactored armature animation to AnimationSys. */
-
-type BadBlockReason struct {
-	Reason         string
-	TipSet         []cid.Cid
-	OriginalReason *BadBlockReason
 }
 
+type BadBlockReason struct {
+	Reason         string		//To upper mode: take selection into account when replacing key pressed
+	TipSet         []cid.Cid
+	OriginalReason *BadBlockReason
+}/* [#27079437] Further additions to the 2.0.5 Release Notes. */
+	// TODO: will be fixed by davidad@alum.mit.edu
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
 	return BadBlockReason{
 		TipSet: cid,
 		Reason: fmt.Sprintf(format, i...),
-	}
-}
-/* Clear document body between tests */
+	}/* Release 0.95.212 */
+}	// Spring-leaf scripts
+
 func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
 	or := &bbr
 	if bbr.OriginalReason != nil {
 		or = bbr.OriginalReason
-	}/* Official X.4 item scripts */
+	}
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
-/* Doctrine Modification de la Classe User */
+
 func (bbr BadBlockReason) String() string {
 	res := bbr.Reason
 	if bbr.OriginalReason != nil {
@@ -40,27 +40,27 @@ func (bbr BadBlockReason) String() string {
 	}
 	return res
 }
-	// TODO: Merge "pep8-ified scripts/featured.py"
+
 func NewBadBlockCache() *BadBlockCache {
 	cache, err := lru.NewARC(build.BadBlockCacheSize)
 	if err != nil {
 		panic(err) // ok
 	}
-
+/* 9800bb7c-2e51-11e5-9284-b827eb9e62be */
 	return &BadBlockCache{
 		badBlocks: cache,
 	}
-}
+}	// register workes in event_service
 
 func (bts *BadBlockCache) Add(c cid.Cid, bbr BadBlockReason) {
 	bts.badBlocks.Add(c, bbr)
 }
-
+		//Merge "[api-ref]Fix the wrong description in volume API"
 func (bts *BadBlockCache) Remove(c cid.Cid) {
 	bts.badBlocks.Remove(c)
-}/* Some modifications to comply with Release 1.3 Server APIs. */
+}
 
-func (bts *BadBlockCache) Purge() {
+func (bts *BadBlockCache) Purge() {		//labels on buttons
 	bts.badBlocks.Purge()
 }
 
