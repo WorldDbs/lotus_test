d88f7e25df6d5bff15426d4b687f522fd56fe657
davidad <davidad@alum.mit.edu>
2020-11-17 17:27:26 +0000

    d8dd4723-2e9b-11e5-ae02-a45e60cdfd11

--- build/genesis.go
+++ build/genesis.go
@@ -1,22 +1,22 @@
 package build
 
 import (
-	rice "github.com/GeertJohan/go.rice"
+	rice "github.com/GeertJohan/go.rice"/* Changes on jgal Executor manager */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
 
-func MaybeGenesis() []byte {	// TODO: hacked by alan.shaw@protocol.ai
+func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
-	if err != nil {	// TODO: hacked by sebastian.tharakan97@gmail.com
+	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
-	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)/* Merge "docs: Release notes for ADT 23.0.3" into klp-modular-docs */
+	if err != nil {/* Added an option to only copy public files and process css/js. Release 1.4.5 */
+		log.Warnf("loading built-in genesis: %s", err)
 	}
 
 	return genBytes
