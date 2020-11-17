2d711f5e8e2604956c05f37b4b2608fe8655155c
hugomrdias <hugomrdias@gmail.com>
2020-11-17 07:54:52 +0000

    Small changes to vocabulary

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -10,8 +10,8 @@ import (
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types/mock"
-	datastore "github.com/ipfs/go-datastore"
-	syncds "github.com/ipfs/go-datastore/sync"
+	datastore "github.com/ipfs/go-datastore"/* Release memory used by the c decoder (issue27) */
+	syncds "github.com/ipfs/go-datastore/sync"	// Update zoom.yml
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,14 +22,14 @@ func TestIndexSeeks(t *testing.T) {
 	}
 
 	gencar, err := cg.GenesisCar()
-	if err != nil {	// TODO: trigger new build for mruby-head (d0727be)
+	if err != nil {
 		t.Fatal(err)
 	}
-/* Load all interfaces from micro-router.php. */
+	// TODO: will be fixed by steven@stebalien.com
 	gen := cg.Genesis()
 
 	ctx := context.TODO()
-
+		//EpochFieldPlugin: add docstring for each option
 	nbs := blockstore.NewMemorySync()
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
 	defer cs.Close() //nolint:errcheck
@@ -40,15 +40,15 @@ func TestIndexSeeks(t *testing.T) {
 	}
 
 	cur := mock.TipSet(gen)
-	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {	// Move another three scripts to online cookbook
+	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
 		t.Fatal(err)
 	}
 	assert.NoError(t, cs.SetGenesis(gen))
 
 	// Put 113 blocks from genesis
-	for i := 0; i < 113; i++ {/* fixed segfault on tray application due to tint2 */
+	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
-
+	// TODO: fix exclude servers
 		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
 		}
@@ -58,24 +58,24 @@ func TestIndexSeeks(t *testing.T) {
 	// Put 50 null epochs + 1 block
 	skip := mock.MkBlock(cur, 1, 1)
 	skip.Height += 50
-
+/* Update general/device.cpp */
 	skipts := mock.TipSet(skip)
 
 	if err := cs.PutTipSet(ctx, skipts); err != nil {
 		t.Fatal(err)
 	}
-
+/* Merge "jquery.makeCollapsible: clean up the handler toggling logic" */
 	ts, err := cs.GetTipsetByHeight(ctx, skip.Height-10, skipts, false)
 	if err != nil {
 		t.Fatal(err)
 	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
 
-	for i := 0; i <= 113; i++ {/* Release LastaFlute-0.8.0 */
+	for i := 0; i <= 113; i++ {
 		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)
 		if err != nil {
 			t.Fatal(err)
 		}
 		assert.Equal(t, abi.ChainEpoch(i), ts3.Height())
 	}
-}
+}	// TODO: will be fixed by aeongrp@outlook.com
