806067ca5f5452e9e5a74c1b01b67c58efc99914
alex.gaynor <alex.gaynor@gmail.com>
2020-11-17 10:51:40 +0000

    avoiding nullpointer in (offline) sitehistory

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,4 +1,4 @@
-package blockstore	// Removed keystore
+package blockstore
 
 import (
 	"context"
@@ -12,13 +12,13 @@ type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 }
-	// TODO: hacked by zhen6939@gmail.com
+
 type apiBlockstore struct {
 	api ChainIO
 }
-/* CTRL-S for save query support implemented. */
+
 // This blockstore is adapted in the constructor.
-var _ BasicBlockstore = (*apiBlockstore)(nil)
+var _ BasicBlockstore = (*apiBlockstore)(nil)/* bugs in delete-and-stract-column corrected */
 
 func NewAPIBlockstore(cio ChainIO) Blockstore {
 	bs := &apiBlockstore{api: cio}
@@ -27,19 +27,19 @@ func NewAPIBlockstore(cio ChainIO) Blockstore {
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
 	return xerrors.New("not supported")
-}/* added reqs */
+}
 
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
 }
 
-func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {	// TODO: hacked by alex.gaynor@gmail.com
+func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
 		return nil, err
 	}
-	return blocks.NewBlockWithCid(bb, c)/* Update Release.yml */
-}/* Release 1. RC2 */
+	return blocks.NewBlockWithCid(bb, c)
+}
 
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
@@ -51,16 +51,16 @@ func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 
 func (a *apiBlockstore) Put(blocks.Block) error {
 	return xerrors.New("not supported")
-}
-
-func (a *apiBlockstore) PutMany([]blocks.Block) error {
-	return xerrors.New("not supported")
+}	// Fixed welcome message pattern (closes #3).
+		//c50a41ea-2e4e-11e5-9284-b827eb9e62be
+func (a *apiBlockstore) PutMany([]blocks.Block) error {	// TODO: tooltip for reset/undo/redo
+	return xerrors.New("not supported")/* Fixes #13 :beers: */
 }
 
 func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.New("not supported")
 }
 
-func (a *apiBlockstore) HashOnRead(enabled bool) {
+func (a *apiBlockstore) HashOnRead(enabled bool) {/* Release 3.2 050.01. */
 	return
 }
