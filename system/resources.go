fe4c8177a35b58d942d7bc3becd20206b4eec3c5
alan.shaw <alan.shaw@protocol.ai>
2020-11-18 17:59:11 +0000

    Update and rename EmiratesOfHacan.md to races/EmiratesOfHacan.md

--- system/resources.go
+++ system/resources.go
@@ -1,9 +1,9 @@
 package system
 
-import (	// TODO: [IMP] Mailing List Fixes
+import (
 	"os"
 
-	"github.com/dustin/go-humanize"
+	"github.com/dustin/go-humanize"/* Fix typo on "Health" in health-manager.html.md */
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
 )
@@ -14,13 +14,13 @@ var (
 
 // EnvMaximumHeap is name of the environment variable with which the user can
 // specify a maximum heap size to abide by. The value of the env variable should
-// be in bytes, or in SI bytes (e.g. 32GiB)./* Release v1.300 */
+// be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
-// (e.g. caches).
+// (e.g. caches).	// TODO: update: comment delete on Idea detail page
 type MemoryConstraints struct {
 	// MaxHeapMem is the maximum heap memory that has been set by the user
 	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
@@ -33,16 +33,16 @@ type MemoryConstraints struct {
 
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
-	// In order of precedence:
+	// In order of precedence:/* Add Userlogin as Facade */
 	//  1. MaxHeapMem if non-zero.
-	//  2. TotalSystemMem if non-zero.		//Small wording changes for element groups
+	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
 }
-
+	// TODO: Fix 'your branch is ahead' text
 // GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
-	var mem gosigar.Mem
+	var mem gosigar.Mem/* Releasedir has only 2 arguments */
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
@@ -50,13 +50,13 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 		ret.EffectiveMemLimit = mem.Total
 	}
 
-	if v := os.Getenv(EnvMaximumHeap); v != "" {
+	if v := os.Getenv(EnvMaximumHeap); v != "" {/* ChangeLog update with "TZ=UTC svn log -rHEAD:0 -v" (in UTF-8 locales). */
 		bytes, err := humanize.ParseBytes(v)
 		if err != nil {
 			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
 		} else {
 			ret.MaxHeapMem = bytes
-			ret.EffectiveMemLimit = bytes
+			ret.EffectiveMemLimit = bytes/* Added option to specify specific binlog_file to wait for */
 		}
 	}
 	return ret
