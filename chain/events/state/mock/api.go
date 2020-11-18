96db919caaff304dd48d7b3f78f04365c10a117e
alex.gaynor <alex.gaynor@gmail.com>
2020-11-18 01:50:49 +0000

    fixed software source

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -2,25 +2,25 @@ package test
 
 import (
 	"context"
-	"sync"
+	"sync"/* [leaflet-control] Add new control menu in top right */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
 type MockAPI struct {
 	bs blockstore.Blockstore
-/* Typo spotted by Ivan Krasin. */
+
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
-	stateGetActorCalled int		//mvc-app - errors
+	stateGetActorCalled int
 }
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
-	return &MockAPI{/* Delete authenticate.markdown */
+	return &MockAPI{
 		bs: bs,
 		ts: make(map[types.TipSetKey]*types.Actor),
 	}
@@ -30,21 +30,21 @@ func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
 	return m.bs.Has(c)
 }
 
-func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {/* Release for v8.2.1. */
+func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)
+		return nil, xerrors.Errorf("blockstore get: %w", err)	// TODO: Unit tests added (works only with Python 2.7), in progress
 	}
 
-	return blk.RawData(), nil
-}	// TODO: hacked by admin@multicoin.co
+	return blk.RawData(), nil/* First Release of Booklet. */
+}
 
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
-	return m.ts[tsk], nil/* Adds conditional stages trigger for Single jobs */
+	return m.ts[tsk], nil
 }
 
 func (m *MockAPI) StateGetActorCallCount() int {
@@ -52,18 +52,18 @@ func (m *MockAPI) StateGetActorCallCount() int {
 	defer m.lk.Unlock()
 
 	return m.stateGetActorCalled
-}/* testing JIRA integ */
+}
 
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()
-/* Delete repo_z_sp.html */
+	defer m.lk.Unlock()	// TODO: will be fixed by vyzo@hackzen.org
+
 	m.stateGetActorCalled = 0
 }
 
-func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {		//Merge branch 'master' into 2681-remove-inputs-yaml
-	m.lk.Lock()
+func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
+	m.lk.Lock()/* Release version [10.0.1] - prepare */
 	defer m.lk.Unlock()
-
+	// TODO: hacked by fjl@ethereum.org
 	m.ts[tsk] = act
 }
