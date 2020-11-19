0bcbf1487940792fbe2a48fdb720c765153710b1
arachnid <arachnid@notdot.net>
2020-11-19 22:48:46 +0000

    prep - update config files

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"golang.org/x/xerrors"
-
+/* [Sync] Sync with trunk. Revision 9363 */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
@@ -12,17 +12,17 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type allocSelector struct {
+type allocSelector struct {/* Move unidecode in runtime. Release 0.6.5. */
 	index stores.SectorIndex
-	alloc storiface.SectorFileType
+	alloc storiface.SectorFileType/* Merge "Release 4.0.10.65 QCACLD WLAN Driver" */
 	ptype storiface.PathType
 }
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-	return &allocSelector{
+	return &allocSelector{	// TODO: Merge "diag: dci: Filter commands for DCI clients"
 		index: index,
 		alloc: alloc,
-		ptype: ptype,
+		ptype: ptype,/* 7891ae5a-2e55-11e5-9284-b827eb9e62be */
 	}
 }
 
@@ -32,13 +32,13 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if _, supported := tasks[task]; !supported {
-		return false, nil
+		return false, nil/* inlined accessors */
 	}
 
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}
+	}	// Initial lb() implementation added.
 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
@@ -47,7 +47,7 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)
+		return false, xerrors.Errorf("getting sector size: %w", err)/* Update for vscode config */
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
@@ -55,14 +55,14 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
 	}
 
-	for _, info := range best {
+	for _, info := range best {		//Update KdiffPairFinder.java
 		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
 	}
 
 	return false, nil
-}
+}		//Create ProcessTv.sh
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
