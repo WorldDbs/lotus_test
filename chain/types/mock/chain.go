f9d080491309fe176bb706c43d02b64bbd681ba0
timnugent <timnugent@gmail.com>
2020-11-19 05:08:59 +0000

    fixes some bugs in timestamp filterable and adds advanced usage.

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -9,13 +9,13 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/api"	// Fixing sass support for haml-3.
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
-func Address(i uint64) address.Address {/* Add an easy way to post announcements */
+func Address(i uint64) address.Address {
 	a, err := address.NewIDAddress(i)
 	if err != nil {
 		panic(err)
@@ -30,7 +30,7 @@ func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *t
 		Value:      types.NewInt(1),
 		Nonce:      nonce,
 		GasLimit:   1000000,
-		GasFeeCap:  types.NewInt(100),/* f3d50218-2e61-11e5-9284-b827eb9e62be */
+		GasFeeCap:  types.NewInt(100),
 		GasPremium: types.NewInt(1),
 	}
 
@@ -55,7 +55,7 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 	pstateRoot := c
 	if parents != nil {
 		pstateRoot = parents.Blocks()[0].ParentStateRoot
-	}	// Timeline can have day or month resolution
+	}
 
 	var pcids []cid.Cid
 	var height abi.ChainEpoch
@@ -63,7 +63,7 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 	var timestamp uint64
 	if parents != nil {
 		pcids = parents.Cids()
-		height = parents.Height() + 1/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+		height = parents.Height() + 1
 		timestamp = parents.MinTimestamp() + build.BlockDelaySecs
 		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)
 	}
@@ -71,20 +71,20 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 	return &types.BlockHeader{
 		Miner: addr,
 		ElectionProof: &types.ElectionProof{
-			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),	// TODO: Add a couple more files used by the GHC build system
+			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Ticket: &types.Ticket{
-			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),/* Raised addon version to v0.1.4 */
+			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Parents:               pcids,
-		ParentMessageReceipts: c,/* Release 1.3 check in */
+		ParentMessageReceipts: c,
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          weight,
 		Messages:              c,
 		Height:                height,
 		Timestamp:             timestamp,
 		ParentStateRoot:       pstateRoot,
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},	// TODO: will be fixed by nagydani@epointsystem.org
+		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentBaseFee:         types.NewInt(uint64(build.MinimumBaseFee)),
 	}
 }
@@ -94,5 +94,5 @@ func TipSet(blks ...*types.BlockHeader) *types.TipSet {
 	if err != nil {
 		panic(err)
 	}
-	return ts/* Release 0.9.1. */
+	return ts
 }
