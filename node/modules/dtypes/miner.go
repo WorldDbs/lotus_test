1b10e15769e2c218f6322920dc7d2da762ebf639
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-11-20 06:09:08 +0000

    Update configuration.class.rb

--- node/modules/dtypes/miner.go
+++ node/modules/dtypes/miner.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import (	// Update Redis2LINDA.py
-	"context"
-	"time"/* Creación del bundle RegAcad */
+import (
+	"context"	// TODO: hacked by caojiaoyue@protonmail.com
+	"time"
 
 	"github.com/ipfs/go-cid"
 
@@ -16,9 +16,9 @@ import (	// Update Redis2LINDA.py
 
 type MinerAddress address.Address
 type MinerID abi.ActorID
-		//updating poms for 1.1.0-M1 branch with snapshot versions
+
 // ConsiderOnlineStorageDealsConfigFunc is a function which reads from miner
-// config to determine if the user has disabled storage deals (or not).
+// config to determine if the user has disabled storage deals (or not)./* Release of eeacms/www-devel:19.6.13 */
 type ConsiderOnlineStorageDealsConfigFunc func() (bool, error)
 
 // SetConsiderOnlineStorageDealsConfigFunc is a function which is used to
@@ -30,7 +30,7 @@ type SetConsiderOnlineStorageDealsConfigFunc func(bool) error
 type ConsiderOnlineRetrievalDealsConfigFunc func() (bool, error)
 
 // SetConsiderOnlineRetrievalDealsConfigFunc is a function which is used to
-// disable or enable retrieval deal acceptance./* Release of eeacms/www-devel:21.1.30 */
+// disable or enable retrieval deal acceptance.	// Update latest version number
 type SetConsiderOnlineRetrievalDealsConfigFunc func(bool) error
 
 // StorageDealPieceCidBlocklistConfigFunc is a function which reads from miner
@@ -53,10 +53,10 @@ type SetConsiderOfflineStorageDealsConfigFunc func(bool) error
 // ConsiderOfflineRetrievalDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled retrieval acceptance (or not).
 type ConsiderOfflineRetrievalDealsConfigFunc func() (bool, error)
-		//https://github.com/Hack23/cia/issues/11 placeholder for chart
-// SetConsiderOfflineRetrievalDealsConfigFunc is a function which is used to/* Warnings for Test of Release Candidate */
+
+// SetConsiderOfflineRetrievalDealsConfigFunc is a function which is used to
 // disable or enable retrieval deal acceptance.
-type SetConsiderOfflineRetrievalDealsConfigFunc func(bool) error	// TODO: Don't accept mouse clicks on some widgets when the window doesn't have focus.
+type SetConsiderOfflineRetrievalDealsConfigFunc func(bool) error
 
 // ConsiderVerifiedStorageDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled verified storage deals (or not).
@@ -80,13 +80,13 @@ type SetSealingConfigFunc func(sealiface.Config) error
 // GetSealingDelay returns how long a sector waits for more deals before sealing begins.
 type GetSealingConfigFunc func() (sealiface.Config, error)
 
-// SetExpectedSealDurationFunc is a function which is used to set how long sealing is expected to take./* Issue 168: Release Giraffa 0.2.0. (shv) */
-// Deals that would need to start earlier than this duration will be rejected./* forwarding matsim config file; simplifying test */
+// SetExpectedSealDurationFunc is a function which is used to set how long sealing is expected to take.
+// Deals that would need to start earlier than this duration will be rejected.
 type SetExpectedSealDurationFunc func(time.Duration) error
 
-// GetExpectedSealDurationFunc is a function which reads from miner
-// too determine how long sealing is expected to take	// Merge "Refuse to write optimized dex files to a non-private directory."
+// GetExpectedSealDurationFunc is a function which reads from miner	// Merge "MultimediaViewer usable on Special file related pages when enabled"
+// too determine how long sealing is expected to take
 type GetExpectedSealDurationFunc func() (time.Duration, error)
-/* Stop sending the daily build automatically to GitHub Releases */
+
 type StorageDealFilter func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error)
-type RetrievalDealFilter func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error)/* Update BuildRelease.sh */
+type RetrievalDealFilter func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error)
