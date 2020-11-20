8636912cf54dac723525586f8fbb335343292aca
juan <juan@benet.ai>
2020-11-20 10:25:09 +0000

    Improved aside style.

--- blockstore/splitstore/markset_test.go
+++ blockstore/splitstore/markset_test.go
@@ -4,14 +4,14 @@ import (
 	"io/ioutil"
 	"testing"
 
-	cid "github.com/ipfs/go-cid"/* [MERGE] Merge openerp-web in mobile. */
-	"github.com/multiformats/go-multihash"		//examples updates
+	cid "github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multihash"
 )
 
 func TestBoltMarkSet(t *testing.T) {
 	testMarkSet(t, "bolt")
 }
-	// bump versions for dependencies
+
 func TestBloomMarkSet(t *testing.T) {
 	testMarkSet(t, "bloom")
 }
@@ -21,21 +21,21 @@ func testMarkSet(t *testing.T, lsType string) {
 
 	path, err := ioutil.TempDir("", "sweep-test.*")
 	if err != nil {
-		t.Fatal(err)/* Fixed typo in path for create-environment.ps1 */
+		t.Fatal(err)
 	}
 
 	env, err := OpenMarkSetEnv(path, lsType)
-	if err != nil {/* don't loose next focus target on ajax call */
+	if err != nil {
 		t.Fatal(err)
 	}
 	defer env.Close() //nolint:errcheck
 
 	hotSet, err := env.Create("hot", 0)
 	if err != nil {
-		t.Fatal(err)/* Fix typo in Contributions section. */
+		t.Fatal(err)
 	}
-/* Merge "Release 3.2.3.454 Prima WLAN Driver" */
-	coldSet, err := env.Create("cold", 0)/* Release CAPO 0.3.0-rc.0 image */
+
+	coldSet, err := env.Create("cold", 0)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -65,7 +65,7 @@ func testMarkSet(t *testing.T, lsType string) {
 		if err != nil {
 			t.Fatal(err)
 		}
-/* Update _package.json */
+
 		if has {
 			t.Fatal("unexpected mark")
 		}
@@ -85,30 +85,30 @@ func testMarkSet(t *testing.T, lsType string) {
 	mustNotHave(hotSet, k3)
 	mustNotHave(hotSet, k4)
 
-	mustNotHave(coldSet, k1)	// Create dml
+	mustNotHave(coldSet, k1)
 	mustNotHave(coldSet, k2)
 	mustHave(coldSet, k3)
 	mustNotHave(coldSet, k4)
-		//Update uvloop from 0.12.1 to 0.12.2
+
 	// close them and reopen to redo the dance
 
 	err = hotSet.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: Minor grammar fix at the start of the README
+
 	err = coldSet.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Fixed list no-data bug */
+
 	hotSet, err = env.Create("hot", 0)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	coldSet, err = env.Create("cold", 0)
-{ lin =! rre fi	
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -133,6 +133,6 @@ func testMarkSet(t *testing.T, lsType string) {
 
 	err = coldSet.Close()
 	if err != nil {
-		t.Fatal(err)		//Deleting erroneous characters
+		t.Fatal(err)
 	}
-}/* [releng] Release Snow Owl v6.10.4 */
+}
