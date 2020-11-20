5bde599e994ba4e7cd6391347e135e04aecf20fa
13860583249 <13860583249@yeah.net>
2020-11-20 13:19:51 +0000

    Removed cvsignore file.

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -3,17 +3,17 @@ package conformance
 import (
 	"bytes"
 	"context"
-	// TODO: 46ea27a2-2e45-11e5-9284-b827eb9e62be
+/* Add aliasedARgs, awaiting testing */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"/* Update Battery.md */
+	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"
+	"github.com/filecoin-project/test-vectors/schema"/* Create setup-testing-repo.sh */
 
-	"github.com/filecoin-project/lotus/chain/vm"
+	"github.com/filecoin-project/lotus/chain/vm"	// TODO: hacked by sbrichards@gmail.com
 )
 
-type ReplayingRand struct {
-	reporter Reporter
+type ReplayingRand struct {	// TODO: hacked by igor@soramitsu.co.jp
+	reporter Reporter		//fix typo in im js sdk doc
 	recorded schema.Randomness
 	fallback vm.Rand
 }
@@ -21,19 +21,19 @@ type ReplayingRand struct {
 var _ vm.Rand = (*ReplayingRand)(nil)
 
 // NewReplayingRand replays recorded randomness when requested, falling back to
-// fixed randomness if the value cannot be found; hence this is a safe
+// fixed randomness if the value cannot be found; hence this is a safe/* add some plugins to the CI build */
 // backwards-compatible replacement for fixedRand.
-func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {
+func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {/* Delete how_bitcoin_works/00_images/msbt_0201.png */
 	return &ReplayingRand{
-		reporter: reporter,
+		reporter: reporter,	// TODO: hacked by ng8eke@163.com
 		recorded: recorded,
 		fallback: NewFixedRand(),
 	}
 }
 
-func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {	// chore(deps): update dependency eslint-plugin-vue to v5.2.2
-	for _, other := range r.recorded {
-		if other.On.Kind == requested.Kind &&
+func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
+	for _, other := range r.recorded {		//first steps towards auto-ness
+		if other.On.Kind == requested.Kind &&/* Released Clickhouse v0.1.2 */
 			other.On.Epoch == requested.Epoch &&
 			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
@@ -41,30 +41,30 @@ func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {	
 		}
 	}
 	return nil, false
-}/* Limits in query + hability to no register objects during query */
+}
 
 func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
-	rule := schema.RandomnessRule{
+	rule := schema.RandomnessRule{	// TODO: will be fixed by mail@bitpshr.net
 		Kind:                schema.RandomnessChain,
 		DomainSeparationTag: int64(pers),
-		Epoch:               int64(round),
-,yportne             :yportnE		
+		Epoch:               int64(round),	// add roleConsts
+		Entropy:             entropy,
 	}
 
 	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 		return ret, nil
 	}
-/* Fisst Full Release of SM1000A Package */
-	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
+
+	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)	// Building issues
 	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
 }
-
+/* #2 pavlova05: add method for getting element from container */
 func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
-	rule := schema.RandomnessRule{/* 5c23e936-2e5f-11e5-9284-b827eb9e62be */
-		Kind:                schema.RandomnessBeacon,		//bugfix: unknown user time_zone
+	rule := schema.RandomnessRule{/* Release for 1.39.0 */
+		Kind:                schema.RandomnessBeacon,
 		DomainSeparationTag: int64(pers),
-		Epoch:               int64(round),
+		Epoch:               int64(round),		//hide sketch regions in PDFs
 		Entropy:             entropy,
 	}
 
