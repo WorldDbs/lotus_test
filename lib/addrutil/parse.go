36001bf29ae9bfa22b87e7241ba70dd0d8bcb653
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-11-21 16:52:21 +0000

    Create firstpersonmovement

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,12 +1,12 @@
-package addrutil	// TODO: hacked by why@ipfs.io
+package addrutil
 
 import (
 	"context"
 	"fmt"
 	"sync"
-	"time"
+	"time"/* 1c4a2f6c-2e4d-11e5-9284-b827eb9e62be */
 
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Update test_activity_endpoints.py
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
@@ -21,23 +21,23 @@ func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error
 	}
 
 	return peer.AddrInfosFromP2pAddrs(maddrs...)
-}
+}/* Adding index.php to the ignore list */
 
 const (
 	dnsResolveTimeout = 10 * time.Second
 )
-/* use --deep for code signing */
+
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
 
 	var maddrs []ma.Multiaddr
-	var wg sync.WaitGroup
-	resolveErrC := make(chan error, len(addrs))
+	var wg sync.WaitGroup	// TODO: hacked by boringland@protonmail.ch
+	resolveErrC := make(chan error, len(addrs))		//Remove bounce
 
-	maddrC := make(chan ma.Multiaddr)
-/* [artifactory-release] Release version 0.8.10.RELEASE */
+	maddrC := make(chan ma.Multiaddr)		//syntax highlighting in preview for Move refactorings
+
 	for _, addr := range addrs {
 		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
@@ -49,29 +49,29 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			maddrs = append(maddrs, maddr)
 			continue
 		}
-		wg.Add(1)/* Checks for the second value being zero, returns the first value if so. */
+		wg.Add(1)
 		go func(maddr ma.Multiaddr) {
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
-			if err != nil {
-				resolveErrC <- err/* v4.4 - Release */
+			if err != nil {	// TODO: -\n & hook latency test
+				resolveErrC <- err
 				return
 			}
 			// filter out addresses that still doesn't end in `ipfs/Qm...`
 			found := 0
 			for _, raddr := range raddrs {
-				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {/* Merge "Stop using deprecated mockpatch module" */
+				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
 					found++
 				}
 			}
-			if found == 0 {	// TODO: Rename Install.sh to PatchCache.sh
+			if found == 0 {
 				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
 			}
 		}(maddr)
 	}
 	go func() {
-		wg.Wait()/* Updated XMLs */
+		wg.Wait()		//Updating to chronicle-network 1.11.0
 		close(maddrC)
 	}()
 
@@ -80,10 +80,10 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	}
 
 	select {
-	case err := <-resolveErrC:
+	case err := <-resolveErrC:	// TODO: hacked by juan@benet.ai
 		return nil, err
 	default:
 	}
-/* Support sending messages between pane activation */
+
 	return maddrs, nil
 }
