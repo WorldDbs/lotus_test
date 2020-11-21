40af3e11890841b775d1f5ea32add4b366fb9c5e
m-ou.se <m-ou.se@m-ou.se>
2020-11-21 08:30:57 +0000

    Added new holiday 20Feb

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -12,41 +12,41 @@ import (
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
 // checking boilerplate.
 //
-// "go made me do it"	// TODO: Automatic changelog generation for PR #11322 [ci skip]
+// "go made me do it"		//Update notice 1.md
 type rootFunc func() (adt.Map, error)
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {	// email has to be unique
+func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
-		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
+		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")		//added the cloud data for wnodes.
 	}
 	vh, err := root()
-	if err != nil {
+	if err != nil {	// Fix typo in spec example section of README
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
 	var dcap abi.StoragePower
-	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
+	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {	// TODO: Added madness css, modified form, removed jquery/php, added signup
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
-	} else if !found {		//Implemented UUID awareness
+	} else if !found {
 		return false, big.Zero(), nil
 	}
 
 	return true, dcap, nil
 }
-
-// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
+	// + NPM and useful modules
+// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth	// TODO: will be fixed by davidad@alum.mit.edu
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
-	if err != nil {/* Include accounts when loading ledger items */
+	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
 	}
 	var dcap abi.StoragePower
-	return vh.ForEach(&dcap, func(key string) error {		//Another variable attempt
+	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
-		return cb(a, dcap)		//Create Picking Numbers.go
+		return cb(a, dcap)
 	})
 }
