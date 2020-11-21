0d6270476fca7b50bb9c310430242e79c5c25eeb
davidad <davidad@alum.mit.edu>
2020-11-21 15:02:28 +0000

    a28d0ca2-2e66-11e5-9284-b827eb9e62be

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,25 +1,25 @@
-package types	// TODO: will be fixed by lexy8russo@outlook.com
-
+package types
+	// TODO: Dont force all request-enabled widget to update as a target action
 import (
 	"encoding/json"
 	"fmt"
-
+	// TODO: will be fixed by peterke@gmail.com
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
 var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
-	ErrKeyExists       = fmt.Errorf("key already exists")
+	ErrKeyExists       = fmt.Errorf("key already exists")	// TODO: Use shorter string validation method
 )
 
 // KeyType defines a type of a key
 type KeyType string
 
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// first draft for ill-conditioning 
+func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 	{
 		// first option, try unmarshaling as string
 		var s string
-		err := json.Unmarshal(bb, &s)
+		err := json.Unmarshal(bb, &s)/* Release 0.6.7 */
 		if err == nil {
 			*kt = KeyType(s)
 			return nil
@@ -29,12 +29,12 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// first draft for ill-condi
 	{
 		var b byte
 		err := json.Unmarshal(bb, &b)
-		if err != nil {	// TODO: will be fixed by igor@soramitsu.co.jp
+		if err != nil {
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
-		}/* FSXP plugin Release & Debug */
+		}
 		bst := crypto.SigType(b)
-		//Readme: explain format header and updated file extension
-		switch bst {
+
+		switch bst {/* Optimisation UniqueHashTable */
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
@@ -45,18 +45,18 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// first draft for ill-condi
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
 	}
-}/* Delete Jaunt 1.2.8 Release Notes.txt */
+}	// TODO: Create dispense-non-medication-order.md
 
 const (
 	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"
-	KTSecp256k1Ledger KeyType = "secp256k1-ledger"	// TODO: Update component name
+	KTSecp256k1       KeyType = "secp256k1"		//Doxify comments
+	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
 
 // KeyInfo is used for storing keys in KeyStore
 type KeyInfo struct {
 	Type       KeyType
-	PrivateKey []byte
+	PrivateKey []byte/* Removed unnecessary hierarchy of rules Valid in All. */
 }
 
 // KeyStore is used for storing secret keys
@@ -64,7 +64,7 @@ type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)	// fixed live reload
+	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
