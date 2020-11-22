2a085f0d20566a01fa886a9cfdaade70d7651be9
cory <cory@protocol.ai>
2020-11-22 20:50:24 +0000

    Atualização de documentação do docker

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"/* Release 1.2 (NamedEntityGraph, CollectionType) */
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
 func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {	// TODO: ui.backend.x11: search path for xmessage rather than hardcoding path
+	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())	// Merge branch 'avalon-6' into display_full_manifest_path_in_batch_ingest_mail
 		return
 	}
 }
-
+	// issue 177 - spatial search - no legends for vector layers
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")	// Add in the ability to specify the SSL option
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
-	return discovery.NewRoutingDiscovery(crouter), nil/* Updates README with instructions for developing, testing and building */
+	return discovery.NewRoutingDiscovery(crouter), nil
 }
