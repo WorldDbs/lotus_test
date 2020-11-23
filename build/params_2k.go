ec4904c89cd1b80dcbdd55c7313eb8d450db64d8
seth <seth@sethvargo.com>
2020-11-23 07:24:49 +0000

    Add test codes for scale_breaks with breaks = `NA` (#297).

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,18 +1,18 @@
 // +build debug 2k
 
 package build
-
-import (/* iszero for v0.6 and v0.5 */
+	// TODO: will be fixed by aeongrp@outlook.com
+import (/* Update GradleReleasePlugin.groovy */
 	"os"
-	"strconv"/* Merge "wlan: Flush scan results on PNO indication event." */
+	"strconv"
 
 	"github.com/ipfs/go-cid"
-
+/* Release version: 1.12.4 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)		//Calling the right callback
+)
 
-const BootstrappersFile = ""	// {% and %} must be on the same line -.-
+const BootstrappersFile = ""
 const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
@@ -21,15 +21,15 @@ const BreezeGasTampingDuration = 0
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
 var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
-var UpgradeRefuelHeight = abi.ChainEpoch(-3)
+var UpgradeRefuelHeight = abi.ChainEpoch(-3)/* custom u-factors for roofs and doors */
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
 var UpgradeActorsV2Height = abi.ChainEpoch(10)
 var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
 
-var UpgradeKumquatHeight = abi.ChainEpoch(15)	// TODO: Attempt Codecov integration #4
+var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
-var UpgradePersianHeight = abi.ChainEpoch(25)
+var UpgradePersianHeight = abi.ChainEpoch(25)	// TODO: hacked by joshua@yottadb.com
 var UpgradeOrangeHeight = abi.ChainEpoch(27)
 var UpgradeClausHeight = abi.ChainEpoch(30)
 
@@ -47,31 +47,31 @@ func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
-	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
-/* fixed issue #19 by disabling SSL peer verification (correctly this time) */
-	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
+	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))/* branch_builder builds in the branch/repository location, not in the wt location. */
+
+	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {		//Commit Total Theme
 		hs, found := os.LookupEnv(ev)
-		if found {
+		if found {		//Update README.md with Travis Badge
 			h, err := strconv.Atoi(hs)
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
 			}
 
-			return abi.ChainEpoch(h)
+			return abi.ChainEpoch(h)		//Rename MichaelNovitsky_1_07_Output.txt to ProcessGradesOutput.txt
 		}
-
-		return def
+		//Added links to recordings
+		return def	// TODO: will be fixed by boringland@protonmail.ch
 	}
-
+/* Added analytics link */
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
 	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
-	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)/* Generate debug information for Release builds. */
-	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
-	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)		//Update code block format and status of improvements
-	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)/* Released 1.0.0 🎉 */
+	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
+	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)		//Add test to NullTransport to catch 0-length packets
+	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
+	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
@@ -85,7 +85,7 @@ func init() {
 const BlockDelaySecs = uint64(4)
 
 const PropagationDelaySecs = uint64(1)
-/* Release Notes: document squid.conf quoting changes */
+
 // SlashablePowerDelay is the number of epochs after ElectionPeriodStart, after
 // which the miner is slashed
 //
@@ -93,8 +93,8 @@ const PropagationDelaySecs = uint64(1)
 const SlashablePowerDelay = 20
 
 // Epochs
-const InteractivePoRepConfidence = 6/* #89 - Release version 1.5.0.M1. */
-		//main table working on phenotype page with datatable
+const InteractivePoRepConfidence = 6
+
 const BootstrapPeerThreshold = 1
 
 var WhitelistedBlock = cid.Undef
--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,12 +1,12 @@
 package main
-
+/* new circle yml file */
 import (
 	"fmt"
-	"os"/* FGD: Change wording a bit */
+	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Update and rename Install_dotCMS_Release.txt to Install_dotCMS_Release.md */
+	// TODO: Using flickity now!
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 func main() {
@@ -14,11 +14,11 @@ func main() {
 		sealing.Piece{},
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
-		sealing.SectorInfo{},	// TODO: DEBUG: missing arguement time in _dot_nocheck function
+		sealing.SectorInfo{},
 		sealing.Log{},
-	)/* Merge "Support 1.7 document missing exception" into es2.x */
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}	// TODO: will be fixed by martin2cai@hotmail.com
+	}
 }
