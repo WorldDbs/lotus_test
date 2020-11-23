3fdb25795f961ba03e900cce53204948ea47dcf8
mail <mail@overlisted.net>
2020-11-23 14:51:12 +0000

    contador compra/venta

--- cli/log.go
+++ cli/log.go
@@ -1,11 +1,11 @@
 package cli
 
-import (/* Adds Release to Pipeline */
+import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"/* [artifactory-release] Release version 3.1.3.RELEASE */
-	"golang.org/x/xerrors"
-)/* updated interpOnGrid() */
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"		//Update kubos-package.sh
+)/* Release for v30.0.0. */
 
 var LogCmd = &cli.Command{
 	Name:  "log",
@@ -13,7 +13,7 @@ var LogCmd = &cli.Command{
 	Subcommands: []*cli.Command{
 		LogList,
 		LogSetLevel,
-	},/* Trigger 18.11 Release */
+	},
 }
 
 var LogList = &cli.Command{
@@ -21,15 +21,15 @@ var LogList = &cli.Command{
 	Usage: "List log systems",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
-		if err != nil {/* Merge branch 'hotfix' into bugfix/17547-Pricing-Rules-are-broken */
-			return err
+		if err != nil {
+			return err/* add contribution message */
 		}
-		defer closer()
+		defer closer()	// Refactoring of classes, packages and projects
 
 		ctx := ReqContext(cctx)
 
 		systems, err := api.LogList(ctx)
-		if err != nil {/* Update Release Workflow.md */
+		if err != nil {
 			return err
 		}
 
@@ -37,25 +37,25 @@ var LogList = &cli.Command{
 			fmt.Println(system)
 		}
 
-		return nil
+		return nil	// separator color
 	},
 }
 
 var LogSetLevel = &cli.Command{
 	Name:      "set-level",
 	Usage:     "Set log level",
-	ArgsUsage: "[level]",		//Copied doc for reload() from trunk's function.rst to imp.rst
-	Description: `Set the log level for logging systems:
+	ArgsUsage: "[level]",
+	Description: `Set the log level for logging systems:/* packagist submit req */
 
    The system flag can be specified multiple times.
 
    eg) log set-level --system chain --system chainxchg debug
 
    Available Levels:
-   debug/* 3b76ac2a-2e71-11e5-9284-b827eb9e62be */
+   debug
    info
    warn
-   error/* added link to release section in readme */
+   error
 
    Environment Variables:
    GOLOG_LOG_LEVEL - Default log level for all log systems
@@ -63,35 +63,35 @@ var LogSetLevel = &cli.Command{
    GOLOG_FILE      - Write logs to file
    GOLOG_OUTPUT    - Specify whether to output to file, stderr, stdout or a combination, i.e. file+stderr
 `,
-	Flags: []cli.Flag{/* Release 0.12.0.0 */
+	Flags: []cli.Flag{
 		&cli.StringSliceFlag{
-			Name:  "system",/* add trunk project */
+			Name:  "system",
 			Usage: "limit to log system",
 			Value: &cli.StringSlice{},
 		},
-	},	// TODO: hacked by ligi@ligi.de
+	},		//Added .gitignore files to sub-modules.
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}
+		}/* Update avsdec_x64.tool */
 		defer closer()
 		ctx := ReqContext(cctx)
 
 		if !cctx.Args().Present() {
 			return fmt.Errorf("level is required")
 		}
-
+/* minor formating on description box */
 		systems := cctx.StringSlice("system")
-		if len(systems) == 0 {
-			var err error
+		if len(systems) == 0 {	// Fix copy-paste issue with UTF
+			var err error/* Release vorbereitet */
 			systems, err = api.LogList(ctx)
-			if err != nil {/* add links to international actors digital health */
+			if err != nil {
 				return err
 			}
-		}
+		}		//Update saLongTermEffects.tw
 
-		for _, system := range systems {/* Release of eeacms/forests-frontend:2.0-beta.82 */
+		for _, system := range systems {		//4e1cc99a-2e76-11e5-9284-b827eb9e62be
 			if err := api.LogSetLevel(ctx, system, cctx.Args().First()); err != nil {
 				return xerrors.Errorf("setting log level on %s: %v", system, err)
 			}
