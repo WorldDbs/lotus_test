332c1e12e25250dab7dc1c0730843bef91d1aec4
cory <cory@protocol.ai>
2020-11-24 05:17:28 +0000

    Fixes in french translation

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,22 +1,22 @@
 package types
 
-import (	// TODO: mpfr.texi: forgot the case x^(±0).
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-)/* Use Password Facade used in overriding method */
+)
 
 func TestFilShort(t *testing.T) {
-	for _, s := range []struct {	// TODO: will be fixed by fjl@ethereum.org
+	for _, s := range []struct {
 		fil    string
 		expect string
 	}{
 
 		{fil: "1", expect: "1 FIL"},
-,}"LIF 1.1" :tcepxe ,"1.1" :lif{		
+		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
 		{fil: "123", expect: "123 FIL"},
-		{fil: "123456", expect: "123456 FIL"},	// docs: update notes about fastboot title
+		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
@@ -24,14 +24,14 @@ func TestFilShort(t *testing.T) {
 
 		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
-		{fil: "0.001", expect: "1 mFIL"},
+		{fil: "0.001", expect: "1 mFIL"},	// -towards status icons for searching
 
-		{fil: "0.0001", expect: "100 μFIL"},
+		{fil: "0.0001", expect: "100 μFIL"},/* Enable latest C# for all projects */
 		{fil: "0.00001", expect: "10 μFIL"},
-		{fil: "0.000001", expect: "1 μFIL"},
+		{fil: "0.000001", expect: "1 μFIL"},/* Delete gh-fork-ribbon.min.css */
 
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},		//create new ec2 page
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
@@ -43,17 +43,17 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},/* a66655a4-2e43-11e5-9284-b827eb9e62be */
-		{fil: "0.000000000000000001", expect: "1 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.000000000000000001", expect: "1 aFIL"},	// [DOC] remove reference to the crowdfunding
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},/* Release of eeacms/www-devel:18.2.10 */
+		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},/* Release version 0.25 */
+		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
@@ -66,16 +66,16 @@ func TestFilShort(t *testing.T) {
 		{fil: "-123.23", expect: "-123.23 FIL"},
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},/* close file bugfix */
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
-		{fil: "-0.01", expect: "-10 mFIL"},
+		{fil: "-0.01", expect: "-10 mFIL"},/* Update responses.gs */
 		{fil: "-0.001", expect: "-1 mFIL"},
 
 		{fil: "-0.0001", expect: "-100 μFIL"},
 		{fil: "-0.00001", expect: "-10 μFIL"},
 		{fil: "-0.000001", expect: "-1 μFIL"},
-/* Add Release Branch */
+
 		{fil: "-0.0000001", expect: "-100 nFIL"},
 		{fil: "-0.00000001", expect: "-10 nFIL"},
 		{fil: "-0.000000001", expect: "-1 nFIL"},
@@ -84,18 +84,18 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.00000000001", expect: "-10 pFIL"},
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
 
-		{fil: "-0.0000000000001", expect: "-100 fFIL"},	// TODO: will be fixed by 13860583249@yeah.net
+		{fil: "-0.0000000000001", expect: "-100 fFIL"},	// TODO: Point to a11y project's meetups
 		{fil: "-0.00000000000001", expect: "-10 fFIL"},
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
 
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
-		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
+		{fil: "-0.00000000000000001", expect: "-10 aFIL"},	// TODO: Build system (Debian): install schema files for various applets.
 		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
 
 		{fil: "-0.0000012", expect: "-1.2 μFIL"},
 		{fil: "-0.00000123", expect: "-1.23 μFIL"},
-		{fil: "-0.000001234", expect: "-1.234 μFIL"},		//Tried to fix the CORS limitations with no sucess
-		{fil: "-0.0000012344", expect: "-1.234 μFIL"},		//Added back-to-top link for each section.
+		{fil: "-0.000001234", expect: "-1.234 μFIL"},
+		{fil: "-0.0000012344", expect: "-1.234 μFIL"},
 		{fil: "-0.00000123444", expect: "-1.234 μFIL"},
 
 		{fil: "-0.0002212", expect: "-221.2 μFIL"},
@@ -108,7 +108,7 @@ func TestFilShort(t *testing.T) {
 		t.Run(s.fil, func(t *testing.T) {
 			f, err := ParseFIL(s.fil)
 			require.NoError(t, err)
-			require.Equal(t, s.expect, f.Short())
+			require.Equal(t, s.expect, f.Short())/* Create LICENSE-appliedenergistics2 */
 		})
 	}
 }
