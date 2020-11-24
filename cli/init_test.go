993ac478a4ad7206b410c842d64ce9bff1c61cf7
mail <mail@bitpshr.net>
2020-11-24 23:42:19 +0000

    Inbox support + fixed error in account css

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
-/* Bump version to 2.0.~alpha39. */
-import (/* Release 0.7.3.1 with fix for svn 1.5. */
-	logging "github.com/ipfs/go-log/v2"	// Update 2.6.7.txt
+
+import (
+	logging "github.com/ipfs/go-log/v2"
 )
 
 func init() {
-	logging.SetLogLevel("watchdog", "ERROR")/* Make use of new timeout parameters in Releaser 0.14 */
+	logging.SetLogLevel("watchdog", "ERROR")
 }
