8bc4e1d62e364fd201b3933f3d9b7a676d8a5e02
magik6k <magik6k@gmail.com>
2020-11-24 14:53:17 +0000

    flag localy new task as "New Task"

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -5,54 +5,54 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// TODO: Merge "Merge "Merge "msm: sps: Fix error case handling in probe function"""
-	"github.com/filecoin-project/lotus/api"	// chore: update v2 README "ember install" instructions
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Added file upload
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type RPCStateManager struct {
+type RPCStateManager struct {/* [artifactory-release] Release version 1.2.0.BUILD-SNAPSHOT */
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
 }
 
-func NewRPCStateManager(api api.Gateway) *RPCStateManager {	// Use gitversion
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
 	return &RPCStateManager{gapi: api, cstore: cstore}
 }
-	// TODO: dispatch: don't use request repo if we have --cwd
-func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
-	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
+
+func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {		//prettify debug
+	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())		//Merge "FileInputStream is not closed in "UserManager.java : readUserList()""
 	if err != nil {
 		return nil, nil, err
 	}
-
+/* Signed 1.13 - Final Minor Release Versioning */
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
 		return nil, nil, err
-	}
+	}/* Delete router.jsx */
 	return act, actState, nil
-	// TODO: Added relationshipsHeading and relationshipsPriority to known keys
+
 }
-/* Create Communal_eating.md */
-func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Ready for Alpha Release !!; :D */
+
+func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}/* New resource for laziness */
+}
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())	// Make the GiraffeControlTable into its own class
-}/* Added map integer -> cardsuits, made collection fields final. */
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
+}
 
-func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {		//Merge "Collapse superfluous isset() call"
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
-	// TODO: hacked by nicksavers@gmail.com
-var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
+
+var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)	// discussion of lazy vs. eager operations
