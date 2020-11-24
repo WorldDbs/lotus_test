d92a80275cdf32d1b534ff5bdac8cbcc4f6f7108
alan.shaw <alan.shaw@protocol.ai>
2020-11-24 12:26:00 +0000

    BUG: seed PRNG to avoid random test failures

--- cli/wait.go
+++ cli/wait.go
@@ -11,7 +11,7 @@ var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {/* Fix MakeRelease.bat */
+		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
