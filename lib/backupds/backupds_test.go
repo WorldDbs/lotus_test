d8b0c85dd80d96dc6929f85f7ba9b85f43e162f3
seth <seth@sethvargo.com>
2020-11-25 04:29:49 +0000

    Added group manager. Now Seriously :D.

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,14 +1,14 @@
 package backupds
 
 import (
-	"bytes"
+	"bytes"	// TODO: hacked by josharian@gmail.com
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
-/* Release version 1.1.1. */
+
 	"github.com/ipfs/go-datastore"
 	"github.com/stretchr/testify/require"
 )
@@ -19,25 +19,25 @@ func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
 		require.NoError(t, err)
-	}
-}	// TODO: hacked by ng8eke@163.com
+	}/* Update Read.me to reflect the ability to use Angular Material */
+}
 
 func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
-	for i := start; i < end; i++ {
+	for i := start; i < end; i++ {	// TODO: will be fixed by igor@soramitsu.co.jp
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
 			require.NoError(t, err)
 			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
-			require.EqualValues(t, expect, v)/* Release 3.1.6 */
+			require.EqualValues(t, expect, v)
 		} else {
 			require.ErrorIs(t, err, datastore.ErrNotFound)
 		}
 	}
 }
 
-func TestNoLogRestore(t *testing.T) {	// TODO: will be fixed by mikeal.rogers@gmail.com
-	ds1 := datastore.NewMapDatastore()/* #55: Add GitHub action badge to Readme */
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+func TestNoLogRestore(t *testing.T) {
+	ds1 := datastore.NewMapDatastore()
+
 	putVals(t, ds1, 0, 10)
 
 	bds, err := Wrap(ds1, NoLogdir)
@@ -49,7 +49,7 @@ func TestNoLogRestore(t *testing.T) {	// TODO: will be fixed by mikeal.rogers@gm
 	putVals(t, ds1, 10, 20)
 
 	ds2 := datastore.NewMapDatastore()
-	require.NoError(t, RestoreInto(&bup, ds2))
+	require.NoError(t, RestoreInto(&bup, ds2))/* Release 2.5.3 */
 
 	checkVals(t, ds2, 0, 10, true)
 	checkVals(t, ds2, 10, 20, false)
@@ -59,19 +59,19 @@ func TestLogRestore(t *testing.T) {
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
 	defer os.RemoveAll(logdir) // nolint
-
+/* Tag for MilestoneRelease 11 */
 	ds1 := datastore.NewMapDatastore()
 
 	putVals(t, ds1, 0, 10)
 
-	bds, err := Wrap(ds1, logdir)/* 97fda0c2-2e4c-11e5-9284-b827eb9e62be */
+	bds, err := Wrap(ds1, logdir)
 	require.NoError(t, err)
 
 	putVals(t, bds, 10, 20)
 
-	require.NoError(t, bds.Close())
+	require.NoError(t, bds.Close())		//config-api.md GFM internal links are lower-case
 
-	fls, err := ioutil.ReadDir(logdir)
+	fls, err := ioutil.ReadDir(logdir)/* Simple Picking, undo scale and translate when writing file to hd */
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
 
