b394dc3bae9ff156d7baa22a80916a6c38d29fcb
nick <nick@perfectabstractions.com>
2020-11-25 14:57:06 +0000

    Kill foodcritic noise

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,23 +1,23 @@
 package paych
 
-import (/* Update README.md with Release history */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	// TODO: Removed unused bindEvents on search view.
+
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//GeoDa 1.5.31 build.
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {
+	if aerr != nil {/* Released version 1.5u */
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
@@ -26,7 +26,7 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	})
 	if aerr != nil {
 		return nil, aerr
-	}
+	}/* FIX: logging would not work with LBFGS */
 
 	return &types.Message{
 		To:     init_.Address,
@@ -63,12 +63,12 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 		Method: builtin2.MethodsPaych.Settle,
 	}, nil
 }
-/* 4a6a9426-2e44-11e5-9284-b827eb9e62be */
-func (m message2) Collect(paych address.Address) (*types.Message, error) {/* Release bzr 1.8 final */
+
+func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,
+		From:   m.from,/* Next is 1.0.0.CR2 */
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Collect,
-	}, nil	// TODO: Honor loss of audio focus in built-in music player.
-}/* Removed MAINTAINER message. */
+	}, nil
+}
