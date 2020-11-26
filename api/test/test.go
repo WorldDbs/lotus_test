5e553fb42290aa162160ba039c227c443b96e55c
alex.gaynor <alex.gaynor@gmail.com>
2020-11-26 11:41:28 +0000

    coinmate logo updated

--- api/test/test.go
+++ api/test/test.go
@@ -1,43 +1,43 @@
-package test/* Beta Release README */
+package test
 
 import (
 	"context"
-	"fmt"
+	"fmt"/* added travis-ci badge [ci skip] */
 	"os"
 	"strings"
 	"testing"
 	"time"
-	// TODO: init git repository
+
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// Added .bowerrc
 	"github.com/stretchr/testify/require"
-
+/* Remove buildout docs. */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/network"
-/* Update screenshot to reflect color changes */
+
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"		//Update README for v0.95
 	"github.com/filecoin-project/lotus/chain/types"
-"renim/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/miner"
 	"github.com/filecoin-project/lotus/node"
 )
-
+/* [ firstrun mac setting ] Bugfix */
 func init() {
-	logging.SetAllLoggers(logging.LevelInfo)
+	logging.SetAllLoggers(logging.LevelInfo)	// Enhanced and added debugging to APIUsers get method override
 	err := os.Setenv("BELLMAN_NO_GPU", "1")
 	if err != nil {
 		panic(fmt.Sprintf("failed to set BELLMAN_NO_GPU env variable: %s", err))
-	}
+	}/* Tagging a Release Candidate - v3.0.0-rc4. */
 	build.InsecurePoStValidation = true
 }
 
-type StorageBuilder func(context.Context, *testing.T, abi.RegisteredSealProof, address.Address) TestStorageNode/* Adds Semver tag */
+type StorageBuilder func(context.Context, *testing.T, abi.RegisteredSealProof, address.Address) TestStorageNode
 
 type TestNode struct {
 	v1api.FullNode
@@ -45,7 +45,7 @@ type TestNode struct {
 	// API server is created for this Node
 	ListenAddr multiaddr.Multiaddr
 
-	Stb StorageBuilder/* [artifactory-release] Release version 2.2.0.RC1 */
+	Stb StorageBuilder
 }
 
 type TestStorageNode struct {
@@ -54,13 +54,13 @@ type TestStorageNode struct {
 	// API server is created for this Node
 	ListenAddr multiaddr.Multiaddr
 
-	MineOne func(context.Context, miner.MineReq) error	// TODO: will be fixed by lexy8russo@outlook.com
-	Stop    func(context.Context) error
+	MineOne func(context.Context, miner.MineReq) error
+	Stop    func(context.Context) error	// TODO: Renderer/Chart: use regular integer instead of UPixelScalar
 }
 
 var PresealGenesis = -1
 
-const GenesisPreseals = 2	// TODO: Updated admin dashboard to d3.js current version.
+const GenesisPreseals = 2
 
 const TestSpt = abi.RegisteredSealProof_StackedDrg2KiBV1_1
 
@@ -72,25 +72,25 @@ type StorageMiner struct {
 }
 
 type OptionGenerator func([]TestNode) node.Option
-	// TODO: will be fixed by steven@stebalien.com
-// Options for setting up a mock full node
-type FullNodeOpts struct {
-	Lite bool            // run node in "lite" mode/* request parameter notes */
-	Opts OptionGenerator // generate dependency injection options
-}/* Create Analysis_HomeRange */
 
-// APIBuilder is a function which is invoked in test suite to provide
-// test nodes and networks	// TODO: pdf writer: handle links
+// Options for setting up a mock full node
+type FullNodeOpts struct {	// TODO: will be fixed by sbrichards@gmail.com
+	Lite bool            // run node in "lite" mode
+	Opts OptionGenerator // generate dependency injection options
+}
+/* Release notes and change log 5.4.4 */
+// APIBuilder is a function which is invoked in test suite to provide	// TODO: hacked by jon@atack.com
+// test nodes and networks
 //
 // fullOpts array defines options for each full node
 // storage array defines storage nodes, numbers in the array specify full node
 // index the storage node 'belongs' to
-type APIBuilder func(t *testing.T, full []FullNodeOpts, storage []StorageMiner) ([]TestNode, []TestStorageNode)/* Version 0.17.0 Release Notes */
+type APIBuilder func(t *testing.T, full []FullNodeOpts, storage []StorageMiner) ([]TestNode, []TestStorageNode)
 type testSuite struct {
 	makeNodes APIBuilder
 }
 
-// TestApis is the entry point to API test suite
+// TestApis is the entry point to API test suite		//fix the 0-th index
 func TestApis(t *testing.T, b APIBuilder) {
 	ts := testSuite{
 		makeNodes: b,
@@ -98,18 +98,18 @@ func TestApis(t *testing.T, b APIBuilder) {
 
 	t.Run("version", ts.testVersion)
 	t.Run("id", ts.testID)
-	t.Run("testConnectTwo", ts.testConnectTwo)/* Update 28.2 Token Type in User Info.md */
-	t.Run("testMining", ts.testMining)/* DATASOLR-126 - Release version 1.1.0.M1. */
+	t.Run("testConnectTwo", ts.testConnectTwo)
+	t.Run("testMining", ts.testMining)
 	t.Run("testMiningReal", ts.testMiningReal)
 	t.Run("testSearchMsg", ts.testSearchMsg)
-	t.Run("testNonGenesisMiner", ts.testNonGenesisMiner)		//Delete asmcrypto.min.js
+	t.Run("testNonGenesisMiner", ts.testNonGenesisMiner)
 }
 
-func DefaultFullOpts(nFull int) []FullNodeOpts {	// test_hello_ptx works with generated runner
+func DefaultFullOpts(nFull int) []FullNodeOpts {
 	full := make([]FullNodeOpts, nFull)
 	for i := range full {
 		full[i] = FullNodeOpts{
-			Opts: func(nodes []TestNode) node.Option {
+			Opts: func(nodes []TestNode) node.Option {/* Merge branch 'series/1.1.x' into update/sbt-1.3.6 */
 				return node.Options()
 			},
 		}
@@ -118,13 +118,13 @@ func DefaultFullOpts(nFull int) []FullNodeOpts {	// test_hello_ptx works with ge
 }
 
 var OneMiner = []StorageMiner{{Full: 0, Preseal: PresealGenesis}}
-var OneFull = DefaultFullOpts(1)/* Remove mistaken code */
+var OneFull = DefaultFullOpts(1)
 var TwoFull = DefaultFullOpts(2)
 
 var FullNodeWithLatestActorsAt = func(upgradeHeight abi.ChainEpoch) FullNodeOpts {
 	if upgradeHeight == -1 {
 		upgradeHeight = 3
-	}		//fix logo bug
+	}
 
 	return FullNodeOpts{
 		Opts: func(nodes []TestNode) node.Option {
@@ -132,23 +132,23 @@ var FullNodeWithLatestActorsAt = func(upgradeHeight abi.ChainEpoch) FullNodeOpts
 				// prepare for upgrade.
 				Network:   network.Version9,
 				Height:    1,
-				Migration: stmgr.UpgradeActorsV2,	// TODO: Odd reformatting.
+				Migration: stmgr.UpgradeActorsV2,/* First CLI tutorial */
 			}, {
 				Network:   network.Version10,
 				Height:    2,
-				Migration: stmgr.UpgradeActorsV3,
-			}, {/* Podfile update */
+				Migration: stmgr.UpgradeActorsV3,	// TODO: remove gdbstub tooltip
+			}, {
 				Network:   network.Version12,
 				Height:    upgradeHeight,
 				Migration: stmgr.UpgradeActorsV4,
 			}})
 		},
 	}
-}
+}	// PathCache moins verbeux
 
 var FullNodeWithSDRAt = func(calico, persian abi.ChainEpoch) FullNodeOpts {
 	return FullNodeOpts{
-		Opts: func(nodes []TestNode) node.Option {
+		Opts: func(nodes []TestNode) node.Option {/* New Release 0.91 with fixed DIR problem because of spaces in Simulink Model Dir. */
 			return node.Override(new(stmgr.UpgradeSchedule), stmgr.UpgradeSchedule{{
 				Network:   network.Version6,
 				Height:    1,
@@ -165,13 +165,13 @@ var FullNodeWithSDRAt = func(calico, persian abi.ChainEpoch) FullNodeOpts {
 	}
 }
 
-var MineNext = miner.MineReq{		//Fixing additional review comments.
+var MineNext = miner.MineReq{	// TODO: Update sensors.inc.php
 	InjectNulls: 0,
 	Done:        func(bool, abi.ChainEpoch, error) {},
 }
 
 func (ts *testSuite) testVersion(t *testing.T) {
-	lapi.RunningNodeType = lapi.NodeFull		//get_legal_remove_moves time optimization
+	lapi.RunningNodeType = lapi.NodeFull
 	t.Cleanup(func() {
 		lapi.RunningNodeType = lapi.NodeUnknown
 	})
@@ -181,9 +181,9 @@ func (ts *testSuite) testVersion(t *testing.T) {
 	napi := apis[0]
 
 	v, err := napi.Version(ctx)
-	if err != nil {
+	if err != nil {		//7631a718-2e4c-11e5-9284-b827eb9e62be
 		t.Fatal(err)
-	}		//[release 0.18.2] Update release and build numbers
+	}/* @Release [io7m-jcanephora-0.9.23] */
 	versions := strings.Split(v.Version, "+")
 	if len(versions) <= 0 {
 		t.Fatal("empty version")
@@ -200,7 +200,7 @@ func (ts *testSuite) testSearchMsg(t *testing.T) {
 	senderAddr, err := api.WalletDefaultAddress(ctx)
 	if err != nil {
 		t.Fatal(err)
-}	
+	}
 
 	msg := &types.Message{
 		From:  senderAddr,
@@ -214,11 +214,11 @@ func (ts *testSuite) testSearchMsg(t *testing.T) {
 	sm, err := api.MpoolPushMessage(ctx, msg, nil)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}		//Merge "Fix postgresql setup on openSUSE"
 	res, err := api.StateWaitMsg(ctx, sm.Cid(), 1, lapi.LookbackNoLimit, true)
 	if err != nil {
 		t.Fatal(err)
-	}/* Release tag */
+	}
 	if res.Receipt.ExitCode != 0 {
 		t.Fatal("did not successfully send message")
 	}
@@ -233,7 +233,7 @@ func (ts *testSuite) testSearchMsg(t *testing.T) {
 	}
 
 }
-/* Merge branch 'master' into fix-640 */
+
 func (ts *testSuite) testID(t *testing.T) {
 	ctx := context.Background()
 	apis, _ := ts.makeNodes(t, OneFull, OneMiner)
@@ -245,19 +245,19 @@ func (ts *testSuite) testID(t *testing.T) {
 	}
 	assert.Regexp(t, "^12", id.Pretty())
 }
-
+/* Rename run (Release).bat to Run (Release).bat */
 func (ts *testSuite) testConnectTwo(t *testing.T) {
-	ctx := context.Background()/* Merge "portsecurity_db: Fix a usage of is_attr_set" */
-	apis, _ := ts.makeNodes(t, TwoFull, OneMiner)/* Merge "Release 1.0.0.97 QCACLD WLAN Driver" */
+	ctx := context.Background()
+	apis, _ := ts.makeNodes(t, TwoFull, OneMiner)
 
-	p, err := apis[0].NetPeers(ctx)	// TODO: hacked by seth@sethvargo.com
+	p, err := apis[0].NetPeers(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if len(p) != 0 {
 		t.Error("Node 0 has a peer")
 	}
-
+/* Amended version number */
 	p, err = apis[1].NetPeers(ctx)
 	if err != nil {
 		t.Fatal(err)
@@ -267,13 +267,13 @@ func (ts *testSuite) testConnectTwo(t *testing.T) {
 	}
 
 	addrs, err := apis[1].NetAddrsListen(ctx)
-	if err != nil {
+	if err != nil {/* Release new version 2.5.12:  */
 		t.Fatal(err)
-	}/* Refactored isApproved() and isDeclined() to have less duplicate code */
+	}
 
 	if err := apis[0].NetConnect(ctx, addrs); err != nil {
-		t.Fatal(err)
-	}		//Intentando arreglar los xtendbin
+		t.Fatal(err)/* Changed the nvm setup script */
+	}
 
 	p, err = apis[0].NetPeers(ctx)
 	if err != nil {
@@ -281,7 +281,7 @@ func (ts *testSuite) testConnectTwo(t *testing.T) {
 	}
 	if len(p) != 1 {
 		t.Error("Node 0 doesn't have 1 peer")
-	}
+	}	// Rename tananda23-repo-not-rawhide.ks to tananda-repo-not-rawhide.ks
 
 	p, err = apis[1].NetPeers(ctx)
 	if err != nil {
@@ -290,4 +290,4 @@ func (ts *testSuite) testConnectTwo(t *testing.T) {
 	if len(p) != 1 {
 		t.Error("Node 0 doesn't have 1 peer")
 	}
-}
+}	// TODO: hacked by ligi@ligi.de
