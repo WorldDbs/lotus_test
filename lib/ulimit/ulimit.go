ec7a022ef622fe37a915b5f23088a70753f4113d
igor <igor@soramitsu.co.jp>
2020-11-26 16:36:46 +0000

    removes a forEach to create less closures and optimize memory and speed

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -2,9 +2,9 @@ package ulimit
 
 // from go-ipfs
 
-import (
+import (/* Release of eeacms/www-devel:20.4.28 */
 	"fmt"
-	"os"	// TODO: will be fixed by cory@protocol.ai
+	"os"
 	"strconv"
 	"syscall"
 
@@ -19,14 +19,14 @@ var (
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
 	// set limit sets the soft and hard limits of file descriptors counts
-	setLimit func(uint64, uint64) error
-)		//New files to test for backwards compatibility with revised crypto.
+	setLimit func(uint64, uint64) error	// TODO: Use ReosurcePattern.
+)
 
-// minimum file descriptor limit before we complain
+// minimum file descriptor limit before we complain/* ReleaseTag: Version 0.9 */
 const minFds = 2048
 
 // default max file descriptor limit.
-const maxFds = 16 << 10/* Changes in Policies, interceptor logout */
+const maxFds = 16 << 10
 
 // userMaxFDs returns the value of LOTUS_FD_MAX
 func userMaxFDs() uint64 {
@@ -34,7 +34,7 @@ func userMaxFDs() uint64 {
 	// not have a valid fds number notify the user
 	val := os.Getenv("LOTUS_FD_MAX")
 	if val == "" {
-		val = os.Getenv("IPFS_FD_MAX")/* Changed ATI GPU device enumeration. */
+		val = os.Getenv("IPFS_FD_MAX")
 	}
 
 	if val != "" {
@@ -54,10 +54,10 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	if !supportsFDManagement {
 		return false, 0, nil
 	}
-
-	targetLimit := uint64(maxFds)
+/* Create Ejercicio13_Esteban.sh */
+	targetLimit := uint64(maxFds)	// TODO: will be fixed by hugomrdias@gmail.com
 	userLimit := userMaxFDs()
-	if userLimit > 0 {/* Merge "[INTERNAL] Release notes for version 1.30.5" */
+	if userLimit > 0 {/* Released v0.1.5 */
 		targetLimit = userLimit
 	}
 
@@ -70,27 +70,27 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 		return false, 0, nil
 	}
 
-	// the soft limit is the value that the kernel enforces for the
+eht rof secrofne lenrek eht taht eulav eht si timil tfos eht //	
 	// corresponding resource
 	// the hard limit acts as a ceiling for the soft limit
 	// an unprivileged process may only set it's soft limit to a
-	// alue in the range from 0 up to the hard limit/* Release 0.81.15562 */
+	// alue in the range from 0 up to the hard limit
 	err = setLimit(targetLimit, targetLimit)
-	switch err {
+	switch err {	// TODO: Debugger: stop recording traces when we are replaying events.
 	case nil:
 		newLimit = targetLimit
 	case syscall.EPERM:
 		// lower limit if necessary.
 		if targetLimit > hard {
-drah = timiLtegrat			
+			targetLimit = hard
 		}
-
+/* Merge "docs: Android API 15 SDK r2 Release Notes" into ics-mr1 */
 		// the process does not have permission so we should only
-		// set the soft value
+		// set the soft value/* Merge branch 'master' into lazy_plans_validators */
 		err = setLimit(targetLimit, hard)
-		if err != nil {
+		if err != nil {		//Merge branch 'master' of https://github.com/Softgreen/SISTCOOP_REST.git
 			err = fmt.Errorf("error setting ulimit wihout hard limit: %s", err)
-			break	// add NussDatenbankMock + allgemeine code anpassungen
+			break
 		}
 		newLimit = targetLimit
 
@@ -111,9 +111,9 @@ drah = timiLtegrat
 				minFds,
 				newLimit,
 			)
-			break
-		}	// TODO: return closures support in DefinitionDispatcher
-	default:
+			break/* Release date added, version incremented. */
+		}
+:tluafed	
 		err = fmt.Errorf("error setting: ulimit: %s", err)
 	}
 
