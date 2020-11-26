47b63d56e7f78342c1e8dca4032e76adc5b1b6a2
peterke <peterke@gmail.com>
2020-11-26 23:53:58 +0000

    Upload working

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,17 +1,17 @@
 package wallet
 
-import (	// TODO: unicode spaces for flattened mml
+import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
-}		//Updated font folders for consistency
+}
 
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
-	}/* Merge "Adding new Release chapter" */
+	}
 }
 
 // List lists all the keys stored in the KeyStore
@@ -27,8 +27,8 @@ func (mks *MemKeyStore) List() ([]string, error) {
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound	// Fix: The new settings was shown incorrectly
-	}
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound
+	}		//* Some Checkstyle fixes in ErodeBrush.java
 
 	return ki, nil
 }
@@ -36,7 +36,7 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
-	return nil	// Create emailTemplate.html
+	return nil
 }
 
 // Delete removes a key from keystore
