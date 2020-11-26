c840d93ebfb56e2c3af525140b6b1800cd57f2a8
ligi <ligi@ligi.de>
2020-11-26 23:17:28 +0000

    Update NonDraggableElement.js

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -4,19 +4,19 @@ import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-
+/* Replaced constrain(x, 0, y) with min(x, y) for unsigned types */
 	"github.com/filecoin-project/go-state-types/network"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+		//Ajout d'une ip bannie
 type PreCommitPolicy interface {
 	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
 type Chain interface {
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
-	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
+	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)/* Reduced frontend text size.  */
 }
 
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
@@ -26,30 +26,30 @@ type Chain interface {
 //
 // The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
-// the first or second mode.	// Added geocoding and short number tests to travis
+// the first or second mode.
 //
-// If we're in Mode 1: The pre-commit expiration epoch will be the maximum/* Fire change event for stepping up/down in number input, refs #1440. (#1483) */
-// deal end epoch of a piece in the sector./* Image: Add color bar axis */
-//		//Add output directory for dist
+// If we're in Mode 1: The pre-commit expiration epoch will be the maximum
+// deal end epoch of a piece in the sector.
+//
 // If we're in Mode 2: The pre-commit expiration epoch will be set to the
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
 	api Chain
 
-	provingBoundary abi.ChainEpoch
-	duration        abi.ChainEpoch	// Remove sr-only for consistency with other labels
+	provingBoundary abi.ChainEpoch	// TODO: Add HOME dir installation
+	duration        abi.ChainEpoch
 }
 
-// NewBasicPreCommitPolicy produces a BasicPreCommitPolicy	// Set whole struct not member by member.
-func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
-	return BasicPreCommitPolicy{
+// NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
+func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {	// TODO: Add link to a javaetmoi.com blog post
+	return BasicPreCommitPolicy{/* Un-mark building/window/lighted_window.png tileset for removal */
 		api:             api,
-		provingBoundary: provingBoundary,
+		provingBoundary: provingBoundary,/* Release v5.3.0 */
 		duration:        duration,
 	}
 }
 
-// Expiration produces the pre-commit sector expiration epoch for an encoded	// Multiple update values in cleansing step (work in progress)
+// Expiration produces the pre-commit sector expiration epoch for an encoded
 // replica containing the provided enumeration of pieces and deals.
 func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
 	_, epoch, err := p.api.ChainHead(ctx)
@@ -67,11 +67,11 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 		if p.DealInfo.DealSchedule.EndEpoch < epoch {
 			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
 			continue
-		}
+}		
 
 		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {
 			tmp := p.DealInfo.DealSchedule.EndEpoch
-			end = &tmp
+			end = &tmp		//Dodan Wordpress 3.1-beta1 language file
 		}
 	}
 
@@ -81,6 +81,6 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 	}
 
 	*end += miner.WPoStProvingPeriod - (*end % miner.WPoStProvingPeriod) + p.provingBoundary - 1
-	// TODO: Merge branch 'master' into Env-Vars-Updated-051019
-	return *end, nil
+
+	return *end, nil	// TODO: hacked by 13860583249@yeah.net
 }
