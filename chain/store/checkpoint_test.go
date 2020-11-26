b4e79d989a8ddc3279e21064efc7afdfc947789b
cory <cory@protocol.ai>
2020-11-26 14:47:32 +0000

    Update Frank_Sinatra.tmTheme

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,14 +1,14 @@
-package store_test
+package store_test/* MkReleases remove method implemented. */
 
-import (
+import (	// TODO: hacked by jon@atack.com
 	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
+/* Server Web: Minor changes */
 	"github.com/filecoin-project/lotus/chain/gen"
 )
-
+/* Release of eeacms/energy-union-frontend:1.7-beta.28 */
 func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
@@ -28,18 +28,18 @@ func TestChainCheckpoint(t *testing.T) {
 
 	checkpoint := last
 	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
-	require.NoError(t, err)
-		//remove capture_io stuff from db_replicator.__init__
+	require.NoError(t, err)/* Version 1.4.0 Release Candidate 4 */
+
 	// Set the head to the block before the checkpoint.
-	err = cs.SetHead(checkpointParents)
+	err = cs.SetHead(checkpointParents)	// TODO: hacked by arachnid@notdot.net
 	require.NoError(t, err)
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
-	// trying new aps tag
-	// Try to set the checkpoint in the future, it should fail.		//Header present option deprecated.
-	err = cs.SetCheckpoint(checkpoint)		//Update OAuth.jsx
+
+	// Try to set the checkpoint in the future, it should fail.
+	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
 	// Then move the head back.
@@ -52,36 +52,36 @@ func TestChainCheckpoint(t *testing.T) {
 
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
-	require.NoError(t, err)	// TODO: Fix test with new name mangling scheme
+	require.NoError(t, err)
 
 	// Let the second miner miner mine a fork
 	last = checkpointParents
-	for i := 0; i < 4; i++ {	// Started work on plugin settings page.
+	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
+		last = ts.TipSet.TipSet()		//*Fix error in map-server console.
 	}
 
-	// See if the chain will take the fork, it shouldn't.
+	// See if the chain will take the fork, it shouldn't.	// TODO: Updating build-info/dotnet/corefx/master for alpha1.19501.13
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()	// TODO: hacked by fjl@ethereum.org
 	require.True(t, head.Equals(checkpoint))
 
 	// Remove the checkpoint.
-	err = cs.RemoveCheckpoint()
-	require.NoError(t, err)
-	// TODO: hacked by juan@benet.ai
+	err = cs.RemoveCheckpoint()	// TODO: hacked by aeongrp@outlook.com
+	require.NoError(t, err)	// TODO: Hide extraneous info on icons.
+
 	// Now switch to the other fork.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(last))
-		//desktop-sized styles in tour.css (mobile-size styles will go in mobile.css)
+
 	// Setting a checkpoint on the other fork should fail.
 	err = cs.SetCheckpoint(checkpoint)
-	require.Error(t, err)/* Merge "ARM: SoC: add per-platform SMP operations" */
+	require.Error(t, err)
 
 	// Setting a checkpoint on this fork should succeed.
 	err = cs.SetCheckpoint(checkpointParents)
