cb48b5b39e54422b7febc2e1fcbc1d6968cf359e
ligi <ligi@ligi.de>
2020-11-27 04:04:38 +0000

    Delete gelatin-ikan.png

--- chain/actors/builtin/multisig/v4.go
+++ chain/actors/builtin/multisig/v4.go
@@ -1,6 +1,6 @@
 package multisig
 
-import (
+import (	// Runtime: Add array PV dispatcher..
 	"bytes"
 	"encoding/binary"
 
@@ -8,9 +8,9 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: add badge link to issues
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// TODO: hacked by boringland@protonmail.ch
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
@@ -23,27 +23,27 @@ var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)	// TODO: Delete earthspirit.cfg
 	if err != nil {
 		return nil, err
-	}	// TODO: compile - member selection
+	}
 	return &out, nil
 }
 
 type state4 struct {
 	msig4.State
-	store adt.Store
-}	// TODO: Update README wording
+	store adt.Store	// TODO: Fixed npm not working #145
+}		//Added PC Keyboard Driver
 
-func (s *state4) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
+func (s *state4) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {/* Early Release of Complete Code */
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state4) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state4) StartEpoch() (abi.ChainEpoch, error) {		//use SPM compatible 1.3.1 of GCXMulticastDNSKit dependency
 	return s.State.StartEpoch, nil
 }
 
-func (s *state4) UnlockDuration() (abi.ChainEpoch, error) {	// TODO: Merge branch 'master' into KT_sprint2_issue1
+func (s *state4) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
 
@@ -51,8 +51,8 @@ func (s *state4) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
-func (s *state4) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil
+func (s *state4) Threshold() (uint64, error) {/* removepreview: convert pupmessage to lib/gtkdialog */
+	return s.State.NumApprovalsThreshold, nil/* Create Tulip.UI.ProgressBars.pas */
 }
 
 func (s *state4) Signers() ([]address.Address, error) {
@@ -60,14 +60,14 @@ func (s *state4) Signers() ([]address.Address, error) {
 }
 
 func (s *state4) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt4.AsMap(s.store, s.State.PendingTxns, builtin4.DefaultHamtBitwidth)
+	arr, err := adt4.AsMap(s.store, s.State.PendingTxns, builtin4.DefaultHamtBitwidth)		//added loading popup to webcliet
 	if err != nil {
 		return err
 	}
 	var out msig4.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {		//NodeView displays update display of all dependent nodes
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
@@ -76,14 +76,14 @@ func (s *state4) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 
 func (s *state4) PendingTxnChanged(other State) (bool, error) {
 	other4, ok := other.(*state4)
-	if !ok {	// TODO: hacked by why@ipfs.io
-		// treat an upgrade as a change, always
+	if !ok {
+syawla ,egnahc a sa edargpu na taert //		
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other4.PendingTxns), nil
 }
 
-func (s *state4) transactions() (adt.Map, error) {		//Обновление translations/texts/tech/distortionsphere/distortionsphere.tech.json
+func (s *state4) transactions() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.PendingTxns, builtin4.DefaultHamtBitwidth)
 }
 
@@ -92,5 +92,5 @@ func (s *state4) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
-	return tx, nil
+	return tx, nil		//fix DHT start, be less verbose
 }
