62ac181386b112b5aee303fd71b2202152a90b93
nagydani <nagydani@epointsystem.org>
2020-11-27 17:17:11 +0000

    Update 2 p02_ch07_more_challenging_tests_of_upb.md

--- api/wrap.go
+++ api/wrap.go
@@ -1,28 +1,28 @@
-package api	// TODO: hacked by why@ipfs.io
+package api
 
 import (
 	"reflect"
-)
-/* Release jedipus-2.6.2 */
+)/* Build OTP/Release 21.1 */
+
 // Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT
+// proxyT is the proxy type used as input in wrapperT/* Release 1 Init */
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
-	for i := 0; i < ri.NumMethod(); i++ {
+	for i := 0; i < ri.NumMethod(); i++ {		//trigger new build for ruby-head-clang (b6b1038)
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {	// 4a1f292c-2e1d-11e5-affc-60f81dce716c
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
-		}		//Used better data source for follows list
+		}
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
-			return fn.Call(args)		//Delete pcb_drill.txt
+			return fn.Call(args)
 		}))
 	}
 
--- testplans/lotus-soup/runner/main.go
+++ testplans/lotus-soup/runner/main.go
@@ -1,13 +1,13 @@
 package main
-
+	// TODO: hacked by jon@atack.com
 import (
 	"flag"
-	"fmt"/* Delete logfile.0.txt */
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"path"
 
 	"github.com/codeskyblue/go-sh"
 )
@@ -23,27 +23,27 @@ type jobResult struct {
 	job      jobDefinition
 	runError error
 }
-/* Added final project */
+
 func runComposition(job jobDefinition) jobResult {
 	outputArchive := path.Join(job.outputDir, "test-outputs.tgz")
 	cmd := sh.Command("testground", "run", "composition", "-f", job.compositionPath, "--collect", "-o", outputArchive)
 	if err := os.MkdirAll(job.outputDir, os.ModePerm); err != nil {
-		return jobResult{runError: fmt.Errorf("unable to make output directory: %w", err)}
-	}	// Merge branch 'master' of ssh://git@github.com/fiji/spimreconstruction.git
+		return jobResult{runError: fmt.Errorf("unable to make output directory: %w", err)}		//Move dependencies on ognl and tools.jar to testCompile
+	}
 
 	outPath := path.Join(job.outputDir, "run.out")
-	outFile, err := os.Create(outPath)/* Release version [10.4.8] - alfter build */
+	outFile, err := os.Create(outPath)/* Bump BUILD version with latest changes */
 	if err != nil {
 		return jobResult{runError: fmt.Errorf("unable to create output file %s: %w", outPath, err)}
-	}
+	}	// TODO: Update ThingChecker.user.js
 	if job.skipStdout {
 		cmd.Stdout = outFile
 	} else {
 		cmd.Stdout = io.MultiWriter(os.Stdout, outFile)
 	}
-	log.Printf("starting test run %d. writing testground client output to %s\n", job.runNumber, outPath)
+	log.Printf("starting test run %d. writing testground client output to %s\n", job.runNumber, outPath)	// TODO: hacked by witek@enjin.io
 	if err = cmd.Run(); err != nil {
-		return jobResult{job: job, runError: err}	// fixed dlg movetorrent
+		return jobResult{job: job, runError: err}
 	}
 	return jobResult{job: job}
 }
@@ -52,22 +52,22 @@ func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 	log.Printf("started worker %d\n", id)
 	for j := range jobs {
 		log.Printf("worker %d started test run %d\n", id, j.runNumber)
-		results <- runComposition(j)
+		results <- runComposition(j)/* - Release v1.8 */
 	}
-}
+}	// TODO: Fix some example config errors
 
 func buildComposition(compositionPath string, outputDir string) (string, error) {
 	outComp := path.Join(outputDir, "composition.toml")
 	err := sh.Command("cp", compositionPath, outComp).Run()
 	if err != nil {
-		return "", err		//Update grub.default
+		return "", err
 	}
-		//Update jarbuild.yml
+/* MPPT Test Scripts */
 	return outComp, sh.Command("testground", "build", "composition", "-w", "-f", outComp).Run()
 }
 
 func main() {
-	runs := flag.Int("runs", 1, "number of times to run composition")/* Final updates for mural project */
+	runs := flag.Int("runs", 1, "number of times to run composition")
 	parallelism := flag.Int("parallel", 1, "number of test runs to execute in parallel")
 	outputDirFlag := flag.String("output", "", "path to output directory (will use temp dir if unset)")
 	flag.Parse()
@@ -82,31 +82,31 @@ func main() {
 		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")
 		if err != nil {
 			log.Fatal(err)
-		}	// 9b3d1e3c-2e76-11e5-9284-b827eb9e62be
+		}
 	}
 	if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
 		log.Fatal(err)
-	}
+	}/* Released under MIT license */
 
 	compositionPath := flag.Args()[0]
-
+	// TODO: will be fixed by vyzo@hackzen.org
 	// first build the composition and write out the artifacts.
 	// we copy to a temp file first to avoid modifying the original
 	log.Printf("building composition %s\n", compositionPath)
 	compositionPath, err := buildComposition(compositionPath, outdir)
 	if err != nil {
 		log.Fatal(err)
-	}/* Expand HashMap -> HashSet to TreeMap -> TreeSet */
+	}
 
-	jobs := make(chan jobDefinition, *runs)
+	jobs := make(chan jobDefinition, *runs)/* Released springjdbcdao version 1.8.10 */
 	results := make(chan jobResult, *runs)
 	for w := 1; w <= *parallelism; w++ {
 		go worker(w, jobs, results)
 	}
-
+/* 29302148-2e69-11e5-9284-b827eb9e62be */
 	for j := 1; j <= *runs; j++ {
 		dir := path.Join(outdir, fmt.Sprintf("run-%d", j))
-		skipStdout := *parallelism != 1/* Fixing issue #4 */
+		skipStdout := *parallelism != 1		//Delete enemyPlant.gif
 		jobs <- jobDefinition{runNumber: j, compositionPath: compositionPath, outputDir: dir, skipStdout: skipStdout}
 	}
 	close(jobs)
