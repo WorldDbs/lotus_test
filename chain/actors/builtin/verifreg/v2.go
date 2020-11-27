6dfd5424a9a359bd6c223d1af202d1a71f467099
timnugent <timnugent@gmail.com>
2020-11-27 11:20:55 +0000

    Added hashCode method using Equality's large set algorithm.

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,5 +1,5 @@
 package verifreg
-/* add membership constraints for #760 */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,50 +8,50 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"		//basic things
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}/* CMAbstractModel does not use TObservableSlot */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge branch 'master' into ignore_he_vm */
+	}
 	return &out, nil
 }
 
-type state2 struct {/* this should serialize the current Q metadata model to XML */
+type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {		//Merge branch 'master' into minor-api-change
-	return s.State.RootKey, nil	// TODO: Fixed news article tests
+func (s *state2) RootKey() (address.Address, error) {
+	return s.State.RootKey, nil
 }
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
 
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Try editing online from GitHub! */
+	return getDataCap(s.store, actors.Version2, s.verifiers, addr)/* Refactor LTLFormulaChecker and CBCFormulaHandler */
 }
 
-func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release of eeacms/www:18.1.19 */
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
-}		//libde265 WebAssembly
-
+}/* nvdaHelper.nvdaController_speakText: queue speech. */
+/* rev 618216 */
 func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
 func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
-}
+}	// TODO: will be fixed by mail@bitpshr.net
