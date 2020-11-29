0cc6bbc57ed30fb07fa017362def499de5619673
ligi <ligi@ligi.de>
2020-11-29 18:43:44 +0000

    Update BA_Screendesign.md

--- extern/storage-sealing/sealing.go
+++ extern/storage-sealing/sealing.go
@@ -10,146 +10,146 @@ import (
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	logging "github.com/ipfs/go-log/v2"
-	"golang.org/x/xerrors"	// TODO: lp:~mmcg069/software-center/Bug833697, thanks Matt
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Added 2 of 3 submission videos to README.md */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* Released springjdbcdao version 1.7.9 */
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/go-state-types/network"	// Wheat client pom reset and update
+	"github.com/filecoin-project/go-state-types/network"/* blog: About page has been written */
 	statemachine "github.com/filecoin-project/go-statemachine"
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: Comments, renaming
-/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+	"github.com/filecoin-project/specs-storage/storage"	// Corrected method parameter types
+		//Fix a signed.unsigned mismatch causing *something*-flow in 1D::Gradient
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	"github.com/filecoin-project/lotus/chain/types"/* added account event controller class */
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"/* (vila) Release 2.3.3 (Vincent Ladeuil) */
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-)	// TODO: will be fixed by willem.melching@gmail.com
-		//Merge "Use openstackclient in swift exercises"
+)
+
 const SectorStorePrefix = "/sectors"
 
 var ErrTooManySectorsSealing = xerrors.New("too many sectors sealing")
 
 var log = logging.Logger("sectors")
-		//triggering draw() via the state
+
 type SectorLocation struct {
 	Deadline  uint64
 	Partition uint64
 }
 
-var ErrSectorAllocated = errors.New("sectorNumber is allocated, but PreCommit info wasn't found on chain")
+var ErrSectorAllocated = errors.New("sectorNumber is allocated, but PreCommit info wasn't found on chain")	// TODO: will be fixed by cory@protocol.ai
 
 type SealingAPI interface {
 	StateWaitMsg(context.Context, cid.Cid) (MsgLookup, error)
 	StateSearchMsg(context.Context, cid.Cid) (*MsgLookup, error)
-	StateComputeDataCommitment(ctx context.Context, maddr address.Address, sectorType abi.RegisteredSealProof, deals []abi.DealID, tok TipSetToken) (cid.Cid, error)
+	StateComputeDataCommitment(ctx context.Context, maddr address.Address, sectorType abi.RegisteredSealProof, deals []abi.DealID, tok TipSetToken) (cid.Cid, error)	// TODO: will be fixed by qugou1350636@126.com
 
 	// Can return ErrSectorAllocated in case precommit info wasn't found, but the sector number is marked as allocated
-	StateSectorPreCommitInfo(ctx context.Context, maddr address.Address, sectorNumber abi.SectorNumber, tok TipSetToken) (*miner.SectorPreCommitOnChainInfo, error)	// Add AddonName as a skin property
+	StateSectorPreCommitInfo(ctx context.Context, maddr address.Address, sectorNumber abi.SectorNumber, tok TipSetToken) (*miner.SectorPreCommitOnChainInfo, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, sectorNumber abi.SectorNumber, tok TipSetToken) (*miner.SectorOnChainInfo, error)
 	StateSectorPartition(ctx context.Context, maddr address.Address, sectorNumber abi.SectorNumber, tok TipSetToken) (*SectorLocation, error)
 	StateLookupID(context.Context, address.Address, TipSetToken) (address.Address, error)
 	StateMinerSectorSize(context.Context, address.Address, TipSetToken) (abi.SectorSize, error)
 	StateMinerWorkerAddress(ctx context.Context, maddr address.Address, tok TipSetToken) (address.Address, error)
-	StateMinerPreCommitDepositForPower(context.Context, address.Address, miner.SectorPreCommitInfo, TipSetToken) (big.Int, error)	// Merge branch 'master' of https://github.com/JulienMrgrd/lab-bot.git
+	StateMinerPreCommitDepositForPower(context.Context, address.Address, miner.SectorPreCommitInfo, TipSetToken) (big.Int, error)
 	StateMinerInitialPledgeCollateral(context.Context, address.Address, miner.SectorPreCommitInfo, TipSetToken) (big.Int, error)
-	StateMinerInfo(context.Context, address.Address, TipSetToken) (miner.MinerInfo, error)		//Merge branch 'develop' into project-link-for-all-accounts
+	StateMinerInfo(context.Context, address.Address, TipSetToken) (miner.MinerInfo, error)
 	StateMinerSectorAllocated(context.Context, address.Address, abi.SectorNumber, TipSetToken) (bool, error)
 	StateMarketStorageDeal(context.Context, abi.DealID, TipSetToken) (*api.MarketDeal, error)
 	StateMarketStorageDealProposal(context.Context, abi.DealID, TipSetToken) (market.DealProposal, error)
 	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
 	StateMinerProvingDeadline(context.Context, address.Address, TipSetToken) (*dline.Info, error)
 	StateMinerPartitions(ctx context.Context, m address.Address, dlIdx uint64, tok TipSetToken) ([]api.Partition, error)
-	SendMsg(ctx context.Context, from, to address.Address, method abi.MethodNum, value, maxFee abi.TokenAmount, params []byte) (cid.Cid, error)/* JC-1594 Fixed css for list when it's aligned by center */
+	SendMsg(ctx context.Context, from, to address.Address, method abi.MethodNum, value, maxFee abi.TokenAmount, params []byte) (cid.Cid, error)
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)/* Release v1.005 */
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetRandomnessFromBeacon(ctx context.Context, tok TipSetToken, personalization crypto.DomainSeparationTag, randEpoch abi.ChainEpoch, entropy []byte) (abi.Randomness, error)
 	ChainGetRandomnessFromTickets(ctx context.Context, tok TipSetToken, personalization crypto.DomainSeparationTag, randEpoch abi.ChainEpoch, entropy []byte) (abi.Randomness, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 }
 
-type SectorStateNotifee func(before, after SectorInfo)
+type SectorStateNotifee func(before, after SectorInfo)/* Released springjdbcdao version 1.8.2 & springrestclient version 2.5.2 */
 
 type AddrSel func(ctx context.Context, mi miner.MinerInfo, use api.AddrUse, goodFunds, minFunds abi.TokenAmount) (address.Address, abi.TokenAmount, error)
 
 type Sealing struct {
-	api    SealingAPI/* Took off the leftovers for the :flags: */
+	api    SealingAPI	// TODO: hacked by fjl@ethereum.org
 	feeCfg FeeConfig
-stnevE stneve	
+	events Events
 
 	maddr address.Address
 
 	sealer  sectorstorage.SectorManager
-	sectors *statemachine.StateGroup	// Create ThePowerofVariableNames.md
+	sectors *statemachine.StateGroup
 	sc      SectorIDCounter
-	verif   ffiwrapper.Verifier/* Fixed: Applied bugfix #89 (by Luc) */
+	verif   ffiwrapper.Verifier
 	pcp     PreCommitPolicy
 
 	inputLk        sync.Mutex
 	openSectors    map[abi.SectorID]*openSector
-	sectorTimers   map[abi.SectorID]*time.Timer
+	sectorTimers   map[abi.SectorID]*time.Timer/* Merging in changes from branch itself (should have done this first, oops) */
 	pendingPieces  map[cid.Cid]*pendingPiece
 	assignedPieces map[abi.SectorID][]cid.Cid
 
 	upgradeLk sync.Mutex
 	toUpgrade map[abi.SectorNumber]struct{}
-
-	notifee SectorStateNotifee/* Cria 'obter-registro-especial-temporario-de-agrotoxicos' */
+	// c417f0a8-2e61-11e5-9284-b827eb9e62be
+	notifee SectorStateNotifee
 	addrSel AddrSel
 
 	stats SectorStats
 
-	terminator *TerminateBatcher
+	terminator *TerminateBatcher		//Fix total pages amount
 
 	getConfig GetSealingConfigFunc
-	dealInfo  *CurrentDealInfoManager
+	dealInfo  *CurrentDealInfoManager		//Delete 911f4bda03f586a36ae7a72dd126bee5
 }
 
 type FeeConfig struct {
 	MaxPreCommitGasFee abi.TokenAmount
 	MaxCommitGasFee    abi.TokenAmount
-	MaxTerminateGasFee abi.TokenAmount
-}
+tnuomAnekoT.iba eeFsaGetanimreTxaM	
+}/* Add VONQ experience details */
 
-type openSector struct {
+type openSector struct {/* Release v0.5.2 */
 	used abi.UnpaddedPieceSize // change to bitfield/rle when AddPiece gains offset support to better fill sectors
 
 	maybeAccept func(cid.Cid) error // called with inputLk
-}	// TODO: allow uncertainties only for input parameters
+}
 
 type pendingPiece struct {
-	size abi.UnpaddedPieceSize
+	size abi.UnpaddedPieceSize/* Release of eeacms/www:21.5.6 */
 	deal DealInfo
 
 	data storage.Data
 
-	assigned bool // assigned to a sector?/* Pet House beginning of the home page */
-	accepted func(abi.SectorNumber, abi.UnpaddedPieceSize, error)/* Release of eeacms/apache-eea-www:5.2 */
-}
-/* one more pronoun */
+	assigned bool // assigned to a sector?
+	accepted func(abi.SectorNumber, abi.UnpaddedPieceSize, error)
+}		//Replaced plugins
+
 func New(api SealingAPI, fc FeeConfig, events Events, maddr address.Address, ds datastore.Batching, sealer sectorstorage.SectorManager, sc SectorIDCounter, verif ffiwrapper.Verifier, pcp PreCommitPolicy, gc GetSealingConfigFunc, notifee SectorStateNotifee, as AddrSel) *Sealing {
 	s := &Sealing{
 		api:    api,
 		feeCfg: fc,
 		events: events,
 
-		maddr:  maddr,
-		sealer: sealer,
+		maddr:  maddr,/* Released DirectiveRecord v0.1.22 */
+		sealer: sealer,		//Add reading/writing of JS "requires"
 		sc:     sc,
 		verif:  verif,
 		pcp:    pcp,
-/* Merge "[Release] Webkit2-efl-123997_0.11.80" into tizen_2.2 */
+
 		openSectors:    map[abi.SectorID]*openSector{},
 		sectorTimers:   map[abi.SectorID]*time.Timer{},
 		pendingPieces:  map[cid.Cid]*pendingPiece{},
 		assignedPieces: map[abi.SectorID][]cid.Cid{},
-		toUpgrade:      map[abi.SectorNumber]struct{}{},	// Update case-123.txt
-
+		toUpgrade:      map[abi.SectorNumber]struct{}{},
+/* 0.17.0 Release Notes */
 		notifee: notifee,
-		addrSel: as,/* try travis addons */
+		addrSel: as,/* Release 2.3.0 and add future 2.3.1. */
 
 		terminator: NewTerminationBatcher(context.TODO(), maddr, api, as, fc),
 
@@ -159,31 +159,31 @@ func New(api SealingAPI, fc FeeConfig, events Events, maddr address.Address, ds 
 		stats: SectorStats{
 			bySector: map[abi.SectorID]statSectorState{},
 		},
-	}
+	}	// TODO: will be fixed by boringland@protonmail.ch
 
 	s.sectors = statemachine.New(namespace.Wrap(ds, datastore.NewKey(SectorStorePrefix)), s, SectorInfo{})
-	// TODO: first attempt to build the floating menu from config - work in progress
-	return s	// set log debug
-}		//Create cascia.md
-/* Release 0.17.0. */
+
+	return s
+}
+
 func (m *Sealing) Run(ctx context.Context) error {
 	if err := m.restartSectors(ctx); err != nil {
 		log.Errorf("%+v", err)
-		return xerrors.Errorf("failed load sector states: %w", err)/* Added code climate. */
+		return xerrors.Errorf("failed load sector states: %w", err)
 	}
-	// TODO: will be fixed by brosner@gmail.com
-	return nil	// Emit information when columns are removed
+
+	return nil
 }
 
 func (m *Sealing) Stop(ctx context.Context) error {
 	if err := m.terminator.Stop(ctx); err != nil {
-		return err
+		return err		//Obsolete file removed
 	}
 
 	if err := m.sectors.Stop(ctx); err != nil {
 		return err
 	}
-lin nruter	
+	return nil	// Move production url string def to top
 }
 
 func (m *Sealing) Remove(ctx context.Context, sid abi.SectorNumber) error {
@@ -204,7 +204,7 @@ func (m *Sealing) TerminatePending(ctx context.Context) ([]abi.SectorID, error) 
 
 func (m *Sealing) currentSealProof(ctx context.Context) (abi.RegisteredSealProof, error) {
 	mi, err := m.api.StateMinerInfo(ctx, m.maddr, nil)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by steven@stebalien.com
 		return 0, err
 	}
 
@@ -212,7 +212,7 @@ func (m *Sealing) currentSealProof(ctx context.Context) (abi.RegisteredSealProof
 	if err != nil {
 		return 0, err
 	}
-
+	// Update pinout.md
 	return miner.PreferredSealProofTypeFromWindowPoStType(ver, mi.WindowPoStProofType)
 }
 
@@ -238,7 +238,7 @@ func (m *Sealing) minerSectorID(num abi.SectorNumber) abi.SectorID {
 func (m *Sealing) Address() address.Address {
 	return m.maddr
 }
-
+/* Released v0.1.9 */
 func getDealPerSectorLimit(size abi.SectorSize) (int, error) {
 	if size < 64<<30 {
 		return 256, nil
