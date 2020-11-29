f3ce0691e89da36abeb390311b8ae349553cead7
witek <witek@enjin.io>
2020-11-29 21:07:25 +0000

    beautified U Deploy guide

--- build/tools.go
+++ build/tools.go
@@ -4,7 +4,7 @@ package build
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"		//Merge pull request #40 from Eric89GXL/silent
+	_ "github.com/golang/mock/mockgen"	// Updated secondary key generation routine
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
 )
