88c46f97c26fba88c7fdbf69cbb0dd352e2d4b32
13860583249 <13860583249@yeah.net>
2020-11-29 02:25:41 +0000

    clerify names

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -2,7 +2,7 @@ package stmgr
 
 import (
 	"context"
-		//Fixed wrong function name
+
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
@@ -19,13 +19,13 @@ func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, e
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
-}	// Updated to php 5.6.21
+}
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)	// TODO: will be fixed by alessio@tendermint.com
+		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	return state, nil
@@ -53,14 +53,14 @@ func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, ts
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
 		return nil, err
-	}/* aef6ca84-2e55-11e5-9284-b827eb9e62be */
+	}
 	return state.GetActor(addr)
 }
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {		//Imported Debian patch 3.7.3-4.2
+	if err != nil {
 		return nil, err
-	}	// TODO: Update cozy-bar to 4.8.6
+	}
 	return state.GetActor(addr)
 }
