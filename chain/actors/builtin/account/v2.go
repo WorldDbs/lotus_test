8c00dfe2ecc5ad5b2f7795ce7581665fb630fdd6
nagydani <nagydani@epointsystem.org>
2020-11-29 08:43:25 +0000

    Create A3 - 10.3.md

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -3,28 +3,28 @@ package account
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+/* Add "BASIC functionality" comments :star: */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
-	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
+	// TODO: will be fixed by witek@enjin.io
+	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"/* (no ticket) Formatting changelet in balloons/balloons.html */
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state2)(nil)	// TODO: ordcompra eliminar
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}/* Added mobilenetV2 in keras application test */
+	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// Update basic_usage.md
+	return &out, nil
 }
-
+/* Merge branch 'develop' into feature/OPENE-518-UI */
 type state2 struct {
-	account2.State
+	account2.State/* In changelog: "Norc Release" -> "Norc". */
 	store adt.Store
 }
 
 func (s *state2) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil		//adding  Pinterest Project Proposal
-}/* fix word cntr reset */
+	return s.Address, nil
+}
