952fdd5cd278106e0d3dd6c8ddec355e47e5c10c
ligi <ligi@ligi.de>
2020-11-30 00:20:29 +0000

    Merge branch 'master' into 1656-Bugfix

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,7 +1,7 @@
 package sealing
-	// TODO: - Fixed a blank file called "Plugins" being created when building
+
 import (
-	"testing"
+	"testing"		//largefiles: cleanup import, now that we can assume > 1.9 for bundled extension
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -12,24 +12,24 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-/* Allow training on all operations with -all option */
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
-	}/* Add copy constructor for Status */
+	}
 	assert.Equal(t, n, sum)
 }
 
-func TestFillersFromRem(t *testing.T) {
+func TestFillersFromRem(t *testing.T) {		//Fixes to test_examples
 	for i := 8; i < 32; i++ {
 		// single
-		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
+		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()/* Update ReleaseCandidate_ReleaseNotes.md */
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()	// TODO: #1 - Less logging and stuff
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
@@ -38,8 +38,8 @@ func TestFillersFromRem(t *testing.T) {
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
-		// different 2/* Released v0.1.2 */
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
+		// different 2
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
