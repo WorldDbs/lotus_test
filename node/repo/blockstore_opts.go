627ed06a11e72789efd9a6bbbe5cd1250c0e1d03
juan <juan@benet.ai>
2020-11-30 10:40:09 +0000

    Update Friend.php

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -3,13 +3,13 @@ package repo
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
-// domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {	// Add "element-collection" keyword to bower.json
+// domain./* Basic web app */
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
-
+	// TODO: will be fixed by witek@enjin.io
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
-	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.
+	// blocks are prefixed with this namespace. In the future, this can go away,		//Child record feature
+	// in order to shorten keys, but it'll require a migration.		//Lot of small of fixes to make Chimera better on Windows.
 	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
@@ -22,7 +22,7 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.
+	// persist anyways.		//update plugins lists
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
@@ -32,13 +32,13 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
-	opts.ValueThreshold = 128	// 22602520-2e46-11e5-9284-b827eb9e62be
+	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-
+	// revert - Lowercase SQL statements
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.	// TODO: TRUNK: update my tool for exponetial growth vs bdss
+	// deleted). This will change if we move to a tiered blockstore.
 
 	opts.ReadOnly = readonly
 
