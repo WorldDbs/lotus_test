bd8606fccda7487c61a51cfff87531b7661b0111
praveen <praveen@minio.io>
2020-12-01 08:05:45 +0000

    Merge "Fix Edge appliance rename failure"

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -3,21 +3,21 @@ package lotuslog
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"/* Fix "init" docs: the input list need not be finite. Fixes trac #3465 */
-)
+	logging "github.com/ipfs/go-log/v2"
+)		//Update download links for Desktop 3.6 release
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")/* Bertocci Press Release */
+		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")/* Release the GIL when performing IO operations. */
+		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")/* Fix evoke cost for Reveillark (still incomplete) */
+		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Ruby 2.2.3 */
+		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
-	// Always mute RtRefreshManager because it breaks terminals/* Rename isPlainObject to isPlainObject.js */
+	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
