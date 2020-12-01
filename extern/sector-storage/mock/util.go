f42c00d421201cfe592167267947428c71265043
witek <witek@enjin.io>
2020-12-01 19:34:25 +0000

    Nettoyage de service_email_user

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
-func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {/* d59ce65e-2e74-11e5-9284-b827eb9e62be */
+func CommDR(in []byte) (out [32]byte) {/* Release 2.4.11: update sitemap */
+	for i, b := range in {
 		out[i] = ^b
-	}
+	}/* [events] add BlEvent>>#parentPosition */
 
 	return out
 }
