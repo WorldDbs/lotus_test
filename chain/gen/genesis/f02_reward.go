1ddb752374d7cea3b6b55305dfb8a842534ed544
lexy8russo <lexy8russo@outlook.com>
2020-12-01 22:44:47 +0000

    ADDED: Dispatch bundle

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,31 +1,31 @@
 package genesis
 
 import (
-	"context"
-
+	"context"		//Create SCCMCore.psm1
+	// eb4e36a2-2e41-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Update platform/domains.md */
-/* Update agi_mopublic_pub_mopublic_gebaeudeadresse.sql */
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Update psx.md */
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Released version 0.3.3 */
+)
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)/* Cambios menores */
-/* Update generalData.py */
+	cst := cbor.NewCborStore(bs)
+
 	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
-		return nil, err/* save to close function */
+		return nil, err
 	}
-
+/* add check for mw before checking config ( for pages wihout lib include ) */
 	return &types.Actor{
-		Code:    builtin.RewardActorCodeID,
+		Code:    builtin.RewardActorCodeID,/* - Release to get a DOI */
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
 	}, nil
