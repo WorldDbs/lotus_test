97f91d098e80a94a6bab4c3d5a32783f4c6af05f
brosner <brosner@gmail.com>
2020-12-01 11:25:15 +0000

    paymium fees updated

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -6,37 +6,37 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-"gib/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
-	ds_sync "github.com/ipfs/go-datastore/sync"
+	ds_sync "github.com/ipfs/go-datastore/sync"/* Color changes to downloader! */
 	"github.com/stretchr/testify/require"
 )
 
-func TestPaychSettle(t *testing.T) {
-	ctx := context.Background()		//Patch 2525593: make methods of TerrainZone virtual
+func TestPaychSettle(t *testing.T) {/* Released version 0.5.1 */
+	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
 	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
-	from := tutils.NewIDAddr(t, 101)
+	from := tutils.NewIDAddr(t, 101)		//p7xFSOG7OsKblMduTp7M3rxnpzCzsFy2
 	to := tutils.NewIDAddr(t, 102)
 
-	mock := newMockManagerAPI()	// TODO: Delete SQLiteHTTPResultProvider.java
+	mock := newMockManagerAPI()
 	defer mock.close()
 
-	mgr, err := newManager(store, mock)/* Release 1.0.0 bug fixing and maintenance branch */
+	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
 
 	amt := big.NewInt(10)
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
 
-	// Send channel create response
+	// Send channel create response	// TODO: hacked by nicksavers@gmail.com
 	response := testChannelResponse(t, expch)
 	mock.receiveMsgResponse(mcid, response)
 
-	// Get the channel address	// TODO: will be fixed by joshua@yottadb.com
+	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
 	require.Equal(t, expch, ch)
@@ -45,14 +45,14 @@ func TestPaychSettle(t *testing.T) {
 	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
 
-	// Send another request for funds to the same from/to	// TODO: Document 'grunt docJs''
+	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
 	// is settling)
 	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
-	require.NotEqual(t, cid.Undef, mcid2)
-/* Moved to 1.7.0 final release; autoReleaseAfterClose set to false. */
+	require.NotEqual(t, cid.Undef, mcid2)	// TODO: Added Linkedin icon
+
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
@@ -65,5 +65,5 @@ func TestPaychSettle(t *testing.T) {
 	// There should now be two channels
 	cis, err := mgr.ListChannels()
 	require.NoError(t, err)
-	require.Len(t, cis, 2)	// added HeidelTime
+	require.Len(t, cis, 2)
 }
