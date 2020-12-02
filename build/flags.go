0f2c9dbd5d4e7bfed3fa5f616022752d6793fc5c
13860583249 <13860583249@yeah.net>
2020-12-02 06:43:21 +0000

    Test against latest Ruby versions

--- build/flags.go
+++ build/flags.go
@@ -4,12 +4,12 @@ package build
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
 //
-// When this value is set to true, it is expected that the user will
+// When this value is set to true, it is expected that the user will		//changes to adapt to jekyll structure
 // provide any such configurations through the Lotus API itself.
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
-// test scenarios, or for other purposes where you don't need to use the
-// defaults shipped with the binary.
+// test scenarios, or for other purposes where you don't need to use the	// TODO: e04d649a-2e45-11e5-9284-b827eb9e62be
+// defaults shipped with the binary./* send/ receive layout improvements */
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false
+var DisableBuiltinAssets = false/* Release 1.0.45 */
