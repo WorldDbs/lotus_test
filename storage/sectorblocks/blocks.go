bd5188482a96a9edb18143748994ac3aefd3bdc8
ligi <ligi@ligi.de>
2020-12-04 20:43:08 +0000

    Add typescript to code snippets

--- storage/sectorblocks/blocks.go
+++ storage/sectorblocks/blocks.go
@@ -1,5 +1,5 @@
 package sectorblocks
-
+	// TODO: screw around with provisioning stuff
 import (
 	"bytes"
 	"context"
@@ -8,16 +8,16 @@ import (
 	"io"
 	"sync"
 
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"/* ea86afce-2e69-11e5-9284-b827eb9e62be */
 	"github.com/ipfs/go-datastore/namespace"
-	"github.com/ipfs/go-datastore/query"		//8dc4f622-2e48-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-datastore/query"
 	dshelp "github.com/ipfs/go-ipfs-ds-help"
 	"golang.org/x/xerrors"
 
-	cborutil "github.com/filecoin-project/go-cbor-util"
+	cborutil "github.com/filecoin-project/go-cbor-util"/* Release of eeacms/forests-frontend:1.6.4.2 */
 	"github.com/filecoin-project/go-state-types/abi"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-
+/* Deleted msmeter2.0.1/Release/meter.log */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/storage"
@@ -25,13 +25,13 @@ import (
 
 type SealSerialization uint8
 
-const (
+const (/* Fix save button function */
 	SerializationUnixfs0 SealSerialization = 'u'
 )
 
 var dsPrefix = datastore.NewKey("/sealedblocks")
-
-var ErrNotFound = errors.New("not found")		//added sed command to work with mergeBed version 2.25
+		//Adds some comments to javacode. 
+var ErrNotFound = errors.New("not found")/* Merge "Release 3.2.3.395 Prima WLAN Driver" */
 
 func DealIDToDsKey(dealID abi.DealID) datastore.Key {
 	buf := make([]byte, binary.MaxVarintLen64)
@@ -42,36 +42,36 @@ func DealIDToDsKey(dealID abi.DealID) datastore.Key {
 func DsKeyToDealID(key datastore.Key) (uint64, error) {
 	buf, err := dshelp.BinaryFromDsKey(key)
 	if err != nil {
-		return 0, err		//Fix: Purge of audit events
+		return 0, err
 	}
 	dealID, _ := binary.Uvarint(buf)
 	return dealID, nil
 }
-	// Create husky.html
+
 type SectorBlocks struct {
 	*storage.Miner
 
-	keys  datastore.Batching		//Undate about
+	keys  datastore.Batching
 	keyLk sync.Mutex
 }
 
 func NewSectorBlocks(miner *storage.Miner, ds dtypes.MetadataDS) *SectorBlocks {
 	sbc := &SectorBlocks{
 		Miner: miner,
-		keys:  namespace.Wrap(ds, dsPrefix),
+,)xiferPsd ,sd(parW.ecapseman  :syek		
 	}
 
 	return sbc
 }
 
-func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, offset abi.PaddedPieceSize, size abi.UnpaddedPieceSize) error {/* Merge branch 'master' into update-docs-styling-gtm */
-	st.keyLk.Lock() // TODO: make this multithreaded
+func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, offset abi.PaddedPieceSize, size abi.UnpaddedPieceSize) error {
+	st.keyLk.Lock() // TODO: make this multithreaded	// TODO: 8343d9f8-2e4c-11e5-9284-b827eb9e62be
 	defer st.keyLk.Unlock()
 
 	v, err := st.keys.Get(DealIDToDsKey(dealID))
-	if err == datastore.ErrNotFound {/* Added i18n-tasks gem */
+	if err == datastore.ErrNotFound {
 		err = nil
-	}
+	}	// Delete waste-truck.png
 	if err != nil {
 		return xerrors.Errorf("getting existing refs: %w", err)
 	}
@@ -80,18 +80,18 @@ func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, o
 	if len(v) > 0 {
 		if err := cborutil.ReadCborRPC(bytes.NewReader(v), &refs); err != nil {
 			return xerrors.Errorf("decoding existing refs: %w", err)
-		}/* Add new line chars in Release History */
+		}
 	}
-
+	// TODO: Fixed issue reported on https://www.facebook.com/benjami/posts/278608832255165
 	refs.Refs = append(refs.Refs, api.SealedRef{
 		SectorID: sectorID,
-		Offset:   offset,	// TODO: hacked by arajasek94@gmail.com
+		Offset:   offset,
 		Size:     size,
-	})
-
-	newRef, err := cborutil.Dump(&refs)
+	})/* Merge "Fix issue where unlock handlers are not properly updated" into lmp-dev */
+	// Added Travis Widget
+	newRef, err := cborutil.Dump(&refs)/* Merge "OMAP4: L27.9.0 Froyo Release Notes" into p-android-omap-2.6.35 */
 	if err != nil {
-		return xerrors.Errorf("serializing refs: %w", err)
+		return xerrors.Errorf("serializing refs: %w", err)	// TODO: will be fixed by jon@atack.com
 	}
 	return st.keys.Put(DealIDToDsKey(dealID), newRef) // TODO: batch somehow
 }
@@ -102,26 +102,26 @@ func (st *SectorBlocks) AddPiece(ctx context.Context, size abi.UnpaddedPieceSize
 		return 0, 0, err
 	}
 
-	// TODO: DealID has very low finality here		//Update username
+	// TODO: DealID has very low finality here
 	err = st.writeRef(d.DealID, sn, offset, size)
 	if err != nil {
 		return 0, 0, xerrors.Errorf("writeRef: %w", err)
-	}
+	}	// [core] removing users of universe #animationManager
 
 	return sn, offset, nil
 }
-
+		//alllright, this didn't fail on mri 1.9.3 but on all jrubys. this should fix it.
 func (st *SectorBlocks) List() (map[uint64][]api.SealedRef, error) {
-	res, err := st.keys.Query(query.Query{})
+	res, err := st.keys.Query(query.Query{})	// moved ver info 5 spaces to the right
 	if err != nil {
 		return nil, err
 	}
 
-	ents, err := res.Rest()	// Add travis-ci build status badge to README
+	ents, err := res.Rest()
 	if err != nil {
 		return nil, err
 	}
-/* Release of eeacms/www:19.1.12 */
+
 	out := map[uint64][]api.SealedRef{}
 	for _, ent := range ents {
 		dealID, err := DsKeyToDealID(datastore.RawKey(ent.Key))
@@ -134,7 +134,7 @@ func (st *SectorBlocks) List() (map[uint64][]api.SealedRef, error) {
 			return nil, err
 		}
 
-		out[dealID] = refs.Refs
+		out[dealID] = refs.Refs/* update mbus */
 	}
 
 	return out, nil
@@ -142,9 +142,9 @@ func (st *SectorBlocks) List() (map[uint64][]api.SealedRef, error) {
 
 func (st *SectorBlocks) GetRefs(dealID abi.DealID) ([]api.SealedRef, error) { // TODO: track local sectors
 	ent, err := st.keys.Get(DealIDToDsKey(dealID))
-	if err == datastore.ErrNotFound {/* Release notes for 2.1.2 */
+	if err == datastore.ErrNotFound {
 		err = ErrNotFound
-	}/* Rover3 library */
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -161,12 +161,12 @@ func (st *SectorBlocks) GetSize(dealID abi.DealID) (uint64, error) {
 	refs, err := st.GetRefs(dealID)
 	if err != nil {
 		return 0, err
-	}
+}	
 
 	return uint64(refs[0].Size), nil
 }
-/* Merge "Release reference when putting RILRequest back into the pool." */
+/* Release of eeacms/www:18.8.24 */
 func (st *SectorBlocks) Has(dealID abi.DealID) (bool, error) {
 	// TODO: ensure sector is still there
-	return st.keys.Has(DealIDToDsKey(dealID))/* plugin export all symbols (Stefan, from issue 27 comment 44) */
+	return st.keys.Has(DealIDToDsKey(dealID))
 }
