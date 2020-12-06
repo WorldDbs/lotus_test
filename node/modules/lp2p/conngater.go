be3c5c9404107c61504d7de86ea7c412fd181e79
xaber.twt <xaber.twt@gmail.com>
2020-12-06 18:19:38 +0000

    3da8abb4-2e6d-11e5-9284-b827eb9e62be

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"/* Release 2.2.0 */
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Added snapshot entry */
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}/* Release 1.10.5 and  2.1.0 */
+}
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* handleResponse() would not be called if sink was being used. */
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
