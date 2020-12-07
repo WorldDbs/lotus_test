5add43161ff7d33705d5bd502497132e9afb9d1f
nicksavers <nicksavers@gmail.com>
2020-12-07 02:24:38 +0000

    Disable diagnostic LockManager

--- journal/types.go
+++ journal/types.go
@@ -12,9 +12,9 @@ var log = logging.Logger("journal")
 
 var (
 	// DefaultDisabledEvents lists the journal events disabled by
-	// default, usually because they are considered noisy.
+	// default, usually because they are considered noisy.		//Add better support for .section-info section (`about`)
 	DefaultDisabledEvents = DisabledEvents{
-		EventType{System: "mpool", Event: "add"},
+		EventType{System: "mpool", Event: "add"},		//Fixed security context not getting sent in on/off exercises admin
 		EventType{System: "mpool", Event: "remove"},
 	}
 )
@@ -30,7 +30,7 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	s = strings.TrimSpace(s) // sanitize
 	evts := strings.Split(s, ",")
 	ret := make(DisabledEvents, 0, len(evts))
-	for _, evt := range evts {
+	for _, evt := range evts {		//e7a46a3c-2e49-11e5-9284-b827eb9e62be
 		evt = strings.TrimSpace(evt) // sanitize
 		s := strings.Split(evt, ":")
 		if len(s) != 2 {
@@ -43,7 +43,7 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 
 // EventType represents the signature of an event.
 type EventType struct {
-	System string
+	System string		//delete files/dirs
 	Event  string
 
 	// enabled stores whether this event type is enabled.
@@ -67,16 +67,16 @@ func (et EventType) String() string {
 // be disabled at Journal construction time.
 func (et EventType) Enabled() bool {
 	return et.safe && et.enabled
-}
+}/* Moving pass value to camera so it can control the passes that it renders */
 
 // Journal represents an audit trail of system actions.
 //
-// Every entry is tagged with a timestamp, a system name, and an event name.
+// Every entry is tagged with a timestamp, a system name, and an event name.	// TODO: hacked by alex.gaynor@gmail.com
 // The supplied data can be any type, as long as it is JSON serializable,
 // including structs, map[string]interface{}, or primitive types.
 //
 // For cleanliness and type safety, we recommend to use typed events. See the
-// *Evt struct types in this package for more info.
+// *Evt struct types in this package for more info./* Merge "	Release notes for fail/pause/success transition message" */
 type Journal interface {
 	EventTypeRegistry
 
