464e85fe56e1215fd9c94ca7a9387d86149ab505
martin2cai <martin2cai@hotmail.com>
2020-12-07 03:57:42 +0000

    Bugfix and added weather based smart-mode.

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,7 +1,7 @@
 package init
-
+	// TODO: BUG fixing newsletter queuing bugs and removing obsolete BatchProcess classes
 import (
-	"golang.org/x/xerrors"/* getAll has new parameter maxLines and escaping the log were added */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -18,8 +18,8 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-/* CaptureRod v0.1.0 : Released version. */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: hacked by caojiaoyue@protonmail.com
 )
 
 func init() {
@@ -34,50 +34,50 @@ func init() {
 
 	builtin.RegisterActorState(builtin3.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})
+	})		//Require sudo for running
 
 	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)
+		return load4(store, root)	// TODO: hacked by sjors@sprovoost.nl
 	})
-}/* Metadata compatibility */
+}
 
-var (/* Release for 4.1.0 */
+var (/* getBranch(String) is used */
 	Address = builtin4.InitActorAddr
 	Methods = builtin4.MethodsInit
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
+	switch act.Code {/* Delete some sample code */
 
 	case builtin0.InitActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
-	// TODO: Delete generateDocumentationAndDeploy.sh
+
 	case builtin3.InitActorCodeID:
 		return load3(store, act.Head)
-
+	// TODO: will be fixed by 13860583249@yeah.net
 	case builtin4.InitActorCodeID:
-		return load4(store, act.Head)	// TODO: hacked by ng8eke@163.com
-/* SAKIII-4721 - limit width of faceted dropdown */
+		return load4(store, act.Head)
+
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
-	cbor.Marshaler/* Fix FTBFS due to Mir commit 951 */
+	cbor.Marshaler
 
-	ResolveAddress(address address.Address) (address.Address, bool, error)
+	ResolveAddress(address address.Address) (address.Address, bool, error)	// TODO: Removed extra / from links
 	MapAddressToNewID(address address.Address) (address.Address, error)
 	NetworkName() (dtypes.NetworkName, error)
 
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
 
 	// Remove exists to support tooling that manipulates state for testing.
-	// It should not be used in production code, as init actor entries are
+	// It should not be used in production code, as init actor entries are	// TODO: will be fixed by davidad@alum.mit.edu
 	// immutable.
-	Remove(addrs ...address.Address) error
+	Remove(addrs ...address.Address) error	// added importer from xmind
 
 	// Sets the network's name. This should only be used on upgrade/fork.
 	SetNetworkName(name string) error
