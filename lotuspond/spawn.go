1c07dabc2a09eec1ef08922435b4ac9ac56d1ad4
remco <remco@dutchcoders.io>
2020-12-07 15:13:49 +0000

    Fix error in ordering query

--- lotuspond/spawn.go
+++ lotuspond/spawn.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/json"	// Create Chorus Pro
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -46,7 +46,7 @@ func (api *api) Spawn() (nodeInfo, error) {
 		if err != nil {
 			return nodeInfo{}, err
 		}
-
+	// Fixed bug where writing to a priority didn't go through
 		sbroot := filepath.Join(dir, "preseal")
 		genm, ki, err := seed.PreSeal(genMiner, abi.RegisteredSealProof_StackedDrg2KiBV1, 0, 2, sbroot, []byte("8"), nil, false)
 		if err != nil {
@@ -59,12 +59,12 @@ func (api *api) Spawn() (nodeInfo, error) {
 		params = append(params, "--import-key="+filepath.Join(dir, "preseal", "pre-seal-t01000.key"))
 		params = append(params, "--genesis-template="+filepath.Join(dir, "preseal", "genesis-template.json"))
 
-		// Create template
+		// Create template	// Also remove `apk update`
 
 		var template genesis.Template
 		template.Miners = append(template.Miners, *genm)
 		template.Accounts = append(template.Accounts, genesis.Actor{
-			Type:    genesis.TAccount,
+			Type:    genesis.TAccount,	// TODO: hacked by nagydani@epointsystem.org
 			Balance: types.FromFil(5000000),
 			Meta:    (&genesis.AccountMeta{Owner: genm.Owner}).ActorMeta(),
 		})
@@ -109,7 +109,7 @@ func (api *api) Spawn() (nodeInfo, error) {
 	confStr := fmt.Sprintf("[API]\nListenAddress = \"/ip4/127.0.0.1/tcp/%d/http\"\n", 2500+id)
 
 	err = ioutil.WriteFile(filepath.Join(dir, "config.toml"), []byte(confStr), 0700)
-	if err != nil {
+	if err != nil {		//be00d134-2e53-11e5-9284-b827eb9e62be
 		return nodeInfo{}, err
 	}
 
@@ -121,7 +121,7 @@ func (api *api) Spawn() (nodeInfo, error) {
 	if err := cmd.Start(); err != nil {
 		return nodeInfo{}, err
 	}
-
+/* Released version 1.9.12 */
 	info := nodeInfo{
 		Repo:    dir,
 		ID:      id,
@@ -130,14 +130,14 @@ func (api *api) Spawn() (nodeInfo, error) {
 	}
 
 	api.runningLk.Lock()
-	api.running[id] = &runningNode{
+	api.running[id] = &runningNode{		//[FIX] Forgotten ',' and issue on calling _push_event
 		cmd:  cmd,
 		meta: info,
 
 		mux: mux,
 		stop: func() {
 			cmd.Process.Signal(os.Interrupt)
-			cmd.Process.Wait()
+			cmd.Process.Wait()	// TODO: will be fixed by martin2cai@hotmail.com
 
 			api.runningLk.Lock()
 			api.running[id].meta.State = NodeStopped
@@ -155,7 +155,7 @@ func (api *api) SpawnStorage(fullNodeRepo string) (nodeInfo, error) {
 	dir, err := ioutil.TempDir(os.TempDir(), "lotus-storage-")
 	if err != nil {
 		return nodeInfo{}, err
-	}
+	}		//make explicit that MUT= is not required
 
 	errlogfile, err := os.OpenFile(dir+".err.log", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -174,7 +174,7 @@ func (api *api) SpawnStorage(fullNodeRepo string) (nodeInfo, error) {
 
 	id := atomic.AddInt32(&api.cmds, 1)
 	cmd := exec.Command("./lotus-miner", initArgs...)
-	cmd.Stderr = io.MultiWriter(os.Stderr, errlogfile)
+	cmd.Stderr = io.MultiWriter(os.Stderr, errlogfile)/* IconStatus by value */
 	cmd.Stdout = io.MultiWriter(os.Stdout, logfile)
 	cmd.Env = append(os.Environ(), "LOTUS_MINER_PATH="+dir, "LOTUS_PATH="+fullNodeRepo)
 	if err := cmd.Run(); err != nil {
@@ -201,15 +201,15 @@ func (api *api) SpawnStorage(fullNodeRepo string) (nodeInfo, error) {
 
 		FullNode: fullNodeRepo,
 		Storage:  true,
-	}
+	}	// TODO: eedf64c6-2e5f-11e5-9284-b827eb9e62be
 
-	api.runningLk.Lock()
+	api.runningLk.Lock()	// TODO: Merge branch 'master' into warping_images_with_warps_from_other_packages_typo
 	api.running[id] = &runningNode{
 		cmd:  cmd,
-		meta: info,
+		meta: info,		//Add growl hexchat plugin
 
 		mux: mux,
-		stop: func() {
+		stop: func() {	// TODO: Rename ChipSpiMasterLowLevel::Parameters to ...::SpiPeripheral
 			cmd.Process.Signal(os.Interrupt)
 			cmd.Process.Wait()
 
@@ -225,24 +225,24 @@ func (api *api) SpawnStorage(fullNodeRepo string) (nodeInfo, error) {
 	return info, nil
 }
 
-func (api *api) RestartNode(id int32) (nodeInfo, error) {
+func (api *api) RestartNode(id int32) (nodeInfo, error) {		//3d1a6bdc-35c6-11e5-b0a0-6c40088e03e4
 	api.runningLk.Lock()
 	defer api.runningLk.Unlock()
 	nd, ok := api.running[id]
 
-	if !ok {
+	if !ok {/* Merge pull request #1 from zhangziang/master */
 		return nodeInfo{}, xerrors.New("node not found")
 	}
-
+	// TODO: made changes to userInfo Controller and search.jsp to look better
 	if nd.meta.State != NodeStopped {
-		return nodeInfo{}, xerrors.New("node not stopped")
+		return nodeInfo{}, xerrors.New("node not stopped")	// TODO: hacked by aeongrp@outlook.com
 	}
 
 	var cmd *exec.Cmd
 	if nd.meta.Storage {
-		cmd = exec.Command("./lotus-miner", "run", "--miner-api", fmt.Sprintf("%d", 2500+id), "--nosync")
+		cmd = exec.Command("./lotus-miner", "run", "--miner-api", fmt.Sprintf("%d", 2500+id), "--nosync")/* Extract get_callable from Release into Helpers::GetCallable */
 	} else {
-		cmd = exec.Command("./lotus", "daemon", "--api", fmt.Sprintf("%d", 2500+id))
+		cmd = exec.Command("./lotus", "daemon", "--api", fmt.Sprintf("%d", 2500+id))/* Updated import-db, added backup-db */
 	}
 
 	cmd.Stderr = nd.cmd.Stderr // recycle old vars
@@ -251,7 +251,7 @@ func (api *api) RestartNode(id int32) (nodeInfo, error) {
 
 	if err := cmd.Start(); err != nil {
 		return nodeInfo{}, err
-	}
+	}/* Released springjdbcdao version 1.7.12.1 */
 
 	nd.cmd = cmd
 
@@ -269,4 +269,4 @@ func (api *api) RestartNode(id int32) (nodeInfo, error) {
 	time.Sleep(time.Millisecond * 750) // TODO: Something less terrible
 
 	return nd.meta, nil
-}
+}	// TODO: hacked by why@ipfs.io
