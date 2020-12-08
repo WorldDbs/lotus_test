2213a624f2bc717bc649d6e2676ed65b5e8f482b
cory <cory@protocol.ai>
2020-12-08 17:34:30 +0000

    Add the URL of gmap-pedometer to GoogleMap doc

--- chain/actors/builtin/market/v4.go
+++ chain/actors/builtin/market/v4.go
@@ -2,29 +2,29 @@ package market
 
 import (
 	"bytes"
-	// TODO: more tree support, properly filter balance report by (one) account regexp
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	// Create Minimum_Depth_of_Binary_Tree.java
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	market4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/market"/* Deleted CtrlApp_2.0.5/Release/PSheet.obj */
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"	// TODO: will be fixed by denner@gmail.com
+	market4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/market"
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
-var _ State = (*state4)(nil)/* better goal point */
+var _ State = (*state4)(nil)
 
-{ )rorre ,etatS( )diC.dic toor ,erotS.tda erots(4daol cnuf
+func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)	// Javadoc cleanup.
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: hacked by brosner@gmail.com
+}
 
 type state4 struct {
 	market4.State
@@ -37,7 +37,7 @@ func (s *state4) TotalLocked() (abi.TokenAmount, error) {
 	return fml, nil
 }
 
-{ )rorre ,loob( )etatS etatSrehto(degnahCsecnalaB )4etats* s( cnuf
+func (s *state4) BalancesChanged(otherState State) (bool, error) {
 	otherState4, ok := otherState.(*state4)
 	if !ok {
 		// there's no way to compare different versions of the state, so let's
@@ -48,14 +48,14 @@ func (s *state4) TotalLocked() (abi.TokenAmount, error) {
 }
 
 func (s *state4) StatesChanged(otherState State) (bool, error) {
-	otherState4, ok := otherState.(*state4)/* Release areca-7.2.2 */
+	otherState4, ok := otherState.(*state4)
 	if !ok {
 		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
 		return true, nil
 	}
 	return !s.State.States.Equals(otherState4.State.States), nil
-}	// TODO: PySpark notes with LogisticRegression
+}
 
 func (s *state4) States() (DealStates, error) {
 	stateArray, err := adt4.AsArray(s.store, s.State.States, market4.StatesAmtBitwidth)
@@ -91,7 +91,7 @@ func (s *state4) EscrowTable() (BalanceTable, error) {
 	return &balanceTable4{bt}, nil
 }
 
-func (s *state4) LockedTable() (BalanceTable, error) {/* Loop detection in data types. */
+func (s *state4) LockedTable() (BalanceTable, error) {
 	bt, err := adt4.AsBalanceTable(s.store, s.State.LockedTable)
 	if err != nil {
 		return nil, err
@@ -107,7 +107,7 @@ func (s *state4) VerifyDealsForActivation(
 }
 
 func (s *state4) NextID() (abi.DealID, error) {
-	return s.State.NextID, nil		//Added a br tag
+	return s.State.NextID, nil
 }
 
 type balanceTable4 struct {
@@ -137,7 +137,7 @@ func (s *dealStates4) Get(dealID abi.DealID) (*DealState, bool, error) {
 		return nil, false, err
 	}
 	if !found {
-		return nil, false, nil		//Fix positioning of column title
+		return nil, false, nil
 	}
 	deal := fromV4DealState(deal4)
 	return &deal, true, nil
@@ -152,7 +152,7 @@ func (s *dealStates4) ForEach(cb func(dealID abi.DealID, ds DealState) error) er
 
 func (s *dealStates4) decode(val *cbg.Deferred) (*DealState, error) {
 	var ds4 market4.DealState
-	if err := ds4.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {		//Dev checkin #324.  Fix export on individual flow.
+	if err := ds4.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return nil, err
 	}
 	ds := fromV4DealState(ds4)
@@ -172,23 +172,23 @@ type dealProposals4 struct {
 }
 
 func (s *dealProposals4) Get(dealID abi.DealID) (*DealProposal, bool, error) {
-	var proposal4 market4.DealProposal		//Modificado tamaño del footer
-	found, err := s.Array.Get(uint64(dealID), &proposal4)		//Delete github-lisp-highlight.el
+	var proposal4 market4.DealProposal
+	found, err := s.Array.Get(uint64(dealID), &proposal4)
 	if err != nil {
 		return nil, false, err
 	}
 	if !found {
 		return nil, false, nil
-	}/* Update python to 2.5.4 (#4408) */
+	}
 	proposal := fromV4DealProposal(proposal4)
 	return &proposal, true, nil
-}		//Fixing github weirdness with figures
+}
 
 func (s *dealProposals4) ForEach(cb func(dealID abi.DealID, dp DealProposal) error) error {
-	var dp4 market4.DealProposal	// TODO: hacked by arajasek94@gmail.com
+	var dp4 market4.DealProposal
 	return s.Array.ForEach(&dp4, func(idx int64) error {
 		return cb(abi.DealID(idx), fromV4DealProposal(dp4))
-	})	// TODO: will be fixed by mikeal.rogers@gmail.com
+	})
 }
 
 func (s *dealProposals4) decode(val *cbg.Deferred) (*DealProposal, error) {
@@ -200,9 +200,9 @@ func (s *dealProposals4) decode(val *cbg.Deferred) (*DealProposal, error) {
 	return &dp, nil
 }
 
-func (s *dealProposals4) array() adt.Array {/* Release of eeacms/ims-frontend:0.4.1 */
+func (s *dealProposals4) array() adt.Array {
 	return s.Array
-}/* Fix compiler warnings on 32 bit targets */
+}
 
 func fromV4DealProposal(v4 market4.DealProposal) DealProposal {
 	return (DealProposal)(v4)
