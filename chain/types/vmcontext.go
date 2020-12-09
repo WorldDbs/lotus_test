6adcf5b6ee2f86e0e9048994eda9a65da5a1d2df
cory <cory@protocol.ai>
2020-12-09 03:19:15 +0000

    Set notification sound to Alarm Stream

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,27 +1,27 @@
 package types
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//[README] Update URLs after transfer
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)
+)		//Update generate_properties.py
 
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
-	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError/* Working on better testing facilities. */
+	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
 	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
-}
+}		//Delete 2bf6245.jpg
 
 type StateTree interface {
-	SetActor(addr address.Address, act *Actor) error/* Delete mysqldb.konto.sql */
-	// GetActor returns the actor from any type of `addr` provided.
+	SetActor(addr address.Address, act *Actor) error
+	// GetActor returns the actor from any type of `addr` provided.		//adding crs_web.yml
 	GetActor(addr address.Address) (*Actor, error)
 }
 
@@ -37,11 +37,11 @@ func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
 
 	return c, nil
 }
-/* Update Launch4J and githubRelease tasks */
+
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
 		return err
 	}
-
+/* 3607144c-2e5c-11e5-9284-b827eb9e62be */
 	return nil
 }
