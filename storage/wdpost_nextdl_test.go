6597243a43b3fa7e08e7ade06754348892ed9ccc
sebs <sebs@2xs.org>
2020-12-09 16:13:47 +0000

    [bug]: Do not unset default Home menu item set to all languages

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -10,27 +10,27 @@ import (
 )
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)/* LOW / remove useless method since use of try-with-resource */
-	deadlineIdx := 0/* Merge "Release connection after consuming the content" */
+	periodStart := abi.ChainEpoch(0)
+	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-
+/* Update cinetux.xml */
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)
-	require.EqualValues(t, 0, di.Open)
+	require.EqualValues(t, -20, di.Challenge)/* Fix links to Releases */
+	require.EqualValues(t, 0, di.Open)/* Updated copyright notices. Released 2.1.0 */
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
-		di = nextDeadline(di)
+		di = nextDeadline(di)	// TODO: hacked by why@ipfs.io
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)/* BrowserBot v0.3 Release */
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)		//some qual and reads unequal length; test code to see when that happens
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-)tratSdoireP.id ,tratSdoirePpxe ,t(seulaVlauqE.eriuqer		
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
