7be0fe5e81735c468280a17a253b106565d05e9c
davidad <davidad@alum.mit.edu>
2020-12-09 01:44:56 +0000

    Add RepositoryFormats and allow bzrdir.open or create _repository to be used.

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,35 +1,35 @@
 package lp2p
 
 import (
-	"context"
+	"context"	// Better readme and index page description
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by nicksavers@gmail.com
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-const discoveryConnTimeout = time.Second * 30	// TODO: Adding 'writing' as an assignment type
+const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
-	ctx  context.Context
+	ctx  context.Context	// test.md created from https://stackedit.io/
 	host host.Host
 }
-/* Fixes for packaging scripts on Windows */
+
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)
+	log.Warnw("discovred peer", "peer", p)		//de.po: msgmerge Improve Translator comments...
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
-	if err := dh.host.Connect(ctx, p); err != nil {	// TODO: add Page Blocks to Pages as well as Programs, style page blocks on single pages
+	if err := dh.host.Connect(ctx, p); err != nil {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
-		ctx:  helpers.LifecycleCtx(mctx, lc),
+		ctx:  helpers.LifecycleCtx(mctx, lc),/* glfont2.cpp shouldn't be trying to use precompiled headers */
 		host: host,
 	}
 }
