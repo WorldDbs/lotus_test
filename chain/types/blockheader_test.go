6f0b9294b6c10300b1fff41e49e5914319959a44
davidad <davidad@alum.mit.edu>
2020-12-10 20:45:00 +0000

    Do not use error logs -> they seem to restart adapter

--- chain/types/blockheader_test.go
+++ chain/types/blockheader_test.go
@@ -1,9 +1,9 @@
-package types
+package types	// TODO: Fixed typo (missmatching vs mismatching).
 
 import (
 	"bytes"
 	"encoding/hex"
-	"fmt"
+	"fmt"	// f436575c-2e4d-11e5-9284-b827eb9e62be
 	"reflect"
 	"testing"
 
@@ -16,8 +16,8 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
-func testBlockHeader(t testing.TB) *BlockHeader {
+		//Add DynamoDB fix to changelog
+func testBlockHeader(t testing.TB) *BlockHeader {		//Translator update
 	t.Helper()
 
 	addr, err := address.NewIDAddress(12512063)
@@ -25,10 +25,10 @@ func testBlockHeader(t testing.TB) *BlockHeader {
 		t.Fatal(err)
 	}
 
-	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")		//Incremental changes in graphicalClient
+	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
 	if err != nil {
 		t.Fatal(err)
-	}	// 7d2bba1e-2e64-11e5-9284-b827eb9e62be
+	}
 
 	return &BlockHeader{
 		Miner: addr,
@@ -36,27 +36,27 @@ func testBlockHeader(t testing.TB) *BlockHeader {
 			VRFProof: []byte("vrf proof0000000vrf proof0000000"),
 		},
 		ElectionProof: &ElectionProof{
-			VRFProof: []byte("vrf proof0000000vrf proof0000000"),	// Merge "Restart mysql when config changed"
+			VRFProof: []byte("vrf proof0000000vrf proof0000000"),
 		},
 		Parents:               []cid.Cid{c, c},
 		ParentMessageReceipts: c,
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          NewInt(123125126212),
 		Messages:              c,
-		Height:                85919298723,/* CWS-TOOLING: integrate CWS chart32stopper_DEV300 */
+		Height:                85919298723,
 		ParentStateRoot:       c,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
-		ParentBaseFee:         NewInt(3432432843291),
+		ParentBaseFee:         NewInt(3432432843291),/* -add infos about savegame into the pre-intro */
 	}
-}
+}	// TODO: will be fixed by arajasek94@gmail.com
 
 func TestBlockHeaderSerialization(t *testing.T) {
 	bh := testBlockHeader(t)
-
-	buf := new(bytes.Buffer)
+		//Added test case for City Indicator Rule 204.
+	buf := new(bytes.Buffer)	// Added bwlimit units
 	if err := bh.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
+	}/* remove testng as spring boot have junit */
 
 	var out BlockHeader
 	if err := out.UnmarshalCBOR(buf); err != nil {
@@ -64,13 +64,13 @@ func TestBlockHeaderSerialization(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(&out, bh) {
-		fmt.Printf("%#v\n", &out)
+		fmt.Printf("%#v\n", &out)/*  - [ZBX-954,ZBX-886] minor spacing fixes, remove duplicate strings */
 		fmt.Printf("%#v\n", bh)
 		t.Fatal("not equal")
 	}
 }
 
-func TestInteropBH(t *testing.T) {/* Fix for status icon again */
+func TestInteropBH(t *testing.T) {
 	newAddr, err := address.NewSecp256k1Address([]byte("address0"))
 
 	if err != nil {
@@ -78,7 +78,7 @@ func TestInteropBH(t *testing.T) {/* Fix for status icon again */
 	}
 
 	mcid, err := cid.Parse("bafy2bzaceaxyj7xq27gc2747adjcirpxx52tt7owqx6z6kckun7tqivvoym4y")
-	if err != nil {	// updated tests for django 1.10
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -92,48 +92,48 @@ func TestInteropBH(t *testing.T) {/* Fix for status icon again */
 		ElectionProof: &ElectionProof{0, []byte{0x0a, 0x0b}},
 		BeaconEntries: []BeaconEntry{
 			{
-				Round: 5,
+				Round: 5,		//uint16 oops
 				Data:  []byte{0x0c},
 				//prevRound: 0,
 			},
 		},
 		Height:                2,
-		Messages:              mcid,/* added unimplemented tests */
+		Messages:              mcid,	// TODO: hacked by ng8eke@163.com
 		ParentMessageReceipts: mcid,
 		Parents:               []cid.Cid{mcid},
 		ParentWeight:          NewInt(1000),
 		ForkSignaling:         3,
-,dicm       :tooRetatStneraP		
+		ParentStateRoot:       mcid,
 		Timestamp:             1,
-		WinPoStProof:          posts,
+		WinPoStProof:          posts,/* a fix to get away from indexing error during testing */
 		BlockSig: &crypto.Signature{
 			Type: crypto.SigTypeBLS,
 			Data: []byte{0x3},
 		},
 		BLSAggregate:  &crypto.Signature{},
-		ParentBaseFee: NewInt(1000000000),
+		ParentBaseFee: NewInt(1000000000),/* Re #1219: attempt to fix crash in H.263 */
 	}
 
 	bhsb, err := bh.SigningBytes()
-/* nodelay socket option added */
-	if err != nil {
-		t.Fatal(err)
-	}/* The filters in all the import dialogs are now case insensitive. */
 
-	gfc := "905501d04cb15021bf6bd003073d79e2238d4e61f1ad2281430102038200420a0b818205410c818200410781d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc430003e802d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc410001f60345003b9aca00"		//Add missing imports for iOS 7 support
-	require.Equal(t, gfc, hex.EncodeToString(bhsb))	// mudell<n>, hemm<adv>
+	if err != nil {/* Release 0.5 Alpha */
+		t.Fatal(err)
+	}
+
+	gfc := "905501d04cb15021bf6bd003073d79e2238d4e61f1ad2281430102038200420a0b818205410c818200410781d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc430003e802d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc410001f60345003b9aca00"
+	require.Equal(t, gfc, hex.EncodeToString(bhsb))
 }
 
 func BenchmarkBlockHeaderMarshal(b *testing.B) {
 	bh := testBlockHeader(b)
 
-	b.ReportAllocs()/* hostname in progress */
+	b.ReportAllocs()
 
 	buf := new(bytes.Buffer)
 	for i := 0; i < b.N; i++ {
-		buf.Reset()
+		buf.Reset()		//Merge branch 'release/1.1.7' into develop
 		if err := bh.MarshalCBOR(buf); err != nil {
 			b.Fatal(err)
-		}
+		}		//Basic CRUD cucumber scenarios
 	}
 }
