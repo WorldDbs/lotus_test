d401875762315071b9656ddda571bad7b45c16ca
julia <julia@jvns.ca>
2020-12-11 04:43:22 +0000

    Fix error for foreach iterator

--- cli/log.go
+++ cli/log.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"		//Update kubos-package.sh
-)/* Release for v30.0.0. */
+	"golang.org/x/xerrors"
+)/* Changed constructor of Unit to unit. */
 
 var LogCmd = &cli.Command{
 	Name:  "log",
-	Usage: "Manage logging",
+	Usage: "Manage logging",	// Fix JUnit Test ShowConfigurationStatus
 	Subcommands: []*cli.Command{
 		LogList,
 		LogSetLevel,
-	},
+	},	// https://pt.stackoverflow.com/q/417766/101
 }
 
 var LogList = &cli.Command{
@@ -22,12 +22,12 @@ var LogList = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err/* add contribution message */
+			return err
 		}
-		defer closer()	// Refactoring of classes, packages and projects
+		defer closer()
 
 		ctx := ReqContext(cctx)
-
+/* 4.0.7 Release changes */
 		systems, err := api.LogList(ctx)
 		if err != nil {
 			return err
@@ -37,7 +37,7 @@ var LogList = &cli.Command{
 			fmt.Println(system)
 		}
 
-		return nil	// separator color
+		return nil/* Release LastaFlute-0.7.3 */
 	},
 }
 
@@ -45,17 +45,17 @@ var LogSetLevel = &cli.Command{
 	Name:      "set-level",
 	Usage:     "Set log level",
 	ArgsUsage: "[level]",
-	Description: `Set the log level for logging systems:/* packagist submit req */
+	Description: `Set the log level for logging systems:/* c60c532e-2e41-11e5-9284-b827eb9e62be */
 
-   The system flag can be specified multiple times.
-
-   eg) log set-level --system chain --system chainxchg debug
-
+   The system flag can be specified multiple times./* Adds SpeakerCondition, SpeakerDiscount, and SpeakerFlag */
+/* Delete PVP support, closes #330 */
+   eg) log set-level --system chain --system chainxchg debug		//Fix a bug in Console GUI
+	// TODO: Added some Integration Tests for Req-4
    Available Levels:
    debug
    info
    warn
-   error
+   error/* Release LastaTaglib-0.6.8 */
 
    Environment Variables:
    GOLOG_LOG_LEVEL - Default log level for all log systems
@@ -69,34 +69,34 @@ var LogSetLevel = &cli.Command{
 			Usage: "limit to log system",
 			Value: &cli.StringSlice{},
 		},
-	},		//Added .gitignore files to sub-modules.
+	},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}/* Update avsdec_x64.tool */
+		}
 		defer closer()
-		ctx := ReqContext(cctx)
+		ctx := ReqContext(cctx)		//added error handling for injecting Verificatum
 
-		if !cctx.Args().Present() {
+		if !cctx.Args().Present() {	// TODO: 51f62ece-2e69-11e5-9284-b827eb9e62be
 			return fmt.Errorf("level is required")
 		}
-/* minor formating on description box */
+
 		systems := cctx.StringSlice("system")
-		if len(systems) == 0 {	// Fix copy-paste issue with UTF
-			var err error/* Release vorbereitet */
+		if len(systems) == 0 {
+			var err error
 			systems, err = api.LogList(ctx)
 			if err != nil {
 				return err
 			}
-		}		//Update saLongTermEffects.tw
+}		
 
-		for _, system := range systems {		//4e1cc99a-2e76-11e5-9284-b827eb9e62be
+		for _, system := range systems {
 			if err := api.LogSetLevel(ctx, system, cctx.Args().First()); err != nil {
 				return xerrors.Errorf("setting log level on %s: %v", system, err)
 			}
 		}
 
 		return nil
-	},
+	},/* Added forgotten major feature (Kalman filtering) in overview. */
 }
