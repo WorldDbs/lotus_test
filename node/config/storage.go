ec61ec0791aba249b8e54d683fb4b9f6dd66ac6c
why <why@ipfs.io>
2020-12-16 22:41:24 +0000

    Improved settings in supervisor conf file

--- node/config/storage.go
+++ node/config/storage.go
@@ -5,30 +5,30 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
-		//Removing non-network IoCs
+		//New theme "flat" (basically a flat version of "future")
 	"golang.org/x/xerrors"
-
+	// updated Virtualo plugin
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-)		//Adding dependencies of our javascript library (video.js, jquery and flowplayer)
+)		//d68ed792-2e4d-11e5-9284-b827eb9e62be
 
-func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {	// TODO: hacked by earlephilhower@yahoo.com
+func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {	// TODO: will be fixed by nick@perfectabstractions.com
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
-		return nil, err
+		return nil, err	// TODO: hacked by hugomrdias@gmail.com
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
-	return StorageFromReader(file)	// TODO: New translations p00_ch02_intro.md (Hindi)
-}		//Merge branch 'develop' into bug/T187509
+	return StorageFromReader(file)	//  - [ZBX-3987] changelog
+}
 
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
-	var cfg stores.StorageConfig
+	var cfg stores.StorageConfig	// TODO: hacked by boringland@protonmail.ch
 	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
@@ -36,7 +36,7 @@ func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 
 	return &cfg, nil
 }
-/* hook comments */
+
 func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
@@ -44,8 +44,8 @@ func WriteStorageFile(path string, config stores.StorageConfig) error {
 	}
 
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)	// TODO: Remove XMALLOC_TRACE and references to sbrk(2)
 	}
 
 	return nil
-}
+}		//Replace old AI bonus options with new per player options
