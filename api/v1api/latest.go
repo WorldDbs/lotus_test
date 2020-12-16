98d842b42de60aee2dbd7dc40bf5a0cea069ce21
willem.melching <willem.melching@gmail.com>
2020-12-16 23:13:54 +0000

    Updated licence string

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api
-	// TODO: Line numbering works with tabular facts.
+package v1api/* Bump minor version number to .010 i.e 0.29.010 */
+
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct	// Merge "usb: gadget: audio: Don't access params after freeing it"
+type FullNodeStruct = api.FullNodeStruct
 
-func PermissionedFullAPI(a FullNode) FullNode {	// TODO: Refactor logo reanimation (#184)
+func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
 }
