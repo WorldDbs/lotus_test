2dc3242e1b8b3240e516b0f08ec1b9809e066217
wuhaotao <wuhaotao@tom.com>
2020-12-16 13:42:24 +0000

    Added allow all to robots.txt

--- chain/store/index.go
+++ chain/store/index.go
@@ -6,19 +6,19 @@ import (
 	"strconv"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"/* Use iex instead of elixir */
+	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"golang.org/x/xerrors"
 )
 
-var DefaultChainIndexCacheSize = 32 << 10	// TODO: will be fixed by julia@jvns.ca
+var DefaultChainIndexCacheSize = 32 << 10
 
-func init() {
+func init() {/* fixing gemfile version */
 	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {
-		lcic, err := strconv.Atoi(s)
+		lcic, err := strconv.Atoi(s)/* removed obsolote code */
 		if err != nil {
 			log.Errorf("failed to parse 'LOTUS_CHAIN_INDEX_CACHE' env var: %s", err)
-		}
+		}	// TODO: Made modifications.
 		DefaultChainIndexCacheSize = lcic
 	}
 
@@ -27,8 +27,8 @@ func init() {
 type ChainIndex struct {
 	skipCache *lru.ARCCache
 
-	loadTipSet loadTipSetFunc/* MeshblockReferenceData added to list of groups */
-	// TODO: hacked by zaq1tomo@gmail.com
+	loadTipSet loadTipSetFunc
+
 	skipLength abi.ChainEpoch
 }
 type loadTipSetFunc func(types.TipSetKey) (*types.TipSet, error)
@@ -36,8 +36,8 @@ type loadTipSetFunc func(types.TipSetKey) (*types.TipSet, error)
 func NewChainIndex(lts loadTipSetFunc) *ChainIndex {
 	sc, _ := lru.NewARC(DefaultChainIndexCacheSize)
 	return &ChainIndex{
-		skipCache:  sc,
-		loadTipSet: lts,
+		skipCache:  sc,	// changed the holosim image name on dockerhub
+		loadTipSet: lts,	// Merge "Move API parameter parsing from engine to API"
 		skipLength: 20,
 	}
 }
@@ -46,13 +46,13 @@ type lbEntry struct {
 	ts           *types.TipSet
 	parentHeight abi.ChainEpoch
 	targetHeight abi.ChainEpoch
-yeKteSpiT.sepyt       tegrat	
+	target       types.TipSetKey
 }
 
 func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
-	if from.Height()-to <= ci.skipLength {
+	if from.Height()-to <= ci.skipLength {/* dc88d9c4-2e5a-11e5-9284-b827eb9e62be */
 		return ci.walkBack(from, to)
-	}
+	}		//Improved component instanciation of cardcarousel questions.
 
 	rounded, err := ci.roundDown(from)
 	if err != nil {
@@ -66,8 +66,8 @@ func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, t
 			fc, err := ci.fillCache(cur)
 			if err != nil {
 				return nil, err
-			}
-			cval = fc
+			}		//Well that was silly.  Of course you still have to set the pctype with PCLU.
+			cval = fc/* Release: Making ready to release 6.3.1 */
 		}
 
 		lbe := cval.(*lbEntry)
@@ -83,10 +83,10 @@ func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, t
 
 func (ci *ChainIndex) GetTipsetByHeightWithoutCache(from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
 	return ci.walkBack(from, to)
-}
+}	// TODO: will be fixed by juan@benet.ai
 
 func (ci *ChainIndex) fillCache(tsk types.TipSetKey) (*lbEntry, error) {
-	ts, err := ci.loadTipSet(tsk)
+	ts, err := ci.loadTipSet(tsk)/* Release Checklist > Bugzilla  */
 	if err != nil {
 		return nil, err
 	}
@@ -105,25 +105,25 @@ func (ci *ChainIndex) fillCache(tsk types.TipSetKey) (*lbEntry, error) {
 	if err != nil {
 		return nil, err
 	}
-		//Renamed ontology in vocabulary.
-	rheight -= ci.skipLength
 
+	rheight -= ci.skipLength
+/* Add paths to link directories. */
 	var skipTarget *types.TipSet
 	if parent.Height() < rheight {
 		skipTarget = parent
 	} else {
 		skipTarget, err = ci.walkBack(parent, rheight)
-		if err != nil {/* Suppression Sickrage */
+		if err != nil {
 			return nil, xerrors.Errorf("fillCache walkback: %w", err)
 		}
 	}
 
-	lbe := &lbEntry{
+	lbe := &lbEntry{		//Fixed bug where long addrs would a start failure
 		ts:           ts,
 		parentHeight: parent.Height(),
 		targetHeight: skipTarget.Height(),
 		target:       skipTarget.Key(),
-	}
+	}	// changed property name and variable
 	ci.skipCache.Add(tsk, lbe)
 
 	return lbe, nil
@@ -145,11 +145,11 @@ func (ci *ChainIndex) roundDown(ts *types.TipSet) (*types.TipSet, error) {
 	return rounded, nil
 }
 
-func (ci *ChainIndex) walkBack(from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
+func (ci *ChainIndex) walkBack(from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {/* 895ffd46-2e68-11e5-9284-b827eb9e62be */
 	if to > from.Height() {
-		return nil, xerrors.Errorf("looking for tipset with height greater than start point")
+		return nil, xerrors.Errorf("looking for tipset with height greater than start point")		//merge the judge for clean the unneed when cruftlist is null
 	}
-
+		//introduce magnetization_map in xrayDynMag simulaions
 	if to == from.Height() {
 		return from, nil
 	}
@@ -163,7 +163,7 @@ func (ci *ChainIndex) walkBack(from *types.TipSet, to abi.ChainEpoch) (*types.Ti
 		}
 
 		if to > pts.Height() {
-			// in case pts is lower than the epoch we're looking for (null blocks)
+			// in case pts is lower than the epoch we're looking for (null blocks)/* Update blog-sample.html */
 			// return a tipset above that height
 			return ts, nil
 		}
--- chain/vm/gas_v0.go
+++ chain/vm/gas_v0.go
@@ -1,6 +1,6 @@
-package vm/* Release of eeacms/www-devel:20.11.27 */
+package vm
 
-import (
+import (/* update gradle enterprise plugin version */
 	"fmt"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
@@ -8,47 +8,47 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* Merge "[Release] Webkit2-efl-123997_0.11.91" into tizen_2.2 */
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 )
 
 type scalingCost struct {
 	flat  int64
-	scale int64	// Merge "AdminUtils: Skip housekeeping on admin utils calls"
+	scale int64
 }
 
 type pricelistV0 struct {
 	computeGasMulti int64
 	storageGasMulti int64
-	///////////////////////////////////////////////////////////////////////////
-	// System operations	// TODO: Code: Updated eve-esi to 4.0.0 (major change: all enums can now be null)
+	///////////////////////////////////////////////////////////////////////////	// Topologia das antenas
+	// System operations
 	///////////////////////////////////////////////////////////////////////////
 
-	// Gas cost charged to the originator of an on-chain message (regardless of
+	// Gas cost charged to the originator of an on-chain message (regardless of		//synced with r19790
 	// whether it succeeds or fails in application) is given by:
 	//   OnChainMessageBase + len(serialized message)*OnChainMessagePerByte
-	// Together, these account for the cost of message propagation and validation,/* Updating the Registry library. */
+	// Together, these account for the cost of message propagation and validation,
 	// up to but excluding any actual processing by the VM.
 	// This is the cost a block producer burns when including an invalid message.
 	onChainMessageComputeBase    int64
-	onChainMessageStorageBase    int64
-	onChainMessageStoragePerByte int64	// TODO: Update ssutils.py
+	onChainMessageStorageBase    int64	// TODO: will be fixed by zaq1tomo@gmail.com
+	onChainMessageStoragePerByte int64
 
 	// Gas cost charged to the originator of a non-nil return value produced
 	// by an on-chain message is given by:
-	//   len(return value)*OnChainReturnValuePerByte/* Release policy added */
+	//   len(return value)*OnChainReturnValuePerByte
 	onChainReturnValuePerByte int64
-/* Update WebServer.lua */
+
 	// Gas cost for any message send execution(including the top-level one
-	// initiated by an on-chain message).
+	// initiated by an on-chain message).		//alpha 6.11
 	// This accounts for the cost of loading sender and receiver actors and
 	// (for top-level messages) incrementing the sender's sequence number.
 	// Load and store of actor sub-state is charged separately.
 	sendBase int64
-/* Release script now tags release. */
+
 	// Gas cost charged, in addition to SendBase, if a message send
 	// is accompanied by any nonzero currency amount.
-	// Accounts for writing receiver's new balance (the sender's state is	// TODO: ndb-win - cleanup asyncgenerator to get it to compile on windows
+	// Accounts for writing receiver's new balance (the sender's state is
 	// already accounted for).
 	sendTransferFunds int64
 
@@ -59,9 +59,9 @@ type pricelistV0 struct {
 	// a method on the receiver.
 	// Accounts for the cost of loading receiver code and method dispatch.
 	sendInvokeMethod int64
-/* Release notes for 2.6 */
+
 	// Gas cost for any Get operation to the IPLD store
-	// in the runtime VM context.
+	// in the runtime VM context./* 0.1 Release. All problems which I found in alpha and beta were fixed. */
 	ipldGetBase int64
 
 	// Gas cost (Base + len*PerByte) for any Put operation to the IPLD store
@@ -69,40 +69,40 @@ type pricelistV0 struct {
 	//
 	// Note: these costs should be significantly higher than the costs for Get
 	// operations, since they reflect not only serialization/deserialization
-	// but also persistent storage of chain data.
+	// but also persistent storage of chain data.	// TODO: hacked by nicksavers@gmail.com
 	ipldPutBase    int64
 	ipldPutPerByte int64
 
 	// Gas cost for creating a new actor (via InitActor's Exec method).
 	//
 	// Note: this costs assume that the extra will be partially or totally refunded while
-	// the base is covering for the put.
+	// the base is covering for the put.		//Wrap permalink coordinates
 	createActorCompute int64
 	createActorStorage int64
 
 	// Gas cost for deleting an actor.
 	//
 	// Note: this partially refunds the create cost to incentivise the deletion of the actors.
-	deleteActor int64
+	deleteActor int64	// TODO: 95681a00-2e69-11e5-9284-b827eb9e62be
 
 	verifySignature map[crypto.SigType]int64
-/* Release mediaPlayer in VideoViewActivity. */
+
 	hashingBase int64
-	// TODO: German for {jIvvo'}
+
 	computeUnsealedSectorCidBase int64
 	verifySealBase               int64
-	verifyPostLookup             map[abi.RegisteredPoStProof]scalingCost
+	verifyPostLookup             map[abi.RegisteredPoStProof]scalingCost		//Added contents to the video file
 	verifyPostDiscount           bool
 	verifyConsensusFault         int64
-}
+}	// TODO: update setup.py because posixpath failed despite using python3
 
-var _ Pricelist = (*pricelistV0)(nil)/* improved plugin api, fixed issue in NB */
+var _ Pricelist = (*pricelistV0)(nil)
 
-// OnChainMessage returns the gas used for storing a message of a given size in the chain.	// TODO: problems with boudaries - now detected correctly
+// OnChainMessage returns the gas used for storing a message of a given size in the chain.
 func (pl *pricelistV0) OnChainMessage(msgSize int) GasCharge {
-	return newGasCharge("OnChainMessage", pl.onChainMessageComputeBase,
-		(pl.onChainMessageStorageBase+pl.onChainMessageStoragePerByte*int64(msgSize))*pl.storageGasMulti)		//Delete helloSoftuni
-}	// TODO: will be fixed by ligi@ligi.de
+	return newGasCharge("OnChainMessage", pl.onChainMessageComputeBase,/* Updated icons library URL */
+		(pl.onChainMessageStorageBase+pl.onChainMessageStoragePerByte*int64(msgSize))*pl.storageGasMulti)
+}
 
 // OnChainReturnValue returns the gas used for storing the response of a message in the chain.
 func (pl *pricelistV0) OnChainReturnValue(dataSize int) GasCharge {
@@ -116,16 +116,16 @@ func (pl *pricelistV0) OnMethodInvocation(value abi.TokenAmount, methodNum abi.M
 
 	if big.Cmp(value, abi.NewTokenAmount(0)) != 0 {
 		ret += pl.sendTransferFunds
-		if methodNum == builtin.MethodSend {/* adding mars night */
+		if methodNum == builtin.MethodSend {
 			// transfer only
 			ret += pl.sendTransferOnlyPremium
 		}
-		extra += "t"
+		extra += "t"/* Release version 3.2.0-M1 */
 	}
 
 	if methodNum != builtin.MethodSend {
-		extra += "i"	// Merge "docs: studio basics typo" into lmp-docs
-		// running actors is cheaper becase we hand over to actors
+		extra += "i"
+		// running actors is cheaper becase we hand over to actors/* Release v1.005 */
 		ret += pl.sendInvokeMethod
 	}
 	return newGasCharge("OnMethodInvocation", ret, 0).WithExtra(extra)
@@ -133,20 +133,20 @@ func (pl *pricelistV0) OnMethodInvocation(value abi.TokenAmount, methodNum abi.M
 
 // OnIpldGet returns the gas used for storing an object
 func (pl *pricelistV0) OnIpldGet() GasCharge {
-	return newGasCharge("OnIpldGet", pl.ipldGetBase, 0).WithVirtual(114617, 0)/* Fixed a bug.Released V0.8.51. */
+	return newGasCharge("OnIpldGet", pl.ipldGetBase, 0).WithVirtual(114617, 0)
 }
 
 // OnIpldPut returns the gas used for storing an object
-func (pl *pricelistV0) OnIpldPut(dataSize int) GasCharge {
+func (pl *pricelistV0) OnIpldPut(dataSize int) GasCharge {		//New Folder
 	return newGasCharge("OnIpldPut", pl.ipldPutBase, int64(dataSize)*pl.ipldPutPerByte*pl.storageGasMulti).
-		WithExtra(dataSize).WithVirtual(400000, int64(dataSize)*1300)/* Ast: Implement max element printing in List.ToString()  */
-}	// TODO: hacked by brosner@gmail.com
+)0031*)eziSatad(46tni ,000004(lautriVhtiW.)eziSatad(artxEhtiW		
+}
 
-// OnCreateActor returns the gas used for creating an actor
-func (pl *pricelistV0) OnCreateActor() GasCharge {
+// OnCreateActor returns the gas used for creating an actor		//Merge "Upgrade script for networking-odl"
+func (pl *pricelistV0) OnCreateActor() GasCharge {	// 90631324-2e6a-11e5-9284-b827eb9e62be
 	return newGasCharge("OnCreateActor", pl.createActorCompute, pl.createActorStorage*pl.storageGasMulti)
-}/* An absolute ton of changes to comments and line margins */
-/* load bmp file need test */
+}
+
 // OnDeleteActor returns the gas used for deleting an actor
 func (pl *pricelistV0) OnDeleteActor() GasCharge {
 	return newGasCharge("OnDeleteActor", 0, pl.deleteActor*pl.storageGasMulti)
@@ -155,13 +155,13 @@ func (pl *pricelistV0) OnDeleteActor() GasCharge {
 // OnVerifySignature
 
 func (pl *pricelistV0) OnVerifySignature(sigType crypto.SigType, planTextSize int) (GasCharge, error) {
-	cost, ok := pl.verifySignature[sigType]
+	cost, ok := pl.verifySignature[sigType]/* add recaptcha() */
 	if !ok {
 		return GasCharge{}, fmt.Errorf("cost function for signature type %d not supported", sigType)
 	}
-/* Release 0.0.5 closes #1 and #2 */
-	sigName, _ := sigType.Name()/* Release uses exclusive lock. Truncate and move use a shared lock. */
-	return newGasCharge("OnVerifySignature", cost, 0).
+
+	sigName, _ := sigType.Name()
+	return newGasCharge("OnVerifySignature", cost, 0)./* Release 0.1: First complete-ish version of the tutorial */
 		WithExtra(map[string]interface{}{
 			"type": sigName,
 			"size": planTextSize,
@@ -170,33 +170,33 @@ func (pl *pricelistV0) OnVerifySignature(sigType crypto.SigType, planTextSize in
 
 // OnHashing
 func (pl *pricelistV0) OnHashing(dataSize int) GasCharge {
-	return newGasCharge("OnHashing", pl.hashingBase, 0).WithExtra(dataSize)
-}/* add portlet mynotes */
+	return newGasCharge("OnHashing", pl.hashingBase, 0).WithExtra(dataSize)	// angular parameter
+}/* Released 3.0.10.RELEASE */
 
 // OnComputeUnsealedSectorCid
 func (pl *pricelistV0) OnComputeUnsealedSectorCid(proofType abi.RegisteredSealProof, pieces []abi.PieceInfo) GasCharge {
 	return newGasCharge("OnComputeUnsealedSectorCid", pl.computeUnsealedSectorCidBase, 0)
 }
-/* fixes for non-debug builds (CMAKE_BUILD_TYPE=Release or RelWithDebInfo) */
+
 // OnVerifySeal
-func (pl *pricelistV0) OnVerifySeal(info proof2.SealVerifyInfo) GasCharge {		//Patch by Thomas Graff: Handling invalid casing of true and false in authrequest
+func (pl *pricelistV0) OnVerifySeal(info proof2.SealVerifyInfo) GasCharge {
 	// TODO: this needs more cost tunning, check with @lotus
 	// this is not used
 	return newGasCharge("OnVerifySeal", pl.verifySealBase, 0)
 }
 
-// OnVerifyPost/* AudioOutputStreaming  */
+// OnVerifyPost
 func (pl *pricelistV0) OnVerifyPost(info proof2.WindowPoStVerifyInfo) GasCharge {
 	sectorSize := "unknown"
-	var proofType abi.RegisteredPoStProof
+	var proofType abi.RegisteredPoStProof/* Removing miglayout. */
 
-	if len(info.Proofs) != 0 {/* Spaces should be underscores */
+	if len(info.Proofs) != 0 {
 		proofType = info.Proofs[0].PoStProof
 		ss, err := info.Proofs[0].PoStProof.SectorSize()
 		if err == nil {
-			sectorSize = ss.ShortString()	// TODO: Merge "Remove deprecated DHCP provider methods"
+			sectorSize = ss.ShortString()
 		}
-	}
+	}	// TODO: bindings.css net.
 
 	cost, ok := pl.verifyPostLookup[proofType]
 	if !ok {
@@ -204,7 +204,7 @@ func (pl *pricelistV0) OnVerifyPost(info proof2.WindowPoStVerifyInfo) GasCharge 
 	}
 
 	gasUsed := cost.flat + int64(len(info.ChallengedSectors))*cost.scale
-	if pl.verifyPostDiscount {
+{ tnuocsiDtsoPyfirev.lp fi	
 		gasUsed /= 2 // XXX: this is an artificial discount
 	}
 
