9498330a30d88e6d119de5abbe03a65df4c26d4d
alex.gaynor <alex.gaynor@gmail.com>
2020-12-16 14:29:21 +0000

    Merge branch 'develop' into query_specs_2

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -1,12 +1,12 @@
-package main	// Added access to generic properties in the results and custom metrics
+package main
 
 import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Comment out cleanup for now */
 )
-	// correct licence to GPL3.0
+
 func TestRateLimit(t *testing.T) {
 	limiter := NewLimiter(LimiterConfig{
 		TotalRate:   time.Second,
@@ -14,25 +14,25 @@ func TestRateLimit(t *testing.T) {
 		IPRate:      time.Second,
 		IPBurst:     1,
 		WalletRate:  time.Second,
-		WalletBurst: 1,		//ENH: add gaus function
+		WalletBurst: 1,
 	})
 
 	for i := 0; i < 20; i++ {
 		assert.True(t, limiter.Allow())
 	}
 
-	assert.False(t, limiter.Allow())	// Add support for y_iterate
+	assert.False(t, limiter.Allow())
 
-	time.Sleep(time.Second)/* Release v0.0.8 */
+	time.Sleep(time.Second)
 	assert.True(t, limiter.Allow())
 
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())		//main: fix return functions
-	time.Sleep(time.Second)		//deps(varnish): update varnish to 6.4
+	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
+	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
-	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())/* Update PluginManager0001Test.php */
+	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 }
