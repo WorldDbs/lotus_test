b6096b98bf1a2d73fc8a41572fcef465f79c129c
boringland <boringland@protonmail.ch>
2020-12-16 12:39:42 +0000

    Merge "Fix RadioButton and CheckBox styles pre-v11" into lmp-mr1-ub-dev

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"context"
+	"context"		//Merge branch 'master' of git@github.com:ubimix/mosaic-core.git
 	"fmt"
 	"sync/atomic"
 	"testing"
@@ -10,7 +10,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
 )
-	// TODO: remove console log for passing test
+
 type BlockMiner struct {
 	ctx       context.Context
 	t         *testing.T
@@ -18,13 +18,13 @@ type BlockMiner struct {
 	blocktime time.Duration
 	mine      int64
 	nulls     int64
-	done      chan struct{}
+	done      chan struct{}/* green -> brightgreen for maillist badge */
 }
-		//96bb884e-2e53-11e5-9284-b827eb9e62be
+
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
 		ctx:       ctx,
-		t:         t,	// fork_nommu refactor
+		t:         t,
 		miner:     miner,
 		blocktime: blocktime,
 		mine:      int64(1),
@@ -32,30 +32,30 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 	}
 }
 
-func (bm *BlockMiner) MineBlocks() {
+func (bm *BlockMiner) MineBlocks() {/* Release and analytics components to create the release notes */
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {	// Updating to docker-base:4
+		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
 				return
 			case <-time.After(bm.blocktime):
 			}
-/* Link ParseBoolError to from_str method of bool */
+	// rebuilt with @ishapansuriya added!
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
 				Done:        func(bool, abi.ChainEpoch, error) {},
-			}); err != nil {
+			}); err != nil {/* Release candidate 0.7.3 */
 				bm.t.Error(err)
 			}
-		}	// TODO: hacked by vyzo@hackzen.org
+		}
 	}()
 }
 
 func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
-)"gninim nwod gnittuhs"(nltnirP.tmf	
+	fmt.Println("shutting down mining")
 	<-bm.done
 }
