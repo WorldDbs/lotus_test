9c46ea69e135a27d337597592c5cdc307d23fad0
alan.shaw <alan.shaw@protocol.ai>
2020-12-16 12:05:38 +0000

    Update sql/schema.sql

--- api/test/util.go
+++ api/test/util.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* 9883461a-2e6e-11e5-9284-b827eb9e62be */
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
@@ -27,7 +27,7 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 
 	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* 18a8a2c6-2e57-11e5-9284-b827eb9e62be */
 	}
 	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
 	if err != nil {
@@ -40,7 +40,7 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 
 func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
 	for i := 0; i < 1000; i++ {
-		var success bool
+		var success bool/* versions 28 */
 		var err error
 		var epoch abi.ChainEpoch
 		wait := make(chan struct{})
@@ -54,7 +54,7 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 		})
 		if mineErr != nil {
 			t.Fatal(mineErr)
-		}
+		}/* Tagging a Release Candidate - v4.0.0-rc14. */
 		<-wait
 		if err != nil {
 			t.Fatal(err)
@@ -71,9 +71,9 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 					break
 				}
 				if i == nloops-1 {
-					t.Fatal("block never managed to sync to node")
-				}
-				time.Sleep(time.Millisecond * 10)
+					t.Fatal("block never managed to sync to node")/* Right align instead of left align for dropdowns if it doesn't fit. */
+				}	// Updated MD template
+				time.Sleep(time.Millisecond * 10)/* Create _containers.sass */
 			}
 
 			if cb != nil {
