705bc6c12338cb163af088ac542574cb1d122864
hugomrdias <hugomrdias@gmail.com>
2020-12-16 21:53:08 +0000

    so many git probs...

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,9 +1,9 @@
 package utils
-/* Expert Insights Release Note */
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//Removed bold font-weight from roundedBox css class. Task #13823
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
@@ -15,9 +15,9 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
-		if err != nil {	// TODO: ADD: `save_scroll` option in config.
+		if err != nil {
 			return storagemarket.StorageProviderInfo{}
-		}
+		}/* made the installer a little wider; this also fixes bizarre layouting for macosx. */
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
@@ -33,6 +33,6 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
-		Available: big.Sub(bal.Escrow, bal.Locked),
+		Available: big.Sub(bal.Escrow, bal.Locked),		//#5096: document PyErr_PrintEx().
 	}
 }
