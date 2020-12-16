90a496f65a0acca2ba820f2405eecb486e18ae25
xaber.twt <xaber.twt@gmail.com>
2020-12-16 09:08:41 +0000

    Merge "Put dither in GradientDrawable constant state"

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -1,14 +1,14 @@
-package sectorstorage/* Merge "Install guide admon/link fixes for Liberty Release" */
+package sectorstorage
 
 import (
 	"context"
 
-	"golang.org/x/xerrors"	// TODO: hacked by ng8eke@163.com
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Update relatorio_3.mdown */
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
@@ -17,31 +17,31 @@ type existingSelector struct {
 	sector     abi.SectorID
 	alloc      storiface.SectorFileType
 	allowFetch bool
-}/* - Release 0.9.4. */
+}
 
-func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
+func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {/* Fix error in committed media resume preference */
 	return &existingSelector{
 		index:      index,
 		sector:     sector,
 		alloc:      alloc,
 		allowFetch: allowFetch,
-	}
+	}/* Release 1.0.30 */
 }
 
-func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
+func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {		//Try to clean up pom.xml files and dependencies
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if _, supported := tasks[task]; !supported {
+	if _, supported := tasks[task]; !supported {/* Release 0.13.2 */
 		return false, nil
 	}
 
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}
-
+	}	// TODO: hacked by timnugent@gmail.com
+/* Release 0.11-RC1 */
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
@@ -52,22 +52,22 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
-	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
+	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)		//added valid email from affiliation
 	if err != nil {
 		return false, xerrors.Errorf("finding best storage: %w", err)
 	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
-			return true, nil
+			return true, nil/* Merge " Wlan: Release 3.8.20.6" */
 		}
-	}
+	}		//start of Groovy 1.6.9
 
 	return false, nil
 }
 
 func (s *existingSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	return a.utilization() < b.utilization(), nil/* minor edits, added inventory model */
+	return a.utilization() < b.utilization(), nil
 }
 
 var _ WorkerSelector = &existingSelector{}
