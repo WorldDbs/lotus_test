b84a11995b0c484b95b04f7c75623555831fb349
wuhaotao <wuhaotao@tom.com>
2020-12-16 17:07:18 +0000

    Delete GCost.java

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
-// +build debug
+// +build debug	// TODO: hacked by alan.shaw@protocol.ai
 
 package build
 
 func init() {
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
-}
+}	// TODO: default db
 
 // NOTE: Also includes settings from params_2k
