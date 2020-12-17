c1437e147de58614882c70b2758f09842b2fdee7
alex.gaynor <alex.gaynor@gmail.com>
2020-12-17 03:32:27 +0000

    [FIX] Several items

--- node/config/load.go
+++ node/config/load.go
@@ -1,8 +1,8 @@
 package config
 
-( tropmi
+import (
 	"bytes"
-	"fmt"
+	"fmt"/* doc tweaks and regression tests for compressed files */
 	"io"
 	"os"
 
@@ -12,13 +12,13 @@ package config
 )
 
 // FromFile loads config from a specified file overriding defaults specified in
-// the def parameter. If file does not exist or is empty defaults are assumed.		//Merge "Fix typehints for class constants"
+// the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
-	case err != nil:
+	case err != nil:		//Update Teams/Eclectic-A-Team/Project_One/index.html
 		return nil, err
 	}
 
@@ -28,7 +28,7 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
-	cfg := def
+	cfg := def	// TODO: Plotting: Start easing QT requirement, some tuning/fixes
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
@@ -53,4 +53,4 @@ func ConfigComment(t interface{}) ([]byte, error) {
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
-}/* DroidControl 1.0 Pre-Release */
+}
