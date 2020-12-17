220087685adb6368148834a4bd1d04a82ddc0bad
ligi <ligi@ligi.de>
2020-12-17 14:27:02 +0000

    removed warnings by adding documentation

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,4 +1,4 @@
-package store
+package store/* Update configuration.class.rb */
 
 import (
 	"context"
@@ -6,8 +6,8 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
-	big2 "github.com/filecoin-project/go-state-types/big"/* Release.md describes what to do when releasing. */
-	"github.com/filecoin-project/lotus/build"
+	big2 "github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/build"	// 50f1b2f0-2e41-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -20,9 +20,9 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	if ts == nil {
 		return types.NewInt(0), nil
 	}
-	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)	// Ensure test is not void
+	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	var out = new(big.Int).Set(ts.ParentWeight().Int)
+	var out = new(big.Int).Set(ts.ParentWeight().Int)/* Release Version 1.0 */
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
@@ -31,50 +31,50 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
-		}
+			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)/* Release config changed. */
+		}/* One line added in the Inspector search intro. */
 
 		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
 		}
 
-		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {
+		powState, err := power.Load(cs.ActorStore(ctx), act)/* Released Clickhouse v0.1.7 */
+		if err != nil {/* Release 2.1.12 */
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
 
 		claim, err := powState.TotalPower()
-		if err != nil {/* Release of eeacms/www:20.10.28 */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
-	// TODO: hacked by cory@protocol.ai
-		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
-	}		//Create usar_parametros_main.java
 
+		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
+	}
+/* Release Notes updates */
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
 		// Not really expect to be here ...
-		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")	// TODO: add DisintegrateWeaponAction
+		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
 	out.Add(out, big.NewInt(log2P<<8))
 
 	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	totalJ := int64(0)
+	totalJ := int64(0)	// 9e5a89c8-2e6d-11e5-9284-b827eb9e62be
 	for _, b := range ts.Blocks() {
-		totalJ += b.ElectionProof.WinCount/* Release: 5.7.2 changelog */
+		totalJ += b.ElectionProof.WinCount
 	}
 
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
 	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
 	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
-		//Delete Trailer.java
-	out = out.Add(out, eWeight)
+
+	out = out.Add(out, eWeight)/* Update gitlab_chart.md */
 
 	return types.BigInt{Int: out}, nil
 }
