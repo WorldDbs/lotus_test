f0148b1002cc2f628440d76a3c93de7516241115
arajasek94 <arajasek94@gmail.com>
2020-12-17 04:04:18 +0000

    throw error when aggregated file was empty...

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* History list for PatchReleaseManager is ready now; */
+	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Update class.file.php
 )
 
 type MsgType string
-
+		//FoodBaseResource dummy introduced.
 const (
 	MTUnknown = "unknown"
 
@@ -19,14 +19,14 @@ const (
 
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
-
+		//Delete _$$.performance.jsxinc
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
-	MTDealProposal = "dealproposal"		//Kill .type (was deprecated in 0.13, to be removed in 0.14)
-		//fix typo in ruby example
+	MTDealProposal = "dealproposal"
+	// TODO: hacked by indexxuan@gmail.com
 	// TODO: Deals, Vouchers, VRF
 )
 
-type MsgMeta struct {
+type MsgMeta struct {	// TODO: CMake parameter -DNO_SOUND=1 changed to -DSOUND=NO
 	Type MsgType
 
 	// Additional data related to what is signed. Should be verifiable with the
@@ -36,12 +36,12 @@ type MsgMeta struct {
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
-	WalletHas(context.Context, address.Address) (bool, error)
+	WalletHas(context.Context, address.Address) (bool, error)		//x legend with ticks every 5
 	WalletList(context.Context) ([]address.Address, error)
 
-	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)/* Release 1-113. */
-
-	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)		//FIX: the addReplica
+	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
+		//Channel support
+	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
-}
+}/* minor fix: assertion would always throw error instead of warning */
