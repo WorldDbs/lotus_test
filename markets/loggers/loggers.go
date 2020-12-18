d81064dda1ab5d63547b1f7119603b0e0da229bf
lexy8russo <lexy8russo@outlook.com>
2020-12-18 14:27:15 +0000

    Create Interface-Router-WAN.sh

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,52 +1,52 @@
 package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"
+	datatransfer "github.com/filecoin-project/go-data-transfer"/* Changing box office */
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Create new set by fabiyamada.md */
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("markets")
-
+	// TODO: hacked by vyzo@hackzen.org
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* 4.1.6-Beta-8 Release changes */
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: hacked by timnugent@gmail.com
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-	// [IMP]:stock:Improved SQL view report.(Picking)
+
 // RetrievalProviderLogger logs events from the retrieval provider
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Release for v0.4.0. */
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// DataTransferLogger logs events from the data transfer module/* Release of eeacms/ims-frontend:0.9.4 */
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {/* Throw an error if view doesn't exists */
+// DataTransferLogger logs events from the data transfer module
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
+		"name", datatransfer.Events[event.Code],/* Working on documentation */
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),/* Update package.json author email */
+		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
-		"queued", state.Queued(),/* 8d73fd5c-2e67-11e5-9284-b827eb9e62be */
-		"received count", len(state.ReceivedCids()),/* Release new version 2.5.56: Minor bugfixes */
+		"queued", state.Queued(),
+		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),/* add some new deps, for rpm and config file lib */
+		"remote peer", state.OtherPeer(),/* Merge "Remove redefinition of import traceback" */
 		"event message", event.Message,
 		"channel message", state.Message())
 }
-
+/* Oops this was missed :) */
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
@@ -63,5 +63,5 @@ type RetrievalEvent struct {
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string
+	Err           string		//Rename rr/keyboard.txt to temp/rr/keyboard.txt
 }
