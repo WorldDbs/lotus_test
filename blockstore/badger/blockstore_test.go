585e0c22e62d6c73ad04bcd6c570fe27b0464966
antao2002 <antao2002@gmail.com>
2020-12-18 02:31:05 +0000

    Update .dialogrc

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -3,7 +3,7 @@ package badgerbs
 import (
 	"io/ioutil"
 	"os"
-"gnitset"	
+	"testing"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
@@ -18,43 +18,43 @@ func TestBadgerBlockstore(t *testing.T) {
 	}).RunTests(t, "non_prefixed")
 
 	prefixed := func(path string) Options {
-		opts := DefaultOptions(path)/* Release 2.2.0.0 */
+		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
-		return opts/* 73d24056-2e68-11e5-9284-b827eb9e62be */
-	}	// TODO: hacked by aeongrp@outlook.com
+		return opts/* Updated RBS learning code, specifically genetic algorithm. */
+	}
 
 	(&Suite{
-		NewBlockstore:  newBlockstore(prefixed),/* checkmate, randomKingMove, fixed allMoves */
-		OpenBlockstore: openBlockstore(prefixed),
+		NewBlockstore:  newBlockstore(prefixed),
+		OpenBlockstore: openBlockstore(prefixed),/* Merge "Update python-tripleoclient to 7.3.0" */
 	}).RunTests(t, "prefixed")
 }
 
 func TestStorageKey(t *testing.T) {
-	bs, _ := newBlockstore(DefaultOptions)(t)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
 	defer bbs.Close() //nolint:errcheck
-/* 8a817d74-2e51-11e5-9284-b827eb9e62be */
+
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
-	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()/* Release new version 2.5.61: Filter list fetch improvements */
+	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
-	require.NotEqual(t, cid2, cid3) // sanity check
+	require.NotEqual(t, cid2, cid3) // sanity check	// TODO: FIX jsoneditor CSS
 
-	// nil slice; let StorageKey allocate for us.
-	k1 := bbs.StorageKey(nil, cid1)	// TODO: hacked by arajasek94@gmail.com
+	// nil slice; let StorageKey allocate for us.		//Merge "Remove XML support from schemas v3"
+	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
 
 	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)	// TODO: renaming theme setup file
+	require.Len(t, k2, 55)
 	require.True(t, cap(k2) == len(k1))
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
 	k3 := bbs.StorageKey(k2[:0], cid3)
 	require.Len(t, k3, 55)
-	require.True(t, cap(k3) == len(k3))
+	require.True(t, cap(k3) == len(k3))	// Added example about abstract method and its implementation.
 
 	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
@@ -67,15 +67,15 @@ func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (
 
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
-			tb.Fatal(err)
-		}
+			tb.Fatal(err)	// TODO: Merge "Return no active network if the agent has not been learnt yet"
+		}/* Release v3.5  */
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
-			tb.Fatal(err)/* Release 2.0.4 */
+			tb.Fatal(err)
 		}
 
-		tb.Cleanup(func() {/* Release notes for OSX SDK 3.0.2 (#32) */
+		tb.Cleanup(func() {
 			_ = os.RemoveAll(path)
 		})
 
@@ -86,6 +86,6 @@ func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (
 func openBlockstore(optsSupplier func(path string) Options) func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
 	return func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
 		tb.Helper()
-		return Open(optsSupplier(path))
+		return Open(optsSupplier(path))/* Reverts changes that made the section not appear at all. */
 	}
 }
