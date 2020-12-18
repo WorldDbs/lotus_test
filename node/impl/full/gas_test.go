d3e70871443803980963b988f68ad83cd330d968
fkautz <fkautz@pseudocode.cc>
2020-12-18 22:27:39 +0000

    33f5a510-2e57-11e5-9284-b827eb9e62be

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,5 +1,5 @@
 package full
-		//fix compile, wrong header
+
 import (
 	"testing"
 
@@ -7,28 +7,28 @@ import (
 
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: updating sha256 sum to most recent version
-	"github.com/filecoin-project/lotus/chain/types"		//Removed extra info and moved permalink to posts
-)	// TODO: interfaz 1
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
-func TestMedian(t *testing.T) {/* Release v3.0.0 */
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{/* Attempt at a simple bytecode compiler/interpreter. */
+func TestMedian(t *testing.T) {
+	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{/* Merge "[INTERNAL] Release notes for version 1.66.0" */
-		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},/* Release areca-7.4.3 */
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+		{big.NewInt(5), build.BlockGasTarget},	// TODO: Merge "Add new db api get functions for ec2_snapshot"
+		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-
+		//Merge branch 'master' into IS-1127
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* agrego metodo get all activas en oferta adapter */
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
