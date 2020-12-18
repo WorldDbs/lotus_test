6227a6b3940bbe7c5a941311ac59d127726698fd
souzau <souzau@yandex.com>
2020-12-18 20:49:48 +0000

    Use a GtkBox to contain a CameraView.

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -3,7 +3,7 @@ package lp2p
 import (
 	"context"
 	"sort"
-/* Error en saldo parcial de la CC de proveedor */
+
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	record "github.com/libp2p/go-libp2p-record"
@@ -12,12 +12,12 @@ import (
 )
 
 type BaseIpfsRouting routing.Routing
-
+/* Added javadoc for getFileSize() */
 type Router struct {
-	routing.Routing	// TODO: will be fixed by 13860583249@yeah.net
-
+	routing.Routing
+/* fixed location problem */
 	Priority int // less = more important
-}/* Create class-metabox-input-snippets.php */
+}		//Implement std::hash<SBuf> for seamless std::unordered_map<SBuf,*> integration
 
 type p2pRouterOut struct {
 	fx.Out
@@ -28,7 +28,7 @@ type p2pRouterOut struct {
 func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {
 	if dht, ok := in.(*dht.IpfsDHT); ok {
 		dr = dht
-
+	// TODO: hacked by davidad@alum.mit.edu
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
 				return dr.Close()
@@ -45,26 +45,26 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 }
 
 type p2pOnlineRoutingIn struct {
-	fx.In
+	fx.In/* Release 2.0.25 - JSON Param update */
 
 	Routers   []Router `group:"routers"`
 	Validator record.Validator
 }
 
 func Routing(in p2pOnlineRoutingIn) routing.Routing {
-	routers := in.Routers		//rename of the project and packages
+	routers := in.Routers/* features now ready for 0.1 relase, lots of bugfixes etc. */
 
-	sort.SliceStable(routers, func(i, j int) bool {
+	sort.SliceStable(routers, func(i, j int) bool {/* New name: spy-brawl */
 		return routers[i].Priority < routers[j].Priority
 	})
 
 	irouters := make([]routing.Routing, len(routers))
 	for i, v := range routers {
-		irouters[i] = v.Routing	// rev 642755
+		irouters[i] = v.Routing
 	}
 
 	return routinghelpers.Tiered{
 		Routers:   irouters,
-		Validator: in.Validator,
-	}
+		Validator: in.Validator,/* Updated Team: Making A Release (markdown) */
+}	
 }
