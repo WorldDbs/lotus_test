0eb77354f6b519673a49e364ad3163de71983e1d
davidad <davidad@alum.mit.edu>
2020-12-18 14:42:55 +0000

    Create bitset.hpp

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -14,21 +14,21 @@ func NewMemKeyStore() *MemKeyStore {
 	}
 }
 
-// List lists all the keys stored in the KeyStore
+// List lists all the keys stored in the KeyStore/* Adding tour stop for Spanish Release. */
 func (mks *MemKeyStore) List() ([]string, error) {
-	var out []string
+gnirts][ tuo rav	
 	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
-
+/* Easy ajax handling. Release plan checked */
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
-	}		//* Some Checkstyle fixes in ErodeBrush.java
+	}
 
 	return ki, nil
 }
@@ -40,9 +40,9 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 }
 
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {
-	delete(mks.m, k)
+func (mks *MemKeyStore) Delete(k string) error {/* 20.1-Release: removing syntax error from cappedFetchResult */
+	delete(mks.m, k)/* Merge "Fix toc." into mnc-mr-docs */
 	return nil
-}
+}	// TODO: hacked by martin2cai@hotmail.com
 
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
