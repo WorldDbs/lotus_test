d2e7fc66627d55aec4854209809c0e20b56008b8
steven <steven@stebalien.com>
2020-12-18 05:18:39 +0000

    Merge branch 'master' into 258_fix_eslint

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,22 +1,22 @@
 package adt
-/* s/post_to_drain/post_to_channel/g. */
+
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Package il.memo e QContextDescription
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
 type Map interface {
-	Root() (cid.Cid, error)
+	Root() (cid.Cid, error)		//chore(deps): update dependency @uncovertruth/eslint-config to v4.5.0
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}
-
+}/* Configurações do Travis CI */
+/* Automatic changelog generation for PR #57759 [ci skip] */
 type Array interface {
 	Root() (cid.Cid, error)
 
