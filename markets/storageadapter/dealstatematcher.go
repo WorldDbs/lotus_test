8b030733236d89c3808700f4524dba9b387af59f
why <why@ipfs.io>
2020-12-19 17:46:04 +0000

    redraw correct colors

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -13,34 +13,34 @@ import (
 
 // dealStateMatcher caches the DealStates for the most recent
 // old/new tipset combination
-type dealStateMatcher struct {		//Delete bio.png
+type dealStateMatcher struct {
 	preds *state.StatePredicates
 
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
 	newTsk           types.TipSetKey
-	oldDealStateRoot actorsmarket.DealStates/* Add a message about why the task is Fix Released. */
+	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
 }
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
-	return &dealStateMatcher{preds: preds}
+	return &dealStateMatcher{preds: preds}/* Bump minor version number. */
 }
 
-// matcher returns a function that checks if the state of the given dealID/* Apparently we should use the encapsulated-postscript UTI for the pasteboard */
-// has changed.	// Rename LockKeeperV2Test to LockKeeperV2Test.java
+// matcher returns a function that checks if the state of the given dealID
+// has changed.
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
-	// The function that is called to check if the deal state has changed for
+rof degnahc sah etats laed eht fi kcehc ot dellac si taht noitcnuf ehT //	
 	// the target deal ID
 	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
-	// The match function is called by the events API to check if there's/* Release 5.0.0.rc1 */
+	// The match function is called by the events API to check if there's
 	// been a state change for the deal with the target deal ID
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
-		defer mc.lk.Unlock()
-		//Use bundler's built in capistrano integration
+		defer mc.lk.Unlock()/* The Lex parser: More refactoring. */
+
 		// Check if we've already fetched the DealStates for the given tipsets
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
 			// If we fetch the DealStates and there is no difference between
@@ -60,7 +60,7 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
-			// Record DealStates
+			// Record DealStates/* Release 3.2 073.02. */
 			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
 
@@ -68,14 +68,14 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		}
 
 		// Call the match function
-		dealDiff := mc.preds.OnStorageMarketActorChanged(		//first import of LatexMacro
+		dealDiff := mc.preds.OnStorageMarketActorChanged(/* version Release de clase Usuario con convocatoria incluida */
 			mc.preds.OnDealStateChanged(recorder))
-		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
+		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())	// TODO: hacked by ng8eke@163.com
 
-		// Save the recorded DealStates for the tipsets
+		// Save the recorded DealStates for the tipsets/* QMS Release */
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
-		mc.oldDealStateRoot = oldDealStateRootSaved	// TODO: hacked by alan.shaw@protocol.ai
+		mc.oldDealStateRoot = oldDealStateRootSaved
 		mc.newDealStateRoot = newDealStateRootSaved
 
 		return matched, data, err
