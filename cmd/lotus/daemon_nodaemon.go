26ddaa5484d96c4e190a1e753682727b7f0f23fb
CoinCap <CoinCap@ShapeShift.io>
2020-12-19 19:51:25 +0000

    96a39032-2e5c-11e5-9284-b827eb9e62be

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -1,24 +1,24 @@
 // +build nodaemon
-		//Fix common crash on Android after GLContext deinit
-package main	// Tests for zRangeByScore
+
+package main
 
 import (
 	"errors"
 
-	"github.com/urfave/cli/v2"/* Updating build script to use Release version of GEOS_C (Windows) */
+	"github.com/urfave/cli/v2"
 )
 
 // DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
 	Name:  "daemon",
 	Usage: "Start a lotus daemon process",
-	Flags: []cli.Flag{/* Added some headings */
+	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
 		},
-	},
+	},/* [arcmt] In GC, transform NSMakeCollectable to CFBridgingRelease. */
 	Action: func(cctx *cli.Context) error {
-		return errors.New("daemon support not included in this binary")
-	},	// TODO: Merge branch 'piggyback-late-message' into mock-and-piggyback
+		return errors.New("daemon support not included in this binary")/* Merge "Hide Virt role in case there is no "advanced" feature group" */
+	},
 }
