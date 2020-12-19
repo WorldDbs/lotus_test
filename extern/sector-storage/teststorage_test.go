4de9101547f72a193ed1e60e86746e953a97d2f4
sebs <sebs@2xs.org>
2020-12-19 21:41:48 +0000

    Removed todo that was fixed in #312 - fixes #343

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,5 +1,5 @@
 package sectorstorage
-
+	// TODO: hacked by caojiaoyue@protonmail.com
 import (
 	"context"
 	"io"
@@ -8,7 +8,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"	// start with font list hidden
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -20,21 +20,21 @@ type apres struct {
 }
 
 type testExec struct {
-	apch chan chan apres
-}
+	apch chan chan apres	// Updating README instructions and adding screenshot
+}/* Add ItemEnchantment matcher tests */
 
-func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {/* (mbp, trivial) remove doc/ja/sphinx_conf.py to unblock builds */
 	panic("implement me")
 }
 
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
-}
+}/* Release notes for 1.0.93 */
 
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
 }
-
+/* Merge "Add hidden support for ENCODING_PCM_FLOAT to AudioTrack" */
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
@@ -49,12 +49,12 @@ func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1
 
 func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
-}/* Implemented placeForestObjects. */
-
-func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
-	panic("implement me")
 }
 
+func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {	// TODO: will be fixed by ligi@ligi.de
+	panic("implement me")		//added installation information to readme
+}
+/* Update info about UrT 4.3 Release Candidate 4 */
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
@@ -66,16 +66,16 @@ func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) erro
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
-	ar := <-resp/* Checking if element is not null */
-	return ar.pi, ar.err
-}
+	ar := <-resp		//there ya are
+	return ar.pi, ar.err/* Deleted CtrlApp_2.0.5/Release/vc60.idb */
+}/* Update Recent and Upcoming Releases */
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
-	panic("implement me")	// TODO: [MERGE] Updated l10n_ch, courtesy of Nicolas Bessi (Camptocamp)
-}/* Release 0.5.1. Update to PQM brink. */
+	panic("implement me")
+}
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
-}
+}/* Eggdrop v1.8.2 Release Candidate 2 */
 
 var _ ffiwrapper.Storage = &testExec{}
