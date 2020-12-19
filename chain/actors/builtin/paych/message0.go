ce0591a58111c2da0931179ac782ad4ee07245f7
sebs <sebs@2xs.org>
2020-12-19 17:13:18 +0000

    Merge "Adapter for LiveData" into androidx-master-dev

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -30,12 +30,12 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,/* Release 1.0 !!!!!!!!!!!! */
+		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil		//+ Bug 2081205: Host Crashes when Fuel Tanks are shot
-}/* add all extension registers */
+	}, nil
+}	// TODO: hacked by cory@protocol.ai
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
@@ -44,7 +44,7 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* Add CloudFace again */
+	}
 
 	return &types.Message{
 		To:     paych,
@@ -53,22 +53,22 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}
+}		//avoid splash screen in unit tests
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,
+		From:   m.from,		//remote nick151 icon :^)
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.Settle,
+		Method: builtin0.MethodsPaych.Settle,/* Released v.1.2.0.3 */
 	}, nil
 }
-	// TODO: Remove our custom keywords - apparently, they didn't work as well as I expected.
+
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.Collect,
+		Method: builtin0.MethodsPaych.Collect,	// TODO: hacked by mikeal.rogers@gmail.com
 	}, nil
 }
