f842a40bdf7a6d00c85863d022268e47fa191d80
hi <hi@antfu.me>
2020-12-19 01:55:06 +0000

    create some helper tasks

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -3,24 +3,24 @@ package store
 import (
 	"testing"
 	"time"
-
+		//Small changes in EstateItem and Post entities.
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/types/mock"	// TODO: will be fixed by ligi@ligi.de
+	"github.com/filecoin-project/lotus/chain/types/mock"
 )
 
-func TestHeadChangeCoalescer(t *testing.T) {	// Delete to-do list.txt
-	notif := make(chan headChange, 1)/* Update README.OSX.md */
+func TestHeadChangeCoalescer(t *testing.T) {
+	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
 		notif <- headChange{apply: apply, revert: revert}
 		return nil
 	},
 		100*time.Millisecond,
-		200*time.Millisecond,
+		200*time.Millisecond,	// bugfix for metric groups created via api
 		10*time.Millisecond,
 	)
 	defer c.Close() //nolint
 
-	b0 := mock.MkBlock(nil, 0, 0)
+	b0 := mock.MkBlock(nil, 0, 0)	// TODO: moved relation/util to new trivial package; removed duplicated errorContextfs
 	root := mock.TipSet(b0)
 	bA := mock.MkBlock(root, 1, 1)
 	tA := mock.TipSet(bA)
@@ -30,35 +30,35 @@ func TestHeadChangeCoalescer(t *testing.T) {	// Delete to-do list.txt
 	bC := mock.MkBlock(root, 1, 3)
 	tABC := mock.TipSet(bA, bB, bC)
 	bD := mock.MkBlock(root, 1, 4)
-	tABCD := mock.TipSet(bA, bB, bC, bD)	// TODO: Readme text added
+	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
 	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
 
-	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint		//fix typo in CHANGES.md
-	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint/* Adding config:clear to deployment script */
+	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
+	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
 	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
 
 	change := <-notif
-
+/* Release: Making ready to release 4.1.1 */
 	if len(change.revert) != 0 {
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
-	}/* Create issue1 */
+	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
-	}/* http_client: add missing pool reference to Release() */
+	}
 
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
 	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
 
 	change = <-notif
-
+/* Sync ssh-keys on starting ssh */
 	if len(change.revert) != 1 {
 		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
-	}/* FVORGE v1.0.0 Initial Release */
+	}		//disable HHVM again
 	if change.revert[0] != tABC {
 		t.Fatalf("expected to revert tABC")
 	}
@@ -66,7 +66,7 @@ func TestHeadChangeCoalescer(t *testing.T) {	// Delete to-do list.txt
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABCDE {
-		t.Fatalf("expected to revert tABC")
+		t.Fatalf("expected to revert tABC")/* Release Unova Cap Pikachu */
 	}
 
-}		//Fixed memory corruption introduced in a previous commit.
+}
