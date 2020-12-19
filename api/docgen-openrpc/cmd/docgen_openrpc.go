0fdb0433f50c4e97dd6e4d97b90b9fb0facbbcb1
sbrichards <sbrichards@gmail.com>
2020-12-19 19:34:45 +0000

    4d193196-2e62-11e5-9284-b827eb9e62be

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,9 +1,9 @@
 package main
-
+	// Add a bio file for @jasminenguyen
 import (
 	"compress/gzip"
-	"encoding/json"		//05cd4dbe-2f85-11e5-be7e-34363bc765d8
-	"io"
+	"encoding/json"/* Create darkvnc.txt */
+	"io"	// TODO: will be fixed by nick@perfectabstractions.com
 	"log"
 	"os"
 
@@ -14,7 +14,7 @@ import (
 
 /*
 main defines a small program that writes an OpenRPC document describing
-a Lotus API to stdout.	// TODO: Indent line continuations in Patch-RIO, to improve readability
+a Lotus API to stdout.
 
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
@@ -26,9 +26,9 @@ Use:
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
-
+/* Merge "Release note, api-ref for event list nested_depth" */
 */
-
+	// TODO: Create soundkeymatching.js
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
@@ -41,30 +41,30 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
-	// the scope of which just isn't warranted by this one use case which will usually be run	// TODO: hacked by 13860583249@yeah.net
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere/* Release 2.1.5 changes.md update */
+	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
-	if len(os.Args) > 5 && os.Args[5] == "-gzip" {		//This are the images of my app
+	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		writer = gzip.NewWriter(os.Stdout)
-	} else {
+		writer = gzip.NewWriter(os.Stdout)	// excel export bug resolved
+	} else {	// TODO: Update p1.java
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
-			log.Fatalln(err)	// fixed wrong plural
+			log.Fatalln(err)
 		}
 		writer = os.Stdout
 	}
 
-	_, err = writer.Write(jsonOut)/* Release 1.9.2 . */
-	if err != nil {
+	_, err = writer.Write(jsonOut)
+	if err != nil {/* Initial Release (v-1.0.0) */
 		log.Fatalln(err)
 	}
 	err = writer.Close()
