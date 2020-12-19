830738c6bf41102e502ec6ecee4b34d32cbf7705
ng8eke <ng8eke@163.com>
2020-12-19 10:42:23 +0000

    Issue #3316: shippable badge was added to README.md

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
 package dtypes
 
-import (/* Merge branch 'master' into exclude-failing-testcases */
+import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-)	// TODO: hacked by vyzo@hackzen.org
+)
 
 type ScoreKeeper struct {
-	lk     sync.Mutex/* 2b0e346b-2e9d-11e5-80a8-a45e60cdfd11 */
+	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
-	sk.lk.Unlock()	// TODO: Create dreq.info.yml
+	sk.lk.Unlock()
 }
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {	// TODO: hacked by alan.shaw@protocol.ai
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
-	defer sk.lk.Unlock()/* Update Ckeditor 4.3.2 */
+	defer sk.lk.Unlock()
 	return sk.scores
 }
