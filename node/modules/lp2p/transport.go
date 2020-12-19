254c9061ad52e6b6677b37068e90bf669f09a0bb
zaq1tomo <zaq1tomo@gmail.com>
2020-12-19 23:52:42 +0000

    fix nav bug (refs #3264)

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,11 +1,11 @@
 package lp2p
-
+/* fixed Release build */
 import (
-	"github.com/libp2p/go-libp2p"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"	// Check for le apt install on 16.04
+	"github.com/libp2p/go-libp2p"/* Update openapi-generator-cli version to 4.1.2 */
+	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"	// TODO: will be fixed by why@ipfs.io
-	tls "github.com/libp2p/go-libp2p-tls"/* ea797359-313a-11e5-b0d4-3c15c2e10482 */
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	tls "github.com/libp2p/go-libp2p-tls"
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
@@ -16,13 +16,13 @@ func Security(enabled, preferTLS bool) interface{} {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)/* Create leader.js */
+		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}		//Update expandables.rst
+		}
 	}
 	return func() (opts Libp2pOpts) {
-		if preferTLS {		//changed how we build stuff, so removed these.
+		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
