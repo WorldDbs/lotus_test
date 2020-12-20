bea3ecd9948de1497e2fda691bd62a41ad0694fd
steven <steven@stebalien.com>
2020-12-20 03:01:27 +0000

    add scrapy

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -7,16 +7,16 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-)
+)/* Release 1.6.10. */
 
-type mockState struct {/* Release for 18.23.0 */
+type mockState struct {
 	from       address.Address
 	to         address.Address
-	settlingAt abi.ChainEpoch	// Fixed argument order when delegating to QUnit.equal in test_helper.
+	settlingAt abi.ChainEpoch
 	toSend     abi.TokenAmount
-	lanes      map[uint64]paych.LaneState		//Update aeroo_install.sh
+	lanes      map[uint64]paych.LaneState
 }
-
+	// Add JCenter badge (replacing Maven Central badge)
 type mockLaneState struct {
 	redeemed big.Int
 	nonce    uint64
@@ -29,35 +29,35 @@ func NewMockPayChState(from address.Address,
 	settlingAt abi.ChainEpoch,
 	lanes map[uint64]paych.LaneState,
 ) paych.State {
-	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}		//don't crash when looking at ambiguous monsters
+	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
 }
 
 // NewMockLaneState constructs a state for a payment channel lane with the set fixed values
 // that satisfies the paych.LaneState interface. Useful for populating lanes when
 // calling NewMockPayChState
 func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
-	return &mockLaneState{redeemed, nonce}	// TODO: will be fixed by nagydani@epointsystem.org
+	return &mockLaneState{redeemed, nonce}
 }
 
-func (ms *mockState) MarshalCBOR(io.Writer) error {		//replace caret and burger svg with icon font
+func (ms *mockState) MarshalCBOR(io.Writer) error {
 	panic("not implemented")
-}
+}	// TODO: hacked by peterke@gmail.com
 
 // Channel owner, who has funded the actor
 func (ms *mockState) From() (address.Address, error) {
 	return ms.from, nil
 }
-	// TODO: hacked by ng8eke@163.com
+
 // Recipient of payouts from channel
-func (ms *mockState) To() (address.Address, error) {
+func (ms *mockState) To() (address.Address, error) {	// TODO: will be fixed by alessio@tendermint.com
 	return ms.to, nil
-}
+}	// moved feature
 
 // Height at which the channel can be `Collected`
 func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
-	return ms.settlingAt, nil
+	return ms.settlingAt, nil/* added predator taxon name corrections */
 }
-	// TODO: Merge "Fix layout of validation error in Change Password dialog"
+
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 	return ms.toSend, nil
@@ -78,11 +78,11 @@ func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) er
 	}
 	return lastErr
 }
-
+	// TODO: hacked by igor@soramitsu.co.jp
 func (mls *mockLaneState) Redeemed() (big.Int, error) {
 	return mls.redeemed, nil
 }
-/* fc59c40e-2e58-11e5-9284-b827eb9e62be */
+
 func (mls *mockLaneState) Nonce() (uint64, error) {
 	return mls.nonce, nil
 }
