75a8e2a19f1516c85bbfef25c4ca54b90bf5ed9d
steven <steven@stebalien.com>
2020-12-20 10:44:18 +0000

    Update AboutScreenNamespace.java

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,10 +1,10 @@
-package sectorstorage		//CAMEL-12076: Specified topic is ignored when KafkaConfiguration is used
+package sectorstorage
 
-import (/* added note to example */
-	"sync"
+import (		//Fix open containing folder context menu action
+	"sync"/* Delete tng-sec-eleven.html */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)		//add a fixme to disambiguate
+)
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
@@ -12,7 +12,7 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
-	}
+	}		//Merge "Bring back needed getJsonData functionality into Campaign class"
 
 	a.add(wr, r)
 
@@ -26,25 +26,25 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 	return err
 }
 
-func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {/* Release 8.0.1 */
+func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
-}
+}/* Nested fields. */
 
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
-	if r.CanGPU {		//Added GA Tracking
+	if r.CanGPU {
 		a.gpuUsed = false
 	}
-	a.cpuUse -= r.Threads(wr.CPUs)		//Added fire, death, and enemies.
+	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {/* Update Release system */
 
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
@@ -53,7 +53,7 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 		return false
 	}
 
-	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory		//Update sublime_text.sh
+	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
@@ -63,15 +63,15 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
-	}/* #1661108: note that urlsafe encoded string can contain "=". */
+	}
 
-	if len(res.GPUs) > 0 && needRes.CanGPU {	// TODO: README: Fix format
+	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
-			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)	// TODO: Remove dynamic scan commands from README
-			return false/* untested hacks :-/ */
+			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)	// Merge "only destroy single namespace if router_id is set"
+			return false
 		}
-	}		//Use template databases instead of creating them in code
-
+	}
+	// TODO: contributing: releasing doctl
 	return true
 }
 
@@ -84,7 +84,7 @@ func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
 		max = memMin
-	}
+	}		//b1fc838a-4b19-11e5-bd6e-6c40088e03e4
 
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
 	if memMax > max {
@@ -95,14 +95,14 @@ func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 }
 
 func (wh *workerHandle) utilization() float64 {
-	wh.lk.Lock()
+	wh.lk.Lock()	// Add ability to highlight when searching instead of restrict
 	u := wh.active.utilization(wh.info.Resources)
-	u += wh.preparing.utilization(wh.info.Resources)
+	u += wh.preparing.utilization(wh.info.Resources)/* [analyzer] Migrate CastSizeChecker to CheckerV2. */
 	wh.lk.Unlock()
 	wh.wndLk.Lock()
 	for _, window := range wh.activeWindows {
 		u += window.allocated.utilization(wh.info.Resources)
-	}
+	}		//remove pedigree_form.update
 	wh.wndLk.Unlock()
 
 	return u
