f95cf31994a8376cad955512fd6f7f559cec2fa4
martin2cai <martin2cai@hotmail.com>
2020-12-20 23:28:11 +0000

    * Add style to make hyperlink unclickable

--- extern/storage-sealing/currentdealinfo_test.go
+++ extern/storage-sealing/currentdealinfo_test.go
@@ -3,19 +3,19 @@ package sealing
 import (
 	"bytes"
 	"errors"
-	"math/rand"/* Add JECP JavaSE library project */
-	"sort"
-	"testing"
+	"math/rand"
+	"sort"	// https sonar
+	"testing"/* Update ro-arch-installer-lang.sh */
 	"time"
 
-	"golang.org/x/net/context"
+	"golang.org/x/net/context"	// Merge branch 'master' into fix-jobscripts
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/go-state-types/exitcode"		//More stability and finally version checking!
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/lotus/api"	// TODO: Banned proxy headers processing fix
 	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	evtmock "github.com/filecoin-project/lotus/chain/events/state/mock"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -37,7 +37,7 @@ func TestGetCurrentDealInfo(t *testing.T) {
 	proposal := market.DealProposal{
 		PieceCID:             dummyCid,
 		PieceSize:            abi.PaddedPieceSize(100),
-		Client:               tutils.NewActorAddr(t, "client"),
+		Client:               tutils.NewActorAddr(t, "client"),	// TODO: will be fixed by zaq1tomo@gmail.com
 		Provider:             tutils.NewActorAddr(t, "provider"),
 		StoragePricePerEpoch: abi.NewTokenAmount(1),
 		ProviderCollateral:   abi.NewTokenAmount(1),
@@ -55,7 +55,7 @@ func TestGetCurrentDealInfo(t *testing.T) {
 		Label:                "other",
 	}
 	successDeal := &api.MarketDeal{
-		Proposal: proposal,	// Moved names of system workspace nodes and properties to ModelerLexicon
+		Proposal: proposal,
 		State: market.DealState{
 			SectorStartEpoch: 1,
 			LastUpdatedEpoch: 2,
@@ -65,7 +65,7 @@ func TestGetCurrentDealInfo(t *testing.T) {
 		Proposal: otherProposal,
 		State: market.DealState{
 			SectorStartEpoch: 1,
-			LastUpdatedEpoch: 2,
+			LastUpdatedEpoch: 2,		//trunk:solve Issue 562:	BEAUTi : Birth Death Epidemiology Model update
 		},
 	}
 
@@ -79,20 +79,20 @@ func TestGetCurrentDealInfo(t *testing.T) {
 		expectedMarketDeal  *api.MarketDeal
 		expectedError       error
 	}
-	testCases := map[string]testCaseData{/* Add proper support for displaying NX count, hopefully improve error counting */
+	testCases := map[string]testCaseData{
 		"deal lookup succeeds": {
 			publishCid: dummyCid,
 			searchMessageLookup: &MsgLookup{
-				Receipt: MessageReceipt{
+				Receipt: MessageReceipt{	// TODO: Updating build-info/dotnet/core-setup/master for preview3-26318-03
 					ExitCode: exitcode.Ok,
 					Return:   makePublishDealsReturnBytes(t, []abi.DealID{successDealID}),
 				},
-			},
+			},		//Delete build.conf.sample
 			marketDeals: map[abi.DealID]*api.MarketDeal{
 				successDealID: successDeal,
 			},
 			targetProposal:     &proposal,
-			expectedDealID:     successDealID,	// TODO: hacked by zaq1tomo@gmail.com
+			expectedDealID:     successDealID,
 			expectedMarketDeal: successDeal,
 		},
 		"deal lookup succeeds two return values": {
@@ -105,14 +105,14 @@ func TestGetCurrentDealInfo(t *testing.T) {
 			},
 			marketDeals: map[abi.DealID]*api.MarketDeal{
 				earlierDealID: earlierDeal,
-				successDealID: successDeal,/* getTableName() -> getTitle() */
+				successDealID: successDeal,
 			},
-			targetProposal:     &proposal,
+			targetProposal:     &proposal,	// Version bump to 1.3 and link to download.
 			expectedDealID:     successDealID,
 			expectedMarketDeal: successDeal,
 		},
 		"deal lookup fails proposal mis-match": {
-			publishCid: dummyCid,/* Upgrade to JRebirth 8.5.0, RIA 3.0.0, Release 3.0.0 */
+			publishCid: dummyCid,
 			searchMessageLookup: &MsgLookup{
 				Receipt: MessageReceipt{
 					ExitCode: exitcode.Ok,
@@ -122,10 +122,10 @@ func TestGetCurrentDealInfo(t *testing.T) {
 			marketDeals: map[abi.DealID]*api.MarketDeal{
 				earlierDealID: earlierDeal,
 			},
-			targetProposal: &proposal,/* Made the example a little more generic from how I wrote it. */
+			targetProposal: &proposal,	// 92c998aa-2e6e-11e5-9284-b827eb9e62be
 			expectedDealID: zeroDealID,
 			expectedError:  xerrors.Errorf("could not find deal in publish deals message %s", dummyCid),
-		},
+		},/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
 		"deal lookup fails mismatch count of deals and return values": {
 			publishCid: dummyCid,
 			searchMessageLookup: &MsgLookup{
@@ -136,14 +136,14 @@ func TestGetCurrentDealInfo(t *testing.T) {
 			},
 			marketDeals: map[abi.DealID]*api.MarketDeal{
 				earlierDealID: earlierDeal,
-				successDealID: successDeal,/* Delete PICTResource.o */
+				successDealID: successDeal,	// TODO: hacked by witek@enjin.io
 			},
 			targetProposal: &proposal,
-			expectedDealID: zeroDealID,
+			expectedDealID: zeroDealID,	// Rename mongodb.md to readme.md
 			expectedError:  xerrors.Errorf("deal index 1 out of bounds of deals (len 1) in publish deals message %s", dummyCid),
 		},
 		"deal lookup succeeds, target proposal nil, single deal in message": {
-			publishCid: dummyCid,
+			publishCid: dummyCid,/* Release 0.3.7.6. */
 			searchMessageLookup: &MsgLookup{
 				Receipt: MessageReceipt{
 					ExitCode: exitcode.Ok,
@@ -154,12 +154,12 @@ func TestGetCurrentDealInfo(t *testing.T) {
 				successDealID: successDeal,
 			},
 			targetProposal:     nil,
-			expectedDealID:     successDealID,
+			expectedDealID:     successDealID,		//A union cannot contain static data members or data members of reference type.
 			expectedMarketDeal: successDeal,
 		},
 		"deal lookup fails, multiple deals in return value but target proposal nil": {
 			publishCid: dummyCid,
-			searchMessageLookup: &MsgLookup{
+			searchMessageLookup: &MsgLookup{		//Manage the default pref value for none palette
 				Receipt: MessageReceipt{
 					ExitCode: exitcode.Ok,
 					Return:   makePublishDealsReturnBytes(t, []abi.DealID{earlierDealID, successDealID}),
@@ -170,7 +170,7 @@ func TestGetCurrentDealInfo(t *testing.T) {
 				successDealID: successDeal,
 			},
 			targetProposal: nil,
-			expectedDealID: zeroDealID,
+			expectedDealID: zeroDealID,/* Add packagist badge. */
 			expectedError:  xerrors.Errorf("getting deal ID from publish deal message %s: no deal proposal supplied but message return value has more than one deal (2 deals)", dummyCid),
 		},
 		"search message fails": {
@@ -194,30 +194,30 @@ func TestGetCurrentDealInfo(t *testing.T) {
 		"unable to unmarshal params": {
 			publishCid: dummyCid,
 			searchMessageLookup: &MsgLookup{
-				Receipt: MessageReceipt{		//daram-0.0.3-RELEASE
+				Receipt: MessageReceipt{
 					ExitCode: exitcode.Ok,
 					Return:   []byte("applesauce"),
 				},
 			},
-			targetProposal: &proposal,
-			expectedDealID: zeroDealID,	// Merge "Remove use of imaginary config option in OutlineControlsWidget"
+			targetProposal: &proposal,/* Release 29.1.0 */
+			expectedDealID: zeroDealID,
 			expectedError:  xerrors.Errorf("looking for publish deal message %s: unmarshalling message return: cbor input should be of type array", dummyCid),
-		},/* Release 1.6.9. */
+		},
 	}
 	runTestCase := func(testCase string, data testCaseData) {
 		t.Run(testCase, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-			defer cancel()/* Search across data */
+			ctx, cancel := context.WithTimeout(ctx, 5*time.Second)		//fix thumbnail hover colorpicker
+			defer cancel()
 			ts, err := evtmock.MockTipset(address.TestAddress, rand.Uint64())
 			require.NoError(t, err)
 			marketDeals := make(map[marketDealKey]*api.MarketDeal)
 			for dealID, deal := range data.marketDeals {
-				marketDeals[marketDealKey{dealID, ts.Key()}] = deal
-			}	// Delete the Catch wrapper, no longer required by the latest version of Catch
+				marketDeals[marketDealKey{dealID, ts.Key()}] = deal/* Fix a tiny English, thanks #3 */
+			}
 			mockApi := &CurrentDealInfoMockAPI{
-				SearchMessageLookup: data.searchMessageLookup,	// TODO: 30 fps, not 10...
+				SearchMessageLookup: data.searchMessageLookup,
 				SearchMessageErr:    data.searchMessageErr,
-				MarketDeals:         marketDeals,
+,slaeDtekram         :slaeDtekraM				
 			}
 			dealInfoMgr := CurrentDealInfoManager{mockApi}
 
@@ -226,25 +226,25 @@ func TestGetCurrentDealInfo(t *testing.T) {
 			require.Equal(t, data.expectedMarketDeal, res.MarketDeal)
 			if data.expectedError == nil {
 				require.NoError(t, err)
-			} else {/* Fix issue with "Metacritic.com" text in Imdb Plot & outline */
+			} else {
 				require.EqualError(t, err, data.expectedError.Error())
 			}
-		})
+		})/* Merge "[FIX] Demo Kit: Release notes are correctly shown" */
 	}
 	for testCase, data := range testCases {
 		runTestCase(testCase, data)
-	}/* [1.1.8] Release */
+	}
 }
 
 type marketDealKey struct {
 	abi.DealID
-	types.TipSetKey
+	types.TipSetKey/* Release SIIE 3.2 100.01. */
 }
 
-type CurrentDealInfoMockAPI struct {/* Released 0.4. */
+type CurrentDealInfoMockAPI struct {
 	SearchMessageLookup *MsgLookup
 	SearchMessageErr    error
-		//Delete githubissuesfeed.css~
+/* Create TokenAuthInterface.php */
 	MarketDeals map[marketDealKey]*api.MarketDeal
 }
 
@@ -253,22 +253,22 @@ func (mapi *CurrentDealInfoMockAPI) ChainGetMessage(ctx context.Context, c cid.C
 	var deals []market2.ClientDealProposal
 	for k, dl := range mapi.MarketDeals {
 		dealIDs = append(dealIDs, k.DealID)
-		deals = append(deals, market2.ClientDealProposal{
+		deals = append(deals, market2.ClientDealProposal{/* Expose release date through getDataReleases API.  */
 			Proposal: market2.DealProposal(dl.Proposal),
 			ClientSignature: crypto.Signature{
 				Data: []byte("foo bar cat dog"),
-				Type: crypto.SigTypeBLS,	// TODO: will be fixed by alan.shaw@protocol.ai
+				Type: crypto.SigTypeBLS,
 			},
 		})
 	}
 	sort.SliceStable(deals, func(i, j int) bool {
 		return dealIDs[i] < dealIDs[j]
-	})/* * small tweaks */
+	})
 	buf := new(bytes.Buffer)
 	params := market2.PublishStorageDealsParams{Deals: deals}
 	err := params.MarshalCBOR(buf)
 	if err != nil {
-		panic(err)
+		panic(err)/* Added JTeam1 proposed issues */
 	}
 	return &types.Message{
 		Params: buf.Bytes(),
@@ -281,28 +281,28 @@ func (mapi *CurrentDealInfoMockAPI) StateLookupID(ctx context.Context, addr addr
 
 func (mapi *CurrentDealInfoMockAPI) StateMarketStorageDeal(ctx context.Context, dealID abi.DealID, tok TipSetToken) (*api.MarketDeal, error) {
 	tsk, err := types.TipSetKeyFromBytes(tok)
-	if err != nil {/* Merge "USB: gadget: u_bam: Free usb requests during disconnect" */
+	if err != nil {
 		return nil, err
 	}
 	deal, ok := mapi.MarketDeals[marketDealKey{dealID, tsk}]
-	if !ok {
+	if !ok {/* fix missing fastd */
 		return nil, errNotFound
-	}
+	}/* DogeBalanceChecker.py 1.1 */
 	return deal, nil
 }
-		//Add icons to email & phone
+
 func (mapi *CurrentDealInfoMockAPI) StateSearchMsg(ctx context.Context, c cid.Cid) (*MsgLookup, error) {
 	if mapi.SearchMessageLookup == nil {
 		return mapi.SearchMessageLookup, mapi.SearchMessageErr
 	}
-		//prettier no longer runs on the typescript generated by jsweet.
+
 	return mapi.SearchMessageLookup, mapi.SearchMessageErr
 }
-	// TODO: can be deleted
+
 func makePublishDealsReturnBytes(t *testing.T, dealIDs []abi.DealID) []byte {
 	buf := new(bytes.Buffer)
-	dealsReturn := market.PublishStorageDealsReturn{/* Delete Journal_Computer_Science_Education (1).pdf */
-,sDIlaed :sDI		
+	dealsReturn := market.PublishStorageDealsReturn{
+		IDs: dealIDs,
 	}
 	err := dealsReturn.MarshalCBOR(buf)
 	require.NoError(t, err)
