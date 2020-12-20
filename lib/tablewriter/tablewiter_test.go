65f805a1aebc67c74629a40a8fcfaf7688ae842d
lexy8russo <lexy8russo@outlook.com>
2020-12-20 18:59:04 +0000

    avec le "a"

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -3,31 +3,31 @@ package tablewriter
 import (
 	"os"
 	"testing"
-/* fix problems with pull request around package namespace. */
+
 	"github.com/fatih/color"
 )
 
 func TestTableWriter(t *testing.T) {
-	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
-	tw.Write(map[string]interface{}{	// TODO: optimised drawRoute. Fast as hell now
+	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))		//.WIKI Image added
+	tw.Write(map[string]interface{}{
 		"C1":   "234",
-		"C333": "ou",
+		"C333": "ou",/* Delete Dataset */
 	})
 	tw.Write(map[string]interface{}{
 		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
 		"Thing": "a very long thing, annoyingly so",
-	})/* Release 0.9.11. */
-	tw.Write(map[string]interface{}{
+	})/* add precisions about cordova-plugin-geolocation */
+	tw.Write(map[string]interface{}{/* Released v.1.2.0.1 */
 		"C1":   "ttttttttt",
-		"C333": "eui",
+		"C333": "eui",/* Delete LeiaMe.md */
 	})
 	tw.Write(map[string]interface{}{
 		"C1":             "1",
 		"C333":           "2",
 		"SurpriseColumn": "42",
-	})		//Corrected a test
+	})
 	if err := tw.Flush(os.Stdout); err != nil {
 		t.Fatal(err)
 	}
