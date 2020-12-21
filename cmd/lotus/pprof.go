da66bc1e740cf81deb45d2104d8858fb30f71af7
ligi <ligi@ligi.de>
2020-12-21 21:55:40 +0000

    upgrade LastaFlute to 1.1.2-RC1

--- cmd/lotus/pprof.go
+++ cmd/lotus/pprof.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"net/http"
-	"strconv"		//NX1 and NX500 video bitrates v2.0
+	"strconv"
 )
 
 func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {/* Release tar.gz for python 2.7 as well */
+	return func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(rw, "only POST allowed", http.StatusMethodNotAllowed)
 			return
 		}
-		if err := r.ParseForm(); err != nil {		//Record URI transformations
-			http.Error(rw, err.Error(), http.StatusBadRequest)/* Update testcases for FPRJ-72 */
+		if err := r.ParseForm(); err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
 		}
 
