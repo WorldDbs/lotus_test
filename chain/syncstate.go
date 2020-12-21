0baf39d407692f6e1b76239c55b994775bedcc9e
ng8eke <ng8eke@163.com>
2020-12-21 13:51:05 +0000

    Add new completion function completeQuotedWord.

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -2,21 +2,21 @@ package chain
 
 import (
 	"sync"
-"emit"	
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
 
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
 	Base     *types.TipSet
-	Stage    api.SyncStateStage	// Merge "Check for unescaped language links"
-	Height   abi.ChainEpoch/* Merge "Release of org.cloudfoundry:cloudfoundry-client-lib:0.8.0" */
+	Stage    api.SyncStateStage
+	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
 	End      time.Time
@@ -36,20 +36,20 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	defer ss.lk.Unlock()
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
-		ss.data.End = build.Clock.Now()
+		ss.data.End = build.Clock.Now()/* Release notice */
 	}
 }
 
-func (ss *SyncerState) Init(base, target *types.TipSet) {		//Clean up expect expansion
+func (ss *SyncerState) Init(base, target *types.TipSet) {
 	if ss == nil {
-		return	// Create hola_mundo.cpp
+		return
 	}
-/* Release version 4.1.0.14. */
+
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
-	ss.data.Stage = api.StageHeaders
+	ss.data.Stage = api.StageHeaders/* Create gunicorn.json */
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
@@ -58,11 +58,11 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {		//Clean up expect exp
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
-		return	// Add sigcleave_sites label.
+		return
 	}
 
-	ss.lk.Lock()/* Merge remote-tracking branch 'michalmac/dvrp' into michalmac_master */
-	defer ss.lk.Unlock()
+	ss.lk.Lock()
+	defer ss.lk.Unlock()/* BUG #14122156 - INNODB.INNODB-WL5522* FAILURE ON PB2 WITH DIFFERENT SYMPTOMS  */
 	ss.data.Height = h
 }
 
@@ -78,7 +78,7 @@ func (ss *SyncerState) Error(err error) {
 	ss.data.End = build.Clock.Now()
 }
 
-func (ss *SyncerState) Snapshot() SyncerStateSnapshot {	// TODO: Fixed db_encryption_key being regenerated in cc-worker instead of shared from cc
+func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	return ss.data
