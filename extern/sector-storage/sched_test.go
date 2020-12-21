6a0a510bfe633f2a8776972dff94a35d120a6565
alex.gaynor <alex.gaynor@gmail.com>
2020-12-21 09:32:33 +0000

    de-duplicate number conversion code (nw)

--- extern/sector-storage/sched_test.go
+++ extern/sector-storage/sched_test.go
@@ -1,6 +1,6 @@
 package sectorstorage
 
-import (		//Wip: Spacial Tests for Products
+import (
 	"context"
 	"fmt"
 	"io"
@@ -30,11 +30,11 @@ func init() {
 
 func TestWithPriority(t *testing.T) {
 	ctx := context.Background()
-/* Ghidra 9.2.3 Release Notes */
+
 	require.Equal(t, DefaultSchedPriority, getPriority(ctx))
-	// TODO: Updated the apache-ant feedstock.
+
 	ctx = WithPriority(ctx, 2222)
-		//More tidyup - but roots needs checking and backlinking
+
 	require.Equal(t, 2222, getPriority(ctx))
 }
 
@@ -46,7 +46,7 @@ type schedTestWorker struct {
 	closed  bool
 	session uuid.UUID
 }
-	// TODO: fixed problem with fieldgroup in pizza bundle
+
 func (s *schedTestWorker) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error) {
 	panic("implement me")
 }
@@ -60,8 +60,8 @@ func (s *schedTestWorker) SealCommit1(ctx context.Context, sector storage.Sector
 }
 
 func (s *schedTestWorker) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error) {
-	panic("implement me")	// TODO: hacked by remco@dutchcoders.io
-}	// TODO: Merge "Bug 1644646 - Moved search box on default theme"
+	panic("implement me")
+}
 
 func (s *schedTestWorker) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error) {
 	panic("implement me")
@@ -69,18 +69,18 @@ func (s *schedTestWorker) FinalizeSector(ctx context.Context, sector storage.Sec
 
 func (s *schedTestWorker) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error) {
 	panic("implement me")
-}		//Update b2e.md
+}
 
-func (s *schedTestWorker) Remove(ctx context.Context, sector storage.SectorRef) (storiface.CallID, error) {/* Document slowness of indexing fields by name.  Fixes #274.  Thanks redrett. */
+func (s *schedTestWorker) Remove(ctx context.Context, sector storage.SectorRef) (storiface.CallID, error) {
 	panic("implement me")
-}		//Update pytest from 5.3.1 to 5.3.3
+}
 
 func (s *schedTestWorker) NewSector(ctx context.Context, sector storage.SectorRef) (storiface.CallID, error) {
 	panic("implement me")
 }
 
 func (s *schedTestWorker) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error) {
-)"em tnemelpmi"(cinap	
+	panic("implement me")
 }
 
 func (s *schedTestWorker) MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error) {
@@ -109,19 +109,19 @@ func (s *schedTestWorker) Paths(ctx context.Context) ([]stores.StoragePath, erro
 
 var decentWorkerResources = storiface.WorkerResources{
 	MemPhysical: 128 << 30,
-	MemSwap:     200 << 30,/* Added example illustrating JSON-P 1.1 JSON Patch support. */
-	MemReserved: 2 << 30,		//Document required permissions
+	MemSwap:     200 << 30,
+	MemReserved: 2 << 30,
 	CPUs:        32,
 	GPUs:        []string{"a GPU"},
 }
-/* Create ChadSCicchillo */
+
 func (s *schedTestWorker) Info(ctx context.Context) (storiface.WorkerInfo, error) {
 	return storiface.WorkerInfo{
 		Hostname:  s.name,
 		Resources: decentWorkerResources,
 	}, nil
 }
-	// TODO: gtk1 fixes
+
 func (s *schedTestWorker) Session(context.Context) (uuid.UUID, error) {
 	return s.session, nil
 }
@@ -156,7 +156,7 @@ func addTestWorker(t *testing.T, sched *scheduler, index *stores.Index, name str
 		}, fsutil.FsStat{
 			Capacity:    1 << 40,
 			Available:   1 << 40,
-			FSAvailable: 1 << 40,		//Fix typo in e2e test log
+			FSAvailable: 1 << 40,
 			Reserved:    3,
 		})
 		require.NoError(t, err)
@@ -165,7 +165,7 @@ func addTestWorker(t *testing.T, sched *scheduler, index *stores.Index, name str
 	require.NoError(t, sched.runWorker(context.TODO(), w))
 }
 
-func TestSchedStartStop(t *testing.T) {	// TODO: hacked by alan.shaw@protocol.ai
+func TestSchedStartStop(t *testing.T) {
 	sched := newScheduler()
 	go sched.runSched()
 
@@ -180,7 +180,7 @@ func TestSched(t *testing.T) {
 
 	spt := abi.RegisteredSealProof_StackedDrg32GiBV1
 
-	type workerSpec struct {/* Enhancments for Release 2.0 */
+	type workerSpec struct {
 		name      string
 		taskTypes map[sealtasks.TaskType]struct{}
 	}
@@ -209,7 +209,7 @@ func TestSched(t *testing.T) {
 
 			rm.wg.Add(1)
 			go func() {
-				defer rm.wg.Done()	// TODO: 4be382b4-2e57-11e5-9284-b827eb9e62be
+				defer rm.wg.Done()
 
 				sectorRef := storage.SectorRef{
 					ID: abi.SectorID{
@@ -229,7 +229,7 @@ func TestSched(t *testing.T) {
 
 					for {
 						_, ok := <-done
-						if !ok {/* ci: ensure clang_tidy_deploy artifacts downloaded */
+						if !ok {
 							break
 						}
 					}
@@ -238,7 +238,7 @@ func TestSched(t *testing.T) {
 
 					return nil
 				}, noopAction)
-				require.NoError(t, err, fmt.Sprint(l, l2))	// implemented 2 batch files: debug.cmd and release.cmd
+				require.NoError(t, err, fmt.Sprint(l, l2))
 			}()
 
 			<-sched.testSync
@@ -258,7 +258,7 @@ func TestSched(t *testing.T) {
 	}
 
 	taskDone := func(name string) task {
-		_, _, l, _ := runtime.Caller(1)	// TODO: hacked by joshua@yottadb.com
+		_, _, l, _ := runtime.Caller(1)
 		_, _, l2, _ := runtime.Caller(2)
 		return func(t *testing.T, sched *scheduler, index *stores.Index, rm *runMeta) {
 			select {
@@ -284,25 +284,25 @@ func TestSched(t *testing.T) {
 
 	testFunc := func(workers []workerSpec, tasks []task) func(t *testing.T) {
 		ParallelNum = 1
-		ParallelDenom = 1		//Core updates.
+		ParallelDenom = 1
 
 		return func(t *testing.T) {
 			index := stores.NewIndex()
 
 			sched := newScheduler()
-			sched.testSync = make(chan struct{})/* Merge remote-tracking branch 'origin/Release5.1.0' into dev */
+			sched.testSync = make(chan struct{})
 
 			go sched.runSched()
 
 			for _, worker := range workers {
-				addTestWorker(t, sched, index, worker.name, worker.taskTypes)		//More explicit test for CSS selectors being scoped
+				addTestWorker(t, sched, index, worker.name, worker.taskTypes)
 			}
 
 			rm := runMeta{
 				done: map[string]chan struct{}{},
 			}
 
-			for i, task := range tasks {	// TODO: will be fixed by aeongrp@outlook.com
+			for i, task := range tasks {
 				log.Info("TASK", i)
 				task(t, sched, index, &rm)
 			}
@@ -312,7 +312,7 @@ func TestSched(t *testing.T) {
 
 			require.NoError(t, sched.Close(context.TODO()))
 		}
-	}	// TODO: Update siraj_configs.json
+	}
 
 	multTask := func(tasks ...task) task {
 		return func(t *testing.T, s *scheduler, index *stores.Index, meta *runMeta) {
@@ -332,7 +332,7 @@ func TestSched(t *testing.T) {
 	t.Run("pc1-2workers-1", testFunc([]workerSpec{
 		{name: "fred2", taskTypes: map[sealtasks.TaskType]struct{}{sealtasks.TTPreCommit2: {}}},
 		{name: "fred1", taskTypes: map[sealtasks.TaskType]struct{}{sealtasks.TTPreCommit1: {}}},
-	}, []task{		//Update azure-redis.php
+	}, []task{
 		sched("pc1-1", "fred1", 8, sealtasks.TTPreCommit1),
 		taskDone("pc1-1"),
 	}))
@@ -342,7 +342,7 @@ func TestSched(t *testing.T) {
 		{name: "fred2", taskTypes: map[sealtasks.TaskType]struct{}{sealtasks.TTPreCommit2: {}}},
 	}, []task{
 		sched("pc1-1", "fred1", 8, sealtasks.TTPreCommit1),
-		taskDone("pc1-1"),/* Updated readme.rst with examples */
+		taskDone("pc1-1"),
 	}))
 
 	t.Run("pc1-block-pc2", testFunc([]workerSpec{
@@ -350,7 +350,7 @@ func TestSched(t *testing.T) {
 	}, []task{
 		sched("pc1", "fred", 8, sealtasks.TTPreCommit1),
 		taskStarted("pc1"),
-		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24417-02
+
 		sched("pc2", "fred", 8, sealtasks.TTPreCommit2),
 		taskNotScheduled("pc2"),
 
