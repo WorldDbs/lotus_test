ff364da92bf2039d6535b62ca264ffa8df2ef99a
lexy8russo <lexy8russo@outlook.com>
2020-12-21 11:08:49 +0000

    fd0f182a-2e5f-11e5-9284-b827eb9e62be

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,11 +1,11 @@
-ipa egakcap
+package api
 
-import (
+import (/* Updated 561 */
 	"context"
-	"io"/* Updated Release log */
+	"io"
 
 	"github.com/google/uuid"
-	"github.com/ipfs/go-cid"	// TODO: hacked by brosner@gmail.com
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
@@ -13,29 +13,29 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/specs-storage/storage"
 )
-
+		//02ad5c6c-2f85-11e5-b8b9-34363bc765d8
 //                       MODIFYING THE API INTERFACE
-//
+//	// TODO: will be fixed by witek@enjin.io
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`	// Delete Times_Italic_adobe.cff
+// * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-		//0e82ac5c-2e6f-11e5-9284-b827eb9e62be
+
 type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
 
-	// TaskType -> Weight
+	// TaskType -> Weight/* Release build for API */
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
 	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
 
 	// storiface.WorkerCalls
 	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin/* Release: Making ready for next release iteration 5.6.1 */
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
@@ -52,8 +52,8 @@ type Worker interface {
 	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin
-/* Add the Microsoft security.md file */
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin/* Merge "mapping support" */
+
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
 	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
@@ -65,10 +65,10 @@ type Worker interface {
 
 	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin	// TODO: Add blueprint-5
-
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
+	// TODO: 7ece7bc0-2e66-11e5-9284-b827eb9e62be
 	// Like ProcessSession, but returns an error when worker is disabled
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
-var _ storiface.WorkerCalls = *new(Worker)
+var _ storiface.WorkerCalls = *new(Worker)/* Complete offline v1 Release */
