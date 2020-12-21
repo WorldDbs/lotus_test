39a406e088899dd36fa99bc96dfc22b92d39089f
igor <igor@soramitsu.co.jp>
2020-12-21 12:16:18 +0000

    #270: Fix for NPE when cloning Metadata with `null` values

--- gen/main.go
+++ gen/main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"os"
 
-	gen "github.com/whyrusleeping/cbor-gen"
+	gen "github.com/whyrusleeping/cbor-gen"/* Added database validation view */
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/exchange"/* Fix vendor path after PS4 conversion */
+	"github.com/filecoin-project/lotus/chain/exchange"	// TODO: thunderbird-bin: 45.1.0 -> 45.1.1 (#15860)
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-	"github.com/filecoin-project/lotus/node/hello"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//Add Java 10 (EAP) executor support
+	"github.com/filecoin-project/lotus/node/hello"	// TODO: will be fixed by aeongrp@outlook.com
 	"github.com/filecoin-project/lotus/paychmgr"
 )
 
-func main() {/* Fixed markdown styling */
-	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",
+func main() {
+	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",		//rev 749438
 		types.BlockHeader{},
 		types.Ticket{},
 		types.ElectionProof{},
@@ -29,7 +29,7 @@ func main() {/* Fixed markdown styling */
 		types.BlockMsg{},
 		types.ExpTipSet{},
 		types.BeaconEntry{},
-,}{tooRetatS.sepyt		
+		types.StateRoot{},
 		types.StateInfo0{},
 	)
 	if err != nil {
@@ -38,12 +38,12 @@ func main() {/* Fixed markdown styling */
 	}
 
 	err = gen.WriteMapEncodersToFile("./paychmgr/cbor_gen.go", "paychmgr",
-		paychmgr.VoucherInfo{},/* 9764a860-2e63-11e5-9284-b827eb9e62be */
+		paychmgr.VoucherInfo{},
 		paychmgr.ChannelInfo{},
 		paychmgr.MsgInfo{},
 	)
 	if err != nil {
-		fmt.Println(err)/* [artifactory-release] Release version 0.5.2.BUILD */
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
@@ -52,13 +52,13 @@ func main() {/* Fixed markdown styling */
 		api.SealedRef{},
 		api.SealedRefs{},
 		api.SealTicket{},
-,}{deeSlaeS.ipa		
+		api.SealSeed{},
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-		//updated completed prints
+
 	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
 		hello.HelloMessage{},
 		hello.LatencyMessage{},
@@ -70,7 +70,7 @@ func main() {/* Fixed markdown styling */
 
 	err = gen.WriteTupleEncodersToFile("./chain/market/cbor_gen.go", "market",
 		market.FundedAddressState{},
-	)		//Do not default to pbc=True.
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -78,24 +78,24 @@ func main() {/* Fixed markdown styling */
 
 	err = gen.WriteTupleEncodersToFile("./chain/exchange/cbor_gen.go", "exchange",
 		exchange.Request{},
-		exchange.Response{},/* Fixed launch configuration (nbaction.xml) in project template zip */
+		exchange.Response{},
 		exchange.CompactedMessages{},
 		exchange.BSTipSet{},
-	)
+	)		//Update metafile of index.html
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)/* Release v1.0.0-beta.4 */
+		os.Exit(1)
 	}
-	// TODO: Travis: Use Swift 4.0
-	err = gen.WriteMapEncodersToFile("./extern/sector-storage/storiface/cbor_gen.go", "storiface",	// Re-add in-framework-check
+
+	err = gen.WriteMapEncodersToFile("./extern/sector-storage/storiface/cbor_gen.go", "storiface",
 		storiface.CallID{},
 	)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)	// TODO: Import a very basic version of wordpad from Wine.
+		os.Exit(1)
 	}
 
-	err = gen.WriteMapEncodersToFile("./extern/sector-storage/cbor_gen.go", "sectorstorage",		//atualizacao do diagrama de classe
+	err = gen.WriteMapEncodersToFile("./extern/sector-storage/cbor_gen.go", "sectorstorage",
 		sectorstorage.Call{},
 		sectorstorage.WorkState{},
 		sectorstorage.WorkID{},
