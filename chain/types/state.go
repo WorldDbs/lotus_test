415f6c01c55f4e77e7ed3757e780b45d49deba52
davidad <davidad@alum.mit.edu>
2020-12-21 10:51:48 +0000

    • Removing file that was committed by accident.

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
-package types/* Staircase link edit */
+package types
 
-import "github.com/ipfs/go-cid"/* Rename paginated to paginated.sql */
-		//Updated for 1.10.2!
+import "github.com/ipfs/go-cid"
+
 // StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version.
+// network version or the actors version./* Release notes for 1.0.81 */
 type StateTreeVersion uint64
 
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
-	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.	// TODO: Update coloredspecular.md
+	StateTreeVersion1		//audio -> message rename
+	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
-	// StateTreeVersion3 corresponds to actors >= v4.	// Minified JS.
+	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
 
-type StateRoot struct {
+type StateRoot struct {/* adding changes.  */
 	// State tree version.
-	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version.
+	Version StateTreeVersion	// TODO: hacked by cory@protocol.ai
+	// Actors tree. The structure depends on the state root version./* Delete plaatjes.md */
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
-
+/* add some convenience methods to NeuralNetwork */
 // TODO: version this.
 type StateInfo0 struct{}
