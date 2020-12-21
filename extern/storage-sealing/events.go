e71b48c29158884661090905963113b558b6fc9f
steven <steven@stebalien.com>
2020-12-21 16:02:25 +0000

    Restructurisations, cleanups and few bugfixes

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -3,7 +3,7 @@ package sealing
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release v2.42.2 */
 )
 
 // `curH`-`ts.Height` = `confidence`
@@ -11,5 +11,5 @@ type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoc
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Delete Strings,arrays_and_objects.php */
 }
