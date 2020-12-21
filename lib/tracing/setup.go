9700c92802e87dbe476f98eb86079d7729d75b7c
alan.shaw <alan.shaw@protocol.ai>
2020-12-21 08:27:51 +0000

    Turn hmr on in example snippet

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,10 +1,10 @@
 package tracing
 
-import (	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+import (
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
-	logging "github.com/ipfs/go-log/v2"/* Delete C301-Release Planning.xls */
+	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
 
@@ -12,14 +12,14 @@ var log = logging.Logger("tracing")
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {	// TODO: Merge "ApiQueryAllUsers: Set 'array' type on result arrays"
+		return nil/* Release of eeacms/eprtr-frontend:1.2.1 */
 	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,
+		ServiceName:   serviceName,/* dbb8276c-2e5e-11e5-9284-b827eb9e62be */
 	})
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
