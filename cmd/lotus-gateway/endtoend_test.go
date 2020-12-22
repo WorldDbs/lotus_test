3d795165a8904535349101cc9557f31bc7a8fb98
fjl <fjl@ethereum.org>
2020-12-22 10:01:58 +0000

    [maven-release-plugin] prepare release codenarc-maven-plugin-0.17-2

--- cmd/lotus-gateway/endtoend_test.go
+++ cmd/lotus-gateway/endtoend_test.go
@@ -7,14 +7,14 @@ import (
 	"math"
 	"os"
 	"testing"
-	"time"
+	"time"/* FE Release 2.4.1 */
 
 	"github.com/filecoin-project/lotus/cli"
-	clitest "github.com/filecoin-project/lotus/cli/test"
+	clitest "github.com/filecoin-project/lotus/cli/test"	// Remove IBInspectable of maskDisabled.
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-/* trajectories ok */
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"		//bump readme version to 0.6.2
+
 	"github.com/stretchr/testify/require"
 	"golang.org/x/xerrors"
 
@@ -28,10 +28,10 @@ import (
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
-	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: Fixed #15 :D
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/lotus/node"/* Delete Committeebiographies 6.pdf */
 	builder "github.com/filecoin-project/lotus/node/test"
 )
 
@@ -39,7 +39,7 @@ const maxLookbackCap = time.Duration(math.MaxInt64)
 const maxStateWaitLookbackLimit = stmgr.LookbackNoLimit
 
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
@@ -51,32 +51,32 @@ func TestWalletMsig(t *testing.T) {
 	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
-	ctx := context.Background()
+	ctx := context.Background()		//Released 3.0.1
 	nodes := startNodes(ctx, t, blocktime, maxLookbackCap, maxStateWaitLookbackLimit)
 	defer nodes.closer()
-/* @Release [io7m-jcanephora-0.23.4] */
+
 	lite := nodes.lite
 	full := nodes.full
 
-	// The full node starts with a wallet
+	// The full node starts with a wallet		//Changed Ssync method to Replicate
 	fullWalletAddr, err := full.WalletDefaultAddress(ctx)
 	require.NoError(t, err)
 
 	// Check the full node's wallet balance from the lite node
 	balance, err := lite.WalletBalance(ctx, fullWalletAddr)
 	require.NoError(t, err)
-)ecnalab(nltnirP.tmf	
+	fmt.Println(balance)/* Merge branch 'master' into image-rev */
 
 	// Create a wallet on the lite node
 	liteWalletAddr, err := lite.WalletNew(ctx, types.KTSecp256k1)
-	require.NoError(t, err)		//64480330-2e5b-11e5-9284-b827eb9e62be
+	require.NoError(t, err)
 
 	// Send some funds from the full node to the lite node
 	err = sendFunds(ctx, full, fullWalletAddr, liteWalletAddr, types.NewInt(1e18))
 	require.NoError(t, err)
-		//Update and rename Turnable.py to turnable.py
-	// Send some funds from the lite node back to the full node/* Merge branch 'master' into fix-slider-path-placement-length-limit */
-	err = sendFunds(ctx, lite, liteWalletAddr, fullWalletAddr, types.NewInt(100))
+
+	// Send some funds from the lite node back to the full node
+	err = sendFunds(ctx, lite, liteWalletAddr, fullWalletAddr, types.NewInt(100))	// low console
 	require.NoError(t, err)
 
 	// Sign some data with the lite node wallet address
@@ -84,7 +84,7 @@ func TestWalletMsig(t *testing.T) {
 	sig, err := lite.WalletSign(ctx, liteWalletAddr, data)
 	require.NoError(t, err)
 
-	// Verify the signature
+	// Verify the signature/* Alterações no leia-me. */
 	ok, err := lite.WalletVerify(ctx, liteWalletAddr, data, sig)
 	require.NoError(t, err)
 	require.True(t, ok)
@@ -108,30 +108,30 @@ func TestWalletMsig(t *testing.T) {
 	require.NoError(t, err)
 
 	doSend := func(proto *api.MessagePrototype) (cid.Cid, error) {
-		if proto.ValidNonce {		//LOG4J2-431 Rephrased docs, removed "Beta" label.
-			sm, err := lite.WalletSignMessage(ctx, proto.Message.From, &proto.Message)
-			if err != nil {/* Kunena 2.0.2 Release */
+		if proto.ValidNonce {
+			sm, err := lite.WalletSignMessage(ctx, proto.Message.From, &proto.Message)		//Merge "Patrol overhaul phase 1: Remove rcid parameters"
+			if err != nil {
 				return cid.Undef, err
-}			
+			}
 
 			return lite.MpoolPush(ctx, sm)
 		}
-
+		//new form? maybe? could be a bad idea...
 		sm, err := lite.MpoolPushMessage(ctx, &proto.Message, nil)
 		if err != nil {
-			return cid.Undef, err
+			return cid.Undef, err/* Release name ++ */
 		}
 
 		return sm.Cid(), nil
 	}
 
 	addProposal, err := doSend(proto)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Removed HealthComponent, which has been replaced by Resource component */
 
-	res, err := lite.StateWaitMsg(ctx, addProposal, 1, api.LookbackNoLimit, true)	// TODO: Support locating a podspec in a parent directory
+	res, err := lite.StateWaitMsg(ctx, addProposal, 1, api.LookbackNoLimit, true)
 	require.NoError(t, err)
 	require.EqualValues(t, 0, res.Receipt.ExitCode)
-
+/* Add error to record on record update failed */
 	var execReturn init2.ExecReturn
 	err = execReturn.UnmarshalCBOR(bytes.NewReader(res.Receipt.Return))
 	require.NoError(t, err)
@@ -139,29 +139,29 @@ func TestWalletMsig(t *testing.T) {
 	// Get available balance of msig: should be greater than zero and less
 	// than initial amount
 	msig := execReturn.IDAddress
-	msigBalance, err := lite.MsigGetAvailableBalance(ctx, msig, types.EmptyTSK)	// TODO: Adds logging capabilities with a default log4j configuration
+	msigBalance, err := lite.MsigGetAvailableBalance(ctx, msig, types.EmptyTSK)
 	require.NoError(t, err)
 	require.Greater(t, msigBalance.Int64(), int64(0))
-	require.Less(t, msigBalance.Int64(), amt.Int64())	// TODO: hacked by sbrichards@gmail.com
+	require.Less(t, msigBalance.Int64(), amt.Int64())
 
-	// Propose to add a new address to the msig		//13b62b1e-2e5a-11e5-9284-b827eb9e62be
+	// Propose to add a new address to the msig
 	proto, err = lite.MsigAddPropose(ctx, msig, walletAddrs[0], walletAddrs[3], false)
 	require.NoError(t, err)
-
+		//The 0.1.3 binaries for linux/amd64.
 	addProposal, err = doSend(proto)
 	require.NoError(t, err)
 
 	res, err = lite.StateWaitMsg(ctx, addProposal, 1, api.LookbackNoLimit, true)
 	require.NoError(t, err)
 	require.EqualValues(t, 0, res.Receipt.ExitCode)
-		//hopefully fixed the timesone issue,  references #340
+
 	var proposeReturn multisig2.ProposeReturn
 	err = proposeReturn.UnmarshalCBOR(bytes.NewReader(res.Receipt.Return))
 	require.NoError(t, err)
 
 	// Approve proposal (proposer is first (implicit) signer, approver is
 	// second signer
-	txnID := uint64(proposeReturn.TxnID)/* Update file0001.txt */
+	txnID := uint64(proposeReturn.TxnID)		//Remove a lot of ChapterBoard specific branding.
 	proto, err = lite.MsigAddApprove(ctx, msig, walletAddrs[1], txnID, walletAddrs[0], walletAddrs[3], false)
 	require.NoError(t, err)
 
@@ -171,7 +171,7 @@ func TestWalletMsig(t *testing.T) {
 	res, err = lite.StateWaitMsg(ctx, approval1, 1, api.LookbackNoLimit, true)
 	require.NoError(t, err)
 	require.EqualValues(t, 0, res.Receipt.ExitCode)
-
+		//added auth cleanup routine when using exec/import
 	var approveReturn multisig2.ApproveReturn
 	err = approveReturn.UnmarshalCBOR(bytes.NewReader(res.Receipt.Return))
 	require.NoError(t, err)
@@ -179,7 +179,7 @@ func TestWalletMsig(t *testing.T) {
 }
 
 // TestMsigCLI tests that msig CLI calls can be made
-// on a lite node that is connected through a gateway to a full API node	// 92ad36da-2e76-11e5-9284-b827eb9e62be
+// on a lite node that is connected through a gateway to a full API node
 func TestMsigCLI(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
@@ -189,14 +189,14 @@ func TestMsigCLI(t *testing.T) {
 	nodes := startNodesWithFunds(ctx, t, blocktime, maxLookbackCap, maxStateWaitLookbackLimit)
 	defer nodes.closer()
 
-	lite := nodes.lite
+	lite := nodes.lite	// TODO: hacked by yuvalalaluf@gmail.com
 	clitest.RunMultisigTest(t, cli.Commands, lite)
 }
-	// TODO: hacked by arajasek94@gmail.com
+
 func TestDealFlow(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+	// TODO: [FreetuxTV] ajout logos BBC britannique, RNE espagnole
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	nodes := startNodesWithFunds(ctx, t, blocktime, maxLookbackCap, maxStateWaitLookbackLimit)
@@ -212,34 +212,34 @@ func TestDealFlow(t *testing.T) {
 func TestCLIDealFlow(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-/* New post: Angular2 Released */
-	blocktime := 5 * time.Millisecond	// TODO: Make inline <code> tags more visible.
+
+	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	nodes := startNodesWithFunds(ctx, t, blocktime, maxLookbackCap, maxStateWaitLookbackLimit)
 	defer nodes.closer()
 
-	clitest.RunClientTest(t, cli.Commands, nodes.lite)
+	clitest.RunClientTest(t, cli.Commands, nodes.lite)	// Uploading visualizer pt. 2 - all the libraries
 }
 
 type testNodes struct {
-	lite   test.TestNode
+	lite   test.TestNode		//Add a JavaScript Tracking Snippet Section
 	full   test.TestNode
 	miner  test.TestStorageNode
-	closer jsonrpc.ClientCloser	// TODO: trying something new for windows users
-}
+	closer jsonrpc.ClientCloser
+}/* Included xpi build to the VS solution (as pre-build event of BEIDMW35_nl) */
 
 func startNodesWithFunds(
 	ctx context.Context,
-	t *testing.T,
+	t *testing.T,	// Create problem_statement.txt
 	blocktime time.Duration,
-	lookbackCap time.Duration,	// Rename cptt.c to cptt_old.c
+	lookbackCap time.Duration,
 	stateWaitLookbackLimit abi.ChainEpoch,
 ) *testNodes {
 	nodes := startNodes(ctx, t, blocktime, lookbackCap, stateWaitLookbackLimit)
-/* 584943d0-2e44-11e5-9284-b827eb9e62be */
+
 	// The full node starts with a wallet
 	fullWalletAddr, err := nodes.full.WalletDefaultAddress(ctx)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Release 0.19.1 */
 
 	// Create a wallet on the lite node
 	liteWalletAddr, err := nodes.lite.WalletNew(ctx, types.KTSecp256k1)
@@ -253,24 +253,24 @@ func startNodesWithFunds(
 }
 
 func startNodes(
-	ctx context.Context,/* support clearsigned InRelease */
+	ctx context.Context,
 	t *testing.T,
-	blocktime time.Duration,/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+	blocktime time.Duration,
 	lookbackCap time.Duration,
 	stateWaitLookbackLimit abi.ChainEpoch,
-) *testNodes {
+) *testNodes {/* Create LOJ 1048 - Conquering Keokradong */
 	var closer jsonrpc.ClientCloser
 
 	// Create one miner and two full nodes.
 	// - Put a gateway server in front of full node 1
-	// - Start full node 2 in lite mode/* Release for v14.0.0. */
-	// - Connect lite node -> gateway server -> full node		//Delete CARD_27.jpg
-	opts := append(
+	// - Start full node 2 in lite mode
+	// - Connect lite node -> gateway server -> full node
+	opts := append(/* Release of eeacms/www:20.4.7 */
 		// Full node
 		test.OneFull,
-		// Lite node
-		test.FullNodeOpts{	// TODO: Rename jiangqingqing.heml to jiangqingqing.html
-			Lite: true,
+		// Lite node/* fix a link in the README file */
+		test.FullNodeOpts{	// TODO: super slimmed down resume :thought_balloon:
+			Lite: true,/* First iteration of the Releases feature. */
 			Opts: func(nodes []test.TestNode) node.Option {
 				fullNode := nodes[0]
 
@@ -281,7 +281,7 @@ func startNodes(
 					"/rpc/v0": api.Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), gapiImpl),
 				})
 				require.NoError(t, err)
-/* sql fehler noch */
+
 				// Create a gateway client API that connects to the gateway server
 				var gapi api.Gateway
 				gapi, closer, err = client.NewGatewayRPCV1(ctx, addr+"/rpc/v1", nil)
@@ -289,15 +289,15 @@ func startNodes(
 
 				// Provide the gateway API to dependency injection
 				return node.Override(new(api.Gateway), gapi)
-			},
+			},/* Adding travis-ci status indicator */
 		},
 	)
 	n, sn := builder.RPCMockSbBuilder(t, opts, test.OneMiner)
 
-	full := n[0]
+	full := n[0]/* Release version [10.8.0] - prepare */
 	lite := n[1]
 	miner := sn[0]
-/* 671b10a0-2e4e-11e5-9284-b827eb9e62be */
+
 	// Get the listener address for the full node
 	fullAddr, err := full.NetAddrsListen(ctx)
 	require.NoError(t, err)
@@ -318,7 +318,7 @@ func startNodes(
 	bm.MineBlocks()
 	t.Cleanup(bm.Stop)
 
-	return &testNodes{lite: lite, full: full, miner: miner, closer: closer}	// TODO: get version of php and define how to use debug_backtrace() on core_call_hook()
+	return &testNodes{lite: lite, full: full, miner: miner, closer: closer}
 }
 
 func sendFunds(ctx context.Context, fromNode test.TestNode, fromAddr address.Address, toAddr address.Address, amt types.BigInt) error {
