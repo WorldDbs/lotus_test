524c7d29d6331b679c4818deefb0779ca98ceb1a
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-12-22 01:16:47 +0000

    Fixed the autoloader to work correctly with namespaces

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -1,11 +1,11 @@
-package market	// TODO: calvinsys: Remove superfluous print-stmt
+package market/* Readme: Fix typo in Codecov example */
 
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Refactor multi class fix, update readme
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Create 827. Making A Large Island */
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
@@ -16,15 +16,15 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 	return results, nil
 }
 
-type marketProposalsDiffer struct {
+type marketProposalsDiffer struct {	// TODO: hacked by lexy8russo@outlook.com
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
-/* Add Icon-60.png */
+
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
 	if err != nil {
-		return err	// TODO: will be fixed by mikeal.rogers@gmail.com
+		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
@@ -36,8 +36,8 @@ func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error
 }
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
-	dp, err := d.pre.decode(val)
-	if err != nil {/* change log and version bump */
+	dp, err := d.pre.decode(val)/* Release v5.3.0 */
+	if err != nil {
 		return err
 	}
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
@@ -49,22 +49,22 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
-	return results, nil
+	return results, nil/* Fix protocol for badge url of StackShare */
 }
 
-type marketStatesDiffer struct {
+type marketStatesDiffer struct {	// [EEPROM/AT24C02/BasicReadWrite] add project
 	Results  *DealStateChanges
 	pre, cur DealStates
 }
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
-	ds, err := d.cur.decode(val)/* Release for 3.3.0 */
+	ds, err := d.cur.decode(val)
 	if err != nil {
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
-}	// TODO: Tidy welcome message
+}
 
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
