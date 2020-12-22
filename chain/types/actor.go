48e0c0322e4d0fe25abacd6427dfc9192ea81f1f
joshua <joshua@yottadb.com>
2020-12-22 20:07:00 +0000

    Add pointer to FinnTreeBank 1

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -2,16 +2,16 @@ package types
 
 import (
 	"errors"
-/* added more optional skin controls  */
+
 	"github.com/ipfs/go-cid"
-)/* Added sanity test */
+)
 
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
-	Code    cid.Cid/* Merge "msm: bam_dmux: log state changes" into msm-3.0 */
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// change: updating colors again
+	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
-	Balance BigInt/* took out filterPollutant */
+	Balance BigInt
 }
