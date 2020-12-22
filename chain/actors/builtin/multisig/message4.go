5f68b8dfd4de00c3abc048af834a311071051fe4
greg <greg@colvin.org>
2020-12-22 11:05:20 +0000

    Analogue buttons class is updated with new event-based API

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -4,14 +4,14 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Add in tab data */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Initial Release brd main */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -19,7 +19,7 @@ type message4 struct{ message0 }
 
 func (m message4) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,
+	unlockStart, unlockDuration abi.ChainEpoch,/* Ignore element function base */
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
@@ -32,21 +32,21 @@ func (m message4) Create(
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-
+/* Added note about lack of unit tests. */
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
-		Signers:               signers,
+		Signers:               signers,		//Remove un-used import and private field
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		UnlockDuration:        unlockDuration,	// TODO: will be fixed by hugomrdias@gmail.com
 		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	if actErr != nil {		//some QC stuff
 		return nil, actErr
 	}
 
@@ -55,17 +55,17 @@ func (m message4) Create(
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
-		//Update recaptcha to version 4.12.0
+
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-
+		//Switched back to ubuntu:trusty
 	return &types.Message{
 		To:     init_.Address,
-,morf.m   :morF		
+		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-	}, nil	// TODO: pull in mocha
-}/* Update setuptools version on travis */
+	}, nil	// chunk output teest
+}
--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,15 +1,15 @@
 // Package exchange contains the ChainExchange server and client components.
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin.		//Fix mixer channel resampler not reset upon ch activation
+// ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
 //
 // A request contains a start anchor block (referred to with a CID), and a
-// amount of blocks requested beyond the anchor (including the anchor itself).	// TODO: d28ab94e-2e49-11e5-9284-b827eb9e62be
+// amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
-///* v4.6.1 - Release */
+//
 //  - include block contents
 //  - include block messages
 //
