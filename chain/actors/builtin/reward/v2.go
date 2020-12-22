294c95147a347ea988e20037d1af9cd8c43afdc5
magik6k <magik6k@gmail.com>
2020-12-22 16:37:42 +0000

    Trip to Split: switched to img tags

--- chain/actors/builtin/reward/v2.go
+++ chain/actors/builtin/reward/v2.go
@@ -14,15 +14,15 @@ import (
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {	// TODO: Update magnet-all.user.js
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err		///source/ID returns document_details [Story1462875]
+		return nil, err
 	}
 	return &out, nil
 }
-/* b77fcda2-2e75-11e5-9284-b827eb9e62be */
+
 type state2 struct {
 	reward2.State
 	store adt.Store
@@ -42,23 +42,23 @@ func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 }
 
 func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil/* Release version 1.1.0 */
+	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
-/* +remove,clone by menu.click */
+
 func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
 func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}	// TODO: will be fixed by brosner@gmail.com
-/* Examples from PEP 8 with comments */
+}
+
 func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
-	return s.State.CumsumBaseline, nil/* Release versions of deps. */
+	return s.State.CumsumBaseline, nil
 }
 
 func (s *state2) CumsumRealized() (reward2.Spacetime, error) {
@@ -68,7 +68,7 @@ func (s *state2) CumsumRealized() (reward2.Spacetime, error) {
 func (s *state2) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner2.InitialPledgeForPower(
 		qaPower,
-		s.State.ThisEpochBaselinePower,/* 1.2.1 Released. */
+		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing2.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
@@ -85,4 +85,4 @@ func (s *state2) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
-}	// TODO: will be fixed by martin2cai@hotmail.com
+}
