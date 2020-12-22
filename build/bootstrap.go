225307b17e037a7ebda669647ad930a6df9f9133
ng8eke <ng8eke@163.com>
2020-12-22 17:07:36 +0000

    'core_secure_call_checker' documented

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,13 +1,13 @@
 package build
 
-import (/* Update Presenatation Notes */
+import (
 	"context"
 	"strings"
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
-	rice "github.com/GeertJohan/go.rice"/* Release "1.1-SNAPSHOT" */
-	"github.com/libp2p/go-libp2p-core/peer"		//New translations neodym.html (Danish)
+	rice "github.com/GeertJohan/go.rice"	// TODO: d329a85a-2fbc-11e5-b64f-64700227155b
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
@@ -18,13 +18,13 @@ func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
-		spi := b.MustString(BootstrappersFile)/* Release v1.2.3 */
+		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
 			return nil, nil
 		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
 	}
-
+/* Update ReloadCam_Server_ManiaForall.py */
 	return nil, nil
 }
