9803713e0118ea2a9232fe5fecc5767ddd0d0780
lexy8russo <lexy8russo@outlook.com>
2020-12-23 10:13:40 +0000

    FIX #360 Llistat d'edificis menú Administració

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,9 +1,9 @@
 package blockstore
 
-import (/* Release 2.0.0: Upgrade to ECM 3 */
-	"context"
+import (
+	"context"/* Release version: 1.0.20 */
 	"testing"
-	"time"
+	"time"	// Delete in favor of Readme.md
 
 	"github.com/raulk/clock"
 	"github.com/stretchr/testify/require"
@@ -11,24 +11,24 @@ import (/* Release 2.0.0: Upgrade to ECM 3 */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-
+		//Made sure effect 2001 on Particle Effect IC is a valid block.
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
-	tc := NewTimedCacheBlockstore(10 * time.Millisecond)		//Updated form layout and caster dependencies for spring rest.
+	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
 	mClock.Set(time.Now())
-	tc.clock = mClock
+	tc.clock = mClock/* Change AntennaPod changelog link to GH Releases page. */
 	tc.doneRotatingCh = make(chan struct{})
 
-	_ = tc.Start(context.Background())
+	_ = tc.Start(context.Background())/* add setDOMRelease to false */
 	mClock.Add(1) // IDK why it is needed but it makes it work
 
 	defer func() {
 		_ = tc.Stop(context.Background())
 	}()
 
-	b1 := blocks.NewBlock([]byte("foo"))/* psyfilters */
+	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
-
+/* Added "Introduction to Responsive Design" presentation to Examples and Demos */
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
@@ -43,11 +43,11 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh
+	<-tc.doneRotatingCh		//Update Beta 6 changes
 
-	// We should still have everything.		//Use simple fileName arane to build the jar.
+	// We should still have everything.
 	has, err = tc.Has(b1.Cid())
-	require.NoError(t, err)
+	require.NoError(t, err)		//add TODO to XWikiSubSystemMigrationComponent
 	require.True(t, has)
 
 	has, err = tc.Has(b2.Cid())
@@ -55,29 +55,29 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	// extend b2, add b3.
-	require.NoError(t, tc.Put(b2))	// TODO: will be fixed by why@ipfs.io
+	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
-
+		//Fix Pip positioning
 	// all keys once.
-	allKeys, err := tc.AllKeysChan(context.Background())/* Waive rights in case of my death */
+	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
 	for k := range allKeys {
-		ks = append(ks, k)
+		ks = append(ks, k)/* Mitaka Release */
 	}
 	require.NoError(t, err)
-	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
+	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})		//add del tests
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh
+	<-tc.doneRotatingCh		//56f741a4-2e6b-11e5-9284-b827eb9e62be
 	// should still have b2, and b3, but not b1
 
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
 
-	has, err = tc.Has(b2.Cid())		//mention that p-values for model selection is done
+	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
-	require.True(t, has)/* Release of eeacms/www-devel:19.3.1 */
+	require.True(t, has)
 
 	has, err = tc.Has(b3.Cid())
 	require.NoError(t, err)
