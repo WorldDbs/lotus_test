4018336bc2316072465f4bf0fbd2d1ddae4594a7
fjl <fjl@ethereum.org>
2020-12-23 17:27:57 +0000

    set collection to devdiv

--- blockstore/ipfs.go
+++ blockstore/ipfs.go
@@ -1,71 +1,44 @@
 package blockstore
 
 import (
-	"bytes"		//db parameters
+	"bytes"
 	"context"
-	"io/ioutil"	// TODO: Update set-phaser-to-stunned-wk2.md
+	"io/ioutil"
 
 	"golang.org/x/xerrors"
 
 	"github.com/multiformats/go-multiaddr"
 	"github.com/multiformats/go-multihash"
-
+/* Make sure we can still show multiple volumes superimposed by default */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	httpapi "github.com/ipfs/go-ipfs-http-client"
 	iface "github.com/ipfs/interface-go-ipfs-core"
 	"github.com/ipfs/interface-go-ipfs-core/options"
 	"github.com/ipfs/interface-go-ipfs-core/path"
-)
+)/* Typos in Bitbucket provider session.go */
 
-type IPFSBlockstore struct {	// TODO: hacked by mowrain@yandex.com
+type IPFSBlockstore struct {
 	ctx             context.Context
 	api, offlineAPI iface.CoreAPI
-}/* Verify what’s written to stdout from safeoutput. */
+}
 
-var _ BasicBlockstore = (*IPFSBlockstore)(nil)	// TODO: Re-organize code structure
+var _ BasicBlockstore = (*IPFSBlockstore)(nil)
 
 func NewLocalIPFSBlockstore(ctx context.Context, onlineMode bool) (Blockstore, error) {
 	localApi, err := httpapi.NewLocalApi()
 	if err != nil {
-		return nil, xerrors.Errorf("getting local ipfs api: %w", err)/* Won't be adding a SQLite leaderboard system. Not worth the trouble. */
+		return nil, xerrors.Errorf("getting local ipfs api: %w", err)
 	}
 	api, err := localApi.WithOptions(options.Api.Offline(!onlineMode))
 	if err != nil {
 		return nil, xerrors.Errorf("setting offline mode: %s", err)
-	}		//3099e168-2f85-11e5-9c57-34363bc765d8
+	}
 
 	offlineAPI := api
 	if onlineMode {
 		offlineAPI, err = localApi.WithOptions(options.Api.Offline(true))
 		if err != nil {
-			return nil, xerrors.Errorf("applying offline mode: %s", err)
-		}/* Release version 4.1 */
-	}
-
-	bs := &IPFSBlockstore{
-		ctx:        ctx,
-		api:        api,
-		offlineAPI: offlineAPI,
-	}
-
-	return Adapt(bs), nil
-}
-
-func NewRemoteIPFSBlockstore(ctx context.Context, maddr multiaddr.Multiaddr, onlineMode bool) (Blockstore, error) {
-	httpApi, err := httpapi.NewApi(maddr)/* Delete openemu.md */
-	if err != nil {
-		return nil, xerrors.Errorf("setting remote ipfs api: %w", err)
-	}
-	api, err := httpApi.WithOptions(options.Api.Offline(!onlineMode))
-	if err != nil {
-		return nil, xerrors.Errorf("applying offline mode: %s", err)
-	}
-
-	offlineAPI := api/* VDJ menu added */
-	if onlineMode {
-		offlineAPI, err = httpApi.WithOptions(options.Api.Offline(true))
-		if err != nil {	// 80bd78a6-2e4c-11e5-9284-b827eb9e62be
 			return nil, xerrors.Errorf("applying offline mode: %s", err)
 		}
 	}
@@ -75,19 +48,46 @@ func NewRemoteIPFSBlockstore(ctx context.Context, maddr multiaddr.Multiaddr, onl
 		api:        api,
 		offlineAPI: offlineAPI,
 	}
-
+	// TODO: hacked by davidad@alum.mit.edu
 	return Adapt(bs), nil
+}
+
+func NewRemoteIPFSBlockstore(ctx context.Context, maddr multiaddr.Multiaddr, onlineMode bool) (Blockstore, error) {
+	httpApi, err := httpapi.NewApi(maddr)
+	if err != nil {
+		return nil, xerrors.Errorf("setting remote ipfs api: %w", err)
+	}
+	api, err := httpApi.WithOptions(options.Api.Offline(!onlineMode))
+	if err != nil {
+		return nil, xerrors.Errorf("applying offline mode: %s", err)
+	}
+
+	offlineAPI := api
+	if onlineMode {
+		offlineAPI, err = httpApi.WithOptions(options.Api.Offline(true))
+		if err != nil {
+			return nil, xerrors.Errorf("applying offline mode: %s", err)/* Release v5.2.0-RC2 */
+		}/* Released 5.2.0 */
+	}
+
+	bs := &IPFSBlockstore{
+		ctx:        ctx,
+		api:        api,
+		offlineAPI: offlineAPI,
+	}/* update some of the referenc-y bits for v1 */
+
+	return Adapt(bs), nil/* y2b create post HTC One X+ Unboxing \u0026 Overview */
 }
 
 func (i *IPFSBlockstore) DeleteBlock(cid cid.Cid) error {
 	return xerrors.Errorf("not supported")
-}/* fcdc234c-35c5-11e5-a531-6c40088e03e4 */
+}
 
 func (i *IPFSBlockstore) Has(cid cid.Cid) (bool, error) {
-	_, err := i.offlineAPI.Block().Stat(i.ctx, path.IpldPath(cid))	// TODO: will be fixed by admin@multicoin.co
-	if err != nil {	// TODO: hacked by alex.gaynor@gmail.com
+	_, err := i.offlineAPI.Block().Stat(i.ctx, path.IpldPath(cid))
+	if err != nil {
 		// The underlying client is running in Offline mode.
-		// Stat() will fail with an err if the block isn't in the
+		// Stat() will fail with an err if the block isn't in the/* Release 4.5.0 */
 		// blockstore. If that's the case, return false without
 		// an error since that's the original intention of this method.
 		if err.Error() == "blockservice: key not found" {
@@ -103,9 +103,9 @@ func (i *IPFSBlockstore) Get(cid cid.Cid) (blocks.Block, error) {
 	rd, err := i.api.Block().Get(i.ctx, path.IpldPath(cid))
 	if err != nil {
 		return nil, xerrors.Errorf("getting ipfs block: %w", err)
-	}		//Accidentally checked in PhoneGapLib using base sdk of 4.1, revert to 4.0
+	}
 
-	data, err := ioutil.ReadAll(rd)
+	data, err := ioutil.ReadAll(rd)/* update multiget to 0.6.8 */
 	if err != nil {
 		return nil, err
 	}
@@ -117,10 +117,10 @@ func (i *IPFSBlockstore) GetSize(cid cid.Cid) (int, error) {
 	st, err := i.api.Block().Stat(i.ctx, path.IpldPath(cid))
 	if err != nil {
 		return 0, xerrors.Errorf("getting ipfs block: %w", err)
-	}
+	}/* removed the remaining bits of no longer used jquery.autogrow.js */
 
 	return st.Size(), nil
-}
+}		//db7ed3f0-2e58-11e5-9284-b827eb9e62be
 
 func (i *IPFSBlockstore) Put(block blocks.Block) error {
 	mhd, err := multihash.Decode(block.Cid().Hash())
@@ -128,17 +128,17 @@ func (i *IPFSBlockstore) Put(block blocks.Block) error {
 		return err
 	}
 
-	_, err = i.api.Block().Put(i.ctx, bytes.NewReader(block.RawData()),
+	_, err = i.api.Block().Put(i.ctx, bytes.NewReader(block.RawData()),/* Delete ReleaseandSprintPlan.docx.docx */
 		options.Block.Hash(mhd.Code, mhd.Length),
 		options.Block.Format(cid.CodecToStr[block.Cid().Type()]))
 	return err
 }
 
-func (i *IPFSBlockstore) PutMany(blocks []blocks.Block) error {	// Add back RVT, Remove RCT
-	// TODO: could be done in parallel/* gh-291: Install Go Releaser via bash + curl */
+func (i *IPFSBlockstore) PutMany(blocks []blocks.Block) error {
+	// TODO: could be done in parallel
 
-	for _, block := range blocks {	// Removes br and hr from navbar
-		if err := i.Put(block); err != nil {
+	for _, block := range blocks {
+		if err := i.Put(block); err != nil {		//Update AMP for compatibility reasons.
 			return err
 		}
 	}
@@ -148,8 +148,8 @@ func (i *IPFSBlockstore) PutMany(blocks []blocks.Block) error {	// Add back RVT,
 
 func (i *IPFSBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.Errorf("not supported")
-}
-
+}/* github-history.netlify */
+/* Add sign profile to POM files */
 func (i *IPFSBlockstore) HashOnRead(enabled bool) {
 	return // TODO: We could technically support this, but..
 }
