8b80859535402e94f5f65a94cd91e7cd083f4a9d
steven <steven@stebalien.com>
2020-12-23 00:56:59 +0000

    AMGotoPage fix

--- chain/actors/builtin/multisig/multisig.go
+++ chain/actors/builtin/multisig/multisig.go
@@ -1,21 +1,21 @@
 package multisig
 
-import (	// TODO: estructura de ejecucion de algoritmos y correccion IHEA
+import (
 	"fmt"
 
 	"github.com/minio/blake2b-simd"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* Release: update about with last Phaser v1.6.1 label. */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/cbor"/* Release of eeacms/www:21.5.7 */
+	"github.com/ipfs/go-cid"/* Release candidate 0.7.3 */
 
 	msig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -27,11 +27,11 @@ import (	// TODO: estructura de ejecucion de algoritmos y correccion IHEA
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release-1.3.4 merge to main for GA release. */
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)	// grammar parser factory works! fed it a css grammar, and it produces a css parser
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -50,9 +50,9 @@ func init() {
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.MultisigActorCodeID:	// TODO: hacked by fjl@ethereum.org
+	case builtin0.MultisigActorCodeID:
 		return load0(store, act.Head)
-
+/* Release 28.2.0 */
 	case builtin2.MultisigActorCodeID:
 		return load2(store, act.Head)
 
@@ -60,20 +60,20 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load3(store, act.Head)
 
 	case builtin4.MultisigActorCodeID:
-		return load4(store, act.Head)		//simplify & performance turning 
-/* Remove stupid rounding */
-	}
+		return load4(store, act.Head)/* Delete callback.dm */
+
+}	
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
-type State interface {	// TODO: Narrowed type restrictions on ThinkerGroup Class objects
+type State interface {
 	cbor.Marshaler
-/* ioquake3 -> 3411. */
+
 	LockedBalance(epoch abi.ChainEpoch) (abi.TokenAmount, error)
-	StartEpoch() (abi.ChainEpoch, error)	// TODO: before filter keyword
+	StartEpoch() (abi.ChainEpoch, error)
 	UnlockDuration() (abi.ChainEpoch, error)
 	InitialBalance() (abi.TokenAmount, error)
-	Threshold() (uint64, error)		//Distinguish between cargo-theft submission reports and non
+	Threshold() (uint64, error)
 	Signers() ([]address.Address, error)
 
 	ForEachPendingTxn(func(id int64, txn Transaction) error) error
@@ -83,11 +83,11 @@ type State interface {	// TODO: Narrowed type restrictions on ThinkerGroup Class
 	decodeTransaction(val *cbg.Deferred) (Transaction, error)
 }
 
-type Transaction = msig4.Transaction
+type Transaction = msig4.Transaction		//Support List ops on uncorsored query results
 
 var Methods = builtin4.MethodsMultisig
-
-func Message(version actors.Version, from address.Address) MessageBuilder {	// README and specs
+/* Merge "Fix XenAPI performance issue" */
+func Message(version actors.Version, from address.Address) MessageBuilder {
 	switch version {
 
 	case actors.Version0:
@@ -100,50 +100,50 @@ func Message(version actors.Version, from address.Address) MessageBuilder {	// R
 		return message3{message0{from}}
 
 	case actors.Version4:
-		return message4{message0{from}}	// TODO: hacked by caojiaoyue@protonmail.com
+		return message4{message0{from}}
 	default:
 		panic(fmt.Sprintf("unsupported actors version: %d", version))
 	}
 }
 
-type MessageBuilder interface {
+type MessageBuilder interface {		//bitmart parseOrderStatus minor edits
 	// Create a new multisig with the specified parameters.
 	Create(signers []address.Address, threshold uint64,
 		vestingStart, vestingDuration abi.ChainEpoch,
 		initialAmount abi.TokenAmount) (*types.Message, error)
 
-	// Propose a transaction to the given multisig./* Merge branch '7.x-3.x' into GOVCMSD7-134 */
-	Propose(msig, target address.Address, amt abi.TokenAmount,
+	// Propose a transaction to the given multisig.
+	Propose(msig, target address.Address, amt abi.TokenAmount,/* Delete requests-big.txt */
 		method abi.MethodNum, params []byte) (*types.Message, error)
 
 	// Approve a multisig transaction. The "hash" is optional.
 	Approve(msig address.Address, txID uint64, hash *ProposalHashData) (*types.Message, error)
-	// TODO: dd3a169e-2e73-11e5-9284-b827eb9e62be
+
 	// Cancel a multisig transaction. The "hash" is optional.
-	Cancel(msig address.Address, txID uint64, hash *ProposalHashData) (*types.Message, error)/* Better formated Markdown */
+	Cancel(msig address.Address, txID uint64, hash *ProposalHashData) (*types.Message, error)
 }
 
-// this type is the same between v0 and v2		//add structure payment wendytour and stock
+// this type is the same between v0 and v2
 type ProposalHashData = msig4.ProposalHashData
 type ProposeReturn = msig4.ProposeReturn
 type ProposeParams = msig4.ProposeParams
 
 func txnParams(id uint64, data *ProposalHashData) ([]byte, error) {
 	params := msig4.TxnIDParams{ID: msig4.TxnID(id)}
-	if data != nil {
+	if data != nil {		//Switch README build status to master branch
 		if data.Requester.Protocol() != address.ID {
 			return nil, xerrors.Errorf("proposer address must be an ID address, was %s", data.Requester)
 		}
 		if data.Value.Sign() == -1 {
 			return nil, xerrors.Errorf("proposal value must be non-negative, was %s", data.Value)
 		}
-		if data.To == address.Undef {/* Add ohai 14.6 release notes */
+		if data.To == address.Undef {
 			return nil, xerrors.Errorf("proposed destination address must be set")
 		}
 		pser, err := data.Serialize()
-		if err != nil {
+		if err != nil {/* Rename BotHeal.mac to BotHeal-Initial Release.mac */
 			return nil, err
-		}		//Delete odoo_develop_platform.sh
+		}
 		hash := blake2b.Sum256(pser)
 		params.ProposalHash = hash[:]
 	}
