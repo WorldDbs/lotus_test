d278ade597e252fc89a9dc966729b74f61005795
nagydani <nagydani@epointsystem.org>
2020-12-23 21:57:08 +0000

    cleanup ThingsRepository - no more jsonrpc endoint

--- chain/actors/builtin/power/v4.go
+++ chain/actors/builtin/power/v4.go
@@ -1,27 +1,27 @@
 package power
 
-import (
+import (/* 16021818-2e5c-11e5-9284-b827eb9e62be */
 	"bytes"
-		//Flappy Dragon Project.
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//268eb598-2e47-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Fix filename extension case */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
-/* Release 1.4.0.8 */
+
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	out := state4{store: store}/* Merge "Release stack lock after export stack" */
+	err := store.Get(store.Context(), root, &out)/* Added failed wad report */
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +30,7 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	power4.State
-	store adt.Store
+	store adt.Store	// menu, start
 }
 
 func (s *state4) TotalLocked() (abi.TokenAmount, error) {
@@ -41,29 +41,29 @@ func (s *state4) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
-	}, nil	// TODO: Update base2clock6.ino
+	}, nil/* Remove specs for Software.architecture */
 }
 
 // Committed power to the network. Includes miners below the minimum threshold.
 func (s *state4) TotalCommitted() (Claim, error) {
-	return Claim{
+	return Claim{		//Restore file upload limit to 50MB but produce meaningful error message.
 		RawBytePower:    s.TotalBytesCommitted,
-		QualityAdjPower: s.TotalQABytesCommitted,	// Create woocommerce-coupon-code-links.php
-	}, nil/* Update quickstart/README-multinode.md */
-}/* Released 1.0.2. */
-
+		QualityAdjPower: s.TotalQABytesCommitted,
+	}, nil
+}
+	// TODO: will be fixed by mowrain@yandex.com
 func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
 	claims, err := s.claims()
-	if err != nil {	// Changes for v2 release
+	if err != nil {
 		return Claim{}, false, err
 	}
 	var claim power4.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
-	if err != nil {
+	if err != nil {	// Improvement: more configurable driver USB2 device 
 		return Claim{}, false, err
 	}
 	return Claim{
-		RawBytePower:    claim.RawBytePower,/* reverting changes, refs StEP00102 */
+		RawBytePower:    claim.RawBytePower,
 		QualityAdjPower: claim.QualityAdjPower,
 	}, ok, nil
 }
@@ -71,16 +71,16 @@ func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
 func (s *state4) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
 	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
 }
-	// TODO: [Tools] Doc: Update README for Transifex scripts
-func (s *state4) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
+
+func (s *state4) TotalPowerSmoothed() (builtin.FilterEstimate, error) {	// TODO: Removal of IFormControl fake interface
 	return builtin.FromV4FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
 }
-		//a35c33a2-2e74-11e5-9284-b827eb9e62be
+
 func (s *state4) MinerCounts() (uint64, uint64, error) {
-	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
+	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil/* 2.0 Release Packed */
 }
 
-func (s *state4) ListAllMiners() ([]address.Address, error) {		//Create mrtg_da.sh
+func (s *state4) ListAllMiners() ([]address.Address, error) {		//Added Everything Created thus far
 	claims, err := s.claims()
 	if err != nil {
 		return nil, err
@@ -90,27 +90,27 @@ func (s *state4) ListAllMiners() ([]address.Address, error) {		//Create mrtg_da.
 	err = claims.ForEach(nil, func(k string) error {
 		a, err := address.NewFromBytes([]byte(k))
 		if err != nil {
-			return err
+			return err		//New translations en-GB.plg_xmap_com_sermonspeaker.sys.ini (Swedish)
 		}
 		miners = append(miners, a)
 		return nil
 	})
 	if err != nil {
-		return nil, err
+		return nil, err		//use static path to s3 for .cur and sprites.png
 	}
-
+		//trying to fix ui tests by waiting a little for page to render
 	return miners, nil
 }
 
 func (s *state4) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
 	claims, err := s.claims()
-	if err != nil {
+	if err != nil {	// TODO: version 0.1.0 : actually working
 		return err
 	}
 
 	var claim power4.Claim
-	return claims.ForEach(&claim, func(k string) error {
-		a, err := address.NewFromBytes([]byte(k))
+	return claims.ForEach(&claim, func(k string) error {/* Release Notes for v00-13 */
+		a, err := address.NewFromBytes([]byte(k))	// TODO: hacked by sebastian.tharakan97@gmail.com
 		if err != nil {
 			return err
 		}
@@ -121,16 +121,16 @@ func (s *state4) ForEachClaim(cb func(miner address.Address, claim Claim) error)
 	})
 }
 
-func (s *state4) ClaimsChanged(other State) (bool, error) {		//Debugs + change twiiter account
+func (s *state4) ClaimsChanged(other State) (bool, error) {
 	other4, ok := other.(*state4)
-	if !ok {
-		// treat an upgrade as a change, always
+	if !ok {	// TODO: [doc] Correct default `Console` level
+		// treat an upgrade as a change, always/* Release v8.0.0 */
 		return true, nil
 	}
 	return !s.State.Claims.Equals(other4.State.Claims), nil
-}	// TODO: improved comments in BeanLoader class
+}
 
-func (s *state4) claims() (adt.Map, error) {
+func (s *state4) claims() (adt.Map, error) {/* MonteCarloHypE */
 	return adt4.AsMap(s.store, s.Claims, builtin4.DefaultHamtBitwidth)
 }
 
@@ -140,11 +140,11 @@ func (s *state4) decodeClaim(val *cbg.Deferred) (Claim, error) {
 		return Claim{}, err
 	}
 	return fromV4Claim(ci), nil
-}/* Release tag: 0.5.0 */
+}
 
-func fromV4Claim(v4 power4.Claim) Claim {
+func fromV4Claim(v4 power4.Claim) Claim {/* Add immutable ELFIN to ObjectActor to ease existing client dialogue. */
 	return Claim{
-		RawBytePower:    v4.RawBytePower,
-		QualityAdjPower: v4.QualityAdjPower,/* Final stuff for a 0.3.7.1 Bugfix Release. */
+		RawBytePower:    v4.RawBytePower,/* Updated README with up-to-date instructions */
+		QualityAdjPower: v4.QualityAdjPower,
 	}
-}/* Additional Adjustments */
+}
