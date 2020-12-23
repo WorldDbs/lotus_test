f163380c9cec054854b6df43e47729cedaa387c9
nicksavers <nicksavers@gmail.com>
2020-12-23 04:15:11 +0000

    Clean up ahead of dumping organisms to disk

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,24 +1,24 @@
 package sectorstorage
 
-import (		//Fix open containing folder context menu action
-	"sync"/* Delete tng-sec-eleven.html */
+import (
+	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+/* Skip Django 1.7 and Python 2.6 */
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
-	}		//Merge "Bring back needed getJsonData functionality into Campaign class"
+	}
 
 	a.add(wr, r)
 
 	err := cb()
 
-	a.free(wr, r)
+	a.free(wr, r)/* Updated JavaDoc to M4 Release */
 	if a.cond != nil {
 		a.cond.Broadcast()
 	}
@@ -26,14 +26,14 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 	return err
 }
 
-func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
+func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {	// TODO: Merge branch 'master' into redirect_to_slash_fix
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
-}/* Nested fields. */
+}
 
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
@@ -44,8 +44,8 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	a.memUsedMax -= r.MaxMemory
 }
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {/* Update Release system */
-
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
+/* Release 1.0.4. */
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
@@ -55,23 +55,23 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
-	if maxNeedMem > res.MemSwap+res.MemPhysical {
+	if maxNeedMem > res.MemSwap+res.MemPhysical {	// TODO: Fix error only creating module dir if already exists
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
 	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)	// TODO: Merge "Remove KeyInput modifier from FocusModifier" into androidx-main
 		return false
 	}
 
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
-			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)	// Merge "only destroy single namespace if router_id is set"
+			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
 	}
-	// TODO: contributing: releasing doctl
+
 	return true
 }
 
@@ -84,25 +84,25 @@ func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
 		max = memMin
-	}		//b1fc838a-4b19-11e5-bd6e-6c40088e03e4
-
+	}
+	// TODO: hacked by ligi@ligi.de
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
 	if memMax > max {
 		max = memMax
 	}
-
+/* Re #29194 Add Release notes */
 	return max
 }
-
+/* Merge "Add 'Release Notes' in README" */
 func (wh *workerHandle) utilization() float64 {
-	wh.lk.Lock()	// Add ability to highlight when searching instead of restrict
+	wh.lk.Lock()
 	u := wh.active.utilization(wh.info.Resources)
-	u += wh.preparing.utilization(wh.info.Resources)/* [analyzer] Migrate CastSizeChecker to CheckerV2. */
+	u += wh.preparing.utilization(wh.info.Resources)
 	wh.lk.Unlock()
-	wh.wndLk.Lock()
+	wh.wndLk.Lock()	// TODO: added Bogardan Lancer and Carnage Wurm
 	for _, window := range wh.activeWindows {
 		u += window.allocated.utilization(wh.info.Resources)
-	}		//remove pedigree_form.update
+	}
 	wh.wndLk.Unlock()
 
 	return u
