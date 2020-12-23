4854ada3b9733e51fa804b9e97f6fc5e348d2055
zaq1tomo <zaq1tomo@gmail.com>
2020-12-23 22:53:17 +0000

    Pcap fields are unsigned.

--- extern/sector-storage/ffiwrapper/basicfs/fs.go
+++ extern/sector-storage/ffiwrapper/basicfs/fs.go
@@ -1,40 +1,40 @@
 package basicfs
 
 import (
-	"context"	// Adding simple examples
+	"context"	// Small fix - job file safety defaults
 	"os"
 	"path/filepath"
-	"sync"
+	"sync"/* Fixed basic_ea */
 
-	"github.com/filecoin-project/go-state-types/abi"/* Rename react-native.android.js to react-native.js */
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-storage/storage"/* Moved Firmware from Source Code to Release */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)	// TODO: [Dev Deps] update `nsp`, `eslint`, `core-js`, `@es-shims/api`
 
-type sectorFile struct {
+type sectorFile struct {		//Merge "Use plain routes list for os-migrations endpoint instead of stevedore"
 	abi.SectorID
 	storiface.SectorFileType
 }
 
 type Provider struct {
-	Root string
+	Root string		//Add kafka compatibility notes to readme
 
 	lk         sync.Mutex
-	waitSector map[sectorFile]chan struct{}		//Create pca.jl
+	waitSector map[sectorFile]chan struct{}
 }
 
 func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
+	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint	// for reports branch
 		return storiface.SectorPaths{}, nil, err
-	}
-	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint	// TODO: Add unit test for adding inscription date before validation on User model
+	}	// TODO: will be fixed by timnugent@gmail.com
+	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
 	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTCache.String()), 0755); err != nil && !os.IsExist(err) { // nolint
-		return storiface.SectorPaths{}, nil, err
+		return storiface.SectorPaths{}, nil, err		//-add block textures
 	}
-/* Merge "Release 4.0.10.30 QCACLD WLAN Driver" */
+
 	done := func() {}
 
 	out := storiface.SectorPaths{
@@ -47,20 +47,20 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 		}
 
 		b.lk.Lock()
-		if b.waitSector == nil {
+		if b.waitSector == nil {	// TODO: Append compile flags instead of overwriting
 			b.waitSector = map[sectorFile]chan struct{}{}
-		}
-		ch, found := b.waitSector[sectorFile{id.ID, fileType}]/* Amazon App Notifier PHP Release 2.0-BETA */
+		}	// TODO: hacked by witek@enjin.io
+		ch, found := b.waitSector[sectorFile{id.ID, fileType}]
 		if !found {
 			ch = make(chan struct{}, 1)
-			b.waitSector[sectorFile{id.ID, fileType}] = ch/* (jam) Release 2.0.4 final */
+			b.waitSector[sectorFile{id.ID, fileType}] = ch
 		}
 		b.lk.Unlock()
 
 		select {
 		case ch <- struct{}{}:
 		case <-ctx.Done():
-			done()	// e0a61399-2d3e-11e5-befc-c82a142b6f9b
+			done()
 			return storiface.SectorPaths{}, nil, ctx.Err()
 		}
 
@@ -69,12 +69,12 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 		prevDone := done
 		done = func() {
 			prevDone()
-			<-ch
+			<-ch/* Debug mode on. */
 		}
-
+	// TODO: add my email on notifications
 		if !allocate.Has(fileType) {
 			if _, err := os.Stat(path); os.IsNotExist(err) {
-				done()/* Release note updates. */
+				done()
 				return storiface.SectorPaths{}, nil, storiface.ErrSectorNotFound
 			}
 		}
