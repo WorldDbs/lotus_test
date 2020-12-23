8999b95e82cc6b07b5a0fca002e951b22ff45815
sbrichards <sbrichards@gmail.com>
2020-12-23 08:51:38 +0000

    Changed homepage in the license file.

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,15 +1,15 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"/* 78131cf0-2e57-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"	// PWM works!!!
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* CSI DoubleRelease. Fixed */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -20,9 +20,9 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init2.ExecParams{
+	enc, aerr := actors.SerializeParams(&init2.ExecParams{/* Test setup */
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
-		ConstructorParams: params,		//Create String Functions.md
+		ConstructorParams: params,
 	})
 	if aerr != nil {
 		return nil, aerr
@@ -37,9 +37,9 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}, nil
 }
 
-func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {	// Merge branch 'develop' into feature/BOLDmask
+func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
-		Sv:     *sv,		//M8Ee0YJP1jguqQGORBN1ezy3dtr4Ub1I
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
@@ -50,25 +50,25 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.UpdateChannelState,	// TODO: hacked by hugomrdias@gmail.com
+		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
-	}, nil/* Release 0.4.12. */
+	}, nil
 }
-
-func (m message2) Settle(paych address.Address) (*types.Message, error) {
+	// - Fixed 'instance_check_party' (bugreport:5948)
+func (m message2) Settle(paych address.Address) (*types.Message, error) {/* Release v1.2.7 */
 	return &types.Message{
 		To:     paych,
-		From:   m.from,
+		From:   m.from,		//Added state machine
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Settle,
 	}, nil
 }
 
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
-	return &types.Message{
+	return &types.Message{		//corrected validate() method issue with handling Object[]
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* Support for Releases */
-		Method: builtin2.MethodsPaych.Collect,	// TODO: rev 691952
+		Value:  abi.NewTokenAmount(0),
+		Method: builtin2.MethodsPaych.Collect,
 	}, nil
 }
