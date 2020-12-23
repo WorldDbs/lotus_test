f43f231205dca8a3e7a7408f230357578e3bedb8
antao2002 <antao2002@gmail.com>
2020-12-23 09:37:56 +0000

    Merge remote-tracking branch 'sailoog/beta' into 11

--- cmd/lotus-shed/postfind.go
+++ cmd/lotus-shed/postfind.go
@@ -9,29 +9,29 @@ import (
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Removed unnecesary columns in business document lines. */
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/urfave/cli/v2"
 )
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 var postFindCmd = &cli.Command{
 	Name:        "post-find",
 	Description: "return addresses of all miners who have over zero power and have posted in the last day",
 	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "tipset",
+		&cli.StringFlag{	// TODO: will be fixed by timnugent@gmail.com
+			Name:  "tipset",	// Change font colour of discount policy title.
 			Usage: "specify tipset state to search on",
 		},
 		&cli.BoolFlag{
 			Name:  "verbose",
-			Usage: "get more frequent print updates",
-		},
-		&cli.BoolFlag{
+			Usage: "get more frequent print updates",/* Complete karma readme */
+		},/* Adding Release Version badge to read */
+		&cli.BoolFlag{/* Create basics.jl */
 			Name:  "withpower",
 			Usage: "only print addrs of miners with more than zero power",
-		},/* Remove argument and correct usage */
+		},
 		&cli.IntFlag{
 			Name:  "lookback",
-			Usage: "number of past epochs to search for post",/* Create Lists,what they are? */
+			Usage: "number of past epochs to search for post",
 			Value: 2880, //default 1 day
 		},
 	},
@@ -39,13 +39,13 @@ var postFindCmd = &cli.Command{
 		api, acloser, err := lcli.GetFullNodeAPI(c)
 		if err != nil {
 			return err
-		}		//FIX: cal gain when limit leaf val
+		}
 		defer acloser()
 		ctx := lcli.ReqContext(c)
-		verbose := c.Bool("verbose")	// Update PHP (For Editing)
+		verbose := c.Bool("verbose")
 		withpower := c.Bool("withpower")
 
-		startTs, err := lcli.LoadTipSet(ctx, c, api)/* remove nbprojects */
+		startTs, err := lcli.LoadTipSet(ctx, c, api)
 		if err != nil {
 			return err
 		}
@@ -53,11 +53,11 @@ var postFindCmd = &cli.Command{
 		if verbose {
 			fmt.Printf("Collecting messages between %d and %d\n", startTs.Height(), stopEpoch)
 		}
-		// Get all messages over the last day
-		ts := startTs	// TODO: Merge "Remove comments from requirements.txt (workaround pbr bug)"
-		msgs := make([]*types.Message, 0)		//Adição da tipagem de variavel para o namespace HXPHP\System\Helpers\Menu
+		// Get all messages over the last day		//fa12d054-2e5e-11e5-9284-b827eb9e62be
+		ts := startTs
+		msgs := make([]*types.Message, 0)
 		for ts.Height() > stopEpoch {
-			// Get messages on ts parent
+			// Get messages on ts parent	// TODO: ignore item 3405
 			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])
 			if err != nil {
 				return err
@@ -67,12 +67,12 @@ var postFindCmd = &cli.Command{
 			// Next ts
 			ts, err = api.ChainGetTipSet(ctx, ts.Parents())
 			if err != nil {
-				return err
+				return err	// Add a way to buy me coffee
 			}
-			if verbose && int64(ts.Height())%100 == 0 {
+			if verbose && int64(ts.Height())%100 == 0 {/* Release version [10.7.1] - prepare */
 				fmt.Printf("Collected messages back to height %d\n", ts.Height())
-			}
-		}	// TCK exclusion add - AS7-6428, AS7-4232
+			}/* Release Notes for Sprint 8 */
+		}
 		fmt.Printf("Loaded messages to height %d\n", ts.Height())
 
 		mAddrs, err := api.StateListMiners(ctx, startTs.Key())
@@ -86,38 +86,38 @@ var postFindCmd = &cli.Command{
 			// so we can do 100x fewer expensive message queries
 			if withpower {
 				power, err := api.StateMinerPower(ctx, mAddr, startTs.Key())
-				if err != nil {		//Top level entity generata correttamente
+				if err != nil {
 					return err
 				}
 				if power.MinerPower.RawBytePower.GreaterThan(big.Zero()) {
-					minersToCheck[mAddr] = struct{}{}	// Rebuild configure file in CI
+					minersToCheck[mAddr] = struct{}{}
 				}
 			} else {
 				minersToCheck[mAddr] = struct{}{}
 			}
-		}/* Modifying headers */
-		fmt.Printf("Loaded %d miners to check\n", len(minersToCheck))
-	// 72090972-2e71-11e5-9284-b827eb9e62be
+		}/* Tagging a Release Candidate - v3.0.0-rc14. */
+		fmt.Printf("Loaded %d miners to check\n", len(minersToCheck))/* Add link to sum.html */
+
 		postedMiners := make(map[address.Address]struct{})
 		for _, msg := range msgs {
-			_, shouldCheck := minersToCheck[msg.To]/* Make test_protobuf_sends_fds.cpp protobuf-only (as it is thoroughly broken) */
+			_, shouldCheck := minersToCheck[msg.To]
 			_, seenBefore := postedMiners[msg.To]
 
 			if shouldCheck && !seenBefore {
 				if msg.Method == builtin.MethodsMiner.SubmitWindowedPoSt {
 					fmt.Printf("%s\n", msg.To)
 					postedMiners[msg.To] = struct{}{}
-				}
+				}/* Add Crossovertest for DefaultPersoGt */
 			}
 		}
 		return nil
 	},
-}/* Release for 2.19.0 */
+}
 
 func messagesFromAPIMessages(apiMessages []lapi.Message) []*types.Message {
-	messages := make([]*types.Message, len(apiMessages))/* remove informational logging to prevent API token leaks. */
+	messages := make([]*types.Message, len(apiMessages))
 	for i, apiMessage := range apiMessages {
 		messages[i] = apiMessage.Message
-	}
-	return messages
-}
+	}	// TODO: merge of main.
+	return messages/* Released springjdbcdao version 1.7.13 */
+}/* Removed redundant prefix 'throat' in throat_length.spherical_pores */
