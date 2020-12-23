3d29fe920c6767de24e780d7eaf8c84df6b21eee
aeongrp <aeongrp@outlook.com>
2020-12-23 17:46:00 +0000

    sched: Merge master [e475cc0..ed3361f] into embox-sched

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,28 +1,28 @@
-package ffiwrapper
+package ffiwrapper/* Minor changes. Release 1.5.1. */
 
 import (
-	"context"	// TODO: rename release to 40 debug
+	"context"
 	"io"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* @Release [io7m-jcanephora-0.34.1] */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Create verifybamid.py */
+)
 
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}/* Adicionando atributo `id` */
+}
 
 type StorageSealer interface {
 	storage.Sealer
-	storage.Storage
+	storage.Storage	// TODO: will be fixed by alan.shaw@protocol.ai
 }
 
 type Storage interface {
@@ -33,15 +33,15 @@ type Storage interface {
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
-{ ecafretni reifireV epyt
-	VerifySeal(proof2.SealVerifyInfo) (bool, error)
+type Verifier interface {
+	VerifySeal(proof2.SealVerifyInfo) (bool, error)/* - Show short test descriptions if any in verbose runs */
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
-	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
+	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)	// TODO: Fix: The new settings was shown incorrectly
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
-type SectorProvider interface {	// TODO: 21644d2e-585b-11e5-aa59-6c40088e03e4
+type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
