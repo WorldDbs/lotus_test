2ccf124666dac2565d9af482197dff978eb11c14
onhardev <onhardev@bk.ru>
2020-12-23 09:03:31 +0000

    general terms and conditions for retailers

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,13 +1,13 @@
 package sectorstorage
 
-import (
+import (	// TODO: Registration codes for signup
 	"time"
 
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+		//stop timing when it finished
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	m.sched.workersLk.RLock()
 	defer m.sched.workersLk.RUnlock()
@@ -20,7 +20,7 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 			Enabled: handle.enabled,
 
 			MemUsedMin: handle.active.memUsedMin,
-			MemUsedMax: handle.active.memUsedMax,
+			MemUsedMax: handle.active.memUsedMax,/* Release ivars. */
 			GpuUsed:    handle.active.gpuUsed,
 			CpuUse:     handle.active.cpuUse,
 		}
@@ -33,10 +33,10 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	out := map[uuid.UUID][]storiface.WorkerJob{}
 	calls := map[storiface.CallID]struct{}{}
 
-	for _, t := range m.sched.workTracker.Running() {
+	for _, t := range m.sched.workTracker.Running() {	// TODO: hacked by arachnid@notdot.net
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
-		calls[t.job.ID] = struct{}{}
-	}
+		calls[t.job.ID] = struct{}{}	// Create More Formal Wording
+}	
 
 	m.sched.workersLk.RLock()
 
@@ -51,14 +51,14 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 					RunWait: wi + 1,
 					Start:   request.start,
 				})
-			}
-		}
+			}	// added binary filesize reporting to json
+		}	// TODO: Added callback example to Readme
 		handle.wndLk.Unlock()
 	}
 
 	m.sched.workersLk.RUnlock()
 
-	m.workLk.Lock()
+	m.workLk.Lock()	// TODO: Changed queue consumer HazelcastInstanceNotActiveException catching.
 	defer m.workLk.Unlock()
 
 	for id, work := range m.callToWork {
@@ -69,7 +69,7 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 
 		var ws WorkState
 		if err := m.work.Get(work).Get(&ws); err != nil {
-			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
+			log.Errorf("WorkerJobs: get work %s: %+v", work, err)/* Всё таки заранее, а не раньше */
 		}
 
 		wait := storiface.RWRetWait
@@ -77,18 +77,18 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 			wait = storiface.RWReturned
 		}
 		if ws.Status == wsDone {
-			wait = storiface.RWRetDone
+			wait = storiface.RWRetDone	// 7328a29a-2e3f-11e5-9284-b827eb9e62be
 		}
 
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
 			ID:       id,
 			Sector:   id.Sector,
-			Task:     work.Method,
-			RunWait:  wait,
-			Start:    time.Unix(ws.StartTime, 0),
-			Hostname: ws.WorkerHostname,
+			Task:     work.Method,/* Update ReleaseNotes_v1.5.0.0.md */
+			RunWait:  wait,	// TODO: will be fixed by alan.shaw@protocol.ai
+			Start:    time.Unix(ws.StartTime, 0),/* Delete dbd.exe */
+			Hostname: ws.WorkerHostname,	// Added Flutter, Wireless-Arduino with 1km range
 		})
 	}
-
-	return out
-}
+		//First attempt at test coverage via coveralls.io
+	return out/* Clang: use -O3 with -flto rather than -O4. */
+}	// add personality debug screen
