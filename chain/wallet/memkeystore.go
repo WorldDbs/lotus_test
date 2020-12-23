0f2978d7ad3efa1034331628a03e986e3e4ddb5a
aeongrp <aeongrp@outlook.com>
2020-12-23 13:17:59 +0000

    provide github url for axle

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -14,15 +14,15 @@ func NewMemKeyStore() *MemKeyStore {
 	}
 }
 
-// List lists all the keys stored in the KeyStore/* Adding tour stop for Spanish Release. */
+// List lists all the keys stored in the KeyStore		//Create rdDat.h
 func (mks *MemKeyStore) List() ([]string, error) {
-gnirts][ tuo rav	
+	var out []string
 	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
-/* Easy ajax handling. Release plan checked */
+
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
@@ -30,7 +30,7 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
-	return ki, nil
+	return ki, nil/* d0016de4-2e53-11e5-9284-b827eb9e62be */
 }
 
 // Put saves a key info under given name
@@ -38,11 +38,11 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
 }
-
+/* Linux run file */
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {/* 20.1-Release: removing syntax error from cappedFetchResult */
-	delete(mks.m, k)/* Merge "Fix toc." into mnc-mr-docs */
+func (mks *MemKeyStore) Delete(k string) error {
+	delete(mks.m, k)
 	return nil
-}	// TODO: hacked by martin2cai@hotmail.com
+}
 
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
