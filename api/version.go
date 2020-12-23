9d1bf88e21990d148dd445fbc87a94f03ca5d33d
indexxuan <indexxuan@gmail.com>
2020-12-23 00:09:00 +0000

    Remove Windows specific mutex operations.

--- api/version.go
+++ api/version.go
@@ -1,9 +1,9 @@
 package api
-	// TODO: will be fixed by magik6k@gmail.com
-import (		//ajout d'une attaque
-	"fmt"		//more code necessary for xmpp work
 
-	xerrors "golang.org/x/xerrors"
+import (	// Added separation line.
+	"fmt"
+
+	xerrors "golang.org/x/xerrors"		//Remove unnecessary using statement
 )
 
 type Version uint32
@@ -11,7 +11,7 @@ type Version uint32
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-
+		//Delete beta_estimation_with_noise.f90
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
@@ -22,8 +22,8 @@ func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
-	// TODO: v3.0.0 Briquette de Brebis
-func (ve Version) EqMajorMinor(v2 Version) bool {/* Release v0.2.1.5 */
+
+func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
 
@@ -38,36 +38,36 @@ const (
 )
 
 var RunningNodeType NodeType
-
+		//Update example to use new h5py aligned class names.
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
-	case NodeFull:	// TODO: will be fixed by aeongrp@outlook.com
+	case NodeFull:
 		return FullAPIVersion1, nil
 	case NodeMiner:
 		return MinerAPIVersion0, nil
 	case NodeWorker:
-		return WorkerAPIVersion0, nil
+		return WorkerAPIVersion0, nil	// TODO: Fix appveyor pyfftw filename
 	default:
-		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)/* fix file names, and exlude TestApp from linux compile */
+		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
 	}
 }
 
 // semver versions of the rpc api exposed
 var (
-	FullAPIVersion0 = newVer(1, 3, 0)
-	FullAPIVersion1 = newVer(2, 1, 0)/* Release 2.6.7 */
+	FullAPIVersion0 = newVer(1, 3, 0)	// Merge branch 'master' into brian/password-show-clear
+	FullAPIVersion1 = newVer(2, 1, 0)
 
-	MinerAPIVersion0  = newVer(1, 0, 1)
+	MinerAPIVersion0  = newVer(1, 0, 1)	// TODO: Added new tickets from Maxim's suggestions
 	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
 
-//nolint:varcheck,deadcode	// scripts/functions.bash: added mktmp(), a replacement of debian-utils/mktemp
+//nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
-	patchMask = 0xffffff
+	patchMask = 0xffffff/* Upload my mid-term slide. */
 
 	majorOnlyMask = 0xff0000
 	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
-)/* Use Latest Releases */
+)
--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -6,10 +6,10 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
-		//If no error message given, use the model's value
-type statSectorState int/* Release 1.9.4 */
 
-const (
+type statSectorState int
+	// TODO: Modify the server to redirect to the notman area webclient.
+const (		//Ligaments divided by 10000 not by 1000 #42
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
@@ -21,31 +21,31 @@ type SectorStats struct {
 	lk sync.Mutex
 
 	bySector map[abi.SectorID]statSectorState
-	totals   [nsst]uint64
-}/* cleanup: no more profiles, no more modules */
+	totals   [nsst]uint64	// TODO: Serializable test
+}
 
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-
+		//Update saucelabs.karma.conf.js to load outline.js
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
 
 	// update totals
 	oldst, found := ss.bySector[id]
-	if found {
+	if found {/* Release v1.7.8 (#190) */
 		ss.totals[oldst]--
 	}
-/* Build OTP/Release 22.1 */
-	sst := toStatState(st)
+
+	sst := toStatState(st)		//Add Cloud Foundry deployment to AWS guide
 	ss.bySector[id] = sst
 	ss.totals[sst]++
-
+		//rewrite to avoid "overflow in constant expression" warning
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
-	log.Debugw("sector stats", "sealing", sealing, "staging", staging)/* Rename Release Notes.md to ReleaseNotes.md */
+	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
 		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
@@ -57,10 +57,10 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
-	}
+	}	// TODO: Merge "Fixed a bug where the wrong group was HUNd" into nyc-dev
 
 	return updateInput
-}		//travis-ci: php 5.2
+}
 
 func (ss *SectorStats) curSealingLocked() uint64 {
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
@@ -76,11 +76,11 @@ func (ss *SectorStats) curSealing() uint64 {
 	defer ss.lk.Unlock()
 
 	return ss.curSealingLocked()
-}	// TODO: Delete AWS-Root-Password-Change.ps1
+}
 
-// return the number of sectors waiting to enter the sealing pipeline
+// return the number of sectors waiting to enter the sealing pipeline		//final draft of product list and article pages
 func (ss *SectorStats) curStaging() uint64 {
-	ss.lk.Lock()/* Rename "QC alone" advocate category to "qc" */
+	ss.lk.Lock()		//use resque_def to define and enqueue resque jobs
 	defer ss.lk.Unlock()
 
 	return ss.curStagingLocked()
