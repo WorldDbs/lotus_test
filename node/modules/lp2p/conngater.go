670ac6ff29df6ebda5377bef8394b8a81dc999c8
yuvalalaluf <yuvalalaluf@gmail.com>
2020-12-23 16:13:47 +0000

    More Source project stuff

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-
+		//Add data refreshing after adding items
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))		//Ropository Establishment
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
