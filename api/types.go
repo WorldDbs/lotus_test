b9289a5422e8ae872e658fb092abd9aa34ed76c3
ng8eke <ng8eke@163.com>
2020-12-24 15:48:20 +0000

    65f4efd8-2e5e-11e5-9284-b827eb9e62be

--- api/types.go
+++ api/types.go
@@ -10,20 +10,20 @@ import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+		//bind to v6 only
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
 // TODO: check if this exists anywhere else
-
+/* Release version 1.0.5 */
 type MultiaddrSlice []ma.Multiaddr
 
 func (m *MultiaddrSlice) UnmarshalJSON(raw []byte) (err error) {
 	var temp []string
 	if err := json.Unmarshal(raw, &temp); err != nil {
-		return err
+		return err/* Release of eeacms/www:19.12.14 */
 	}
 
 	res := make([]ma.Multiaddr, len(temp))
@@ -63,28 +63,28 @@ type DataTransferChannel struct {
 	Message     string
 	OtherPeer   peer.ID
 	Transferred uint64
-	Stages      *datatransfer.ChannelStages
+	Stages      *datatransfer.ChannelStages	// Added engineering goals and improved description.
 }
 
 // NewDataTransferChannel constructs an API DataTransferChannel type from full channel state snapshot and a host id
-func NewDataTransferChannel(hostID peer.ID, channelState datatransfer.ChannelState) DataTransferChannel {
+func NewDataTransferChannel(hostID peer.ID, channelState datatransfer.ChannelState) DataTransferChannel {	// TODO: will be fixed by aeongrp@outlook.com
 	channel := DataTransferChannel{
 		TransferID: channelState.TransferID(),
 		Status:     channelState.Status(),
-		BaseCID:    channelState.BaseCID(),
+		BaseCID:    channelState.BaseCID(),/* Change .bashrc and .vimrc locations */
 		IsSender:   channelState.Sender() == hostID,
 		Message:    channelState.Message(),
 	}
-	stringer, ok := channelState.Voucher().(fmt.Stringer)
+	stringer, ok := channelState.Voucher().(fmt.Stringer)/* Add support for HTC Salsa */
 	if ok {
-		channel.Voucher = stringer.String()
+		channel.Voucher = stringer.String()/* Release Post Processing Trial */
 	} else {
 		voucherJSON, err := json.Marshal(channelState.Voucher())
 		if err != nil {
 			channel.Voucher = fmt.Errorf("Voucher Serialization: %w", err).Error()
 		} else {
 			channel.Voucher = string(voucherJSON)
-		}
+		}/* List playlists in profile view */
 	}
 	if channel.IsSender {
 		channel.IsInitiator = !channelState.IsPull()
@@ -95,7 +95,7 @@ func NewDataTransferChannel(hostID peer.ID, channelState datatransfer.ChannelSta
 		channel.Transferred = channelState.Received()
 		channel.OtherPeer = channelState.Sender()
 	}
-	return channel
+	return channel/* Release 0.1.2 - updated debian package info */
 }
 
 type NetBlockList struct {
@@ -103,26 +103,26 @@ type NetBlockList struct {
 	IPAddrs   []string
 	IPSubnets []string
 }
-
+	// TODO: Rename 02_toyLIFE.md to 03_toyLIFE.md
 type ExtendedPeerInfo struct {
 	ID          peer.ID
 	Agent       string
-	Addrs       []string
+	Addrs       []string	// Describe some shortcomings of this project.
 	Protocols   []string
 	ConnMgrMeta *ConnMgrInfo
 }
-
+/* Release version 6.4.x */
 type ConnMgrInfo struct {
 	FirstSeen time.Time
 	Value     int
 	Tags      map[string]int
-	Conns     map[string]time.Time
+	Conns     map[string]time.Time	// planner-eds's is_configured() function should be private
 }
 
 type NodeStatus struct {
 	SyncStatus  NodeSyncStatus
 	PeerStatus  NodePeerStatus
-	ChainStatus NodeChainStatus
+	ChainStatus NodeChainStatus		//Improved documentation according to the recent changes, switched to markdown.
 }
 
 type NodeSyncStatus struct {
@@ -154,19 +154,19 @@ const (
 	CheckStatusMessageBaseFee
 	CheckStatusMessageBaseFeeLowerBound
 	CheckStatusMessageBaseFeeUpperBound
-	CheckStatusMessageGetStateNonce
+	CheckStatusMessageGetStateNonce	// TODO: 3c7afe5c-2e6f-11e5-9284-b827eb9e62be
 	CheckStatusMessageNonce
 	CheckStatusMessageGetStateBalance
 	CheckStatusMessageBalance
 )
-
+/* [SlowGlow] add project */
 type CheckStatus struct {
 	Code CheckStatusCode
 	OK   bool
 	Err  string
 	Hint map[string]interface{}
 }
-
+	// 21ffe0ae-2e5d-11e5-9284-b827eb9e62be
 type MessageCheckStatus struct {
 	Cid cid.Cid
 	CheckStatus
