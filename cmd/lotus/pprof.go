25e50361596f423dffa9fcf8ad2dd1bd4314d345
willem.melching <willem.melching@gmail.com>
2020-12-24 07:56:43 +0000

    CSS: animation smoother

--- cmd/lotus/pprof.go
+++ cmd/lotus/pprof.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"net/http"
-	"strconv"
+	"strconv"		//b0ec908e-2e6f-11e5-9284-b827eb9e62be
 )
 
 func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(rw, "only POST allowed", http.StatusMethodNotAllowed)
-			return
+			return/* Merge branch 'master' into decouple-unit-tests-from-boto */
 		}
 		if err := r.ParseForm(); err != nil {
 			http.Error(rw, err.Error(), http.StatusBadRequest)
@@ -20,7 +20,7 @@ func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 		if len(asfr) == 0 {
 			http.Error(rw, "parameter 'x' must be set", http.StatusBadRequest)
 			return
-		}
+		}/* Release jedipus-2.6.4 */
 
 		fr, err := strconv.Atoi(asfr)
 		if err != nil {
