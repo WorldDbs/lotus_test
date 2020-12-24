95703bee2fde96a2bc14de96fe92f6f1b1e1ed83
alessio <alessio@tendermint.com>
2020-12-24 14:05:07 +0000

    fos + facebook  fixed

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -3,28 +3,28 @@ package account
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release Notes added */
-
+		//Only generate javadoc of fi.laverca classes. Change javadocs name to apidocs.
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+		//1ae70d9e-2e42-11e5-9284-b827eb9e62be
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {
+func load2(store adt.Store, root cid.Cid) (State, error) {/* Release plugin configuration added */
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}
-	// TODO: hacked by xaber.twt@gmail.com
+}	// Add UI Persistence for Consoles, Groovy Object Stage and Preferences
+
 type state2 struct {
 	account2.State
 	store adt.Store
 }
-
+/* Update FacturaReleaseNotes.md */
 func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}		//Getting const ref as point parameter.
+}
