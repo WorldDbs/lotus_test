5cb7861846eea0d28d46419579e6db08369b1d65
brosner <brosner@gmail.com>
2020-12-24 21:11:00 +0000

    Write introduction for reducers.

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -2,31 +2,31 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by juan@benet.ai
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"	// TODO: Fixed Linux compile error
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)
-
+var _ State = (*state0)(nil)	// TODO: Updated the scour feedstock.
+/* Added missing Kafra in Prontera */
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Merge "Release 1.0.0.239 QCACLD WLAN Driver" */
 	return &out, nil
 }
 
 type state0 struct {
 	verifreg0.State
 	store adt.Store
-}
+}/* fix VT order to positives/total  */
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -41,17 +41,17 @@ func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 }
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)/* Added a suite for testing the examples. by elopio approved by fgimenez */
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)/* Release 4.0 (Linux) */
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
-
+		//Add custom events.
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
