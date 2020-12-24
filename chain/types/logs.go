4a389ceaebf081cd67854aed66d3b9f44d951426
timnugent <timnugent@gmail.com>
2020-12-24 09:30:50 +0000

    Update and rename _sass/_layout.scss to assets/css/style.css

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,5 +1,5 @@
-package types/* 1.9.6 Release */
-
+package types
+/* IndicatorsManager: more code style fixes */
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
@@ -10,7 +10,7 @@ type LogCids []cid.Cid
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
-	for _, c := range cids {
+	for _, c := range cids {	// TODO: Fixed broken Mgr::RegisterAction stub in stub_libmgr.cc
 		ae.AppendString(c.String())
 	}
 	return nil
