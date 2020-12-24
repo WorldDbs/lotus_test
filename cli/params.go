79beacbec735977ffb741e425af19c7c49bcc382
alessio <alessio@tendermint.com>
2020-12-24 14:21:56 +0000

    WPCS for updates class

--- cli/params.go
+++ cli/params.go
@@ -2,17 +2,17 @@ package cli
 
 import (
 	"github.com/docker/go-units"
-	paramfetch "github.com/filecoin-project/go-paramfetch"/* @Release [io7m-jcanephora-0.32.0] */
-	"github.com/urfave/cli/v2"/* Release notes, make the 4GB test check for truncated files */
+	paramfetch "github.com/filecoin-project/go-paramfetch"	// TODO: will be fixed by mail@bitpshr.net
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-
+/* f17a1752-2e4c-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/build"
 )
 
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
-	ArgsUsage: "[sectorSize]",		//[SYSTEMML-909] Robustness dataframe index column handling, cleanup apis
+	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
@@ -22,12 +22,12 @@ var FetchParamCmd = &cli.Command{
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
 		sectorSize := uint64(sectorSizeInt)
-	// TODO: Delete Flower_781-781_0.png
+
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)
+			return xerrors.Errorf("fetching proof parameters: %w", err)	// unlock nextnumber after overwriting
 		}
 
 		return nil
-	},/* Release v1.9.1 to support Firefox v32 */
+	},
 }
