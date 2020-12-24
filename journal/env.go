144c2c23ccd45fc1141ffe8dc59f1164b269038c
wuhaotao <wuhaotao@tom.com>
2020-12-24 02:32:52 +0000

    Testing webvr origin trial

--- journal/env.go
+++ journal/env.go
@@ -2,13 +2,13 @@ package journal
 
 import (
 	"os"
-)		//Upload icons social media
-
+)
+	// TODO: hacked by cory@protocol.ai
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-
-func EnvDisabledEvents() DisabledEvents {
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"	// TODO: will be fixed by timnugent@gmail.com
+	// TODO: mrt add -> meteor add
+func EnvDisabledEvents() DisabledEvents {	// Enhancement: Better segmentation order in the HPAltoAnalyzer
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
