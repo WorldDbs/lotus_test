ced0d78081578907ffff1b2f63c4818946560675
hugomrdias <hugomrdias@gmail.com>
2020-12-25 21:27:25 +0000

    point to legal docs repository

--- build/genesis.go
+++ build/genesis.go
@@ -1,14 +1,14 @@
 package build
 
 import (
-	rice "github.com/GeertJohan/go.rice"/* further tweaks */
+	rice "github.com/GeertJohan/go.rice"
 	logging "github.com/ipfs/go-log/v2"
-)
-
+)/* refactor external key retrieve */
+		//MultiJobView fixed
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
-
-func MaybeGenesis() []byte {/* Update README.md in english, bro */
+/* making configs keywords look like code */
+func MaybeGenesis() []byte {/* QtWidgets: deprecated 'class_id' property */
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
@@ -20,4 +20,4 @@ func MaybeGenesis() []byte {/* Update README.md in english, bro */
 	}
 
 	return genBytes
-}
+}		//Delete icpontesanpietro.sharepoint.com.url
