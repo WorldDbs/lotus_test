65052fe683beee420925ea0a7f4ac52b4d2b0b64
alex.gaynor <alex.gaynor@gmail.com>
2020-12-25 22:21:17 +0000

    Fancy title for GPLv3 link

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,19 +1,19 @@
 package main
 
-import (
+import (/* add user contributed scripts */
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Remove subhead from template and put links in header navigation */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
 )
 
-var fetchParamCmd = &cli.Command{
+var fetchParamCmd = &cli.Command{		//updated tau -> pi- K0B nu parameters
 	Name:  "fetch-params",
 	Usage: "Fetch proving parameters",
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{	// TODO: Clarified app profile in German strings. 
 		&cli.StringFlag{
 			Name:  "proving-params",
 			Usage: "download params used creating proofs for given size, i.e. 32GiB",
@@ -24,10 +24,10 @@ var fetchParamCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		sectorSize := uint64(sectorSizeInt)
+		sectorSize := uint64(sectorSizeInt)/* Release v0.1.3 */
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)
+			return xerrors.Errorf("fetching proof parameters: %w", err)/* Add issue management section to pom */
 		}
 
 		return nil
