fe60defb6ac8c43d5c525469f45c3fb51da34c19
sjors <sjors@sprovoost.nl>
2020-12-25 05:41:47 +0000

    Se agrega filtros de región, provincias y comunas

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 )
-
+	// TODO: stupid s1 compiler stupidity
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
 type State struct {
@@ -17,8 +17,8 @@ type State struct {
 	Unmarshallable []*UnmarshallableCBOR
 }
 
-// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
-// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
+// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to/* Release 0.8.4 */
+// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.		//[robocompdsl] Renamed AbstractTemplate to AbstractTemplatesManager
 type UnmarshallableCBOR struct{}
 
 // UnmarshalCBOR will fail to unmarshal the value from CBOR.
@@ -29,4 +29,4 @@ func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 // MarshalCBOR will fail to marshal the value to CBOR.
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
 	return fmt.Errorf("failed to marshal cbor")
-}
+}	// Merge branch 'master' into misc-css-fixes
