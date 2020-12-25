6c7e3efbe38cb202ce714ece5db838ffe0e29db2
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-12-25 20:18:31 +0000

    Stokes limit added

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,23 +1,23 @@
 package state
 
-import (
+import (/* 8850dec0-2e40-11e5-9284-b827eb9e62be */
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: hacked by cory@protocol.ai
 )
 
 type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
-/* Remove some unused classes, move NewsModelDataMapper to proper package */
+
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)	// TODO: hacked by ligi@ligi.de
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
