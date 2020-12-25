2582e1a078e2f4f68ba29db022b98de89ec96977
m-ou.se <m-ou.se@m-ou.se>
2020-12-25 06:18:14 +0000

    update react

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,6 +1,6 @@
 package chaos
 
-import (
+import (	// TODO: will be fixed by boringland@protonmail.ch
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
@@ -8,21 +8,21 @@ import (
 
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
-	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
+	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}/* isc dhcp fixes */
 	c, err := builder.Sum([]byte("fil/1/chaos"))
-	if err != nil {/* Release 0.4.0 */
+	if err != nil {
 		panic(err)
 	}
 	return c
 }()
-/* insert random library */
+
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
 // singleton.
 var Address = func() address.Address {
 	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
-	if err != nil {/* AniImageList.java Fix. Ability WSYSTEMFC removed. ANISMILES in InfoWindow.java */
+	if err != nil {
 		panic(err)
 	}
 	return addr
