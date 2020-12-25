9ca4f7824717cd74d6e9750a3df4114124ae7548
praveen <praveen@minio.io>
2020-12-25 03:51:38 +0000

    Deselect read on EOF.

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
-package dtypes
-/* Update dependency broccoli-asset-rev to v2.7.0 */
-import "github.com/libp2p/go-libp2p-core/peer"		//#98 Made the background of the SegmentedLineEdge transparent.
+package dtypes		//pb hotkey 4
+
+import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
-	// TODO: hacked by igor@soramitsu.co.jp
+
 type Bootstrapper bool
