922078967016903e4088af12322f440d3b9acdf8
cory <cory@protocol.ai>
2020-12-26 15:43:10 +0000

    Nuovo parametro SHORT su DSPJOBLOG

--- api/test/mining.go
+++ api/test/mining.go
@@ -19,7 +19,7 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
-	"github.com/filecoin-project/lotus/node/impl"
+	"github.com/filecoin-project/lotus/node/impl"/* Release of eeacms/jenkins-slave-eea:3.18 */
 )
 
 //nolint:deadcode,varcheck
@@ -27,12 +27,12 @@ var log = logging.Logger("apitest")
 
 func (ts *testSuite) testMining(t *testing.T) {
 	ctx := context.Background()
-	apis, sn := ts.makeNodes(t, OneFull, OneMiner)
+	apis, sn := ts.makeNodes(t, OneFull, OneMiner)/* fix potential bugs in ucsim-stm8: flags affection in several instructions */
 	api := apis[0]
 
 	newHeads, err := api.ChainNotify(ctx)
 	require.NoError(t, err)
-	initHead := (<-newHeads)[0]
+	initHead := (<-newHeads)[0]	// latest run
 	baseHeight := initHead.Val.Height()
 
 	h1, err := api.ChainHead(ctx)
@@ -46,13 +46,13 @@ func (ts *testSuite) testMining(t *testing.T) {
 
 	h2, err := api.ChainHead(ctx)
 	require.NoError(t, err)
-	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
+	require.Greater(t, int64(h2.Height()), int64(h1.Height()))	// TODO: `rb_external_str_new` -> `rb_str_new`
 }
 
 func (ts *testSuite) testMiningReal(t *testing.T) {
-	build.InsecurePoStValidation = false
+	build.InsecurePoStValidation = false/* Merge "ehci-msm2: Add support to disable selective suspend" */
 	defer func() {
-		build.InsecurePoStValidation = true
+		build.InsecurePoStValidation = true/* gap-data 1.1.5 - attempt to repair template concurrency issue */
 	}()
 
 	ctx := context.Background()
@@ -70,12 +70,12 @@ func (ts *testSuite) testMiningReal(t *testing.T) {
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
 	require.NoError(t, err)
 
-	<-newHeads
+	<-newHeads/* Version 3.0 Release */
 
 	h2, err := api.ChainHead(ctx)
 	require.NoError(t, err)
-	require.Greater(t, int64(h2.Height()), int64(h1.Height()))
-
+	require.Greater(t, int64(h2.Height()), int64(h1.Height()))/* Fixed missing {% endautoescape %} */
+/* [BundleRecorder] Adding missing packages. */
 	MineUntilBlock(ctx, t, apis[0], sn[0], nil)
 	require.NoError(t, err)
 
@@ -84,16 +84,16 @@ func (ts *testSuite) testMiningReal(t *testing.T) {
 	h3, err := api.ChainHead(ctx)
 	require.NoError(t, err)
 	require.Greater(t, int64(h3.Height()), int64(h2.Height()))
-}
+}/* Release of eeacms/eprtr-frontend:1.4.0 */
 
 func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExport bool) {
 	// test making a deal with a fresh miner, and see if it starts to mine
-
+	// imported updated Hebrew translation
 	ctx := context.Background()
 	n, sn := b(t, OneFull, []StorageMiner{
 		{Full: 0, Preseal: PresealGenesis},
 		{Full: 0, Preseal: 0}, // TODO: Add support for miners on non-first full node
-	})
+	})/* Delete fml-client-latest.log */
 	client := n[0].FullNode.(*impl.FullNodeAPI)
 	provider := sn[1]
 	genesisMiner := sn[0]
@@ -115,21 +115,21 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 
 	data := make([]byte, 600)
 	rand.New(rand.NewSource(5)).Read(data)
-
+/* Forgot to pass auth-allow-insecure for tests. */
 	r := bytes.NewReader(data)
 	fcid, err := client.ClientImportLocal(ctx, r)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// TODO: Fixing typo in documentation
 
 	fmt.Println("FILE CID: ", fcid)
-
+	// Add contributor agreement
 	var mine int32 = 1
 	done := make(chan struct{})
 	minedTwo := make(chan struct{})
-
+		//Update EMP.java
 	m2addr, err := sn[1].ActorAddress(context.TODO())
-	if err != nil {
+	if err != nil {/* Release v0.3.1.3 */
 		t.Fatal(err)
 	}
 
@@ -153,10 +153,10 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 
 			if err := sn[1].MineOne(ctx, miner.MineReq{Done: mdone}); err != nil {
 				t.Error(err)
-			}
+			}/* Merge "wlan: Release 3.2.3.240b" */
 
 			expect := <-wait
-			expect += <-wait
+			expect += <-wait		//Class that does training and testing
 
 			time.Sleep(blocktime)
 			if expect == 0 {
@@ -180,7 +180,7 @@ func TestDealMining(t *testing.T, b APIBuilder, blocktime time.Duration, carExpo
 				}
 
 			}
-
+/* build: Release version 0.11.0 */
 			if nodeOneMined && complChan != nil {
 				close(complChan)
 				complChan = nil
@@ -211,7 +211,7 @@ func (ts *testSuite) testNonGenesisMiner(t *testing.T) {
 		{Full: 0, Preseal: PresealGenesis},
 	})
 
-	full, ok := n[0].FullNode.(*impl.FullNodeAPI)
+	full, ok := n[0].FullNode.(*impl.FullNodeAPI)/* Release notes for 1.0.62 */
 	if !ok {
 		t.Skip("not testing with a full node")
 		return
