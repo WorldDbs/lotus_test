25918f72c6f5293a738029f85aa7b169bdb6b713
joshua <joshua@yottadb.com>
2020-12-26 07:00:02 +0000

    Update pollAndPrintJob.sh

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
 package v0api
 
-import (		//4f55e3ce-2e61-11e5-9284-b827eb9e62be
+import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
-		//Export DI from Data/FileStore/Generic.hs.
+
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
-/* fix radio_options_for */
+type StorageMinerStruct = api.StorageMinerStruct/* Update readme some more. */
+
 type Worker = api.Worker
-type WorkerStruct = api.WorkerStruct		//Boostrap et nouvelle vue
+type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	return api.PermissionedStorMinerAPI(a)
+	return api.PermissionedStorMinerAPI(a)		//get vectorizer and classifier
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)	// lock down modular scale dependency
+	return api.PermissionedWorkerAPI(a)
 }
