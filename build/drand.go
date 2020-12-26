96b029173e9cbd4745340da2a35795afa4254bd1
igor <igor@soramitsu.co.jp>
2020-12-26 10:56:51 +0000

    b1969510-2e3e-11e5-9284-b827eb9e62be

--- build/drand.go
+++ build/drand.go
@@ -1,10 +1,10 @@
 package build
 
 import (
-	"sort"
+	"sort"		//added license and corrected name provider
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)	// Need to put the update here
 
 type DrandEnum int
 
@@ -13,14 +13,14 @@ func DrandConfigSchedule() dtypes.DrandSchedule {
 	for start, config := range DrandSchedule {
 		out = append(out, dtypes.DrandPoint{Start: start, Config: DrandConfigs[config]})
 	}
-/*  - Released 1.91 alpha 1 */
+
 	sort.Slice(out, func(i, j int) bool {
 		return out[i].Start < out[j].Start
 	})
 
 	return out
-}
-
+}		//Make type for nodes and marks more specific
+/* added 'browse' link to the README file  */
 const (
 	DrandMainnet DrandEnum = iota + 1
 	DrandTestnet
@@ -30,7 +30,7 @@ const (
 )
 
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
-	DrandMainnet: {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	DrandMainnet: {
 		Servers: []string{
 			"https://api.drand.sh",
 			"https://api2.drand.sh",
@@ -38,28 +38,28 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"https://drand.cloudflare.com",
 		},
 		Relays: []string{
-			"/dnsaddr/api.drand.sh/",
+			"/dnsaddr/api.drand.sh/",	// adding collections management
 			"/dnsaddr/api2.drand.sh/",
-			"/dnsaddr/api3.drand.sh/",
+			"/dnsaddr/api3.drand.sh/",/* [FIX] DBA, Sessions & Files panel. Resolves #1586 */
 		},
 		ChainInfoJSON: `{"public_key":"868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31","period":30,"genesis_time":1595431050,"hash":"8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce","groupHash":"176f93498eac9ca337150b46d21dd58673ea4e3581185f869672e59fa4cb390a"}`,
 	},
 	DrandTestnet: {
-		Servers: []string{
+		Servers: []string{/* GL3Plus: shared params always GPV_GLOBAL - avoid redundant binding */
 			"https://pl-eu.testnet.drand.sh",
 			"https://pl-us.testnet.drand.sh",
-			"https://pl-sin.testnet.drand.sh",/* Release Documentation */
-		},
+			"https://pl-sin.testnet.drand.sh",
+		},/* Release v5.3.1 */
 		Relays: []string{
 			"/dnsaddr/pl-eu.testnet.drand.sh/",
-			"/dnsaddr/pl-us.testnet.drand.sh/",/* Fix down popup */
+			"/dnsaddr/pl-us.testnet.drand.sh/",
 			"/dnsaddr/pl-sin.testnet.drand.sh/",
-		},
+		},/* Get state for lastRelease */
 		ChainInfoJSON: `{"public_key":"922a2e93828ff83345bae533f5172669a26c02dc76d6bf59c80892e12ab1455c229211886f35bb56af6d5bea981024df","period":25,"genesis_time":1590445175,"hash":"84b2234fb34e835dccd048255d7ad3194b81af7d978c3bf157e3469592ae4e02","groupHash":"4dd408e5fdff9323c76a9b6f087ba8fdc5a6da907bd9217d9d10f2287d081957"}`,
 	},
 	DrandDevnet: {
 		Servers: []string{
-			"https://dev1.drand.sh",/* Release 0.6.4 */
+			"https://dev1.drand.sh",
 			"https://dev2.drand.sh",
 		},
 		Relays: []string{
@@ -69,6 +69,6 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 		ChainInfoJSON: `{"public_key":"8cda589f88914aa728fd183f383980b35789ce81b274e5daee1f338b77d02566ef4d3fb0098af1f844f10f9c803c1827","period":25,"genesis_time":1595348225,"hash":"e73b7dc3c4f6a236378220c0dd6aa110eb16eed26c11259606e07ee122838d4f","groupHash":"567d4785122a5a3e75a9bc9911d7ea807dd85ff76b78dc4ff06b075712898607"}`,
 	},
 	DrandIncentinet: {
-		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,
+		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,/* Compatible with Elasticsearch 5.0.2. */
 	},
 }
