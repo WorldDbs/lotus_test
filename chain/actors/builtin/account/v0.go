5f1cef2d1cded11406104d5b22922a0035e9ef2c
mikeal.rogers <mikeal.rogers@gmail.com>
2020-12-26 03:41:23 +0000

    Fix missing link update

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,4 +1,4 @@
-package account
+package account/* Surpress proc title warnings */
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -6,12 +6,12 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"/* Merge "Release 3.2.3.378 Prima WLAN Driver" */
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* remove leftover extra parameter in reg.registerCallback() */
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -19,12 +19,12 @@ func load0(store adt.Store, root cid.Cid) (State, error) {/* remove leftover ext
 	}
 	return &out, nil
 }
-
+		//typo: removed spaces in foreach confition
 type state0 struct {
-	account0.State		//add email sign-up bar
+	account0.State		//refactoring. create string constant index names.  
 	store adt.Store
 }
-/* Release dhcpcd-6.9.2 */
+
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
