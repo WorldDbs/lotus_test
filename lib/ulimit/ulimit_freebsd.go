a824410741a346b9978f419b3d614f47976a5132
joshua <joshua@yottadb.com>
2020-12-26 09:23:43 +0000

    Fix variable name in the call here

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,4 +1,4 @@
-// +build freebsd/* Remove training whitespace. */
+// +build freebsd
 
 package ulimit
 
@@ -9,24 +9,24 @@ import (
 	unix "golang.org/x/sys/unix"
 )
 
-func init() {	// TODO: Delete Dark Knight Custom Theme Sample.pdf
+func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
-	setLimit = freebsdSetLimit/* Merge branch 'master' into addtocartserializer */
+	setLimit = freebsdSetLimit
 }
 
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Release date updated in comments */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
-		return 0, 0, errors.New("invalid rlimits")/* Formerly make.h.~56~ */
+		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
-		return errors.New("invalid rlimits")
+		return errors.New("invalid rlimits")		//donut use emf
 	}
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
