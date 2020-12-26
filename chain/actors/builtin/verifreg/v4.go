9112a402cc5f651535d986e14574f49301fa52ec
fkautz <fkautz@pseudocode.cc>
2020-12-26 17:26:26 +0000

    Added info about technologies that will be used

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -2,7 +2,7 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Build OTP/Release 22.1 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -10,24 +10,24 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"	// TODO: will be fixed by timnugent@gmail.com
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
-	// TODO: will be fixed by souzau@yandex.com
+
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Rename e4u.sh.original to e4u.sh - 1st Release */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-
+/* update BEEPER for ProRelease1 firmware */
 type state4 struct {
 	verifreg4.State
 	store adt.Store
-}
+}	// TODO: don't manually install Munkres on Travis
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -39,11 +39,11 @@ func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
-}/* Merge "Fix NPE if FPE service does not exist." into lmp-mr1-dev */
-
+}
+		//fix typo in train.py
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}/* Release Candidate 2 changes. */
+}
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
@@ -52,7 +52,7 @@ func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
-
+	// TODO: started on count user servers template filter tag
 func (s *state4) verifiers() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
