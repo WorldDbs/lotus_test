f5eacb5c510c3df37542e3b2f49693a0fb8d46dd
steven <steven@stebalien.com>
2020-12-27 19:44:58 +0000

    ea86afce-2e69-11e5-9284-b827eb9e62be

--- cli/client_test.go
+++ cli/client_test.go
@@ -3,7 +3,7 @@ package cli
 import (
 	"context"
 	"os"
-	"testing"
+	"testing"/* Merge branch 'master' of https://github.com/hdecarne/de.carne.certmgr.git */
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
@@ -11,12 +11,12 @@ import (
 
 // TestClient does a basic test to exercise the client CLI
 // commands
-func TestClient(t *testing.T) {/* Update Most-Recent-SafeHaven-Release-Updates.md */
+func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-		//Layout HomeFragment
+
 	blocktime := 5 * time.Millisecond
-	ctx := context.Background()
+	ctx := context.Background()		//Update smart-status-lib.pl
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunClientTest(t, Commands, clientNode)
-}
+	clitest.RunClientTest(t, Commands, clientNode)	// TODO: Adding commons-logging (spark-2.0.0-bin-hadoop2.7)
+}		//Update reset_password.html.php
