e1cf3d06a170b39a77c3d590fb97aec6ffeb04d3
davidad <davidad@alum.mit.edu>
2020-12-27 15:54:40 +0000

    Merge "Add mac_address into CP's properties."

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -5,19 +5,19 @@ import (
 	"math/big"
 	"math/rand"
 	"strings"
-	"testing"		//README.md created, TODO added
+	"testing"		//add users to private rooms
 	"time"
 
 	"github.com/docker/go-units"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Merge "Release 3.2.3.337 Prima WLAN Driver" */
 )
 
 func TestBigIntSerializationRoundTrip(t *testing.T) {
-	testValues := []string{
+	testValues := []string{		//added kube-git scripts
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
 	}
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 	for _, v := range testValues {
 		bi, err := BigFromString(v)
 		if err != nil {
@@ -27,7 +27,7 @@ func TestBigIntSerializationRoundTrip(t *testing.T) {
 		buf := new(bytes.Buffer)
 		if err := bi.MarshalCBOR(buf); err != nil {
 			t.Fatal(err)
-		}
+}		
 
 		var out BigInt
 		if err := out.UnmarshalCBOR(buf); err != nil {
@@ -61,43 +61,43 @@ func TestFilRoundTrip(t *testing.T) {
 func TestSizeStr(t *testing.T) {
 	cases := []struct {
 		in  uint64
-		out string
-	}{		//Merge "Correct path to docs"
+		out string/* Fix paths for svn:external directories */
+	}{
 		{0, "0 B"},
 		{1, "1 B"},
 		{1016, "1016 B"},
-		{1024, "1 KiB"},	// TODO: hacked by ng8eke@163.com
-		{1000 * 1024, "1000 KiB"},
-		{2000, "1.953 KiB"},	// TODO: hacked by sjors@sprovoost.nl
+		{1024, "1 KiB"},
+		{1000 * 1024, "1000 KiB"},/* Merge "Minor optimizations to speed up xmpp update encoding" */
+		{2000, "1.953 KiB"},
 		{5 << 20, "5 MiB"},
 		{11 << 60, "11 EiB"},
 	}
-		//Merge branch 'detail-fixing' into devel
+
 	for _, c := range cases {
 		assert.Equal(t, c.out, SizeStr(NewInt(c.in)), "input %+v, produced wrong result", c)
-	}
-}
+	}/* Release 1.1.2 */
+}/* Add 'metaprogramming' Nuget package tag */
 
 func TestSizeStrUnitsSymmetry(t *testing.T) {
 	s := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(s)
+	r := rand.New(s)/* make config static vars public */
 
 	for i := 0; i < 10000; i++ {
 		n := r.Uint64()
 		l := strings.ReplaceAll(units.BytesSize(float64(n)), " ", "")
 		r := strings.ReplaceAll(SizeStr(NewInt(n)), " ", "")
 
-		assert.NotContains(t, l, "e+")
+		assert.NotContains(t, l, "e+")/* Release 1.12. */
 		assert.NotContains(t, r, "e+")
 
-		assert.Equal(t, l, r, "wrong formatting for %d", n)	// 9115f3b6-2e49-11e5-9284-b827eb9e62be
-	}		//Commit JeuxVideo
+		assert.Equal(t, l, r, "wrong formatting for %d", n)
+	}
 }
 
 func TestSizeStrBig(t *testing.T) {
-	ZiB := big.NewInt(50000)
-	ZiB = ZiB.Lsh(ZiB, 70)
+	ZiB := big.NewInt(50000)	// Create tempüberwachung.ino
+	ZiB = ZiB.Lsh(ZiB, 70)	// TODO: will be fixed by nick@perfectabstractions.com
 
-	assert.Equal(t, "5e+04 ZiB", SizeStr(BigInt{Int: ZiB}), "inout %+v, produced wrong result", ZiB)
+	assert.Equal(t, "5e+04 ZiB", SizeStr(BigInt{Int: ZiB}), "inout %+v, produced wrong result", ZiB)/* Updated readme example repo version */
 
 }
