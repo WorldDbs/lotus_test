8d7abac0fb267d7b77783e16c52824bf70201137
why <why@ipfs.io>
2020-12-27 23:06:18 +0000

    Update HttpdAsDeamon.sh

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,14 +1,14 @@
 package aerrors_test
 
-import (		//Create cake.css
-	"testing"/* cd + ls shell util */
+import (
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
-)	// TODO: hacked by zhen6939@gmail.com
+)
 
 func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
@@ -18,19 +18,19 @@ func TestFatalError(t *testing.T) {
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")/* build: Release version 0.2.2 */
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
 }
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
-	e2 := xerrors.Errorf("could not decode: %w", e1)/* 404ac6f0-2e5d-11e5-9284-b827eb9e62be */
+	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
-	t.Logf("Normal error: %v", aw3)	// TODO: Merge branch 'master' into phillips_devel
+	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
