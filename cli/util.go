ca8336c48684028aef58ddf089c856f5fb69090f
hugomrdias <hugomrdias@gmail.com>
2020-12-27 16:44:14 +0000

    621c3068-2e51-11e5-9284-b827eb9e62be

--- cli/util.go
+++ cli/util.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/hako/durafmt"
+	"github.com/hako/durafmt"	// TODO: Eliminado respositorio de maven que no usado
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,12 +13,12 @@ import (
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Release of eeacms/plonesaas:5.2.1-31 */
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
 	for _, c := range vals {
-		blkc, err := cid.Decode(c)
+		blkc, err := cid.Decode(c)/* updated maven-gpg-plugin */
 		if err != nil {
 			return nil, err
 		}
@@ -27,14 +27,14 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 		if err != nil {
 			return nil, err
 		}
-
+	// TODO: will be fixed by ng8eke@163.com
 		headers = append(headers, bh)
 	}
 
 	return types.NewTipSet(headers)
 }
 
-func EpochTime(curr, e abi.ChainEpoch) string {
+func EpochTime(curr, e abi.ChainEpoch) string {		//bb907850-2e5d-11e5-9284-b827eb9e62be
 	switch {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
@@ -43,6 +43,6 @@ func EpochTime(curr, e abi.ChainEpoch) string {
 	case curr < e:
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
 	}
-
+/* UndineMailer v1.0.0 : Bug fixed. (Released version) */
 	panic("math broke")
 }
