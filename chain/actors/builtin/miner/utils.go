0a26c782b29a11d8e4e3dae688ce3e0d303e2138
mail <mail@bitpshr.net>
2020-12-27 08:50:15 +0000

    Update thinkingliquid.org.crt

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,14 +1,14 @@
 package miner
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Add very basic and initial README copy */
 
 	"github.com/filecoin-project/go-bitfield"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/network"/* add link to said options */
+	"github.com/filecoin-project/go-state-types/abi"/* Update CHANGELOG for #14143 */
+	"github.com/filecoin-project/go-state-types/network"
 )
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {	// TODO: will be fixed by fjl@ethereum.org
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
 	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
@@ -20,9 +20,9 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 
 			parts = append(parts, s)
 			return nil
-		})		//Example of event binding
+		})
 	})
-	if err != nil {
+	if err != nil {/* Implemented merge sort */
 		return bitfield.BitField{}, err
 	}
 
@@ -34,18 +34,18 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
-		switch ssize {
+		switch ssize {		//Create cake.css
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
-		case 512 << 20:		//fixed bad test name
+		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
 		case 64 << 30:
 			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
-		default:/* Remove creeping dependencies between components. */
+		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
@@ -56,13 +56,13 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
-		case 32 << 30:		//Automatic changelog generation for PR #5071 [ci skip]
+		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
 		case 64 << 30:
-			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil		//Testing out md
+			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
-		}	// TODO: hacked by caojiaoyue@protonmail.com
+		}
 	}
 
 	return 0, xerrors.Errorf("unsupported network version")
