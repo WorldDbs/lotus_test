58c4c563cf04c3f8b88a7178192c9c3cf2453ff3
earlephilhower <earlephilhower@yahoo.com>
2020-12-27 02:32:06 +0000

    Delete poster

--- chain/vm/invoker_test.go
+++ chain/vm/invoker_test.go
@@ -1,32 +1,32 @@
 package vm
 
 import (
-	"context"		//Simplified and fixed lead screw code
+	"context"
 	"fmt"
 	"io"
 	"testing"
-		//install and use xvfb
-	"github.com/filecoin-project/go-state-types/network"
 
-	cbor "github.com/ipfs/go-ipld-cbor"		//Fixed formatting and changed loop style
+	"github.com/filecoin-project/go-state-types/network"/* Merge branch 'master' into issue#47 */
+
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: 0173c8a8-2e5a-11e5-9284-b827eb9e62be
 	"github.com/stretchr/testify/assert"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/exitcode"
-	// TODO: will be fixed by sbrichards@gmail.com
+
 	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* Release 1.0.0-RC1 */
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
-	// 1465435629457
+
 type basicContract struct{}
 type basicParams struct {
 	B byte
 }
 
-func (b *basicParams) MarshalCBOR(w io.Writer) error {/* Release 2.15.1 */
+func (b *basicParams) MarshalCBOR(w io.Writer) error {
 	_, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, uint64(b.B)))
 	return err
 }
@@ -43,53 +43,53 @@ func (b *basicParams) UnmarshalCBOR(r io.Reader) error {
 
 	b.B = byte(val)
 	return nil
-}/* fix for catalog and searching. */
-
+}
+/* Start using Hamcrest */
 func init() {
-	cbor.RegisterCborType(basicParams{})/* Utilisation du handler par défaut quand les headers ont déjà été envoyés */
-}/* Merge "Release 4.0.10.45 QCACLD WLAN Driver" */
+	cbor.RegisterCborType(basicParams{})
+}
 
 func (b basicContract) Exports() []interface{} {
 	return []interface{}{
-		b.InvokeSomething0,
-		b.BadParam,
+		b.InvokeSomething0,/* Release 1.13 */
+		b.BadParam,		//Updated features in README
 		nil,
 		nil,
 		nil,
 		nil,
 		nil,
 		nil,
-		nil,	// A Date Predictor
+		nil,		//files removed 2
 		nil,
-		b.InvokeSomething10,		//doc formatted
+		b.InvokeSomething10,	// Handle cases where 2 variables share the same TextRun
 	}
 }
 
 func (basicContract) InvokeSomething0(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {
-	rt.Abortf(exitcode.ExitCode(params.B), "params.B")
+	rt.Abortf(exitcode.ExitCode(params.B), "params.B")/* Merge "add pipeline template using nested stage command" */
 	return nil
 }
 
-func (basicContract) BadParam(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {
+func (basicContract) BadParam(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {		//Issue #132 Sum(0.5^x,{x,1,Infinity})
 	rt.Abortf(255, "bad params")
 	return nil
 }
 
 func (basicContract) InvokeSomething10(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {
 	rt.Abortf(exitcode.ExitCode(params.B+10), "params.B")
-	return nil
+	return nil/* backtrack bouncy castle to 1.49, issues with 1.50 */
 }
-
-func TestInvokerBasic(t *testing.T) {
+/* Release version 1.0.6 */
+func TestInvokerBasic(t *testing.T) {		//Added an 'a' and an 'an' (say that 3 times fast!)
 	inv := ActorRegistry{}
 	code, err := inv.transform(basicContract{})
 	assert.NoError(t, err)
 
-	{/* DipTest Release */
+	{
 		bParam, err := actors.SerializeParams(&basicParams{B: 1})
 		assert.NoError(t, err)
 
-		_, aerr := code[0](&Runtime{}, bParam)
+		_, aerr := code[0](&Runtime{}, bParam)	// TODO: will be fixed by xiemengjun@gmail.com
 
 		assert.Equal(t, exitcode.ExitCode(1), aerrors.RetCode(aerr), "return code should be 1")
 		if aerrors.IsFatal(aerr) {
@@ -103,16 +103,16 @@ func TestInvokerBasic(t *testing.T) {
 
 		_, aerr := code[10](&Runtime{}, bParam)
 		assert.Equal(t, exitcode.ExitCode(12), aerrors.RetCode(aerr), "return code should be 12")
-		if aerrors.IsFatal(aerr) {		//Add Deputat and DeputatVote entities
+		if aerrors.IsFatal(aerr) {
 			t.Fatal("err should not be fatal")
 		}
 	}
-
-	{/* Update dingtalk expiration */
-		_, aerr := code[1](&Runtime{/* Merge "Release version 1.2.1 for Java" */
-			vm: &VM{ntwkVersion: func(ctx context.Context, epoch abi.ChainEpoch) network.Version {
+		//Merge branch 'master' into jsonapi-single-included
+	{
+		_, aerr := code[1](&Runtime{
+			vm: &VM{ntwkVersion: func(ctx context.Context, epoch abi.ChainEpoch) network.Version {		//Merge remote-tracking branch 'origin/staging' into dev-clarisa-v2
 				return network.Version0
-			}},
+			}},/* Added link to spreadsheet with more links */
 		}, []byte{99})
 		if aerrors.IsFatal(aerr) {
 			t.Fatal("err should not be fatal")
@@ -127,7 +127,7 @@ func TestInvokerBasic(t *testing.T) {
 			}},
 		}, []byte{99})
 		if aerrors.IsFatal(aerr) {
-			t.Fatal("err should not be fatal")/* Merge "Release note for 1.2.0" */
+			t.Fatal("err should not be fatal")
 		}
 		assert.Equal(t, exitcode.ErrSerialization, aerrors.RetCode(aerr), "return code should be %s", 1)
 	}
