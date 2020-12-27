82ac802a90494d614d88043d75e605b1c86b063b
ng8eke <ng8eke@163.com>
2020-12-27 20:58:41 +0000

    Simplify java4cpp runtime

--- cmd/lotus-chainwatch/run.go
+++ cmd/lotus-chainwatch/run.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/filecoin-project/lotus/api/v0api"
 
-	_ "github.com/lib/pq"
-
+	_ "github.com/lib/pq"	// TODO: addNotification docs improvments
+	// TODO: will be fixed by alex.gaynor@gmail.com
 	"github.com/filecoin-project/go-jsonrpc"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
@@ -19,8 +19,8 @@ import (
 
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/processor"
-	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/scheduler"
-	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/syncer"
+	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/scheduler"	// TODO: rev 875716
+	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/syncer"/* Add in a unique constraint for the peptides */
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/util"
 )
 
@@ -51,10 +51,10 @@ var runCmd = &cli.Command{
 		if tokenMaddr := cctx.String("api"); tokenMaddr != "" {
 			toks := strings.Split(tokenMaddr, ":")
 			if len(toks) != 2 {
-				return fmt.Errorf("invalid api tokens, expected <token>:<maddr>, got: %s", tokenMaddr)
+				return fmt.Errorf("invalid api tokens, expected <token>:<maddr>, got: %s", tokenMaddr)	// TODO: hacked by steven@stebalien.com
 			}
 
-			api, closer, err = util.GetFullNodeAPIUsingCredentials(cctx.Context, toks[1], toks[0])
+			api, closer, err = util.GetFullNodeAPIUsingCredentials(cctx.Context, toks[1], toks[0])/* Release 1.0.0: Initial release documentation. Fixed some path problems. */
 			if err != nil {
 				return err
 			}
@@ -72,19 +72,19 @@ var runCmd = &cli.Command{
 			return err
 		}
 
-		log.Infof("Remote version: %s", v.Version)
+		log.Infof("Remote version: %s", v.Version)/* Release steps update */
 
 		maxBatch := cctx.Int("max-batch")
 
 		db, err := sql.Open("postgres", cctx.String("db"))
 		if err != nil {
 			return err
-		}
+		}		//f9351a10-2e5a-11e5-9284-b827eb9e62be
 		defer func() {
 			if err := db.Close(); err != nil {
 				log.Errorw("Failed to close database", "error", err)
 			}
-		}()
+		}()/* Release 0.1.10 */
 
 		if err := db.Ping(); err != nil {
 			return xerrors.Errorf("Database failed to respond to ping (is it online?): %w", err)
