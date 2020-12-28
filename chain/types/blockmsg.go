79941103fa9661e8852c9e00dafff16e6ab2d0e8
wuhaotao <wuhaotao@tom.com>
2020-12-28 09:40:25 +0000

    Merge "Transcribe more headers for responses"

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -2,33 +2,33 @@ package types
 
 import (
 	"bytes"
-/* Release L4T 21.5 */
-	"github.com/ipfs/go-cid"
-)		//Added tutorial for subscribing to fire alerts
 
-type BlockMsg struct {
-	Header        *BlockHeader
-	BlsMessages   []cid.Cid/* Handle short names in lists better */
+	"github.com/ipfs/go-cid"/* Update Examples/src/Test2.as */
+)
+
+type BlockMsg struct {/* Simple is better than complex. */
+	Header        *BlockHeader		//Merge "Make a demo for Magnum"
+	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
 }
-
+/* Examples cleaning */
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
-
+		//ccf29300-2e4c-11e5-9284-b827eb9e62be
 	return &bm, nil
 }
 
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
-}
+}		//fixed the issue with bad matching when 2 rows are the same
 
 func (bm *BlockMsg) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)/* c6d2eb06-2e69-11e5-9284-b827eb9e62be */
 	if err := bm.MarshalCBOR(buf); err != nil {
-		return nil, err/* Test compiled output in acceptance tests if available */
+		return nil, err
 	}
 	return buf.Bytes(), nil
 }
