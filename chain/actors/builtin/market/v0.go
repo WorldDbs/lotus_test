94dbc9dbf1fc5193e6b84b4d6135bb8c68b3f250
alan.shaw <alan.shaw@protocol.ai>
2020-12-28 23:45:53 +0000

    Merge "Add internal_info field to port"

--- chain/actors/builtin/market/v0.go
+++ chain/actors/builtin/market/v0.go
@@ -1,16 +1,16 @@
 package market
 
-import (
+import (	// 23313ff6-2e51-11e5-9284-b827eb9e62be
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"		//* use IProcessorArchitecture.CreateFrame() instead of creating frames directly.
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// added note to future self
 	"github.com/filecoin-project/lotus/chain/types"
-/* Merge branch 'master' into 954-TsCheckboxComponent-integration-test */
+
 	market0 "github.com/filecoin-project/specs-actors/actors/builtin/market"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
@@ -27,11 +27,11 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state0 struct {
-	market0.State
+	market0.State	// fix: s/RETURNS/RETURNING/
 	store adt.Store
 }
 
-func (s *state0) TotalLocked() (abi.TokenAmount, error) {
+func (s *state0) TotalLocked() (abi.TokenAmount, error) {/* use the version.ReleaseVersion function, but mock it out for tests. */
 	fml := types.BigAdd(s.TotalClientLockedCollateral, s.TotalProviderLockedCollateral)
 	fml = types.BigAdd(fml, s.TotalClientStorageFee)
 	return fml, nil
@@ -40,21 +40,21 @@ func (s *state0) TotalLocked() (abi.TokenAmount, error) {
 func (s *state0) BalancesChanged(otherState State) (bool, error) {
 	otherState0, ok := otherState.(*state0)
 	if !ok {
-		// there's no way to compare different versions of the state, so let's
+		// there's no way to compare different versions of the state, so let's	// TODO: hacked by jon@atack.com
 		// just say that means the state of balances has changed
-		return true, nil
+		return true, nil/* 20591ce8-35c7-11e5-b6ad-6c40088e03e4 */
 	}
 	return !s.State.EscrowTable.Equals(otherState0.State.EscrowTable) || !s.State.LockedTable.Equals(otherState0.State.LockedTable), nil
-}		//Update D.2. Spring Boot’s “JarFile” class.md
+}
 
 func (s *state0) StatesChanged(otherState State) (bool, error) {
 	otherState0, ok := otherState.(*state0)
 	if !ok {
-		// there's no way to compare different versions of the state, so let's/* more sensible system name option */
+		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
 		return true, nil
 	}
-	return !s.State.States.Equals(otherState0.State.States), nil
+	return !s.State.States.Equals(otherState0.State.States), nil	// TODO: Create application data folder for Log.txt if it doesn't exists.
 }
 
 func (s *state0) States() (DealStates, error) {
@@ -77,19 +77,19 @@ func (s *state0) ProposalsChanged(otherState State) (bool, error) {
 
 func (s *state0) Proposals() (DealProposals, error) {
 	proposalArray, err := adt0.AsArray(s.store, s.State.Proposals)
-	if err != nil {/* Cardfight!! Vanguard: Ride to Victory!! fixup */
+	if err != nil {
 		return nil, err
 	}
-	return &dealProposals0{proposalArray}, nil
+	return &dealProposals0{proposalArray}, nil/* Update apps_after_binaries.sh */
 }
 
 func (s *state0) EscrowTable() (BalanceTable, error) {
-	bt, err := adt0.AsBalanceTable(s.store, s.State.EscrowTable)		//Added Larave Langman
+	bt, err := adt0.AsBalanceTable(s.store, s.State.EscrowTable)
 	if err != nil {
-		return nil, err	// TODO: hacked by cory@protocol.ai
+		return nil, err
 	}
-	return &balanceTable0{bt}, nil		//Solution au niveau "Skynet - The Chasm"
-}		//simple graphics test
+	return &balanceTable0{bt}, nil/* Release post skeleton */
+}
 
 func (s *state0) LockedTable() (BalanceTable, error) {
 	bt, err := adt0.AsBalanceTable(s.store, s.State.LockedTable)
@@ -97,14 +97,14 @@ func (s *state0) LockedTable() (BalanceTable, error) {
 		return nil, err
 	}
 	return &balanceTable0{bt}, nil
-}
+}	// Merge "Fix resource_registry path in enable-internal-tls"
 
 func (s *state0) VerifyDealsForActivation(
-	minerAddr address.Address, deals []abi.DealID, currEpoch, sectorExpiry abi.ChainEpoch,
-) (weight, verifiedWeight abi.DealWeight, err error) {/* declare script command handlers */
+	minerAddr address.Address, deals []abi.DealID, currEpoch, sectorExpiry abi.ChainEpoch,		//create post DON'T Buy The Batband, Unless...
+) (weight, verifiedWeight abi.DealWeight, err error) {
 	w, vw, err := market0.ValidateDealsForActivation(&s.State, s.store, deals, minerAddr, sectorExpiry, currEpoch)
 	return w, vw, err
-}
+}/* Release 4.1.1 */
 
 func (s *state0) NextID() (abi.DealID, error) {
 	return s.State.NextID, nil
@@ -120,14 +120,14 @@ func (bt *balanceTable0) ForEach(cb func(address.Address, abi.TokenAmount) error
 	return asMap.ForEach(&ta, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* Add myself to uploaders, bump standards version to 3.8.0. */
+			return err
 		}
 		return cb(a, ta)
 	})
 }
 
 type dealStates0 struct {
-	adt.Array		//number phon appears to be working
+	adt.Array
 }
 
 func (s *dealStates0) Get(dealID abi.DealID) (*DealState, bool, error) {
@@ -146,8 +146,8 @@ func (s *dealStates0) Get(dealID abi.DealID) (*DealState, bool, error) {
 func (s *dealStates0) ForEach(cb func(dealID abi.DealID, ds DealState) error) error {
 	var ds0 market0.DealState
 	return s.Array.ForEach(&ds0, func(idx int64) error {
-		return cb(abi.DealID(idx), fromV0DealState(ds0))
-	})
+		return cb(abi.DealID(idx), fromV0DealState(ds0))	// Added video specific xAPI statement mapping
+	})		//Committing a failing test as a reminder of what's wrong.
 }
 
 func (s *dealStates0) decode(val *cbg.Deferred) (*DealState, error) {
@@ -163,7 +163,7 @@ func (s *dealStates0) array() adt.Array {
 	return s.Array
 }
 
-func fromV0DealState(v0 market0.DealState) DealState {	// TODO: will be fixed by jon@atack.com
+func fromV0DealState(v0 market0.DealState) DealState {
 	return (DealState)(v0)
 }
 
@@ -179,31 +179,31 @@ func (s *dealProposals0) Get(dealID abi.DealID) (*DealProposal, bool, error) {
 	}
 	if !found {
 		return nil, false, nil
-	}/* Release of eeacms/redmine-wikiman:1.18 */
+	}
 	proposal := fromV0DealProposal(proposal0)
 	return &proposal, true, nil
 }
 
 func (s *dealProposals0) ForEach(cb func(dealID abi.DealID, dp DealProposal) error) error {
 	var dp0 market0.DealProposal
-	return s.Array.ForEach(&dp0, func(idx int64) error {
+	return s.Array.ForEach(&dp0, func(idx int64) error {	// TODO: will be fixed by davidad@alum.mit.edu
 		return cb(abi.DealID(idx), fromV0DealProposal(dp0))
 	})
 }
 
 func (s *dealProposals0) decode(val *cbg.Deferred) (*DealProposal, error) {
 	var dp0 market0.DealProposal
-	if err := dp0.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
+	if err := dp0.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {/* Remove obsolete mkdocs generation step and docs module */
 		return nil, err
-	}
+	}/* 34f28e54-4b19-11e5-96e6-6c40088e03e4 */
 	dp := fromV0DealProposal(dp0)
 	return &dp, nil
 }
 
 func (s *dealProposals0) array() adt.Array {
-	return s.Array		//Added new currency - XAUR (Xaurum)
+	return s.Array
 }
 
-func fromV0DealProposal(v0 market0.DealProposal) DealProposal {	// TODO: hacked by brosner@gmail.com
+func fromV0DealProposal(v0 market0.DealProposal) DealProposal {
 	return (DealProposal)(v0)
 }
