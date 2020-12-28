1445f5cf24c543af00cc295ff6c8dcdd135059e8
m-ou.se <m-ou.se@m-ou.se>
2020-12-28 07:51:53 +0000

    (getloadavg): Add OSF_ALPHA support.

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
 package journal
 
-type nilJournal struct{}	// TODO: hacked by alex.gaynor@gmail.com
+type nilJournal struct{}
 
-// nilj is a singleton nil journal./* Merge "Removed duplicated class in exception.py" */
+// nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
-
-func NilJournal() Journal {		//Reduce probability of fragmented file (useless with tmpfs)
+		//[AI-361] FIXED filtering on multiple attributes
+func NilJournal() Journal {
 	return nilj
-}
+}/* Running stuff in lz-dev. */
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
-/* Release version 0.1.5 */
+
 func (n *nilJournal) Close() error { return nil }
