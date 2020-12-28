6702bf765570f0923651d005399eedae01151246
ligi <ligi@ligi.de>
2020-12-28 11:28:47 +0000

    fix ffmpeg vaapi

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -3,27 +3,27 @@ package storage
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Add script to restart bonjour
 
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/types"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* bc1ab874-2e69-11e5-9284-b827eb9e62be */
 )
-
+	// TODO: will be fixed by steven@stebalien.com
 var _ sealing.Events = new(EventsAdapter)
 
 type EventsAdapter struct {
 	delegate *events.Events
-}
-		//Delete Leaflet_Example-master.zip
-func NewEventsAdapter(api *events.Events) EventsAdapter {		//predict example corrected
+}/* Update localhost.json */
+
+func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
-}/* Show information on video. */
+}
 
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
 		return rev(ctx, ts.Key().Bytes())
-	}, confidence, h)		//Merge branch 'development' into feature/string_quiz
+	}, confidence, h)
 }
