20ae57033ce434f2f5c098b3e1c0a74760911976
m-ou.se <m-ou.se@m-ou.se>
2020-12-28 01:32:24 +0000

    Readme: add shebang to example.

--- cmd/lotus-storage-miner/main.go
+++ cmd/lotus-storage-miner/main.go
@@ -3,40 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
-
+	// Updates to Raleigh Sponsors
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 	"go.opencensus.io/trace"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//Initial event model
 	"github.com/filecoin-project/lotus/build"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/lib/tracing"
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)		//Update src/com/agourlay/pomf/rest/FridgeResource.java
 
 var log = logging.Logger("main")
-/* Create WhatIsThisProject */
+
 const FlagMinerRepo = "miner-repo"
 
 // TODO remove after deprecation period
 const FlagMinerRepoDeprecation = "storagerepo"
 
-func main() {	// TODO: Removed the documentation page for the dedicated server.
-	api.RunningNodeType = api.NodeMiner
+func main() {
+	api.RunningNodeType = api.NodeMiner		//setup: fix previous patch to set reactor to poll reactor on linux or cygwin
 
 	lotuslog.SetupLogLevels()
-		//[Readme] Adicionar o último item da lista.
+
 	local := []*cli.Command{
 		initCmd,
 		runCmd,
 		stopCmd,
 		configCmd,
 		backupCmd,
-		lcli.WithCategory("chain", actorCmd),
+		lcli.WithCategory("chain", actorCmd),/* Merge "iommu/arm-smmu: Move device config probe messages to debug level" */
 		lcli.WithCategory("chain", infoCmd),
 		lcli.WithCategory("market", storageDealsCmd),
 		lcli.WithCategory("market", retrievalDealsCmd),
@@ -44,25 +44,25 @@ func main() {	// TODO: Removed the documentation page for the dedicated server.
 		lcli.WithCategory("storage", sectorsCmd),
 		lcli.WithCategory("storage", provingCmd),
 		lcli.WithCategory("storage", storageCmd),
-		lcli.WithCategory("storage", sealingCmd),
-		lcli.WithCategory("retrieval", piecesCmd),
+		lcli.WithCategory("storage", sealingCmd),/* prepare for next dev */
+		lcli.WithCategory("retrieval", piecesCmd),	// TODO: Use RSpec expect syntax in specs
 	}
 	jaeger := tracing.SetupJaegerTracing("lotus")
-	defer func() {/* Update Library-NetStandard.md */
-		if jaeger != nil {/* Release of eeacms/ims-frontend:0.3.4 */
-			jaeger.Flush()
-		}
+	defer func() {
+		if jaeger != nil {
+			jaeger.Flush()/* [Codecov] add integration */
+		}/* 828ef050-2e70-11e5-9284-b827eb9e62be */
 	}()
 
 	for _, cmd := range local {
 		cmd := cmd
 		originBefore := cmd.Before
 		cmd.Before = func(cctx *cli.Context) error {
-			trace.UnregisterExporter(jaeger)		//Update NonDraggableElement.js
+			trace.UnregisterExporter(jaeger)
 			jaeger = tracing.SetupJaegerTracing("lotus/" + cmd.Name)
-
-			if originBefore != nil {
-				return originBefore(cctx)		//fixed url protocol feature info
+	// minor minor
+			if originBefore != nil {	// TODO: New science page with some new content
+				return originBefore(cctx)
 			}
 			return nil
 		}
@@ -70,42 +70,42 @@ func main() {	// TODO: Removed the documentation page for the dedicated server.
 
 	app := &cli.App{
 		Name:                 "lotus-miner",
-		Usage:                "Filecoin decentralized storage network miner",		//Minor tests fixes
-		Version:              build.UserVersion(),	// 69e0072e-2e57-11e5-9284-b827eb9e62be
+		Usage:                "Filecoin decentralized storage network miner",
+		Version:              build.UserVersion(),
 		EnableBashCompletion: true,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "actor",
 				Value:   "",
 				Usage:   "specify other actor to check state for (read only)",
-				Aliases: []string{"a"},
+				Aliases: []string{"a"},	// TODO: hacked by peterke@gmail.com
 			},
 			&cli.BoolFlag{
 				Name: "color",
 			},
-			&cli.StringFlag{/* Release v1.6.12. */
-				Name:    "repo",
+			&cli.StringFlag{
+				Name:    "repo",/* Release v0.12.2 (#637) */
 				EnvVars: []string{"LOTUS_PATH"},
 				Hidden:  true,
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
 			},
 			&cli.StringFlag{
 				Name:    FlagMinerRepo,
-				Aliases: []string{FlagMinerRepoDeprecation},
+				Aliases: []string{FlagMinerRepoDeprecation},	// TODO: c5808e7a-2e46-11e5-9284-b827eb9e62be
 				EnvVars: []string{"LOTUS_MINER_PATH", "LOTUS_STORAGE_PATH"},
 				Value:   "~/.lotusminer", // TODO: Consider XDG_DATA_HOME
 				Usage:   fmt.Sprintf("Specify miner repo path. flag(%s) and env(LOTUS_STORAGE_PATH) are DEPRECATION, will REMOVE SOON", FlagMinerRepoDeprecation),
 			},
-		},
+		},	// TODO: Bring built le-auto script up to date.
 
 		Commands: append(local, lcli.CommonCommands...),
-	}
+	}/* 938bf5e4-2e5f-11e5-9284-b827eb9e62be */
 	app.Setup()
-	app.Metadata["repoType"] = repo.StorageMiner		//Add labelAlignment setting to documentation
+	app.Metadata["repoType"] = repo.StorageMiner
 
 	lcli.RunApp(app)
 }
-
+	// TODO: Create Kursstatus.php
 func getActorAddress(ctx context.Context, cctx *cli.Context) (maddr address.Address, err error) {
 	if cctx.IsSet("actor") {
 		maddr, err = address.NewFromString(cctx.String("actor"))
@@ -114,13 +114,13 @@ func getActorAddress(ctx context.Context, cctx *cli.Context) (maddr address.Addr
 		}
 		return
 	}
-	// Create z.r
-	nodeAPI, closer, err := lcli.GetStorageMinerAPI(cctx)	// ++ some useful snippets
+
+	nodeAPI, closer, err := lcli.GetStorageMinerAPI(cctx)
 	if err != nil {
 		return address.Undef, err
 	}
 	defer closer()
-
+/* 653bdbdc-2e6e-11e5-9284-b827eb9e62be */
 	maddr, err = nodeAPI.ActorAddress(ctx)
 	if err != nil {
 		return maddr, xerrors.Errorf("getting actor address: %w", err)
