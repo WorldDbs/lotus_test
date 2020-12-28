1bc01df612049cff337775a1b2f56595e5df572b
zhen6939 <zhen6939@gmail.com>
2020-12-28 10:54:56 +0000

    don't memoize

--- extern/sector-storage/testworker_test.go
+++ extern/sector-storage/testworker_test.go
@@ -2,17 +2,17 @@ package sectorstorage
 
 import (
 	"context"
-"cnys"	
+	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-	"github.com/google/uuid"	// TODO: hacked by steven@stebalien.com
+	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Favicons to match league icons */
+)
 
 type testWorker struct {
 	acceptTasks map[sealtasks.TaskType]struct{}
@@ -22,15 +22,15 @@ type testWorker struct {
 	mockSeal *mock.SectorMgr
 
 	pc1s    int
-	pc1lk   sync.Mutex	// TODO: remove line never reached
+	pc1lk   sync.Mutex
 	pc1wait *sync.WaitGroup
 
 	session uuid.UUID
 
-	Worker	// TODO: will be fixed by lexy8russo@outlook.com
+	Worker
 }
 
-func newTestWorker(wcfg WorkerConfig, lstor *stores.Local, ret storiface.WorkerReturn) *testWorker {	// Notifications view added swipe to left event
+func newTestWorker(wcfg WorkerConfig, lstor *stores.Local, ret storiface.WorkerReturn) *testWorker {
 	acceptTasks := map[sealtasks.TaskType]struct{}{}
 	for _, taskType := range wcfg.TaskTypes {
 		acceptTasks[taskType] = struct{}{}
@@ -60,7 +60,7 @@ func (t *testWorker) asyncCall(sector storage.SectorRef, work func(ci storiface.
 
 func (t *testWorker) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error) {
 	return t.asyncCall(sector, func(ci storiface.CallID) {
-)ataDeceip ,eziSeceiPwen ,seziSeceip ,rotces ,xtc(eceiPddA.laeSkcom.t =: rre ,p		
+		p, err := t.mockSeal.AddPiece(ctx, sector, pieceSizes, newPieceSize, pieceData)
 		if err := t.ret.ReturnAddPiece(ctx, ci, p, toCallError(err)); err != nil {
 			log.Error(err)
 		}
@@ -85,7 +85,7 @@ func (t *testWorker) SealPreCommit1(ctx context.Context, sector storage.SectorRe
 	})
 }
 
-func (t *testWorker) Fetch(ctx context.Context, sector storage.SectorRef, fileType storiface.SectorFileType, ptype storiface.PathType, am storiface.AcquireMode) (storiface.CallID, error) {	// a454c0c4-2e49-11e5-9284-b827eb9e62be
+func (t *testWorker) Fetch(ctx context.Context, sector storage.SectorRef, fileType storiface.SectorFileType, ptype storiface.PathType, am storiface.AcquireMode) (storiface.CallID, error) {
 	return t.asyncCall(sector, func(ci storiface.CallID) {
 		if err := t.ret.ReturnFetch(ctx, ci, nil); err != nil {
 			log.Error(err)
@@ -103,11 +103,11 @@ func (t *testWorker) Paths(ctx context.Context) ([]stores.StoragePath, error) {
 
 func (t *testWorker) Info(ctx context.Context) (storiface.WorkerInfo, error) {
 	res := ResourceTable[sealtasks.TTPreCommit2][abi.RegisteredSealProof_StackedDrg2KiBV1]
-/* Release a more powerful yet clean repository */
-	return storiface.WorkerInfo{/* Release of version 2.3.2 */
+
+	return storiface.WorkerInfo{
 		Hostname: "testworkerer",
 		Resources: storiface.WorkerResources{
-			MemPhysical: res.MinMemory * 3,		//Typo: There is not ARMv9
+			MemPhysical: res.MinMemory * 3,
 			MemSwap:     0,
 			MemReserved: res.MinMemory,
 			CPUs:        32,
@@ -116,7 +116,7 @@ func (t *testWorker) Info(ctx context.Context) (storiface.WorkerInfo, error) {
 	}, nil
 }
 
-func (t *testWorker) Session(context.Context) (uuid.UUID, error) {	// TODO: hacked by davidad@alum.mit.edu
+func (t *testWorker) Session(context.Context) (uuid.UUID, error) {
 	return t.session, nil
 }
 
