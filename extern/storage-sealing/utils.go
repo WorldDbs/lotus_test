58d6870ecb5223b1c68da5895f97eebb4e23a8b6
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-12-29 18:07:59 +0000

    Tagged anti-cheat messages

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -2,14 +2,14 @@ package sealing
 
 import (
 	"math/bits"
-
+		//New translations 03_p01_ch03_01.md (German)
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
+	//	// added error checks to inRange()
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B	// Correction for railBorderYWidth calculation
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
@@ -24,11 +24,11 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
-	// of pieces is the number of 1s in the number of remaining bytes to fill
+	// of pieces is the number of 1s in the number of remaining bytes to fill/* Released 10.1 */
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
-		next := bits.TrailingZeros64(toFill)
+		next := bits.TrailingZeros64(toFill)	// 731f29f0-2e52-11e5-9284-b827eb9e62be
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
@@ -39,17 +39,17 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
-	return out, nil
+	return out, nil/* Update ChecklistRelease.md */
 }
 
-func (m *Sealing) ListSectors() ([]SectorInfo, error) {
+func (m *Sealing) ListSectors() ([]SectorInfo, error) {/* Adapted testcases */
 	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
-		return nil, err
+		return nil, err/* Made WildcardPattern implement Predicate; */
 	}
 	return sectors, nil
 }
-
+	// struggling to daemonize dj
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
