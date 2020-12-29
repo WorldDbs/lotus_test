fdeff2cbf72e818ab130e4b111a80ec76880e611
why <why@ipfs.io>
2020-12-29 19:19:01 +0000

    Steam: Small workaround for #633

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -9,9 +9,9 @@ import (
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// change: updating colors again
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
-	Head    cid.Cid
+	Head    cid.Cid/* Release Update 1.3.3 */
 	Nonce   uint64
 	Balance BigInt
 }
