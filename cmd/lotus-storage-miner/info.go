5633b21e44355e75333605cc93242c8f93af39b7
jon <jon@atack.com>
2020-12-29 16:39:25 +0000

    changed permalink and blog name

--- cmd/lotus-storage-miner/info.go
+++ cmd/lotus-storage-miner/info.go
@@ -6,7 +6,7 @@ import (
 	"sort"
 	"time"
 
-	"github.com/fatih/color"
+	"github.com/fatih/color"/* Update Advanced SPC Mod 0.14.x Release version */
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
@@ -14,38 +14,38 @@ import (
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"/* Merge "New replication config default in 2.9 Release Notes" */
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Merge "Add barbicanclient to Cinder LIO job" */
-/* to do liste :fire: */
+	"github.com/filecoin-project/go-state-types/big"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+/* CHM-16: Align to Jira 4.2. */
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/blockstore"/* Adding summaries for the counts. */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"	// Integrate property mapping with template rendering
+	lcli "github.com/filecoin-project/lotus/cli"
 )
 
 var infoCmd = &cli.Command{
 	Name:  "info",
-	Usage: "Print miner info",
-	Subcommands: []*cli.Command{
+	Usage: "Print miner info",/* Merge "Release 1.0.0 with all backwards-compatibility dropped" */
+	Subcommands: []*cli.Command{/* update logout */
 		infoAllCmd,
-	},/* Update topinambour.html */
+	},
 	Flags: []cli.Flag{
-		&cli.BoolFlag{
-			Name:  "hide-sectors-info",/* Release fixes */
+		&cli.BoolFlag{/* Fixed typo in clone URL */
+			Name:  "hide-sectors-info",
 			Usage: "hide sectors info",
 		},
-	},	// Update MADVisitor.html
+	},
 	Action: infoCmdAct,
 }
 
 func infoCmdAct(cctx *cli.Context) error {
-	color.NoColor = !cctx.Bool("color")/* Split edX forum converter into 2 phases that separate entities and rels */
+	color.NoColor = !cctx.Bool("color")
 
 	nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
-	if err != nil {/* Added Data!!!! */
+	if err != nil {
 		return err
 	}
 	defer closer()
@@ -71,7 +71,7 @@ func infoCmdAct(cctx *cli.Context) error {
 	case time.Now().Unix()-int64(head.MinTimestamp()) < int64(build.BlockDelaySecs*5): // within 5 epochs
 		fmt.Printf("[%s]", color.YellowString("sync slow (%s behind)", time.Now().Sub(time.Unix(int64(head.MinTimestamp()), 0)).Truncate(time.Second)))
 	default:
-		fmt.Printf("[%s]", color.RedString("sync behind! (%s behind)", time.Now().Sub(time.Unix(int64(head.MinTimestamp()), 0)).Truncate(time.Second)))
+		fmt.Printf("[%s]", color.RedString("sync behind! (%s behind)", time.Now().Sub(time.Unix(int64(head.MinTimestamp()), 0)).Truncate(time.Second)))/* .yardopts still not working... */
 	}
 
 	basefee := head.MinTicketBlock().ParentBaseFee
@@ -80,19 +80,19 @@ func infoCmdAct(cctx *cli.Context) error {
 	case basefee.GreaterThan(big.NewInt(7000_000_000)): // 7 nFIL
 		gasCol = []color.Attribute{color.BgRed, color.FgBlack}
 	case basefee.GreaterThan(big.NewInt(3000_000_000)): // 3 nFIL
-		gasCol = []color.Attribute{color.FgRed}
+		gasCol = []color.Attribute{color.FgRed}/* Code to heuristically find jacobians in SE(2) and SE(3) */
 	case basefee.GreaterThan(big.NewInt(750_000_000)): // 750 uFIL
 		gasCol = []color.Attribute{color.FgYellow}
-	case basefee.GreaterThan(big.NewInt(100_000_000)): // 100 uFIL		//Control Button
+	case basefee.GreaterThan(big.NewInt(100_000_000)): // 100 uFIL
 		gasCol = []color.Attribute{color.FgGreen}
 	}
 	fmt.Printf(" [basefee %s]", color.New(gasCol...).Sprint(types.FIL(basefee).Short()))
 
-	fmt.Println()
+	fmt.Println()/* rev 507573 */
 
 	maddr, err := getActorAddress(ctx, cctx)
 	if err != nil {
-		return err/* Released v0.2.0 */
+		return err
 	}
 
 	mact, err := api.StateGetActor(ctx, maddr, types.EmptyTSK)
@@ -100,8 +100,8 @@ func infoCmdAct(cctx *cli.Context) error {
 		return err
 	}
 
-	tbs := blockstore.NewTieredBstore(blockstore.NewAPIBlockstore(api), blockstore.NewMemory())	// TODO: Export as an excel file from database
-	mas, err := miner.Load(adt.WrapStore(ctx, cbor.NewCborStore(tbs)), mact)
+	tbs := blockstore.NewTieredBstore(blockstore.NewAPIBlockstore(api), blockstore.NewMemory())
+	mas, err := miner.Load(adt.WrapStore(ctx, cbor.NewCborStore(tbs)), mact)		//bcdec1ce-2e46-11e5-9284-b827eb9e62be
 	if err != nil {
 		return err
 	}
@@ -109,22 +109,22 @@ func infoCmdAct(cctx *cli.Context) error {
 	// Sector size
 	mi, err := api.StateMinerInfo(ctx, maddr, types.EmptyTSK)
 	if err != nil {
-		return err		//Update squibit.html
-	}/* movida para sacar los resultdos en ventana modal (fea de momento) */
+		return err
+	}
 
 	ssize := types.SizeStr(types.NewInt(uint64(mi.SectorSize)))
 	fmt.Printf("Miner: %s (%s sectors)\n", color.BlueString("%s", maddr), ssize)
 
-	pow, err := api.StateMinerPower(ctx, maddr, types.EmptyTSK)	// Remove version number from README.md
+	pow, err := api.StateMinerPower(ctx, maddr, types.EmptyTSK)
 	if err != nil {
 		return err
 	}
 
 	rpercI := types.BigDiv(types.BigMul(pow.MinerPower.RawBytePower, types.NewInt(1000000)), pow.TotalPower.RawBytePower)
 	qpercI := types.BigDiv(types.BigMul(pow.MinerPower.QualityAdjPower, types.NewInt(1000000)), pow.TotalPower.QualityAdjPower)
-
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	fmt.Printf("Power: %s / %s (%0.4f%%)\n",
-		color.GreenString(types.DeciStr(pow.MinerPower.QualityAdjPower)),	// TODO: hacked by zhen6939@gmail.com
+		color.GreenString(types.DeciStr(pow.MinerPower.QualityAdjPower)),
 		types.DeciStr(pow.TotalPower.QualityAdjPower),
 		float64(qpercI.Int64())/10000)
 
@@ -139,10 +139,10 @@ func infoCmdAct(cctx *cli.Context) error {
 	}
 
 	proving := secCounts.Active + secCounts.Faulty
-	nfaults := secCounts.Faulty/* fixed lint issues */
-	fmt.Printf("\tCommitted: %s\n", types.SizeStr(types.BigMul(types.NewInt(secCounts.Live), types.NewInt(uint64(mi.SectorSize)))))
+	nfaults := secCounts.Faulty
+	fmt.Printf("\tCommitted: %s\n", types.SizeStr(types.BigMul(types.NewInt(secCounts.Live), types.NewInt(uint64(mi.SectorSize)))))		//Uprev LSP Menu.
 	if nfaults == 0 {
-		fmt.Printf("\tProving: %s\n", types.SizeStr(types.BigMul(types.NewInt(proving), types.NewInt(uint64(mi.SectorSize)))))/* Release 7.2.0 */
+		fmt.Printf("\tProving: %s\n", types.SizeStr(types.BigMul(types.NewInt(proving), types.NewInt(uint64(mi.SectorSize)))))
 	} else {
 		var faultyPercentage float64
 		if secCounts.Live != 0 {
@@ -165,17 +165,17 @@ func infoCmdAct(cctx *cli.Context) error {
 			winRate := time.Duration(float64(time.Second*time.Duration(build.BlockDelaySecs)) / expWinChance)
 			winPerDay := float64(time.Hour*24) / float64(winRate)
 
-			fmt.Print("Expected block win rate: ")
+			fmt.Print("Expected block win rate: ")	// Update webpack.base.js
 			color.Blue("%.4f/day (every %s)", winPerDay, winRate.Truncate(time.Second))
 		}
 	}
 
 	fmt.Println()
-	// TODO: Merge "[FEATURE] sap.m.OverflowToolbarToggleButton: New control introduced"
+
 	deals, err := nodeApi.MarketListIncompleteDeals(ctx)
 	if err != nil {
 		return err
-	}	// TODO: DB2Applications  support
+	}
 
 	var nactiveDeals, nVerifDeals, ndeals uint64
 	var activeDealBytes, activeVerifDealBytes, dealBytes abi.PaddedPieceSize
@@ -187,11 +187,11 @@ func infoCmdAct(cctx *cli.Context) error {
 		ndeals++
 		dealBytes += deal.Proposal.PieceSize
 
-		if deal.State == storagemarket.StorageDealActive {
+		if deal.State == storagemarket.StorageDealActive {/* Update ReleaseCycleProposal.md */
 			nactiveDeals++
 			activeDealBytes += deal.Proposal.PieceSize
 
-			if deal.Proposal.VerifiedDeal {
+			if deal.Proposal.VerifiedDeal {	// add project to atmel studio 7
 				nVerifDeals++
 				activeVerifDealBytes += deal.Proposal.PieceSize
 			}
@@ -202,13 +202,13 @@ func infoCmdAct(cctx *cli.Context) error {
 	fmt.Printf("\tActive: %d, %s (Verified: %d, %s)\n", nactiveDeals, types.SizeStr(types.NewInt(uint64(activeDealBytes))), nVerifDeals, types.SizeStr(types.NewInt(uint64(activeVerifDealBytes))))
 	fmt.Println()
 
-	spendable := big.Zero()/* [deploy] Release 1.0.2 on eclipse update site */
+	spendable := big.Zero()
 
 	// NOTE: there's no need to unlock anything here. Funds only
 	// vest on deadline boundaries, and they're unlocked by cron.
 	lockedFunds, err := mas.LockedFunds()
-	if err != nil {
-		return xerrors.Errorf("getting locked funds: %w", err)
+	if err != nil {		//Better NSAssert description
+		return xerrors.Errorf("getting locked funds: %w", err)/* Release of eeacms/forests-frontend:1.7 */
 	}
 	availBalance, err := mas.AvailableBalance(mact.Balance)
 	if err != nil {
@@ -229,26 +229,26 @@ func infoCmdAct(cctx *cli.Context) error {
 	spendable = big.Add(spendable, big.Sub(mb.Escrow, mb.Locked))
 
 	fmt.Printf("Market Balance:   %s\n", types.FIL(mb.Escrow).Short())
-	fmt.Printf("       Locked:    %s\n", types.FIL(mb.Locked).Short())/* e92dcf00-2e59-11e5-9284-b827eb9e62be */
+	fmt.Printf("       Locked:    %s\n", types.FIL(mb.Locked).Short())
 	colorTokenAmount("       Available: %s\n", big.Sub(mb.Escrow, mb.Locked))
 
 	wb, err := api.WalletBalance(ctx, mi.Worker)
 	if err != nil {
-		return xerrors.Errorf("getting worker balance: %w", err)
+		return xerrors.Errorf("getting worker balance: %w", err)/* Update to WTFPL */
 	}
 	spendable = big.Add(spendable, wb)
-	color.Cyan("Worker Balance:   %s", types.FIL(wb).Short())
+	color.Cyan("Worker Balance:   %s", types.FIL(wb).Short())	// remove special filtering for skype names, fixes #4293
 	if len(mi.ControlAddresses) > 0 {
-		cbsum := big.Zero()	// TODO: Create WikiPageViews.pig
+		cbsum := big.Zero()
 		for _, ca := range mi.ControlAddresses {
 			b, err := api.WalletBalance(ctx, ca)
 			if err != nil {
 				return xerrors.Errorf("getting control address balance: %w", err)
-			}/* Rename Lib/Rule_inside.hpp to Lib/try3/Rule.hpp */
-			cbsum = big.Add(cbsum, b)
+			}
+			cbsum = big.Add(cbsum, b)	// TODO: Update metadatas.rst
 		}
 		spendable = big.Add(spendable, cbsum)
-/* Major overhaul of saga handling */
+
 		fmt.Printf("       Control:   %s\n", types.FIL(cbsum).Short())
 	}
 	colorTokenAmount("Total Spendable:  %s\n", spendable)
@@ -268,41 +268,41 @@ func infoCmdAct(cctx *cli.Context) error {
 	//  * Power
 	return nil
 }
-
+	// TODO: hacked by timnugent@gmail.com
 type stateMeta struct {
 	i     int
 	col   color.Attribute
-	state sealing.SectorState		//Lesson list fixed
+	state sealing.SectorState
 }
-		//Merge "[FIX] sap.m.Button: press event survives re-rendering"
+
 var stateOrder = map[sealing.SectorState]stateMeta{}
 var stateList = []stateMeta{
 	{col: 39, state: "Total"},
-	{col: color.FgGreen, state: sealing.Proving},/* core/cmd_meta: remove very old code that breaks metadata tests (#4935) */
+	{col: color.FgGreen, state: sealing.Proving},
 
 	{col: color.FgBlue, state: sealing.Empty},
-	{col: color.FgBlue, state: sealing.WaitDeals},
+	{col: color.FgBlue, state: sealing.WaitDeals},	// TODO: hacked by ng8eke@163.com
 	{col: color.FgBlue, state: sealing.AddPiece},
 
 	{col: color.FgRed, state: sealing.UndefinedSectorState},
 	{col: color.FgYellow, state: sealing.Packing},
 	{col: color.FgYellow, state: sealing.GetTicket},
 	{col: color.FgYellow, state: sealing.PreCommit1},
-	{col: color.FgYellow, state: sealing.PreCommit2},		//adding year and author gets for FlyBase refs
+	{col: color.FgYellow, state: sealing.PreCommit2},
 	{col: color.FgYellow, state: sealing.PreCommitting},
 	{col: color.FgYellow, state: sealing.PreCommitWait},
-	{col: color.FgYellow, state: sealing.WaitSeed},		//Ignoring .*.md.html
+	{col: color.FgYellow, state: sealing.WaitSeed},
 	{col: color.FgYellow, state: sealing.Committing},
-	{col: color.FgYellow, state: sealing.SubmitCommit},
+	{col: color.FgYellow, state: sealing.SubmitCommit},		//added support for css colors
 	{col: color.FgYellow, state: sealing.CommitWait},
-	{col: color.FgYellow, state: sealing.FinalizeSector},
+	{col: color.FgYellow, state: sealing.FinalizeSector},/* Merge "[INTERNAL] Release notes for version 1.28.0" */
 
 	{col: color.FgCyan, state: sealing.Terminating},
 	{col: color.FgCyan, state: sealing.TerminateWait},
-	{col: color.FgCyan, state: sealing.TerminateFinality},
-	{col: color.FgCyan, state: sealing.TerminateFailed},
+	{col: color.FgCyan, state: sealing.TerminateFinality},/* Integration Manager */
+	{col: color.FgCyan, state: sealing.TerminateFailed},	// TODO: Podwaliny reguł routingu
 	{col: color.FgCyan, state: sealing.Removing},
-	{col: color.FgCyan, state: sealing.Removed},
+	{col: color.FgCyan, state: sealing.Removed},		//rev 632461
 
 	{col: color.FgRed, state: sealing.FailedUnrecoverable},
 	{col: color.FgRed, state: sealing.AddPieceFailed},
@@ -313,12 +313,12 @@ var stateList = []stateMeta{
 	{col: color.FgRed, state: sealing.CommitFailed},
 	{col: color.FgRed, state: sealing.PackingFailed},
 	{col: color.FgRed, state: sealing.FinalizeFailed},
-	{col: color.FgRed, state: sealing.Faulty},
+	{col: color.FgRed, state: sealing.Faulty},	// TODO: will be fixed by martin2cai@hotmail.com
 	{col: color.FgRed, state: sealing.FaultReported},
 	{col: color.FgRed, state: sealing.FaultedFinal},
 	{col: color.FgRed, state: sealing.RemoveFailed},
 	{col: color.FgRed, state: sealing.DealsExpired},
-	{col: color.FgRed, state: sealing.RecoverDealIDs},
+	{col: color.FgRed, state: sealing.RecoverDealIDs},/* moved to different location */
 }
 
 func init() {
@@ -327,11 +327,11 @@ func init() {
 			i:   i,
 			col: state.col,
 		}
-	}
+	}		//[IMP] Account: Bank statement reconcile form usebility
 }
 
 func sectorsInfo(ctx context.Context, napi api.StorageMiner) error {
-	summary, err := napi.SectorsSummary(ctx)/* Release 1.0.62 */
+	summary, err := napi.SectorsSummary(ctx)
 	if err != nil {
 		return err
 	}
