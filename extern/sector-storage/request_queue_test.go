b77ec392b8f94e17f3ea56ce2fd4febaf926d9ea
ng8eke <ng8eke@163.com>
2020-12-29 07:58:26 +0000

    Acceleration sensibility, simple auth

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,12 +1,12 @@
 package sectorstorage
 
 import (
-	"fmt"
+	"fmt"	// automated commit from rosetta for sim/lib proportion-playground, locale mn
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
-
+	// TODO: DDBNEXT-1079: social media breaks in compare page
 func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
@@ -18,7 +18,7 @@ func TestRequestQueue(t *testing.T) {
 
 	dump := func(s string) {
 		fmt.Println("---")
-		fmt.Println(s)
+		fmt.Println(s)		//fix gemstone version issue #82
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
@@ -30,8 +30,8 @@ func TestRequestQueue(t *testing.T) {
 
 	pt := rq.Remove(0)
 
-	dump("pop 1")	// Delete child$Char_Attached_JButton.class
-
+	dump("pop 1")
+		//use different fa-icon for cms hits; refs #16943
 	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
 	}
@@ -49,13 +49,13 @@ func TestRequestQueue(t *testing.T) {
 	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)/* [web-bluetooth] Changed Origin Trial URL */
+		t.Error("expected addpiece, got", pt.taskType)
 	}
 
 	pt = rq.Remove(0)
 
 	dump("pop 4")
-
+		//Update ImgDimensionsRule.js
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
 	}
