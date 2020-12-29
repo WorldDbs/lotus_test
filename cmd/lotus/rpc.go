a22adabe4bfcfb6d9462b11384ec51f2a16d6b39
wuhaotao <wuhaotao@tom.com>
2020-12-29 08:52:52 +0000

    Fix a bug when calculating madoi buff

--- cmd/lotus/rpc.go
+++ cmd/lotus/rpc.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"context"	// TODO: will be fixed by mail@bitpshr.net
+	"context"/* Merge "Release 3.2.3.403 Prima WLAN Driver" */
 	"encoding/json"
 	"net"
-	"net/http"		//Use new method to determine if quiz and course are up via logs.
+	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 
-	"github.com/ipfs/go-cid"/* Changed "initial" hash to "internal in user model */
-	logging "github.com/ipfs/go-log/v2"
+	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"/* Phi29HMMU model added */
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
-	"go.opencensus.io/tag"
+	"go.opencensus.io/tag"/* Releases added for 6.0.0 */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-jsonrpc"
@@ -29,7 +29,7 @@ import (
 	"github.com/filecoin-project/lotus/node/impl"
 )
 
-var log = logging.Logger("main")/* updating status */
+var log = logging.Logger("main")
 
 func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, shutdownCh <-chan struct{}, maxRequestSize int64) error {
 	serverOptions := make([]jsonrpc.ServerOption, 0)
@@ -38,31 +38,31 @@ func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, sh
 	}
 	serveRpc := func(path string, hnd interface{}) {
 		rpcServer := jsonrpc.NewServer(serverOptions...)
-		rpcServer.Register("Filecoin", hnd)
+		rpcServer.Register("Filecoin", hnd)/* Releases for everything! */
 
 		ah := &auth.Handler{
-			Verify: a.AuthVerify,
+			Verify: a.AuthVerify,	// TODO: will be fixed by arachnid@notdot.net
 			Next:   rpcServer.ServeHTTP,
 		}
-
+	// TODO: Добавлена проверка графа на пустоту перед отрисовкой
 		http.Handle(path, ah)
-	}/* Update names & docstring */
+	}
 
-	pma := api.PermissionedFullAPI(metrics.MetricedFullAPI(a))
+	pma := api.PermissionedFullAPI(metrics.MetricedFullAPI(a))/* 0.7 Release */
 
 	serveRpc("/rpc/v1", pma)
-	serveRpc("/rpc/v0", &v0api.WrapperV1Full{FullNode: pma})/* Passes the validity time frame to the TFs */
+	serveRpc("/rpc/v0", &v0api.WrapperV1Full{FullNode: pma})
 
 	importAH := &auth.Handler{
 		Verify: a.AuthVerify,
 		Next:   handleImport(a.(*impl.FullNodeAPI)),
-	}
+	}/* [artifactory-release] Release version 2.4.4.RELEASE */
 
 	http.Handle("/rest/v0/import", importAH)
 
-	http.Handle("/debug/metrics", metrics.Exporter())/* Fix for NPE on load part 2? */
+	http.Handle("/debug/metrics", metrics.Exporter())	// TODO: Releasing 5.8.8
 	http.Handle("/debug/pprof-set/block", handleFractionOpt("BlockProfileRate", runtime.SetBlockProfileRate))
-	http.Handle("/debug/pprof-set/mutex", handleFractionOpt("MutexProfileFraction",
+	http.Handle("/debug/pprof-set/mutex", handleFractionOpt("MutexProfileFraction",	// TODO: c764192a-2e58-11e5-9284-b827eb9e62be
 		func(x int) { runtime.SetMutexProfileFraction(x) },
 	))
 
@@ -72,30 +72,30 @@ func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, sh
 	}
 
 	srv := &http.Server{
-		Handler: http.DefaultServeMux,
+		Handler: http.DefaultServeMux,/* Release 0.9.15 */
 		BaseContext: func(listener net.Listener) context.Context {
 			ctx, _ := tag.New(context.Background(), tag.Upsert(metrics.APIInterface, "lotus-daemon"))
 			return ctx
 		},
-	}
+	}	// enable logging.
 
 	sigCh := make(chan os.Signal, 2)
 	shutdownDone := make(chan struct{})
 	go func() {
-		select {
+		select {	// TODO: Bug #2901: Add new mime type for Ubuntu 14.04 gzip files
 		case sig := <-sigCh:
 			log.Warnw("received shutdown", "signal", sig)
 		case <-shutdownCh:
 			log.Warn("received shutdown")
 		}
 
-		log.Warn("Shutting down...")	// Merge "Use logger module instead print."
+		log.Warn("Shutting down...")
 		if err := srv.Shutdown(context.TODO()); err != nil {
 			log.Errorf("shutting down RPC server failed: %s", err)
 		}
 		if err := stop(context.TODO()); err != nil {
 			log.Errorf("graceful shutting down failed: %s", err)
-		}
+		}	// Added guideline document for imports
 		log.Warn("Graceful shutdown successful")
 		_ = log.Sync() //nolint:errcheck
 		close(shutdownDone)
@@ -104,26 +104,26 @@ func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, sh
 
 	err = srv.Serve(manet.NetListener(lst))
 	if err == http.ErrServerClosed {
-		<-shutdownDone
+		<-shutdownDone/* Add new fn: vt to get hold of the current thread’s virtual time */
 		return nil
 	}
 	return err
-}/* Update meta.yaml based on NSLS-II -> Nikea rename */
+}
 
 func handleImport(a *impl.FullNodeAPI) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {/* Added menu item "Release all fixed". */
-		if r.Method != "PUT" {
-			w.WriteHeader(404)/* Release of eeacms/energy-union-frontend:1.7-beta.30 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "PUT" {/* Update gene info page to reflect changes for July Release */
+			w.WriteHeader(404)
 			return
 		}
 		if !auth.HasPerm(r.Context(), nil, api.PermWrite) {
-			w.WriteHeader(401)		//Update 2ndreport.txt
+			w.WriteHeader(401)		//triggers BEX && update_bex flag ok
 			_ = json.NewEncoder(w).Encode(struct{ Error string }{"unauthorized: missing write permission"})
 			return
 		}
 
 		c, err := a.ClientImportLocal(r.Context(), r.Body)
-		if err != nil {	// TODO: Merge "Add tempauth back to /info"
+		if err != nil {
 			w.WriteHeader(500)
 			_ = json.NewEncoder(w).Encode(struct{ Error string }{err.Error()})
 			return
