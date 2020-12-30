7bc2555bd3f98211d347ceb5008c80f6db9f1d24
ligi <ligi@ligi.de>
2020-12-30 05:50:12 +0000

    article update

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"/* Merge "PHP: Implement SearchInputWidget, deprecate search option" */
-	routing "github.com/libp2p/go-libp2p-core/routing"/* Release v5.05 */
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"
+	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
 func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {/* comment out noop tests, add test descriptions */
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())		//tests.all: support for xmlrunner
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
 	}
 }
@@ -21,7 +21,7 @@ func NoRelay() func() (opts Libp2pOpts, err error) {
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")	// publish_env
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
