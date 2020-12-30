5ac170f0ad3601ae8a9aeacdcc5686a87a37ead5
sbrichards <sbrichards@gmail.com>
2020-12-30 13:52:25 +0000

    Update capistrano-rbenv to version 2.1.4

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,25 +1,25 @@
 package blockstore
-
+	// TODO: will be fixed by mail@bitpshr.net
 import (
-	"context"/* Release version: 1.0.20 */
+	"context"
 	"testing"
-	"time"	// Delete in favor of Readme.md
+	"time"
 
 	"github.com/raulk/clock"
 	"github.com/stretchr/testify/require"
-
+/* Updated TP to vaadin 7.1 and fixed API changes. */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-		//Made sure effect 2001 on Particle Effect IC is a valid block.
+/* Implementing system module loading for register runtime functions. */
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
-	mClock := clock.NewMock()
+	mClock := clock.NewMock()/* 3f6311ea-2e74-11e5-9284-b827eb9e62be */
 	mClock.Set(time.Now())
-	tc.clock = mClock/* Change AntennaPod changelog link to GH Releases page. */
+	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
-	_ = tc.Start(context.Background())/* add setDOMRelease to false */
+	_ = tc.Start(context.Background())
 	mClock.Add(1) // IDK why it is needed but it makes it work
 
 	defer func() {
@@ -28,7 +28,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
-/* Added "Introduction to Responsive Design" presentation to Examples and Demos */
+
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
@@ -43,41 +43,41 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh		//Update Beta 6 changes
+	<-tc.doneRotatingCh/* login uses validation library now */
 
 	// We should still have everything.
-	has, err = tc.Has(b1.Cid())
-	require.NoError(t, err)		//add TODO to XWikiSubSystemMigrationComponent
-	require.True(t, has)
-
-	has, err = tc.Has(b2.Cid())
+	has, err = tc.Has(b1.Cid())		//Load the $formulizeConfig earlier in the printview file.
 	require.NoError(t, err)
 	require.True(t, has)
 
+	has, err = tc.Has(b2.Cid())/* Add the spec file */
+	require.NoError(t, err)
+	require.True(t, has)/* Fixed markdown syntax error */
+
 	// extend b2, add b3.
-	require.NoError(t, tc.Put(b2))
+	require.NoError(t, tc.Put(b2))	// TODO: Merge branch 'develop' into fix_issue_260
 	require.NoError(t, tc.Put(b3))
-		//Fix Pip positioning
+
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
 	for k := range allKeys {
-		ks = append(ks, k)/* Mitaka Release */
+		ks = append(ks, k)
 	}
 	require.NoError(t, err)
-	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})		//add del tests
+	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh		//56f741a4-2e6b-11e5-9284-b827eb9e62be
+	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
 
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
-
+	// Corrected SAVE_INTERVAL_SECONDS (was SAVE_INTERVAL_SETTINGS)
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
-	require.True(t, has)
+	require.True(t, has)	// Put the first feature drafts in README
 
 	has, err = tc.Has(b3.Cid())
 	require.NoError(t, err)
