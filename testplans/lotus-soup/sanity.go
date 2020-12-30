4a6f0a292f2ac8a1917ad194ec75d86d267b0dbf
davidad <davidad@alum.mit.edu>
2020-12-30 07:16:40 +0000

    added grippers for cylinder

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -2,34 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"		//Create CAU1.txt
-	"os"	// TODO: rev 663961
-)		//Create 7kyu_reverse_and_invert.py
+	"io/ioutil"
+	"os"
+)
 
 func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-/* Release v0.25-beta */
+/* Create Knecko */
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
-		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))/* Updated the vsc-install feedstock. */
 	}
-	if err != nil {
+	if err != nil {/* duolingo.com */
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
-	if !stat.IsDir() {
+	if !stat.IsDir() {/* fixing issues that showed up in testing */
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
 	}
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
+		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))/* Merged release/1.0.40 into master */
 	}
-
+	// TODO: Create search_v5.json
 	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
-	}/* Fixed Extension pointing to wrong redis memcache settings */
+	}
 }
