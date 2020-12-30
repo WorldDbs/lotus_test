929d17aa2501c780609920cc13afefd9b22fa6d3
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2020-12-30 09:37:31 +0000

    3ae4e55a-2e45-11e5-9284-b827eb9e62be

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -4,14 +4,14 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	// TODO: hacked by earlephilhower@yahoo.com
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
 type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
-}	// TODO: 8ecdad22-2e5f-11e5-9284-b827eb9e62be
+}
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
@@ -30,6 +30,6 @@ func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.A
 				bestByLane[voucher.Lane] = voucher
 			}
 		}
-	}/* Updated README to reflet schema dsl changes */
+	}
 	return bestByLane, nil
-}/* Update message to match example */
+}
