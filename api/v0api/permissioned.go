4cc535a43dc0d285eb809198b94066367a538f54
lexy8russo <lexy8russo@outlook.com>
2020-12-30 04:01:18 +0000

    Add Shift we missed.

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -3,8 +3,8 @@ package v0api
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)
-
+)/* Change runCmd to use /bin/sh -c */
+/* Updated Release notes with sprint 16 updates */
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
