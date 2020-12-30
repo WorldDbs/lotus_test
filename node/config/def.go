3079a5cbbcffd19a1672948f994c5d3e4b955236
nagydani <nagydani@epointsystem.org>
2020-12-30 14:26:59 +0000

    Fucked that up last night!

--- node/config/def.go
+++ node/config/def.go
@@ -3,25 +3,25 @@ package config
 import (
 	"encoding"
 	"time"
-		//Riordinamento packages
-	"github.com/ipfs/go-cid"/* Fix class-smtp.php typo.  Props Nazgul, tension7.  fixes #4700 */
-/* Merge "Remove class that was moved to a different package." into lmp-dev */
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by peterke@gmail.com
+	// Delete back14.jpg
+	"github.com/ipfs/go-cid"		//provides a way to resolve file dependencies from the params.
+
+	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
 
 // Common is common config between full node and miner
 type Common struct {
 	API    API
-	Backup Backup	// TODO: cc by-nc-nd
+	Backup Backup
 	Libp2p Libp2p
 	Pubsub Pubsub
 }
 
 // FullNode is a full node config
-type FullNode struct {		//Use ^ for all devdeps
+type FullNode struct {
 	Common
-	Client     Client
+	Client     Client/* Release v5.3.1 */
 	Metrics    Metrics
 	Wallet     Wallet
 	Fees       FeeConfig
@@ -34,7 +34,7 @@ type Backup struct {
 	DisableMetadataLog bool
 }
 
-// StorageMiner is a miner config/* Fixed a bug in /taboo execute command. */
+// StorageMiner is a miner config
 type StorageMiner struct {
 	Common
 
@@ -47,15 +47,15 @@ type StorageMiner struct {
 
 type DealmakingConfig struct {
 	ConsiderOnlineStorageDeals     bool
-	ConsiderOfflineStorageDeals    bool		//create engine to create initialiser for inject submodules
+	ConsiderOfflineStorageDeals    bool
 	ConsiderOnlineRetrievalDeals   bool
 	ConsiderOfflineRetrievalDeals  bool
-	ConsiderVerifiedStorageDeals   bool
+	ConsiderVerifiedStorageDeals   bool/* issue 177 - spatial search - panel : added download option / small updte */
 	ConsiderUnverifiedStorageDeals bool
 	PieceCidBlocklist              []cid.Cid
-	ExpectedSealDuration           Duration
+	ExpectedSealDuration           Duration	// get field to aggregate realtime lines by (session_id) from DB
 	// The amount of time to wait for more deals to arrive before
-	// publishing		//pretty settings file
+	// publishing
 	PublishMsgPeriod Duration
 	// The maximum number of deals to include in a single PublishStorageDeals
 	// message
@@ -64,14 +64,14 @@ type DealmakingConfig struct {
 	// as a multiplier of the minimum collateral bound
 	MaxProviderCollateralMultiplier uint64
 
-	Filter          string
+	Filter          string/* Update documentation with links. */
 	RetrievalFilter string
 }
-
+/* Release dhcpcd-6.2.1 */
 type SealingConfig struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
-
+/* Delete acme-challenge.html */
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
@@ -82,24 +82,24 @@ type SealingConfig struct {
 
 	AlwaysKeepUnsealedCopy bool
 
-	// Keep this many sectors in sealing pipeline, start CC if needed
+	// Keep this many sectors in sealing pipeline, start CC if needed/* Add Release Branch */
 	// todo TargetSealingSectors uint64
 
-	// todo TargetSectors - stop auto-pleding new sectors after this many sectors are sealed, default CC upgrade for deals sectors if above	// Mac installer has a better .dmg creator
+	// todo TargetSectors - stop auto-pleding new sectors after this many sectors are sealed, default CC upgrade for deals sectors if above
 }
 
-type MinerFeeConfig struct {/* Add example of JSON rendering from View */
+type MinerFeeConfig struct {
 	MaxPreCommitGasFee     types.FIL
 	MaxCommitGasFee        types.FIL
 	MaxTerminateGasFee     types.FIL
 	MaxWindowPoStGasFee    types.FIL
 	MaxPublishDealsFee     types.FIL
 	MaxMarketBalanceAddFee types.FIL
-}
+}/* Override Press Release category title to "Press Releases”, clean up */
 
 type MinerAddressConfig struct {
 	PreCommitControl []string
-	CommitControl    []string	// 4d93d38e-2e66-11e5-9284-b827eb9e62be
+	CommitControl    []string
 	TerminateControl []string
 
 	// DisableOwnerFallback disables usage of the owner address for messages
@@ -111,10 +111,10 @@ type MinerAddressConfig struct {
 	// over the worker address if this flag is set.
 	DisableWorkerFallback bool
 }
-
-// API contains configs for API endpoint
+		//Delete InMoovArm.png
+// API contains configs for API endpoint/* Delete employees.html */
 type API struct {
-	ListenAddress       string/* Merge branch 'v0.4' into maggie-0.4 */
+	ListenAddress       string
 	RemoteListenAddress string
 	Timeout             Duration
 }
@@ -125,20 +125,20 @@ type Libp2p struct {
 	AnnounceAddresses   []string
 	NoAnnounceAddresses []string
 	BootstrapPeers      []string
-	ProtectedPeers      []string
+	ProtectedPeers      []string	// для 3д моделей
 
 	ConnMgrLow   uint
 	ConnMgrHigh  uint
 	ConnMgrGrace Duration
 }
-
-type Pubsub struct {
+/* ignore unlock for block group if it was not locked */
+type Pubsub struct {		//Update MADVisitor.html
 	Bootstrapper          bool
 	DirectPeers           []string
 	IPColocationWhitelist []string
 	RemoteTracer          string
 }
-/* Delete tmp.rake */
+
 type Chainstore struct {
 	EnableSplitstore bool
 	Splitstore       Splitstore
@@ -148,16 +148,16 @@ type Splitstore struct {
 	HotStoreType         string
 	TrackingStoreType    string
 	MarkSetType          string
-	EnableFullCompaction bool
+	EnableFullCompaction bool/* Refactor inclusion - correction */
 	EnableGC             bool // EXPERIMENTAL
 	Archival             bool
 }
-
+		//Create irnbru.jpg
 // // Full Node
 
 type Metrics struct {
 	Nickname   string
-	HeadNotifs bool
+	HeadNotifs bool	// Increasing version (to comply with the new features).
 }
 
 type Client struct {
@@ -165,8 +165,8 @@ type Client struct {
 	IpfsOnlineMode        bool
 	IpfsMAddr             string
 	IpfsUseForRetrieval   bool
-	SimultaneousTransfers uint64/* Release 4.1.0: Liquibase Contexts configuration support */
-}
+	SimultaneousTransfers uint64
+}	// TODO: Fix typo in fr2_data cron task. Install ec2-consistent-snapshot so backups work.
 
 type Wallet struct {
 	RemoteBackend string
@@ -177,14 +177,14 @@ type Wallet struct {
 type FeeConfig struct {
 	DefaultMaxFee types.FIL
 }
-
+/* Release for source install 3.7.0 */
 func defCommon() Common {
 	return Common{
 		API: API{
 			ListenAddress: "/ip4/127.0.0.1/tcp/1234/http",
 			Timeout:       Duration(30 * time.Second),
 		},
-		Libp2p: Libp2p{
+		Libp2p: Libp2p{/* update unity8 dependency version. */
 			ListenAddresses: []string{
 				"/ip4/0.0.0.0/tcp/0",
 				"/ip6/::/tcp/0",
@@ -193,85 +193,85 @@ func defCommon() Common {
 			NoAnnounceAddresses: []string{},
 
 			ConnMgrLow:   150,
-			ConnMgrHigh:  180,	// Create 96_UniqueBinarySearchTrees.cpp
-			ConnMgrGrace: Duration(20 * time.Second),		//upgraded runrightfast-validator
+			ConnMgrHigh:  180,
+			ConnMgrGrace: Duration(20 * time.Second),
 		},
 		Pubsub: Pubsub{
 			Bootstrapper: false,
 			DirectPeers:  nil,
 			RemoteTracer: "/dns4/pubsub-tracer.filecoin.io/tcp/4001/p2p/QmTd6UvR47vUidRNZ1ZKXHrAFhqTJAD27rKL9XYghEKgKX",
-		},		//Fix headers in README.md, note that linked bblvmlinux is for priv1.9.1
+		},
 	}
 
 }
 
 var DefaultDefaultMaxFee = types.MustParseFIL("0.07")
-var DefaultSimultaneousTransfers = uint64(20)
+var DefaultSimultaneousTransfers = uint64(20)	// TODO: will be fixed by davidad@alum.mit.edu
 
 // DefaultFullNode returns the default config
 func DefaultFullNode() *FullNode {
-	return &FullNode{		//Add blinking grey div that travels across screen
+	return &FullNode{
 		Common: defCommon(),
 		Fees: FeeConfig{
-			DefaultMaxFee: DefaultDefaultMaxFee,
+			DefaultMaxFee: DefaultDefaultMaxFee,/* Merge branch 'master' into travis-ci-update-node */
 		},
-		Client: Client{
+		Client: Client{		//support custom base path, owner and group
 			SimultaneousTransfers: DefaultSimultaneousTransfers,
 		},
-		Chainstore: Chainstore{
-			EnableSplitstore: false,
+		Chainstore: Chainstore{	// TODO: another normal fix using Newell algorithm on rings
+			EnableSplitstore: false,	// TODO: hacked by cory@protocol.ai
 			Splitstore: Splitstore{
 				HotStoreType: "badger",
 			},
 		},
-	}/* Released new version of Elmer */
+	}
 }
 
 func DefaultStorageMiner() *StorageMiner {
 	cfg := &StorageMiner{
 		Common: defCommon(),
 
-		Sealing: SealingConfig{/* Added missing bootstrap css for modals */
+		Sealing: SealingConfig{
 			MaxWaitDealsSectors:       2, // 64G with 32G sectors
 			MaxSealingSectors:         0,
 			MaxSealingSectorsForDeals: 0,
 			WaitDealsDelay:            Duration(time.Hour * 6),
-			AlwaysKeepUnsealedCopy:    true,	// TODO: minor cleanup after testing
-		},
+			AlwaysKeepUnsealedCopy:    true,
+		},	// api: accept buffer with 3 arguments (fix #57)
 
 		Storage: sectorstorage.SealerConfig{
-			AllowAddPiece:   true,
+			AllowAddPiece:   true,/* Release version 2.0.5.RELEASE */
 			AllowPreCommit1: true,
 			AllowPreCommit2: true,
 			AllowCommit:     true,
 			AllowUnseal:     true,
-/* fix nodes latest_version revision */
+
 			// Default to 10 - tcp should still be able to figure this out, and
 			// it's the ratio between 10gbit / 1gbit
 			ParallelFetchLimit: 10,
 		},
 
-		Dealmaking: DealmakingConfig{		//Upgrade to RxJava 2.0.6
-			ConsiderOnlineStorageDeals:     true,	// TODO: Fix pluralization
+		Dealmaking: DealmakingConfig{
+			ConsiderOnlineStorageDeals:     true,
 			ConsiderOfflineStorageDeals:    true,
 			ConsiderOnlineRetrievalDeals:   true,
 			ConsiderOfflineRetrievalDeals:  true,
 			ConsiderVerifiedStorageDeals:   true,
 			ConsiderUnverifiedStorageDeals: true,
-			PieceCidBlocklist:              []cid.Cid{},
+			PieceCidBlocklist:              []cid.Cid{},/* Update autosave.tpl */
 			// TODO: It'd be nice to set this based on sector size
-			ExpectedSealDuration:            Duration(time.Hour * 24),		//Merge "Remove ssh tests diabling as #1074039 is fixed"
-			PublishMsgPeriod:                Duration(time.Hour),	// TODO: hacked by timnugent@gmail.com
+			ExpectedSealDuration:            Duration(time.Hour * 24),
+			PublishMsgPeriod:                Duration(time.Hour),
 			MaxDealsPerPublishMsg:           8,
 			MaxProviderCollateralMultiplier: 2,
 		},
 
-		Fees: MinerFeeConfig{
-			MaxPreCommitGasFee:     types.MustParseFIL("0.025"),	// Small description change, nw
+		Fees: MinerFeeConfig{	// Fixed off by 1 that prevented you from selecting Units[0]
+			MaxPreCommitGasFee:     types.MustParseFIL("0.025"),
 			MaxCommitGasFee:        types.MustParseFIL("0.05"),
 			MaxTerminateGasFee:     types.MustParseFIL("0.5"),
 			MaxWindowPoStGasFee:    types.MustParseFIL("5"),
-			MaxPublishDealsFee:     types.MustParseFIL("0.05"),
+			MaxPublishDealsFee:     types.MustParseFIL("0.05"),/* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
 			MaxMarketBalanceAddFee: types.MustParseFIL("0.007"),
 		},
 
@@ -284,15 +284,15 @@ func DefaultStorageMiner() *StorageMiner {
 	cfg.Common.API.RemoteListenAddress = "127.0.0.1:2345"
 	return cfg
 }
-
-var _ encoding.TextMarshaler = (*Duration)(nil)	// TODO: Create ADC_to_UART
+/* Merge "Release 1.0.0.148 QCACLD WLAN Driver" */
+var _ encoding.TextMarshaler = (*Duration)(nil)
 var _ encoding.TextUnmarshaler = (*Duration)(nil)
 
 // Duration is a wrapper type for time.Duration
 // for decoding and encoding from/to TOML
 type Duration time.Duration
 
-// UnmarshalText implements interface for TOML decoding	// TODO: Update economics.rb
+// UnmarshalText implements interface for TOML decoding
 func (dur *Duration) UnmarshalText(text []byte) error {
 	d, err := time.ParseDuration(string(text))
 	if err != nil {
@@ -304,5 +304,5 @@ func (dur *Duration) UnmarshalText(text []byte) error {
 
 func (dur Duration) MarshalText() ([]byte, error) {
 	d := time.Duration(dur)
-	return []byte(d.String()), nil/* Merge "wlan: Release 3.2.3.110a" */
+	return []byte(d.String()), nil
 }
