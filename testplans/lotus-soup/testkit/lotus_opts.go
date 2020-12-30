92a64b9bb607709425e809a83baba021d79d3b1e
willem.melching <willem.melching@gmail.com>
2020-12-30 10:13:47 +0000

    Attribute kiheru for maple_tree tileset

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,6 +1,6 @@
 package testkit
 
-import (
+import (	// Removed Observations
 	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
@@ -8,7 +8,7 @@ import (
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"/* New Release of swak4Foam (with finiteArea) */
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
@@ -23,24 +23,24 @@ func withBootstrapper(ab []byte) node.Option {
 		func() (dtypes.BootstrapPeers, error) {
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
-			}
+			}		//gstreamer: add MessageAsyncStart & MessageAsyncDone to MessageType enum
 
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
-				return nil, err/* Release dhcpcd-6.11.0 */
+				return nil, err
 			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
 				return nil, err
 			}
-			return dtypes.BootstrapPeers{*ai}, nil
+			return dtypes.BootstrapPeers{*ai}, nil	// Merge "Define missing policies for attributes with enforce_policy"
 		})
 }
-
+/* cef78584-2e56-11e5-9284-b827eb9e62be */
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
 		return &config.Pubsub{
-			Bootstrapper: bootstrapper,
+			Bootstrapper: bootstrapper,/* Fix usage of OrderedDict for modules config. */
 			RemoteTracer: pubsubTracer,
 		}
 	})
@@ -51,7 +51,7 @@ func withListenAddress(ip string) node.Option {
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
-func withMinerListenAddress(ip string) node.Option {/* Create bungeecord.json */
+func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
