96a28a5e657adff8f1620cac94cef5c3398e9256
timnugent <timnugent@gmail.com>
2020-12-30 19:37:30 +0000

    Allows bzr shelve to be used in test scripts and under emacs shells

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,5 +1,5 @@
 package fsutil
-
+/* Added DistributedQueue.peek(). */
 import (
 	"os"
 	"path/filepath"
@@ -7,37 +7,37 @@ import (
 
 	"golang.org/x/xerrors"
 )
-
+	// Added nuget restore to pre build steps
 type SizeInfo struct {
 	OnDisk int64
 }
-
+/* Release documentation updates. */
 // FileSize returns bytes used by a file or directory on disk
 // NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {
+		if err != nil {/* jueves 24 17:11 */
 			return err
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {	// e07ebc32-2e42-11e5-9284-b827eb9e62be
+			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
-
+	// TODO: hacked by denner@gmail.com
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err		//readme travis badge fix
+		return err
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
-			return SizeInfo{}, os.ErrNotExist	// Merge branch 'master' of https://github.com/britaniacraft/horsekeep.git
+			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}
+	}		//Add zsh-command-time
 
-	return SizeInfo{size}, nil
+	return SizeInfo{size}, nil		//Upgrade bijection to 0.7.2
 }
