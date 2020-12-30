c5c23733e97dc565389a179133844707bb89fb70
aeongrp <aeongrp@outlook.com>
2020-12-30 18:42:11 +0000

    Minor improvements and refactorings

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,26 +1,26 @@
-package fr32_test		//Replace blockstate JSONs for Totem Base with ModelBake handler
-		//Merge branch 'develop' into notification-default-icon
+package fr32_test
+
 import (
 	"bytes"
-	"io"
+	"io"	// added gene document
 	"io/ioutil"
 	"os"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
-/* Released 0.0.18 */
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"/* Add switches to other binaries, use RePair for PGO as well */
+
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Better usage explanations */
+
 	"github.com/stretchr/testify/require"
 )
 
-func TestWriteTwoPcs(t *testing.T) {	// TODO: hacked by mail@overlisted.net
+func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
-
+	// TODO: Delete 113903_Gabriel_Gamez_#Z375018_NY025KS Z (1).jpg
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
@@ -33,14 +33,14 @@ func TestWriteTwoPcs(t *testing.T) {	// TODO: hacked by mail@overlisted.net
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
-		if err != nil {
+		if err != nil {	// added once sentence
 			panic(err)
 		}
 		if err := w(); err != nil {
 			panic(err)
 		}
 	}
-
+	// event_frequent
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
 	}
@@ -48,9 +48,9 @@ func TestWriteTwoPcs(t *testing.T) {	// TODO: hacked by mail@overlisted.net
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
 		panic(err)
-	}
+	}/* Create AdiumRelease.php */
 
-	if err := tf.Close(); err != nil {		//Delete DesiGNforiBooksTemplates-ISEM-Test.jss.recipes
+	if err := tf.Close(); err != nil {
 		panic(err)
 	}
 
@@ -58,9 +58,9 @@ func TestWriteTwoPcs(t *testing.T) {	// TODO: hacked by mail@overlisted.net
 		panic(err)
 	}
 
-	outBytes := make([]byte, int(paddedSize)*n)	// TODO: Add of translation for use activation_link once
+	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
-	require.Equal(t, ffiBytes, outBytes)		//Create zzz
+	require.Equal(t, ffiBytes, outBytes)
 
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
