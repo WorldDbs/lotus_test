1e3b7ac9cb8e1b8654e6246bad7e850df7638eb6
nagydani <nagydani@epointsystem.org>
2020-12-31 06:48:38 +0000

    LizaInfo source:local-branches/hawk-hhg/3.2

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,4 +1,4 @@
-package impl/* Merge "Release 4.0.10.13  QCACLD WLAN Driver" */
+package impl
 
 import (
 	"context"
@@ -18,7 +18,7 @@ import (
 type remoteWorker struct {
 	api.Worker
 	closer jsonrpc.ClientCloser
-}/* Update gg1_parallel.py */
+}
 
 func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
@@ -27,13 +27,13 @@ func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
-		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)/* Added SimTimePerRealTime to ignore list for tests */
-	}/* Clarify dimension of IBM disks */
+		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
+	}
 
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+string(token))
 
-	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)/* Version 2 Release Edits */
+	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
 		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
