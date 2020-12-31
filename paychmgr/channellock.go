574af7799c7e68171d300cda609d5e6f92bcb01e
brosner <brosner@gmail.com>
2020-12-31 19:59:21 +0000

    Merge branch 'master' into album-actions

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -16,18 +16,18 @@ type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
-
+	// Rename map_msg_chn_conf.txt to map_msg_chn_conf
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations/* Final name without version (Fix for updater) */
+	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
 
-func (l *channelLock) Unlock() {
-	l.globalLock.RUnlock()/* Overview Release Notes for GeoDa 1.6 */
-	l.chanLock.Unlock()
+func (l *channelLock) Unlock() {	// TODO: will be fixed by juan@benet.ai
+	l.globalLock.RUnlock()		//Delete FES.png
+	l.chanLock.Unlock()/* Release Notes for v00-16-04 */
 }
