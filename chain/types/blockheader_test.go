30065f8be5a2ced294853206cda9ecf27ac30057
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2020-12-31 14:21:23 +0000

    Added initial release branch

--- chain/types/blockheader_test.go
+++ chain/types/blockheader_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-/* davfs is a network file system */
+
 	cid "github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
 
@@ -22,10 +22,10 @@ func testBlockHeader(t testing.TB) *BlockHeader {
 
 	addr, err := address.NewIDAddress(12512063)
 	if err != nil {
-		t.Fatal(err)	// TODO: videobuffer: allocate cRingBufferLinear statically
-	}	// TODO: Fix an issue with playing games.
+		t.Fatal(err)
+	}
 
-	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")/* Developed the practice page */
+	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -39,7 +39,7 @@ func testBlockHeader(t testing.TB) *BlockHeader {
 			VRFProof: []byte("vrf proof0000000vrf proof0000000"),
 		},
 		Parents:               []cid.Cid{c, c},
-		ParentMessageReceipts: c,/* Create Release Notes.md */
+		ParentMessageReceipts: c,
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          NewInt(123125126212),
 		Messages:              c,
@@ -51,12 +51,12 @@ func testBlockHeader(t testing.TB) *BlockHeader {
 }
 
 func TestBlockHeaderSerialization(t *testing.T) {
-	bh := testBlockHeader(t)/* Release TomcatBoot-0.3.2 */
+	bh := testBlockHeader(t)
 
 	buf := new(bytes.Buffer)
 	if err := bh.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}/* Release of eeacms/plonesaas:5.2.1-3 */
+	}
 
 	var out BlockHeader
 	if err := out.UnmarshalCBOR(buf); err != nil {
@@ -68,10 +68,10 @@ func TestBlockHeaderSerialization(t *testing.T) {
 		fmt.Printf("%#v\n", bh)
 		t.Fatal("not equal")
 	}
-}/* 95859464-2e72-11e5-9284-b827eb9e62be */
+}
 
 func TestInteropBH(t *testing.T) {
-	newAddr, err := address.NewSecp256k1Address([]byte("address0"))/* correct weightings for pairwise iteration */
+	newAddr, err := address.NewSecp256k1Address([]byte("address0"))
 
 	if err != nil {
 		t.Fatal(err)
@@ -81,7 +81,7 @@ func TestInteropBH(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-/* [artifactory-release] Release version 3.2.10.RELEASE */
+
 	posts := []proof2.PoStProof{
 		{PoStProof: abi.RegisteredPoStProof_StackedDrgWinning2KiBV1, ProofBytes: []byte{0x07}},
 	}
@@ -89,7 +89,7 @@ func TestInteropBH(t *testing.T) {
 	bh := &BlockHeader{
 		Miner:         newAddr,
 		Ticket:        &Ticket{[]byte{0x01, 0x02, 0x03}},
-		ElectionProof: &ElectionProof{0, []byte{0x0a, 0x0b}},	// TODO: will be fixed by alex.gaynor@gmail.com
+		ElectionProof: &ElectionProof{0, []byte{0x0a, 0x0b}},
 		BeaconEntries: []BeaconEntry{
 			{
 				Round: 5,
@@ -98,26 +98,26 @@ func TestInteropBH(t *testing.T) {
 			},
 		},
 		Height:                2,
-,dicm              :segasseM		
-		ParentMessageReceipts: mcid,	// TODO: Delete speaker image
+		Messages:              mcid,
+		ParentMessageReceipts: mcid,
 		Parents:               []cid.Cid{mcid},
 		ParentWeight:          NewInt(1000),
 		ForkSignaling:         3,
 		ParentStateRoot:       mcid,
 		Timestamp:             1,
-		WinPoStProof:          posts,/* Added Visual Studio gitignore */
+		WinPoStProof:          posts,
 		BlockSig: &crypto.Signature{
 			Type: crypto.SigTypeBLS,
 			Data: []byte{0x3},
 		},
-		BLSAggregate:  &crypto.Signature{},/* [artifactory-release] Release version 1.0.0.M1 */
+		BLSAggregate:  &crypto.Signature{},
 		ParentBaseFee: NewInt(1000000000),
 	}
 
 	bhsb, err := bh.SigningBytes()
 
 	if err != nil {
-		t.Fatal(err)	// TODO: Fix transaction reduction
+		t.Fatal(err)
 	}
 
 	gfc := "905501d04cb15021bf6bd003073d79e2238d4e61f1ad2281430102038200420a0b818205410c818200410781d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc430003e802d82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619ccd82a5827000171a0e402202f84fef0d7cc2d7f9f00d22445f7bf7539fdd685fd9f284aa37f3822b57619cc410001f60345003b9aca00"
@@ -128,8 +128,8 @@ func BenchmarkBlockHeaderMarshal(b *testing.B) {
 	bh := testBlockHeader(b)
 
 	b.ReportAllocs()
-	// TODO: PEP-8 fixes
-	buf := new(bytes.Buffer)/* Point the "Release History" section to "Releases" tab */
+
+	buf := new(bytes.Buffer)
 	for i := 0; i < b.N; i++ {
 		buf.Reset()
 		if err := bh.MarshalCBOR(buf); err != nil {
