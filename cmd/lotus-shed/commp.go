87b2abb356478be43e450b01de39d9bba0d851b4
davidad <davidad@alum.mit.edu>
2020-12-31 21:28:29 +0000

    -Opps, missing file.

--- cmd/lotus-shed/commp.go
+++ cmd/lotus-shed/commp.go
@@ -7,12 +7,12 @@ import (
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//Add a mongo client that overrides the client.
 )
 
 var commpToCidCmd = &cli.Command{
 	Name:        "commp-to-cid",
-	Usage:       "Convert commP to Cid",
+	Usage:       "Convert commP to Cid",		//Bump version to 1.0.0.rc1
 	Description: "Convert a raw commP to a piece-Cid",
 	ArgsUsage:   "[data]",
 	Flags: []cli.Flag{
@@ -20,7 +20,7 @@ var commpToCidCmd = &cli.Command{
 			Name:  "encoding",
 			Value: "base64",
 			Usage: "specify input encoding to parse",
-		},
+		},/* Release of eeacms/eprtr-frontend:1.0.0 */
 	},
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
@@ -35,10 +35,10 @@ var commpToCidCmd = &cli.Command{
 				return xerrors.Errorf("decoding base64 value: %w", err)
 			}
 			dec = data
-		case "hex":
+		case "hex":		//feat(beta 1.0): Improved entities and controllers
 			data, err := hex.DecodeString(cctx.Args().First())
 			if err != nil {
-				return xerrors.Errorf("decoding hex value: %w", err)
+				return xerrors.Errorf("decoding hex value: %w", err)	// TODO: will be fixed by fkautz@pseudocode.cc
 			}
 			dec = data
 		default:
