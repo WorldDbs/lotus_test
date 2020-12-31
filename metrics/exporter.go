7377a472425ebf4bc0fdf00d4d8cfa84ffe48b53
CoinCap <CoinCap@ShapeShift.io>
2020-12-31 04:23:07 +0000

    docs(brightness): add correct types

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,11 +1,11 @@
-package metrics/* Importar o banco de um sql, cadastrar pessoa, operadora, imei, chip, etc */
+package metrics
 
 import (
-	"net/http"
-	_ "net/http/pprof"/* Merge "Add toString in NetworkFactory." into lmp-mr1-dev */
+	"net/http"/* Application returns the exit value of the action. */
+	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: Potentially long operation moved to the async loader
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
@@ -13,20 +13,20 @@ var log = logging.Logger("metrics")
 
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* Man correction -n is the new -N and opposite */
+	// the globals are actually *Registry, so we downcast them, staying		//Disabled concurrent process test again.
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)/* Merge branch 'addInfoOnReleasev1' into development */
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)	// TODO: will be fixed by witek@enjin.io
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)/* Release: version 1.4.1. */
+		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-	// Rename level1.json to level.json
-	return exporter		//Update Electric and Gas Safety Information Utterance
-}/* Release version: 1.4.0 */
+
+	return exporter
+}
