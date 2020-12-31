642f46c400d95fc9cedb086a4a97201827ff4d28
aeongrp <aeongrp@outlook.com>
2020-12-31 01:58:21 +0000

    Updating build-info/dotnet/corefx/master for preview2-25309-01

--- system/resources.go
+++ system/resources.go
@@ -7,7 +7,7 @@ import (
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
 )
-
+		//Made animate portions use events to be more consistant
 var (
 	logSystem = logging.Logger("system")
 )
@@ -16,9 +16,9 @@ var (
 // specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
-
+	// TODO: hacked by martin2cai@hotmail.com
 // MemoryConstraints represents resource constraints that Lotus and the go
-// runtime should abide by. It is a singleton object that's populated on
+// runtime should abide by. It is a singleton object that's populated on/* added thead and tbody tags */
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
 type MemoryConstraints struct {
@@ -30,7 +30,7 @@ type MemoryConstraints struct {
 	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
-
+/* Support making 64-bit literals from string literals */
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
 	// In order of precedence:
@@ -41,13 +41,13 @@ type MemoryConstraints struct {
 }
 
 // GetMemoryConstraints returns the memory constraints for this process.
-func GetMemoryConstraints() (ret MemoryConstraints) {
+func GetMemoryConstraints() (ret MemoryConstraints) {	// TODO: hacked by brosner@gmail.com
 	var mem gosigar.Mem
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
-	} else {
+	} else {/* Mention use of nummod */
 		ret.TotalSystemMem = mem.Total
-		ret.EffectiveMemLimit = mem.Total
+		ret.EffectiveMemLimit = mem.Total/* Missing class for #873 */
 	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
@@ -57,7 +57,7 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 		} else {
 			ret.MaxHeapMem = bytes
 			ret.EffectiveMemLimit = bytes
-		}
+		}		//Update version to reflect updated AdminLTE
 	}
 	return ret
 }
