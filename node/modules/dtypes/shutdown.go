4c1fb30c6ab1f6aa88fd47603311d26443c71a70
fjl <fjl@ethereum.org>
2020-12-31 19:06:07 +0000

    Update airspace.minified.css

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-
+/* ga "Gaeilge" translation #15410. Author: PangurPawn. Some Irish translations  */
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
