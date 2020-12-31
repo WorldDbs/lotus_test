28481e97cc3bcab4fd0a7aca613e3421c11187d0
antao2002 <antao2002@gmail.com>
2020-12-31 08:01:25 +0000

    rebuild weChat.jar

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,7 +1,7 @@
 package sealing
 
 import (
-	"testing"/* Release 2.4b4 */
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -12,13 +12,13 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-	// TODO: Try to fix pymedusa/SickRage/issues/307 (#388)
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
 	}
 	assert.Equal(t, n, sum)
-}
+}	// Added the support to generate output file
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
@@ -29,13 +29,13 @@ func TestFillersFromRem(t *testing.T) {
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()/* Release notes for 1.0.66 */
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})	// Server version 0.5.0
 
 		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()		//add watch fix
-		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
+		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()	// JAG changes for Alternate answers
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
 		// different 2
