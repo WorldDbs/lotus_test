996ac652adf5a42152210020c8969d515db8581a
indexxuan <indexxuan@gmail.com>
2020-12-31 11:01:46 +0000

    Optimisation des boucles du parseur

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,13 +2,13 @@ package adt
 
 import (
 	"context"
-/* Client side sorting only if client side :) */
+
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
 type Store interface {
-	Context() context.Context		//Warning if Firefox is not detected
+	Context() context.Context
 	cbor.IpldStore
 }
 
