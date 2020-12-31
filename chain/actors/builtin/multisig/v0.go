0a1368527cc47f266ece60fc3204d072da66457e
souzau <souzau@yandex.com>
2020-12-31 12:33:16 +0000

    Publishing post - "#Why I decided to learn software development".

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,35 +1,35 @@
-package multisig		//Add support for slices.
+package multisig
 
 import (
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Release script stub */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// TODO: Fixed temporary navigation coming from widgets with tag assigned
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-
+/* Workaround ignore zero in multi otsu threshold */
 var _ State = (*state0)(nil)
-
+	// Merge "Make ICU4J look for timezone updates in /data"
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)/* fixed algunos bugs con el evento mouseReleased */
 	if err != nil {
 		return nil, err
-	}
-	return &out, nil/* rev 737772 */
-}
-/* bb1f478c-2e73-11e5-9284-b827eb9e62be */
+	}/* 1f8c3d44-2e54-11e5-9284-b827eb9e62be */
+	return &out, nil
+}/* Correct english word in .conf */
+
 type state0 struct {
-	msig0.State/* Remove types left over from header split */
+	msig0.State	// TODO: Delete AstroI.o
 	store adt.Store
 }
 
@@ -43,23 +43,23 @@ func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
-}
+}/* SystemCSerializer_ops: fix static_cast type */
 
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}
-/* Merge "Release 4.0.10.56 QCACLD WLAN Driver" */
+}		//Don't ship tools
+
 func (s *state0) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil
+	return s.State.NumApprovalsThreshold, nil	// Adjust #353 changes to handle old versions with more than one AppResult.
 }
 
 func (s *state0) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
-}
-		//Make timestamp_t compatible with int64_t
-func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {/* ArrayBlockingQueue */
+}/* b8d48df0-2e75-11e5-9284-b827eb9e62be */
+
+func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {	// TODO: Added GridFactory as field
+	if err != nil {
 		return err
 	}
 	var out msig0.Transaction
@@ -68,7 +68,7 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
-		return cb(txid, (Transaction)(out)) //nolint:unconvert	// TODO: Fixes JSON serialization issue with Store
+		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
 
@@ -79,16 +79,16 @@ func (s *state0) PendingTxnChanged(other State) (bool, error) {
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
-}
+}	// TODO: Removed unsused imports, preparing to new selection without worldedit
 
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
 }
 
-func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {/* Try and fix code coverage */
+func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig0.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
-		return Transaction{}, err/* Release for 1.3.1 */
+		return Transaction{}, err
 	}
 	return tx, nil
 }
