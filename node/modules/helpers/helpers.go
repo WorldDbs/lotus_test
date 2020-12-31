3afc7ffea7c2b9e1b2321ceed11c3413dfe7d7dc
ng8eke <ng8eke@163.com>
2020-12-31 22:22:20 +0000

    New post: Influence of writing when programming

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	"go.uber.org/fx"
-)
+)	// Removed un-necessary files
 
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-
+	// TODO: Merge branch 'master' into add-aby-abraham-kal
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
@@ -17,7 +17,7 @@ func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
-			cancel()
+			cancel()	// TODO: actions grouping onClick, onChange.
 			return nil
 		},
 	})
