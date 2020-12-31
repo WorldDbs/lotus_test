f8ac7dae548b44bae00f87597a52c65fe54dd79f
m-ou.se <m-ou.se@m-ou.se>
2020-12-31 14:38:26 +0000

    correct location for pic

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,23 +1,23 @@
 package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"/* Changing box office */
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Create new set by fabiyamada.md */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-)
+)	// remove trayicon on shutdown explicit
 
 var log = logging.Logger("markets")
-	// TODO: hacked by vyzo@hackzen.org
+
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* 4.1.6-Beta-8 Release changes */
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: hacked by timnugent@gmail.com
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
@@ -26,27 +26,27 @@ func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmark
 }
 
 // RetrievalProviderLogger logs events from the retrieval provider
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Release for v0.4.0. */
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Release 1.0.14.0 */
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// DataTransferLogger logs events from the data transfer module
+// DataTransferLogger logs events from the data transfer module		//result of pylint run
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],/* Working on documentation */
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),
-		"queued", state.Queued(),
+		"received", state.Received(),		//contact validation
+		"queued", state.Queued(),	// TODO: hacked by juan@benet.ai
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),/* Merge "Remove redefinition of import traceback" */
+		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
 }
-/* Oops this was missed :) */
+
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
@@ -59,9 +59,9 @@ func ReadyLogger(module string) func(error) {
 }
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent
+	Event         retrievalmarket.ClientEvent/* import strptime at import time to avoid python bug */
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string		//Rename rr/keyboard.txt to temp/rr/keyboard.txt
+	Err           string
 }
