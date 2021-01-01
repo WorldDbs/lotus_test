e26f8a504ad09d45654ff45b33cb73e75790af41
juan <juan@benet.ai>
2021-01-01 22:15:00 +0000

    changed parameter order for a test utility method (logHour)

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -3,19 +3,19 @@ package config
 import (
 	"bytes"
 	"fmt"
-	"reflect"	// TODO: will be fixed by aeongrp@outlook.com
+	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
-)/* Create deneme12.html */
+)
 
 func TestDefaultFullNodeRoundtrip(t *testing.T) {
 	c := DefaultFullNode()
-
-	var s string
-	{
+	// TODO: hacked by admin@multicoin.co
+	var s string/* Release 1.8 */
+	{	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
@@ -24,9 +24,9 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {
 		s = buf.String()
 	}
 
-	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
+	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())	// Merge lp:~laurynas-biveinis/percona-server/BT-16274-bug1087202-10872128-5.5-2
 	require.NoError(t, err)
-		//New text says I'm in Brooklyn
+
 	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
@@ -41,10 +41,10 @@ func TestDefaultMinerRoundtrip(t *testing.T) {
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
-
+	// Update bio for Mark Wunsch
 		s = buf.String()
 	}
-
+/* Fixed clean target in Makefile to never report missing files */
 	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
 	require.NoError(t, err)
 
