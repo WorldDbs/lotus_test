81474c49fb3b7ff2ccdb7c8fb68c4e9d0a2512ec
igor <igor@soramitsu.co.jp>
2021-01-01 08:27:22 +0000

    Add unresponsive flag to SeasonPlayer and highlight in rosters

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -3,17 +3,17 @@
 // +build !testground
 // +build !calibnet
 // +build !nerpanet
-// +build !butterflynet
+// +build !butterflynet/* Release 8.0.8 */
 
 package build
 
-import (
+import (/* Released MonetDB v0.1.2 */
 	"math"
 	"os"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: Rename Ex01EquipamentoSonoro to Lista Ex01EquipamentoSonoro
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
@@ -28,12 +28,12 @@ const GenesisFile = "mainnet.car"
 const UpgradeBreezeHeight = 41280
 
 const BreezeGasTampingDuration = 120
-
+		//a7281b1c-2e6b-11e5-9284-b827eb9e62be
 const UpgradeSmokeHeight = 51000
-
+	// Merge "lwyszomirski | #442 | Added support for nonexistent datetime"
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
-
+	// TODO: will be fixed by hugomrdias@gmail.com
 const UpgradeActorsV2Height = 138720
 
 const UpgradeTapeHeight = 140760
@@ -45,11 +45,11 @@ const UpgradeLiftoffHeight = 148888
 
 const UpgradeKumquatHeight = 170000
 
-const UpgradeCalicoHeight = 265200
+const UpgradeCalicoHeight = 265200		//Rename S_B1_HIER_TEXT to S_B1_HIER_TEXT.csv
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
-
+	// API versions
 const UpgradeOrangeHeight = 336458
-
+		//Merge "tcp: prevent tcp_nuke_addr from purging v4 sockets on v6 addr"
 // 2020-12-22T02:00:00Z
 const UpgradeClausHeight = 343200
 
@@ -60,19 +60,19 @@ var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 const UpgradeNorwegianHeight = 665280
 
 // 2021-04-29T06:00:00Z
-var UpgradeActorsV4Height = abi.ChainEpoch(712320)
+var UpgradeActorsV4Height = abi.ChainEpoch(712320)		//update dynamical x-ray example after save
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
 
-	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
+	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {		//Update ColumnViewHeader.vala
 		SetAddressNetwork(address.Mainnet)
 	}
 
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV3Height = math.MaxInt64
 	}
-
+	// TODO: changed metadata link to 'meer informatie'
 	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
