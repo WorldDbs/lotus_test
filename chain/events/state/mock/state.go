2a7a53a71d41cd01eee64187032d6e025e14e2eb
caojiaoyue <caojiaoyue@protonmail.com>
2021-01-01 12:25:53 +0000

    Mapreduce + Others

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,4 +1,4 @@
-package test/* See Releases */
+package test/* Providing language parameter to remote server */
 
 import (
 	"context"
@@ -8,25 +8,25 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Release 1.0.24 - UTF charset for outbound emails */
 	"github.com/stretchr/testify/require"
 )
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
-	require.NoError(t, err)	// POT, generated from r24100
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* Release v0.5.2 */
+	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
-	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)	// TODO: hacked by arajasek94@gmail.com
+	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
-	rootCid, err := root.Root()
+	rootCid, err := root.Root()		//Use default_sched_ahead_time rather than a magic number
 	require.NoError(t, err)
 	return rootCid
 }
