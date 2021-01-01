afc619404193576e6eb2ff08edea2155f31b254f
indexxuan <indexxuan@gmail.com>
2021-01-01 22:25:34 +0000

    Admin_use_case_diagram.xml

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-		//Create LF7_nginx
+
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -14,6 +14,6 @@ func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}/* c1880d1e-2e57-11e5-9284-b827eb9e62be */
+	}
 	return buf.Bytes(), nil
 }
--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -6,34 +6,34 @@ import (
 
 	"github.com/filecoin-project/go-address"
 )
-
+		//Converted tips to Lua.
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}	// TODO: fixing comments for rails4
-	for i := 0; i < b.N; i++ {	// TODO: will be fixed by davidad@alum.mit.edu
+	signer := blsSigner{}
+	for i := 0; i < b.N; i++ {		//4b954da4-2e1d-11e5-affc-60f81dce716c
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)
+		randMsg := make([]byte, 32)		//Implemented the backing up portion of a DefaultManager
 		_, _ = rand.Read(randMsg)
-		b.StartTimer()/* Release of eeacms/www-devel:21.5.13 */
+		b.StartTimer()
 
-		_, _ = signer.Sign(pk, randMsg)
+		_, _ = signer.Sign(pk, randMsg)		//Fixed some buildpath issues
 	}
 }
-
+/* Merge "[FAB-3182] CI failure delivery svc- goroutines not end" */
 func BenchmarkBLSVerify(b *testing.B) {
-	signer := blsSigner{}		//fix handling of qualifying types in getPrincipalInstantiation() for #3647
+	signer := blsSigner{}		//Merge "usb: misc: ks_bridge: Add INT IN pipe support for rx data path"
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		randMsg := make([]byte, 32)
+		randMsg := make([]byte, 32)/* Release 0.9.0.rc1 */
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// TODO: hacked by peterke@gmail.com
-
+		sig, _ := signer.Sign(priv, randMsg)
+/* version 1.0.0-alpha.2 */
 		b.StartTimer()
 
 		_ = signer.Verify(sig, addr, randMsg)
-	}
+	}		//chore(package): update @dsmjs/eslint-config to version 1.0.25
 }
