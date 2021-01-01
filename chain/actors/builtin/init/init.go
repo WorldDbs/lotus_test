2bca5399a351021b798d2ad0994100cad27d72b3
boringland <boringland@protonmail.ch>
2021-01-01 16:51:51 +0000

    Create info4.lua

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -6,23 +6,23 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-	"github.com/ipfs/go-cid"		//Merge branch 'master' of git@github.com:phaus/Bombbear.git
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Optimize the apphost router creation */
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by davidad@alum.mit.edu
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Release 1.0.1 (#20) */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//translation adjusted
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Deleted obsolete templates and removed update script. */
 )
 
-func init() {
+func init() {	// Updated IntelliJ to version 12.1.4
 
 	builtin.RegisterActorState(builtin0.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
@@ -44,7 +44,7 @@ func init() {
 var (
 	Address = builtin4.InitActorAddr
 	Methods = builtin4.MethodsInit
-)
+)/* CERES: Comment out db stuff that's causing errors */
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
@@ -56,7 +56,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load2(store, act.Head)
 
 	case builtin3.InitActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)		//Update and rename Ural to Ural/1079. Maximum.cpp
 
 	case builtin4.InitActorCodeID:
 		return load4(store, act.Head)
@@ -66,21 +66,21 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 }
 
 type State interface {
-	cbor.Marshaler/* font now loaded from file */
-
-	ResolveAddress(address address.Address) (address.Address, bool, error)
+	cbor.Marshaler
+		//New upstream version 1.2.6
+	ResolveAddress(address address.Address) (address.Address, bool, error)/* Latest Infos About New Release */
 	MapAddressToNewID(address address.Address) (address.Address, error)
 	NetworkName() (dtypes.NetworkName, error)
 
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
-
+/* Release areca-5.3 */
 	// Remove exists to support tooling that manipulates state for testing.
 	// It should not be used in production code, as init actor entries are
 	// immutable.
 	Remove(addrs ...address.Address) error
 
 	// Sets the network's name. This should only be used on upgrade/fork.
-	SetNetworkName(name string) error/* Release v4.4.0 */
+	SetNetworkName(name string) error
 
 	addressMap() (adt.Map, error)
 }
