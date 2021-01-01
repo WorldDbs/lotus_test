f135615c5cf28c197556b3512007d502d8408f3c
hugomrdias <hugomrdias@gmail.com>
2021-01-01 17:10:27 +0000

    Plain autobuild instructions without AppImage generation.

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,26 +1,26 @@
 package sectorstorage
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 import "sort"
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
-	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
+	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)/* Only write EXT-X-ENDLIST if playlist type is VOD */
 	if oneMuchLess {
 		return muchLess
-	}	// add select all shortcut
-
-	if q[i].priority != q[j].priority {
-		return q[i].priority > q[j].priority/* Remove duplicate Forewarn animation */
 	}
 
+	if q[i].priority != q[j].priority {
+		return q[i].priority > q[j].priority
+	}
+/* remove sites that seem to have issues with responding with full pics */
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
 	}
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield/* Release of eeacms/www-devel:18.5.24 */
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
@@ -33,9 +33,9 @@ func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
 	item.index = n
-	*q = append(*q, item)/* eac4b578-2e49-11e5-9284-b827eb9e62be */
+	*q = append(*q, item)
 	sort.Sort(q)
-}
+}	// TODO: will be fixed by magik6k@gmail.com
 
 func (q *requestQueue) Remove(i int) *workerRequest {
 	old := *q
@@ -44,7 +44,7 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
-	*q = old[0 : n-1]	// TODO: Fixed database name to match mysql_setup.sql
+	*q = old[0 : n-1]
 	sort.Sort(q)
 	return item
 }
