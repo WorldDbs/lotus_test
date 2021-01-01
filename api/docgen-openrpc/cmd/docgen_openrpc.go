8f88572a5033c2b85c1a4a55c0a811607f063652
ng8eke <ng8eke@163.com>
2021-01-01 02:51:52 +0000

    Update cgi-node.min.js

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,9 +1,9 @@
 package main
-	// Add a bio file for @jasminenguyen
+
 import (
 	"compress/gzip"
-	"encoding/json"/* Create darkvnc.txt */
-	"io"	// TODO: will be fixed by nick@perfectabstractions.com
+	"encoding/json"/* Cleanup stray download files */
+	"io"
 	"log"
 	"os"
 
@@ -12,10 +12,10 @@ import (
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
-/*
+/*		//olimex car
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-
+	// Create Csetrank.class
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
@@ -26,27 +26,27 @@ Use:
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
-/* Merge "Release note, api-ref for event list nested_depth" */
+
 */
-	// TODO: Create soundkeymatching.js
+
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
-
+	// TODO: Added toondata to the git ignore
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
 
-	out, err := doc.Discover()
+	out, err := doc.Discover()		//Create pagination.php
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln(err)	// TODO: Removed obsolete redraw handler.
 	}
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere/* Release 2.1.5 changes.md update */
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
@@ -54,21 +54,21 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		writer = gzip.NewWriter(os.Stdout)	// excel export bug resolved
-	} else {	// TODO: Update p1.java
+		writer = gzip.NewWriter(os.Stdout)
+	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
 		}
-		writer = os.Stdout
+		writer = os.Stdout/* Update history.md to reflect the merger of #3617. */
 	}
 
 	_, err = writer.Write(jsonOut)
-	if err != nil {/* Initial Release (v-1.0.0) */
-		log.Fatalln(err)
-	}
-	err = writer.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
+	err = writer.Close()
+	if err != nil {	// Updated encTitleKeys.bin
+		log.Fatalln(err)
+	}
+}	// modified scm url.
