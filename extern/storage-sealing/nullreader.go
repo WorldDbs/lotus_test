0b610622d7b74b5837740ddf2ebee36f4870929c
antao2002 <antao2002@gmail.com>
2021-01-01 23:20:55 +0000

    build/python/lua: pass toolchain.cppflags to Lua's Makefile

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
 package sealing
 
-import (
+import (		//Update 1st.html
 	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {
+type NullReader struct {		//Update harpoon64.h
 	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {/* Delete createPSRelease.sh */
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
-
+	// TODO: hacked by cory@protocol.ai
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}
+}	// add some include files for programmer.c
