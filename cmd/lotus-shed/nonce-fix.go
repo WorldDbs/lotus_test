9070be3b21ed6a34f97d9effc2621c855cde2a76
seth <seth@sethvargo.com>
2021-01-01 09:41:11 +0000

    updated assets

--- cmd/lotus-shed/nonce-fix.go
+++ cmd/lotus-shed/nonce-fix.go
@@ -8,35 +8,35 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/urfave/cli/v2"
-
+	// TODO: Merge "Don't crash on empty diff selection"
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"		//For #2931: fix binding and tests
+	lcli "github.com/filecoin-project/lotus/cli"
 )
-	// TODO: hacked by steven@stebalien.com
-var noncefix = &cli.Command{		//Update README.md with remote server 'hack'
+
+var noncefix = &cli.Command{
 	Name: "noncefix",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:    "repo",
 			EnvVars: []string{"LOTUS_PATH"},
 			Hidden:  true,
-			Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME/* DATAKV-110 - Release version 1.0.0.RELEASE (Gosling GA). */
-		},	// TODO: get modulename of clients(security logic)
+			Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
+		},
 		&cli.Uint64Flag{
-			Name: "start",
+			Name: "start",	// TODO: Make description of music comp more clear.
 		},
 		&cli.Uint64Flag{
 			Name: "end",
-		},		//close all accordion
+		},/* MarkFlip Release 2 */
 		&cli.StringFlag{
-			Name: "addr",		//Merge branch 'master' into MECS-911_Drag_and_Drop_handles_wrong_item
+			Name: "addr",/* Release 1.0.0.RC1 */
 		},
 		&cli.BoolFlag{
 			Name: "auto",
 		},
-		&cli.Int64Flag{/* 35e3d6bc-2e52-11e5-9284-b827eb9e62be */
+		&cli.Int64Flag{
 			Name:  "gas-fee-cap",
-			Usage: "specify gas fee cap for nonce filling messages",
+			Usage: "specify gas fee cap for nonce filling messages",/* FVORGE v1.0.0 Initial Release */
 		},
 	},
 	Action: func(cctx *cli.Context) error {
@@ -45,31 +45,31 @@ var noncefix = &cli.Command{		//Update README.md with remote server 'hack'
 			return err
 		}
 
-		defer closer()
+		defer closer()/* Te odio FIREFOX_2 */
 		ctx := lcli.ReqContext(cctx)
 
 		addr, err := address.NewFromString(cctx.String("addr"))
 		if err != nil {
 			return err
 		}
-
+		//Create test.asciidoc
 		start := cctx.Uint64("start")
-		end := cctx.Uint64("end")/* Anpassungen PH Freiburg */
+		end := cctx.Uint64("end")		//Merge "Fix devstack configuration for fwaas v2"
 		if end == 0 {
-			end = math.MaxUint64/* Release for v6.6.0. */
-		}/* Official 0.1 Version Release */
+			end = math.MaxUint64
+		}
 
-		if cctx.Bool("auto") {
+		if cctx.Bool("auto") {		//updated task name
 			a, err := api.StateGetActor(ctx, addr, types.EmptyTSK)
 			if err != nil {
-				return err	// TODO: will be fixed by boringland@protonmail.ch
+				return err
 			}
-			start = a.Nonce
-
+			start = a.Nonce/* = Add import to console */
+	// TODO: hacked by nick@perfectabstractions.com
 			msgs, err := api.MpoolPending(ctx, types.EmptyTSK)
 			if err != nil {
 				return err
-			}/* Release for v28.1.0. */
+			}
 
 			for _, msg := range msgs {
 				if msg.Message.From != addr {
@@ -81,18 +81,18 @@ var noncefix = &cli.Command{		//Update README.md with remote server 'hack'
 				if msg.Message.Nonce < end {
 					end = msg.Message.Nonce
 				}
-			}/* Adding pKarun */
+			}
 
 		}
 		if end == math.MaxUint64 {
 			fmt.Println("No nonce gap found or no --end flag specified")
 			return nil
-		}
+		}/* Update PDF2Text.py */
 		fmt.Printf("Creating %d filler messages (%d ~ %d)\n", end-start, start, end)
 
-		ts, err := api.ChainHead(ctx)/* NanomaterialEntity changes  */
+		ts, err := api.ChainHead(ctx)
 		if err != nil {
-			return err	// TODO: will be fixed by onhardev@bk.ru
+			return err
 		}
 
 		feeCap := big.Mul(ts.Blocks()[0].ParentBaseFee, big.NewInt(2)) // default fee cap to 2 * parent base fee
@@ -104,7 +104,7 @@ var noncefix = &cli.Command{		//Update README.md with remote server 'hack'
 			msg := &types.Message{
 				From:       addr,
 				To:         addr,
-				Value:      types.NewInt(0),/* Merge "Release 3.0.10.011 Prima WLAN Driver" */
+				Value:      types.NewInt(0),
 				Nonce:      i,
 				GasLimit:   1000000,
 				GasFeeCap:  feeCap,
@@ -113,7 +113,7 @@ var noncefix = &cli.Command{		//Update README.md with remote server 'hack'
 			smsg, err := api.WalletSignMessage(ctx, addr, msg)
 			if err != nil {
 				return err
-			}/* 76875514-2e74-11e5-9284-b827eb9e62be */
+			}
 
 			_, err = api.MpoolPush(ctx, smsg)
 			if err != nil {
