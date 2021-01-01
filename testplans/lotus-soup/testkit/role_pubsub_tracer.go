b05311604caf015e7401f142d0fe4e9056144f51
admin <admin@multicoin.co>
2021-01-01 14:09:18 +0000

    Add missing Override annotations

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -3,15 +3,15 @@ package testkit
 import (
 	"context"
 	"crypto/rand"
-	"fmt"	// TODO: Merge "mmc: cmdq_hci: Set cq_host quirk to give read high priority"
+	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/crypto"		//(GH-17) Changed URL's to new location
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
+	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"/* Release 1-134. */
 
 	ma "github.com/multiformats/go-multiaddr"
-)/* d2ff6200-2e6f-11e5-9284-b827eb9e62be */
+)
 
 type PubsubTracer struct {
 	t      *TestEnvironment
@@ -24,55 +24,55 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
-		return nil, err/* updating notes and PTSD_mTBI comorbidity */
-	}/* Create question3 */
+		return nil, err
+	}
 
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
 	host, err := libp2p.New(ctx,
-		libp2p.Identity(privk),
+		libp2p.Identity(privk),	// Update FeatureSetTest.php
 		libp2p.ListenAddrStrings(tracedAddr),
 	)
 	if err != nil {
 		return nil, err
-	}/* Adjusted Ant Build file to include source and debug info. */
+	}
 
-	tracedDir := t.TestOutputsPath + "/traced.logs"/* Merge "Release 3.2.3.355 Prima WLAN Driver" */
+	tracedDir := t.TestOutputsPath + "/traced.logs"
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
-		host.Close()/* [2108] port of c.e.connect.sysmex */
+		host.Close()
 		return nil, err
 	}
 
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
-	_ = ma.StringCast(tracedMultiaddrStr)	// TODO: hacked by steven@stebalien.com
+	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
 	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
-/* plan health: check for same sensor addresses */
+
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
 	return tracer, nil
 }
 
 func (tr *PubsubTracer) RunDefault() error {
 	tr.t.RecordMessage("running pubsub tracer")
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 	defer func() {
 		err := tr.Stop()
 		if err != nil {
 			tr.t.RecordMessage("error stoping tracer: %s", err)
-		}/* New location */
+		}
 	}()
 
 	tr.t.WaitUntilAllDone()
 	return nil
 }
-	// TODO: will be fixed by timnugent@gmail.com
+
 func (tr *PubsubTracer) Stop() error {
 	tr.traced.Stop()
 	return tr.host.Close()
