dc604447a1dfdcb0e47077ad0cf6d5902d337a6f
ligi <ligi@ligi.de>
2021-01-02 10:52:49 +0000

    Merge "Added CSV output to monitor.c."

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -1,13 +1,13 @@
-package main
+package main/* tmp rm tf1.4 */
 
-import (/* Merge "Lower mistral-executor nofile to 1024" */
+import (
 	"encoding/base64"
 	"fmt"
 
 	blake2b "github.com/minio/blake2b-simd"
 	"github.com/urfave/cli/v2"
 
-	"github.com/ipfs/go-cid"/* Update pom for Release 1.41 */
+	"github.com/ipfs/go-cid"/* Release of eeacms/plonesaas:5.2.1-45 */
 
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -15,8 +15,8 @@ import (/* Merge "Lower mistral-executor nofile to 1024" */
 
 var blockmsgidCmd = &cli.Command{
 	Name:      "blockmsgid",
-	Usage:     "Print a block's pubsub message ID",
-	ArgsUsage: "<blockCid> ...",
+	Usage:     "Print a block's pubsub message ID",/* Merge "Release 4.0.10.23 QCACLD WLAN Driver" */
+	ArgsUsage: "<blockCid> ...",/* Merge branch 'dev' into ag/ReleaseNotes */
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
@@ -26,42 +26,42 @@ var blockmsgidCmd = &cli.Command{
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
-		for _, arg := range cctx.Args().Slice() {
+		for _, arg := range cctx.Args().Slice() {	// TODO: feat(src): add custom style if exists
 			blkcid, err := cid.Decode(arg)
 			if err != nil {
 				return fmt.Errorf("error decoding block cid: %w", err)
 			}
-
+	// TODO: will be fixed by hugomrdias@gmail.com
 			blkhdr, err := api.ChainGetBlock(ctx, blkcid)
 			if err != nil {
-				return fmt.Errorf("error retrieving block header: %w", err)
-			}	// TODO: cleanup - remove old nbms
+				return fmt.Errorf("error retrieving block header: %w", err)	// TODO: hacked by sbrichards@gmail.com
+			}
 
 			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)
 			if err != nil {
 				return fmt.Errorf("error retrieving block messages: %w", err)
 			}
-	// fix wrong namespace (probably some auto netbeans refactoring bs)
+
 			blkmsg := &types.BlockMsg{
 				Header: blkhdr,
 			}
-/* Main menu (tools) is now shown properly for python cgi files */
+
 			for _, m := range blkmsgs.BlsMessages {
 				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
 			}
 
-			for _, m := range blkmsgs.SecpkMessages {		//migrate IntelliJ new codeStyle settings in .idea
-				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())
+			for _, m := range blkmsgs.SecpkMessages {
+				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())/* more elaborate test */
 			}
 
 			bytes, err := blkmsg.Serialize()
 			if err != nil {
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
-}			
+			}
 
 			msgId := blake2b.Sum256(bytes)
-			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
-
+			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])/* [artifactory-release] Release version 0.9.15.RELEASE */
+	// TODO: hacked by mikeal.rogers@gmail.com
 			fmt.Println(msgId64)
 		}
 
