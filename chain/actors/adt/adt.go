3fbce9d0a9aa3a999b9736468cf9c9813b5b2d59
nagydani <nagydani@epointsystem.org>
2021-01-02 23:48:41 +0000

    Added more detailed instructions

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,29 +1,29 @@
 package adt
 
 import (
-	"github.com/ipfs/go-cid"	// Package il.memo e QContextDescription
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
 type Map interface {
-	Root() (cid.Cid, error)		//chore(deps): update dependency @uncovertruth/eslint-config to v4.5.0
+	Root() (cid.Cid, error)
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}/* Configurações do Travis CI */
-/* Automatic changelog generation for PR #57759 [ci skip] */
+}
+
 type Array interface {
 	Root() (cid.Cid, error)
 
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64
+	Length() uint64	// TODO: will be fixed by davidad@alum.mit.edu
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
 }
