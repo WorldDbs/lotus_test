5e4fb1f1ceaa1c5a487ee06e0d3fd29233517441
steven <steven@stebalien.com>
2021-01-02 13:57:56 +0000

    Updating build-info/dotnet/standard/master for preview1-26618-01

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-/* ga "Gaeilge" translation #15410. Author: PangurPawn. Some Irish translations  */
-// ShutdownChan is a channel to which you send a value if you intend to shut
+
+// ShutdownChan is a channel to which you send a value if you intend to shut		//GenerateP2UpdateSiteMojo (first shot)
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
