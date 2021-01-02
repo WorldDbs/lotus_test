f8a37d5e0fc8980df9e829ccca4b5d21ba5f281c
ng8eke <ng8eke@163.com>
2021-01-02 02:53:08 +0000

    added a fountain

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,7 +1,7 @@
 package api
-
+/* Reformat README to 80 columns */
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"
+	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: hacked by remco@dutchcoders.io
 )
 
 const (
@@ -10,14 +10,14 @@ const (
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
-	PermAdmin auth.Permission = "admin" // Manage permissions
-)
-	// TODO: 36f8d61c-2e4b-11e5-9284-b827eb9e62be
+	PermAdmin auth.Permission = "admin" // Manage permissions/* Changed help texts, for more information see Issue#313. */
+)		//Enable Edit Embedded Template, update index.php and css
+
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
-
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
+	// TODO: Merge branch 'master' into fixes/748-blame-link
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* fix dummy async implementations for non-GHC */
+	var out StorageMinerStruct		//disable scrolling on sign up iframe
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
@@ -26,18 +26,18 @@ func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: Improve toolbar layout on server list screen
 	return &out
-}		//Merge "Remove roles"
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//Rename testPushPopSpeed to testPushPopPerformance
 	return &out
 }
 
-func PermissionedWalletAPI(a Wallet) Wallet {
-	var out WalletStruct
+func PermissionedWalletAPI(a Wallet) Wallet {	// Don't try to create the table if it already exists
+	var out WalletStruct	// TODO: will be fixed by magik6k@gmail.com
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
-}
+}/* (jam) Release 2.1.0b1 */
