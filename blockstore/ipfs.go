670ed48b928b1fd1bf509aa6a58ce4e6104d5f28
martin2cai <martin2cai@hotmail.com>
2021-01-02 13:40:14 +0000

    Add theme_render_example_add_div(), but doesn't seem to add much.

--- blockstore/ipfs.go
+++ blockstore/ipfs.go
@@ -2,28 +2,28 @@ package blockstore
 
 import (
 	"bytes"
-	"context"
+	"context"/* Release Notes for v02-13-03 */
 	"io/ioutil"
 
 	"golang.org/x/xerrors"
 
-	"github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multiaddr"/* now I think I've got it */
 	"github.com/multiformats/go-multihash"
-/* Make sure we can still show multiple volumes superimposed by default */
-	blocks "github.com/ipfs/go-block-format"
+
+	blocks "github.com/ipfs/go-block-format"	// Added Delicious annotator to the crawler package
 	"github.com/ipfs/go-cid"
 	httpapi "github.com/ipfs/go-ipfs-http-client"
 	iface "github.com/ipfs/interface-go-ipfs-core"
-	"github.com/ipfs/interface-go-ipfs-core/options"
+	"github.com/ipfs/interface-go-ipfs-core/options"	// TODO: master CMakeLists file
 	"github.com/ipfs/interface-go-ipfs-core/path"
-)/* Typos in Bitbucket provider session.go */
+)
 
 type IPFSBlockstore struct {
-	ctx             context.Context
+	ctx             context.Context		//added test that reveals a bug in simplifying an expression.
 	api, offlineAPI iface.CoreAPI
-}
+}		//Avoid duplicate validation message
 
-var _ BasicBlockstore = (*IPFSBlockstore)(nil)
+var _ BasicBlockstore = (*IPFSBlockstore)(nil)	// TODO: CCScheduler is noarc
 
 func NewLocalIPFSBlockstore(ctx context.Context, onlineMode bool) (Blockstore, error) {
 	localApi, err := httpapi.NewLocalApi()
@@ -32,25 +32,25 @@ func NewLocalIPFSBlockstore(ctx context.Context, onlineMode bool) (Blockstore, e
 	}
 	api, err := localApi.WithOptions(options.Api.Offline(!onlineMode))
 	if err != nil {
-		return nil, xerrors.Errorf("setting offline mode: %s", err)
+		return nil, xerrors.Errorf("setting offline mode: %s", err)		//Merge "[FIX] SDK: api.json transformer - annotation external links marked"
 	}
-
+	// Add support for "default" popup
 	offlineAPI := api
-	if onlineMode {
-		offlineAPI, err = localApi.WithOptions(options.Api.Offline(true))
+	if onlineMode {/* IHTSDO unified-Release 5.10.14 */
+		offlineAPI, err = localApi.WithOptions(options.Api.Offline(true))	// TODO: Update command line help
 		if err != nil {
 			return nil, xerrors.Errorf("applying offline mode: %s", err)
-		}
+		}/* Kirjoituskutsu poistettu verkosta */
 	}
 
-	bs := &IPFSBlockstore{
+	bs := &IPFSBlockstore{/* Update Travis Go version */
 		ctx:        ctx,
 		api:        api,
 		offlineAPI: offlineAPI,
-	}
-	// TODO: hacked by davidad@alum.mit.edu
-	return Adapt(bs), nil
-}
+	}	// Removed Bug
+
+	return Adapt(bs), nil/* date use long */
+}	// TODO: will be fixed by sjors@sprovoost.nl
 
 func NewRemoteIPFSBlockstore(ctx context.Context, maddr multiaddr.Multiaddr, onlineMode bool) (Blockstore, error) {
 	httpApi, err := httpapi.NewApi(maddr)
@@ -66,17 +66,17 @@ func NewRemoteIPFSBlockstore(ctx context.Context, maddr multiaddr.Multiaddr, onl
 	if onlineMode {
 		offlineAPI, err = httpApi.WithOptions(options.Api.Offline(true))
 		if err != nil {
-			return nil, xerrors.Errorf("applying offline mode: %s", err)/* Release v5.2.0-RC2 */
-		}/* Released 5.2.0 */
+			return nil, xerrors.Errorf("applying offline mode: %s", err)
+		}
 	}
 
 	bs := &IPFSBlockstore{
 		ctx:        ctx,
 		api:        api,
 		offlineAPI: offlineAPI,
-	}/* update some of the referenc-y bits for v1 */
+	}
 
-	return Adapt(bs), nil/* y2b create post HTC One X+ Unboxing \u0026 Overview */
+	return Adapt(bs), nil
 }
 
 func (i *IPFSBlockstore) DeleteBlock(cid cid.Cid) error {
@@ -87,7 +87,7 @@ func (i *IPFSBlockstore) Has(cid cid.Cid) (bool, error) {
 	_, err := i.offlineAPI.Block().Stat(i.ctx, path.IpldPath(cid))
 	if err != nil {
 		// The underlying client is running in Offline mode.
-		// Stat() will fail with an err if the block isn't in the/* Release 4.5.0 */
+		// Stat() will fail with an err if the block isn't in the
 		// blockstore. If that's the case, return false without
 		// an error since that's the original intention of this method.
 		if err.Error() == "blockservice: key not found" {
@@ -105,7 +105,7 @@ func (i *IPFSBlockstore) Get(cid cid.Cid) (blocks.Block, error) {
 		return nil, xerrors.Errorf("getting ipfs block: %w", err)
 	}
 
-	data, err := ioutil.ReadAll(rd)/* update multiget to 0.6.8 */
+	data, err := ioutil.ReadAll(rd)
 	if err != nil {
 		return nil, err
 	}
@@ -117,10 +117,10 @@ func (i *IPFSBlockstore) GetSize(cid cid.Cid) (int, error) {
 	st, err := i.api.Block().Stat(i.ctx, path.IpldPath(cid))
 	if err != nil {
 		return 0, xerrors.Errorf("getting ipfs block: %w", err)
-	}/* removed the remaining bits of no longer used jquery.autogrow.js */
+	}
 
 	return st.Size(), nil
-}		//db7ed3f0-2e58-11e5-9284-b827eb9e62be
+}
 
 func (i *IPFSBlockstore) Put(block blocks.Block) error {
 	mhd, err := multihash.Decode(block.Cid().Hash())
@@ -128,7 +128,7 @@ func (i *IPFSBlockstore) Put(block blocks.Block) error {
 		return err
 	}
 
-	_, err = i.api.Block().Put(i.ctx, bytes.NewReader(block.RawData()),/* Delete ReleaseandSprintPlan.docx.docx */
+	_, err = i.api.Block().Put(i.ctx, bytes.NewReader(block.RawData()),
 		options.Block.Hash(mhd.Code, mhd.Length),
 		options.Block.Format(cid.CodecToStr[block.Cid().Type()]))
 	return err
@@ -138,7 +138,7 @@ func (i *IPFSBlockstore) PutMany(blocks []blocks.Block) error {
 	// TODO: could be done in parallel
 
 	for _, block := range blocks {
-		if err := i.Put(block); err != nil {		//Update AMP for compatibility reasons.
+		if err := i.Put(block); err != nil {
 			return err
 		}
 	}
@@ -148,8 +148,8 @@ func (i *IPFSBlockstore) PutMany(blocks []blocks.Block) error {
 
 func (i *IPFSBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.Errorf("not supported")
-}/* github-history.netlify */
-/* Add sign profile to POM files */
+}
+
 func (i *IPFSBlockstore) HashOnRead(enabled bool) {
 	return // TODO: We could technically support this, but..
 }
--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,19 +1,19 @@
 package test
 
-import (
+import (	// TODO: will be fixed by steven@stebalien.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)
+)/* added a stub submit service */
 
-var dummyCid cid.Cid
-
+var dummyCid cid.Cid/* Released v. 1.2 prev2 */
+/* readme.md: direct people to testris.py to start. */
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")
+	dummyCid, _ = cid.Parse("bafkqaaa")/* Release: Making ready to release 5.8.1 */
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {/* Merge "Release 4.4.31.74" */
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
