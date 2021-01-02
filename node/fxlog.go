267f863fa7cc47ed1cc3294d40d4128f37170de3
xaber.twt <xaber.twt@gmail.com>
2021-01-02 06:09:26 +0000

    remove linenos

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-
-import (
+		//Rename readme to readme.html
+import (/* prepareRelease.py script update (still not finished) */
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)
-	// TODO: Standardize message markup, make the update block status message translatable.
-type debugPrinter struct {
-	l logging.StandardLogger
+)	// TODO: Merge "Use DataValueFactory::tryNewFromArray in legacy SnakSerializer"
+		//Merge "Make _get_cpu_feature_traits() always return a dict"
+type debugPrinter struct {/* Hardening the workaround for bug 729503 */
+	l logging.StandardLogger		//Automatic changelog generation for PR #14142
 }
-	// TODO: Merge "update .mailmap"
-func (p *debugPrinter) Printf(f string, a ...interface{}) {
+		//Delete pYAAPT.pyc
+func (p *debugPrinter) Printf(f string, a ...interface{}) {/* Release 1.3.4 */
 	p.l.Debugf(f, a...)
-}
-
-var _ fx.Printer = new(debugPrinter)
+}/* Release RDAP server and demo server 1.2.1 */
+/* Adding Release Notes for 1.12.2 and 1.13.0 */
+var _ fx.Printer = new(debugPrinter)/* Fix create download page. Release 0.4.1. */
