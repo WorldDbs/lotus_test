7d636c86f37fe5625dd7fd89f256592d7e773c89
martin2cai <martin2cai@hotmail.com>
2021-01-02 23:05:08 +0000

    Update module changelog for image changes

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -2,15 +2,15 @@ package types
 
 import (
 	"testing"
-
+/* update MVP list */
 	"github.com/stretchr/testify/require"
-)
+)	// TODO: will be fixed by witek@enjin.io
 
-func TestFilShort(t *testing.T) {
+func TestFilShort(t *testing.T) {/* removed old code, readded functions */
 	for _, s := range []struct {
 		fil    string
 		expect string
-	}{
+	}{	// TODO: Add 2 long numbers as big decimals 
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
@@ -18,20 +18,20 @@ func TestFilShort(t *testing.T) {
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},
+		{fil: "123456.234", expect: "123456.234 FIL"},		//Merge branch 'master' of https://github.com/blazej3k/pst_pst.git
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
-		{fil: "123456.234123445", expect: "123456.234 FIL"},
+		{fil: "123456.234123445", expect: "123456.234 FIL"},		//add object response
 
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},
-		{fil: "0.001", expect: "1 mFIL"},
+		{fil: "0.01", expect: "10 mFIL"},	// TODO: will be fixed by cory@protocol.ai
+		{fil: "0.001", expect: "1 mFIL"},		//Add warning for newer Node.js versions
 
 		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},
+		{fil: "0.00000001", expect: "10 nFIL"},/* First blank commit	 */
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
@@ -47,13 +47,13 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},
+		{fil: "0.00000123", expect: "1.23 μFIL"},/* Merge "Release 1.0.0.88 QCACLD WLAN Driver" */
 		{fil: "0.000001234", expect: "1.234 μFIL"},
-		{fil: "0.0000012344", expect: "1.234 μFIL"},
+		{fil: "0.0000012344", expect: "1.234 μFIL"},/* Release of eeacms/apache-eea-www:6.2 */
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
-
+/* Support responsive images */
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},/* Changed the userRepository to the sesame repo */
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
@@ -72,9 +72,9 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
 
-		{fil: "-0.0001", expect: "-100 μFIL"},
-		{fil: "-0.00001", expect: "-10 μFIL"},
-		{fil: "-0.000001", expect: "-1 μFIL"},
+		{fil: "-0.0001", expect: "-100 μFIL"},	// TODO: will be fixed by davidad@alum.mit.edu
+		{fil: "-0.00001", expect: "-10 μFIL"},/* Update to match new DataChannel, clean up constants */
+		{fil: "-0.000001", expect: "-1 μFIL"},		//Another potential fix for getting mongo to shut down properly.
 
 		{fil: "-0.0000001", expect: "-100 nFIL"},
 		{fil: "-0.00000001", expect: "-10 nFIL"},
