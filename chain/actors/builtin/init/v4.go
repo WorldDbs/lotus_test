d8a6d197c50b21f1d1041dccdc5bc57d19d849b6
souzau <souzau@yandex.com>
2021-01-02 08:29:12 +0000

    Added zip_safe flag to setup.py and removed unneeded package_dir statement.

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -1,6 +1,6 @@
 package init
 
-import (		//Rename 1.html to 1.txt
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -9,7 +9,7 @@ import (		//Rename 1.html to 1.txt
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-/* Update toWPA2E.sh */
+
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
@@ -33,24 +33,24 @@ type state4 struct {
 }
 
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)	// Update cleanup-merged-branches-git.md
+	return s.State.ResolveAddress(s.store, address)
 }
 
 func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
-}	// TODO: Webhooks no longer used.
+}
 
 func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
-	addrs, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)	// Merge branch 'master' of https://github.com/Adouairy/RolandGarros.git
+	addrs, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}/* Release of eeacms/jenkins-master:2.263.2 */
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err	// Rename NameplatesThreat.toc to NamePlatesThreat.toc
-		}/* Tidy up distance calculation method to use Models instead of Foursquare objects */
+			return err
+		}
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
@@ -79,8 +79,8 @@ func (s *state4) Remove(addrs ...address.Address) (err error) {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
 	s.State.AddressMap = amr
-	return nil/* Fix line breaks in readme. */
-}		//Created ComponentInitializer
+	return nil
+}
 
 func (s *state4) addressMap() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.AddressMap, builtin4.DefaultHamtBitwidth)
