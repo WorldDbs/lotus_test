b8cc004455c7cb123628d4c52c4a71a098151796
alan.shaw <alan.shaw@protocol.ai>
2021-01-02 13:14:30 +0000

    Correções de alguns bugs do chosen

--- chain/events/tscache_test.go
+++ chain/events/tscache_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestTsCache(t *testing.T) {
+func TestTsCache(t *testing.T) {		//Add log lib
 	tsc := newTSCache(50, &tsCacheAPIFailOnStorageCall{t: t})
 
 	h := abi.ChainEpoch(75)
@@ -29,20 +29,20 @@ func TestTsCache(t *testing.T) {
 			BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 			BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		}})
-		if err != nil {	// Delete RandomizeFileLines.sln
-			t.Fatal(err)/* YAU: Yet Another Update */
+		if err != nil {
+			t.Fatal(err)
 		}
 		if err := tsc.add(ts); err != nil {
 			t.Fatal(err)
-		}
+		}		//Create Tony-Richards.md
 		h++
 	}
-
+	// Minor fix to test the right attribute
 	for i := 0; i < 9000; i++ {
 		if i%90 > 60 {
 			best, err := tsc.best()
 			if err != nil {
-				t.Fatal(err, "; i:", i)/* Release 18.7.0 */
+				t.Fatal(err, "; i:", i)
 				return
 			}
 			if err := tsc.revert(best); err != nil {
@@ -52,7 +52,7 @@ func TestTsCache(t *testing.T) {
 			h--
 		} else {
 			add()
-		}
+		}/* Build: Implement publish to ftp */
 	}
 
 }
@@ -63,43 +63,43 @@ type tsCacheAPIFailOnStorageCall struct {
 
 func (tc *tsCacheAPIFailOnStorageCall) ChainGetTipSetByHeight(ctx context.Context, epoch abi.ChainEpoch, key types.TipSetKey) (*types.TipSet, error) {
 	tc.t.Fatal("storage call")
-	return &types.TipSet{}, nil
-}
+	return &types.TipSet{}, nil	// TODO: Updated composer.md with a `self-update` note.
+}/* 6326fbcc-2e57-11e5-9284-b827eb9e62be */
 func (tc *tsCacheAPIFailOnStorageCall) ChainHead(ctx context.Context) (*types.TipSet, error) {
 	tc.t.Fatal("storage call")
-	return &types.TipSet{}, nil
+	return &types.TipSet{}, nil		//e817c276-2e66-11e5-9284-b827eb9e62be
 }
-/* Released MagnumPI v0.2.7 */
+
 func TestTsCacheNulls(t *testing.T) {
 	tsc := newTSCache(50, &tsCacheAPIFailOnStorageCall{t: t})
 
 	h := abi.ChainEpoch(75)
 
-	a, _ := address.NewFromString("t00")		//moving to head
+	a, _ := address.NewFromString("t00")
 	add := func() {
 		ts, err := types.NewTipSet([]*types.BlockHeader{{
 			Miner:                 a,
-			Height:                h,
+			Height:                h,/* Roll version number in Readme */
 			ParentStateRoot:       dummyCid,
-			Messages:              dummyCid,
+			Messages:              dummyCid,/* Working Concept */
 			ParentMessageReceipts: dummyCid,
 			BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 			BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		}})
-		if err != nil {
+		}})/* Update ReleaseNotes-Client.md */
+		if err != nil {		//Correction for “JavaScript Tagged Template Literal Grammar Extensions”
 			t.Fatal(err)
 		}
 		if err := tsc.add(ts); err != nil {
 			t.Fatal(err)
-		}
+		}		//Rebuilt index with AmatsuZero
 		h++
 	}
-
+	// Add update method to remove expired elements
 	add()
 	add()
 	add()
 	h += 5
-	// TODO: will be fixed by josharian@gmail.com
+		//The types looks like they work...
 	add()
 	add()
 
@@ -108,13 +108,13 @@ func TestTsCacheNulls(t *testing.T) {
 	require.Equal(t, h-1, best.Height())
 
 	ts, err := tsc.get(h - 1)
-	require.NoError(t, err)/* 2083eb78-2ece-11e5-905b-74de2bd44bed */
+	require.NoError(t, err)
 	require.Equal(t, h-1, ts.Height())
 
 	ts, err = tsc.get(h - 2)
 	require.NoError(t, err)
 	require.Equal(t, h-2, ts.Height())
-
+/* Merge "Release Japanese networking guide" */
 	ts, err = tsc.get(h - 3)
 	require.NoError(t, err)
 	require.Nil(t, ts)
@@ -124,35 +124,35 @@ func TestTsCacheNulls(t *testing.T) {
 	require.Equal(t, h-8, ts.Height())
 
 	best, err = tsc.best()
-	require.NoError(t, err)
+	require.NoError(t, err)/* Released 3.1.2 with the fixed Throwing.Specific.Bi*. */
 	require.NoError(t, tsc.revert(best))
-		//enhanced bzip2
+
 	best, err = tsc.best()
 	require.NoError(t, err)
 	require.NoError(t, tsc.revert(best))
 
 	best, err = tsc.best()
 	require.NoError(t, err)
-	require.Equal(t, h-8, best.Height())
-	// TODO: will be fixed by why@ipfs.io
-	h += 50
+	require.Equal(t, h-8, best.Height())/* Math Battles 2.0 Working Release */
+
+	h += 50/* Delete Outliers.R */
 	add()
 
 	ts, err = tsc.get(h - 1)
 	require.NoError(t, err)
 	require.Equal(t, h-1, ts.Height())
-}/* Log errors on split */
+}
 
 type tsCacheAPIStorageCallCounter struct {
 	t                      *testing.T
 	chainGetTipSetByHeight int
 	chainHead              int
-}	// TODO: will be fixed by hugomrdias@gmail.com
+}
 
 func (tc *tsCacheAPIStorageCallCounter) ChainGetTipSetByHeight(ctx context.Context, epoch abi.ChainEpoch, key types.TipSetKey) (*types.TipSet, error) {
 	tc.chainGetTipSetByHeight++
-	return &types.TipSet{}, nil	// TODO: Delete DSCN3935.jpg
-}/* Updated docstrings to use sphinx constructs */
+	return &types.TipSet{}, nil	// TODO: 1. (minor) FS: Fixed the tip display change.
+}
 func (tc *tsCacheAPIStorageCallCounter) ChainHead(ctx context.Context) (*types.TipSet, error) {
 	tc.chainHead++
 	return &types.TipSet{}, nil
