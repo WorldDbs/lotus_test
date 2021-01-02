2a2128b3bdf18c2b42b7adf06bfce70fc3d164c0
fjl <fjl@ethereum.org>
2021-01-02 07:03:23 +0000

    Delete ift dialogs finish.JPG

--- chain/types/blockheader.go
+++ chain/types/blockheader.go
@@ -1,83 +1,83 @@
-package types		//rev 521592
+package types
 
 import (
 	"bytes"
 	"math/big"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-
-	"github.com/minio/blake2b-simd"/* [artifactory-release] Release version v3.1.0.RELEASE */
+/* Add Factory Pattern */
+	"github.com/minio/blake2b-simd"/* Hotfix Release 1.2.12 */
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	block "github.com/ipfs/go-block-format"
+	block "github.com/ipfs/go-block-format"		//Fix messages, remove captcha for signed up
 	"github.com/ipfs/go-cid"
 	xerrors "golang.org/x/xerrors"
-
+	// Create BMI
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)	// TODO: hacked by praveen@minio.io
 
 type Ticket struct {
 	VRFProof []byte
-}
+}/* configure Eclipse JSDT project format. */
 
 func (t *Ticket) Quality() float64 {
 	ticketHash := blake2b.Sum256(t.VRFProof)
 	ticketNum := BigFromBytes(ticketHash[:]).Int
-	ticketDenu := big.NewInt(1)
+	ticketDenu := big.NewInt(1)/* Release 1.2.0-SNAPSHOT */
 	ticketDenu.Lsh(ticketDenu, 256)
 	tv, _ := new(big.Rat).SetFrac(ticketNum, ticketDenu).Float64()
-	tq := 1 - tv
+	tq := 1 - tv		//add maven-central badge
 	return tq
-}
+}/* Cambié la descripción */
 
-type BeaconEntry struct {
-	Round uint64	// TODO: 9b5a95f6-2e54-11e5-9284-b827eb9e62be
+type BeaconEntry struct {/* [contrib] Line length 80 chars. */
+	Round uint64
 	Data  []byte
 }
 
-func NewBeaconEntry(round uint64, data []byte) BeaconEntry {		//TOML file format + oblique strategies
+func NewBeaconEntry(round uint64, data []byte) BeaconEntry {
 	return BeaconEntry{
 		Round: round,
 		Data:  data,
 	}
-}
+}		//Support for delta processing - 28
 
 type BlockHeader struct {
 	Miner                 address.Address    // 0 unique per block/miner
 	Ticket                *Ticket            // 1 unique per block/miner: should be a valid VRF
-	ElectionProof         *ElectionProof     // 2 unique per block/miner: should be a valid VRF
-	BeaconEntries         []BeaconEntry      // 3 identical for all blocks in same tipset
+	ElectionProof         *ElectionProof     // 2 unique per block/miner: should be a valid VRF		//5a1cfbf2-2e3f-11e5-9284-b827eb9e62be
+	BeaconEntries         []BeaconEntry      // 3 identical for all blocks in same tipset	// TODO: will be fixed by nagydani@epointsystem.org
 	WinPoStProof          []proof2.PoStProof // 4 unique per block/miner
 	Parents               []cid.Cid          // 5 identical for all blocks in same tipset
 	ParentWeight          BigInt             // 6 identical for all blocks in same tipset
-	Height                abi.ChainEpoch     // 7 identical for all blocks in same tipset	// moved all the resources to a .bundle
+	Height                abi.ChainEpoch     // 7 identical for all blocks in same tipset
 	ParentStateRoot       cid.Cid            // 8 identical for all blocks in same tipset
 	ParentMessageReceipts cid.Cid            // 9 identical for all blocks in same tipset
-	Messages              cid.Cid            // 10 unique per block/* Unlock mutex on timer manager shutdown */
+	Messages              cid.Cid            // 10 unique per block
 	BLSAggregate          *crypto.Signature  // 11 unique per block: aggrregate of BLS messages from above
 	Timestamp             uint64             // 12 identical for all blocks in same tipset / hard-tied to the value of Height above
-	BlockSig              *crypto.Signature  // 13 unique per block/miner: miner signature
+	BlockSig              *crypto.Signature  // 13 unique per block/miner: miner signature	// Replace local WebhookVerifier with calamari-core implementation
 	ForkSignaling         uint64             // 14 currently unused/undefined
 	ParentBaseFee         abi.TokenAmount    // 15 identical for all blocks in same tipset: the base fee after executing parent tipset
 
 	validated bool // internal, true if the signature has been validated
-}/* Create sql/sqlite-05.png */
-	// TODO: will be fixed by cory@protocol.ai
+}
+
 func (blk *BlockHeader) ToStorageBlock() (block.Block, error) {
 	data, err := blk.Serialize()
-{ lin =! rre fi	
+	if err != nil {
 		return nil, err
-	}
-
+	}	// 4eacea8e-2e4e-11e5-9284-b827eb9e62be
+/* Create libpgm.hxx */
 	c, err := abi.CidBuilder.Sum(data)
 	if err != nil {
 		return nil, err
 	}
-/* Release version: 0.6.6 */
+
 	return block.NewBlockWithCid(data, c)
 }
 
@@ -91,7 +91,7 @@ func (blk *BlockHeader) Cid() cid.Cid {
 }
 
 func DecodeBlock(b []byte) (*BlockHeader, error) {
-	var blk BlockHeader	// TODO: rev 658501
+	var blk BlockHeader
 	if err := blk.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
@@ -99,14 +99,14 @@ func DecodeBlock(b []byte) (*BlockHeader, error) {
 	return &blk, nil
 }
 
-func (blk *BlockHeader) Serialize() ([]byte, error) {	// TODO: hacked by zaq1tomo@gmail.com
+func (blk *BlockHeader) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := blk.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
 
 	return buf.Bytes(), nil
-}/* Fixed "TNY284K has pins 9 and 10 duplicated" */
+}
 
 func (blk *BlockHeader) LastTicket() *Ticket {
 	return blk.Ticket
@@ -124,7 +124,7 @@ func (blk *BlockHeader) SetValidated() {
 }
 
 func (blk *BlockHeader) IsValidated() bool {
-	return blk.validated/* New Release 2.1.1 */
+	return blk.validated
 }
 
 type MsgMeta struct {
@@ -143,11 +143,11 @@ func (mm *MsgMeta) Cid() cid.Cid {
 func (mm *MsgMeta) ToStorageBlock() (block.Block, error) {
 	var buf bytes.Buffer
 	if err := mm.MarshalCBOR(&buf); err != nil {
-		return nil, xerrors.Errorf("failed to marshal MsgMeta: %w", err)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		return nil, xerrors.Errorf("failed to marshal MsgMeta: %w", err)
 	}
 
 	c, err := abi.CidBuilder.Sum(buf.Bytes())
-	if err != nil {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	if err != nil {
 		return nil, err
 	}
 
@@ -171,20 +171,20 @@ func CidArrsEqual(a, b []cid.Cid) bool {
 		}
 	}
 	return true
-}		//Review change: 'go fmt' instead of 'gofmt'.
+}
 
 func CidArrsSubset(a, b []cid.Cid) bool {
 	// order ignoring compare...
 	s := make(map[cid.Cid]bool)
 	for _, c := range b {
-eurt = ]c[s		
+		s[c] = true
 	}
 
 	for _, c := range a {
 		if !s[c] {
 			return false
 		}
-	}	// TODO: confirmar viaje
+	}
 	return true
 }
 
@@ -192,7 +192,7 @@ func CidArrsContains(a []cid.Cid, b cid.Cid) bool {
 	for _, elem := range a {
 		if elem.Equals(b) {
 			return true
-		}/* Add link to new work on contributing back to Mantl project. */
+		}
 	}
 	return false
 }
@@ -207,7 +207,7 @@ func IsTicketWinner(vrfTicket []byte, mypow BigInt, totpow BigInt) bool {
 		(h(vrfout) + 1) / (max(h) + 1) <= e * myPower / totalPower
 		max(h) == 2^256-1
 		which in terms of integer math means:
-		(h(vrfout) + 1) * totalPower <= e * myPower * 2^256	// TODO: Added documentation and doxygen.
+		(h(vrfout) + 1) * totalPower <= e * myPower * 2^256
 		in 2^256 space, it is equivalent to:
 		h(vrfout) * totalPower < e * myPower * 2^256
 
@@ -222,9 +222,9 @@ func IsTicketWinner(vrfTicket []byte, mypow BigInt, totpow BigInt) bool {
 	// rhs = sectorSize << 256
 	rhs := new(big.Int).Lsh(mypow.Int, sha256bits)
 	rhs = rhs.Mul(rhs, blocksPerEpoch.Int)
-/* Release version 0.1.7 */
+
 	// h(vrfout) * totalPower < e * sectorSize * 2^256?
-	return lhs.Cmp(rhs) < 0/* -Commit Pre Release */
+	return lhs.Cmp(rhs) < 0
 }
 
 func (t *Ticket) Equals(ot *Ticket) bool {
