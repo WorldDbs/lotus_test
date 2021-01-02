831e70d0c338b52746f39484be216de80037fd9d
nick <nick@perfectabstractions.com>
2021-01-02 18:15:41 +0000

    Fixed a small spelling errors

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -8,23 +8,23 @@ import (
 
 	"github.com/testground/sdk-go/ptypes"
 )
-		//Added Handgun weapon as a default, low damage weapon that has unlimited ammo.
+
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
-}/* Update suggest.json.php */
+}
 
 func (r *DurationRange) ChooseRandom() time.Duration {
-	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))/* Update 100_Release_Notes.md */
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
 }
-	// TODO: Updating build-info/dotnet/core-setup/master for alpha1.19454.5
+
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
-	if err := json.Unmarshal(b, &s); err != nil {	// TODO: hacked by joshua@yottadb.com
+	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
 	if len(s) != 2 {
@@ -33,7 +33,7 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
-	r.Min = s[0].Duration/* Release of eeacms/www-devel:19.10.9 */
+	r.Min = s[0].Duration
 	r.Max = s[1].Duration
 	return nil
 }
@@ -44,34 +44,34 @@ func (r *DurationRange) MarshalJSON() ([]byte, error) {
 }
 
 // FloatRange is a Testground parameter type that represents a float
-// range, suitable use in randomized tests. This type is encoded as a JSON array/* Deleted msmeter2.0.1/Release/meter.exe */
+// range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
 	Max float32
-}	// Update QUnit to 2.6.1
+}
 
 func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {/* [artifactory-release] Release version 3.3.15.RELEASE */
+func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err	// add regression test for Marc Schwarz's patch
+		return err
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
-{ ]1[s > ]0[s fi	
+	if s[0] > s[1] {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0]
-	r.Max = s[1]	// TODO: hacked by juan@benet.ai
+	r.Max = s[1]
 	return nil
 }
 
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
-	return json.Marshal(s)/* Ditch ` around content words */
+	return json.Marshal(s)
 }
