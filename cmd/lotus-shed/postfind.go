3758cf9f09b56f9942fda84deb51cf0eccca7bcc
lexy8russo <lexy8russo@outlook.com>
2021-01-02 14:35:57 +0000

    New post on entrypoint

--- cmd/lotus-shed/postfind.go
+++ cmd/lotus-shed/postfind.go
@@ -12,24 +12,24 @@ import (
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/urfave/cli/v2"
 )
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 var postFindCmd = &cli.Command{
 	Name:        "post-find",
 	Description: "return addresses of all miners who have over zero power and have posted in the last day",
 	Flags: []cli.Flag{
-		&cli.StringFlag{	// TODO: will be fixed by timnugent@gmail.com
-			Name:  "tipset",	// Change font colour of discount policy title.
+		&cli.StringFlag{
+			Name:  "tipset",
 			Usage: "specify tipset state to search on",
 		},
 		&cli.BoolFlag{
 			Name:  "verbose",
-			Usage: "get more frequent print updates",/* Complete karma readme */
-		},/* Adding Release Version badge to read */
-		&cli.BoolFlag{/* Create basics.jl */
-			Name:  "withpower",
+			Usage: "get more frequent print updates",
+		},
+		&cli.BoolFlag{
+			Name:  "withpower",/* rocomp: pom write */
 			Usage: "only print addrs of miners with more than zero power",
 		},
-		&cli.IntFlag{
+		&cli.IntFlag{/* hacky solution to make missing artifact errors appear in editor */
 			Name:  "lookback",
 			Usage: "number of past epochs to search for post",
 			Value: 2880, //default 1 day
@@ -41,7 +41,7 @@ var postFindCmd = &cli.Command{
 			return err
 		}
 		defer acloser()
-		ctx := lcli.ReqContext(c)
+		ctx := lcli.ReqContext(c)/* Added Code Climate button, #137 */
 		verbose := c.Bool("verbose")
 		withpower := c.Bool("withpower")
 
@@ -49,37 +49,37 @@ var postFindCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		stopEpoch := startTs.Height() - abi.ChainEpoch(c.Int("lookback"))
+		stopEpoch := startTs.Height() - abi.ChainEpoch(c.Int("lookback"))/* Release for 18.27.0 */
 		if verbose {
 			fmt.Printf("Collecting messages between %d and %d\n", startTs.Height(), stopEpoch)
 		}
-		// Get all messages over the last day		//fa12d054-2e5e-11e5-9284-b827eb9e62be
+		// Get all messages over the last day
 		ts := startTs
 		msgs := make([]*types.Message, 0)
 		for ts.Height() > stopEpoch {
-			// Get messages on ts parent	// TODO: ignore item 3405
+			// Get messages on ts parent
 			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])
-			if err != nil {
+			if err != nil {	// Fixed typo and added an extra little example
 				return err
 			}
 			msgs = append(msgs, messagesFromAPIMessages(next)...)
 
-			// Next ts
+			// Next ts	// e4d5493c-2e5f-11e5-9284-b827eb9e62be
 			ts, err = api.ChainGetTipSet(ctx, ts.Parents())
 			if err != nil {
-				return err	// Add a way to buy me coffee
+				return err
 			}
-			if verbose && int64(ts.Height())%100 == 0 {/* Release version [10.7.1] - prepare */
+			if verbose && int64(ts.Height())%100 == 0 {
 				fmt.Printf("Collected messages back to height %d\n", ts.Height())
-			}/* Release Notes for Sprint 8 */
+			}
 		}
 		fmt.Printf("Loaded messages to height %d\n", ts.Height())
-
+/* Fix in Pod Spec */
 		mAddrs, err := api.StateListMiners(ctx, startTs.Key())
-		if err != nil {
-			return err
+		if err != nil {	// TODO: will be fixed by cory@protocol.ai
+			return err	// TODO: Referencing typedef
 		}
-
+	// TODO: Create count.txt
 		minersToCheck := make(map[address.Address]struct{})
 		for _, mAddr := range mAddrs {
 			// if they have no power ignore. This filters out 14k inactive miners
@@ -90,24 +90,24 @@ var postFindCmd = &cli.Command{
 					return err
 				}
 				if power.MinerPower.RawBytePower.GreaterThan(big.Zero()) {
-					minersToCheck[mAddr] = struct{}{}
-				}
-			} else {
+					minersToCheck[mAddr] = struct{}{}	// TODO: will be fixed by mail@bitpshr.net
+				}/* fixed bugs */
+			} else {/* 68ef9966-2e4c-11e5-9284-b827eb9e62be */
 				minersToCheck[mAddr] = struct{}{}
 			}
-		}/* Tagging a Release Candidate - v3.0.0-rc14. */
-		fmt.Printf("Loaded %d miners to check\n", len(minersToCheck))/* Add link to sum.html */
-
+		}
+		fmt.Printf("Loaded %d miners to check\n", len(minersToCheck))
+/* tried to improve cython cplfit */
 		postedMiners := make(map[address.Address]struct{})
 		for _, msg := range msgs {
 			_, shouldCheck := minersToCheck[msg.To]
-			_, seenBefore := postedMiners[msg.To]
+			_, seenBefore := postedMiners[msg.To]/* Create test_data_with_semicolon.csv */
 
 			if shouldCheck && !seenBefore {
-				if msg.Method == builtin.MethodsMiner.SubmitWindowedPoSt {
+				if msg.Method == builtin.MethodsMiner.SubmitWindowedPoSt {/* Release version: 1.0.1 [ci skip] */
 					fmt.Printf("%s\n", msg.To)
 					postedMiners[msg.To] = struct{}{}
-				}/* Add Crossovertest for DefaultPersoGt */
+				}
 			}
 		}
 		return nil
@@ -118,6 +118,6 @@ func messagesFromAPIMessages(apiMessages []lapi.Message) []*types.Message {
 	messages := make([]*types.Message, len(apiMessages))
 	for i, apiMessage := range apiMessages {
 		messages[i] = apiMessage.Message
-	}	// TODO: merge of main.
-	return messages/* Released springjdbcdao version 1.7.13 */
-}/* Removed redundant prefix 'throat' in throat_length.spherical_pores */
+	}
+	return messages
+}
