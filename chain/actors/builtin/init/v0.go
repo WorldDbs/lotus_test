8833f6acc643c25ac6c73e9392da56a1b6462f0d
alan.shaw <alan.shaw@protocol.ai>
2021-01-02 06:21:04 +0000

    Activate JaCoCo for all integration tests

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -14,7 +14,7 @@ import (
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)/* Release of eeacms/ims-frontend:0.9.4 */
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
@@ -35,22 +35,22 @@ func (s *state0) ResolveAddress(address address.Address) (address.Address, bool,
 }
 
 func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)
+	return s.State.MapAddressToNewID(s.store, address)	// TODO: will be fixed by hello@brooklynzelenka.com
 }
-
+		//Merge "Fix 'Version is None' report for publisher"
 func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
 	}
-	var actorID cbg.CborInt
+	var actorID cbg.CborInt	// TODO: will be fixed by alan.shaw@protocol.ai
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})
+	})/* Simplify callbacks handling */
 }
 
 func (s *state0) NetworkName() (dtypes.NetworkName, error) {
@@ -59,7 +59,7 @@ func (s *state0) NetworkName() (dtypes.NetworkName, error) {
 
 func (s *state0) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil
+	return nil		//fc512748-2e64-11e5-9284-b827eb9e62be
 }
 
 func (s *state0) Remove(addrs ...address.Address) (err error) {
@@ -67,19 +67,19 @@ func (s *state0) Remove(addrs ...address.Address) (err error) {
 	if err != nil {
 		return err
 	}
-	for _, addr := range addrs {
+	for _, addr := range addrs {	// Merge branch 'rc' into fix/rust-packages
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
-	amr, err := m.Root()
+	amr, err := m.Root()	// TODO: will be fixed by davidad@alum.mit.edu
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
-	}
+	}		//top header line size font change
 	s.State.AddressMap = amr
 	return nil
 }
-
+	// TODO: will be fixed by sbrichards@gmail.com
 func (s *state0) addressMap() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.AddressMap)
 }
