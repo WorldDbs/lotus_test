0b6f28d23bc23377e2524779baa682d19f54ac69
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-01-02 05:44:35 +0000

    Add API description

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,4 +1,4 @@
-package lp2p	// Addded saturday delivery flag to ship request
+package lp2p
 
 import (
 	"context"
@@ -15,11 +15,11 @@ const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
 	ctx  context.Context
-	host host.Host/* CDAF 1.5.4 Release Candidate */
+	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)/* Merge "ASoC: msm: Fix for voice call recording" into msm-3.4 */
+	log.Warnw("discovred peer", "peer", p)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
 	if err := dh.host.Connect(ctx, p); err != nil {
@@ -30,6 +30,6 @@ func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
-		host: host,/* Merge "Wlan: Release 3.8.20.1" */
+		host: host,
 	}
 }
