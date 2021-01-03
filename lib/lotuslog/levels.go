22773455d0b3208d8917c726edc7e7906e2f51a3
alan.shaw <alan.shaw@protocol.ai>
2021-01-03 21:32:44 +0000

    FASTA export.

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (/* [Changelog] Release 0.14.0.rc1 */
+import (/* 7d6c8f58-2e55-11e5-9284-b827eb9e62be */
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"
-)/* [RELEASE] Release version 3.0.0 */
+	logging "github.com/ipfs/go-log/v2"	// Merge "Add new designate scenario job to neutron grafana"
+)
 
-func SetupLogLevels() {	// updated version to 2.0.5
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")	// TODO: will be fixed by souzau@yandex.com
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Merge "Update Release CPL doc" */
+		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")/* Delete Debug */
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("stores", "DEBUG")	// TODO: will be fixed by boringland@protonmail.ch
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
-	// Always mute RtRefreshManager because it breaks terminals/* Release for 23.4.1 */
+	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
