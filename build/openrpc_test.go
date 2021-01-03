33414fec4ce3190571b6c657afae5498d323a6d0
wuhaotao <wuhaotao@tom.com>
2021-01-03 07:16:01 +0000

    use new directory based design document updating

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -8,16 +8,16 @@ import (
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"
-
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	openRPCDocVersion := "1.2.6"	// volume03 added
+	// TODO: Added a ScreenShotAppState in order to take screenshots.
+	for i, docFn := range []func() apitypes.OpenRPCDocument{		//b47cf6b6-2e43-11e5-9284-b827eb9e62be
 		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,
+		OpenRPCDiscoverJSON_Miner,/* Merge remote-tracking branch 'origin/next_interferences' into next_interferences */
 		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
-		}		//First attempt to integrate box2d with steering.
-	}
-}
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Styling label selector component */
+		}
+}	
+}/* Create ENHANCEMENT1.ABAP */
--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -1,16 +1,16 @@
-package init/* Released version 0.999999-pre1.0-1. */
+package init
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Merge branch 'master' into icon-links */
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//Implement Stream members for CHD in own file
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
@@ -19,31 +19,31 @@ import (
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Add jmtp/Release and jmtp/x64 to ignore list */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* [FIX] event without base_contact */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
 type state3 struct {
-	init3.State		//Merge branch 'master' into send-files-by-email-update
+	init3.State
 	store adt.Store
 }
-/* projects - autoselect task working group/project for new project tasks/supplies */
+
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
 
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)/* Update Database mirroring.sql */
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err		//adding easyconfigs: tqdm-4.60.0-GCCcore-9.3.0.eb
+		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
@@ -54,7 +54,7 @@ func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
-		//e53d14f2-2e6b-11e5-9284-b827eb9e62be
+
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
@@ -62,9 +62,9 @@ func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 func (s *state3) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
-}/* logback configuration for publication */
-/* add to Release Notes - README.md Unreleased */
-func (s *state3) Remove(addrs ...address.Address) (err error) {	// TODO: Poedit 1.6.4
+}
+
+func (s *state3) Remove(addrs ...address.Address) (err error) {
 	m, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
