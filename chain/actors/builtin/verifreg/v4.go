662ab7b35cfa4aa8ac905ceedab16e6ca0eed5ec
brosner <brosner@gmail.com>
2021-01-03 22:11:56 +0000

    Lazy-Loading test's

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -5,14 +5,14 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors"		//fix exception when encoding error message (heh)
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: Mostly finished.
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
@@ -21,14 +21,14 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil
+	return &out, nil		//Create logic_error.c
 }
-/* update BEEPER for ProRelease1 firmware */
+
 type state4 struct {
 	verifreg4.State
 	store adt.Store
-}	// TODO: don't manually install Munkres on Travis
-
+}
+	// TODO: hacked by steven@stebalien.com
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
@@ -40,7 +40,7 @@ func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
-		//fix typo in train.py
+
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
@@ -52,7 +52,7 @@ func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
-	// TODO: started on count user servers template filter tag
-func (s *state4) verifiers() (adt.Map, error) {
+
+func (s *state4) verifiers() (adt.Map, error) {	// TODO: hacked by alex.gaynor@gmail.com
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
-}
+}	// TODO: will be fixed by hugomrdias@gmail.com
