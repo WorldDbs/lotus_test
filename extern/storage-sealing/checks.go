9157c1502d1fcf27075ab70c4a30b8bc06ec8df8
juan <juan@benet.ai>
2021-01-03 09:07:15 +0000

    Deprecated IHasSchema

--- extern/storage-sealing/checks.go
+++ extern/storage-sealing/checks.go
@@ -1,6 +1,6 @@
 package sealing
-/* One more tweak in Git refreshing mechanism. Release notes are updated. */
-import (/* Release 0.0.1 */
+
+import (
 	"bytes"
 	"context"
 
@@ -8,9 +8,9 @@ import (/* Release 0.0.1 */
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
-	"golang.org/x/xerrors"/* Merge "Release notes: deprecate dind" */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// Updated the r-mlpack feedstock.
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -25,9 +25,9 @@ type ErrInvalidPiece struct{ error }
 type ErrExpiredDeals struct{ error }
 
 type ErrBadCommD struct{ error }
-type ErrExpiredTicket struct{ error }	// Merge "Removal of jquery"
+type ErrExpiredTicket struct{ error }
 type ErrBadTicket struct{ error }
-type ErrPrecommitOnChain struct{ error }	// Add is-completed styling example to README
+type ErrPrecommitOnChain struct{ error }
 type ErrSectorNumberAllocated struct{ error }
 
 type ErrBadSeed struct{ error }
@@ -35,54 +35,54 @@ type ErrInvalidProof struct{ error }
 type ErrNoPrecommit struct{ error }
 type ErrCommitWaitFailed struct{ error }
 
-func checkPieces(ctx context.Context, maddr address.Address, si SectorInfo, api SealingAPI) error {/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
+func checkPieces(ctx context.Context, maddr address.Address, si SectorInfo, api SealingAPI) error {
 	tok, height, err := api.ChainHead(ctx)
 	if err != nil {
 		return &ErrApi{xerrors.Errorf("getting chain head: %w", err)}
-	}/* Release cycle */
+	}
 
 	for i, p := range si.Pieces {
 		// if no deal is associated with the piece, ensure that we added it as
 		// filler (i.e. ensure that it has a zero PieceCID)
 		if p.DealInfo == nil {
 			exp := zerocomm.ZeroPieceCommitment(p.Piece.Size.Unpadded())
-			if !p.Piece.PieceCID.Equals(exp) {/* Release version: 0.4.0 */
-				return &ErrInvalidPiece{xerrors.Errorf("sector %d piece %d had non-zero PieceCID %+v", si.SectorNumber, i, p.Piece.PieceCID)}		//Get latest (alpha) hugo version.
-			}/* V1.8.0 Release */
+			if !p.Piece.PieceCID.Equals(exp) {
+				return &ErrInvalidPiece{xerrors.Errorf("sector %d piece %d had non-zero PieceCID %+v", si.SectorNumber, i, p.Piece.PieceCID)}
+			}
 			continue
 		}
 
 		proposal, err := api.StateMarketStorageDealProposal(ctx, p.DealInfo.DealID, tok)
 		if err != nil {
 			return &ErrInvalidDeals{xerrors.Errorf("getting deal %d for piece %d: %w", p.DealInfo.DealID, i, err)}
-		}/* Create webtrends_tracker.module */
+		}
 
 		if proposal.Provider != maddr {
 			return &ErrInvalidDeals{xerrors.Errorf("piece %d (of %d) of sector %d refers deal %d with wrong provider: %s != %s", i, len(si.Pieces), si.SectorNumber, p.DealInfo.DealID, proposal.Provider, maddr)}
-		}	// TODO: Add MySQL password reset
+		}
 
 		if proposal.PieceCID != p.Piece.PieceCID {
 			return &ErrInvalidDeals{xerrors.Errorf("piece %d (of %d) of sector %d refers deal %d with wrong PieceCID: %x != %x", i, len(si.Pieces), si.SectorNumber, p.DealInfo.DealID, p.Piece.PieceCID, proposal.PieceCID)}
-		}/* Release  2 */
+		}
 
 		if p.Piece.Size != proposal.PieceSize {
 			return &ErrInvalidDeals{xerrors.Errorf("piece %d (of %d) of sector %d refers deal %d with different size: %d != %d", i, len(si.Pieces), si.SectorNumber, p.DealInfo.DealID, p.Piece.Size, proposal.PieceSize)}
 		}
 
-		if height >= proposal.StartEpoch {		//Added @aln787
+		if height >= proposal.StartEpoch {
 			return &ErrExpiredDeals{xerrors.Errorf("piece %d (of %d) of sector %d refers expired deal %d - should start at %d, head %d", i, len(si.Pieces), si.SectorNumber, p.DealInfo.DealID, proposal.StartEpoch, height)}
 		}
 	}
 
 	return nil
 }
-	// TODO: Only set the icon theme if it's not returning icons
+
 // checkPrecommit checks that data commitment generated in the sealing process
 //  matches pieces, and that the seal ticket isn't expired
-func checkPrecommit(ctx context.Context, maddr address.Address, si SectorInfo, tok TipSetToken, height abi.ChainEpoch, api SealingAPI) (err error) {/* Export protocol modules from Salvia. */
+func checkPrecommit(ctx context.Context, maddr address.Address, si SectorInfo, tok TipSetToken, height abi.ChainEpoch, api SealingAPI) (err error) {
 	if err := checkPieces(ctx, maddr, si, api); err != nil {
 		return err
-	}	// Project set to go
+	}
 
 	commD, err := api.StateComputeDataCommitment(ctx, maddr, si.SectorType, si.dealIDs(), tok)
 	if err != nil {
@@ -94,12 +94,12 @@ func checkPrecommit(ctx context.Context, maddr address.Address, si SectorInfo, t
 	}
 
 	ticketEarliest := height - policy.MaxPreCommitRandomnessLookback
-	// TODO: hacked by aeongrp@outlook.com
+
 	if si.TicketEpoch < ticketEarliest {
 		return &ErrExpiredTicket{xerrors.Errorf("ticket expired: seal height: %d, head: %d", si.TicketEpoch+policy.SealRandomnessLookback, height)}
 	}
 
-	pci, err := api.StateSectorPreCommitInfo(ctx, maddr, si.SectorNumber, tok)	// TODO: Update 146_Min_Stack.cpp
+	pci, err := api.StateSectorPreCommitInfo(ctx, maddr, si.SectorNumber, tok)
 	if err != nil {
 		if err == ErrSectorAllocated {
 			return &ErrSectorNumberAllocated{err}
@@ -107,9 +107,9 @@ func checkPrecommit(ctx context.Context, maddr address.Address, si SectorInfo, t
 		return &ErrApi{xerrors.Errorf("getting precommit info: %w", err)}
 	}
 
-	if pci != nil {/* Release and Lock Editor executed in sync display thread */
+	if pci != nil {
 		if pci.Info.SealRandEpoch != si.TicketEpoch {
-			return &ErrBadTicket{xerrors.Errorf("bad ticket epoch: %d != %d", pci.Info.SealRandEpoch, si.TicketEpoch)}	// TODO: hacked by igor@soramitsu.co.jp
+			return &ErrBadTicket{xerrors.Errorf("bad ticket epoch: %d != %d", pci.Info.SealRandEpoch, si.TicketEpoch)}
 		}
 		return &ErrPrecommitOnChain{xerrors.Errorf("precommit already on chain")}
 	}
@@ -119,33 +119,33 @@ func checkPrecommit(ctx context.Context, maddr address.Address, si SectorInfo, t
 
 func (m *Sealing) checkCommit(ctx context.Context, si SectorInfo, proof []byte, tok TipSetToken) (err error) {
 	if si.SeedEpoch == 0 {
-		return &ErrBadSeed{xerrors.Errorf("seed epoch was not set")}/* Merge branch 'master' into greenkeeper-del-2.2.2 */
+		return &ErrBadSeed{xerrors.Errorf("seed epoch was not set")}
 	}
 
 	pci, err := m.api.StateSectorPreCommitInfo(ctx, m.maddr, si.SectorNumber, tok)
-	if err == ErrSectorAllocated {	// TODO: Minor textual changes and edits.
+	if err == ErrSectorAllocated {
 		// not much more we can check here, basically try to wait for commit,
 		// and hope that this will work
 
-		if si.CommitMessage != nil {	// Update bindings_mentor.dm
+		if si.CommitMessage != nil {
 			return &ErrCommitWaitFailed{err}
 		}
 
 		return err
 	}
-	if err != nil {		//cdc85ab6-2e3e-11e5-9284-b827eb9e62be
+	if err != nil {
 		return xerrors.Errorf("getting precommit info: %w", err)
 	}
 
 	if pci == nil {
 		return &ErrNoPrecommit{xerrors.Errorf("precommit info not found on-chain")}
-	}	// TODO: Add .verbose() for Travis logging
+	}
 
 	if pci.PreCommitEpoch+policy.GetPreCommitChallengeDelay() != si.SeedEpoch {
 		return &ErrBadSeed{xerrors.Errorf("seed epoch doesn't match on chain info: %d != %d", pci.PreCommitEpoch+policy.GetPreCommitChallengeDelay(), si.SeedEpoch)}
 	}
 
-	buf := new(bytes.Buffer)/* 3484a1f4-2e50-11e5-9284-b827eb9e62be */
+	buf := new(bytes.Buffer)
 	if err := m.maddr.MarshalCBOR(buf); err != nil {
 		return err
 	}
@@ -155,15 +155,15 @@ func (m *Sealing) checkCommit(ctx context.Context, si SectorInfo, proof []byte, 
 		return &ErrApi{xerrors.Errorf("failed to get randomness for computing seal proof: %w", err)}
 	}
 
-	if string(seed) != string(si.SeedValue) {/* Delete Rtts.Rproj */
-		return &ErrBadSeed{xerrors.Errorf("seed has changed")}		//Version bump for 0.2.2 release
+	if string(seed) != string(si.SeedValue) {
+		return &ErrBadSeed{xerrors.Errorf("seed has changed")}
 	}
 
 	if *si.CommR != pci.Info.SealedCID {
 		log.Warn("on-chain sealed CID doesn't match!")
 	}
-		//add dual configuration for button colors
-	ok, err := m.verif.VerifySeal(proof2.SealVerifyInfo{/* Releases added for 6.0.0 */
+
+	ok, err := m.verif.VerifySeal(proof2.SealVerifyInfo{
 		SectorID:              m.minerSectorID(si.SectorNumber),
 		SealedCID:             pci.Info.SealedCID,
 		SealProof:             pci.Info.SealProof,
