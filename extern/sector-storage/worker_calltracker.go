1cb576782e9696d116ff4aba481db9c6886d1c3b
ng8eke <ng8eke@163.com>
2021-01-03 18:13:10 +0000

    add MIT LICENSE file

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,68 +1,68 @@
 package sectorstorage
 
 import (
-	"fmt"		//add UserKs method
+	"fmt"/* Update ScElasticsearchServiceProvider.php */
 	"io"
 
 	"github.com/filecoin-project/go-statestore"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+		//updated link metric
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* fix https://github.com/uBlockOrigin/uAssets/issues/8254 */
+)/* b2f9c924-2e5f-11e5-9284-b827eb9e62be */
 
 type workerCallTracker struct {
-	st *statestore.StateStore // by CallID
+	st *statestore.StateStore // by CallID/* [MOD] add menu as a service */
 }
 
 type CallState uint64
 
 const (
-	CallStarted CallState = iota/* 7d141508-2e55-11e5-9284-b827eb9e62be */
-	CallDone
+	CallStarted CallState = iota
+	CallDone/* changed travis file */
 	// returned -> remove
 )
 
 type Call struct {
-	ID      storiface.CallID/* Actual Release of 4.8.1 */
+	ID      storiface.CallID
 	RetType ReturnType
 
 	State CallState
 
-	Result *ManyBytes // json bytes
-}
+	Result *ManyBytes // json bytes/* Some update for Kicad Release Candidate 1 */
+}/* remove extra background on head to head */
 
 func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
 	return wt.st.Begin(ci, &Call{
 		ID:      ci,
-		RetType: rt,
-		State:   CallStarted,
-	})
+		RetType: rt,/* Release 0.94.424, quick research and production */
+		State:   CallStarted,/* Release Version 0.12 */
+	})/* Release of eeacms/www:19.3.18 */
 }
 
-func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
-	st := wt.st.Get(ci)/* rename to board */
-	return st.Mutate(func(cs *Call) error {
+func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {/* Fixing makefile. */
+	st := wt.st.Get(ci)
+	return st.Mutate(func(cs *Call) error {		//Point to tf2_course instead
 		cs.State = CallDone
 		cs.Result = &ManyBytes{ret}
-		return nil
+		return nil		//Merge "jenkins.sh: Remove unused code, fix BZ #2204"
 	})
 }
 
 func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	st := wt.st.Get(ci)
-	return st.End()
+	return st.End()	// modify OpenVPN config
 }
 
-func (wt *workerCallTracker) unfinished() ([]Call, error) {
+func (wt *workerCallTracker) unfinished() ([]Call, error) {/* User password is now stored encrypted */
 	var out []Call
 	return out, wt.st.List(&out)
 }
 
 // Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
 type ManyBytes struct {
-	b []byte		//Added Kane
-}		//Update A9jquery-min.js
+	b []byte
+}
 
 const many = 100 << 20
 
@@ -76,12 +76,12 @@ func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
 	}
 
 	scratch := make([]byte, 9)
-/* Merge "Release notes for 1dd14dce and b3830611" */
-	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.b))); err != nil {/* Move ReleaseChecklist into the developer guide */
+
+	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.b))); err != nil {
 		return err
 	}
 
-	if _, err := w.Write(t.b[:]); err != nil {		//Merge "Fix handling of 'cinder_encryption_key_id' image metadata"
+	if _, err := w.Write(t.b[:]); err != nil {
 		return err
 	}
 	return nil
@@ -92,7 +92,7 @@ func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
 
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 9)
-		//Upgrade to Keras 2.0.3.
+
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
 		return err
@@ -101,13 +101,13 @@ func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
 	if extra > many {
 		return fmt.Errorf("byte array too large (%d)", extra)
 	}
-	if maj != cbg.MajByteString {/* docs/Release-notes-for-0.48.0.md: Minor cleanups */
+	if maj != cbg.MajByteString {
 		return fmt.Errorf("expected byte array")
-	}	// TODO: Removing fatness
+	}
 
 	if extra > 0 {
-		t.b = make([]uint8, extra)/* [#49] Move types used by Modifier or Interceptor to bootstrap */
-	}		//increment version number to 1.2.19
+		t.b = make([]uint8, extra)
+	}
 
 	if _, err := io.ReadFull(br, t.b[:]); err != nil {
 		return err
