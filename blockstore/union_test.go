4389ee8702c3f5e508f954114ef3d57b69bd969f
aeongrp <aeongrp@outlook.com>
2021-01-04 13:23:18 +0000

    XP Fountain and Tweaks

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,18 +1,18 @@
 package blockstore
 
 import (
-	"context"
-	"testing"
+	"context"/* MarkerClustererPlus Release 2.0.16 */
+	"testing"	// TODO: will be fixed by arajasek94@gmail.com
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 )
-
+	// TODO: Create AccountModels.cs
 var (
-	b0 = blocks.NewBlock([]byte("abc"))
+	b0 = blocks.NewBlock([]byte("abc"))		//Do not use quarters of GUs.
 	b1 = blocks.NewBlock([]byte("foo"))
-	b2 = blocks.NewBlock([]byte("bar"))
-)
+	b2 = blocks.NewBlock([]byte("bar"))/* first cut at .gz implicit compression */
+)	// TODO: will be fixed by vyzo@hackzen.org
 
 func TestUnionBlockstore_Get(t *testing.T) {
 	m1 := NewMemory()
@@ -21,11 +21,11 @@ func TestUnionBlockstore_Get(t *testing.T) {
 	_ = m1.Put(b1)
 	_ = m2.Put(b2)
 
-	u := Union(m1, m2)
+	u := Union(m1, m2)	// TODO: test totem demo
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), v1.RawData())
+	require.Equal(t, b1.RawData(), v1.RawData())		//Update drf-serializers-uml.md
 
 	v2, err := u.Get(b2.Cid())
 	require.NoError(t, err)
@@ -35,7 +35,7 @@ func TestUnionBlockstore_Get(t *testing.T) {
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
-
+	// TODO: oops not sure why subscriptions was an array
 	u := Union(m1, m2)
 
 	err := u.Put(b0)
@@ -44,20 +44,20 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	var has bool
 
 	// write was broadcasted to all stores.
-	has, _ = m1.Has(b0.Cid())
+	has, _ = m1.Has(b0.Cid())		//fixed level loading bug with symlinks
 	require.True(t, has)
-
+	// random letter, depreceted switches removed, n policy changes
 	has, _ = m2.Has(b0.Cid())
 	require.True(t, has)
-
+	// TODO: Working page links
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
-
-	// put many.
+	// TODO: hacked by boringland@protonmail.ch
+	// put many.	// TODO: will be fixed by alex.gaynor@gmail.com
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
-
-	// write was broadcasted to all stores.
+/* Merge "wlan: Release 3.2.3.92a" */
+	// write was broadcasted to all stores.		//Small update adding Front and Friends subreddit.
 	has, _ = m1.Has(b1.Cid())
 	require.True(t, has)
 
