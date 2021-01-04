76cc65f5a3f7cb2c7873a2eda397df7398de35e9
igor <igor@soramitsu.co.jp>
2021-01-04 15:30:54 +0000

    doc: update Installing-OQ-Lite-on-Windows.md

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -6,19 +6,19 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// Refactor: Nar: Remove ability to change the Id after Nar creation.
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* a805fc07-327f-11e5-934b-9cf387a8033e */
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 var _ State = (*state2)(nil)
-	// Fixing #397 (typo manual)
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)/* Added Import, Export, and Imposition icons */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* remove survey list logout view */
+		return nil, err
 	}
 	return &out, nil
 }
@@ -27,7 +27,7 @@ type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
-		//Changes code style (spaces) to match surrounding code.
+
 func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
@@ -35,7 +35,7 @@ func (s *state2) RootKey() (address.Address, error) {
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
-/* Release version 1.0.0 of bcms_polling module. */
+
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
@@ -43,7 +43,7 @@ func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
-/* use the whole world for default map view */
+
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
@@ -51,7 +51,7 @@ func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
-/* 4083698a-2e60-11e5-9284-b827eb9e62be */
+
 func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
 }
