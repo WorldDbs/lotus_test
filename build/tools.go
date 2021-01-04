a126c96b9cc272c800f31541b04d31ed455d2e9b
sbrichards <sbrichards@gmail.com>
2021-01-04 07:45:07 +0000

    Added init function for rc file

--- build/tools.go
+++ build/tools.go
@@ -1,9 +1,9 @@
-//+build tools
+//+build tools		//Finalize Javadoc
 
 package build
 
 import (
-	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/GeertJohan/go.rice/rice"/* Release of eeacms/www:18.7.26 */
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,6 +1,6 @@
 package fsutil
 
-import (/* Updated broken link on InfluxDB Release */
+import (
 	"syscall"
 
 	"golang.org/x/xerrors"
@@ -15,7 +15,7 @@ func Statfs(path string) (FsStat, error) {
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Request data from Facebook once the change notification is received
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
