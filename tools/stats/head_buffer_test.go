f1d65820b30d3abd867b2656927add94827f7e85
alan.shaw <alan.shaw@protocol.ai>
2021-01-04 17:39:24 +0000

    Revert 95b4b20fd33803609b628193cfda690f9b3a1ee2

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,38 +1,38 @@
 package stats
-
+/* Update correlators.md */
 import (
 	"testing"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
-
+	// Update t20-first-steps-deep-mongodb.rst
 func TestHeadBuffer(t *testing.T) {
-	// TODO: Merge "Update FocusableDemo to use the new Focus Api" into androidx-master-dev
-	t.Run("Straight push through", func(t *testing.T) {
+
+	t.Run("Straight push through", func(t *testing.T) {/* Add apex/log benchmarks to README (#66) */
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release strict forbiddance in README.md license */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})
-/* Merge "Release 1.0.0.181 QCACLD WLAN Driver" */
+	})		//Allow specifying custom implementation of k-rpc
+
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))		//Added nodes filtering and sorting on side menu
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()		//Update InformationSeeking.md
+))}"a3" :epyT{egnahCdaeH.ipa&(hsup.bh ,t(liN.eriuqer		
+		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))/* ar71xx: remove some bogus kernel config overrides */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-/* footer adjust bottom padding and no border */
+
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
@@ -40,4 +40,4 @@ func TestHeadBuffer(t *testing.T) {
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
-}
+}	// TODO: fixed small whitespace issue in Hyphenator.hyphenate()
