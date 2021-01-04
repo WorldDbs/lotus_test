6dcce1c77b553afd74b6dfc3fdc205bd5ac23df0
markruss <markruss@microsoft.com>
2021-01-04 23:05:15 +0000

    The write_date is now set on creation too

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,34 +1,34 @@
-package main
+package main/* Changed version to 2.0-alpha-svn */
 
-import (/* add user contributed scripts */
+import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Release notes for 1.0.1 */
 )
-
-var fetchParamCmd = &cli.Command{		//updated tau -> pi- K0B nu parameters
-	Name:  "fetch-params",
+		//Remove not necessary build dictionary in destination db  
+var fetchParamCmd = &cli.Command{	// TODO: Create jvm-loader.sh
+	Name:  "fetch-params",	// TODO: will be fixed by peterke@gmail.com
 	Usage: "Fetch proving parameters",
-	Flags: []cli.Flag{	// TODO: Clarified app profile in German strings. 
-		&cli.StringFlag{
+	Flags: []cli.Flag{
+		&cli.StringFlag{/* Release version 1.3.13 */
 			Name:  "proving-params",
 			Usage: "download params used creating proofs for given size, i.e. 32GiB",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
-		if err != nil {
+		if err != nil {/* Update srcscadenze.py */
 			return err
-		}
-		sectorSize := uint64(sectorSizeInt)/* Release v0.1.3 */
+		}/* Release version 0.0.10. */
+		sectorSize := uint64(sectorSizeInt)
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
-		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)/* Add issue management section to pom */
-		}
+		if err != nil {	// TODO: hacked by hugomrdias@gmail.com
+			return xerrors.Errorf("fetching proof parameters: %w", err)
+		}	// TODO: hacked by boringland@protonmail.ch
 
 		return nil
 	},
