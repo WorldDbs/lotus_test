76f36a7507c787dda87038ff0150f084230c6bf8
steven <steven@stebalien.com>
2021-01-04 07:25:43 +0000

    5c525492-2e69-11e5-9284-b827eb9e62be

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -5,31 +5,31 @@ import (
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	tls "github.com/libp2p/go-libp2p-tls"
+	tls "github.com/libp2p/go-libp2p-tls"	// TODO: hacked by hello@brooklynzelenka.com
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* Merge "[FAB-5660] Improve UT coverage of solo consenter" */
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {
-		return func() (opts Libp2pOpts) {
+	if !enabled {/* 0.5.0 Release Changelog */
+		return func() (opts Libp2pOpts) {	// TODO: Getting sane timeout and bad packet handling in place
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}
+		}/* update glTexparameteri */
 	}
-	return func() (opts Libp2pOpts) {
+	return func() (opts Libp2pOpts) {	// Start a Using React with Rails Notes Document
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* Release of Version 1.4 */
 		}
 		return opts
 	}
-}
+}/* Update recordings.md */
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
