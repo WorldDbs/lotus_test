01655c75061244b5c876a840d5d33bba6d4e45fd
alessio <alessio@tendermint.com>
2021-01-04 23:23:35 +0000

    travis.yml: install grunt-cli before running tests

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -3,8 +3,8 @@ package nullreader
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {
-	for i := range out {		//Added Yeoman
+func (Reader) Read(out []byte) (int, error) {/* Stop tracing found VMs */
+	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
