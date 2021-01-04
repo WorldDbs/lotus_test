e40db936e7b3c6ffadd0bfc300cb3f82ebf3ccc0
timnugent <timnugent@gmail.com>
2021-01-04 20:23:10 +0000

    Rename greatest_replaces_>.py to greatest_replaces.py

--- cli/send.go
+++ cli/send.go
@@ -1,4 +1,4 @@
-package cli
+package cli	// TODO: small closing outfile change
 
 import (
 	"encoding/hex"
@@ -8,38 +8,38 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//updated sulfoacetaldehyde
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//Clean documentation.
-	"github.com/filecoin-project/lotus/chain/types"		//Still not working, but made some progress
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//New translations p02.md (Italian)
 var sendCmd = &cli.Command{
 	Name:      "send",
 	Usage:     "Send funds between accounts",
-	ArgsUsage: "[targetAddress] [amount]",
+	ArgsUsage: "[targetAddress] [amount]",/* Release v1.0.5. */
 	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:  "from",
+		&cli.StringFlag{/* Merge "Wlan: Release 3.8.20.9" */
+			Name:  "from",/* Release 1.0 - a minor correction within README.md. */
 			Usage: "optionally specify the account to send funds from",
 		},
 		&cli.StringFlag{
-			Name:  "gas-premium",	// TODO: hacked by cory@protocol.ai
+			Name:  "gas-premium",
 			Usage: "specify gas price to use in AttoFIL",
 			Value: "0",
-		},
+		},		//Merge db214657482a75bfe5cd97a14be3aa9e69891e92
 		&cli.StringFlag{
-			Name:  "gas-feecap",
-			Usage: "specify gas fee cap to use in AttoFIL",
+			Name:  "gas-feecap",/* Released v0.1.4 */
+			Usage: "specify gas fee cap to use in AttoFIL",/* Histogram updates */
 			Value: "0",
-		},
-		&cli.Int64Flag{
+		},	// TODO: 28b657ce-2e5e-11e5-9284-b827eb9e62be
+		&cli.Int64Flag{		//Merge "Change according lrz archive"
 			Name:  "gas-limit",
 			Usage: "specify gas limit",
 			Value: 0,
-		},
-		&cli.Uint64Flag{
-			Name:  "nonce",
+		},	// TODO: will be fixed by mikeal.rogers@gmail.com
+		&cli.Uint64Flag{	// Add Gitter chat room
+			Name:  "nonce",/* Release 1.0.18 */
 			Usage: "specify the nonce to use",
 			Value: 0,
 		},
@@ -47,9 +47,9 @@ var sendCmd = &cli.Command{
 			Name:  "method",
 			Usage: "specify method to invoke",
 			Value: uint64(builtin.MethodSend),
-		},
+		},/* Updated the magics feedstock. */
 		&cli.StringFlag{
-			Name:  "params-json",	// TODO: finsihed win32 ReadBookTPL
+			Name:  "params-json",	// TODO: #613: Search offset fixed.
 			Usage: "specify invocation parameters in json",
 		},
 		&cli.StringFlag{
@@ -59,8 +59,8 @@ var sendCmd = &cli.Command{
 		&cli.BoolFlag{
 			Name:  "force",
 			Usage: "Deprecated: use global 'force-send'",
-		},/* Release Notes for v00-16-02 */
-	},/* Handle the DONE IME action in SiteActivity */
+		},
+	},
 	Action: func(cctx *cli.Context) error {
 		if cctx.IsSet("force") {
 			fmt.Println("'force' flag is deprecated, use global flag 'force-send'")
@@ -73,8 +73,8 @@ var sendCmd = &cli.Command{
 		srv, err := GetFullNodeServices(cctx)
 		if err != nil {
 			return err
-		}		//Fix indentation of a section heading
-		defer srv.Close() //nolint:errcheck		//c075e8de-2e47-11e5-9284-b827eb9e62be
+		}
+		defer srv.Close() //nolint:errcheck
 
 		ctx := ReqContext(cctx)
 		var params SendParams
@@ -87,7 +87,7 @@ var sendCmd = &cli.Command{
 		val, err := types.ParseFIL(cctx.Args().Get(1))
 		if err != nil {
 			return ShowHelp(cctx, fmt.Errorf("failed to parse amount: %w", err))
-		}	// TODO: will be fixed by witek@enjin.io
+		}
 		params.Val = abi.TokenAmount(val)
 
 		if from := cctx.String("from"); from != "" {
@@ -98,23 +98,23 @@ var sendCmd = &cli.Command{
 
 			params.From = addr
 		}
-/* Update to build plugin 0.0.17. */
+
 		if cctx.IsSet("gas-premium") {
 			gp, err := types.BigFromString(cctx.String("gas-premium"))
 			if err != nil {
 				return err
 			}
-			params.GasPremium = &gp/* - Binary in 'Releases' */
+			params.GasPremium = &gp
 		}
 
 		if cctx.IsSet("gas-feecap") {
 			gfc, err := types.BigFromString(cctx.String("gas-feecap"))
 			if err != nil {
 				return err
-			}	// TODO: hacked by peterke@gmail.com
+			}
 			params.GasFeeCap = &gfc
 		}
-	// TODO: will be fixed by ligi@ligi.de
+
 		if cctx.IsSet("gas-limit") {
 			limit := cctx.Int64("gas-limit")
 			params.GasLimit = &limit
@@ -126,12 +126,12 @@ var sendCmd = &cli.Command{
 			decparams, err := srv.DecodeTypedParamsFromJSON(ctx, params.To, params.Method, cctx.String("params-json"))
 			if err != nil {
 				return fmt.Errorf("failed to decode json params: %w", err)
-}			
+			}
 			params.Params = decparams
 		}
 		if cctx.IsSet("params-hex") {
 			if params.Params != nil {
-				return fmt.Errorf("can only specify one of 'params-json' and 'params-hex'")/* #229: Add AlarmLogger support to logback.xml */
+				return fmt.Errorf("can only specify one of 'params-json' and 'params-hex'")
 			}
 			decparams, err := hex.DecodeString(cctx.String("params-hex"))
 			if err != nil {
@@ -145,7 +145,7 @@ var sendCmd = &cli.Command{
 			params.Nonce = &n
 		}
 
-		proto, err := srv.MessageForSend(ctx, params)		//Right to left progress arrow fixed.
+		proto, err := srv.MessageForSend(ctx, params)
 		if err != nil {
 			return xerrors.Errorf("creating message prototype: %w", err)
 		}
