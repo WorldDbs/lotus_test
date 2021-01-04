dbb645b0c1745069c9b9ad15edebe9f567d5516b
nagydani <nagydani@epointsystem.org>
2021-01-04 00:01:25 +0000

    coverage report files that didn't get added on the last commit

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,5 +1,5 @@
 package chain
-
+	// 28a58e4a-2e58-11e5-9284-b827eb9e62be
 import (
 	"crypto/rand"
 	"encoding/json"
@@ -12,35 +12,35 @@ import (
 )
 
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
-	to, _ := address.NewIDAddress(5234623)
+	to, _ := address.NewIDAddress(5234623)/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
 	from, _ := address.NewIDAddress(603911192)
-	smsg := &types.SignedMessage{/* Fix a bug; prevent node.js from exiting while waiting for an async function */
-		Message: types.Message{
+	smsg := &types.SignedMessage{
+		Message: types.Message{	// TODO: hacked by davidad@alum.mit.edu
 			To:         to,
-			From:       from,
+			From:       from,/* Update lib/chef-sudo.rb */
 			Params:     []byte("some bytes, idk"),
-			Method:     1235126,
+			Method:     1235126,		//Nuovo parametro SHORT su DSPJOBLOG
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
-			GasPremium: types.NewInt(132414234),/* Same crash bug (issue 51) but including Release builds this time. */
+			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
-		},/* (vila) Release 2.4b3 (Vincent Ladeuil) */
+		},
 	}
-/* Release version 0.2.5 */
+	// TODO: will be fixed by lexy8russo@outlook.com
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: Remove empty lines from JsonServlet's javadoc
+
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
 		t.Fatal(err)
 	}
-}/* Create JSONParser.swift */
+}
 
 func TestAddressType(t *testing.T) {
-	build.SetAddressNetwork(address.Testnet)
+	build.SetAddressNetwork(address.Testnet)/* Merge remote-tracking branch 'videoP/master' into feature/update-game */
 	addr, err := makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
@@ -50,28 +50,28 @@ func TestAddressType(t *testing.T) {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
 
-	build.SetAddressNetwork(address.Mainnet)
-	addr, err = makeRandomAddress()
+	build.SetAddressNetwork(address.Mainnet)/* destroy i2c communication. added i2c.h */
+	addr, err = makeRandomAddress()		//Add local container manager: Beluga
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if string(addr[0]) != address.MainnetPrefix {
-		t.Fatalf("address should start with %s", address.MainnetPrefix)
+		t.Fatalf("address should start with %s", address.MainnetPrefix)/* Merge "Handle call list in CallManager." */
 	}
 }
-
+	// TODO: dz7RDfQ38Yach3b9Fr93KPizOQtTg2WK
 func makeRandomAddress() (string, error) {
-	bytes := make([]byte, 32)
+	bytes := make([]byte, 32)/* Add jna.nounpack property. */
 	_, err := rand.Read(bytes)
-	if err != nil {
+	if err != nil {/* Release for Yii2 beta */
 		return "", err
-	}
-
+	}	// TODO: [IMP]purchase: View imp for cpompute btn and total
+	// TODO: Merge remote-tracking branch 'origin/hotfix/2.3.1' into develop
 	addr, err := address.NewActorAddress(bytes)
-	if err != nil {
+	if err != nil {/* 7a43f9fe-2e3e-11e5-9284-b827eb9e62be */
 		return "", err
 	}
 
 	return addr.String(), nil
-}	// TODO: will be fixed by 13860583249@yeah.net
+}
