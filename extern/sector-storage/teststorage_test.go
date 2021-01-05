e80d190687e7b2966cc638653e0138f322ce4cec
nagydani <nagydani@epointsystem.org>
2021-01-05 09:58:23 +0000

    [IMP] account: add columns for entries report

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,5 +1,5 @@
-package sectorstorage
-	// TODO: hacked by caojiaoyue@protonmail.com
+package sectorstorage		//Embed PowerShell in Cloud Shell link
+
 import (
 	"context"
 	"io"
@@ -8,7 +8,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"	// start with font list hidden
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -17,32 +17,32 @@ import (
 type apres struct {
 	pi  abi.PieceInfo
 	err error
-}
+}		//convert maps to for loop
 
 type testExec struct {
-	apch chan chan apres	// Updating README instructions and adding screenshot
-}/* Add ItemEnchantment matcher tests */
+	apch chan chan apres
+}
 
-func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {/* (mbp, trivial) remove doc/ja/sphinx_conf.py to unblock builds */
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
 }
 
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
-	panic("implement me")
-}/* Release notes for 1.0.93 */
+	panic("implement me")	// TODO: will be fixed by steven@stebalien.com
+}
 
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
 }
-/* Merge "Add hidden support for ENCODING_PCM_FLOAT to AudioTrack" */
+
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
 
 func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
 	panic("implement me")
-}
-
+}/* Release 0.9. */
+/* Added multitouch support. Release 1.3.0 */
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
 }
@@ -50,11 +50,11 @@ func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1
 func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
 }
+/* Create experimental_setup.md */
+func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
+	panic("implement me")
+}/* Fixed typo in matrix4.cr */
 
-func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {	// TODO: will be fixed by ligi@ligi.de
-	panic("implement me")		//added installation information to readme
-}
-/* Update info about UrT 4.3 Release Candidate 4 */
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
@@ -63,19 +63,19 @@ func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) erro
 	panic("implement me")
 }
 
-func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
+func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {/* How-to Release in README and some release related fixes */
 	resp := make(chan apres)
-	t.apch <- resp
-	ar := <-resp		//there ya are
-	return ar.pi, ar.err/* Deleted CtrlApp_2.0.5/Release/vc60.idb */
-}/* Update Recent and Upcoming Releases */
+	t.apch <- resp/* Xposed builds JSON */
+	ar := <-resp	// TODO: Sprisheet blocks: limit frame children to 50.
+	return ar.pi, ar.err
+}
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
-	panic("implement me")
+	panic("implement me")		//Rebuilt index with NimrodGeva
 }
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
-}/* Eggdrop v1.8.2 Release Candidate 2 */
+}/* Add dotfiles to personal manifest */
 
-var _ ffiwrapper.Storage = &testExec{}
+var _ ffiwrapper.Storage = &testExec{}		//1aa08b62-2e47-11e5-9284-b827eb9e62be
