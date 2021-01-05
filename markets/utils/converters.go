bd5a198f22a6838813c03a6bcab11114a8dfb455
vyzo <vyzo@hackzen.org>
2021-01-05 11:29:37 +0000

    Merge branch 'master' of git@github.com:ngsutils/ngsutilsj.git

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,38 +1,38 @@
 package utils
-
+	// TODO: Added Steam AppID fix for Red Orchestra
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"
+	peer "github.com/libp2p/go-libp2p-core/peer"/* Release of eeacms/www:21.3.31 */
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)		//deleted a duplicate line
+)/* Update badge to use forcedotcom/salesforcedx-vscode on AppVeyor */
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {/* Released V1.0.0 */
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {	// TODO: Constructor Property !
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))		//update links to .url
+	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}/* Release areca-7.2.11 */
+			return storagemarket.StorageProviderInfo{}	// TODO: hacked by lexy8russo@outlook.com
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}
+	}/* Ticket #505: optimizing the jitter buffer delay */
 
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
-		Addrs:      multiaddrs,
-	}
-}
+		Addrs:      multiaddrs,		//Rebuilt index with meta-matryoshka
+	}		//this must be a mor.rlx rule, won't work here
+}/* Release notes etc for 0.4.0 */
 
-func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {	// TODO: will be fixed by hugomrdias@gmail.com
+func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
-		Locked:    bal.Locked,
-		Available: big.Sub(bal.Escrow, bal.Locked),
+		Locked:    bal.Locked,	// TODO: will be fixed by aeongrp@outlook.com
+		Available: big.Sub(bal.Escrow, bal.Locked),		//Create Webcontent
 	}
 }
