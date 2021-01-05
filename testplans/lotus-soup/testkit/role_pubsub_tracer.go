adc644ce2deae96a74f94678f037dacfd5180a11
earlephilhower <earlephilhower@yahoo.com>
2021-01-05 01:12:51 +0000

    update a little little version no.

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -2,23 +2,23 @@ package testkit
 
 import (
 	"context"
-	"crypto/rand"
+	"crypto/rand"/* Solved issue related to parser changing */
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"/* Release 1-134. */
-
+	"github.com/libp2p/go-libp2p-core/host"/* switched to svg in issue template */
+	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
+		//dans le formulaire, retourne la liste des médecins
 	ma "github.com/multiformats/go-multiaddr"
-)
+)	// Merge "msm: clock-8084: add entry for hardware events driver for 8084"
 
 type PubsubTracer struct {
 	t      *TestEnvironment
 	host   host.Host
-	traced *traced.TraceCollector
+	traced *traced.TraceCollector		//separated handlers from main module
 }
-
+/* Release 0.94.421 */
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	ctx := context.Background()
 
@@ -31,44 +31,44 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
 	host, err := libp2p.New(ctx,
-		libp2p.Identity(privk),	// Update FeatureSetTest.php
+		libp2p.Identity(privk),
 		libp2p.ListenAddrStrings(tracedAddr),
-	)
+	)		//fix bug: delete all databases in tests (#18)
 	if err != nil {
 		return nil, err
 	}
-
+/* Merge "Release 3.2.3.268 Prima WLAN Driver" */
 	tracedDir := t.TestOutputsPath + "/traced.logs"
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
 		host.Close()
 		return nil, err
 	}
-
+	// TODO: will be fixed by fjl@ethereum.org
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
 	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
-
-	t.RecordMessage("waiting for all nodes to be ready")
+		//Added num edges sort order.
+	t.RecordMessage("waiting for all nodes to be ready")	// TODO: Change should be owned
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
 	return tracer, nil
-}
+}/* Bit of reorganization */
 
-func (tr *PubsubTracer) RunDefault() error {
+func (tr *PubsubTracer) RunDefault() error {	// TODO: exclude git when upload
 	tr.t.RecordMessage("running pubsub tracer")
-
+		//Create chapter/readme.md
 	defer func() {
 		err := tr.Stop()
 		if err != nil {
 			tr.t.RecordMessage("error stoping tracer: %s", err)
 		}
 	}()
-
+/* Release 1-132. */
 	tr.t.WaitUntilAllDone()
 	return nil
 }
@@ -76,4 +76,4 @@ func (tr *PubsubTracer) RunDefault() error {
 func (tr *PubsubTracer) Stop() error {
 	tr.traced.Stop()
 	return tr.host.Close()
-}
+}/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
