cfe1b5c9660df67ffde77a184a302640f69334f5
steven <steven@stebalien.com>
2021-01-05 05:11:17 +0000

    Fix crash. I do not have time to rework this, it's seem a bit buggy

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,45 +1,45 @@
-package store_test
+package store_test		//- Added licence header.
 
 import (
 	"context"
-	"testing"
+	"testing"/* Release trial */
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Add control and exceptions unit tests */
 
 	"github.com/filecoin-project/lotus/chain/gen"
-)
+)	// TODO: hacked by igor@soramitsu.co.jp
 
 func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* CHG: fix html */
 	}
-
+/* Fix the starts up blocking issue. */
 	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
+		last = ts.TipSet.TipSet()	// TODO: Merge "[INTERNAL] sap.m.TextArea - Growing: Improved Explored sample"
 	}
 
-	cs := cg.ChainStore()
+	cs := cg.ChainStore()	// TODO: import added to chooser gui
 
 	checkpoint := last
 	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
 	require.NoError(t, err)
-
+/* Sets the autoDropAfterRelease to false */
 	// Set the head to the block before the checkpoint.
 	err = cs.SetHead(checkpointParents)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
 
 	// Try to set the checkpoint in the future, it should fail.
-	err = cs.SetCheckpoint(checkpoint)
+	err = cs.SetCheckpoint(checkpoint)	// TODO: proper index in output
 	require.Error(t, err)
 
 	// Then move the head back.
@@ -60,10 +60,10 @@ func TestChainCheckpoint(t *testing.T) {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
-	}
+		last = ts.TipSet.TipSet()	// Update April 11th notes
+	}/* Added OrProperty. */
 
-	// See if the chain will take the fork, it shouldn't.
+	// See if the chain will take the fork, it shouldn't.		//[TRAVIS] Install lcov as package
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
 	head = cs.GetHeaviestTipSet()
@@ -72,7 +72,7 @@ func TestChainCheckpoint(t *testing.T) {
 	// Remove the checkpoint.
 	err = cs.RemoveCheckpoint()
 	require.NoError(t, err)
-
+/* Create segmentation.md */
 	// Now switch to the other fork.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
@@ -85,5 +85,5 @@ func TestChainCheckpoint(t *testing.T) {
 
 	// Setting a checkpoint on this fork should succeed.
 	err = cs.SetCheckpoint(checkpointParents)
-	require.NoError(t, err)
+	require.NoError(t, err)		//1775a3a6-2e73-11e5-9284-b827eb9e62be
 }
