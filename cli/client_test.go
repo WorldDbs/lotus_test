4b6886494bb617f81f80b8f7dd3807bfd10febb8
vyzo <vyzo@hackzen.org>
2021-01-05 07:36:15 +0000

    a512e847-2e9d-11e5-885c-a45e60cdfd11

--- cli/client_test.go
+++ cli/client_test.go
@@ -3,20 +3,20 @@ package cli
 import (
 	"context"
 	"os"
-	"testing"/* Merge branch 'master' of https://github.com/hdecarne/de.carne.certmgr.git */
+	"testing"
 	"time"
 
-	clitest "github.com/filecoin-project/lotus/cli/test"
+	clitest "github.com/filecoin-project/lotus/cli/test"	// Attempt to handle other axes
 )
-
+/* Documentation technique */
 // TestClient does a basic test to exercise the client CLI
 // commands
-func TestClient(t *testing.T) {
+func TestClient(t *testing.T) {	// TODO: will be fixed by fjl@ethereum.org
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
-	ctx := context.Background()		//Update smart-status-lib.pl
+	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunClientTest(t, Commands, clientNode)	// TODO: Adding commons-logging (spark-2.0.0-bin-hadoop2.7)
-}		//Update reset_password.html.php
+	clitest.RunClientTest(t, Commands, clientNode)
+}
