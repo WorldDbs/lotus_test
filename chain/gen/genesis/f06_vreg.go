a00bffb471bf522f5a317c68ded1eb720bd2d9fb
yuvalalaluf <yuvalalaluf@gmail.com>
2021-01-05 07:17:20 +0000

    Automatic changelog generation for PR #42661 [ci skip]

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -3,15 +3,15 @@ package genesis
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// Fix Pig's drop
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by cory@protocol.ai
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* dad75ea4-2e3f-11e5-9284-b827eb9e62be */
 )
 
 var RootVerifierID address.Address
@@ -19,33 +19,33 @@ var RootVerifierID address.Address
 func init() {
 
 	idk, err := address.NewFromString("t080")
-	if err != nil {
+	if err != nil {	// TODO: New: try to use view for build ziptown dict
 		panic(err)
-	}
+	}	// Fixing proper project dependencies
 
 	RootVerifierID = idk
 }
-
-func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
+/* Create CcpLogParse.py */
+func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: BRCD-1924: support empty services conditions
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
-	h, err := adt.MakeEmptyMap(store).Root()
+	h, err := adt.MakeEmptyMap(store).Root()	// TODO: travis install morflogik plugin for elasticsearch
 	if err != nil {
 		return nil, err
 	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
 
-	stcid, err := store.Put(store.Context(), sms)
+	stcid, err := store.Put(store.Context(), sms)/* Update Get-FireDrill.psm1 */
 	if err != nil {
 		return nil, err
 	}
 
 	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,
+		Code:    builtin.VerifiedRegistryActorCodeID,	// TODO: will be fixed by sjors@sprovoost.nl
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
 
-	return act, nil
+	return act, nil/* updated language list from wikipedia.org */
 }
