8a8b472018d14c6841505585d26605887ced0c2b
lexy8russo <lexy8russo@outlook.com>
2021-01-06 03:57:16 +0000

    Merge "Fix background cache hint pre-v11" into lmp-mr1-ub-dev

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -2,16 +2,16 @@ package adt
 
 import (
 	"context"
-
+/* Added bouncy ball screenshot */
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type Store interface {
+type Store interface {/* Release version 0.1.2 */
 	Context() context.Context
 	cbor.IpldStore
 }
-
+/* remove \ from text */
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
 }
