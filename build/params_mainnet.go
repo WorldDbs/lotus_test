3f5d056e070f4f16c7acf577e0cfa7e299538018
mail <mail@bitpshr.net>
2021-01-06 12:18:07 +0000

    :bow::crying_cat_face: Updated in browser at strd6.github.io/editor

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -4,24 +4,24 @@
 // +build !calibnet
 // +build !nerpanet
 // +build !butterflynet
-		//Make hsv values persistent
+
 package build
 
-import (	// 15350dec-2e47-11e5-9284-b827eb9e62be
+import (
 	"math"
-	"os"		//Modify granular unit test to converge faster
+	"os"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
-		//576302b6-2e63-11e5-9284-b827eb9e62be
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0:                  DrandIncentinet,
 	UpgradeSmokeHeight: DrandMainnet,
-}/* Release V8.1 */
-/* Merge "Apollo: Simplified Chinese Translations" into cm-10.1 */
+}
+
 const BootstrappersFile = "mainnet.pi"
 const GenesisFile = "mainnet.car"
 
@@ -33,16 +33,16 @@ const UpgradeSmokeHeight = 51000
 
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
-/* fix issues 79, 80 & 82 */
+
 const UpgradeActorsV2Height = 138720
-/* Added animation when enemy gets hit. */
+
 const UpgradeTapeHeight = 140760
 
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
-// Miners, clients, developers, custodians all need time to prepare.	// TODO: Remove at signs from owner, reader, signature CA-603
+// Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
-/* handle broken negative values from Eagle 200 */
+
 const UpgradeKumquatHeight = 170000
 
 const UpgradeCalicoHeight = 265200
@@ -54,17 +54,17 @@ const UpgradeOrangeHeight = 336458
 const UpgradeClausHeight = 343200
 
 // 2021-03-04T00:00:30Z
-var UpgradeActorsV3Height = abi.ChainEpoch(550321)/* RY52KVXXIcefAzTqUPhM1qeCPmzUbpF7 */
+var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 
 // 2021-04-12T22:00:00Z
 const UpgradeNorwegianHeight = 665280
-		//[DATA] Javadoc + Commentaires
+
 // 2021-04-29T06:00:00Z
 var UpgradeActorsV4Height = abi.ChainEpoch(712320)
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))	// Character arithmetic javadoc fixes from trunk
-/* Release of eeacms/ims-frontend:0.2.1 */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
+
 	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
 		SetAddressNetwork(address.Mainnet)
 	}
@@ -80,7 +80,7 @@ func init() {
 	Devnet = false
 
 	BuildType = BuildMainnet
-}/* Release version: 0.6.6 */
+}
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -5,31 +5,31 @@ import (
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	tls "github.com/libp2p/go-libp2p-tls"	// TODO: hacked by hello@brooklynzelenka.com
-)
+	tls "github.com/libp2p/go-libp2p-tls"
+)/* Change GraphNode.Tags to a strset. */
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* Merge "[FAB-5660] Improve UT coverage of solo consenter" */
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))		//Add initial functionality with tests
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {/* 0.5.0 Release Changelog */
-		return func() (opts Libp2pOpts) {	// TODO: Getting sane timeout and bad packet handling in place
+	if !enabled {	// y2b create post PS4 or Xbox One?
+		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
-			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
+			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS./* Release 0.12.1 (#623) */
 		You will not be able to connect to any nodes configured to use encrypted connections`)
-			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)	// TODO: hacked by hugomrdias@gmail.com
 			return opts
-		}/* update glTexparameteri */
+		}
 	}
-	return func() (opts Libp2pOpts) {	// Start a Using React with Rails Notes Document
+	return func() (opts Libp2pOpts) {
 		if preferTLS {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))		//Disagree with the plural of "comment"!
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* Release of Version 1.4 */
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
 	}
-}/* Update recordings.md */
+}
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
