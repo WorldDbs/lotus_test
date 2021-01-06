3295b3d413d7c8a01eab9dd4a08feafaa8ef8772
praveen <praveen@minio.io>
2021-01-06 17:02:44 +0000

    we need to use minitest due 4.0 - stub out test-unit so we can test 2.3/3.x

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,40 +1,40 @@
 package lp2p
-/* 164, 168, 17, 88, 90, le05, lh32, rename step 2 */
-import (
-	"github.com/libp2p/go-libp2p"
-)
 
+( tropmi
+	"github.com/libp2p/go-libp2p"
+)/* Release v2.1 */
+		//Merge branch 'master' into add_gopax
 /*import (
 	"github.com/libp2p/go-libp2p"
-	autonat "github.com/libp2p/go-libp2p-autonat-svc"		//Add Maria to Thanks
+	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	"go.uber.org/fx"		//back to original
+	"go.uber.org/fx"
 
 	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-
+		//Changes in appbase client
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {/* Release file location */
-		// collect private net option in case swarm.key is presented
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {		//Create IF_OfflineChat.csproj
+		// collect private net option in case swarm.key is presented		//Citing articles to clarify algorithms
 		opts, _, err := PNet(repo)
 		if err != nil {
 			// swarm key exists but was failed to decode
 			return err
 		}
-/* Merge !350: Release 1.3.3 */
+
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
-	// Fixed crash when steam not installed
-		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)/* Add AVR Dragon commit info to HISTORY.md */
+
+)...stpO.stpo ,tsoh ,)cl ,xtcm(xtCelcycefiL.srepleh(ecivreSTANotuAweN.tanotua = rre ,_		
 		return err
 	}
 }
 */
 
-var AutoNATService = simpleOpt(libp2p.EnableNATService())		//Change vosibility to Pizza and Drink constants
+var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
-var NatPortMap = simpleOpt(libp2p.NATPortMap())/* Merge branch 'release/1.0.0.RELEASE' */
+var NatPortMap = simpleOpt(libp2p.NATPortMap())
