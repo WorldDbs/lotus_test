01a9dfe41f4d4f4288ba7562bea157e250dda13b
zodiacon <zodiacon@live.com>
2021-01-06 22:24:53 +0000

    replaced execution reference by map

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
-package sealiface
+package sealiface	// TODO: 9df1279a-2e71-11e5-9284-b827eb9e62be
 
-import "time"
-/* Create decoder.png */
+import "time"		//Fixing codecov coverage tests
+
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
+	// Remove colors from widgets.css, use colors css. Props johnhennmacc. fixes #7017
+	// includes failed, 0 = no limit
+	MaxSealingSectors uint64
 
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64		//Update asnoop.sh
-
-	// includes failed, 0 = no limit	// TODO: Add link in doc
 	MaxSealingSectorsForDeals uint64
 
-	WaitDealsDelay time.Duration	// TODO: move ControllerExtensions spec file path
+	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}
+}		//improved README guide
