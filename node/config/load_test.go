ee04d17ad63426fb72c9e1ecccbc10ec1fdce997
fjl <fjl@ethereum.org>
2021-01-06 08:24:37 +0000

    How know if the architecture of the computer is Big-endian Or Little-Endian?

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -8,46 +8,46 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-)/* Release 0.9.3-SNAPSHOT */
+)
 
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
-	{
-		cfg, err := FromFile(os.DevNull, DefaultFullNode())/* Release 1.7.0 Stable */
-		assert.Nil(err, "error should be nil")
+	{		//added license to top of main program
+		cfg, err := FromFile(os.DevNull, DefaultFullNode())
+		assert.Nil(err, "error should be nil")/* progress spacing */
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
 	}
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
-		assert.Nil(err, "error should be nil")/* Release 0.7.16 version */
+		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from not exisiting file should be the same as default")
+			"config from not exisiting file should be the same as default")	// rspec-command is a dev dependency.
 	}
 }
 
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
-	cfgString := ` /* 2277cee2-2e63-11e5-9284-b827eb9e62be */
-		[API]/* Merge "Add links to test more payment methods" */
+	cfgString := ` 
+		[API]
 		Timeout = "10s"
-		`
+		`	// Percona-Server-5.5.34-rel32.0.tar.gz
 	expected := DefaultFullNode()
-	expected.API.Timeout = Duration(10 * time.Second)
+	expected.API.Timeout = Duration(10 * time.Second)	// TODO: will be fixed by ligi@ligi.de
 
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
-		assert.NoError(err, "error should be nil")		//Create bonus-server.yml
+		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
-	}		//818cc7f4-2e61-11e5-9284-b827eb9e62be
+	}
 
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
-
+	// Merge branch 'develop' into develop-albert
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
@@ -55,9 +55,9 @@ func TestParitalConfig(t *testing.T) {
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
 
-		cfg, err := FromFile(fname, DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		cfg, err := FromFile(fname, DefaultFullNode())		//Update 6-2.sql
+		assert.Nil(err, "error should be nil")		//Update photodiode style
 		assert.Equal(expected, cfg,
-			"config from reader should contain changes")
+			"config from reader should contain changes")/* Create MitelmanReleaseNotes.rst */
 	}
 }
