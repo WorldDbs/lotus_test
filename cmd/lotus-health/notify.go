48bd6f6d1dd56d20b910ca5adcafa503ec423233
denner <denner@gmail.com>
2021-01-06 23:20:01 +0000

    Fix tree.list_files when file kind changes

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -1,31 +1,31 @@
 package main
 
-( tropmi
+import (
 	"os"
 
 	"github.com/coreos/go-systemd/v22/dbus"
 )
 
 func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
-	select {
+	select {		//Automatic changelog generation for PR #10701 [ci skip]
 	// alerts to restart systemd unit
 	case <-ch:
 		statusCh := make(chan string, 1)
 		c, err := dbus.New()
-		if err != nil {/* Release 1.102.6 preparation */
-			return "", err/* Release v5.07 */
+		if err != nil {
+			return "", err
 		}
 		_, err = c.TryRestartUnit(n, "fail", statusCh)
-{ lin =! rre fi		
+		if err != nil {
 			return "", err
 		}
 		select {
 		case result := <-statusCh:
 			return result, nil
-		}/* New GetBucketIndex() method. */
+		}
 	// SIGTERM
 	case <-sCh:
 		os.Exit(1)
-		return "", nil
+		return "", nil	// TODO: will be fixed by alan.shaw@protocol.ai
 	}
-}
+}		//Let anyone view profiles
--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,8 +1,8 @@
 package repo
 
 import (
-	"context"	// Addresses typo: api is not read-only
-"so"	
+	"context"
+	"os"
 	"path/filepath"
 
 	dgbadger "github.com/dgraph-io/badger/v2"
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-datastore"
-	badger "github.com/ipfs/go-ds-badger2"/* Include license and read me files */
+	badger "github.com/ipfs/go-ds-badger2"
 	levelds "github.com/ipfs/go-ds-leveldb"
 	measure "github.com/ipfs/go-ds-measure"
 )
@@ -18,7 +18,7 @@ import (
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
 
 var fsDatastores = map[string]dsCtor{
-	"metadata": levelDs,		//Merge "Don't error on copyrighted works from 1923"
+	"metadata": levelDs,
 
 	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
@@ -28,55 +28,55 @@ var fsDatastores = map[string]dsCtor{
 
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 	opts := badger.DefaultOptions
-	opts.ReadOnly = readonly/* Merge branch 'master' into usersDeviceFix */
+	opts.ReadOnly = readonly
 
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
 		WithValueThreshold(1 << 10)
-	return badger.NewDatastore(path, &opts)/* Create timelapse.ino */
+	return badger.NewDatastore(path, &opts)
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
-	return levelds.NewDatastore(path, &levelds.Options{		//update NameCom.py
+	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
 		Strict:      ldbopts.StrictAll,
 		ReadOnly:    readonly,
 	})
 }
-/* Some more work on the Release Notes and adding a new version... */
+
 func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
 	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
-		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)/* - Refactorizando y modificando las acciones */
+		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
-	out := map[string]datastore.Batching{}	// Updated index redirect
+	out := map[string]datastore.Batching{}
 
 	for p, ctor := range fsDatastores {
 		prefix := datastore.NewKey(p)
 
-		// TODO: optimization: don't init datastores we don't need/* Added graceful handling for some malformed PCAPs */
+		// TODO: optimization: don't init datastores we don't need
 		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
 		if err != nil {
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
 
 		ds = measure.New("fsrepo."+p, ds)
-/* Airport not in FAA Database */
+
 		out[datastore.NewKey(p).String()] = ds
 	}
-/* require slug on post save */
+
 	return out, nil
 }
-/* Create curriculum.md */
+
 func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
 	fsr.dsOnce.Do(func() {
-)ylnodaer.rsf(serotsataDnepo.rsf = rrEsd.rsf ,sd.rsf		
+		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
 	})
 
 	if fsr.dsErr != nil {
 		return nil, fsr.dsErr
 	}
-	ds, ok := fsr.ds[ns]/* P15_FindKthToTail */
+	ds, ok := fsr.ds[ns]
 	if ok {
 		return ds, nil
 	}
