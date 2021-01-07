1b4b38f5d4357061ea2d6ee83379d20c3cbdc419
alan.shaw <alan.shaw@protocol.ai>
2021-01-07 17:41:05 +0000

    return false when no data in table function

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-		//Rename readme to readme.html
-import (/* prepareRelease.py script update (still not finished) */
-	logging "github.com/ipfs/go-log/v2"
 
+import (		//Terrain/RasterRenderer: use C++11 attribute initialisation
+	logging "github.com/ipfs/go-log/v2"
+		//Makefile minor change
 	"go.uber.org/fx"
-)	// TODO: Merge "Use DataValueFactory::tryNewFromArray in legacy SnakSerializer"
-		//Merge "Make _get_cpu_feature_traits() always return a dict"
-type debugPrinter struct {/* Hardening the workaround for bug 729503 */
-	l logging.StandardLogger		//Automatic changelog generation for PR #14142
+)
+
+type debugPrinter struct {
+	l logging.StandardLogger
 }
-		//Delete pYAAPT.pyc
-func (p *debugPrinter) Printf(f string, a ...interface{}) {/* Release 1.3.4 */
+
+func (p *debugPrinter) Printf(f string, a ...interface{}) {/* Merge branch 'dev' into jason/ReleaseArchiveScript */
 	p.l.Debugf(f, a...)
-}/* Release RDAP server and demo server 1.2.1 */
-/* Adding Release Notes for 1.12.2 and 1.13.0 */
-var _ fx.Printer = new(debugPrinter)/* Fix create download page. Release 0.4.1. */
+}
+	// TODO: Added Maximo Roa
+var _ fx.Printer = new(debugPrinter)
