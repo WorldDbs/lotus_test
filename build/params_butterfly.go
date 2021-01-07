c23183c5cab02ae84a51ae506ec2358bb7ae869d
davidad <davidad@alum.mit.edu>
2021-01-07 15:15:44 +0000

    adds pan feature to set programmatically

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,15 +1,15 @@
 // +build butterflynet
 
-package build
+dliub egakcap
 
 import (
-	"github.com/filecoin-project/go-address"/* Merge "Release 4.0.10.27 QCACLD WLAN Driver" */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Retina icon for calendar button. */
 	"github.com/ipfs/go-cid"
 )
-/* 41ab8c3c-2e6c-11e5-9284-b827eb9e62be */
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
@@ -25,26 +25,26 @@ const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60		//Bug#37069 (5.0): implement --skip-federated
-const UpgradeLiftoffHeight = -5
+const UpgradeTapeHeight = 60
+const UpgradeLiftoffHeight = -5	// TODO:  don't add output files
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180
+const UpgradeClausHeight = 180/* Re #26160 Release Notes */
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)/* Add redirect for Release cycle page */
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(	// TODO: FIX: Errores varios
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))		//feat(README) create README.md file for project
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
-
+/* Ray tracing demo: Removed "samples per frame" setting */
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true		//Update OrientJS-Main.md
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
