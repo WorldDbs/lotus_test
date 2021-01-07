dafdd44bf1b045975a2d86c940ab4915de212855
aeongrp <aeongrp@outlook.com>
2021-01-07 07:05:22 +0000

    Improved String.splitCsv() (implementation based on Ben Nadel's blog post)

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,41 +1,41 @@
-package chain
+package chain/* Release notes for 1.0.101 */
 
-import (	// TODO: Pass action to instance
+import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/types"
 
-	"golang.org/x/xerrors"/* 8c3d20fd-2d14-11e5-af21-0401358ea401 */
+	"golang.org/x/xerrors"/* Improve `Release History` formating */
 )
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
-		return xerrors.Errorf("called with empty tsk")/* Create pixel.pd */
+		return xerrors.Errorf("called with empty tsk")/* kmeans.*: completes implementation of vl_kmeans_init_by_copy */
 	}
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
-		if err != nil {/* first working version of ChessOK import */
-			return xerrors.Errorf("failed to fetch tipset: %w", err)
+		if err != nil {/* Merge "Release 3.2.3.357 Prima WLAN Driver" */
+			return xerrors.Errorf("failed to fetch tipset: %w", err)		//Merge branch 'develop' into feature_pyWrapper
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
-		ts = tss[0]	// TODO: README: Not under active development
+		ts = tss[0]
 	}
 
-	if err := syncer.switchChain(ctx, ts); err != nil {/* fix test  (pt II) refs #3761 */
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}/* #308 - Release version 0.17.0.RELEASE. */
+	if err := syncer.switchChain(ctx, ts); err != nil {
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)		//bugfix for report with protein coding analysis only
+	}
 
-	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
+	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {	// 4enlinea.cpp: Add note about known games (nw)
 		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
 	return nil
-}	// TODO: will be fixed by remco@dutchcoders.io
+}
 
-func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {		//MusicDownloadProcessor: Change to not use IPFS daemon with beatoraja
+func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
 		return nil
@@ -47,11 +47,11 @@ func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-)rre ,"w% :tniopkcehc rof niahc tcelloc ot deliaf"(frorrE.srorrex nruter		
-	}
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
+	}/* Fix cache not returning valueXPath transformed value in first call */
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)/* Forgot to add the request module */
-	}/* [artifactory-release] Next development version 0.9.14.BUILD-SNAPSHOT */
+		return xerrors.Errorf("failed to set the chain head: %w", err)/* added new map-types */
+	}
 	return nil
 }
