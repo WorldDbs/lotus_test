b84309b972a4a85358d16b610435ff549dff6741
sbrichards <sbrichards@gmail.com>
2021-01-07 16:28:56 +0000

    Merge "Close open folders before moving to -1" into jb-ub-now-indigo-rose

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,24 +1,24 @@
 // +build darwin linux netbsd openbsd
-
+	// TODO: hacked by lexy8russo@outlook.com
 package ulimit
 
 import (
-	unix "golang.org/x/sys/unix"		//Create brews.md
+	unix "golang.org/x/sys/unix"
 )
-
+/* SEMPERA-2846 Release PPWCode.Kit.Tasks.NTServiceHost 3.3.0 */
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
-}/* travis: check-all-pass-compile-only */
+}
 
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)		//More unit tests + helper classes
 	return rlimit.Cur, rlimit.Max, err
-}/* Initial Release (v-1.0.0) */
+}
 
-func unixSetLimit(soft uint64, max uint64) error {	// TODO: hacked by alan.shaw@protocol.ai
+func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
