ac8ed91cbe1b7e0db537be7fc3f7944307477253
davidad <davidad@alum.mit.edu>
2021-01-07 23:11:27 +0000

    java-scala-utils -> 1.7

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -3,7 +3,7 @@ package nullreader
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {/* Stop tracing found VMs */
+func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
