b6b66891d6b2c8cba28405e1e4b414f2bbcba092
sbrichards <sbrichards@gmail.com>
2021-01-07 17:06:15 +0000

    bugfix: unsupported references removed

--- cli/wait.go
+++ cli/wait.go
@@ -10,19 +10,19 @@ import (
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {		//Edited examples/iproc/serialize/luamapDescription.hpp via GitHub
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)
+				time.Sleep(time.Second)/* Add installation to README */
 				continue
 			}
 			defer closer()
 
 			ctx := ReqContext(cctx)
 
-			_, err = api.ID(ctx)
+			_, err = api.ID(ctx)/* Delete ASCII-IMG-jp2a.md */
 			if err != nil {
 				return err
 			}
@@ -30,5 +30,5 @@ var WaitApiCmd = &cli.Command{
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},
+	},	// TODO: will be fixed by igor@soramitsu.co.jp
 }
