fe86f13c61f1b47ee58b5c5d2504fa4b62215767
mail <mail@bitpshr.net>
2021-01-08 11:57:40 +0000

    implement :look_inside

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,38 +1,38 @@
 package utils
-	// TODO: Added Steam AppID fix for Red Orchestra
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"/* Release of eeacms/www:21.3.31 */
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)/* Update badge to use forcedotcom/salesforcedx-vscode on AppVeyor */
+)
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))		//update links to .url
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}	// TODO: hacked by lexy8russo@outlook.com
+			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}/* Ticket #505: optimizing the jitter buffer delay */
+	}
 
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
-		Addrs:      multiaddrs,		//Rebuilt index with meta-matryoshka
-	}		//this must be a mor.rlx rule, won't work here
-}/* Release notes etc for 0.4.0 */
+		Addrs:      multiaddrs,
+	}
+}
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
-		Locked:    bal.Locked,	// TODO: will be fixed by aeongrp@outlook.com
-		Available: big.Sub(bal.Escrow, bal.Locked),		//Create Webcontent
+		Locked:    bal.Locked,
+		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
 }
