10f386d4050532ec03da3a492002a81616d10028
timnugent <timnugent@gmail.com>
2021-01-09 23:35:30 +0000

    RangeUtils: Remove depricated methods.

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,65 +1,65 @@
-package stmgr
-/* Release version 2.2.6 */
+package stmgr/* update patron list [skip ci] */
+		//Generated from 05123a9ba41f02c6e8ad24c6737881ba84353e38
 import (
 	"context"
 
 	"golang.org/x/xerrors"
-/* FLUX updated report publisher interface  */
+/* add: fetch-one,update! and destroy! */
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/go-address"/* break up the parser tests into individual files */
-	"github.com/filecoin-project/lotus/chain/state"/* Some more work on the Release Notes and adding a new version... */
-	"github.com/filecoin-project/lotus/chain/types"/* Create HydroEvents.txt */
-)/* Release of eeacms/varnish-eea-www:3.8 */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/state"
+	"github.com/filecoin-project/lotus/chain/types"/* Remove unused code environment_setup? */
+)
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
-	ts, err := sm.cs.GetTipSetFromKey(tsk)	// Merge branch 'staging' into fix_customer_query
-	if err != nil {	// TODO: will be fixed by yuvalalaluf@gmail.com
-		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)	// TODO: hacked by timnugent@gmail.com
+	ts, err := sm.cs.GetTipSetFromKey(tsk)/* Release: Making ready for next release iteration 6.7.2 */
+	if err != nil {
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
-}/* reslientLog/component recipe integration in software.cfg */
-/* Release 2.7 (Restarted) */
-func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+}/* Fix for #238 - Release notes for 2.1.5 */
+
+func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {	// TODO: will be fixed by timnugent@gmail.com
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// TODO: hacked by hello@brooklynzelenka.com
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
-	if err != nil {	// TODO: will be fixed by igor@soramitsu.co.jp
-		return nil, xerrors.Errorf("load state tree: %w", err)
-	}	// python.rb: prepare for Python 3.9
+	if err != nil {	// TODO: hacked by mail@bitpshr.net
+		return nil, xerrors.Errorf("load state tree: %w", err)/* Put the shit tour rank results in. */
+	}
 
 	return state, nil
 }
 
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {/* Merge "Release 3.2.3.379 Prima WLAN Driver" */
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}		//6eebc2da-2e3e-11e5-9284-b827eb9e62be
+	}
 
 	return state, nil
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
-	if err != nil {/* Release status posting fixes. */
+	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
-}
+}	// Moar testing.
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	state, err := sm.ParentStateTsk(tsk)	// TODO: HTML updates to layout (for now), including navigation bars
+	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
-		return nil, err
+		return nil, err/* #102 New configuration for Release 1.4.1 which contains fix 102. */
 	}
 	return state.GetActor(addr)
 }
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
-	state, err := sm.StateTree(st)
-	if err != nil {
+	state, err := sm.StateTree(st)	// Merge "Report correct return value from pep8 check"
+	if err != nil {/* update README.TXT with instructions to test the issue */
 		return nil, err
 	}
 	return state.GetActor(addr)
