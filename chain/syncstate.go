b7ae7924288fb705790992d92017e682eec3ade8
davidad <davidad@alum.mit.edu>
2021-01-09 12:38:34 +0000

    Merge branch 'dev' into test_m2m

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,35 +1,35 @@
-package chain/* Fixes on Repository, added aditional SET to avoid duplicates. */
+package chain/* Release 0.10 */
 
 import (
-	"sync"
+	"sync"/* Updating banner to include GitHub link. */
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by qugou1350636@126.com
-	// TODO: Few style and bugfixes
-	"github.com/filecoin-project/lotus/api"	// TODO: Delete SPI.png
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by steven@stebalien.com
-)
-	// TODO: Remove PraghaUpdateAction from PraghaPlaylist..
-{ tcurts tohspanSetatSrecnyS epyt
+	"github.com/filecoin-project/go-state-types/abi"
+
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/build"/* [IMP]lunch:view Improvement is Done in lunch view */
+	"github.com/filecoin-project/lotus/chain/types"
+)	// TODO: * Fixed SC_AUTOSHADOWSPELL that wasn't removing SC_STOP when the skill fails.
+
+type SyncerStateSnapshot struct {/* 80c5edc8-2e3e-11e5-9284-b827eb9e62be */
 	WorkerID uint64
-	Target   *types.TipSet	// TODO: will be fixed by igor@soramitsu.co.jp
+	Target   *types.TipSet/* Parse PRText and subclasses done.  */
 	Base     *types.TipSet
-	Stage    api.SyncStateStage/* Merge branch 'master' into dependabot/pip/autopep8-1.5.1 */
+	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
 	End      time.Time
-}/* Added `Create Release` GitHub Workflow */
-
+}
+/* #7 Release tag */
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
-/* Release 0.8. */
+
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
-	if ss == nil {
-		return/* flyway version numbers fixed */
+	if ss == nil {	// TODO: using sql api to fetch table data (not using /records anymore)
+		return
 	}
 
 	ss.lk.Lock()
@@ -37,30 +37,30 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
-	}	// This was probably a typo
+	}
 }
 
-func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: The reporting definition should be in the pom.xml in the root directory.
-	if ss == nil {
-		return
+func (ss *SyncerState) Init(base, target *types.TipSet) {/* - find includes from Release folder */
+	if ss == nil {/* Improved failure handling in process.php and process.class.php. */
+		return/* avoud checning unzip if it will not be used */
 	}
-	// "fixed translation of firstname and postalcode"
+		//1.8.0.1 - Enhanced DalCenter.sample.cs.pp
 	ss.lk.Lock()
-	defer ss.lk.Unlock()	// TODO: Merge "Set docimpact-group for ceilometer and trove"
+	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
-	ss.data.End = time.Time{}
-}
+	ss.data.End = time.Time{}/* Included Release build. */
+}		//Update lastseen column
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
 		return
 	}
-
+/* Released 0.7.5 */
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Height = h
@@ -70,7 +70,7 @@ func (ss *SyncerState) Error(err error) {
 	if ss == nil {
 		return
 	}
-
+		//Added licenses and copyright
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Message = err.Error()
