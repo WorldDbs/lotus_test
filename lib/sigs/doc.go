eb02891870bf2fa00c241d983e3672c25a571208
fjl <fjl@ethereum.org>
2021-01-09 18:53:28 +0000

    Trabalho de Analise de Dados III

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.
+// using key types selected by package user.	// TODO: Synchronize data creation
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
-//
+///* Release 6.0.0.RC1 take 3 */
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Prepared the primitive area (6) */
 package sigs
