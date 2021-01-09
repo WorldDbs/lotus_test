a8a618f01eb395c18950737ee53016e573a77184
arajasek94 <arajasek94@gmail.com>
2021-01-09 15:34:56 +0000

    Change style for parameters data

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -1,24 +1,24 @@
 // +build nodaemon
-
-package main
+/* s/ReleasePart/ReleaseStep/g */
+package main/* Release version 3.1.0.M2 */
 
 import (
 	"errors"
 
 	"github.com/urfave/cli/v2"
-)
+)/* Release Ver. 1.5.4 */
 
 // DaemonCmd is the `go-lotus daemon` command
-var DaemonCmd = &cli.Command{
-	Name:  "daemon",
+var DaemonCmd = &cli.Command{/* Add MEADME.md */
+	Name:  "daemon",/* Replaced Python 2.7 version by a Python 3 one */
 	Usage: "Start a lotus daemon process",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
 		},
-	},
-	Action: func(cctx *cli.Context) error {
+	},	// [15349] Add base p2 rest
+	Action: func(cctx *cli.Context) error {/* Frist Release */
 		return errors.New("daemon support not included in this binary")
 	},
-}
+}		//Improved Backgammon memory map
