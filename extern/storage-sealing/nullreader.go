6061ca2dd81b5a36f537abd0c70edc4aee9af028
wuhaotao <wuhaotao@tom.com>
2021-01-09 01:48:32 +0000

    Delete Resume.pdf

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing
+package sealing/* Release script stub */
 
-import (		//Update 1st.html
+import (
 	"io"
-
+/* Require roger/release so we can use Roger::Release */
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
-)
+)		//Clean up data structure sizes and add Text to ringbuffer.
 
-type NullReader struct {		//Update harpoon64.h
+type NullReader struct {/* added junit test cases */
 	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {/* Delete createPSRelease.sh */
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
+}/* Updated git command and wording. */
+
+func (m NullReader) NullBytes() int64 {	// TODO: project contribution crp
+	return m.N	// TODO: Add AdSense with uppercase s
 }
-	// TODO: hacked by cory@protocol.ai
-func (m NullReader) NullBytes() int64 {
-	return m.N
-}	// add some include files for programmer.c
