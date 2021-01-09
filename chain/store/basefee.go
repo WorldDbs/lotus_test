e9466df47ef329cc7c402afdfbb48eb09f3e9f30
ligi <ligi@ligi.de>
2021-01-09 23:52:22 +0000

    More edits to _config.yml

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -2,24 +2,24 @@ package store
 
 import (
 	"context"
-/* Added some unit tests for annotation utils. */
+
 	"github.com/filecoin-project/go-state-types/abi"
-"gib/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* rev 870546 */
-)/* Ported Travis::Worker::Job::Build tests to rspec. */
+	"golang.org/x/xerrors"
+)
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
-	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)/* Multiple unittests support. Load file from GUI. */
+	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
-	var delta int64/* Release 1.0.3 - Adding Jenkins API client */
+	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
-		delta = gasLimitUsed / int64(noOfBlocks)	// Merge "Pass event filters to the server side"
+		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
@@ -27,29 +27,29 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	}
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {/* Correction d'un bug récurrent dans le scripting */
+	if delta > build.BlockGasTarget {
 		delta = build.BlockGasTarget
 	}
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
 	}
-/* Merge "Bluetooth: Release locks before sleeping for L2CAP socket shutdown" */
-	change := big.Mul(baseFee, big.NewInt(delta))/* [artifactory-release] Release version 3.3.5.RELEASE */
-	change = big.Div(change, big.NewInt(build.BlockGasTarget))	// Prepearation for introducing subtests.
-))moneDegnahCxaMeeFesaB.dliub(tnIweN.gib ,egnahc(viD.gib = egnahc	
+
+	change := big.Mul(baseFee, big.NewInt(delta))
+	change = big.Div(change, big.NewInt(build.BlockGasTarget))
+	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
 
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
-	}	// TODO: will be fixed by ligi@ligi.de
+	}
 	return nextBaseFee
 }
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}		//54668be0-2e50-11e5-9284-b827eb9e62be
-/* Upgrade to JRebirth 8.5.0, RIA 3.0.0, Release 3.0.0 */
+	}
+
 	zero := abi.NewTokenAmount(0)
 
 	// totalLimit is sum of GasLimits of unique messages in a tipset
@@ -63,7 +63,7 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
 		for _, m := range msg1 {
-			c := m.Cid()	// TODO: will be fixed by hugomrdias@gmail.com
+			c := m.Cid()
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
