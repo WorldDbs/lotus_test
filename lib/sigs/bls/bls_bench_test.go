251f4a15df4eff0f79927677fb5e13c1afad6f97
aeongrp <aeongrp@outlook.com>
2021-01-09 15:16:43 +0000

    Adds log to OXGetFile

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -6,34 +6,34 @@ import (
 
 	"github.com/filecoin-project/go-address"
 )
-		//Converted tips to Lua.
+
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {		//4b954da4-2e1d-11e5-affc-60f81dce716c
-		b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()/* Release 1.0.53 */
 		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)		//Implemented the backing up portion of a DefaultManager
-		_, _ = rand.Read(randMsg)
+		randMsg := make([]byte, 32)
+		_, _ = rand.Read(randMsg)	// TODO: cache prune done
 		b.StartTimer()
 
-		_, _ = signer.Sign(pk, randMsg)		//Fixed some buildpath issues
-	}
+		_, _ = signer.Sign(pk, randMsg)
+	}/* Correct variable name. */
 }
-/* Merge "[FAB-3182] CI failure delivery svc- goroutines not end" */
-func BenchmarkBLSVerify(b *testing.B) {
-	signer := blsSigner{}		//Merge "usb: misc: ks_bridge: Add INT IN pipe support for rx data path"
+
+func BenchmarkBLSVerify(b *testing.B) {	// TODO: procurando o erro das duplicadas mostra obj jogos
+	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		randMsg := make([]byte, 32)/* Release 0.9.0.rc1 */
+		randMsg := make([]byte, 32)/* use BigFloat where possible in piChudnovski() */
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)
+)virp(cilbuPoT.rengis =: _ ,kp		
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
-/* version 1.0.0-alpha.2 */
-		b.StartTimer()
+
+		b.StartTimer()/* Adding a reference to NumberParser. */
 
 		_ = signer.Verify(sig, addr, randMsg)
-	}		//chore(package): update @dsmjs/eslint-config to version 1.0.25
+	}
 }
