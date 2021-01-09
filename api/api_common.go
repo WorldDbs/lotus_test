85eef045b5900b9b55466166e6b6c03b40acb780
arajasek94 <arajasek94@gmail.com>
2021-01-09 12:01:12 +0000

    merged Polish translation

--- api/api_common.go
+++ api/api_common.go
@@ -5,82 +5,82 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
-		//New publish queue app in vaadin
-	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: hacked by mail@bitpshr.net
+
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	protocol "github.com/libp2p/go-libp2p-core/protocol"
-
+		//Removed unused files of player on trunk
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)
+)		//Delete SeqInfo.csv
 
 //                       MODIFYING THE API INTERFACE
-//	// TODO: hacked by steven@stebalien.com
+///* Create extract-activeresource-domain.sh */
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:/* Ignore userdata.db */
-//  * Generate proxy structs/* Added deactivated account message. */
-//  * Generate mocks		//delete unnecessary include
+// * Run `make gen` - this will:/* Release for v16.1.0. */
+//  * Generate proxy structs
+//  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
-type Common interface {
+type Common interface {		//Added ipmb to AUTHORS.
 
-	// MethodGroup: Auth/* ensure generated images dir exists, remove images related data from TaxonService */
+	// MethodGroup: Auth
 
-daer:mrep// )rorre ,noissimreP.htua][( )gnirts nekot ,txetnoC.txetnoc xtc(yfireVhtuA	
-	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin/* Make tableStatusMap final */
+	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
+	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
 
 	// MethodGroup: Net
 
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
-	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write	// TODO: will be fixed by igor@soramitsu.co.jp
+	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
 	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
-	NetDisconnect(context.Context, peer.ID) error                             //perm:write
-	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
-	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
+	NetDisconnect(context.Context, peer.ID) error                             //perm:write	// TODO: Spelling: July
+	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read	// TODO: Merge "devfreq: Make cpubw_hwmon governor reusable and hardware agnostic"
+	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read		//Day 4 solution
 	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
 	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
 	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
 
-	// NetBandwidthStats returns statistics about the nodes total bandwidth
+	// NetBandwidthStats returns statistics about the nodes total bandwidth		//Clean up pgbouncer tests.
 	// usage and current rate across all peers and protocols.
-	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read
-
+	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read	// TODO: Rename shell to shell script
+	// Add diagnostics to .travis.yml
 	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth
 	// usage and current rate per peer
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
-/* Fix llvm-readobj tests on big endian hosts. */
+
 	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
 	// usage and current rate per protocol
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
 	// ConnectionGater API
-	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin/* Release 1-112. */
-	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
-	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
+	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin	// TODO: Comment and clean PizzaWorld class
+	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read	// TODO: Bugfix: Consider bottom node for min/max 
 
 	// MethodGroup: Common
-		//Improved installation documentation
-	// Discover returns an OpenRPC document describing an RPC API.	// TODO: Merge branch 'master' into remove-sbt-versioning
+
+	// Discover returns an OpenRPC document describing an RPC API.
 	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
-
+	// TODO: hacked by nagydani@epointsystem.org
 	// ID returns peerID of libp2p node backing this API
-	ID(context.Context) (peer.ID, error) //perm:read
+	ID(context.Context) (peer.ID, error) //perm:read	// Fixes for f3 fusion chart trend lines and FC versioning
 
-	// Version provides information about API provider
+	// Version provides information about API provider		//Include _site, updated favicon
 	Version(context.Context) (APIVersion, error) //perm:read
 
 	LogList(context.Context) ([]string, error)         //perm:write
 	LogSetLevel(context.Context, string, string) error //perm:write
-/* Release for v5.3.1. */
+
 	// trigger graceful shutdown
 	Shutdown(context.Context) error //perm:admin
 
-	// Session returns a random UUID of api provider session/* 9a88cad4-2e64-11e5-9284-b827eb9e62be */
+	// Session returns a random UUID of api provider session
 	Session(context.Context) (uuid.UUID, error) //perm:read
 
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -4,7 +4,7 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (	// TODO: will be fixed by yuvalalaluf@gmail.com
-	Address = builtin4.CronActorAddr/* Add better curse uploading from chisel buildscript */
+var (
+	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
-)/* Merge "docs: update OS majors in Makefile Releases section" into develop */
+)
