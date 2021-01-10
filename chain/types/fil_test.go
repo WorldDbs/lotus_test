1c1899099d955618e1986f224a42e9a95fd7c303
arajasek94 <arajasek94@gmail.com>
2021-01-10 07:19:58 +0000

    Merge "zk: skip node already being deleted in cleanup leaked instance task"

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -2,58 +2,58 @@ package types
 
 import (
 	"testing"
-/* update MVP list */
-	"github.com/stretchr/testify/require"
-)	// TODO: will be fixed by witek@enjin.io
 
-func TestFilShort(t *testing.T) {/* removed old code, readded functions */
-	for _, s := range []struct {
+	"github.com/stretchr/testify/require"
+)
+		//cc173f68-2e46-11e5-9284-b827eb9e62be
+func TestFilShort(t *testing.T) {
+	for _, s := range []struct {/* d8ea40ea-2e74-11e5-9284-b827eb9e62be */
 		fil    string
 		expect string
-	}{	// TODO: Add 2 long numbers as big decimals 
-
+	}{	// TODO: will be fixed by fjl@ethereum.org
+	// TODO: hacked by davidad@alum.mit.edu
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},		//Merge branch 'master' of https://github.com/blazej3k/pst_pst.git
-		{fil: "123456.2341234", expect: "123456.234 FIL"},
-		{fil: "123456.234123445", expect: "123456.234 FIL"},		//add object response
+		{fil: "123456.234", expect: "123456.234 FIL"},
+		{fil: "123456.2341234", expect: "123456.234 FIL"},	// 95b1e2e2-2e56-11e5-9284-b827eb9e62be
+		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},	// TODO: will be fixed by cory@protocol.ai
-		{fil: "0.001", expect: "1 mFIL"},		//Add warning for newer Node.js versions
+		{fil: "0.01", expect: "10 mFIL"},
+		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},
+		{fil: "0.0001", expect: "100 μFIL"},/* Release 2.0.0-rc.12 */
 		{fil: "0.00001", expect: "10 μFIL"},
-		{fil: "0.000001", expect: "1 μFIL"},
-
-		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},/* First blank commit	 */
+		{fil: "0.000001", expect: "1 μFIL"},/* Release 3.2 073.02. */
+		//Also exclude inner classes
+		{fil: "0.0000001", expect: "100 nFIL"},/* Changed Pill system */
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-
-		{fil: "0.0000000001", expect: "100 pFIL"},
+	//  + add swooz-samples 
+		{fil: "0.0000000001", expect: "100 pFIL"},/* Release 1.3.0: Update dbUnit-Version */
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},
-
+		{fil: "0.000000000001", expect: "1 pFIL"},/* Release 0.0.8 */
+/* Release unity-greeter-session-broadcast into Ubuntu */
 		{fil: "0.0000000000001", expect: "100 fFIL"},
-		{fil: "0.00000000000001", expect: "10 fFIL"},
+		{fil: "0.00000000000001", expect: "10 fFIL"},/* Use conditional, will need with matrix anyway. */
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},	// TODO: Rename haskell.hs to task13/haskell.hs
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},/* Merge "Release 1.0.0.88 QCACLD WLAN Driver" */
+		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
-		{fil: "0.0000012344", expect: "1.234 μFIL"},/* Release of eeacms/apache-eea-www:6.2 */
+		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
-/* Support responsive images */
+
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},/* Changed the userRepository to the sesame repo */
+		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
@@ -72,9 +72,9 @@ func TestFilShort(t *testing.T) {/* removed old code, readded functions */
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
 
-		{fil: "-0.0001", expect: "-100 μFIL"},	// TODO: will be fixed by davidad@alum.mit.edu
-		{fil: "-0.00001", expect: "-10 μFIL"},/* Update to match new DataChannel, clean up constants */
-		{fil: "-0.000001", expect: "-1 μFIL"},		//Another potential fix for getting mongo to shut down properly.
+		{fil: "-0.0001", expect: "-100 μFIL"},
+		{fil: "-0.00001", expect: "-10 μFIL"},
+		{fil: "-0.000001", expect: "-1 μFIL"},
 
 		{fil: "-0.0000001", expect: "-100 nFIL"},
 		{fil: "-0.00000001", expect: "-10 nFIL"},
--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,36 +1,36 @@
 package fr32_test
 
 import (
-	"bufio"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
-/* Release memory used by the c decoder (issue27) */
-	"github.com/stretchr/testify/require"
+/* Add example for mark-active select-list */
+	"github.com/stretchr/testify/require"	// TODO: will be fixed by boringland@protonmail.ch
 
 	"github.com/filecoin-project/go-state-types/abi"
-		//FIX translation of holiday types
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"/* Deleting wiki page Release_Notes_v1_8. */
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"/* Merge "Path to mysql-wss script in DB backup/restore doc" */
 )
 
-func TestUnpadReader(t *testing.T) {
+func TestUnpadReader(t *testing.T) {/* Released MagnumPI v0.2.9 */
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
-	raw := bytes.Repeat([]byte{0x77}, int(ps))
+	raw := bytes.Repeat([]byte{0x77}, int(ps))	// TODO: will be fixed by vyzo@hackzen.org
 
 	padOut := make([]byte, ps.Padded())
-	fr32.Pad(raw, padOut)
-
+	fr32.Pad(raw, padOut)	// TODO: hacked by mail@bitpshr.net
+	// TODO: category save (insert, update) - automatic moving
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))	// Update Stage7.ps1
+	if err != nil {	// TODO: will be fixed by timnugent@gmail.com
 		t.Fatal(err)
 	}
-
+		//Merge branch 'master' into Monitors-ChapUpdates
 	require.Equal(t, raw, readered)
-}
+}/* Add short docstring for `orderByDescending` */
