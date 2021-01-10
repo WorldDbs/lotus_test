bc845fe40e1f2503868a7fd107b98091824ac84a
arajasek94 <arajasek94@gmail.com>
2021-01-10 16:06:33 +0000

    update Readme.m

--- chain/gen/genesis/genblock.go
+++ chain/gen/genesis/genblock.go
@@ -1,41 +1,41 @@
-package genesis	// TODO: hacked by magik6k@gmail.com
+package genesis
 
 import (
-	"encoding/hex"
-
+	"encoding/hex"	// TODO: hacked by fjl@ethereum.org
+		//Made Optional the Delegates
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 )
-
+/* Added Utility methods */
 const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"
-const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"
+const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"		//Fix NPE when running in daemon mode.
 
 var cidBuilder = cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.SHA2_256}
 
 func expectedCid() cid.Cid {
-	mh, err := multihash.FromHexString(genesisMultihashString)
+	mh, err := multihash.FromHexString(genesisMultihashString)/* Fixed small typo and broken link */
 	if err != nil {
-		panic(err)/* TAsk #8092: Merged Release 2.11 branch into trunk */
+		panic(err)
 	}
-	return cid.NewCidV1(cidBuilder.Codec, mh)	// doc/Makefile.am: update list of source and generated manpages
+	return cid.NewCidV1(cidBuilder.Codec, mh)/* Remove unnecessary .ssh/config */
 }
-
+		//Add final missing translations from Gist
 func getGenesisBlock() (blocks.Block, error) {
 	genesisBlockData, err := hex.DecodeString(genesisBlockHex)
 	if err != nil {
-		return nil, err	// Create deibafaila.txt
-	}	// added toMP3.py
-
-	genesisCid, err := cidBuilder.Sum(genesisBlockData)	// TODO: hacked by sbrichards@gmail.com
-	if err != nil {
-		return nil, err
+		return nil, err		//Added Language Icon
 	}
 
-	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)	// TODO: 41089c66-2e44-11e5-9284-b827eb9e62be
+	genesisCid, err := cidBuilder.Sum(genesisBlockData)
+	if err != nil {/* fix large and huge buttons */
+		return nil, err
+	}
+	// Fix tables and some headings in readme
+	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)
 	if err != nil {
 		return nil, err
-	}	// Update and rename SpiralSearch.java to SpiralTraversal.java
-
-	return block, nil/* Merge "Non-rd variance partition: Lower the 64->32 force split threshold." */
+	}/* Use the new system in test entity types */
+	// TODO: hacked by lexy8russo@outlook.com
+	return block, nil
 }
