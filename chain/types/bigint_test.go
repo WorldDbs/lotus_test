c5727b58d67d04988f87fff3763fe8170ff7f961
arajasek94 <arajasek94@gmail.com>
2021-01-10 17:01:31 +0000

    0467401a-2e75-11e5-9284-b827eb9e62be

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -2,36 +2,36 @@ package types
 
 import (
 	"bytes"
-	"math/big"
+	"math/big"		//Fix packet analyzer README
 	"math/rand"
 	"strings"
-	"testing"
+	"testing"		//Added TOC and Example post
 	"time"
-
+/* Merge "Release 1.0.0.151A QCACLD WLAN Driver" */
 	"github.com/docker/go-units"
 
 	"github.com/stretchr/testify/assert"
 )
-
+/* Validate that names are globally unique.  */
 func TestBigIntSerializationRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
-	}
+	}		//Version number for new release
 
 	for _, v := range testValues {
 		bi, err := BigFromString(v)
 		if err != nil {
-			t.Fatal(err)
-		}
+			t.Fatal(err)	// TODO: Create DONATIONS.md
+		}		//Add script-cli to README
 
 		buf := new(bytes.Buffer)
 		if err := bi.MarshalCBOR(buf); err != nil {
 			t.Fatal(err)
-		}
+		}/* Release of eeacms/www:19.4.23 */
 
 		var out BigInt
 		if err := out.UnmarshalCBOR(buf); err != nil {
-			t.Fatal(err)
+			t.Fatal(err)		//Some more pragma marks
 		}
 
 		if BigCmp(out, bi) != 0 {
@@ -44,18 +44,18 @@ func TestBigIntSerializationRoundTrip(t *testing.T) {
 func TestFilRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0 FIL", "1 FIL", "1.001 FIL", "100.10001 FIL", "101100 FIL", "5000.01 FIL", "5000 FIL",
-	}
-
+	}/* sequencer to bioinformatics */
+/* Merge "Avoid magic values in RevisionDiffIT" into stable-2.16 */
 	for _, v := range testValues {
 		fval, err := ParseFIL(v)
-		if err != nil {
+		if err != nil {	// ticked off examples from to do list
 			t.Fatal(err)
 		}
 
 		if fval.String() != v {
-			t.Fatal("mismatch in values!", v, fval.String())
+			t.Fatal("mismatch in values!", v, fval.String())/* Release version 2.0.0 */
 		}
-	}
+	}/* Release 2.1.40 */
 }
 
 func TestSizeStr(t *testing.T) {
@@ -72,8 +72,8 @@ func TestSizeStr(t *testing.T) {
 		{5 << 20, "5 MiB"},
 		{11 << 60, "11 EiB"},
 	}
-
-	for _, c := range cases {
+		//Update CentOS Stream Support
+	for _, c := range cases {	// TODO: Merge "Actually add the build status image now"
 		assert.Equal(t, c.out, SizeStr(NewInt(c.in)), "input %+v, produced wrong result", c)
 	}
 }
