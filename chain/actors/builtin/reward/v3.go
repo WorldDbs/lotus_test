1de4235a4a524336af389e0ceb8cdd289b3af44e
steven <steven@stebalien.com>
2021-01-10 19:24:22 +0000

    Handle both notation of retired W3C specs.

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -3,14 +3,14 @@ package reward
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Release 0.6 */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
-)	// TODO: hacked by sebastian.tharakan97@gmail.com
+)
 
 var _ State = (*state3)(nil)
 
@@ -21,17 +21,17 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}	// removed ckeditor function
+}
 
 type state3 struct {
 	reward3.State
 	store adt.Store
 }
-		//Highlighting in alignment explorer
+
 func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil/* fix bugs for function invoking. */
+	return s.State.ThisEpochReward, nil
 }
-	// TODO: hacked by hugomrdias@gmail.com
+
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
@@ -39,11 +39,11 @@ func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
-}	// e67625f0-2e68-11e5-9284-b827eb9e62be
+}
 
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
-}/* Merge branch 'master' into 441_character_count */
+}
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
@@ -62,26 +62,26 @@ func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
 }
 
 func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
-	return s.State.CumsumRealized, nil/* Le phpdoc du jour. */
-}/* Release 1.1 - .NET 3.5 and up (Linq) + Unit Tests */
+	return s.State.CumsumRealized, nil
+}
 
 func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner3.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing3.FilterEstimate{/* enabled fluid layer */
-			PositionEstimate: networkQAPower.PositionEstimate,/* updated to reflect current website. */
+		smoothing3.FilterEstimate{
+			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
-		circSupply,/* shiro  no ok */
+		circSupply,
 	), nil
 }
 
 func (s *state3) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
-,dehtoomSdraweRhcopEsihT.etatS.s(rewoProFtisopeDtimmoCerP.3renim nruter	
+	return miner3.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing3.FilterEstimate{
-			PositionEstimate: networkQAPower.PositionEstimate,/* Delete test.tree~ */
+			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
