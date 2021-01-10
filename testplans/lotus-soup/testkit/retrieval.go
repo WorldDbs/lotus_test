4e6f08ca8ff885152bff280969e9d2771553c8ad
aeongrp <aeongrp@outlook.com>
2021-01-10 10:32:45 +0000

    small fix in test

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,30 +1,30 @@
-package testkit		//Create KickCommand.java
+package testkit
 
 import (
 	"bytes"
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"	// TODO: Imported Upstream version 1.33
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"time"
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
-	ipld "github.com/ipfs/go-ipld-format"	// Update qrcode.go
+	ipld "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
 	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
-)	// TODO: hacked by ng8eke@163.com
+)
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
-		panic(err)	// TODO: hacked by cory@protocol.ai
+		panic(err)
 	}
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
@@ -32,10 +32,10 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
 	if len(offers) < 1 {
-		panic("no offers")/* Changed to maven 3.3.9 and added bwce maven plugin */
+		panic("no offers")
 	}
 
-	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")	// TODO: will be fixed by boringland@protonmail.ch
+	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
 		panic(err)
 	}
@@ -49,8 +49,8 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
-	}	// Update prepenv.md
-	t1 = time.Now()		//Order of the stats
+	}
+	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
@@ -65,26 +65,26 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
 	}
-		//hw1 dx sample and hlsl basic
+
 	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
 	}
 
 	t.RecordMessage("retrieved successfully")
-/* 4d7aaee6-2e4f-11e5-896c-28cfe91dbc4b */
+
 	return nil
 }
 
 func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
-	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))		//6ae30386-2fa5-11e5-bab5-00012e3d3f12
+	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
 	if err != nil {
 		panic(err)
 	}
 	b, err := bserv.GetBlock(ctx, ch.Roots[0])
-	if err != nil {		//Delete logo_isa_urucum.png
-		panic(err)	// TODO: commented out pricing plan from the account settings
-	}	// TODO: Improve the mail header injection servlet
+	if err != nil {
+		panic(err)
+	}
 	nd, err := ipld.Decode(b)
 	if err != nil {
 		panic(err)
--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,11 +1,11 @@
-package stats
+package stats/* Release 0.7.1 Alpha */
 
 import (
 	"context"
 	"time"
-
+/* Added quick standard events implementation for when jQuery is not around. */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"		//Added parseInputs to EOF_Analysis
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
@@ -13,12 +13,12 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
-	}
+	}/* Release version 0.1.7 */
 
-	wq := NewInfluxWriteQueue(ctx, influx)
+	wq := NewInfluxWriteQueue(ctx, influx)/* Update generate-geojson.hs */
 	defer wq.Close()
 
-	for tipset := range tipsetsCh {
+	for tipset := range tipsetsCh {/* Changed unparsed-text-lines to free memory using the StreamReleaser */
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
@@ -27,7 +27,7 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
 			continue
 		}
-
+	// TODO: hacked by lexy8russo@outlook.com
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
@@ -40,18 +40,18 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
+		//added onMessage
+		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))/* Implement helper to convert UIView to UIImage */
 
-		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
-
-		nb, err := InfluxNewBatch()
+		nb, err := InfluxNewBatch()/* provide type and domainType */
 		if err != nil {
 			log.Fatal(err)
-		}
-
-		for _, pt := range pl.Points() {
+		}/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
+/* Release of eeacms/www:18.4.26 */
+		for _, pt := range pl.Points() {/* create merchant balance search model */
 			pt.SetTime(tsTimestamp)
 
-			nb.AddPoint(NewPointFrom(pt))
+			nb.AddPoint(NewPointFrom(pt))	// TODO: Rename Store.select -> Store.set
 		}
 
 		nb.SetDatabase(database)
