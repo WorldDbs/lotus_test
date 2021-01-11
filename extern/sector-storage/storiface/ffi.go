2c4fa64fd785d241b6cf764ac167e8e52260c05a
markruss <markruss@microsoft.com>
2021-01-11 12:30:43 +0000

    c1497b8a-2e4d-11e5-9284-b827eb9e62be

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,22 +1,22 @@
 package storiface
-
+/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
 import (
 	"context"
 	"errors"
-	// TODO: will be fixed by juan@benet.ai
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: will be fixed by witek@enjin.io
-var ErrSectorNotFound = errors.New("sector not found")/* more startup icons */
+
+var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
+/* ER:Add a POT file containing unique strings of the application. */
+func (i UnpaddedByteIndex) Padded() PaddedByteIndex {	// TODO: will be fixed by aeongrp@outlook.com
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
+}		//https://pt.stackoverflow.com/q/417766/101
 
-func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())		//Merge remote-tracking branch 'origin/hansel' into hansel
-}/* more about closure */
-
-type PaddedByteIndex uint64
+type PaddedByteIndex uint64	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
