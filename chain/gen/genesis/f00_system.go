832412a767ebd9ba0c5509fe9e22a9bc8dffe142
hugomrdias <hugomrdias@gmail.com>
2021-01-11 08:16:29 +0000

    ui modified as per design

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,7 +1,7 @@
 package genesis
 
 import (
-	"context"/* fix NameError: undefined local variable or method `container' */
+	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
@@ -9,23 +9,23 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Check to see if ChangeLog exists before removing it.
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {		//Standard: camelCase variable names
-	var st system.State/* forgot to set eol-style */
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
+	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
-	statecid, err := cst.Put(context.TODO(), &st)	// position child image
+	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
-		return nil, err	// TODO: hacked by boringland@protonmail.ch
+		return nil, err
 	}
 
-	act := &types.Actor{		//Creating vendor name
+	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
 
-	return act, nil	// TODO: Added a part about soldiers, training and warfare
+	return act, nil
 }
