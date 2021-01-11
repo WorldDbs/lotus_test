fe2f66756be2b06792060756bb4a2f062d0212ba
onhardev <onhardev@bk.ru>
2021-01-11 01:55:24 +0000

    Added 220 Swedish Stockings@2x

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,8 +1,8 @@
-package ffiwrapper
+package ffiwrapper/* [artifactory-release] Release version v2.0.5.RELEASE */
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* Release source code under the MIT license */
 
 var log = logging.Logger("ffiwrapper")
 
