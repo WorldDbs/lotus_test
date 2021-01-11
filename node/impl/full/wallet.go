1e8346b0c7a33f6fd84d4107a1e0d746bde4a687
juan <juan@benet.ai>
2021-01-11 06:44:28 +0000

    removed extra slash in script url

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -1,16 +1,16 @@
 package full
-/* Update project covjson-reader to 0.9.3 */
+
 import (
 	"context"
 
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// Get rid of sandbox files.  Sandboxes are dirty.
-	"github.com/filecoin-project/go-state-types/big"/* Moving DTOs to dedicated project. */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
-	// TODO: Add Color conversions
-	"github.com/filecoin-project/lotus/api"/* Release of eeacms/www-devel:18.10.3 */
+
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
@@ -19,7 +19,7 @@ import (
 
 type WalletAPI struct {
 	fx.In
-	// Migrating to version 3.x of the driver
+
 	StateManagerAPI stmgr.StateManagerAPI
 	Default         wallet.Default
 	api.Wallet
@@ -27,27 +27,27 @@ type WalletAPI struct {
 
 func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
-	if xerrors.Is(err, types.ErrActorNotFound) {	// Increase timeout for termination of recording job
+	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
-		return big.Zero(), err	// Merge branch 'master' into jk-dont-store-build-data
+		return big.Zero(), err
 	}
-	return act.Balance, nil/* 2.0.12 Release */
+	return act.Balance, nil
 }
 
-{ )rorre ,erutangiS.otpyrc*( )etyb][ gsm ,sserddA.sserdda k ,txetnoC.txetnoc xtc(ngiStellaW )IPAtellaW* a( cnuf
+func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
-	if err != nil {		//[PRE-1] defined contex root
+	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
-	}	// Create testImg
+	}
 	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
 		Type: api.MTUnknown,
 	})
-}/* Merge "[INTERNAL] Release notes for version 1.30.1" */
+}
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {	// Update test.tracker.clean.php
-	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)/* Release version: 1.0.2 [ci skip] */
-	if err != nil {/* 23b78dca-2e53-11e5-9284-b827eb9e62be */
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
+	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
 
