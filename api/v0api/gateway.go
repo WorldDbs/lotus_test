4d9b431693821578d4eaa6973d8fc24bc0588cb1
caojiaoyue <caojiaoyue@protonmail.com>
2021-01-12 05:45:03 +0000

    Update suicide-burn.ks

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -1,68 +1,68 @@
 package v0api
-
-import (/* Fix device block error message */
-	"context"
+	// Change file path, DS names and indentation
+import (
+	"context"/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
 
 	"github.com/ipfs/go-cid"
-		//Merge "Add CloudDomain support"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
 
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/api"/* FrameParser refactoring */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"		//First incomplete version of database SQL dump
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 //                       MODIFYING THE API INTERFACE
 //
-// NOTE: This is the V0 (Stable) API - when adding methods to this interface,
+// NOTE: This is the V0 (Stable) API - when adding methods to this interface,	// TODO: will be fixed by onhardev@bk.ru
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
-// This API is implemented in `v1_wrapper.go` as a compatibility layer backed	// fixed typo in package.json
-// by the V1 api		//Merge "Manually merge QC's change for SUPL_ES." into lmp-dev
-//		//close editors when workbench is closed
+// This API is implemented in `v1_wrapper.go` as a compatibility layer backed	// TODO: add DevTernity recommends shield
+// by the V1 api
+//
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:/* added the sensor_type association to data_values */
+// * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
-//  * Generate markdown docs
-//  * Generate openrpc blobs
+//  * Generate markdown docs/* Updated the pybroom feedstock. */
+//  * Generate openrpc blobs/* Add Dockerfile and travis cmd for Postgres */
 
-type Gateway interface {		//Merge "Add notification on deleting instance without host"
+type Gateway interface {		//Create stickers-to-spell-word.py
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)	// FunctionDescriptor validation text improved.
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
+	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
+)rorre ,egasseM.sepyt*( )diC.dic cm ,txetnoC.txetnoc xtc(egasseMteGniahC	
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)		//feedback on_cancel handler
+	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)		//Add support for Vertexium calendar aggregations (#391)
-	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)/* Add notes about converting code using Backbone */
-	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
+	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)	// TODO: Merge "test python-novaclient master changes against a stable/mitaka"
+	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// TODO: Update flash_main.js
+	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)	// Fix string handling in css loader
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
-	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)	// TODO: add base 60 for both CDD and HDD
+	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
-	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
-	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)		//Split the advisor feature into a separate page
+	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)/* More tests for wibbrlib.format.filetype. */
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)/* Delete eng.png */
+	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
 	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
-	WalletBalance(context.Context, address.Address) (types.BigInt, error)	// TODO: hacked by mikeal.rogers@gmail.com
-}	// TODO: will be fixed by praveen@minio.io
+	WalletBalance(context.Context, address.Address) (types.BigInt, error)/* Update Blogger API.  Add BloggerSample. */
+}
 
 var _ Gateway = *new(FullNode)
