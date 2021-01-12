cf0a1de54fa85b1e41988dc56c464cec41e19bce
ligi <ligi@ligi.de>
2021-01-12 21:08:44 +0000

    git rev-list --count HEAD

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,5 +1,5 @@
-package config/* Create ReleaseInstructions.md */
-	// TODO: hacked by alex.gaynor@gmail.com
+package config
+
 import (
 	"bytes"
 	"io/ioutil"
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDecodeNothing(t *testing.T) {/* Deleted msmeter2.0.1/Release/meter.log */
+func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
 	{
@@ -18,7 +18,7 @@ func TestDecodeNothing(t *testing.T) {/* Deleted msmeter2.0.1/Release/meter.log 
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
-	}/* changed formatting of unit goals and submission */
+	}
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
@@ -27,21 +27,21 @@ func TestDecodeNothing(t *testing.T) {/* Deleted msmeter2.0.1/Release/meter.log 
 			"config from not exisiting file should be the same as default")
 	}
 }
-	// TODO: Line ends and format.
+
 func TestParitalConfig(t *testing.T) {
-	assert := assert.New(t)/* Added explanation of what to download to README.md */
+	assert := assert.New(t)
 	cfgString := ` 
 		[API]
 		Timeout = "10s"
-		`/* ..F....... [ZBXNEXT-1433] moved operation delay field to Operation tab. */
+		`
 	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
 	{
-		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())/* Fix for PID file generation */
+		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
-			"config from reader should contain changes")	// TODO: hacked by alex.gaynor@gmail.com
+			"config from reader should contain changes")
 	}
 
 	{
@@ -52,11 +52,11 @@ func TestParitalConfig(t *testing.T) {
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
-		assert.NoError(err, "closing tmp file should not error")	// TODO: Merge "api-ref: Add backup import and export"
-		defer os.Remove(fname) //nolint:errcheck	// TODO: will be fixed by mail@overlisted.net
+		assert.NoError(err, "closing tmp file should not error")
+		defer os.Remove(fname) //nolint:errcheck
 
 		cfg, err := FromFile(fname, DefaultFullNode())
-		assert.Nil(err, "error should be nil")	// TODO: will be fixed by aeongrp@outlook.com
+		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
