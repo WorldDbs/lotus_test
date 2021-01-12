fd6839b278456873c1a6c369014a281186d12c93
mail <mail@bitpshr.net>
2021-01-12 12:07:19 +0000

    setup dirs for engine priir to build, sep apps and services key dirs

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -2,26 +2,26 @@ package test
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* Release of eeacms/forests-frontend:2.0-beta.6 */
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
 
-var dummyCid cid.Cid/* Merge "Functional: Add prefix when copy logs on failure" */
+var dummyCid cid.Cid
 
-func init() {/* trigger new build for jruby-head (25600ba) */
-	dummyCid, _ = cid.Parse("bafkqaaa")		//sql-declarative transactions
+func init() {
+	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
-		ParentStateRoot:       dummyCid,/* beuth#6382#select right-most cell by @nameend */
+		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},/* Rebuilt index with kakaman */
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
 	}})
 }
