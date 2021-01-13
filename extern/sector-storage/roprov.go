15e49fc9236feb4f252443d375b3f79fea1531b4
sjors <sjors@sprovoost.nl>
2021-01-13 08:05:28 +0000

    Merge branch 'master' into pr/add-module-trailmaking-test

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,8 +1,8 @@
-package sectorstorage		//refactor(main): element probe only in dev
-		//Update bsp_int.c
+package sectorstorage
+
 import (
 	"context"
-/* Fixed DrawForm view helper, matchTemplate() remove labels */
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
@@ -11,30 +11,30 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type readonlyProvider struct {	// fixed bug in avoid_readback disk cache eviction algorithm
+type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
-}		//Adding SFEIR styling
+}
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
-		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")/* Avoid upcasting ASN1ObjectIdentifier to DERObjectIdentifier */
-	}/* Release for 18.34.0 */
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
+	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	// use TryLock to avoid blocking/* Test PHP 7.0 */
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)	// Remove a pile of unnecessary state management from Scrollpanels.
+	// use TryLock to avoid blocking
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)	// updated test to use probability=T for prediction
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}/* Update Release#banner to support commenting */
+	}
 
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: will be fixed by ligi@ligi.de
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
 }
