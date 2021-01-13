e55ec3f9486573fcb5d35ac836a296e0ff5760d4
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-01-13 18:18:13 +0000

    introduce an adaptable way to hook extented resource exploerer icons.

--- chain/gen/genesis/genblock.go
+++ chain/gen/genesis/genblock.go
@@ -1,41 +1,41 @@
 package genesis
 
 import (
-	"encoding/hex"	// TODO: hacked by fjl@ethereum.org
-		//Made Optional the Delegates
+	"encoding/hex"
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 )
-/* Added Utility methods */
+
 const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"
-const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"		//Fix NPE when running in daemon mode.
+const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"
 
 var cidBuilder = cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.SHA2_256}
 
 func expectedCid() cid.Cid {
-	mh, err := multihash.FromHexString(genesisMultihashString)/* Fixed small typo and broken link */
+	mh, err := multihash.FromHexString(genesisMultihashString)
 	if err != nil {
 		panic(err)
 	}
-	return cid.NewCidV1(cidBuilder.Codec, mh)/* Remove unnecessary .ssh/config */
+	return cid.NewCidV1(cidBuilder.Codec, mh)
 }
-		//Add final missing translations from Gist
+
 func getGenesisBlock() (blocks.Block, error) {
 	genesisBlockData, err := hex.DecodeString(genesisBlockHex)
 	if err != nil {
-		return nil, err		//Added Language Icon
+		return nil, err
 	}
 
 	genesisCid, err := cidBuilder.Sum(genesisBlockData)
-	if err != nil {/* fix large and huge buttons */
+	if err != nil {
 		return nil, err
 	}
-	// Fix tables and some headings in readme
+
 	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)
 	if err != nil {
 		return nil, err
-	}/* Use the new system in test entity types */
-	// TODO: hacked by lexy8russo@outlook.com
+	}
+
 	return block, nil
 }
