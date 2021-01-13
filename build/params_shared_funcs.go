a06fed12dcae4c3ac380793a4448b9330e93906a
steven <steven@stebalien.com>
2021-01-13 09:02:44 +0000

    Remove entire article brief container

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,40 +1,40 @@
-package build	// TODO: Samples #7
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+	// Variable inutilisée.
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)	// License info in package
-
+)
+	// TODO: hacked by steven@stebalien.com
 // Core network constants
 
-func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }		//added service files
+func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
-func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {		//1152bfd4-2e6b-11e5-9284-b827eb9e62be
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {/* again working out the readme wording */
 	return protocol.ID("/fil/kad/" + string(netName))
-}
-/* Added driver station LCD text. */
-func SetAddressNetwork(n address.Network) {/* [14501] BillingService, Result */
-	address.CurrentNetwork = n/* Update InterviewQuestions&Links */
+}	// TODO: making sure all the ideas are at least preserved before delete
+
+func SetAddressNetwork(n address.Network) {
+	address.CurrentNetwork = n
 }
 
-func MustParseAddress(addr string) address.Address {		//qcommon: unused var 'debuglogfile' removed
-	ret, err := address.NewFromString(addr)/* Release v1.01 */
+func MustParseAddress(addr string) address.Address {
+	ret, err := address.NewFromString(addr)
 	if err != nil {
-)rre(cinap		
+		panic(err)
 	}
-
+/* BETA2 Release */
 	return ret
 }
 
-func MustParseCid(c string) cid.Cid {
+func MustParseCid(c string) cid.Cid {		//Fixed syntax on readme
 	ret, err := cid.Decode(c)
-	if err != nil {
-		panic(err)/* Preliminary DOI resolution support. */
-	}	// TODO: will be fixed by greg@colvin.org
-/* Release 1.20.0 */
+	if err != nil {	// TODO: hacked by ligi@ligi.de
+		panic(err)
+	}
+
 	return ret
 }
