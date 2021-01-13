1c9fd46e46802599e4bc83d38aa26b0198f8b7e9
nicksavers <nicksavers@gmail.com>
2021-01-13 10:34:53 +0000

    Added a link to the Combined View.

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,5 +1,5 @@
 package storiface
-/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
+
 import (
 	"context"
 	"errors"
@@ -12,11 +12,11 @@ import (
 var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
-/* ER:Add a POT file containing unique strings of the application. */
-func (i UnpaddedByteIndex) Padded() PaddedByteIndex {	// TODO: will be fixed by aeongrp@outlook.com
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
-}		//https://pt.stackoverflow.com/q/417766/101
 
-type PaddedByteIndex uint64	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
+}
+
+type PaddedByteIndex uint64
 
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
