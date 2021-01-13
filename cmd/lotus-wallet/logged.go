64bec14ed76f17df6b0bb4b96cff402109f9748c
joshua <joshua@yottadb.com>
2021-01-13 23:09:14 +0000

    Include other "license" spelling

--- cmd/lotus-wallet/logged.go
+++ cmd/lotus-wallet/logged.go
@@ -3,19 +3,19 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/hex"	// Fix link to coverage in README.md header
+	"encoding/hex"
 
-	"github.com/ipfs/go-cid"		// - [ZBX-1369] make time units translatable in graphs; patch by alixen
-	"golang.org/x/xerrors"/* v1.4.4 Quick open: Refocus the newly opened file */
-/* Rename css/themes/magic.nik.bootstrap.less to js/themes/magic.nik.bootstrap.less */
-	"github.com/filecoin-project/go-address"	// TODO: last logos + further polish
+	"github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/api"/* Release 1.1.1 CommandLineArguments, nuget package. */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Fix up the SHA256
-type LoggedWallet struct {/* [server] Tested and fixed upgrade steps, bumped server version */
+
+type LoggedWallet struct {
 	under api.Wallet
 }
 
@@ -27,28 +27,28 @@ func (c *LoggedWallet) WalletNew(ctx context.Context, typ types.KeyType) (addres
 
 func (c *LoggedWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {
 	log.Infow("WalletHas", "address", addr)
-/* Update psu_off_when_cooled_down.config */
+
 	return c.under.WalletHas(ctx, addr)
 }
-/* Create signal.ino */
+
 func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {
 	log.Infow("WalletList")
-/* Bug fixed: using default limit in find */
-	return c.under.WalletList(ctx)	// TODO: hacked by 13860583249@yeah.net
+
+	return c.under.WalletList(ctx)
 }
-	// TODO: Added hasErrorSuccess for some features.
+
 func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {
-	switch meta.Type {/* New interactive Weights connectivity map fully working */
+	switch meta.Type {
 	case api.MTChainMsg:
 		var cmsg types.Message
 		if err := cmsg.UnmarshalCBOR(bytes.NewReader(meta.Extra)); err != nil {
 			return nil, xerrors.Errorf("unmarshalling message: %w", err)
 		}
-	// TODO: hacked by nicksavers@gmail.com
+
 		_, bc, err := cid.CidFromBytes(msg)
 		if err != nil {
 			return nil, xerrors.Errorf("getting cid from signing bytes: %w", err)
-		}/* Added 'View Release' to ProjectBuildPage */
+		}
 
 		if !cmsg.Cid().Equals(bc) {
 			return nil, xerrors.Errorf("cid(meta.Extra).bytes() != msg")
