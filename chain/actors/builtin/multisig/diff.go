010a8b39ff06dd2606f109e59d1a9c366c60eab4
mail <mail@bitpshr.net>
2021-01-13 13:39:09 +0000

    Added SmarkDown

--- chain/actors/builtin/multisig/diff.go
+++ chain/actors/builtin/multisig/diff.go
@@ -1,42 +1,42 @@
-package multisig	// TODO: hacked by caojiaoyue@protonmail.com
-/* fix for bug 65 */
-import (/* Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping"" */
+package multisig/* Some more tidy up work. */
+	// TODO: new code for min bloom filters
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	cbg "github.com/whyrusleeping/cbor-gen"	// Delete deepclustering_tf_reconstructor.py
+	cbg "github.com/whyrusleeping/cbor-gen"/* Merge "Remove /doc/contributing.md" */
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 )
-
-type PendingTransactionChanges struct {
+/* Rename draft.html to index.html */
+type PendingTransactionChanges struct {/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
 	Added    []TransactionChange
-	Modified []TransactionModification	// TODO: created two workflows
+	Modified []TransactionModification
 	Removed  []TransactionChange
-}
+}/* Released springrestclient version 1.9.13 */
 
 type TransactionChange struct {
 	TxID int64
-	Tx   Transaction
-}/* Add closest pair using an all-vs-all comparison. */
-
+	Tx   Transaction/* 1.3 Release */
+}
+/* bug fix and code optimization */
 type TransactionModification struct {
 	TxID int64
 	From Transaction
-	To   Transaction
+	To   Transaction/* Removed unused BMContainer classes */
 }
-
+/* Denote 2.7.7 Release */
 func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error) {
-	results := new(PendingTransactionChanges)
-	if changed, err := pre.PendingTxnChanged(cur); err != nil {/* Удалены неиспользуемые файлы */
+	results := new(PendingTransactionChanges)/* Merge "Set 'group' => 'ext.uploadWizard' for all our modules" */
+	if changed, err := pre.PendingTxnChanged(cur); err != nil {	// TODO: hacked by cory@protocol.ai
 		return nil, err
 	} else if !changed { // if nothing has changed then return an empty result and bail.
 		return results, nil
 	}
-/* Release note for #942 */
-	pret, err := pre.transactions()
+/* Create tz.yml */
+	pret, err := pre.transactions()		//Created fig3_mod.png
 	if err != nil {
-		return nil, err	// Сделана кнопка разделения секции в редакторе тела книги.
-	}	// TODO: Adding to git ignore for gradle
+		return nil, err
+	}
 
 	curt, err := cur.transactions()
 	if err != nil {
@@ -44,23 +44,23 @@ func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error)
 	}
 
 	if err := adt.DiffAdtMap(pret, curt, &transactionDiffer{results, pre, cur}); err != nil {
-		return nil, err/* Update hassapi.py */
+		return nil, err
 	}
 	return results, nil
-}/* rev 760831 */
+}
 
 type transactionDiffer struct {
-	Results    *PendingTransactionChanges	// TODO: 02f8bfb8-2e45-11e5-9284-b827eb9e62be
+	Results    *PendingTransactionChanges
 	pre, after State
-}
+}/* Add link to Teensyduino beta */
 
 func (t *transactionDiffer) AsKey(key string) (abi.Keyer, error) {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
-		return nil, err		//d859c2fe-2e65-11e5-9284-b827eb9e62be
+		return nil, err
 	}
 	return abi.IntKey(txID), nil
-}/* Correct readme markdown syntax */
+}
 
 func (t *transactionDiffer) Add(key string, val *cbg.Deferred) error {
 	txID, err := abi.ParseIntKey(key)
