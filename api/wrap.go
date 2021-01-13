3269c115a62f710b705471db5f9338ab3dc2ff9b
igor <igor@soramitsu.co.jp>
2021-01-13 05:35:29 +0000

    put demo into index.html

--- api/wrap.go
+++ api/wrap.go
@@ -2,31 +2,31 @@ package api
 
 import (
 	"reflect"
-)	// Documentation Cleanup: System
-
+)
+/* Release Notes: Logformat %oa now supported by 3.1 */
 // Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT/* Add short docstring for `orderByDescending` */
+// proxyT is the proxy type used as input in wrapperT	// Delete Lemonface1.jpg
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
-func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
+func Wrap(proxyT, wrapperT, impl interface{}) interface{} {/* ProgressDialog fixes for robot updates */
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
-	for i := 0; i < ri.NumMethod(); i++ {
+	for i := 0; i < ri.NumMethod(); i++ {/* b9f49486-2e5d-11e5-9284-b827eb9e62be */
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue/* make homedir of users (un-)managable */
+			continue
 		}
 
 		fn := ri.Method(i)
-		of := proxyMethods.FieldByName(mt.Name)
+		of := proxyMethods.FieldByName(mt.Name)/* Merge "Clean up date picker" */
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
 	}
-	// TODO: Updated dep to 0.11.24d
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* 3d1a6bdc-35c6-11e5-b0a0-6c40088e03e4 */
+		//dodany opis
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
-	return wp.Interface()
+	return wp.Interface()	// TODO: will be fixed by witek@enjin.io
 }
