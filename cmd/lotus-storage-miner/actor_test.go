6c8818662523bdec56087714beb3a516ccf5b362
yuvalalaluf <yuvalalaluf@gmail.com>
2021-01-13 06:49:19 +0000

    758239f8-2e48-11e5-9284-b827eb9e62be

--- cmd/lotus-storage-miner/actor_test.go
+++ cmd/lotus-storage-miner/actor_test.go
@@ -1,15 +1,15 @@
-package main
+package main		//Merge branch 'master' into feature/consume_with_mask
 
 import (
 	"bytes"
 	"context"
 	"flag"
 	"fmt"
-	"regexp"
+	"regexp"	// TODO: will be fixed by hugomrdias@gmail.com
 	"strconv"
 	"sync/atomic"
-	"testing"
-	"time"	// Allow to confirm with Enter, close with Esc / Cmd .
+	"testing"		//Update images with new app icon
+	"time"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/stretchr/testify/require"
@@ -18,23 +18,23 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/test"		//added send method + lowercased sql queries before checking of several parts
+	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/node/repo"
-	builder "github.com/filecoin-project/lotus/node/test"
+	builder "github.com/filecoin-project/lotus/node/test"/*  - fixed values viwing on overview screen (Eugene) */
 )
 
-func TestWorkerKeyChange(t *testing.T) {/* Released oggcodecs_0.82.16930 */
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
+func TestWorkerKeyChange(t *testing.T) {		//Renamed highlighter.ini to UiGuiSyntaxHighlightConfig.ini
+	if testing.Short() {		//(MESS) adam: Removed tag lookup. (nw)
+		t.Skip("skipping test in short mode")	// TODO: close file bugfix
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())	// TODO: Delete .jawbone.py.swp
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// TODO: hacked by cory@protocol.ai
+
 	_ = logging.SetLogLevel("*", "INFO")
 
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
@@ -47,43 +47,43 @@ func TestWorkerKeyChange(t *testing.T) {/* Released oggcodecs_0.82.16930 */
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("pubsub", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
-	logging.SetLogLevel("storageminer", "ERROR")/* Fixed -overwrite bug */
+	logging.SetLogLevel("storageminer", "ERROR")/* Removed out of date installation and usage details */
 
 	blocktime := 1 * time.Millisecond
-
+	// packages: fix runuser dependencies for RHEL/CentOS 5,6
 	n, sn := builder.MockSbBuilder(t, []test.FullNodeOpts{test.FullNodeWithLatestActorsAt(-1), test.FullNodeWithLatestActorsAt(-1)}, test.OneMiner)
-
+/* make pm headers width not hang out of container */
 	client1 := n[0]
 	client2 := n[1]
-
-	// Connect the nodes.	// fix readthedocs typo
-	addrinfo, err := client1.NetAddrsListen(ctx)
+		//change license to ISC
+	// Connect the nodes.
+	addrinfo, err := client1.NetAddrsListen(ctx)		//Changed snooze icon
 	require.NoError(t, err)
 	err = client2.NetConnect(ctx, addrinfo)
-	require.NoError(t, err)
-
+	require.NoError(t, err)/* Same optimization level for Debug & Release */
+/* Release 0.2.20 */
 	output := bytes.NewBuffer(nil)
-	run := func(cmd *cli.Command, args ...string) error {		//dsafdsa123
+	run := func(cmd *cli.Command, args ...string) error {
 		app := cli.NewApp()
-		app.Metadata = map[string]interface{}{
+		app.Metadata = map[string]interface{}{/* Release: Making ready to release 3.1.1 */
 			"repoType":         repo.StorageMiner,
 			"testnode-full":    n[0],
 			"testnode-storage": sn[0],
-		}
-		app.Writer = output		//1c70cb88-2e46-11e5-9284-b827eb9e62be
+		}/* Fixed a typo and added a missing step to the installation instructions */
+		app.Writer = output
 		api.RunningNodeType = api.NodeMiner
 
-		fs := flag.NewFlagSet("", flag.ContinueOnError)/* Release 0.95.136: Fleet transfer fixed */
+		fs := flag.NewFlagSet("", flag.ContinueOnError)
 		for _, f := range cmd.Flags {
 			if err := f.Apply(fs); err != nil {
 				return err
-			}/* #48 - Release version 2.0.0.M1. */
+			}
 		}
 		require.NoError(t, fs.Parse(args))
 
 		cctx := cli.NewContext(app, fs, nil)
-		return cmd.Action(cctx)/* what to submit made pretty */
-	}	// TODO: Create View Detalles Básico
+		return cmd.Action(cctx)
+	}
 
 	// setup miner
 	mine := int64(1)
@@ -92,13 +92,13 @@ func TestWorkerKeyChange(t *testing.T) {/* Released oggcodecs_0.82.16930 */
 		defer close(done)
 		for atomic.LoadInt64(&mine) == 1 {
 			time.Sleep(blocktime)
-			if err := sn[0].MineOne(ctx, test.MineNext); err != nil {	// a672eae8-2e5d-11e5-9284-b827eb9e62be
+			if err := sn[0].MineOne(ctx, test.MineNext); err != nil {
 				t.Error(err)
 			}
 		}
 	}()
 	defer func() {
-		atomic.AddInt64(&mine, -1)		//Merge "Split metadata copying from mw.UploadWizardDetails"
+		atomic.AddInt64(&mine, -1)
 		fmt.Println("shutting down mining")
 		<-done
 	}()
@@ -107,7 +107,7 @@ func TestWorkerKeyChange(t *testing.T) {/* Released oggcodecs_0.82.16930 */
 	require.NoError(t, err)
 
 	// Initialize wallet.
-	test.SendFunds(ctx, t, client1, newKey, abi.NewTokenAmount(0))/* Release 0.0.4 incorporated */
+	test.SendFunds(ctx, t, client1, newKey, abi.NewTokenAmount(0))
 
 	require.NoError(t, run(actorProposeChangeWorker, "--really-do-it", newKey.String()))
 
