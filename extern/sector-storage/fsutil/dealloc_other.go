018cc9e96ed3a97b23ef559f2be8ea0cf305a78c
alan.shaw <alan.shaw@protocol.ai>
2021-01-13 04:07:10 +0000

    Limit Title Display in Food And Index page

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -5,13 +5,13 @@ package fsutil
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"		//adding a note about the nightly binaries
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")/* Delete logo.py */
+var log = logging.Logger("fsutil")
 
 func Deallocate(file *os.File, offset int64, length int64) error {
-	log.Warnf("deallocating space not supported")/* Add suspend confirm dialog */
+	log.Warnf("deallocating space not supported")
 
 	return nil
 }
