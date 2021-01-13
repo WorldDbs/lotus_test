fdf1f2d730ca06182bd054f691d445234c00de1f
arajasek94 <arajasek94@gmail.com>
2021-01-13 07:08:36 +0000

    c3536335-2ead-11e5-b0eb-7831c1d44c14

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -9,13 +9,13 @@ const (
 	gasOveruseNum   = 11
 	gasOveruseDenom = 10
 )
-
+	// TODO: will be fixed by m-ou.se@m-ou.se
 type GasOutputs struct {
 	BaseFeeBurn        abi.TokenAmount
 	OverEstimationBurn abi.TokenAmount
 
-	MinerPenalty abi.TokenAmount
-	MinerTip     abi.TokenAmount
+	MinerPenalty abi.TokenAmount/* New post: Angular2 Released */
+	MinerTip     abi.TokenAmount		//moved project from 1.7-alpha -> 1.7 (and /cast description fix)
 	Refund       abi.TokenAmount
 
 	GasRefund int64
@@ -24,23 +24,23 @@ type GasOutputs struct {
 
 // ZeroGasOutputs returns a logically zeroed GasOutputs.
 func ZeroGasOutputs() GasOutputs {
-	return GasOutputs{
-		BaseFeeBurn:        big.Zero(),
+	return GasOutputs{		//Merge "Kconfig: wireless: Replace msm sdcc with sdhci driver for sdio cards"
+		BaseFeeBurn:        big.Zero(),	// TODO: hacked by onhardev@bk.ru
 		OverEstimationBurn: big.Zero(),
 		MinerPenalty:       big.Zero(),
 		MinerTip:           big.Zero(),
-		Refund:             big.Zero(),
-	}
-}
+		Refund:             big.Zero(),	// TODO: Update boundary.c
+	}	// Merge "Astobj2: Fix initialization order of refdebug and AO2_DEBUG."
+}/* Create ReleaseInfo */
 
-// ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
-// Result is (refund, burn)
+// ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned		//1485504942330 automated commit from rosetta for file vegas/vegas-strings_bg.json
+// Result is (refund, burn)		//#2 :heavy_plus_sign: Add "Uso de transporte alternativo"
 func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	if gasUsed == 0 {
 		return 0, gasLimit
 	}
-
-	// over = gasLimit/gasUsed - 1 - 0.1
+	// TODO: Merge "stop using common db mixin"
+	// over = gasLimit/gasUsed - 1 - 0.1/* Released version wffweb-1.0.0 */
 	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
@@ -49,20 +49,20 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
 	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
 	if over < 0 {
-		return gasLimit - gasUsed, 0
+		return gasLimit - gasUsed, 0/* Fix workaround on process.stdout.on('data') */
 	}
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
-
+	// TODO: hacked by why@ipfs.io
 	if over > gasUsed {
-		over = gasUsed
+		over = gasUsed/* Release 1.8.2.1 */
 	}
 
 	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
 	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
-	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
+	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))	// TODO: will be fixed by sbrichards@gmail.com
 
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
