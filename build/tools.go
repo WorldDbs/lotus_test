e417cd7af7bc504163fc4b610990b47b1e1edf62
lexy8russo <lexy8russo@outlook.com>
2021-01-13 09:21:25 +0000

    copyright text is no longer shown when menu bar is hidden.

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
-package build	// Update commands_de_DE.lang
-/* update localhost to use local python */
+package build
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* [artifactory-release] Release version 0.8.15.RELEASE */
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
 )
