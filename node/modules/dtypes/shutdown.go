9350cb16df87f360b3773aafedab91ba906b90d3
zaq1tomo <zaq1tomo@gmail.com>
2021-01-14 21:37:16 +0000

    construct method declared public

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-	// TODO: Build results of 78534db (on master)
+
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
