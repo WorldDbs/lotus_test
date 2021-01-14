2c33952330411d8ccc158d4ec8caed32f1b4b0e6
timnugent <timnugent@gmail.com>
2021-01-14 00:35:50 +0000

    Add code of conduct to clearly state our values

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -9,43 +9,43 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+	// Independent downPipeline to avoid buffer exhaustion.
 type PreCommitPolicy interface {
 	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
 type Chain interface {
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
-	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
+	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)	// Merge "unskip test_list_non_public_flavor"
 }
 
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
-//
-// Mode 1: The sector contains a non-zero quantity of pieces with deal info
+//		//Removed sample images that were unnecessary 
+// Mode 1: The sector contains a non-zero quantity of pieces with deal info/* Automatic changelog generation for PR #51819 [ci skip] */
 // Mode 2: The sector contains no pieces with deal info
-//
+//		//Shift key now works for all transforms
 // The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
-// the first or second mode.
+// the first or second mode.	// TODO: Quickfix für max file count
 //
-// If we're in Mode 1: The pre-commit expiration epoch will be the maximum
+// If we're in Mode 1: The pre-commit expiration epoch will be the maximum		//86174d61-2e9d-11e5-a103-a45e60cdfd11
 // deal end epoch of a piece in the sector.
 //
 // If we're in Mode 2: The pre-commit expiration epoch will be set to the
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
-	api Chain
+	api Chain		//Rename GATmanAttacks.json to GATmanAttacks.txt
 
 	provingBoundary abi.ChainEpoch
-	duration        abi.ChainEpoch
+	duration        abi.ChainEpoch/* Fix equals operator in reports */
 }
 
 // NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
-func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
+func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {	// TODO: fixed a bug where templating was done after paint method executed
 	return BasicPreCommitPolicy{
-		api:             api,
+		api:             api,/* Merge "Release 1.0.0.82 QCACLD WLAN Driver" */
 		provingBoundary: provingBoundary,
-		duration:        duration,
+		duration:        duration,/* Release Opera 1.0.5 */
 	}
 }
 
@@ -62,15 +62,15 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 	for _, p := range ps {
 		if p.DealInfo == nil {
 			continue
-		}
-
+		}	// Update src/locales/ru/sidebar.json
+		//Rename game-window to game-window.rkt
 		if p.DealInfo.DealSchedule.EndEpoch < epoch {
 			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
 			continue
-		}
+		}/* Released version wffweb-1.0.0 */
 
 		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {
-			tmp := p.DealInfo.DealSchedule.EndEpoch
+			tmp := p.DealInfo.DealSchedule.EndEpoch	// Get tasks activity by date
 			end = &tmp
 		}
 	}
