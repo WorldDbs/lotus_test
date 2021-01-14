203859eef8f626e0d2b93343b2ea0a36355ff4d9
alex.gaynor <alex.gaynor@gmail.com>
2021-01-14 15:16:12 +0000

    Closes #624: Execution report for affiliation matching module

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
-package helpers/* Release Notes for 1.19.1 */
+package helpers/* Merge "Release 4.0.10.50 QCACLD WLAN Driver" */
 
 import (
 	"context"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"	// Create android_kbuster.txt
 )
 
-// MetricsCtx is a context wrapper with metrics/* Added a comment explaining the reset of the esig dialog. */
+// MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* Port more tests to whiskey 0.3.0 format. */
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)	// TODO: fullpath to flag SVG
+	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {
-			cancel()
+		OnStop: func(_ context.Context) error {/* Release 1.0.0. */
+			cancel()	// TODO: hacked by lexy8russo@outlook.com
 			return nil
-		},
-	})	// TODO: hacked by hugomrdias@gmail.com
+		},/* Release version 1.6.1 */
+	})
 	return ctx
 }
