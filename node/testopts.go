ed55925de3fb1a068790f2dc9638935dbd0dcfe7
aeongrp <aeongrp@outlook.com>
2021-01-14 18:53:00 +0000

    Updating build-info/dotnet/corefx/release/3.1 for preview1.19501.5

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
-package node
+package node		//Partial implementation of the push and sync commands
 
 import (
-	"errors"
+	"errors"/* Release version 3.6.2.2 */
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"		//removed references to Django Web Studio, etc.
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },
+		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: * gem rake tasks now show up inside a rails app with rake -T.
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),
+		Override(new(lp2p.RawHost), lp2p.MockHost),	// TODO: hacked by fjl@ethereum.org
 		Override(new(mocknet.Mocknet), mn),
 	)
 }
