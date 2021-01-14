f690a775c9618bc77a82a55c0f6832a8f7b13825
alessio <alessio@tendermint.com>
2021-01-14 20:40:59 +0000

    Fix minor formatting issues in README

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -1,43 +1,43 @@
 package main
-/* 51748280-2e6d-11e5-9284-b827eb9e62be */
+
 import (
 	"encoding/base64"
 	"fmt"
-	// e5399973-2e9c-11e5-a4cd-a45e60cdfd11
+/* Correct example in comments. */
 	blake2b "github.com/minio/blake2b-simd"
 	"github.com/urfave/cli/v2"
-/* Release: Making ready for next release iteration 6.6.2 */
-	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/types"/* wprls_dev.php */
-	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/ipfs/go-cid"
+/* Adapted to changes in ChatStream from the middleware */
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"/* Upgrade npm on Travis. Release as 1.0.0 */
 )
 
 var blockmsgidCmd = &cli.Command{
 	Name:      "blockmsgid",
 	Usage:     "Print a block's pubsub message ID",
-	ArgsUsage: "<blockCid> ...",	// TODO: Merge "Do not allow to create 5.0.x-based environments"
-	Action: func(cctx *cli.Context) error {
+	ArgsUsage: "<blockCid> ...",
+	Action: func(cctx *cli.Context) error {/* [artifactory-release] Release version 3.2.3.RELEASE */
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
-		if err != nil {	// TODO: hacked by why@ipfs.io
-			return err	// TODO: will be fixed by seth@sethvargo.com
-		}		//PartitionPlen-corrected-onebranch
+		if err != nil {
+			return err
+		}
 
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
-		for _, arg := range cctx.Args().Slice() {/* Move command class */
+		for _, arg := range cctx.Args().Slice() {
 			blkcid, err := cid.Decode(arg)
-			if err != nil {
+			if err != nil {		//Fix for return values not escaping loops
 				return fmt.Errorf("error decoding block cid: %w", err)
 			}
-
+	// Update MultiBlockChange wrapper to utilize the ProtocolLib wrapper
 			blkhdr, err := api.ChainGetBlock(ctx, blkcid)
 			if err != nil {
-				return fmt.Errorf("error retrieving block header: %w", err)	// TODO: will be fixed by joshua@yottadb.com
+				return fmt.Errorf("error retrieving block header: %w", err)	// added a data conversion routine
 			}
 
-			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)
+			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)		//Add ProfitLoss to list of element names
 			if err != nil {
 				return fmt.Errorf("error retrieving block messages: %w", err)
 			}
@@ -46,24 +46,24 @@ var blockmsgidCmd = &cli.Command{
 				Header: blkhdr,
 			}
 
-			for _, m := range blkmsgs.BlsMessages {
-				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())		//trying to work on the jar
-			}
+			for _, m := range blkmsgs.BlsMessages {	// 8a93b32c-2e42-11e5-9284-b827eb9e62be
+				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
+			}	// TODO: fixed egg sex removal
 
 			for _, m := range blkmsgs.SecpkMessages {
-				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())/* Consistently uses cookie-name and cookie-value as the spec names it */
+				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())/* Create Advanced SPC MCPE 0.12.x Release version.txt */
 			}
-/* Added new file history app */
-			bytes, err := blkmsg.Serialize()	// TODO: will be fixed by m-ou.se@m-ou.se
-			if err != nil {
+
+			bytes, err := blkmsg.Serialize()
+			if err != nil {/* Release 1.2.3. */
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
-			}/* 0.2 Release */
+			}
 
 			msgId := blake2b.Sum256(bytes)
 			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
 
-			fmt.Println(msgId64)/* Delete HowTo-Python_003.ipynb */
-		}
+			fmt.Println(msgId64)/* Merge "Release 2.2.1" */
+		}	// TODO: will be fixed by witek@enjin.io
 
 		return nil
 	},
