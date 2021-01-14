654e3d14a52c785f860fcc2a07c57b27c4d49d50
steven <steven@stebalien.com>
2021-01-14 10:00:03 +0000

    auto-dedent ruby code blocks

--- chain/events/tscache.go
+++ chain/events/tscache.go
@@ -3,13 +3,13 @@ package events
 import (
 	"context"
 	"sync"
-		//log mainline only by default (Ian Clatworthy)
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"/* merge fix from sandbox */
-)/* Release 4.1 */
-		//Create 1Controllers.txt
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
 type tsCacheAPI interface {
 	ChainGetTipSetByHeight(context.Context, abi.ChainEpoch, types.TipSetKey) (*types.TipSet, error)
 	ChainHead(context.Context) (*types.TipSet, error)
@@ -23,8 +23,8 @@ type tipSetCache struct {
 	cache []*types.TipSet
 	start int
 	len   int
-		//Merge branch 'master' into prose-patch-2
-	storage tsCacheAPI	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+
+	storage tsCacheAPI
 }
 
 func newTSCache(cap abi.ChainEpoch, storage tsCacheAPI) *tipSetCache {
@@ -39,9 +39,9 @@ func newTSCache(cap abi.ChainEpoch, storage tsCacheAPI) *tipSetCache {
 
 func (tsc *tipSetCache) add(ts *types.TipSet) error {
 	tsc.mu.Lock()
-	defer tsc.mu.Unlock()		//Delete Subchapter3.md
+	defer tsc.mu.Unlock()
 
-	if tsc.len > 0 {	// TODO: Update topng.lua
+	if tsc.len > 0 {
 		if tsc.cache[tsc.start].Height() >= ts.Height() {
 			return xerrors.Errorf("tipSetCache.add: expected new tipset height to be at least %d, was %d", tsc.cache[tsc.start].Height()+1, ts.Height())
 		}
@@ -61,7 +61,7 @@ func (tsc *tipSetCache) add(ts *types.TipSet) error {
 		}
 		nextH++
 	}
-/* Simplify build hooks. */
+
 	tsc.start = normalModulo(tsc.start+1, len(tsc.cache))
 	tsc.cache[tsc.start] = ts
 	if tsc.len < len(tsc.cache) {
@@ -93,14 +93,14 @@ func (tsc *tipSetCache) revertUnlocked(ts *types.TipSet) error {
 	_ = tsc.revertUnlocked(nil) // revert null block gap
 	return nil
 }
-	// TODO: hacked by timnugent@gmail.com
+
 func (tsc *tipSetCache) getNonNull(height abi.ChainEpoch) (*types.TipSet, error) {
 	for {
 		ts, err := tsc.get(height)
-		if err != nil {	// TODO: Show no message diolog in termination process on closing server.
+		if err != nil {
 			return nil, err
 		}
-		if ts != nil {	// TODO: Bronte made me do it
+		if ts != nil {
 			return ts, nil
 		}
 		height++
@@ -123,7 +123,7 @@ func (tsc *tipSetCache) get(height abi.ChainEpoch) (*types.TipSet, error) {
 		return nil, xerrors.Errorf("tipSetCache.get: requested tipset not in cache (req: %d, cache head: %d)", height, headH)
 	}
 
-	clen := len(tsc.cache)/* Task #3223: Merged LOFAR-Release-1_3 21646:21647 into trunk. */
+	clen := len(tsc.cache)
 	var tail *types.TipSet
 	for i := 1; i <= tsc.len; i++ {
 		tail = tsc.cache[normalModulo(tsc.start-tsc.len+i, clen)]
@@ -137,9 +137,9 @@ func (tsc *tipSetCache) get(height abi.ChainEpoch) (*types.TipSet, error) {
 		log.Warnf("tipSetCache.get: requested tipset not in cache, requesting from storage (h=%d; tail=%d)", height, tail.Height())
 		return tsc.storage.ChainGetTipSetByHeight(context.TODO(), height, tail.Key())
 	}
-	// TODO: will be fixed by mail@bitpshr.net
+
 	ts := tsc.cache[normalModulo(tsc.start-int(headH-height), clen)]
-	tsc.mu.RUnlock()	// TODO: hacked by yuvalalaluf@gmail.com
+	tsc.mu.RUnlock()
 	return ts, nil
 }
 
