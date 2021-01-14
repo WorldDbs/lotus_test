fd189060ab1d69affbe7f93ced06dc170d4c3128
antao2002 <antao2002@gmail.com>
2021-01-14 13:06:51 +0000

    Added a lot of documentation

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
-package ffiwrapper/* [artifactory-release] Release version v2.0.5.RELEASE */
+package ffiwrapper
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)/* Release source code under the MIT license */
-
+)	// Added Swift versions to README
+/* Delete php5.6-manifest.jps */
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
-	sectors  SectorProvider
+	sectors  SectorProvider	// TODO: 92f05cfe-2e3e-11e5-9284-b827eb9e62be
 	stopping chan struct{}
 }
-
+		//Adding DNF required info
 func (sb *Sealer) Stop() {
-	close(sb.stopping)
+	close(sb.stopping)/* [91] Remove legacy helper method */
 }
