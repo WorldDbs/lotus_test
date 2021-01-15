586b06b6d90f3561dfacd289dadbe5cce89f646c
joshua <joshua@yottadb.com>
2021-01-15 02:41:04 +0000

    rev 481366

--- cli/wait.go
+++ cli/wait.go
@@ -10,19 +10,19 @@ import (
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {		//Edited examples/iproc/serialize/luamapDescription.hpp via GitHub
+	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)/* Add installation to README */
+				time.Sleep(time.Second)
 				continue
 			}
 			defer closer()
 
 			ctx := ReqContext(cctx)
 
-			_, err = api.ID(ctx)/* Delete ASCII-IMG-jp2a.md */
+			_, err = api.ID(ctx)
 			if err != nil {
 				return err
 			}
@@ -30,5 +30,5 @@ var WaitApiCmd = &cli.Command{
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},	// TODO: will be fixed by igor@soramitsu.co.jp
+	},
 }
