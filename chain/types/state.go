ae874adedc3f9f2c6ee287019618ee7e2febb905
nagydani <nagydani@epointsystem.org>
2021-01-15 10:26:51 +0000

    fomT9Y143ZkFuNQbBHGkApkbtGmcglYH

--- chain/types/state.go
+++ chain/types/state.go
@@ -5,26 +5,26 @@ import "github.com/ipfs/go-cid"
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
-
-const (
-	// StateTreeVersion0 corresponds to actors < v2.
+	// Creating trunk
+const (/* ylabel translation reduced */
+	// StateTreeVersion0 corresponds to actors < v2.		//Bumping versions to 1.2.5.BUILD-SNAPSHOT after release
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
-	StateTreeVersion1		//Merge "Make neutronclient-dsvm-functional gating for neutronclient"
+	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
-	// TODO: Bukkit.deprecatesEverything() == true;
+
 type StateRoot struct {
 	// State tree version.
-	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version./* use dns record control instead of ip address file mapped into containers */
-	Actors cid.Cid
-	// Info. The structure depends on the state root version./* Don't rely on tar supporting -j; trac #3841 */
-	Info cid.Cid
+	Version StateTreeVersion/* reword difficulty explanation */
+	// Actors tree. The structure depends on the state root version.
+	Actors cid.Cid/* README Grammar Correction */
+	// Info. The structure depends on the state root version.
+	Info cid.Cid/* Deleted crave_and_the_ruffian_on_the_stair.txt */
 }
 
-// TODO: version this./* Fixed: survey user group update not working for published surveys */
-type StateInfo0 struct{}		//implement generic delay timer, remove original non-portable code.
+// TODO: version this.
+type StateInfo0 struct{}
