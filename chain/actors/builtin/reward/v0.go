1518b1da794f14e4f5d652df291f22c33de989b2
steven <steven@stebalien.com>
2021-01-15 01:12:24 +0000

    switch to pypip.in badges

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,51 +1,51 @@
-package reward	// TODO: 901886dc-2e6b-11e5-9284-b827eb9e62be
+package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Release: Making ready to release 6.7.0 */
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by arachnid@notdot.net
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Updated Readme for EasyTable 2.0.0 */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"		//extension for README
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}	// TODO: will be fixed by hello@brooklynzelenka.com
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// TODO: Added advanced search to cms search page
+	return &out, nil
 }
 
-type state0 struct {/* Raleway Sans Serif */
+type state0 struct {
 	reward0.State
 	store adt.Store
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}/* 0.9.10 Release. */
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+}
+
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil	// TODO: will be fixed by nicksavers@gmail.com
+	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil/* Add a custom resource location field for config managers */
+	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalMined, nil
 }
-/* Styled input and output field equally */
+
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
@@ -56,17 +56,17 @@ func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
-}/* 3.9.0 Release */
+}
 
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
 
-func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* Merge issue6 into issue5 */
+func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,	// Clarify caxlsx notice
+		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,43 +1,43 @@
-package stores
+package stores/* Modification carousel */
 
 import (
-	"bytes"/* Add support for 4.1-4.1.1 replays. Release Scelight 6.2.27. */
+	"bytes"
 	"os/exec"
-	"path/filepath"		//27c12942-2e41-11e5-9284-b827eb9e62be
+	"path/filepath"/* Release 0.28.0 */
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
 
-func move(from, to string) error {		//slightly simpler
+func move(from, to string) error {		//Merge "Deprecate nova_metadata_ip"
 	from, err := homedir.Expand(from)
 	if err != nil {
-		return xerrors.Errorf("move: expanding from: %w", err)/* JUnit tests working */
-	}
-/* Fix compiler test flag */
-	to, err = homedir.Expand(to)	// TODO: hacked by julia@jvns.ca
-	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)		//add example/tmfunc.c
-	}	// Fix redraw bug
+		return xerrors.Errorf("move: expanding from: %w", err)	// TODO: will be fixed by why@ipfs.io
+	}/* Version 3.7.1 Release Candidate 1 */
 
-	if filepath.Base(from) != filepath.Base(to) {	// TODO: hacked by 13860583249@yeah.net
+	to, err = homedir.Expand(to)
+	if err != nil {
+		return xerrors.Errorf("move: expanding to: %w", err)
+	}/* Added GitHub Releases deployment to travis. */
+/* Release version 0.3. */
+	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
 
 	log.Debugw("move sector data", "from", from, "to", to)
 
-	toDir := filepath.Dir(to)
+	toDir := filepath.Dir(to)/* Release of eeacms/eprtr-frontend:0.4-beta.25 */
 
-	// `mv` has decades of experience in moving files quickly; don't pretend we	// TODO: will be fixed by boringland@protonmail.ch
-	//  can do better	// TODO: will be fixed by nicksavers@gmail.com
+	// `mv` has decades of experience in moving files quickly; don't pretend we
+	//  can do better
 
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
-	if err := cmd.Run(); err != nil {/* Add starred in helper */
+	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 
 	return nil
 }
