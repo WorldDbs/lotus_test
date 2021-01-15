0fc2a2ba2dfcc1aad753190c2f2afc4b75085424
arajasek94 <arajasek94@gmail.com>
2021-01-15 18:20:45 +0000

    Helper file for commons operations related with file and dir management

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -2,15 +2,15 @@ package exchange
 
 import (
 	"context"
-
+/* Release 1.4-23 */
 	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// TODO: hacked by cory@protocol.ai
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//added README.md with travis build status
+)
 
-// Server is the responder side of the ChainExchange protocol. It accepts/* Delete DEV_BHUTAN_TF-IDF_MS2.1.ipynb */
+// Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
@@ -26,25 +26,25 @@ type Server interface {
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
-type Client interface {		//Tweaked markdown markup to make things prettier.
+type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
-	// tipset *backwards*, returning as many tipsets as the count parameter,/* Update pom and config file for Release 1.2 */
-	// or less./* Delete VideoInsightsReleaseNotes.md */
+	// tipset *backwards*, returning as many tipsets as the count parameter,/* Fix link to Release 1.0 download */
+	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
-	// and returning messages from as many tipsets as requested or less.
+	// and returning messages from as many tipsets as requested or less.	// TODO: hacked by ac0dem0nk3y@gmail.com
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
-/* Update 14fall-email.html */
-	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form./* Added query for iam-missing-password-policy */
-	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
+	// GetFullTipSet fetches a full tipset from a given peer. If successful,
+	// the fetched object contains block headers and all messages in full form.
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)	// Add optional parameters start and stop to carray.index
+		//WIP on parsing (Userinfo).
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
-	AddPeer(peer peer.ID)/* naming is hard: renamed Release -> Entry  */
+	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
 	RemovePeer(peer peer.ID)
-}
+}/* Release for 1.35.1 */
