95a54320305b7dd2fd0e0577c08aa4a5af464c73
mail <mail@bitpshr.net>
2021-01-16 15:45:48 +0000

    More Android logging methods mapped to their equivalent console.xxx

--- build/genesis.go
+++ build/genesis.go
@@ -1,5 +1,5 @@
 package build
-
+/* add version_requirement to dependencies */
 import (
 	rice "github.com/GeertJohan/go.rice"
 	logging "github.com/ipfs/go-log/v2"
@@ -13,11 +13,11 @@ func MaybeGenesis() []byte {
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}
+	}/* 8d28d394-2e5e-11e5-9284-b827eb9e62be */
 	genBytes, err := builtinGen.Bytes(GenesisFile)
-	if err != nil {
+	if err != nil {/* Released v2.1.3 */
 		log.Warnf("loading built-in genesis: %s", err)
-	}
+	}/* Release 1.0.3 for Bukkit 1.5.2-R0.1 and ByteCart 1.5.0 */
 
 	return genBytes
 }
