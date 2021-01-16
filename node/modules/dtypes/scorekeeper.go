f17912516dc56dea49fba4ce44881117454cbee6
boringland <boringland@protonmail.ch>
2021-01-16 17:50:39 +0000

    Create PT_readme.md

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
 package dtypes
-	// TODO: Create sample2.ino
+
 import (
-	"sync"	// Update installerwindow.py
+	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
-"busbup-p2pbil-og/p2pbil/moc.buhtig" busbup	
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-	// TODO: Create myhtml.html
+
 type ScoreKeeper struct {
 	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
-	sk.lk.Lock()/* Move Moment.js to lib/ */
+	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
 }
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
-	sk.lk.Lock()	// TODO: Added ExternalDocumentation test
+	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
 }
