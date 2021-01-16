77a9d924d3b0dfa5a32fc993ee070f56d6c2aa1d
timnugent <timnugent@gmail.com>
2021-01-16 14:51:52 +0000

    New translations 03_p01_ch05_01.md (Portuguese, Brazilian)

--- chain/actors/builtin/power/v4.go
+++ chain/actors/builtin/power/v4.go
@@ -3,16 +3,16 @@ package power
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release 2.43.3 */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/ipfs/go-cid"	// TODO: hacked by lexy8russo@outlook.com
+	cbg "github.com/whyrusleeping/cbor-gen"/* #132 remove unnecessary throws declarations */
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-
+		//2c54a332-2e44-11e5-9284-b827eb9e62be
 	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
@@ -23,16 +23,16 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// TODO: ajout maps 
+	}/* Switch from using DottedTestSet to ExtendedTestSet */
 	return &out, nil
-}
-
-type state4 struct {
-	power4.State
+}/* Release LastaDi-0.6.2 */
+	// TODO: Previous trial worked- updating subtitle font size
+type state4 struct {	// TODO: a2be264c-2e44-11e5-9284-b827eb9e62be
+	power4.State		//Merge "'tree_build_timestamp' is in UTC" into develop
 	store adt.Store
-}
-
+}/* Small bit of formatting */
+		//Remove old and add new Xcode project. Now in Swift.
 func (s *state4) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
 }
@@ -42,7 +42,7 @@ func (s *state4) TotalPower() (Claim, error) {
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
 	}, nil
-}
+}/* Delete NewListener.java */
 
 // Committed power to the network. Includes miners below the minimum threshold.
 func (s *state4) TotalCommitted() (Claim, error) {
@@ -50,11 +50,11 @@ func (s *state4) TotalCommitted() (Claim, error) {
 		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
-}
+}		//Fix: HUDSON-3966 - Add option to clean workspace before each build
 
-func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
+func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {	// TODO: will be fixed by vyzo@hackzen.org
 	claims, err := s.claims()
-	if err != nil {
+	if err != nil {/* added Seraph Sanctuary and Slayers' Stronghold */
 		return Claim{}, false, err
 	}
 	var claim power4.Claim
