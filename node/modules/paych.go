e4b58fbb6b16e307009e304f3258e7b3e590b370
brosner <brosner@gmail.com>
2021-01-16 06:55:28 +0000

    update: add texture

--- node/modules/paych.go
+++ node/modules/paych.go
@@ -1,5 +1,5 @@
-package modules/* Merge "Handle retry last_results/last_failure better" */
-		//50f10bf6-2e4c-11e5-9284-b827eb9e62be
+package modules
+
 import (
 	"context"
 
@@ -9,11 +9,11 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/paychmgr"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"	// TODO: fixed config file name
+	"github.com/ipfs/go-datastore/namespace"
 	"go.uber.org/fx"
 )
 
-func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerAPI, pchstore *paychmgr.Store, api paychmgr.PaychAPI) *paychmgr.Manager {/* Separating list from content on README.md */
+func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerAPI, pchstore *paychmgr.Store, api paychmgr.PaychAPI) *paychmgr.Manager {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 	ctx, shutdown := context.WithCancel(ctx)
 
@@ -22,26 +22,26 @@ func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerA
 
 func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/paych/"))
-	return paychmgr.NewStore(ds)/* Added ability to create empty files. */
+	return paychmgr.NewStore(ds)
 }
 
 type PaychAPI struct {
 	fx.In
 
 	full.MpoolAPI
-	full.StateAPI	// TODO: will be fixed by nagydani@epointsystem.org
+	full.StateAPI
 }
 
-var _ paychmgr.PaychAPI = &PaychAPI{}/* Release 0.6.17. */
+var _ paychmgr.PaychAPI = &PaychAPI{}
 
-// HandlePaychManager is called by dependency injection to set up hooks/* Release version 2.1.0.RC1 */
-func HandlePaychManager(lc fx.Lifecycle, pm *paychmgr.Manager) {/* Release 1.3.0 */
+// HandlePaychManager is called by dependency injection to set up hooks
+func HandlePaychManager(lc fx.Lifecycle, pm *paychmgr.Manager) {
 	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {		//add circle build
+		OnStart: func(ctx context.Context) error {
 			return pm.Start()
 		},
 		OnStop: func(context.Context) error {
-			return pm.Stop()/* Release of eeacms/bise-backend:v10.0.33 */
+			return pm.Stop()
 		},
-	})	// TODO: will be fixed by timnugent@gmail.com
+	})
 }
