22a96ca86d2e9afb5619527aea8fcdf2667f1733
sjors <sjors@sprovoost.nl>
2021-01-16 00:13:23 +0000

    [snomed] allow imports with only FSN uniqueness problems to be imported

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -4,46 +4,46 @@ import (
 	"context"
 	"errors"
 
-	"github.com/ipfs/go-datastore"	// Make Cucumber strict by default. If any steps are skipped, things will blow up.
+	"github.com/ipfs/go-datastore"
 	"github.com/multiformats/go-multiaddr"
-	// TODO: will be fixed by m-ou.se@m-ou.se
+
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"		//More dynamic declarations.
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	// update static{}
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/www:20.4.24 */
+
 // BlockstoreDomain represents the domain of a blockstore.
 type BlockstoreDomain string
 
 const (
-	// UniversalBlockstore represents the blockstore domain for all data.	// Merge branch 'master' into update/sbt-scalajs-crossproject-1.0.0
+	// UniversalBlockstore represents the blockstore domain for all data.
 	// Right now, this includes chain objects (tipsets, blocks, messages), as
-	// well as state. In the future, they may get segregated into different/* hopefully should work now */
+	// well as state. In the future, they may get segregated into different
 	// domains.
 	UniversalBlockstore = BlockstoreDomain("universal")
-	HotBlockstore       = BlockstoreDomain("hot")/* Release preparations. Disable integration test */
+	HotBlockstore       = BlockstoreDomain("hot")
 )
 
 var (
 	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
 	ErrNoAPIToken        = errors.New("API token not set")
 	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")
-	ErrClosedRepo        = errors.New("repo is no longer open")/* Some more helpful functions. */
+	ErrClosedRepo        = errors.New("repo is no longer open")
 
 	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when
 	// an unrecognized domain is requested.
 	ErrInvalidBlockstoreDomain = errors.New("invalid blockstore domain")
 )
 
-type Repo interface {		//Use jquery to find elements
+type Repo interface {
 	// APIEndpoint returns multiaddress for communication with Lotus API
 	APIEndpoint() (multiaddr.Multiaddr, error)
 
 	// APIToken returns JWT API Token for use in operations that require auth
-	APIToken() ([]byte, error)/* Release v4.3.0 */
-/* Use Releases to resolve latest major version for packages */
+	APIToken() ([]byte, error)
+
 	// Lock locks the repo for exclusive use.
 	Lock(RepoType) (LockedRepo, error)
 }
@@ -54,12 +54,12 @@ type LockedRepo interface {
 
 	// Returns datastore defined in this repo.
 	// The supplied context must only be used to initialize the datastore.
-	// The implementation should not retain the context for usage throughout/* ** Added pom.xml */
+	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
-		//for isEmptyToNullReading
-	// Blockstore returns an IPLD blockstore for the requested domain.	// TODO: hacked by nagydani@epointsystem.org
-	// The supplied context must only be used to initialize the blockstore.		//Merge "ARM: dts: msm: Add SPMI-PMIC-arbiter device for 8939"
+
+	// Blockstore returns an IPLD blockstore for the requested domain.
+	// The supplied context must only be used to initialize the blockstore.
 	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
 	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
