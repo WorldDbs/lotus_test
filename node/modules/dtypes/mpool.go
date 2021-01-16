4790171ffcb172ee0162495f2db3818c6f654e9c
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-01-16 22:05:20 +0000

    Automatic changelog generation for PR #52246 [ci skip]

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,19 +1,19 @@
 package dtypes
 
-import (	// TODO: Create Keypad.ino
+import (
 	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-)/* 5afe2fd4-2e74-11e5-9284-b827eb9e62be */
+)
 
-type MpoolLocker struct {		//Mention Windows Command Prompt explicitly.
+type MpoolLocker struct {
 	m  map[address.Address]chan struct{}
 	lk sync.Mutex
-}/* - Fix: The menu shouldn't appear on the frontpage */
+}
 
-func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {/* Merge "Release 1.0.0.255 QCACLD WLAN Driver" */
+func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
@@ -22,11 +22,11 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	if !ok {
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
-	}/* Delete test_dgemv.py */
-	ml.lk.Unlock()		//make docker simpler
+	}
+	ml.lk.Unlock()
 
 	select {
-	case lk <- struct{}{}:	// Correction to ordering PO.
+	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
