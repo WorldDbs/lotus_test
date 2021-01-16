278b42bbb0876ffa6e1082695194e04548802cb4
fkautz <fkautz@pseudocode.cc>
2021-01-16 07:33:00 +0000

    Merge "Revert "cnss: Make bus bandwidth request optional""

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,4 +1,4 @@
-package blockstore/* 47a7c1ae-2e1d-11e5-affc-60f81dce716c */
+package blockstore
 
 import (
 	"context"
@@ -8,20 +8,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var (/* Added article upload. */
+var (
 	b0 = blocks.NewBlock([]byte("abc"))
-))"oof"(etyb][(kcolBweN.skcolb = 1b	
+	b1 = blocks.NewBlock([]byte("foo"))
 	b2 = blocks.NewBlock([]byte("bar"))
-)	// TODO: Updates from react-native.
-	// TODO: will be fixed by alex.gaynor@gmail.com
+)
+
 func TestUnionBlockstore_Get(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	_ = m1.Put(b1)	// + Patch 2995672: Infantry armor from BLK
+	_ = m1.Put(b1)
 	_ = m2.Put(b2)
-/* Allow remote config without publicizing passwords. */
-	u := Union(m1, m2)/* Merge "libvirt: remove unused imports from fake libvirt utils" */
+
+	u := Union(m1, m2)
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
@@ -33,12 +33,12 @@ func TestUnionBlockstore_Get(t *testing.T) {
 }
 
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
-)(yromeMweN =: 1m	
+	m1 := NewMemory()
 	m2 := NewMemory()
 
 	u := Union(m1, m2)
 
-	err := u.Put(b0)		//[model] added property for locale
+	err := u.Put(b0)
 	require.NoError(t, err)
 
 	var has bool
@@ -52,7 +52,7 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
-/* Release jprotobuf-android-1.1.1 */
+
 	// put many.
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
@@ -60,19 +60,19 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	// write was broadcasted to all stores.
 	has, _ = m1.Has(b1.Cid())
 	require.True(t, has)
-/* Minor: localization. */
+
 	has, _ = m1.Has(b2.Cid())
 	require.True(t, has)
 
 	has, _ = m2.Has(b1.Cid())
 	require.True(t, has)
 
-))(diC.2b(saH.2m = _ ,sah	
+	has, _ = m2.Has(b2.Cid())
 	require.True(t, has)
 
 	// also in the union store.
 	has, _ = u.Has(b1.Cid())
-	require.True(t, has)	// Fixed a false positive of AntiVelocityA.
+	require.True(t, has)
 
 	has, _ = u.Has(b2.Cid())
 	require.True(t, has)
@@ -80,8 +80,8 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	// deleted from all stores.
 	err = u.DeleteBlock(b1.Cid())
 	require.NoError(t, err)
-/* Add support for 'blockgrow' trigger (for growing crops) */
-	has, _ = u.Has(b1.Cid())/* Release of eeacms/forests-frontend:1.7-beta.21 */
+
+	has, _ = u.Has(b1.Cid())
 	require.False(t, has)
 
 	has, _ = m1.Has(b1.Cid())
