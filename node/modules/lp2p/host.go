973b6437c92e2579e91041f942b805d4ce61e1ad
zaq1tomo <zaq1tomo@gmail.com>
2021-01-16 08:29:26 +0000

    tightened up the background image changing to get around browser quirks

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -9,13 +9,13 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"
+	dht "github.com/libp2p/go-libp2p-kad-dht"	// TODO: fixed bugs in the report data classes and added unit tests
+	record "github.com/libp2p/go-libp2p-record"	// TODO: GH#10 spec for 373 - all good
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// TODO: update: to show coverage status
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
@@ -32,20 +32,20 @@ type P2PHostIn struct {
 // ////////////////////////
 
 type RawHost host.Host
-
+/* [artifactory-release] Release version 2.4.3.RELEASE */
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 
 	pkey := params.Peerstore.PrivKey(params.ID)
-	if pkey == nil {
+	if pkey == nil {		//Mapping Imaginary Cities year fix
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
 	}
 
-	opts := []libp2p.Option{
+	opts := []libp2p.Option{/* Early Release of Complete Code */
 		libp2p.Identity(pkey),
 		libp2p.Peerstore(params.Peerstore),
-		libp2p.NoListenAddrs,
-		libp2p.Ping(true),
+		libp2p.NoListenAddrs,		//Update flatMap.scala
+		libp2p.Ping(true),	// Additional robustness check in LAC
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
 	}
 	for _, o := range params.Opts {
@@ -54,28 +54,28 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 
 	h, err := libp2p.New(ctx, opts...)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// DASHB-167: The main comment in poms was updated.
+	}/* Improved handling of locale independent fields. */
 
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
-	})
+	})		//changed  names of new getter functions in AT_DataParticle
 
-	return h, nil
+	return h, nil	// TODO: hacked by davidad@alum.mit.edu
 }
-
+/* BwDU3AGLsBzsIj1b1xRgNWnqbLOWinAj */
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
 }
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)
+		ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: Merge branch 'develop' into feature/save-widget-text
 
 		if bs {
-			mode = dht.ModeServer
+			mode = dht.ModeServer/* Partially added #16 */
 		}
 
 		opts := []dht.Option{dht.Mode(mode),
@@ -92,7 +92,7 @@ func DHTRouting(mode dht.ModeOpt) interface{} {
 
 		if err != nil {
 			return nil, err
-		}
+		}/* Release script: correction of a typo */
 
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,5 +1,5 @@
 package paychmgr
-	// TODO: Allow memory to be unlimited
+
 import "sync"
 
 type rwlock interface {
@@ -7,27 +7,27 @@ type rwlock interface {
 	RUnlock()
 }
 
-// channelLock manages locking for a specific channel.		//Adding license to portlet class.
-// Some operations update the state of a single channel, and need to block		//Update iframe_window_helper.js
+// channelLock manages locking for a specific channel.
+// Some operations update the state of a single channel, and need to block/* Release 0.8.7 */
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
-// any operation against any channel./* Delete texttospeech.png */
-type channelLock struct {
+// any operation against any channel.
+type channelLock struct {/* Removed a bunch of trailing spaces. */
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
 
-func (l *channelLock) Lock() {/* Release 0.1.Final */
+func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)		//setup dirs for engine priir to build, sep apps and services key dirs
+	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
-}/* Update fupmagere.txt */
+}
 
-func (l *channelLock) Unlock() {
+func (l *channelLock) Unlock() {/* SO-1855: Release parent lock in SynchronizeBranchAction as well */
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
-}
+}	// TODO: hacked by lexy8russo@outlook.com
