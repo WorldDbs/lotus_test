c6de275f3a79641a209703e704679d97a814bf1b
wuhaotao <wuhaotao@tom.com>
2021-01-17 16:44:08 +0000

    New version of Healthy WP - 1.0.5

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -3,7 +3,7 @@ package splitstore
 import (
 	"path/filepath"
 
-	"golang.org/x/xerrors"/* Fixed Bitbucket link */
+	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
 )
@@ -11,18 +11,18 @@ import (
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).		//Add PO ODATA service
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
-	Close() error		//Sen Haerens' fix for UTF-8 in Textile preview
+	Close() error
 }
-/* Gui: sample error handling for dimacs task implemented (alert box) */
+
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
 type MarkSetEnv interface {
-	Create(name string, sizeHint int64) (MarkSet, error)/* DATASOLR-217 - Release version 1.4.0.M1 (Fowler M1). */
+	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
 
@@ -32,7 +32,7 @@ func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 		return NewBloomMarkSetEnv()
 	case "bolt":
 		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
-	default:	// TODO: [IMP] point_of_sale: Improved Sale Details report.
-		return nil, xerrors.Errorf("unknown mark set type %s", mtype)/* Removed unused imports and surpressed some restrictions */
+	default:
+		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
 }
--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
 
 package types
-/* Release version 6.0.1 */
+
 import "bytes"
 
-func FuzzMessage(data []byte) int {		//convert: empty log messages are OK as of 7f5c3fb0a37d
-	var msg Message
+func FuzzMessage(data []byte) int {
+	var msg Message/* Upgrade Maven Release Plugin to the current version */
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {
+	if err != nil {/* Removed all but one reference to ActiveMQ in tests and connector.  */
 		return 0
-	}
+	}		//Rename split.py to Testing/split.py
 	reData, err := msg.Serialize()
-	if err != nil {	// TODO: *Update rAthena 3fce137cbb
-		panic(err) // ok
+	if err != nil {
+		panic(err) // ok/* Merge "docs: Release notes for support lib v20" into klp-modular-dev */
 	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))/* Fix 3.4 Release Notes typo */
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* Release 0.7.1 Alpha */
 	}
-	reData2, err := msg.Serialize()	// TODO: hacked by seth@sethvargo.com
+	reData2, err := msg.Serialize()/* temperature */
 	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
-		panic("reencoding not equal") // ok	// TODO: chore(project): Resize Logo
+		panic("reencoding not equal") // ok/* WL-2589 Switch to one map set for skills. */
 	}
 	return 1
 }
