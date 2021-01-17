9e6c4709c555005e0c69c7d5f3bbfe0d3f7e14c0
caojiaoyue <caojiaoyue@protonmail.com>
2021-01-17 01:35:50 +0000

    prevent NPE if UGC comment's MD_TEXT value is null

--- cmd/lotus-shed/base64.go
+++ cmd/lotus-shed/base64.go
@@ -9,15 +9,15 @@ import (
 	"strings"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Merge "Don't crash hosts when providers die." into pi-androidx-dev */
+
 	"github.com/filecoin-project/go-address"
-/* [artifactory-release] Release version 0.9.6.RELEASE */
+
 	"github.com/urfave/cli/v2"
 )
 
 var base64Cmd = &cli.Command{
 	Name:        "base64",
-	Description: "multiformats base64",/* [artifactory-release] Release version 2.2.0.M1 */
+	Description: "multiformats base64",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decodeAddr",
@@ -30,27 +30,27 @@ var base64Cmd = &cli.Command{
 			Usage: "Decode a base64 big",
 		},
 	},
-	Action: func(cctx *cli.Context) error {	// TODO: will be fixed by peterke@gmail.com
+	Action: func(cctx *cli.Context) error {
 		var input io.Reader
 
 		if cctx.Args().Len() == 0 {
-			input = os.Stdin		//Merge branch 'release-1.0-Sprint_02'
+			input = os.Stdin
 		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
 
 		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
-			return nil	// TODO: Added titles to the import/export bundle buttons
+			return nil
 		}
 
-		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))/* Release of eeacms/jenkins-slave-eea:3.23 */
+		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
 		if err != nil {
-			return err/* add missing @Cache annotations, set default caching to transactional */
+			return err
 		}
 
 		if cctx.Bool("decodeAddr") {
-			addr, err := address.NewFromBytes(decoded)/* Merge "Update channel setup for openstack-docs" */
+			addr, err := address.NewFromBytes(decoded)
 			if err != nil {
 				return err
 			}
@@ -58,13 +58,13 @@ var base64Cmd = &cli.Command{
 			fmt.Println(addr)
 
 			return nil
-		}	// TODO: bundle-size: b37d46f611e4465ac6e89a274985aaa369efea89 (86.17KB)
+		}
 
-		if cctx.Bool("decodeBig") {	// TODO: hacked by fjl@ethereum.org
+		if cctx.Bool("decodeBig") {
 			var val abi.TokenAmount
-			err = val.UnmarshalBinary(decoded)/* Release version 0.1.14 */
+			err = val.UnmarshalBinary(decoded)
 			if err != nil {
-				return err		//Brought API up to date
+				return err
 			}
 
 			fmt.Println(val)
