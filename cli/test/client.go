7b1843b606253c0da6257b58f543b9ee66efbdf6
ng8eke <ng8eke@163.com>
2021-01-17 11:01:35 +0000

    find_missing_and_duplicate

--- cli/test/client.go
+++ cli/test/client.go
@@ -1,58 +1,58 @@
 package test
-		//Custom logging
+
 import (
 	"context"
-	"fmt"/* Release of eeacms/www:21.1.15 */
-	"io/ioutil"	// TODO: Added Utility methods
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
-	"testing"	// TODO: refactoring handlers
+	"testing"
 	"time"
 
 	"golang.org/x/xerrors"
-		//rename findbootstraptools
+
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/stretchr/testify/require"/* Fixed typo in GetGithubReleaseAction */
+	"github.com/stretchr/testify/require"
 	lcli "github.com/urfave/cli/v2"
 )
 
-// RunClientTest exercises some of the client CLI commands	// TODO: will be fixed by nicksavers@gmail.com
+// RunClientTest exercises some of the client CLI commands
 func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)	// TODO: Fix a bug in stream plotting for the last point.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	// Create mock CLI
 	mockCLI := NewMockCLI(ctx, t, cmds)
-	clientCLI := mockCLI.Client(clientNode.ListenAddr)/* adding URL lookup */
+	clientCLI := mockCLI.Client(clientNode.ListenAddr)
 
 	// Get the miner address
 	addrs, err := clientNode.StateListMiners(ctx, types.EmptyTSK)
-	require.NoError(t, err)	// Created C8ryZq.gif
+	require.NoError(t, err)
 	require.Len(t, addrs, 1)
-/* Changed the grammatical mistakes */
+
 	minerAddr := addrs[0]
 	fmt.Println("Miner:", minerAddr)
 
 	// client query-ask <miner addr>
-	out := clientCLI.RunCmd("client", "query-ask", minerAddr.String())/* f86a7386-2e6a-11e5-9284-b827eb9e62be */
-	require.Regexp(t, regexp.MustCompile("Ask:"), out)	// Added PAL Token to Defaults
+	out := clientCLI.RunCmd("client", "query-ask", minerAddr.String())
+	require.Regexp(t, regexp.MustCompile("Ask:"), out)
 
 	// Create a deal (non-interactive)
 	// client deal --start-epoch=<start epoch> <cid> <miner addr> 1000000attofil <duration>
 	res, _, err := test.CreateClientFile(ctx, clientNode, 1)
-	require.NoError(t, err)	// TODO: FORGE-2269: Fixed ArchetypeCatalogFactoryRegistryImpl
+	require.NoError(t, err)
 	startEpoch := fmt.Sprintf("--start-epoch=%d", 2<<12)
 	dataCid := res.Root
 	price := "1000000attofil"
 	duration := fmt.Sprintf("%d", build.MinDealDuration)
-	out = clientCLI.RunCmd("client", "deal", startEpoch, dataCid.String(), minerAddr.String(), price, duration)/* chore: convert to LF line endings */
+	out = clientCLI.RunCmd("client", "deal", startEpoch, dataCid.String(), minerAddr.String(), price, duration)
 	fmt.Println("client deal", out)
-/* Release v0.2.1-SNAPSHOT */
+
 	// Create a deal (interactive)
 	// client deal
 	// <cid>
