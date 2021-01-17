6dc683df1630e09f3945d19ff5b092d0ab87000a
nagydani <nagydani@epointsystem.org>
2021-01-17 06:51:43 +0000

    Delete Paratii_Icone4_colorido2.png

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,4 +1,4 @@
-package verifreg/* Update HYPImagePicker.m */
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -12,16 +12,16 @@ import (
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* Release Tag V0.10 */
+
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {/* Release of eeacms/www-devel:20.9.9 */
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Switched build path to universal */
+	return &out, nil
 }
 
 type state3 struct {
@@ -30,7 +30,7 @@ type state3 struct {
 }
 
 func (s *state3) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil	// TODO: will be fixed by magik6k@gmail.com
+	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
@@ -50,7 +50,7 @@ func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 }
 
 func (s *state3) verifiedClients() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)/* Work on the deploy package */
+	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
 
 func (s *state3) verifiers() (adt.Map, error) {
