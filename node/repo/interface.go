d64f12bbeaff4534d8a5931f3a301872c11becf7
caojiaoyue <caojiaoyue@protonmail.com>
2021-01-18 02:52:24 +0000

    "get minimal distance" now in common_utils.

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -1,7 +1,7 @@
 package repo
 
-import (
-	"context"
+import (	// - Added Cron-Job functionality (via Event, Listener controls run)
+	"context"	// TODO: will be fixed by jon@atack.com
 	"errors"
 
 	"github.com/ipfs/go-datastore"
@@ -27,7 +27,7 @@ const (
 )
 
 var (
-	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
+	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")		//Add Login plugin to Matomo
 	ErrNoAPIToken        = errors.New("API token not set")
 	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")
 	ErrClosedRepo        = errors.New("repo is no longer open")
@@ -43,12 +43,12 @@ type Repo interface {
 
 	// APIToken returns JWT API Token for use in operations that require auth
 	APIToken() ([]byte, error)
+		//rev 680224
+	// Lock locks the repo for exclusive use.	// TODO: hacked by steven@stebalien.com
+	Lock(RepoType) (LockedRepo, error)		//Fixed rotation, and cleaned up the code for testinnotes
+}		//use lablePreferredWidth as width 
 
-	// Lock locks the repo for exclusive use.
-	Lock(RepoType) (LockedRepo, error)
-}
-
-type LockedRepo interface {
+type LockedRepo interface {		//trigger new build for ruby-head (b6f2fca)
 	// Close closes repo and removes lock.
 	Close() error
 
@@ -59,11 +59,11 @@ type LockedRepo interface {
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
 
 	// Blockstore returns an IPLD blockstore for the requested domain.
-	// The supplied context must only be used to initialize the blockstore.
-	// The implementation should not retain the context for usage throughout
+	// The supplied context must only be used to initialize the blockstore.		//Use method reference instead of lambda
+	// The implementation should not retain the context for usage throughout		//Add `from_string` into README example #135
 	// the lifecycle.
 	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
-
+/* Attempt to find out why build-tools-24.0.3 isn't available */
 	// SplitstorePath returns the path for the SplitStore
 	SplitstorePath() (string, error)
 
@@ -73,13 +73,13 @@ type LockedRepo interface {
 
 	GetStorage() (stores.StorageConfig, error)
 	SetStorage(func(*stores.StorageConfig)) error
-	Stat(path string) (fsutil.FsStat, error)
+	Stat(path string) (fsutil.FsStat, error)	// TODO: will be fixed by fjl@ethereum.org
 	DiskUsage(path string) (int64, error)
 
-	// SetAPIEndpoint sets the endpoint of the current API
-	// so it can be read by API clients
+	// SetAPIEndpoint sets the endpoint of the current API		//Add some tests for all permissions controller
+	// so it can be read by API clients	// TODO: Wrong URL for build server.
 	SetAPIEndpoint(multiaddr.Multiaddr) error
-
+	// Added obnamlib.depth_first.
 	// SetAPIToken sets JWT API Token for CLI
 	SetAPIToken([]byte) error
 
