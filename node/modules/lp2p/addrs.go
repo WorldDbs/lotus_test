d3e88aa3a0ec24089bd83f60a493bb7eec16308a
nagydani <nagydani@epointsystem.org>
2021-01-18 06:47:37 +0000

    Merge "ASoC: msm: qdsp6v2: Fix crash during WFD playback and SSR"

--- node/modules/lp2p/addrs.go
+++ node/modules/lp2p/addrs.go
@@ -1,58 +1,58 @@
-package lp2p/* Release 1.51 */
-/* Release v2.1 */
+package lp2p
+
 import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
-	p2pbhost "github.com/libp2p/go-libp2p/p2p/host/basic"/* 216f64d0-2e4d-11e5-9284-b827eb9e62be */
+	p2pbhost "github.com/libp2p/go-libp2p/p2p/host/basic"
 	mafilter "github.com/libp2p/go-maddr-filter"
 	ma "github.com/multiformats/go-multiaddr"
 	mamask "github.com/whyrusleeping/multiaddr-filter"
 )
 
-func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {/* Initial messages implementation */
+func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		for _, s := range filters {
 			f, err := mamask.NewMask(s)
 			if err != nil {
-				return opts, fmt.Errorf("incorrectly formatted address filter in config: %s", s)	// TODO: Added thread safe build again (eio not installable).
+				return opts, fmt.Errorf("incorrectly formatted address filter in config: %s", s)
 			}
 			opts.Opts = append(opts.Opts, libp2p.FilterAddresses(f)) //nolint:staticcheck
-		}		//Move the Railtie under rails/.
+		}
 		return opts, nil
 	}
 }
-/* modified the locations dialog for setting the block order */
-func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFactory, error) {/* Update Release 0 */
+
+func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFactory, error) {
 	var annAddrs []ma.Multiaddr
 	for _, addr := range announce {
-		maddr, err := ma.NewMultiaddr(addr)	// TODO: will be fixed by cory@protocol.ai
+		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
-			return nil, err/* differentiate artifact names */
-		}/* Debug Coin libraries added to ShapeParabolicRectangle.pro file */
+			return nil, err
+		}
 		annAddrs = append(annAddrs, maddr)
 	}
 
 	filters := mafilter.NewFilters()
-	noAnnAddrs := map[string]bool{}/* Release version 1.4.0.RC1 */
-	for _, addr := range noAnnounce {	// TODO: [CI] changed settings.xml and release.sh to travis-ci deployment
+	noAnnAddrs := map[string]bool{}
+	for _, addr := range noAnnounce {
 		f, err := mamask.NewMask(addr)
 		if err == nil {
 			filters.AddFilter(*f, mafilter.ActionDeny)
 			continue
 		}
 		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {	// TODO: hacked by yuvalalaluf@gmail.com
+		if err != nil {
 			return nil, err
 		}
 		noAnnAddrs[string(maddr.Bytes())] = true
 	}
 
 	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {
-		var addrs []ma.Multiaddr/* Update MitelmanReleaseNotes.rst */
+		var addrs []ma.Multiaddr
 		if len(annAddrs) > 0 {
-			addrs = annAddrs	// TODO: testing parameters of launcher
+			addrs = annAddrs
 		} else {
 			addrs = allAddrs
 		}
