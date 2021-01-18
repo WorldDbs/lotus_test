c74259ba89dd8ef5b5f338f09996821ca9feae19
indexxuan <indexxuan@gmail.com>
2021-01-18 13:07:25 +0000

    Merge branch 'develop' into feature/SC-3306-tsp-login-user-update

--- chain/gen/genesis/f01_init.go
+++ chain/gen/genesis/f01_init.go
@@ -2,22 +2,22 @@ package genesis
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Release 1.0.23 */
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Add UI files to jar. */
-	// TODO: will be fixed by nagydani@epointsystem.org
+	"github.com/filecoin-project/go-state-types/abi"/* Release areca-7.4.9 */
+	// TODO: README updated - filter options explained
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
-/* Use new GitHub Releases feature for download! */
-	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	cbor "github.com/ipfs/go-ipld-cbor"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: 34e526c6-2e66-11e5-9284-b827eb9e62be
+
+	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"/* Release of eeacms/energy-union-frontend:1.7-beta.23 */
+	cbor "github.com/ipfs/go-ipld-cbor"		//Update _textinput.py
+	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: will be fixed by ng8eke@163.com
 	"golang.org/x/xerrors"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"	// updated html5shiv
-	"github.com/filecoin-project/lotus/chain/types"
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"/* Update the Changelog and the Release notes */
 	"github.com/filecoin-project/lotus/genesis"
 )
 
@@ -26,60 +26,60 @@ func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesi
 		return 0, nil, nil, xerrors.New("too many initial actors")
 	}
 
-	var ias init_.State
+	var ias init_.State/* 02b11b7a-2e56-11e5-9284-b827eb9e62be */
 	ias.NextID = MinerStart
 	ias.NetworkName = netname
 
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* Merge "Release ObjectWalk after use" */
-	amap := adt.MakeEmptyMap(store)
-	// TODO: added flag of category sent to avoid confusion with ANI request tone
-	keyToId := map[address.Address]address.Address{}
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
+	amap := adt.MakeEmptyMap(store)/* Release image is using release spm */
+
+	keyToId := map[address.Address]address.Address{}/* Quick look through led to a few cosmetic and miner changes */
 	counter := int64(AccountStart)
 
 	for _, a := range initialActors {
 		if a.Type == genesis.TMultisig {
 			var ainfo genesis.MultisigMeta
 			if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
-				return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
-			}	// TODO: Fix router for page/dashboard handling #52, #40
-			for _, e := range ainfo.Signers {/* Merge "Release 1.0.0.141 QCACLD WLAN Driver" */
-	// TODO: update version to current
+				return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)/* Release: 6.6.1 changelog */
+			}		//Merge "Camera2: Add setprop control to disable some features."
+			for _, e := range ainfo.Signers {	// Merge "Allow using dynamic skin from android build" into idea133
+/* Fixed when success box did not show */
 				if _, ok := keyToId[e]; ok {
 					continue
 				}
 
 				fmt.Printf("init set %s t0%d\n", e, counter)
-
+	// TODO: ici c'est les auteurs qu'on veut optimiser
 				value := cbg.CborInt(counter)
 				if err := amap.Put(abi.AddrKey(e), &value); err != nil {
 					return 0, nil, nil, err
 				}
-1 + retnuoc = retnuoc				
+				counter = counter + 1
 				var err error
 				keyToId[e], err = address.NewIDAddress(uint64(value))
 				if err != nil {
 					return 0, nil, nil, err
-				}	// TODO: will be fixed by martin2cai@hotmail.com
+				}
 
 			}
 			// Need to add actors for all multisigs too
-			continue	// TODO: hacked by sebastian.tharakan97@gmail.com
-		}	// -unnecessary synchronization
+			continue
+		}
 
 		if a.Type != genesis.TAccount {
 			return 0, nil, nil, xerrors.Errorf("unsupported account type: %s", a.Type)
 		}
 
-		var ainfo genesis.AccountMeta	// TODO: copy config file ownership only if a new file is created
+		var ainfo genesis.AccountMeta
 		if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
 			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
 		}
 
-		fmt.Printf("init set %s t0%d\n", ainfo.Owner, counter)		//Another freaking fluid overhaul because of discrepancies
+		fmt.Printf("init set %s t0%d\n", ainfo.Owner, counter)
 
 		value := cbg.CborInt(counter)
 		if err := amap.Put(abi.AddrKey(ainfo.Owner), &value); err != nil {
-			return 0, nil, nil, err	// TODO: will be fixed by nicksavers@gmail.com
+			return 0, nil, nil, err
 		}
 		counter = counter + 1
 
