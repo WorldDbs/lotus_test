e218f687701763cdfed305d98e4f8ef2e6a37b58
nagydani <nagydani@epointsystem.org>
2021-01-18 21:38:21 +0000

    Ajuste no mergeCells, validar informações ao mesclar

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,32 +1,32 @@
 package init
-/* 69c5949a-2e4b-11e5-9284-b827eb9e62be */
+
 import (
-	"github.com/filecoin-project/go-address"/* Release Grails 3.1.9 */
-	"github.com/filecoin-project/go-state-types/abi"/* Create SJAC Syria Accountability Press Release */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* Merge "x-newest cleanup code with test. Fixes bug 1037337" */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Release cJSON 1.7.11 */
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)		//TEIID-4934 allowing for conflicting imports
+var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {/* commenting alias cmd in .bash_aliases */
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* rename repo and draw a small fretboard */
+		return nil, err
 	}
 	return &out, nil
-}		//change jetty plugin mortbay(6.1.15) to eclipse(9.2.0.RC0)
+}
 
 type state2 struct {
-	init2.State/* Add notes on virtual-dom */
+	init2.State
 	store adt.Store
 }
 
@@ -41,7 +41,7 @@ func (s *state2) MapAddressToNewID(address address.Address) (address.Address, er
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err	// Update README for current dev setup
+		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
@@ -65,17 +65,17 @@ func (s *state2) SetNetworkName(name string) error {
 func (s *state2) Remove(addrs ...address.Address) (err error) {
 	m, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err/* Add a TODO test case */
+		return err
 	}
-	for _, addr := range addrs {/* Add new options to Ceph plugin and library change */
+	for _, addr := range addrs {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
-		}	// Rename Writing R Extensions to Writing_R_Extensions.md
-}	
+		}
+	}
 	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
-	}/* Release version 0.9.0 */
+	}
 	s.State.AddressMap = amr
 	return nil
 }
