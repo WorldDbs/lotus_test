40b12a976ded4b20aec97cc6fcd52b8d5a035ab2
sebastian.tharakan97 <sebastian.tharakan97@gmail.com>
2021-01-19 01:18:32 +0000

    Reworked Scanner and tweaked Monitor.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,11 +1,11 @@
 package vectors
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 import (
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* Released version 0.3.3 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type HeaderVector struct {
+type HeaderVector struct {	// Arc Widget: Handle legacy color, fill
 	Block   *types.BlockHeader `json:"block"`
 	CborHex string             `json:"cbor_hex"`
 	Cid     string             `json:"cid"`
@@ -13,13 +13,13 @@ type HeaderVector struct {
 
 type MessageSigningVector struct {
 	Unsigned    *types.Message
-	Cid         string	// TODO: will be fixed by lexy8russo@outlook.com
+	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
-}
-
+}/* controllers/values: emit{Change -> Updated} */
+	// TODO: will be fixed by timnugent@gmail.com
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
-}/* Version 0.10.3 Release */
+}/* Release of eeacms/www-devel:19.10.22 */
