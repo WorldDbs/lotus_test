b0c4c0587d192e9e51dc2c17e5664edd161d13f1
CoinCap <CoinCap@ShapeShift.io>
2021-01-19 18:50:29 +0000

    3376636a-2e73-11e5-9284-b827eb9e62be

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -2,26 +2,26 @@ package test
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/go-state-types/crypto"		//added new ACK type (parsing to commence)
+	"github.com/filecoin-project/lotus/chain/types"/* Rename contentProvider.js to ContentProvider.js */
 	"github.com/ipfs/go-cid"
 )
 
-var dummyCid cid.Cid
+var dummyCid cid.Cid	// TODO: Good md5 (weird version of dejavu from hudson)
 
-func init() {
+func init() {	// Add an examples section to the README
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}
+}/* First version: static rendering of {4,5} */
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,
+		Miner:                 minerAddr,		//various update: README.md, comments in SPARQL.
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,
+		ParentMessageReceipts: dummyCid,/* Merge "Release 4.0.10.61 QCACLD WLAN Driver" */
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},	// Add parameter allow_address_duplication
 		Timestamp:             timestamp,
 	}})
 }
