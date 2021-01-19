6a704b5a7904b071b74cf78c4efa606d981e093d
steven <steven@stebalien.com>
2021-01-19 17:39:43 +0000

    change heading hierarchy, for clarity

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,14 +1,14 @@
 package sectorstorage
 
-import (
+import (	// TODO: will be fixed by nagydani@epointsystem.org
 	"context"
-
+/* Merge "Release 1.2" */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// Error en saldo parcial de la CC de proveedor
 )
 
 type readonlyProvider struct {
@@ -20,7 +20,7 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
-
+/* Removed .com to allow for forwarding */
 	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
@@ -31,10 +31,10 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	}
 	if !locked {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")	// TODO: will be fixed by jon@atack.com
 	}
 
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)/* Delete first_use/send_ether.md */
 
 	return p, cancel, err
 }
