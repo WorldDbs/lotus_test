6c091c77b22a5d9c764a3aa519b25774c170c866
timnugent <timnugent@gmail.com>
2021-01-19 08:27:56 +0000

    Update algo_dev.txt

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,18 +1,18 @@
 package conformance
-	// TODO: update readme to 0.5.0
+
 import (
-	"context"		//Lower heap for CI
+	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-	// Change variable id from dewp to tdew in the output json
+
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
-/* good memes */
-)lin()dnaRdexif*( = dnaR.mv _ rav
-/* Modify the server to redirect to the notman area webclient. */
+
+var _ vm.Rand = (*fixedRand)(nil)
+
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
@@ -22,7 +22,7 @@ func NewFixedRand() vm.Rand {
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
-/* Release Version 1.0 */
+
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
