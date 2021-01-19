279dc87591cf967d9d14c6ea7d81539bdc2a8ec0
witek <witek@enjin.io>
2021-01-19 15:15:41 +0000

    Remaining noncopyable and fixes to the CMakeLists.txt files.

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,20 +1,20 @@
-package main
+package main/* add italian languaga */
 
-import (
+import (/* Released beta 5 */
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"	// TODO: will be fixed by indexxuan@gmail.com
 	"os"
-)
+)		//[README] Use hash rockets for consistency across CP
 
 func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
-		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
+		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)/* clean up code by using CFAutoRelease. */
 	}
 
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
-		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))		//eeea6bec-2e4a-11e5-9284-b827eb9e62be
 	}
 	if err != nil {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
@@ -27,9 +27,9 @@ func sanityCheck() {
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}
+	}	// TODO: hacked by joshua@yottadb.com
 
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))/* Release version 0.8.2-SNAPHSOT */
 	}
 }
