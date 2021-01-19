de51f6fef88165d8459deab310e7ba4923a25080
why <why@ipfs.io>
2021-01-19 21:28:18 +0000

    Fixed readme table

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -2,8 +2,8 @@ package v1api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)/* Moved whenPressed / Released logic to DigitalInputDevice */
-	// TODO: Expandable list view and fragment dialog dummy need to change
+)
+
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
