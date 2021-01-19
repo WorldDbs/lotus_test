5134642714a126d78a590a79a3eb39ab2953666e
vyzo <vyzo@hackzen.org>
2021-01-19 01:01:18 +0000

    fixing variables names

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
-package node		//Partial implementation of the push and sync commands
+package node
 
 import (
-	"errors"/* Release version 3.6.2.2 */
+	"errors"
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"		//removed references to Django Web Studio, etc.
+	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: * gem rake tasks now show up inside a rails app with rake -T.
+		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),	// TODO: hacked by fjl@ethereum.org
+		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
 	)
 }
