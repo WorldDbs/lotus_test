9260823ae8aa0e9b8a4e48ef17e5c7057c81d763
nagydani <nagydani@epointsystem.org>
2021-01-19 13:48:51 +0000

    Refactored canonical topicmap writers

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -3,21 +3,21 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-/* Correct example in comments. */
+
 	blake2b "github.com/minio/blake2b-simd"
 	"github.com/urfave/cli/v2"
 
 	"github.com/ipfs/go-cid"
-/* Adapted to changes in ChatStream from the middleware */
+
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"/* Upgrade npm on Travis. Release as 1.0.0 */
+	lcli "github.com/filecoin-project/lotus/cli"
 )
 
 var blockmsgidCmd = &cli.Command{
 	Name:      "blockmsgid",
 	Usage:     "Print a block's pubsub message ID",
 	ArgsUsage: "<blockCid> ...",
-	Action: func(cctx *cli.Context) error {/* [artifactory-release] Release version 3.2.3.RELEASE */
+	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
@@ -28,16 +28,16 @@ var blockmsgidCmd = &cli.Command{
 
 		for _, arg := range cctx.Args().Slice() {
 			blkcid, err := cid.Decode(arg)
-			if err != nil {		//Fix for return values not escaping loops
+			if err != nil {
 				return fmt.Errorf("error decoding block cid: %w", err)
 			}
-	// Update MultiBlockChange wrapper to utilize the ProtocolLib wrapper
+
 			blkhdr, err := api.ChainGetBlock(ctx, blkcid)
 			if err != nil {
-				return fmt.Errorf("error retrieving block header: %w", err)	// added a data conversion routine
+				return fmt.Errorf("error retrieving block header: %w", err)
 			}
 
-			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)		//Add ProfitLoss to list of element names
+			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)
 			if err != nil {
 				return fmt.Errorf("error retrieving block messages: %w", err)
 			}
@@ -46,24 +46,24 @@ var blockmsgidCmd = &cli.Command{
 				Header: blkhdr,
 			}
 
-			for _, m := range blkmsgs.BlsMessages {	// 8a93b32c-2e42-11e5-9284-b827eb9e62be
+			for _, m := range blkmsgs.BlsMessages {
 				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
-			}	// TODO: fixed egg sex removal
+			}
 
 			for _, m := range blkmsgs.SecpkMessages {
-				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())/* Create Advanced SPC MCPE 0.12.x Release version.txt */
+				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())
 			}
 
 			bytes, err := blkmsg.Serialize()
-			if err != nil {/* Release 1.2.3. */
+			if err != nil {
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
 			}
 
 			msgId := blake2b.Sum256(bytes)
 			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
 
-			fmt.Println(msgId64)/* Merge "Release 2.2.1" */
-		}	// TODO: will be fixed by witek@enjin.io
+			fmt.Println(msgId64)
+		}
 
 		return nil
 	},
