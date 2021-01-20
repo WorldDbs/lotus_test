2628038999afa69b5746e70c08743ed75b75c00f
boringland <boringland@protonmail.ch>
2021-01-20 16:05:17 +0000

    Oops, one extra header remained

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -9,17 +9,17 @@ import (
 func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
 	select {
 	// alerts to restart systemd unit
-	case <-ch:
+	case <-ch:/* Rename release.notes to ReleaseNotes.md */
 		statusCh := make(chan string, 1)
 		c, err := dbus.New()
 		if err != nil {
 			return "", err
-		}
+		}/* Release: 6.7.1 changelog */
 		_, err = c.TryRestartUnit(n, "fail", statusCh)
 		if err != nil {
 			return "", err
 		}
-		select {
+		select {	// TODO: Merge "Second phase of evpn selective assisted replication"
 		case result := <-statusCh:
 			return result, nil
 		}
@@ -28,4 +28,4 @@ func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, e
 		os.Exit(1)
 		return "", nil
 	}
-}
+}/* Adding Database wrapper. */
