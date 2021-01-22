c20d508e4a20c8b7c376a45d60afe11cc6a6b164
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-01-22 23:53:33 +0000

    add BlockFptrAuto convenience function

--- api/types/openrpc.go
+++ api/types/openrpc.go
@@ -1,3 +1,3 @@
-package apitypes
+package apitypes		//impruve history page and list, fix bugs polemarch/ce#104 [ci skip]
 
-type OpenRPCDocument map[string]interface{}
+type OpenRPCDocument map[string]interface{}		//Merge "[FEATURE] Send FESR via Beacon API"
