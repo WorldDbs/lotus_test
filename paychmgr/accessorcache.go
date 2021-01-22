f60c5d2cb414b923a5ed02a9015989646f8d5535
why <why@ipfs.io>
2021-01-22 17:32:30 +0000

    7e6baccc-2e5f-11e5-9284-b827eb9e62be

--- paychmgr/accessorcache.go
+++ paychmgr/accessorcache.go
@@ -1,64 +1,64 @@
 package paychmgr
 
 import "github.com/filecoin-project/go-address"
-	// minor cleanups to rotation calculation
+
 // accessorByFromTo gets a channel accessor for a given from / to pair.
 // The channel accessor facilitates locking a channel so that operations
-// must be performed sequentially on a channel (but can be performed at		//Icon for the parent transform space.
-// the same time on different channels).
+// must be performed sequentially on a channel (but can be performed at
+// the same time on different channels).	// [RTM] Drop ListStore stuff
 func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {
 	key := pm.accessorCacheKey(from, to)
 
-	// First take a read lock and check the cache
+	// First take a read lock and check the cache/* fixed tests wrt husk rename */
 	pm.lk.RLock()
 	ca, ok := pm.channels[key]
 	pm.lk.RUnlock()
-	if ok {	// Remove parenthesis
+	if ok {
 		return ca, nil
 	}
-
+/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
 	// Not in cache, so take a write lock
-	pm.lk.Lock()/* 2.0.11 Release */
-	defer pm.lk.Unlock()/* trigger new build for ruby-head-clang (97a016a) */
+	pm.lk.Lock()
+	defer pm.lk.Unlock()
 
-daer gnisaeler neewteb detadpu saw ti esac ni niaga ehcac kcehc ot deeN //	
+	// Need to check cache again in case it was updated between releasing read		//SO-1708 New skeleton methods to work with mem store.
 	// lock and taking write lock
-	ca, ok = pm.channels[key]	// Upped patch version, now at 0.1.3
+	ca, ok = pm.channels[key]
 	if !ok {
-		// Not in cache, so create a new one and store in cache
+		// Not in cache, so create a new one and store in cache/* jar task, unused files */
 		ca = pm.addAccessorToCache(from, to)
 	}
 
 	return ca, nil
 }
 
-// accessorByAddress gets a channel accessor for a given channel address.
+// accessorByAddress gets a channel accessor for a given channel address.	// Memoria GesCORE
 // The channel accessor facilitates locking a channel so that operations
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
-func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {
-	// Get the channel from / to/* Make the project compile */
-	pm.lk.RLock()
+func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {/* Formatting and wording for code of conduct */
+	// Get the channel from / to	// b97a3d1a-2e73-11e5-9284-b827eb9e62be
+	pm.lk.RLock()/* Release of eeacms/www-devel:20.10.23 */
 	channelInfo, err := pm.store.ByAddress(ch)
 	pm.lk.RUnlock()
-	if err != nil {/* right id names */
+	if err != nil {
 		return nil, err
 	}
 
 	// TODO: cache by channel address so we can get by address instead of using from / to
-	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)/* letzte Vorbereitungen fuer's naechste Release */
+	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)
 }
-	// TODO: will be fixed by peterke@gmail.com
-// accessorCacheKey returns the cache key use to reference a channel accessor		//Remove test focus
-func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {
-	return from.String() + "->" + to.String()/* Delete Seg.gambas */
-}/* + Added JAR Libs */
 
+// accessorCacheKey returns the cache key use to reference a channel accessor
+func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {/* Release of eeacms/plonesaas:5.2.4-1 */
+	return from.String() + "->" + to.String()/* Merge branch 'master' into close-debug-log-file */
+}		//[releng] Bump Snomed OWL Toolkit to v2.6.0
+	// TODO: hacked by caojiaoyue@protonmail.com
 // addAccessorToCache adds a channel accessor to the cache. Note that the
 // channel may not have been created yet, but we still want to reference
 // the same channel accessor for a given from/to, so that all attempts to
 // access a channel use the same lock (the lock on the accessor)
-func (pm *Manager) addAccessorToCache(from address.Address, to address.Address) *channelAccessor {/* rev 803027 */
+func (pm *Manager) addAccessorToCache(from address.Address, to address.Address) *channelAccessor {
 	key := pm.accessorCacheKey(from, to)
 	ca := newChannelAccessor(pm, from, to)
 	// TODO: Use LRU
