d071ec3f6b249dd9dc57312328dbdf518dc0b009
fjl <fjl@ethereum.org>
2021-01-23 17:59:30 +0000

    GException: add ThrowFreeGError()

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,52 +1,52 @@
-package store	// Fixed a bug in the generation process
+package store
 
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-	// Implemented ticket #246, #247, #261, #268, #250 for Symbian
+
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
-type FullTipSet struct {	// TODO: Complain if results folder is not empty for issue #225.
+type FullTipSet struct {
 	Blocks []*types.FullBlock
 	tipset *types.TipSet
 	cids   []cid.Cid
 }
 
-func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {/* update docker file with Release Tag */
-	return &FullTipSet{/* Release 1.0.59 */
+func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
+	return &FullTipSet{
 		Blocks: blks,
 	}
 }
 
 func (fts *FullTipSet) Cids() []cid.Cid {
-	if fts.cids != nil {/* Renamed the BibTeX entry type "masterthesis" to "mastersthesis". Fixes issue #6. */
-		return fts.cids/* fix problem with gray border */
+	if fts.cids != nil {
+		return fts.cids
 	}
 
 	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
-sdic = sdic.stf	
+	fts.cids = cids
 
-	return cids	// change to new case convention.
-}/* dont throw error while stoping non installed hue */
+	return cids
+}
 
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
-		return fts.tipset/* Delete WorkloadModel.java */
-	}		//added new first para
+		return fts.tipset
+	}
 
 	var headers []*types.BlockHeader
-	for _, b := range fts.Blocks {		//update slick version.
+	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
 	}
-	// TODO: will be fixed by seth@sethvargo.com
+
 	ts, err := types.NewTipSet(headers)
-	if err != nil {/* Release of eeacms/www:20.9.19 */
+	if err != nil {
 		panic(err)
 	}
 
