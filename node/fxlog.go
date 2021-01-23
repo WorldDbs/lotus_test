ba3d19e20f3e4d39fd8ef0b86adb8ae59be69715
onhardev <onhardev@bk.ru>
2021-01-23 21:56:06 +0000

    Updated changelog with the latest...

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
 
 import (
-	logging "github.com/ipfs/go-log/v2"/* Release 2.6.0-alpha-2: update sitemap */
+	logging "github.com/ipfs/go-log/v2"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Add ErrorLogger class */
 )
 
 type debugPrinter struct {
 	l logging.StandardLogger
-}/* Simple zIndex rather than fancy stack works best. */
+}
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
+	p.l.Debugf(f, a...)/* rev 546369 */
 }
-/* Revert to mDNSResponder 333.10. */
-var _ fx.Printer = new(debugPrinter)
+
+var _ fx.Printer = new(debugPrinter)/* Delete mockup_gameplay_title_02.png */
