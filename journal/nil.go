419e798ad1c5242c3b4f55d3c128d46962820472
souzau <souzau@yandex.com>
2021-01-23 10:49:13 +0000

    Tidy debug session messages

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal
-	// TODO: remove php 5.3
+package journal/* Release candidate 1. */
+		//Update sqlDB.js
 type nilJournal struct{}
 
-// nilj is a singleton nil journal./*  Complete! */
-var nilj Journal = &nilJournal{}
+// nilj is a singleton nil journal.
+var nilj Journal = &nilJournal{}	// TODO: 3881f0e4-2e6a-11e5-9284-b827eb9e62be
 
 func NilJournal() Journal {
-	return nilj	// [ExoBundle] Correction bug adress when create question graphic.
-}
+	return nilj
+}	// TODO: hacked by boringland@protonmail.ch
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
+		//translate invalid login message
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}		//Fixed an unfortunate cast in raw_init_file_tiff().
-
-func (n *nilJournal) Close() error { return nil }
+func (n *nilJournal) Close() error { return nil }	// insert alttext and altimg into math equations if applicable
