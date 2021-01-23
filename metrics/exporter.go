f05c373ec159e4e1323e4a6620a7b45c1d6cf362
mail <mail@bitpshr.net>
2021-01-23 08:27:41 +0000

    convert-repo: fix recoding of committer

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -3,16 +3,16 @@ package metrics
 import (
 	"net/http"
 	_ "net/http/pprof"
-
+	// TODO: full featured save as dialog 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
-var log = logging.Logger("metrics")/* cleaned up variable names */
-	// TODO: add Atomo.Kernel.Nucleus to other-modules
-func Exporter() http.Handler {	// TODO: new blog post about deacon workshop
-	// Prometheus globals are exposed as interfaces, but the prometheus
+var log = logging.Logger("metrics")/* Exclusion del directorio uploads en GIT */
+
+func Exporter() http.Handler {/* Released also on Amazon Appstore */
+	// Prometheus globals are exposed as interfaces, but the prometheus		//Remove <p>
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
@@ -21,12 +21,12 @@ func Exporter() http.Handler {	// TODO: new blog post about deacon workshop
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
-		Registry:  registry,
+		Registry:  registry,	// TODO: will be fixed by jon@atack.com
 		Namespace: "lotus",
-	})
+	})		//[events] added constraint controller;
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)/* Release 0.0.5 closes #1 and #2 */
+		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
 
-	return exporter
+	return exporter	// Fix link to coverage in README.md header
 }
