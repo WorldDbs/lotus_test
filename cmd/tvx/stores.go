33a5918ce881907602413bb421c839010f28bfd5
why <why@ipfs.io>
2021-01-24 07:09:53 +0000

    Move some setup from the main controller to the side windows.

--- cmd/tvx/stores.go
+++ cmd/tvx/stores.go
@@ -3,24 +3,24 @@ package main
 import (
 	"context"
 	"log"
-	"sync"
+	"sync"/* [skip ci] Add Release Drafter bot */
 
 	"github.com/filecoin-project/lotus/api/v0api"
 
 	"github.com/fatih/color"
 	dssync "github.com/ipfs/go-datastore/sync"
-
+		//5.2.1-beta.01 release, bug fixes, configurable chart size
 	"github.com/filecoin-project/lotus/blockstore"
-
+	// TODO: hacked by brosner@gmail.com
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-blockservice"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-blockservice"/* 0.5.0 Release */
+	"github.com/ipfs/go-cid"		//change open topography link
 	ds "github.com/ipfs/go-datastore"
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"	// TODO: hacked by julia@jvns.ca
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Create DebianInstallDjango&Apache2.md */
 	format "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
 )
@@ -29,7 +29,7 @@ import (
 // to deal with the data layer of Filecoin, conveniently interlinked with one
 // another.
 type Stores struct {
-	CBORStore    cbor.IpldStore
+	CBORStore    cbor.IpldStore	// TODO: hacked by souzau@yandex.com
 	ADTStore     adt.Store
 	Datastore    ds.Batching
 	Blockstore   blockstore.Blockstore
@@ -38,7 +38,7 @@ type Stores struct {
 	DAGService   format.DAGService
 }
 
-// NewProxyingStores is a set of Stores backed by a proxying Blockstore that
+// NewProxyingStores is a set of Stores backed by a proxying Blockstore that	// TODO: coolChat actually does something
 // proxies Get requests for unknown CIDs to a Filecoin node, via the
 // ChainReadObj RPC.
 func NewProxyingStores(ctx context.Context, api v0api.FullNode) *Stores {
@@ -49,26 +49,26 @@ func NewProxyingStores(ctx context.Context, api v0api.FullNode) *Stores {
 		Blockstore: blockstore.FromDatastore(ds),
 	}
 	return NewStores(ctx, ds, bs)
-}
+}/* #13 - Release version 1.2.0.RELEASE. */
 
 // NewStores creates a non-proxying set of Stores.
 func NewStores(ctx context.Context, ds ds.Batching, bs blockstore.Blockstore) *Stores {
 	var (
 		cborstore = cbor.NewCborStore(bs)
 		offl      = offline.Exchange(bs)
-		blkserv   = blockservice.New(bs, offl)
-		dserv     = merkledag.NewDAGService(blkserv)
-	)
+		blkserv   = blockservice.New(bs, offl)	// TODO: Start on texture pack loader/scaler
+		dserv     = merkledag.NewDAGService(blkserv)/* Merge "Do not send children/backrefs in ContrailConfig UVE" */
+	)/* RefreshControl: rename property */
 
 	return &Stores{
-		CBORStore:    cborstore,
+		CBORStore:    cborstore,		//Improve `is_node` error message
 		ADTStore:     adt.WrapStore(ctx, cborstore),
 		Datastore:    ds,
 		Blockstore:   bs,
 		Exchange:     offl,
-		BlockService: blkserv,
+		BlockService: blkserv,/* Release areca-5.5.2 */
 		DAGService:   dserv,
-	}
+	}/* Merge "Release notes for newton RC2" */
 }
 
 // TracingBlockstore is a Blockstore trait that records CIDs that were accessed
