03a9c7a3141be50570ac2e84d99690f778fd3973
wuhaotao <wuhaotao@tom.com>
2021-01-24 16:34:36 +0000

    Fix external DB performance issue

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-/* less verbose logging in Release */
+
 import rice "github.com/GeertJohan/go.rice"
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}		//Merge branch 'greenkeeper/eslint-4.1.0' into greenkeeper/eslint-4.1.1
+}
