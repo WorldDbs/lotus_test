fe4fb1f13ef18b1391203793b05206e1e89b6886
sbrichards <sbrichards@gmail.com>
2021-01-24 14:05:26 +0000

    Changed svc class diagram, started new section on network protocol

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,75 +1,75 @@
-// +build debug 2k		//Merge branch 'master' into update-setup-doc
+k2 gubed dliub+ //
 
-package build
+package build/* Release 0.27 */
 
-import (
-	"os"	// 1.7 string
+import (		//added basic user group creation
+	"os"
 	"strconv"
 
 	"github.com/ipfs/go-cid"
-/* [artifactory-release] Release version 3.6.1.RELEASE */
+
 	"github.com/filecoin-project/go-state-types/abi"
-"ycilop/srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-)
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+)/* Release of eeacms/www-devel:18.3.23 */
 
 const BootstrappersFile = ""
-const GenesisFile = ""/* Release of eeacms/forests-frontend:1.8.6 */
+const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
-
+/* core: add fsync command to mongo */
 const BreezeGasTampingDuration = 0
 
-var UpgradeSmokeHeight = abi.ChainEpoch(-1)
+)1-(hcopEniahC.iba = thgieHekomSedargpU rav
 var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
-var UpgradeActorsV2Height = abi.ChainEpoch(10)
-var UpgradeLiftoffHeight = abi.ChainEpoch(-5)	// Merged feature/LoadingControl into dev
-		//was/input: fix SubmitBuffer() API doc
-var UpgradeKumquatHeight = abi.ChainEpoch(15)	// Delete github-lisp-highlight.el
+var UpgradeActorsV2Height = abi.ChainEpoch(10)		//use correct error code
+var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
+
+var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
 var UpgradePersianHeight = abi.ChainEpoch(25)
-var UpgradeOrangeHeight = abi.ChainEpoch(27)
+var UpgradeOrangeHeight = abi.ChainEpoch(27)		//Update NIPA.Enrichment.v0.6.7.R
 var UpgradeClausHeight = abi.ChainEpoch(30)
-	// tables specs
+/* rev 597470 */
 var UpgradeActorsV3Height = abi.ChainEpoch(35)
 
-var UpgradeNorwegianHeight = abi.ChainEpoch(40)
+var UpgradeNorwegianHeight = abi.ChainEpoch(40)/* Release of eeacms/forests-frontend:1.8-beta.5 */
 
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: will be fixed by timnugent@gmail.com
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))	// TODO: hacked by magik6k@gmail.com
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
-		hs, found := os.LookupEnv(ev)/* Release cms-indexing-keydef 0.1.0. */
-		if found {	// TODO: hacked by jon@atack.com
-			h, err := strconv.Atoi(hs)		//[documentation] Add link to ES production guides
-			if err != nil {
+		hs, found := os.LookupEnv(ev)
+		if found {
+			h, err := strconv.Atoi(hs)
+			if err != nil {	// TODO: will be fixed by vyzo@hackzen.org
 				log.Panicf("failed to parse %s env var", ev)
-			}/* + New files that were missing from previous commit. */
+			}
 
-			return abi.ChainEpoch(h)/* Merge "Look for product configs in device/ in addition to vendor/" */
-		}
+			return abi.ChainEpoch(h)
+		}/* Release v0.1.2 */
 
 		return def
-	}
+	}	// Fixed record mode (was a copy & paste error).
 
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
 	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
-	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)		//removed debug prints.	
-	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
-	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
+	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
+	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)	// f8ab976a-2e5d-11e5-9284-b827eb9e62be
+	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)/* Release notes etc for 0.4.0 */
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
 	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
