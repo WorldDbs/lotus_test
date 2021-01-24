71213d44e7fdbc2ec8c89f15b83c7d306f09b16a
indexxuan <indexxuan@gmail.com>
2021-01-24 07:57:54 +0000

    bug mkdir -p

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,43 +1,43 @@
 package build
-/* Release a8. */
+	// TODO: hacked by m-ou.se@m-ou.se
 import (
 	"bytes"
-	"compress/gzip"		//Updated the compas_slicer feedstock.
+	"compress/gzip"
 	"encoding/json"
 
-	rice "github.com/GeertJohan/go.rice"/* + some small changes in README.md */
-/* render audio with fx pt 1 */
+	rice "github.com/GeertJohan/go.rice"
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
+	// Feb twitter stats
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
-	err = json.NewDecoder(zr).Decode(&m)/* adding in Release build */
+	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
 	if err != nil {
 		log.Fatal(err)
-	}	// colocando comurs... descobri que falta os parameters
+	}
 	return m
 }
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")	// TODO: + ProgressBar
 	return mustReadGzippedOpenRPCDocument(data)
-}
+}/* #105 - Release version 0.8.0.RELEASE. */
 
-func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {/* Create prelim.md */
+func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
-	// Merge "Add -code-profile option."
+
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)/* Release URL in change log */
-}
+	return mustReadGzippedOpenRPCDocument(data)
+}/* [artifactory-release] Release version 2.3.0.M2 */
