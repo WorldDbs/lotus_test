92f2a6fc6014ed82363c24188c58a82da4e27b75
lexy8russo <lexy8russo@outlook.com>
2021-01-24 18:37:56 +0000

    Merge "Add workaround notice for apparmor issue."

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
-package journal		//version 0.6
-	// Merge "Include ansible config when syncing repo"
-import (		//Added MANIFEST.in to allow creation of source distribution.
+package journal
+
+import (
 	"os"
 )
-/* Release  v0.6.3 */
-// envJournalDisabledEvents is the environment variable through which disabled
-// journal events can be customized.
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
+// envJournalDisabledEvents is the environment variable through which disabled
+// journal events can be customized.	// TODO: Merge branch 'master' of local repository into mccaskey/puma
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
+/* Delete table_cache.h */
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
-		if ret, err := ParseDisabledEvents(env); err == nil {
+		if ret, err := ParseDisabledEvents(env); err == nil {/* Исправление classpath для библиотеки */
 			return ret
 		}
 	}
-	// fallback if env variable is not set, or if it failed to parse./* Update qs_ticket.py */
-	return DefaultDisabledEvents/* Merge "Update node modules" */
-}/* minor improvements in text */
+	// fallback if env variable is not set, or if it failed to parse.
+	return DefaultDisabledEvents
+}		//ADD: Data notebook items
