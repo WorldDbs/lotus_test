03b77c045780723492642c0e3dc07c921072563e
alan.shaw <alan.shaw@protocol.ai>
2021-01-25 23:53:38 +0000

    p_bb_os_flent

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -3,14 +3,14 @@ package sealing
 import (
 	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release 1.0.0-RC2. */
 
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
-	defer m.inputLk.Unlock()
+	defer m.inputLk.Unlock()/* Merge "Removing pip-missing-reqs from default tox jobs" */
 
 	cfg, err := m.getConfig()
 	if err != nil {
@@ -18,24 +18,24 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	}
 
 	if cfg.MaxSealingSectors > 0 {
-		if m.stats.curSealing() >= cfg.MaxSealingSectors {
+		if m.stats.curSealing() >= cfg.MaxSealingSectors {/* #29: added some logging to locate where threaded example hangs */
 			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
 	}
-	// TODO: will be fixed by xiemengjun@gmail.com
+
 	spt, err := m.currentSealProof(ctx)
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
-
+/* MWL#89 automatic merge with 5.3 */
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
 		return storage.SectorRef{}, err
-	}		//Delete icon311.png
+	}
 
-	log.Infof("Creating CC sector %d", sid)	// limit to 100 mappings
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
+	log.Infof("Creating CC sector %d", sid)
+	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{	// TODO: Cleaning up RSpec support files
 		ID:         sid,
 		SectorType: spt,
-	})
-}/* Release of eeacms/www:19.6.12 */
+	})	// Metrics table working for files now.
+}
