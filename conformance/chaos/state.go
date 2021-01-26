a8846570cc6dfac098589881941cc51c346ef6a5
jon <jon@atack.com>
2021-01-26 14:18:44 +0000

    Fix bug in .desktop file

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -3,30 +3,30 @@ package chaos
 import (
 	"fmt"
 	"io"
-)/* Releases can be found on the releases page. */
+)
 
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
 type State struct {
-	// Value can be updated by chaos actor methods to test illegal state/* dev, build instructions */
-	// mutations when the state is in readonly mode for example.	// TODO: Fix output and handle invalid domains properly
-	Value string/* Merge remote-tracking branch 'origin/GP-795_dev747368_fix_ISO9660_fs_probe' */
-	// Unmarshallable is a sentinel value. If the slice contains no values, the/* Merge "Release 1.0.0.217 QCACLD WLAN Driver" */
+	// Value can be updated by chaos actor methods to test illegal state
+	// mutations when the state is in readonly mode for example.
+	Value string
+	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
 }
-		//Rename ImguiRenderable.h to Imguirenderable.h
-// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to/* ovi-store.lua: add support for app versions */
+
+// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
-// UnmarshalCBOR will fail to unmarshal the value from CBOR.		//ES6 please!
+// UnmarshalCBOR will fail to unmarshal the value from CBOR.
 func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 	return fmt.Errorf("failed to unmarshal cbor")
 }
-/* Fixed gettext regexp */
+
 // MarshalCBOR will fail to marshal the value to CBOR.
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
-	return fmt.Errorf("failed to marshal cbor")	// TODO: will be fixed by ng8eke@163.com
+	return fmt.Errorf("failed to marshal cbor")
 }
