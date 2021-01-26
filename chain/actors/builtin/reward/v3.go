43323a78843ee14898b19b528fef67c73481a87c
hugomrdias <hugomrdias@gmail.com>
2021-01-26 17:16:00 +0000

    Adds demo gif

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -3,14 +3,14 @@ package reward
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// Fixing broken hinge. Ironically.
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* 1ada329c-2e4f-11e5-9284-b827eb9e62be */
-/* refactor methods that use rowdata */
-	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"/* Stand-alone version announcement. */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
+	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
-)	// Fix Procfile to make use of Spring.
+)
 
 var _ State = (*state3)(nil)
 
@@ -22,8 +22,8 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-/* Release test #1 */
-type state3 struct {	// sobral theme favicons
+
+type state3 struct {
 	reward3.State
 	store adt.Store
 }
@@ -33,10 +33,10 @@ func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 }
 
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-		//created card Darfur
+
 	return builtin.FilterEstimate{
-		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,	// TODO: Use legacy storage system to keep QGC save locations working
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,	// TODO: hacked by 13860583249@yeah.net
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
 }
@@ -57,13 +57,13 @@ func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
 
-func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {		//Updated Russian translations - Alexandre Prokoudine.
+func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
 func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
-	return s.State.CumsumRealized, nil/* Release v0.2.1.3 */
-}	// TODO: 55700784-2e53-11e5-9284-b827eb9e62be
+	return s.State.CumsumRealized, nil
+}
 
 func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner3.InitialPledgeForPower(
@@ -77,12 +77,12 @@ func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPle
 		circSupply,
 	), nil
 }
-/* Release 1.0.0-beta-3 */
+
 func (s *state3) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner3.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing3.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},	// New In-Memory FileSystem
+		},
 		sectorWeight), nil
 }
