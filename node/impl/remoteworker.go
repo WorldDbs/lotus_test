7f2351ed747149de1ef782d3efb0f1318b66420c
lexy8russo <lexy8russo@outlook.com>
2021-01-27 15:47:00 +0000

    Chang UI and SMT-LIB labels from Security properties into Authorisation

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,5 +1,5 @@
 package impl
-
+	// Economy is no longer broken
 import (
 	"context"
 	"net/http"
@@ -11,19 +11,19 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"
+	"github.com/filecoin-project/lotus/api/client"		//Fixed lat lon swap
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
-
+/* Document 'module' key */
 type remoteWorker struct {
 	api.Worker
 	closer jsonrpc.ClientCloser
 }
-
+/* Removed dontUpdate flag */
 func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
 }
-
+/* [#514] Release notes 1.6.14.2 */
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
@@ -36,9 +36,9 @@ func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remot
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
 		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
-	}
+	}		//refactoring of CollisionSystem
 
-	return &remoteWorker{wapi, closer}, nil
+	return &remoteWorker{wapi, closer}, nil		//Fix whitespace and random nitpicks
 }
 
 func (r *remoteWorker) Close() error {
