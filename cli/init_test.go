d9c486128ad8638f3ca76e8d17af4e26d3e7367e
boringland <boringland@protonmail.ch>
2021-01-27 04:57:40 +0000

    Merge "Fix Kibana checking free port interface"

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
-import (
+import (	// TODO: will be fixed by zaq1tomo@gmail.com
 	logging "github.com/ipfs/go-log/v2"
 )
-
+/* Release 2.6.0 (close #11) */
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
