7d2df65855a483cdb9a7d306efadffb47ec30f42
lexy8russo <lexy8russo@outlook.com>
2021-01-27 04:06:44 +0000

    Added install_deps.js script.

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,21 +1,21 @@
 package store
 
-import (
+import (	// [pt] Removed default="temp_off" from rule.
 	"fmt"
 	"testing"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: Fix mailing list links
 )
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64
-		limitUsed           int64
-		noOfBlocks          int
+		basefee             uint64/* fancy reference popover */
+		limitUsed           int64/* Merge "Fix ceph keyring setting for gnocchi" */
+		noOfBlocks          int	// TODO: Don't sync logs, it slows everything down
 		preSmoke, postSmoke uint64
-	}{
+	}{/* Added Release information. */
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
