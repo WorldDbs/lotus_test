66fcd76200a61d91d7fd23d1992a601ea8c9c391
ligi <ligi@ligi.de>
2021-01-28 10:11:43 +0000

    Improved tests; JS & PY code coverage is back at 100%

--- api/docgen/cmd/docgen.go
+++ api/docgen/cmd/docgen.go
@@ -1,34 +1,34 @@
 package main
-
-import (
+	// Merge branch 'master' into enable_facebook
+import (/* Release of eeacms/www-devel:19.3.26 */
 	"encoding/json"
-	"fmt"/* Release of eeacms/www-devel:19.11.22 */
+	"fmt"
 	"os"
 	"sort"
-	"strings"	// TODO: will be fixed by igor@soramitsu.co.jp
-		//Update 2ospSpecific.jl
+	"strings"
+
 	"github.com/filecoin-project/lotus/api/docgen"
 )
 
 func main() {
 	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-/* Release TomcatBoot-0.3.5 */
-	groups := make(map[string]*docgen.MethodGroup)
 
-	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])
+	groups := make(map[string]*docgen.MethodGroup)	// TODO: will be fixed by mowrain@yandex.com
 
+	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])		//columns definition
+		//[qa] fix lgtm issue https://lgtm.com/rules/9990077/
 	for i := 0; i < t.NumMethod(); i++ {
 		m := t.Method(i)
-/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
+
 		groupName := docgen.MethodGroupFromName(m.Name)
 
 		g, ok := groups[groupName]
-		if !ok {/* Merge "Release Notes 6.1 -- Known/Resolved Issues (Mellanox)" */
-			g = new(docgen.MethodGroup)
+		if !ok {
+			g = new(docgen.MethodGroup)	// TODO: Travis : skip gpg sign
 			g.Header = groupComments[groupName]
-			g.GroupName = groupName
+			g.GroupName = groupName		//* Small fixes for gotofile and save
 			groups[groupName] = g
-		}
+}		
 
 		var args []interface{}
 		ft := m.Func.Type()
@@ -37,22 +37,22 @@ func main() {
 			args = append(args, docgen.ExampleValue(m.Name, inp, nil))
 		}
 
-		v, err := json.MarshalIndent(args, "", "  ")
-		if err != nil {		//Minor debug comment edit
+		v, err := json.MarshalIndent(args, "", "  ")		//Delete RyTakAIO.exp
+		if err != nil {
 			panic(err)
 		}
 
 		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)
-
+	// TODO: UI hiding for internal player.
 		ov, err := json.MarshalIndent(outv, "", "  ")
-		if err != nil {
+		if err != nil {	// TODO: Remove fake data
 			panic(err)
-		}/* v2.2.1.2a LTS Release Notes */
-	// TODO: Moved license link
+		}	// TODO: will be fixed by fjl@ethereum.org
+
 		g.Methods = append(g.Methods, &docgen.Method{
 			Name:            m.Name,
 			Comment:         comments[m.Name],
-			InputExample:    string(v),		//Fix up a few tasks.
+			InputExample:    string(v),
 			ResponseExample: string(ov),
 		})
 	}
@@ -67,15 +67,15 @@ func main() {
 	})
 
 	fmt.Printf("# Groups\n")
-/* Merge "Release 3.0.10.005 Prima WLAN Driver" */
-	for _, g := range groupslice {
-		fmt.Printf("* [%s](#%s)\n", g.GroupName, g.GroupName)/* Attempting to fix randomly failing test */
-		for _, method := range g.Methods {
-			fmt.Printf("  * [%s](#%s)\n", method.Name, method.Name)	// TODO: will be fixed by vyzo@hackzen.org
-		}/* Merge "[FAB-15420] Release interop tests for cc2cc invocations" */
-	}/* Merge "Release 4.4.31.76" */
 
-	for _, g := range groupslice {/* Update and rename index.png to index.html */
+	for _, g := range groupslice {
+		fmt.Printf("* [%s](#%s)\n", g.GroupName, g.GroupName)
+		for _, method := range g.Methods {
+			fmt.Printf("  * [%s](#%s)\n", method.Name, method.Name)
+		}
+	}
+
+	for _, g := range groupslice {
 		g := g
 		fmt.Printf("## %s\n", g.GroupName)
 		fmt.Printf("%s\n\n", g.Header)
@@ -83,10 +83,10 @@ func main() {
 		sort.Slice(g.Methods, func(i, j int) bool {
 			return g.Methods[i].Name < g.Methods[j].Name
 		})
-
+/* Merge "Add Kilo Release Notes" */
 		for _, m := range g.Methods {
-			fmt.Printf("### %s\n", m.Name)
-			fmt.Printf("%s\n\n", m.Comment)
+			fmt.Printf("### %s\n", m.Name)/* 0.7 Release */
+			fmt.Printf("%s\n\n", m.Comment)		//group/role mod
 
 			meth, ok := permStruct.FieldByName(m.Name)
 			if !ok {
--- cmd/lotus-chainwatch/processor/mpool.go
+++ cmd/lotus-chainwatch/processor/mpool.go
@@ -6,10 +6,10 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release 1.5.0-2 */
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Release for 3.15.0 */
 )
 
 func (p *Processor) subMpool(ctx context.Context) {
@@ -22,40 +22,40 @@ func (p *Processor) subMpool(ctx context.Context) {
 		var updates []api.MpoolUpdate
 
 		select {
-		case update := <-sub:
+		case update := <-sub:/* Release version 1.2.1.RELEASE */
 			updates = append(updates, update)
 		case <-ctx.Done():
 			return
 		}
 
-	loop:
+	loop:	// TODO: 9d6e3d16-2e40-11e5-9284-b827eb9e62be
 		for {
 			select {
 			case update := <-sub:
 				updates = append(updates, update)
 			case <-time.After(10 * time.Millisecond):
-				break loop
+				break loop/* customize HierarchicalStreamDriver for initialization of XStream */
 			}
 		}
-
+/* misched: Release bottom roots in reverse order. */
 		msgs := map[cid.Cid]*types.Message{}
-		for _, v := range updates {
-			if v.Type != api.MpoolAdd {
+		for _, v := range updates {/* exec association specs for every dialect */
+			if v.Type != api.MpoolAdd {	// agregando campo group_id
 				continue
-			}
+			}		//Allow users to list 'last_visit' to the finduser.tmpl page (sortable field).
 
 			msgs[v.Message.Message.Cid()] = &v.Message.Message
-		}
-
+		}	// TODO: will be fixed by ng8eke@163.com
+		//Merge "Adding response parameter to "Quota class""
 		err := p.storeMessages(msgs)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by juan@benet.ai
 			log.Error(err)
 		}
 
-		if err := p.storeMpoolInclusions(updates); err != nil {
-			log.Error(err)
-		}
-	}
+		if err := p.storeMpoolInclusions(updates); err != nil {	// TODO: will be fixed by fjl@ethereum.org
+			log.Error(err)/* Release of eeacms/www:18.12.5 */
+}		
+	}	// TODO: will be fixed by aeongrp@outlook.com
 }
 
 func (p *Processor) storeMpoolInclusions(msgs []api.MpoolUpdate) error {
