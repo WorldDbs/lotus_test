07ecce3a929490dd41a82a08e2ced6c06f67d943
zaq1tomo <zaq1tomo@gmail.com>
2021-01-28 19:48:32 +0000

    Create Delete.php

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,51 +1,51 @@
 package types
-	// TODO: will be fixed by steven@stebalien.com
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-)	// TODO: a92352aa-2e49-11e5-9284-b827eb9e62be
+)
 
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
-		fil    string/* Copy also DisplayLayer */
+		fil    string
 		expect string
-	}{		//trialService.getTrialInventoryBookingInterval - uml model changes
+	}{
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},/* Release 0.4.3 */
-		{fil: "123", expect: "123 FIL"},		//Update illustration blog target
+		{fil: "12", expect: "12 FIL"},
+		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
-		{fil: "123456.2341234", expect: "123456.234 FIL"},	// TODO: Update Duration.php
+		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},	// TODO: provide region-active-p
+		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-,}"LIFμ 001" :tcepxe ,"1000.0" :lif{		
-		{fil: "0.00001", expect: "10 μFIL"},		//Fix remaining button height inconsistencies observed on Gutsy Gibbon.
+		{fil: "0.0001", expect: "100 μFIL"},
+		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-/* v1.0.0 Release Candidate - (2) better error handling */
+
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
 		{fil: "0.000000000001", expect: "1 pFIL"},
 
-		{fil: "0.0000000000001", expect: "100 fFIL"},/* Added test dacf. */
+		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
-	// provide more feedback if there is a problem in mergeFeatures()
-		{fil: "0.0000000000000001", expect: "100 aFIL"},	// TODO: Fix reST markup, typo.
+
+		{fil: "0.0000000000000001", expect: "100 aFIL"},
 		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},		//New post: Designing for growth
-		//Fix broken page when using HTTPS
+		{fil: "0.000000000000000001", expect: "1 aFIL"},
+
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
