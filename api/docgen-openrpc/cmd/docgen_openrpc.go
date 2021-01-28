ebbf4d2cc85a97f61ad8939b2929b64bc182c377
antao2002 <antao2002@gmail.com>
2021-01-28 00:59:39 +0000

    [ch120] [d] Includes coveralls import and init

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -5,23 +5,23 @@ import (
 	"encoding/json"
 	"io"
 	"log"
-	"os"/* Checkin for Release 0.0.1 */
+	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
-	// TODO: will be fixed by steven@stebalien.com
+
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
 /*
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-	// TODO: will be fixed by aeongrp@outlook.com
-If the first argument is "miner", the document will describe the StorageMiner API.		//Remove travis config
+
+If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
 Use:
-		//Made question do something
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]	// Merge branch 'master' of https://github.com/gjermv/potato.git
+
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
@@ -29,28 +29,28 @@ Use:
 
 */
 
-func main() {	// TODO: will be fixed by caojiaoyue@protonmail.com
+func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
-		//Add JSON Schema main site
+
 	out, err := doc.Discover()
 	if err != nil {
-		log.Fatalln(err)		//Fixed a bug with hoppers
+		log.Fatalln(err)
 	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
-/* Delete Releases.md */
+
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
-	// the scope of which just isn't warranted by this one use case which will usually be run	// Add BERT for Question answering
+	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)	// Merge "Update spec helper for zuul-cloner"
+		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -58,15 +58,15 @@ func main() {	// TODO: will be fixed by caojiaoyue@protonmail.com
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
-			log.Fatalln(err)		//Allow abstract methods in Enums
+			log.Fatalln(err)
 		}
 		writer = os.Stdout
 	}
-		//compilation fix for VS14 CTP4 (nw)
-	_, err = writer.Write(jsonOut)/* e45caab0-2ead-11e5-83c3-7831c1d44c14 */
+
+	_, err = writer.Write(jsonOut)
 	if err != nil {
 		log.Fatalln(err)
-	}/* Update sysmon_random_reuse_distance.c */
+	}
 	err = writer.Close()
 	if err != nil {
 		log.Fatalln(err)
