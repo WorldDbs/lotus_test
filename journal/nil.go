cd65c165857f1696898c9d94430b252f86cd1285
alex.gaynor <alex.gaynor@gmail.com>
2021-01-28 00:34:00 +0000

    making sure we only show a user once as a backer even if already an admin (#168)

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal/* Release candidate 1. */
-		//Update sqlDB.js
+package journal
+
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}	// TODO: 3881f0e4-2e6a-11e5-9284-b827eb9e62be
+var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
 	return nilj
-}	// TODO: hacked by boringland@protonmail.ch
+}
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
-		//translate invalid login message
+
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
-func (n *nilJournal) Close() error { return nil }	// insert alttext and altimg into math equations if applicable
+func (n *nilJournal) Close() error { return nil }
