4f390868e63856b447683f099baf75c08b24bf7a
onhardev <onhardev@bk.ru>
2021-01-28 06:20:54 +0000

    Prefer assertEqual() over assertEquals() for forward compatibility.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
 import "github.com/raulk/clock"
-	// b48a87b8-2e5c-11e5-9284-b827eb9e62be
+
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines./* Delete Release.key */
+// clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
--- paychmgr/util.go
+++ paychmgr/util.go
@@ -7,29 +7,29 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
-/* Released v1.0.0 */
+
 type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
-	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)/* 8962ed00-35c6-11e5-8d3e-6c40088e03e4 */
-}	// Merge "input: touchscreen: atmel_mxt_ts: avoid memory leakage"
-	// TODO: helloworldtimes4.c
-func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {		//finish add parent
+	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
+}
+
+func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
-		return nil, err/* merge 7.1->7.2 */
+		return nil, err
 	}
-	// TODO: Delete swap-details-view.png
+
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
 	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
 			return nil, err
-		}	// TODO: hacked by steven@stebalien.com
+		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
-				bestByLane[voucher.Lane] = voucher/* Release MailFlute-0.4.0 */
+				bestByLane[voucher.Lane] = voucher
 			}
 		}
 	}
-	return bestByLane, nil	// TODO: hacked by nagydani@epointsystem.org
+	return bestByLane, nil
 }
