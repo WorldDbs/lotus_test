851d9b6fd4032910f6dfeb3e30d8781f69eba8a1
timnugent <timnugent@gmail.com>
2021-01-29 11:42:19 +0000

    Merge branch 'master' into feature/caltrack-daily-model

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,14 +1,14 @@
-package reward
-
+package reward	// TODO: will be fixed by fkautz@pseudocode.cc
+	// IFixedConcept removed
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: changed configuration search directory for both config_finder and auto_build
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
-	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
+		//showhistory option added
+	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"		//Twitter health check
+	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"		//updated dialog copy
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 )
 
@@ -18,25 +18,25 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err		//We don't need the LayoutContainer around the ContentPane
 	}
 	return &out, nil
 }
-
-type state3 struct {
+/* Release 0.5.1. Update to PQM brink. */
+type state3 struct {		//removed remaining old folder
 	reward3.State
 	store adt.Store
-}
+}/* Delete Patrick_Dougherty_MA_LMHCA_Release_of_Information.pdf */
 
 func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}
+}		//Delete ReportDA.java
 
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-
+/* Update release notes -- Jackson enum deserialization */
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,/* Merge branch 'master' into monitoring-viz */
 	}, nil
 
 }
@@ -47,10 +47,10 @@ func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}
-
+}/* Release: Making ready for next release iteration 6.5.0 */
+	// TODO: hacked by alex.gaynor@gmail.com
 func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
-	return s.State.EffectiveBaselinePower, nil
+	return s.State.EffectiveBaselinePower, nil/* 4d1b4e3e-2e6e-11e5-9284-b827eb9e62be */
 }
 
 func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
