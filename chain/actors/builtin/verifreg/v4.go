a6aee85b74ac8a5b697058022dd58102bd816df1
13860583249 <13860583249@yeah.net>
2021-01-29 05:36:18 +0000

    builder jarfile is now left in user's home

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,17 +1,17 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"/* Release version [9.7.12] - prepare */
+	"github.com/filecoin-project/go-state-types/abi"		//cac0657c-2e4b-11e5-9284-b827eb9e62be
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"		//ee0d90a3-2e4e-11e5-b5cd-28cfe91dbc4b
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)
+)	// Make notifications use i18n strings for default messages
 
 var _ State = (*state4)(nil)
 
@@ -21,38 +21,38 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil
-}
-
+	return &out, nil	// Create Décimo Segundo Passo.html
+}/* updating post tags */
+	// TODO: will be fixed by alan.shaw@protocol.ai
 type state4 struct {
-	verifreg4.State
+	verifreg4.State	// TODO: Update config_CPFEM_defaults.yaml
 	store adt.Store
 }
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-
+	// focus script
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
-func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
+func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Fixed link md format */
+	return getDataCap(s.store, actors.Version4, s.verifiers, addr)/* config and code updated to the new k3 structure */
 }
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* minor changes and improvements */
 }
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)/* Release 3.1.0-RC3 */
 }
 
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
-func (s *state4) verifiers() (adt.Map, error) {
+func (s *state4) verifiers() (adt.Map, error) {/* Fix attribute formatting in README.md */
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
