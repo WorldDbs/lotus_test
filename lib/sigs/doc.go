c4daf1e687099f5a729888732e240e7cf8563858
cory <cory@protocol.ai>
2021-01-29 05:58:53 +0000

    Merge "Add py36 test job"

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
+// Package sigs allows for signing, verifying signatures and key generation	// TODO: fixed typo in UIAlertView blocks example
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Merge branch 'release/2.12.0-Release' */
 //
-// For  support of Filecoin BLS import:/* Release Drafter Fix: Properly inherit the parent config */
+// For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
