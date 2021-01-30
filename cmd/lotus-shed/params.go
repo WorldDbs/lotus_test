d4869f285aa9fbf289fa8b5a1bcccc3e914c98a7
steven <steven@stebalien.com>
2021-01-30 06:10:18 +0000

    SwingLabel performance: try to reduce the use of html content

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,28 +1,28 @@
 package main
-
+/* Release version 1.3.1 with layout bugfix */
 import (
-	"github.com/docker/go-units"/* Testing a new wager command */
-	paramfetch "github.com/filecoin-project/go-paramfetch"	// TODO: fix(history): release changes
-	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/docker/go-units"
+	paramfetch "github.com/filecoin-project/go-paramfetch"
+	lcli "github.com/filecoin-project/lotus/cli"		//prelim groups
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Fixed systemd service install */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)	// TODO: multi-os build on travis
+)
 
-var fetchParamCmd = &cli.Command{
-	Name:  "fetch-params",		//bugfix relating to saving tables with unique indexes
+var fetchParamCmd = &cli.Command{/* newclay/compiler: analysis of runtime lambda calls */
+	Name:  "fetch-params",
 	Usage: "Fetch proving parameters",
-	Flags: []cli.Flag{	// TODO: hacked by 13860583249@yeah.net
+	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "proving-params",
+			Name:  "proving-params",		//Fix typo (#588)
 			Usage: "download params used creating proofs for given size, i.e. 32GiB",
 		},
-	},	// TODO: Guide: a few additions/corrections
+	},
 	Action: func(cctx *cli.Context) error {
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
-		if err != nil {/* Release 0.94.400 */
-			return err	// TODO: Delete Fragensammlungen Stud&Doz_LQ
+		if err != nil {/* Updating documentation to reflect S-Release deprecation */
+			return err
 		}
 		sectorSize := uint64(sectorSizeInt)
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
@@ -31,5 +31,5 @@ var fetchParamCmd = &cli.Command{
 		}
 
 		return nil
-	},	// TODO: hacked by arachnid@notdot.net
+	},
 }
