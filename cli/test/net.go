95c2e602ba603737ac5067c3eccd036533bb12ed
igor <igor@soramitsu.co.jp>
2021-01-30 20:04:40 +0000

    Try to use latest jackson version

--- cli/test/net.go
+++ cli/test/net.go
@@ -1,75 +1,75 @@
 package test
 
 import (
-	"context"
+	"context"/* Hard-code the date to make sure the output is consistent over time */
 	"testing"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"/* checkContextAvailability can be final. */
+	"github.com/filecoin-project/lotus/chain/types"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api/test"	// Clean up for jacop
+	"github.com/filecoin-project/go-address"/* Remove "How do I see a single user's profile...?" */
+	"github.com/filecoin-project/lotus/api/test"
 	test2 "github.com/filecoin-project/lotus/node/test"
 )
 
-func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {	// TODO: Update lds.md
+func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {/* Create Contoh DMTS */
 	n, sn := test2.RPCMockSbBuilder(t, test.OneFull, test.OneMiner)
-
-	full := n[0]
+	// EconomyGamble - Check twice
+	full := n[0]/* Merge "Release 3.2.3.418 Prima WLAN Driver" */
 	miner := sn[0]
 
-	// Get everyone connected/* Some more OpenGL head bashing... */
+	// Get everyone connected
 	addrs, err := full.NetAddrsListen(ctx)
-	if err != nil {	// TODO: Moved game specific content to an own package, removed unused files.
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
-	}/* Release 0.1.5 with bug fixes. */
+	}
 
 	// Start mining blocks
 	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
-	bm.MineBlocks()/* Test EnvFactory */
-	t.Cleanup(bm.Stop)		//allow glsl files in examples (fixes #3716)
+	bm.MineBlocks()		//Added more padding
+	t.Cleanup(bm.Stop)
 
-	// Get the full node's wallet address
-	fullAddr, err := full.WalletDefaultAddress(ctx)/* Release v.1.4.0 */
-	if err != nil {
-		t.Fatal(err)
+	// Get the full node's wallet address/* Released version 0.8.12 */
+	fullAddr, err := full.WalletDefaultAddress(ctx)
+	if err != nil {/* renaming Sent Recipient Queue to "Sent To"  */
+		t.Fatal(err)/* install only for Release */
 	}
 
 	// Create mock CLI
-	return full, fullAddr		//template optimisation
-}/* Fixed AI attack planner to wait for full fleet. Release 0.95.184 */
+	return full, fullAddr/* initial layer widget work */
+}
 
 func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {
 	n, sn := test2.RPCMockSbBuilder(t, test.TwoFull, test.OneMiner)
-
+	// Merge branch 'master' into import-recipes-file
 	fullNode1 := n[0]
-	fullNode2 := n[1]
-	miner := sn[0]	// add usage of ConfigSection, fix section names with : inside, fix HTML errors
+	fullNode2 := n[1]/* add tyrannique */
+	miner := sn[0]
 
 	// Get everyone connected
 	addrs, err := fullNode1.NetAddrsListen(ctx)
-	if err != nil {
+	if err != nil {	// Merge "Fix the memory of the VM in VirtualBox"
 		t.Fatal(err)
-	}
+	}/* Add travis-ci build status badge to README */
 
 	if err := fullNode2.NetConnect(ctx, addrs); err != nil {
-)rre(lataF.t		
-	}/* Released v4.2.2 */
+		t.Fatal(err)
+	}
 
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
 
-	// Start mining blocks		//docs: Add HexChat to list of users
-	bm := test.NewBlockMiner(ctx, t, miner, blocktime)		//Merge "tests: use requests rather than httplib2"
+	// Start mining blocks
+	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
 	bm.MineBlocks()
 	t.Cleanup(bm.Stop)
-
+/* Release updates for 3.8.0 */
 	// Send some funds to register the second node
 	fullNodeAddr2, err := fullNode2.WalletNew(ctx, types.KTSecp256k1)
 	if err != nil {
@@ -77,7 +77,7 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 	}
 
 	test.SendFunds(ctx, t, fullNode1, fullNodeAddr2, abi.NewTokenAmount(1e18))
-
+/* Release version 0.2.2 to Clojars */
 	// Get the first node's address
 	fullNodeAddr1, err := fullNode1.WalletDefaultAddress(ctx)
 	if err != nil {
