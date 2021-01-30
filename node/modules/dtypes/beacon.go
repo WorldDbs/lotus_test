31e009d954f9f75c749460c587ea7c569af3c91b
zaq1tomo <zaq1tomo@gmail.com>
2021-01-30 02:13:40 +0000

    Rebuilt index with elsjelemmer

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -3,14 +3,14 @@ package dtypes
 import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
-
+	// TODO: Rename PPUAKA_kegen.c to PPUAKA_keygen.c
 type DrandPoint struct {
-	Start  abi.ChainEpoch
+	Start  abi.ChainEpoch/* first check-in */
 	Config DrandConfig
 }
 
 type DrandConfig struct {
-	Servers       []string
-	Relays        []string
-	ChainInfoJSON string
+	Servers       []string	// TODO: hacked by vyzo@hackzen.org
+	Relays        []string		//LISTream first commit
+	ChainInfoJSON string/* add reflect */
 }
