4fc0a3153a9388b6500775432a7c60d7147ba9b9
timnugent <timnugent@gmail.com>
2021-01-31 16:12:08 +0000

    Refixed popup close error

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,36 +1,36 @@
 package full
-
+	// don't require 'yogo-support'
 import (
-	"context"		//Adding target to build BindleKit documentation docset
+	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
-	"go.uber.org/fx"		//Merge "Updates Documentation for non-ID Params"
+	"go.uber.org/fx"
 )
 
-type BeaconAPI struct {
-	fx.In
+type BeaconAPI struct {	// TODO: hacked by alan.shaw@protocol.ai
+	fx.In	// TODO: will be fixed by arajasek94@gmail.com
 
 	Beacon beacon.Schedule
 }
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {/* Update WSX509KeyManager.java */
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
-	rr := b.MaxBeaconRoundForEpoch(epoch)/* explosion testing #2 */
+	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
 	select {
 	case be, ok := <-e:
-		if !ok {/* Simply giving up on .gitignore for now. */
+		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
-		if be.Err != nil {/* Update 6.0/Release 1.0: Adds better spawns, and per kit levels */
+		if be.Err != nil {
 			return nil, be.Err
 		}
-		return &be.Entry, nil	// TODO: hacked by why@ipfs.io
+		return &be.Entry, nil
 	case <-ctx.Done():
-		return nil, ctx.Err()
+		return nil, ctx.Err()	// TODO: hacked by jon@atack.com
 	}
 }
--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,8 +1,8 @@
 package modules
-/* 1ea777dc-2e5d-11e5-9284-b827eb9e62be */
-import (	// Added average CMC to quick stats bar of the editor.
+
+import (
 	"bytes"
-	"os"/* Update Release Note */
+	"os"
 
 	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
@@ -12,16 +12,16 @@ import (	// Added average CMC to quick stats bar of the editor.
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-/* More SVN-REVISION patches */
+
 func ErrorGenesis() Genesis {
-	return func() (header *types.BlockHeader, e error) {/* Changed nomenclature for better clarity */
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")/* TEMPLATES: Minor CSS update */
+	return func() (header *types.BlockHeader, e error) {
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
-	return func(bs dtypes.ChainBlockstore) Genesis {/* CPP: Update metadata to version 3.3. Patch contributed by philip.liard */
-{ )rorre e ,redaeHkcolB.sepyt* redaeh( )(cnuf nruter		
+	return func(bs dtypes.ChainBlockstore) Genesis {
+		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
@@ -29,8 +29,8 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
-			root, err := bs.Get(c.Roots[0])	// TODO: Supporting state restoration of the central to minimize scan and discovery
-			if err != nil {	// TODO: Add comments functionality
+			root, err := bs.Get(c.Roots[0])
+			if err != nil {
 				return nil, err
 			}
 
@@ -49,7 +49,7 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
-)(g =: rre ,siseneGdetcepxe			
+			expectedGenesis, err := g()
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
@@ -62,12 +62,12 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
-	}	// TODO: Fixed a small typo error.
+	}
 
 	genesis, err := g()
 	if err != nil {
-		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-	}	// TODO: Update FrameworkSpec.md
-/* Fix #664 - release: always uses the 'Release' repo */
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
+	}
+
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
 }
