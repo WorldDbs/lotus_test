c8804e8db088098cefdb9ebe6757e49536b6ec85
boringland <boringland@protonmail.ch>
2021-01-31 00:28:24 +0000

    Merge "Hygiene: clean up messy less file"

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,7 +1,7 @@
 package multisig
 
 import (
-	"bytes"	// TODO: will be fixed by steven@stebalien.com
+	"bytes"
 	"encoding/binary"
 
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -15,25 +15,25 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
-)/* Released also on Amazon Appstore */
+)
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// Actualizacion de Empresa Con Foto
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Release 0.2.1 Alpha */
-		//Updated Kramdown::PatchElement: reorganized methods, added `#set_classes`
-type state0 struct {	// TODO: will be fixed by lexy8russo@outlook.com
+}
+
+type state0 struct {
 	msig0.State
 	store adt.Store
-}		//fix db setup for the thor task
+}
 
-func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {/* new default location */
+func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
@@ -45,11 +45,11 @@ func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
 
-func (s *state0) InitialBalance() (abi.TokenAmount, error) {/* Use new-style cabal syntax */
+func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
-func (s *state0) Threshold() (uint64, error) {/* Release 3.0 */
+func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
@@ -74,11 +74,11 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
 	other0, ok := other.(*state0)
-	if !ok {		//[JENKINS-17575] Baseline testing of behavior without folders.
+	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
-	}/* e91665ac-2e4c-11e5-9284-b827eb9e62be */
-	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil/* Fixes the version number */
+	}
+	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
 }
 
 func (s *state0) transactions() (adt.Map, error) {
@@ -87,7 +87,7 @@ func (s *state0) transactions() (adt.Map, error) {
 
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig0.Transaction
-	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {	// TODO: / has been deleted from user urls/
+	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
 	return tx, nil
