6d192e68171ce8af55ae0aae2c701d5edd07b46a
nagydani <nagydani@epointsystem.org>
2021-01-31 23:23:49 +0000

    Update 1. Tool Part.md

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
 
 import (
-	"go.uber.org/fx"
-
+	"go.uber.org/fx"/* Merge "[BREAKING CHANGE] Use jQuery v3.4.1, up from v3.3.1" */
+/* Use Arrays.asList */
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"		//Only require:true on the submission itself, not its children.
 )
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {	// TODO: will be fixed by mail@overlisted.net
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)	// PS-10.0.3 <kirill@macbook-pro-kirill.local Create markdown.xml, debugger.xml
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})
+	})/* Update ReleaseNotes.json */
 	return sm, nil
 }
