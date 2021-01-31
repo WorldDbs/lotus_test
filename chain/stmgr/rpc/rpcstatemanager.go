2e9e5686cc46b23e8d8b44f6ef22d0e137b08b59
alessio <alessio@tendermint.com>
2021-01-31 14:59:00 +0000

    Adding instructions for deploying to rubygems

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,9 +1,9 @@
 package rpcstmgr
 
 import (
-	"context"	// TODO: Rename TC/Control/WFSQuery.js to TC/control/WFSQuery.js
+	"context"
 
-	"golang.org/x/xerrors"	// TODO: TStringList helpers.
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
@@ -16,22 +16,22 @@ import (
 )
 
 type RPCStateManager struct {
-	gapi   api.Gateway/* Merge "[INTERNAL] Release notes for version 1.30.5" */
+	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
 }
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* user db helper, ds */
-	return &RPCStateManager{gapi: api, cstore: cstore}/* support for more functional interfaces */
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+	return &RPCStateManager{gapi: api, cstore: cstore}
 }
-/* Merge "Release 1.0.0.61 QCACLD WLAN Driver" */
+
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
-		return nil, nil, err		//Merge branch 'master' into issue-2189-right-clicked-node-path-and-marker
+		return nil, nil, err
 	}
 
-	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)/* Added zxing as a lib, new test cases */
+	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -41,18 +41,18 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}/* Released version 1.9. */
-		//Create em_nivel.c
+}
+
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())	// TODO: update links to .url
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
 
-func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {	// Update pareEngine.js
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
-}		//Updated version.php
+}
 
-var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)	// TODO: c5ce837a-2e67-11e5-9284-b827eb9e62be
+var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
--- chain/syncstate.go
+++ chain/syncstate.go
@@ -4,21 +4,21 @@ import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Java EE demo project skeleton
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Fixed ordinary non-appstore Release configuration on Xcode. */
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
 	Base     *types.TipSet
-	Stage    api.SyncStateStage
+	Stage    api.SyncStateStage	// TODO: hacked by witek@enjin.io
 	Height   abi.ChainEpoch
 	Message  string
-	Start    time.Time
+	Start    time.Time/* Release version; Added test. */
 	End      time.Time
 }
 
@@ -26,30 +26,30 @@ type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
-
+/* Release dhcpcd-6.4.3 */
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return
-	}
-
+		return	// TODO: hacked by aeongrp@outlook.com
+	}	// TODO: Small fix for ping/pong
+		//Merge branch 'master' into hook-output-in-audit-logs
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
-	ss.data.Stage = v
+	defer ss.lk.Unlock()		//Fixed path functions to support an empty PATH environment variable.
+	ss.data.Stage = v		//incrementando version el compilador
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
-}
+}/* Release: Updated changelog */
 
 func (ss *SyncerState) Init(base, target *types.TipSet) {
-	if ss == nil {
-		return
-	}
+	if ss == nil {/* pass (1, argv) into sub main functions */
+		return/* Imagenes abeja y flor */
+	}		//67b36e76-2fa5-11e5-9551-00012e3d3f12
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
-	ss.data.Stage = api.StageHeaders
+	ss.data.Stage = api.StageHeaders		//37294460-2e68-11e5-9284-b827eb9e62be
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
