5ce353ee171e84f83cddb28cec669f1a2441d3f8
aeongrp <aeongrp@outlook.com>
2021-01-31 00:05:48 +0000

    c79739fc-2e73-11e5-9284-b827eb9e62be

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -4,7 +4,7 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("ffiwrapper")
+var log = logging.Logger("ffiwrapper")	// TODO: limitar escola para o admin
 
 type Sealer struct {
 	sectors  SectorProvider
