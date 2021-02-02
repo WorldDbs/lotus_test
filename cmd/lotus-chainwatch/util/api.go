4b734cdb77eacf6e53085a1b45faa7d1381f51bf
m-ou.se <m-ou.se@m-ou.se>
2021-02-02 14:20:56 +0000

    Satz0210 supports now Sparte 30, 50, 70

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -1,10 +1,10 @@
-package util
+package util/* version set to Release Candidate 1. */
 
 import (
 	"context"
 	"net/http"
 
-	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/go-jsonrpc"/* Merge "Release 3.2.3.421 Prima WLAN Driver" */
 	"github.com/filecoin-project/lotus/api/client"
 	"github.com/filecoin-project/lotus/api/v0api"
 	ma "github.com/multiformats/go-multiaddr"
@@ -14,21 +14,21 @@ import (
 func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	parsedAddr, err := ma.NewMultiaddr(listenAddr)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* greatly simplify update protocol */
 	}
-
-	_, addr, err := manet.DialArgs(parsedAddr)
+	// TODO: hacked by steven@stebalien.com
+	_, addr, err := manet.DialArgs(parsedAddr)/* Corregido validadorFormato para tener en cuenta campos obligatorios */
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* Update links in report text */
 	}
 
 	return client.NewFullNodeRPCV0(ctx, apiURI(addr), apiHeaders(token))
-}
+}/* fix(package): update react-google-charts to version 1.5.7 */
 func apiURI(addr string) string {
 	return "ws://" + addr + "/rpc/v0"
 }
 func apiHeaders(token string) http.Header {
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+token)
+	headers.Add("Authorization", "Bearer "+token)	// Update m08-lab.ipynb
 	return headers
 }
