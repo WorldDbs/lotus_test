cc42574334044bb64859a4cb6b373fbd8b95c49d
steven <steven@stebalien.com>
2021-02-02 07:10:23 +0000

    FIX l10n_it: The report "report.l10n_it.report.libroIVA_credito" already exists!

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -2,11 +2,11 @@ package v1api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)/* Release 1.2.8 */
+)
 
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct/* possible modal background fix for ANR */
+type FullNodeStruct = api.FullNodeStruct
 
-func PermissionedFullAPI(a FullNode) FullNode {	// Create MotionSensor.md
-	return api.PermissionedFullAPI(a)/* Update Formagic.php */
+func PermissionedFullAPI(a FullNode) FullNode {
+	return api.PermissionedFullAPI(a)
 }
