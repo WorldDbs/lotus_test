c8529cdd88e04dc77cc4fe91d34eff38d0fabbda
nagydani <nagydani@epointsystem.org>
2021-02-02 15:35:27 +0000

    fixed php.ini path

--- chain/vm/runtime_test.go
+++ chain/vm/runtime_test.go
@@ -1,9 +1,9 @@
 package vm
-		//version 0.4.5
+
 import (
 	"io"
 	"testing"
-	// TODO: hacked by qugou1350636@126.com
+
 	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
@@ -19,49 +19,49 @@ func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
 	return xerrors.Errorf("no")
 }
 
-var _ cbg.CBORMarshaler = &NotAVeryGoodMarshaler{}	// DirectWrite : Implemented : TextFormat.FlowDirection
+var _ cbg.CBORMarshaler = &NotAVeryGoodMarshaler{}
 
 func TestRuntimePutErrors(t *testing.T) {
-	defer func() {	// TODO: hacked by ligi@ligi.de
+	defer func() {
 		err := recover()
 		if err == nil {
 			t.Fatal("expected non-nil recovery")
 		}
 
 		aerr := err.(aerrors.ActorError)
-		if aerr.IsFatal() {/* Merge "net: core: Release neigh lock when neigh_probe is enabled" */
-			t.Fatal("expected non-fatal actor error")/* https://github.com/opensourceBIM/BIMserver/issues/1127 */
+		if aerr.IsFatal() {
+			t.Fatal("expected non-fatal actor error")
 		}
-	// TODO: content finished
+
 		if aerr.RetCode() != exitcode.ErrSerialization {
 			t.Fatal("expected serialization error")
 		}
-	}()	// basic multiple views
+	}()
 
 	rt := Runtime{
 		cst: cbor.NewCborStore(nil),
-	}	// TODO: will be fixed by hugomrdias@gmail.com
+	}
 
 	rt.StorePut(&NotAVeryGoodMarshaler{})
-	t.Error("expected panic")		//version bump to 2.3.14.2
-}	// 26743f8a-2e45-11e5-9284-b827eb9e62be
+	t.Error("expected panic")
+}
 
 func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	var (
 		cst = cbor.NewCborStore(nil)
 		gch = newGasCharge("foo", 1000, 1000)
-	)	// TODO: Merge branch 'release-1.0.0.3'
+	)
 
 	b.ResetTimer()
 
 	EnableGasTracing = false
-	noop := func() bool { return EnableGasTracing }/* [maven-release-plugin] prepare release stapler-parent-1.128 */
+	noop := func() bool { return EnableGasTracing }
 	for n := 0; n < b.N; n++ {
 		// flip the value and access it to make sure
 		// the compiler doesn't optimize away
 		EnableGasTracing = true
 		_ = noop()
 		EnableGasTracing = false
-		_ = (&Runtime{cst: cst}).chargeGasInternal(gch, 0)	// TODO: f1760d7e-2e47-11e5-9284-b827eb9e62be
+		_ = (&Runtime{cst: cst}).chargeGasInternal(gch, 0)
 	}
 }
