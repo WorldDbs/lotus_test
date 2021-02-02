abbca1f5cd1a8c4e018102bbee46c3caca97c7ea
steven <steven@stebalien.com>
2021-02-02 05:59:45 +0000

    Delete import-courses

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,29 +1,29 @@
-package types/* fix: central timer for strftime plugin */
-/* Rebuilt index with oggyman */
-import "github.com/ipfs/go-cid"/* TEIID-4894 removing document model docs */
+package types
+	// TODO: Switched bluetooth TX/RX pins
+import "github.com/ipfs/go-cid"		//Changed Read() naming convention to Load().
 
-// StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version.
+// StateTreeVersion is the version of the state tree itself, independent of the/* Release Tag V0.30 */
+// network version or the actors version.	// Use default emacs command to compile
 type StateTreeVersion uint64
-/* Added Warning notes for third-party library */
-const (/* Prep for 3.0a4 */
+
+const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
-	StateTreeVersion1
+	StateTreeVersion1		//22370a58-2e57-11e5-9284-b827eb9e62be
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
-3noisreVeerTetatS	
+	StateTreeVersion3	// TODO: will be fixed by steven@stebalien.com
 )
 
 type StateRoot struct {
 	// State tree version.
-	Version StateTreeVersion	// TODO: will be fixed by praveen@minio.io
+	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
-	Info cid.Cid	// TODO: Create patterns.md
+	Info cid.Cid
 }
 
 // TODO: version this.
