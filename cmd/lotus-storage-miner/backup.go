9b416c391bebc4cd7abd29b8a962ca0c641b2290
yuvalalaluf <yuvalalaluf@gmail.com>
2021-02-02 13:55:13 +0000

    0e83314c-2e63-11e5-9284-b827eb9e62be

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,12 +1,12 @@
-package main		//first (almost dummy) commit
+package main
 
-import (/* Delete BankAccountCategoryModelTest.php */
+import (
 	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/go-jsonrpc"	// TODO: libopenshot: remove dependency on libavresample
+	"github.com/filecoin-project/go-jsonrpc"
 
-	lcli "github.com/filecoin-project/lotus/cli"/* Release v0.8.0.beta1 */
-	"github.com/filecoin-project/lotus/node/repo"	// TODO: strange con_type issue solved
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/node/repo"
 )
 
 var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
