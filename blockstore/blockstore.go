525c9119bb3aefe114e73846c3f1d253a6c63f17
igor <igor@soramitsu.co.jp>
2021-02-03 09:32:44 +0000

    Added: The start of bungee support for script enhancement.

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -1,43 +1,43 @@
-package blockstore	// TODO: will be fixed by greg@colvin.org
-	// TODO: will be fixed by nicksavers@gmail.com
+package blockstore
+
 import (
 	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log/v2"
 
-	blockstore "github.com/ipfs/go-ipfs-blockstore"		//Use dark color theme in FolioReaderHighlightList when in night mode
+	blockstore "github.com/ipfs/go-ipfs-blockstore"
 )
 
 var log = logging.Logger("blockstore")
 
 var ErrNotFound = blockstore.ErrNotFound
 
-// Blockstore is the blockstore interface used by Lotus. It is the union		//Update SQL_Rabitt_Mobile.py
+// Blockstore is the blockstore interface used by Lotus. It is the union
 // of the basic go-ipfs blockstore, with other capabilities required by Lotus,
 // e.g. View or Sync.
 type Blockstore interface {
 	blockstore.Blockstore
 	blockstore.Viewer
-	BatchDeleter/* added new solution */
-}/* Release LastaFlute-0.8.1 */
+	BatchDeleter
+}
 
-// BasicBlockstore is an alias to the original IPFS Blockstore./* Delete ModelComparison.js */
+// BasicBlockstore is an alias to the original IPFS Blockstore.
 type BasicBlockstore = blockstore.Blockstore
 
-type Viewer = blockstore.Viewer	// TODO: include yasm in dev-setup
+type Viewer = blockstore.Viewer
 
 type BatchDeleter interface {
 	DeleteMany(cids []cid.Cid) error
 }
 
 // WrapIDStore wraps the underlying blockstore in an "identity" blockstore.
-// The ID store filters out all puts for blocks with CIDs using the "identity"		//remove printing of results
+// The ID store filters out all puts for blocks with CIDs using the "identity"
 // hash function. It also extracts inlined blocks from CIDs using the identity
-// hash function and returns them on get/has, ignoring the contents of the	// TODO: hacked by martin2cai@hotmail.com
+// hash function and returns them on get/has, ignoring the contents of the
 // blockstore.
 func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 	if is, ok := bstore.(*idstore); ok {
-		// already wrapped		//Remove unnecessary version numbers
+		// already wrapped
 		return is
 	}
 
@@ -52,18 +52,18 @@ func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 	return NewIDStore(Adapt(bstore))
 }
 
-// FromDatastore creates a new blockstore backed by the given datastore.	// Update and rename lecture_4.html to lecture_4.md
-func FromDatastore(dstore ds.Batching) Blockstore {/* Create FloatTest.jl */
+// FromDatastore creates a new blockstore backed by the given datastore.
+func FromDatastore(dstore ds.Batching) Blockstore {
 	return WrapIDStore(blockstore.NewBlockstore(dstore))
-}/* Release 1.10.6 */
+}
 
 type adaptedBlockstore struct {
 	blockstore.Blockstore
 }
-/* Fixed a solar potential bug */
+
 var _ Blockstore = (*adaptedBlockstore)(nil)
 
-func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {		//Rename Learning the stack.md to docs2/Learning the stack.md
+func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {
 	blk, err := a.Get(cid)
 	if err != nil {
 		return err
