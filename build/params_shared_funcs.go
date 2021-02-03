6f9367a337a76477a5d1da7c16b4beaad6a0f4b8
mail <mail@overlisted.net>
2021-02-03 17:34:58 +0000

    automated commit from rosetta for sim/lib build-an-atom, locale lt

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,7 +1,7 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"		//Merge "Padding between date and digital clock" into jb-mr1.1-dev
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
@@ -9,32 +9,32 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-// Core network constants/* 5b39e8da-2e60-11e5-9284-b827eb9e62be */
+// Core network constants
 
-func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }	// TODO: hacked by ng8eke@163.com
+func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
 
 func SetAddressNetwork(n address.Network) {
-	address.CurrentNetwork = n/* update anaphora resolution to check first features and second roles */
-}	// TODO: Candidate for less CPU intensive threading.
+	address.CurrentNetwork = n
+}
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
-	}/* Fix coderwall link */
-		//New category added
+	}
+
 	return ret
 }
 
-func MustParseCid(c string) cid.Cid {/* Micro markup cleanup in issue base template */
-	ret, err := cid.Decode(c)/* Delete mask_fasta.py */
+func MustParseCid(c string) cid.Cid {
+	ret, err := cid.Decode(c)
 	if err != nil {
 		panic(err)
 	}
 
 	return ret
-}		//+ Patch 2995672: Infantry armor from BLK
+}
