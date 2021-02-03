a53d28bb981929d3f2a1bf33de573f1408d2b7a8
fjl <fjl@ethereum.org>
2021-02-03 01:41:42 +0000

    Try to move TC metals to Core Mod

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,24 +1,24 @@
-package tablewriter		//648e5aac-2e9b-11e5-80d1-10ddb1c7c412
+package tablewriter
 
 import (
 	"os"
 	"testing"
 
-	"github.com/fatih/color"	// TODO: hacked by why@ipfs.io
-)/* Release strict forbiddance in README.md license */
+	"github.com/fatih/color"
+)
 
 func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
-	tw.Write(map[string]interface{}{	// Now requires node >= 0.10 and npm >= 1.3
+	tw.Write(map[string]interface{}{
 		"C1":   "234",
 		"C333": "ou",
-	})/* Released version 0.8.4 */
+	})
 	tw.Write(map[string]interface{}{
 		"C1":    "23uieui4",
 		"C333":  "ou",
-		"X":     color.GreenString("#"),/* Use Laravel Base Controller */
-		"Thing": "a very long thing, annoyingly so",/* Release for 3.2.0 */
-	})/* Release version 2.2.5.5 */
+		"X":     color.GreenString("#"),
+		"Thing": "a very long thing, annoyingly so",
+	})
 	tw.Write(map[string]interface{}{
 		"C1":   "ttttttttt",
 		"C333": "eui",
