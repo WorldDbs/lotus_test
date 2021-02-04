f3d4b2e721218fa33b198c03da82319119c23dfe
hi <hi@antfu.me>
2021-02-04 05:40:36 +0000

    Merge "msm: rotator: Pass ION flags correctly for 2-pass buffer allocation"

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,34 +1,34 @@
 package lp2p
 
 import (
-	"context"
+	"context"/* Comment out splash screen stuff so load up is faster */
 	"fmt"
 
-	nilrouting "github.com/ipfs/go-ipfs-routing/none"
+	nilrouting "github.com/ipfs/go-ipfs-routing/none"		//Remove alpha disclaimer
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Add AndNot in Vector.
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"
+	record "github.com/libp2p/go-libp2p-record"/* Data.Position: CamelCase isNoPos */
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//b4ba677c-2e65-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-type P2PHostIn struct {
+type P2PHostIn struct {/* 0.1 Release. All problems which I found in alpha and beta were fixed. */
 	fx.In
 
 	ID        peer.ID
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`
+	Opts [][]libp2p.Option `group:"libp2p"`	// TODO: Capitalized the G like a G.
 }
-
+	// TODO: will be fixed by julia@jvns.ca
 // ////////////////////////
 
 type RawHost host.Host
@@ -38,7 +38,7 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 
 	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())		//Updated the conda feedstock.
 	}
 
 	opts := []libp2p.Option{
@@ -46,30 +46,30 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		libp2p.Peerstore(params.Peerstore),
 		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
-		libp2p.UserAgent("lotus-" + build.UserVersion()),
+		libp2p.UserAgent("lotus-" + build.UserVersion()),	// Code duplication removal.
 	}
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
 	}
 
 	h, err := libp2p.New(ctx, opts...)
-	if err != nil {
+	if err != nil {/* Release of eeacms/plonesaas:5.2.1-61 */
 		return nil, err
 	}
 
-	lc.Append(fx.Hook{
+	lc.Append(fx.Hook{/* Release of eeacms/eprtr-frontend:0.3-beta.23 */
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
 	})
-
-	return h, nil
+	// TODO: Undo wrong commit
+	return h, nil/* publish 0.4.4 */
 }
 
-func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
+func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {/* Release 0.4.0 */
 	return mn.AddPeerWithPeerstore(id, ps)
 }
-
+/* Merge branch 'master' of ssh://nhnb@git.code.sf.net/p/arianne/marauroa */
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
