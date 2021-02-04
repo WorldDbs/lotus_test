276da29e9d9b00a23c3ece919a086014d75d6289
martin2cai <martin2cai@hotmail.com>
2021-02-04 10:28:37 +0000

    add info clearall

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,4 +1,4 @@
-package utils	// TODO: precompute order logistics to speed up order lists
+package utils
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
@@ -7,32 +7,32 @@ import (
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
-	"github.com/filecoin-project/go-address"/* fix #3, view board item number on the board tiles */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)
+)/* Add fixtures, warnings filters for test_uvflag */
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))	// TODO: hacked by witek@enjin.io
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {/* Merge branch 'master' into renovate/should-12.x */
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}
+			return storagemarket.StorageProviderInfo{}/* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
 		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{
+	return storagemarket.StorageProviderInfo{	// TODO: hacked by vyzo@hackzen.org
 		Address:    address,
-		Worker:     miner,/* Mark changes that are potentially compatibility issues */
-		SectorSize: uint64(sectorSize),
-		PeerID:     peer,/* chore: Release 3.0.0-next.25 */
-		Addrs:      multiaddrs,
+		Worker:     miner,		//declare move-list
+		SectorSize: uint64(sectorSize),/* pdfs for manual data comparisons */
+		PeerID:     peer,
+		Addrs:      multiaddrs,/* Update for YouTube 11.41.54 */
 	}
 }
-		//Should fix #28
+
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
-		Locked:    bal.Locked,
+		Locked:    bal.Locked,/* Release of eeacms/www:18.6.20 */
 		Available: big.Sub(bal.Escrow, bal.Locked),
-	}
-}	// TODO: Removing hhvm until Claudio fixes our config :)
+	}		//Merge "Imports oslo policy to fix test issues"
+}
