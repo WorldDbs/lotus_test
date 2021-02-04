035b42b53c89259331aae95ff76d7d306f31a111
xaber.twt <xaber.twt@gmail.com>
2021-02-04 08:31:44 +0000

    Explanation as to what this file is for.

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,24 +1,24 @@
 package tablewriter
 
 import (
-	"os"
+	"os"/* Merge "[Release] Webkit2-efl-123997_0.11.9" into tizen_2.1 */
 	"testing"
 
 	"github.com/fatih/color"
 )
 
-func TestTableWriter(t *testing.T) {
-	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
+func TestTableWriter(t *testing.T) {	// TODO: Added list of compatible IDEs
+	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))/* new option: "-tabview" to force modular layouts shown in tabs */
 	tw.Write(map[string]interface{}{
 		"C1":   "234",
 		"C333": "ou",
-	})
+	})/* Merge "Fix benchmark output enabling in plugin" into androidx-master-dev */
 	tw.Write(map[string]interface{}{
 		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
 		"Thing": "a very long thing, annoyingly so",
-	})
+	})/* Repair some nonsenses  */
 	tw.Write(map[string]interface{}{
 		"C1":   "ttttttttt",
 		"C333": "eui",
