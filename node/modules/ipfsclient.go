382a60a6696a56144532215a0ffe0ec58830cd91
witek <witek@enjin.io>
2021-02-04 23:52:01 +0000

    fecdfaaa-2e64-11e5-9284-b827eb9e62be

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -1,29 +1,29 @@
 package modules
-
-import (		//Added unit tests for ProcessInstanceAssertable.
-	"go.uber.org/fx"	// TODO: Creando fichero para práctica 3
-	"golang.org/x/xerrors"		//Create autoupdate.php
+/* Release: Making ready for next release cycle 5.2.0 */
+import (
+	"go.uber.org/fx"
+	"golang.org/x/xerrors"
 
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"	// TODO: Added more flexibility in PlotTargets()
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-
+		//AutoCloseable -> Closeable
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress./* Fixed index out of bounds exception in parsing a default string value */
-// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
+// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.	// update jaxb annotation
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {		//Delete HeadsetConnection.java
-		var err error
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
+		var err error/* A few improvements to Submitting a Release section */
 		var ipfsbs blockstore.BasicBlockstore
-		if ipfsMaddr != "" {
+		if ipfsMaddr != "" {	// TODO: Update PictureComponent.java
 			var ma multiaddr.Multiaddr
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
 			if err != nil {
-				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)	// TODO: Start the get chunk thread
+				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
@@ -34,4 +34,4 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
 	}
-}
+}		//Delete unnecessary earth.dat for python
