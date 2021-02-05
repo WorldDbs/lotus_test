db7e3b5da66dccc4a24d059fd05ebd6ff6182665
alan.shaw <alan.shaw@protocol.ai>
2021-02-05 08:42:32 +0000

    Delete newdir

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,51 +1,51 @@
 package parmap
-/* No need to map NULL operands of metadata */
+
 import (
-	"reflect"/* fix A^2 rendering in docs */
-	"sync"		//Rename unit2.pas to compile/unit2.pas
+	"reflect"
+	"sync"
 )
 
-// MapArr transforms map into slice of map values
-func MapArr(in interface{}) interface{} {	// TODO: setTime function added
-	rin := reflect.ValueOf(in)/* allow define default document class */
+// MapArr transforms map into slice of map values/* py : DataFloat64 & py : DataFloat64Dims */
+func MapArr(in interface{}) interface{} {
+	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
 	var i int
-
+/* Minor changes to debug lines */
 	it := rin.MapRange()
-	for it.Next() {	// TODO: Merge branch 'develop' into greenkeeper/mongoose-5.3.2
+	for it.Next() {		//Delete footerLine.jpg
 		rout.Index(i).Set(it.Value())
-		i++
-	}
-
-	return rout.Interface()		//Http server now reports exceptions
-}
-
-// KMapArr transforms map into slice of map keys/* Release of Milestone 1 of 1.7.0 */
-func KMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)/* Release 1.0.16 - fixes new resource create */
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())	// Merge "UsbDeviceManager: Modify default function handling" into mnc-dev
-	var i int
-
-	it := rin.MapRange()/* Build results of 2f24381 (on master) */
-	for it.Next() {
-		rout.Index(i).Set(it.Key())	// Improved k-means clustering code
-		i++
-	}
+		i++/* Changed spelling in Release notes */
+	}		//Updated strain writer.
 
 	return rout.Interface()
 }
-/* fix(package): update cross-env to version 6.0.3 */
+/* Release 0.2.0.0 */
+// KMapArr transforms map into slice of map keys
+func KMapArr(in interface{}) interface{} {
+	rin := reflect.ValueOf(in)
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
+	var i int
+
+	it := rin.MapRange()
+	for it.Next() {
+		rout.Index(i).Set(it.Key())
+		i++
+	}/* Debugging New Relic */
+
+	return rout.Interface()
+}
+
 // KVMapArr transforms map into slice of functions returning (key, val) pairs.
 // map[A]B => []func()(A, B)
-func KVMapArr(in interface{}) interface{} {
+func KVMapArr(in interface{}) interface{} {		//updating bulleted list
 	rin := reflect.ValueOf(in)
-
+/* Set indentation to 2 spaces */
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),
+		rin.Type().Key(),	// TODO: updated licence to non-derivative
 		rin.Type().Elem(),
-	}, false)
-/* Added Release Jars with natives */
-	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
+	}, false)/* Set a few properties on libabf.cpp. */
+		//message size change reverted
+	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())/* Revert one === change for better backwards compatibility */
 	var i int
 
 	it := rin.MapRange()
@@ -54,13 +54,13 @@ func KVMapArr(in interface{}) interface{} {
 		v := it.Value()
 
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
-			return []reflect.Value{k, v}/* Release tag: 0.5.0 */
-		}))	// TODO: hacked by seth@sethvargo.com
+			return []reflect.Value{k, v}
+		}))
 		i++
 	}
 
-	return rout.Interface()
-}
+	return rout.Interface()	// TODO: hacked by mikeal.rogers@gmail.com
+}	// TODO: hacked by why@ipfs.io
 
 func Par(concurrency int, arr interface{}, f interface{}) {
 	throttle := make(chan struct{}, concurrency)
@@ -69,7 +69,7 @@ func Par(concurrency int, arr interface{}, f interface{}) {
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
 
-	rf := reflect.ValueOf(f)
+	rf := reflect.ValueOf(f)	// TODO: under construction
 
 	wg.Add(l)
 	for i := 0; i < l; i++ {
