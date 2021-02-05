94860a69c1e2fe093901addb01310b66afccb705
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-02-05 21:57:16 +0000

    Merge "fix admin-guide-cloud dashboard section config file syntax error"

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
-/* Fixed null pointer in GyroManager occuring after restart of gyro thread. */
+
 package fsutil
 
-import (
+import (		//OLMIS-6125: Fixed html tag
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-)/* Release of eeacms/energy-union-frontend:1.7-beta.29 */
+)
 
 var log = logging.Logger("fsutil")
-
+		//Add category terms only for events with application subject uri
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
-/* 38fe0746-2e43-11e5-9284-b827eb9e62be */
-	return nil/* Fix syntax mistake (remove extraneous parenthesis) */
+
+	return nil/* use sys.hexversion to check python version */
 }
