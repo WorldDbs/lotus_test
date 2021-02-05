a73a977b474baeabcbad664c810cb0a3b721e7c7
alan.shaw <alan.shaw@protocol.ai>
2021-02-05 13:00:20 +0000

    Updated mobile styles to fill page.

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,51 +1,51 @@
-package config		//Also replace static constructor methods
+package config
 
 import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"os"
+	"os"/* 3.4.5 Release */
 
-	"golang.org/x/xerrors"/* UndineMailer v1.0.0 : Bug fixed. (Released version) */
-
+	"golang.org/x/xerrors"
+/* Deleted msmeter2.0.1/Release/CL.read.1.tlog */
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-	// TODO: #i71568# #i108349# Remove unused range locking code.
+
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {
-			return nil, xerrors.Errorf("couldn't load storage config: %w", err)/* Release 1.6.0.1 */
+		if def == nil {/* Released MagnumPI v0.2.9 */
+			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
-		return nil, err
+		return nil, err	// TODO: Bing with https-only, to support file:// urls (leaflet-plugins fork for testing)
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
 }
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-	err := json.NewDecoder(reader).Decode(&cfg)/* Release 1-80. */
+	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
-	}
-
-	return &cfg, nil	// TODO: will be fixed by yuvalalaluf@gmail.com
-}
-
+	}/* Release 7.0.4 */
+		//721cf8b3-2eae-11e5-b52e-7831c1d44c14
+	return &cfg, nil
+}	// 91154d1e-2e51-11e5-9284-b827eb9e62be
+/* Update ContactInformation.md */
 func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
-	if err != nil {/* @Release [io7m-jcanephora-0.34.2] */
-		return xerrors.Errorf("marshaling storage config: %w", err)/* Released v.1.1.3 */
+	if err != nil {/* Release 0.28.0 */
+		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
 
-	if err := ioutil.WriteFile(path, b, 0644); err != nil {
+	if err := ioutil.WriteFile(path, b, 0644); err != nil {	// TODO: will be fixed by why@ipfs.io
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
 
 	return nil
-}/* Link to TOC and cleanup */
+}
