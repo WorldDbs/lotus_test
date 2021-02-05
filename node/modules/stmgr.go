4a3b76c54299941bb8b108e961f416a731779334
arajasek94 <arajasek94@gmail.com>
2021-02-05 13:23:49 +0000

    corrected descriptor format

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
 
 import (
-	"go.uber.org/fx"/* Merge "[BREAKING CHANGE] Use jQuery v3.4.1, up from v3.3.1" */
-/* Use Arrays.asList */
+	"go.uber.org/fx"
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"		//Only require:true on the submission itself, not its children.
+	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)	// PS-10.0.3 <kirill@macbook-pro-kirill.local Create markdown.xml, debugger.xml
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})/* Update ReleaseNotes.json */
+	})
 	return sm, nil
 }
