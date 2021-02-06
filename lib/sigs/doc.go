043d2910a9e223d9e41b2c7b8a06c593b1d25560
arajasek94 <arajasek94@gmail.com>
2021-02-06 21:13:30 +0000

    - bugfix for install target

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation	// TODO: fixed typo in UIAlertView blocks example
+// Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
-//
+///* Нарисован новый шаблон карточки товара */
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Merge branch 'release/2.12.0-Release' */
-//
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"		//Stoch info
+//		//6f12967c-2e6a-11e5-9284-b827eb9e62be
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+package sigs	// TODO: Class and packages renamed
