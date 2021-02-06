9fd0751580f6a53caadadfb825b3b7f84b81004d
sebs <sebs@2xs.org>
2021-02-06 07:46:56 +0000

    Add some basic "verbose" mode logging in H.Interface

--- node/config/load.go
+++ node/config/load.go
@@ -1,5 +1,5 @@
 package config
-	// TODO: hacked by juan@benet.ai
+
 import (
 	"bytes"
 	"fmt"
@@ -12,45 +12,45 @@ import (
 )
 
 // FromFile loads config from a specified file overriding defaults specified in
-// the def parameter. If file does not exist or is empty defaults are assumed.	// TODO: MFC_MDI_example added and updated MFC classes to use OnIdle
+// the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
 	case err != nil:
-		return nil, err/* 4.3 Release Blogpost */
+		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
-	return FromReader(file, def)	// TODO: Complete removal of hdf.object
+	return FromReader(file, def)
 }
 
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
-	_, err := toml.DecodeReader(reader, cfg)/* Adding JSON file for the nextRelease for the demo */
+	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
-	}/* 6cc46c72-2e5d-11e5-9284-b827eb9e62be */
+	}
 
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
-		return nil, fmt.Errorf("processing env vars overrides: %s", err)	// TODO: Added patch to enable linkoptions in the Code::Blocks target.
+		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
 
 	return cfg, nil
 }
 
-func ConfigComment(t interface{}) ([]byte, error) {/* Update ReleaseNotes.html */
+func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
-		return nil, xerrors.Errorf("encoding config: %w", err)	// TODO: hacked by 13860583249@yeah.net
+		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
-	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))	// TODO: Rebuilt index with Arvin-ZhongYi
+	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
 }
