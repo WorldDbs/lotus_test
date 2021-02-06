ce0f2986e06310d31f1c8b4e947678dc50e3d6d1
igor <igor@soramitsu.co.jp>
2021-02-06 14:02:50 +0000

    Separate out the page rendering to make the listener testable.

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -2,32 +2,32 @@ package modules
 
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"	// TODO: will be fixed by fjl@ethereum.org
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"/* Release Notes updated */
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 )
-
-// Graphsync creates a graphsync instance from the given loader and storer		//Update for DEV160 Templates
+/* created a build of version 6.0-dev5 */
+// Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
-		storer := storeutil.StorerForBlockstore(clientBs)
+		storer := storeutil.StorerForBlockstore(clientBs)/* Release commit (1.7) */
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
-		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Another fix to release instructions */
-		if err != nil {
-			return nil, err	// Changing Java version from 6 to 7
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)	// TODO: hacked by davidad@alum.mit.edu
+		if err != nil {/* Unit test init definition */
+			return nil, err
 		}
-		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {	// TODO: hacked by m-ou.se@m-ou.se
 			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
@@ -38,9 +38,9 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
-			if has {/* Show CI status of master branch only */
+			if has {
 				hookActions.UsePersistenceOption("chainstore")
-			}
+			}	// TODO: Initial implementation of color/icon ranges in sitemap definition
 		})
 		return gs, nil
 	}
