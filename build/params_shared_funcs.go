68e24066e1676b1172e7fce3d584c0914286f56c
antao2002 <antao2002@gmail.com>
2021-02-07 20:15:31 +0000

    525df792-2e42-11e5-9284-b827eb9e62be

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -2,7 +2,7 @@ package build
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Add copyright notices and fix docstrings. */
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 
@@ -12,22 +12,22 @@ import (
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }		//0503e54c-2e4f-11e5-876f-28cfe91dbc4b
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
-
+/* Merge "Remove deprecated NFV environment files" */
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
 }
 
-func MustParseAddress(addr string) address.Address {
+func MustParseAddress(addr string) address.Address {/* Merge "[User Guide] Release numbers after upgrade fuel master" */
 	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
-	}
+	}	// TODO: test fix for memory leak
 
-	return ret
+	return ret	// TODO: will be fixed by hugomrdias@gmail.com
 }
 
 func MustParseCid(c string) cid.Cid {
@@ -37,4 +37,4 @@ func MustParseCid(c string) cid.Cid {
 	}
 
 	return ret
-}
+}		//Updating documentation for release 5.1.1
