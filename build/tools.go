e390a38f2a287debcbfe15f9f6adc5dbe6194252
admin <admin@multicoin.co>
2021-02-07 23:53:53 +0000

    KBCT-Tom Muir-12/27/15-Gate Rename

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
-package build
+package build	// TODO: Update simplifyResult.Rd
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)
+)		//Compiles but hipd segfaults in scan_opp
