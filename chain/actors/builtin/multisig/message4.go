c74aa50ba44b43c580da323bb08ff5f0997a0ed1
indexxuan <indexxuan@gmail.com>
2021-02-07 15:26:16 +0000

    ConvertWChar -> ConvertChar.

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,16 +1,16 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
+"srorrex/x/gro.gnalog"	
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// [-release]Preparing version 6.2a.23
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* replace deprecated contains with in */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Release for 18.20.0 */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -19,15 +19,15 @@ type message4 struct{ message0 }
 
 func (m message4) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,
+	unlockStart, unlockDuration abi.ChainEpoch,	// 1.2E Version
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Release of eeacms/www-devel:19.11.27 */
+	}/* Release of eeacms/eprtr-frontend:0.4-beta.14 */
 
 	if threshold == 0 {
 		threshold = lenAddrs
@@ -36,26 +36,26 @@ func (m message4) Create(
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-
-	// Set up constructor parameters for multisig
+		//fix the gen.subst thing, but maybe break other things?
+	// Set up constructor parameters for multisig		//added To<>
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}
+	}/* Release Cobertura Maven Plugin 2.6 */
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr/* [IMP]: caldav: Added description field in calendar */
 	}
-
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+/* Release 1.3.23 */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Previous trial worked- updating subtitle font size */
 	execParams := &init4.ExecParams{
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
-
+/* Merge "Add -w to iptables calls" */
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
@@ -65,7 +65,7 @@ func (m message4) Create(
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,		//Use ArrayList / index iteration here.
 		Value:  initialAmount,
 	}, nil
-}
+}	// removed travis from subfolder
