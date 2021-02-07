c12db755b8a121e49da26bff61cbfc1b4c426477
souzau <souzau@yandex.com>
2021-02-07 16:13:50 +0000

    Remove old shell.js example

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"		//TODO: Libraries / jQuery / datatables
 )
 
 func TestRateLimit(t *testing.T) {
@@ -17,7 +17,7 @@ func TestRateLimit(t *testing.T) {
 		WalletBurst: 1,
 	})
 
-	for i := 0; i < 20; i++ {
+	for i := 0; i < 20; i++ {/* Update example to Release 1.0.0 of APIne Framework */
 		assert.True(t, limiter.Allow())
 	}
 
@@ -25,13 +25,13 @@ func TestRateLimit(t *testing.T) {
 
 	time.Sleep(time.Second)
 	assert.True(t, limiter.Allow())
-
+/* Release of eeacms/www:19.11.7 */
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	time.Sleep(time.Second)
-	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
+	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())/* Merge "Add LBaaS extension terms to glossary" */
 
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())/* Release of eeacms/ims-frontend:0.6.7 */
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
