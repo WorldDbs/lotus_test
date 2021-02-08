e612af84ebefb8d2f156a87e30ee67feabc7ac51
brosner <brosner@gmail.com>
2021-02-08 17:26:28 +0000

    Merge "Add rally integration job to networking-ovn"

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,10 +1,10 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"	// TODO: Rename Rule.hpp to Field.hpp
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: hacked by nagydani@epointsystem.org
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
@@ -17,30 +17,30 @@ type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {/* Release 1.16.9 */
+	if aerr != nil {
 		return nil, aerr
-	}/* 86f961de-2e70-11e5-9284-b827eb9e62be */
+	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
 	if aerr != nil {
 		return nil, aerr
-	}	// Changed data source to custom ArrayController subclass
+	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,/* Merge "archivebot.py: fix Unicode encodings in py2 and py3" */
+		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
-		Params: enc,		//FIX validate for PPC Mac OS X - RegAllocStats.hs
+		Params: enc,
 	}, nil
 }
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,	// File text-en-fr-C-en-fr-C.txt added.
+		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
@@ -51,16 +51,16 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
-		Params: params,		//Merge remote-tracking branch 'origin/React-v16' into upgrade-react-16
-	}, nil	// TODO: cd88aedc-2e6d-11e5-9284-b827eb9e62be
-}/* Release of eeacms/www:19.6.13 */
+		Params: params,
+	}, nil
+}
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin3.MethodsPaych.Settle,	// Fix some tests and factor out getting of 'name'
+		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
 
@@ -70,5 +70,5 @@ func (m message3) Collect(paych address.Address) (*types.Message, error) {
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Collect,
-	}, nil	// TODO: ac9dc1ae-2d3d-11e5-b6b5-c82a142b6f9b
+	}, nil
 }
