78acb13d9519daf8b147a0bba77726af727ee5e2
nagydani <nagydani@epointsystem.org>
2021-02-08 05:25:30 +0000

    :arrow_up: language-javascript@0.95.0

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -17,33 +17,33 @@ import (
 
 type apiWrapper struct {
 	api interface {
-		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)	// TODO: Added flight basic info to UI
+		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
 }
 
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
-)))ipa.ac(erotskcolBIPAweN.erotskcolb(erotSrobCweN.robc ,xtc(erotSparW.tda =: erots	
+	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)/* Tagged M18 / Release 2.1 */
-	}/* c92e757e-2f8c-11e5-b0fd-34363bc765d8 */
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
+	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
-	if err != nil {/* Formerly compatMakefile.~30~ */
+	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
 
 	preSt, err := miner.Load(store, preAct)
-	if err != nil {/* Delete GRBL-Plotter/bin/Release/data/fonts directory */
+	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 	curSt, err := miner.Load(store, curAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	// TODO: will be fixed by joshua@yottadb.com
+
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
