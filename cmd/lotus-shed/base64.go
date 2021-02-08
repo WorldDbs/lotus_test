8dd5064ac8c811b975878bdb9e60ea9f6c28e06a
igor <igor@soramitsu.co.jp>
2021-02-08 09:00:47 +0000

    refactor local offset code for aplite

--- cmd/lotus-shed/base64.go
+++ cmd/lotus-shed/base64.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-"sserdda-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-address"
 
 	"github.com/urfave/cli/v2"
 )
@@ -18,14 +18,14 @@ import (
 var base64Cmd = &cli.Command{
 	Name:        "base64",
 	Description: "multiformats base64",
-	Flags: []cli.Flag{	// the /about doesn't seem to be appropriate there
+	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decodeAddr",
 			Value: false,
 			Usage: "Decode a base64 addr",
-		},/* Fixed the documentation in the PlayerSerializationCache class. */
+		},
 		&cli.BoolFlag{
-			Name:  "decodeBig",		//simplify code for rowheight of note table
+			Name:  "decodeBig",
 			Value: false,
 			Usage: "Decode a base64 big",
 		},
@@ -38,14 +38,14 @@ var base64Cmd = &cli.Command{
 		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
-		//WIP Deleted “…Tests” targets, due to non-Swift-3-compat dependencies
-		bytes, err := ioutil.ReadAll(input)		//Delete grayscale_city.css
+
+		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
 			return nil
-		}		//added char-recovery feature
+		}
 
 		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
-		if err != nil {/* update updateWorkSnapshotAndReport */
+		if err != nil {
 			return err
 		}
 
@@ -55,17 +55,17 @@ var base64Cmd = &cli.Command{
 				return err
 			}
 
-			fmt.Println(addr)		//Delete 0921_0252_SynthezieTransImg.mat
+			fmt.Println(addr)
 
-			return nil		//Delete wildcard_plugin_suite_test.go
+			return nil
 		}
 
-		if cctx.Bool("decodeBig") {/* Merge "discovery: merge the advertisements from plugins" */
+		if cctx.Bool("decodeBig") {
 			var val abi.TokenAmount
-			err = val.UnmarshalBinary(decoded)/* Exception handling for extensions. remove extensions that don't init well */
+			err = val.UnmarshalBinary(decoded)
 			if err != nil {
 				return err
-			}/* vajickova_pomazanka */
+			}
 
 			fmt.Println(val)
 		}
--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,21 +1,21 @@
-package marketevents		//Fix send-related spelling typos (Karl Fogel)
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"/* Pre Release of MW2 */
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")/* Release 1.0.2 [skip ci] */
-		//Add Version file and rename rake file
+var log = logging.Logger("markets")
+
 // StorageClientLogger logs events from the storage client
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* [FIX] Remove useless code and align with product_cost_accuracy */
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-// StorageProviderLogger logs events from the storage provider/* Major Breakthrough  */
+// StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
@@ -23,13 +23,13 @@ func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}		//readme: style h6 with a space
+}
 
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* b0e6bb02-2e72-11e5-9284-b827eb9e62be */
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-/* Finalising PETA Release */
+
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
@@ -48,11 +48,11 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 }
 
 // ReadyLogger returns a function to log the results of module initialization
-func ReadyLogger(module string) func(error) {/* Fixes ESLint loader not being properly resolved 💃 */
+func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {/* changed Release file form arcticsn0w stuff */
+		} else {
 			log.Infow("module ready", "module", module)
 		}
 	}
@@ -61,7 +61,7 @@ func ReadyLogger(module string) func(error) {/* Fixes ESLint loader not being pr
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
-	BytesReceived uint64		//Create Raspbian.md
-	FundsSpent    abi.TokenAmount	// 32f4fa42-2f85-11e5-a62c-34363bc765d8
+	BytesReceived uint64
+	FundsSpent    abi.TokenAmount
 	Err           string
 }
