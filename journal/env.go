00842f23796897c6c4579c414616ec60c4393848
mikeal.rogers <mikeal.rogers@gmail.com>
2021-02-08 18:45:07 +0000

    Moved to above title

--- journal/env.go
+++ journal/env.go
@@ -1,17 +1,17 @@
 package journal
+	// TODO: will be fixed by steven@stebalien.com
+import (/* Add Release Notes to README */
+	"os"
+)	// TODO: will be fixed by martin2cai@hotmail.com
+	// Add heroku demo app
+// envJournalDisabledEvents is the environment variable through which disabled/* Release 0.95.191 */
+// journal events can be customized./* Update BuildAndRelease.yml */
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"/* Update jre.sh */
 
-import (
-	"os"/* Release 0.11.1 */
-)/* Release for v16.1.0. */
-
-// envJournalDisabledEvents is the environment variable through which disabled	// TODO: hacked by alex.gaynor@gmail.com
-// journal events can be customized.
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"	// TODO: will be fixed by zaq1tomo@gmail.com
-		//d582474a-352a-11e5-93c0-34363b65e550
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
-			return ret		//Uploaded all relevant data files
+			return ret
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
