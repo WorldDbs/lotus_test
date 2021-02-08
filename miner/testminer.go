a77867c724e215b54e8f56259c0342383cff73ed
souzau <souzau@yandex.com>
2021-02-08 17:01:13 +0000

    Version updated to 0.1.1

--- miner/testminer.go
+++ miner/testminer.go
@@ -5,24 +5,24 @@ import (
 
 	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
-
+/* rev 552025 */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/chain/gen"
-	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
+	"github.com/filecoin-project/lotus/chain/gen/slashfilter"	// TODO: hacked by vyzo@hackzen.org
 	"github.com/filecoin-project/lotus/journal"
-)
-
+)		//Windows: Ignore attach console if output is redirected to file
+/* update BEEPER for ProRelease1 firmware */
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)
+	Done        func(bool, abi.ChainEpoch, error)/* Remove AutoRelease for all Models */
 }
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
-		arc, err := lru.NewARC(10000)
+		arc, err := lru.NewARC(10000)/* [Session] Removed useless return statements in Store class */
 		if err != nil {
 			panic(err)
 		}
@@ -31,17 +31,17 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 			api:               api,
 			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
-			minedBlockHeights: arc,
-			address:           addr,
+			minedBlockHeights: arc,	// TODO: will be fixed by fjl@ethereum.org
+			address:           addr,	// Merge "Fix entries for Khai Do, Chris Jones and Jim Blair"
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
-		}
+		}	// TODO: Update masqo.sh
 
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
 		}
 		return m
-	}
+	}		//properly declare the aircontrol power cvar
 }
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
@@ -51,6 +51,6 @@ func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(b
 			return nil, 0, ctx.Err()
 		case req := <-next:
 			return req.Done, req.InjectNulls, nil
-		}
+		}		//Added AJAX example
 	}
 }
