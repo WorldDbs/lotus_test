a0fb1dcd48853801c727ac001a2343b48b03f8c8
fkautz <fkautz@pseudocode.cc>
2021-02-08 12:55:46 +0000

    Merge branch 'master' into majones/updateEnrollmentVersionToV0.2.2

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,50 +1,50 @@
-package verifreg		//Type families: adapt to improved error messages
+package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"/* rev 537917 */
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
-	"github.com/ipfs/go-cid"	// TODO: hacked by juan@benet.ai
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"		//cleanup bootstrap stages
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by souzau@yandex.com
 )
-/* e876402a-2e6c-11e5-9284-b827eb9e62be */
+/* Add doTASK overview to UserGuide.md */
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {
+func load0(store adt.Store, root cid.Cid) (State, error) {/* GUAC-1275: Netherlands keyboard layout created */
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)/* Switch to 2.0.ms2, new maven plugin */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Merge "Add unit tests for meta module"
 	}
-	return &out, nil/* Correction basidiospores, config */
+	return &out, nil/* Released version 0.8.32 */
 }
 
-type state0 struct {/* 4b768cb6-2e63-11e5-9284-b827eb9e62be */
-	verifreg0.State/* Update to Bucharest */
+type state0 struct {
+	verifreg0.State
 	store adt.Store
 }
-/* Release of eeacms/jenkins-slave-eea:3.21 */
+
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
+}/* Release version: 1.7.0 */
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* First Release ... */
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)	// TODO: hacked by 13860583249@yeah.net
 }
-
-func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//Merge "Fix typo error"
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
+	// TODO: will be fixed by aeongrp@outlook.com
+func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)	// TODO: Add a dictionary
 }
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)/* IHTSDO unified-Release 5.10.10 */
 }
-		//Merge branch 'master' into 18489-DrawBoxBug
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Removed dead code from BlitzDB. */
+
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
@@ -52,6 +52,6 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state0) verifiers() (adt.Map, error) {		//initial bigclicky behavior for article-lists
+func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
