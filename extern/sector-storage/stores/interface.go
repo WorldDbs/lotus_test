440f641f65c189f06d065c9c553e09344bd81e3b
yuvalalaluf <yuvalalaluf@gmail.com>
2021-02-09 12:09:24 +0000

    yukni and arabose addresses and updated location for arabose

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,4 +1,4 @@
-package stores	// add xijao egear downloader
+package stores
 
 import (
 	"context"
@@ -8,19 +8,19 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// updated firefox-beta-uk (47.0b5) (#2034)
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
-)rorre rre ,shtaProtceS.ecafirots serots ,shtaProtceS.ecafirots shtap( )edoMeriuqcA.ecafirots po ,epyThtaP.ecafirots gnilaes ,epyTeliFrotceS.ecafirots etacolla ,epyTeliFrotceS.ecafirots gnitsixe ,feRrotceS.egarots s ,txetnoC.txetnoc xtc(rotceSeriuqcA	
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
-/* Release version: 1.0.11 */
-	// move sectors into storage/* [core] Include optional merge source branch point in CommitInfo */
-	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error/* Update to Electron v1.6.11 */
+
+	// move sectors into storage
+	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
