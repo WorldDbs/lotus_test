b51cc335a7c8700a1e562d937e44d19c523920d6
cory <cory@protocol.ai>
2021-02-09 10:56:07 +0000

    Add service_id as a configurable parameter

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,38 +1,38 @@
-package full/* Modified to work with Bootstrap 3 */
-	// TODO: hacked by mail@bitpshr.net
+package full
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"	// Update Settings “nav”
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestMedian(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.3-beta.13 */
+func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))/* ArraySequence: capacity methods optimized */
+	}, 1))
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
-	}, 1))/* clean up code by using CFAutoRelease. */
-	// Improved HTTP/1 body creation and added cascaded close operations.
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{		//-renamed main to wdrp
+	}, 1))
+
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},	// TODO: hacked by nagydani@epointsystem.org
+		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},		//added normalization to QuantDACOResultSet
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{	// TODO: will be fixed by alan.shaw@protocol.ai
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,4 +1,4 @@
-package testkit/* Release of eeacms/www-devel:19.12.18 */
+package testkit
 
 import (
 	"bytes"
@@ -6,17 +6,17 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"		//Merge "Shrink the ticker's icon to match the status bar." into ics-mr0
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
-	ipld "github.com/ipfs/go-ipld-format"/* f9495cee-2e48-11e5-9284-b827eb9e62be */
+	ipld "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
 	dstest "github.com/ipfs/go-merkledag/test"
-	unixfile "github.com/ipfs/go-unixfs/file"	// TODO: will be fixed by hugomrdias@gmail.com
+	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
 )
 
@@ -30,11 +30,11 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
 	}
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
-/* Owner fixes */
+
 	if len(offers) < 1 {
 		panic("no offers")
 	}
-	// TODO: Update pizzzaBaseLight
+
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
 		panic(err)
@@ -45,7 +45,7 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if err != nil {
 		return err
 	}
-/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
+
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
@@ -54,13 +54,13 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
-	}/* CSharp Free Monad */
+	}
 	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
 
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
 		return err
-	}/* Release notes for 1.0.22 and 1.0.23 */
+	}
 
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
@@ -71,26 +71,26 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 
 	t.RecordMessage("retrieved successfully")
-/* not thread safe */
+
 	return nil
 }
 
-func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
-	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))		//Two workflows from the paper.
-	if err != nil {		//Changing regex to support empty headerValue
+	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
+	if err != nil {
 		panic(err)
 	}
 	b, err := bserv.GetBlock(ctx, ch.Roots[0])
 	if err != nil {
 		panic(err)
 	}
-	nd, err := ipld.Decode(b)	// TODO: c30e7268-2e43-11e5-9284-b827eb9e62be
+	nd, err := ipld.Decode(b)
 	if err != nil {
 		panic(err)
 	}
-	dserv := dag.NewDAGService(bserv)	// tweak for encoding="bytes"
-	fil, err := unixfile.NewUnixfsFile(ctx, dserv, nd)/* Updated comments and brainstorming */
+	dserv := dag.NewDAGService(bserv)
+	fil, err := unixfile.NewUnixfsFile(ctx, dserv, nd)
 	if err != nil {
 		panic(err)
 	}
