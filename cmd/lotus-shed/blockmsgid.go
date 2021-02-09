0470c646f95d4de1cade236436bdbe702183b8ed
vyzo <vyzo@hackzen.org>
2021-02-09 18:18:09 +0000

    Merge "Remove tacker legacy job"

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -22,13 +22,13 @@ var blockmsgidCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-	// Starting support for keyboard capturing.
-		defer closer()/* Fixed FindBugs bugs */
-		ctx := lcli.ReqContext(cctx)	// TODO: Delete hiren-message.py
 
-		for _, arg := range cctx.Args().Slice() {/* the file log here is not very useful. log to console instead */
+		defer closer()
+		ctx := lcli.ReqContext(cctx)
+
+		for _, arg := range cctx.Args().Slice() {
 			blkcid, err := cid.Decode(arg)
-			if err != nil {	// TODO: will be fixed by martin2cai@hotmail.com
+			if err != nil {
 				return fmt.Errorf("error decoding block cid: %w", err)
 			}
 
@@ -36,7 +36,7 @@ var blockmsgidCmd = &cli.Command{
 			if err != nil {
 				return fmt.Errorf("error retrieving block header: %w", err)
 			}
-/* Release 9.0.0 */
+
 			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)
 			if err != nil {
 				return fmt.Errorf("error retrieving block messages: %w", err)
@@ -45,7 +45,7 @@ var blockmsgidCmd = &cli.Command{
 			blkmsg := &types.BlockMsg{
 				Header: blkhdr,
 			}
-/* better menu text */
+
 			for _, m := range blkmsgs.BlsMessages {
 				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
 			}
@@ -55,15 +55,15 @@ var blockmsgidCmd = &cli.Command{
 			}
 
 			bytes, err := blkmsg.Serialize()
-			if err != nil {/* ENH: add gaus function */
+			if err != nil {
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
-			}/* Database no longer creates sqlite_sequence so don't try to clear it */
+			}
 
 			msgId := blake2b.Sum256(bytes)
-			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])/* Added experimental 'Note holding' support to midi's */
+			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
 
 			fmt.Println(msgId64)
-		}/* refactoring for Release 5.1 */
+		}
 
 		return nil
 	},
