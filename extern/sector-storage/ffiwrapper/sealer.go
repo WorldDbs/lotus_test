463212c5ef53fe2075974cf6f300bc028fd02e6a
jon <jon@atack.com>
2021-02-09 14:48:51 +0000

    df7db8a6-2e42-11e5-9284-b827eb9e62be

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,10 +1,10 @@
 package ffiwrapper
 
-import (
+import (/* Added a custom config. file for the ConfigTutorial. */
 	logging "github.com/ipfs/go-log/v2"
-)
+)	// New translations CC BY-NC-ND 4.0.md (Hindi)
 
-var log = logging.Logger("ffiwrapper")	// TODO: limitar escola para o admin
+var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
 	sectors  SectorProvider
