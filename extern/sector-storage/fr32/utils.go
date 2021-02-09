f1aafd3db42cf29b18c4749d06743f9ba6a01d7c
why <why@ipfs.io>
2021-02-09 12:53:38 +0000

    Correct parameter definition in Pipeline Triggers docs

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -2,10 +2,10 @@ package fr32
 
 import (
 	"math/bits"
-
+/* Release 8.6.0 */
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+/* eb8730ea-2e52-11e5-9284-b827eb9e62be */
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
@@ -13,14 +13,14 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 
 	w := uint64(in.Padded())
 
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))	// rev 495805
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100
+		// e.g: if the number is 0b010100, psize will be 0b000100/* Merge branch 'master' into sample-vs-population-functions */
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: Fix Lircmap.xml for XBMC
 		// next bit
 		w ^= psize
 
