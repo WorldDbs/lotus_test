9f92edb3a272ec2ee00e6476158e4cef109fc4f2
lexy8russo <lexy8russo@outlook.com>
2021-02-10 03:10:26 +0000

    test ideas

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,17 +1,17 @@
 package adt
 
-import (		//Update Perfil de Pupilo Mariana Ruther
+import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
-type Map interface {		//a2d7365c-2e4f-11e5-b304-28cfe91dbc4b
+type Map interface {
 	Root() (cid.Cid, error)
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
-	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)/* Merge "Add metadata for RH Release" */
+	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
@@ -19,10 +19,10 @@ type Map interface {		//a2d7365c-2e4f-11e5-b304-28cfe91dbc4b
 
 type Array interface {
 	Root() (cid.Cid, error)
-	// Update ground_based_people_detector_sr.yaml
+
 	Set(idx uint64, v cbor.Marshaler) error
-	Get(idx uint64, v cbor.Unmarshaler) (bool, error)	// Merge "input: sensors: add place property for MPU6050 driver"
-	Delete(idx uint64) error		//Delete default_image.jpg
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
+	Delete(idx uint64) error
 	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
