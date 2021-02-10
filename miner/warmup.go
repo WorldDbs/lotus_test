9513abc42b0244c0a481dcee9dd298c15a512cd8
davidad <davidad@alum.mit.edu>
2021-02-10 05:12:45 +0000

    Adding missing library functions.

--- miner/warmup.go
+++ miner/warmup.go
@@ -1,45 +1,45 @@
-package miner
-
+package miner	// Added ACRA library to project
+		//HeaderAndMessageKeyStore: Add more noexcept
 import (
 	"context"
 	"crypto/rand"
 	"math"
-	"time"
-
-	"golang.org/x/xerrors"
+	"time"		//Using bootstrap nav
+/* Release notes etc for MAUS-v0.4.1 */
+	"golang.org/x/xerrors"/* Fixed Winning screen */
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"/* Release: Making ready for next release iteration 5.8.0 */
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
-	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
+	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)/* Ticket #269: Fixed multiple permission validation issues + query efficiency. */
 	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
 	}
 
-	var sector abi.SectorNumber = math.MaxUint64
+	var sector abi.SectorNumber = math.MaxUint64/* web: show +subs link only when there are subaccounts */
 
 out:
-	for dlIdx := range deadlines {
+	for dlIdx := range deadlines {/* Release 0.1.0 (alpha) */
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
-		if err != nil {
+		if err != nil {	// TODO: hacked by mail@bitpshr.net
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
 		}
 
 		for _, partition := range partitions {
-			b, err := partition.ActiveSectors.First()
+			b, err := partition.ActiveSectors.First()	// TODO: Update owncloud_install_test.yaml
 			if err == bitfield.ErrNoBitsSet {
 				continue
 			}
 			if err != nil {
 				return err
 			}
-
+	// TODO: will be fixed by mail@bitpshr.net
 			sector = abi.SectorNumber(b)
 			break out
 		}
@@ -47,15 +47,15 @@ out:
 
 	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
-		return nil
+		return nil/* Merge "Remove Release page link" */
 	}
-
+/* Release Notes: more 3.4 documentation */
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
 	_, _ = rand.Read(r)
-
+		//Create genomics.md
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
 	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
@@ -64,7 +64,7 @@ out:
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
 			SealProof:    si.SealProof,
-			SectorNumber: sector,
+			SectorNumber: sector,/* Merge branch 'master' into feature/move-url-retrieval-to-middleware */
 			SealedCID:    si.SealedCID,
 		},
 	}, r)
