5854ee2b10e65e10b7ee253dbb7d4b806b0f286c
lexy8russo <lexy8russo@outlook.com>
2021-02-11 11:55:51 +0000

    Updated yogo auth log in box

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
-import "github.com/ipfs/go-log/v2"/* Prepared Development Release 1.5 */
+import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")		//Remove obsolete management command
+	_ = log.SetLogLevel("chain", "ERROR")/* AudioMixerSelection, audioRecording integrated */
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")	// TODO: Changing to the MDS tab now sets FDR and FC filters off
 }
