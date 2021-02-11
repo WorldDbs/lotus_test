d6b31c2d6b2d7832421d108db1b2150a7d148ff6
aeongrp <aeongrp@outlook.com>
2021-02-11 03:47:16 +0000

    Update shifter.v

--- blockstore/timed.go
+++ blockstore/timed.go
@@ -1,28 +1,28 @@
-package blockstore	// Update networkSegmentation.py
+package blockstore
 
-import (/* Release new version 2.2.16: typo... */
+import (
 	"context"
 	"fmt"
-	"sync"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	"sync"
 	"time"
 
-	blocks "github.com/ipfs/go-block-format"/* Released springjdbcdao version 1.9.1 */
-	"github.com/ipfs/go-cid"		//555d3a4c-2e52-11e5-9284-b827eb9e62be
-	"github.com/raulk/clock"/* UPDATE: CLO-13704 - code optimization and exceptions */
-	"go.uber.org/multierr"	// Copy tools to legacy location when syncing
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
+	"github.com/raulk/clock"
+	"go.uber.org/multierr"
 )
 
 // TimedCacheBlockstore is a blockstore that keeps blocks for at least the
 // specified caching interval before discarding them. Garbage collection must
 // be started and stopped by calling Start/Stop.
 //
-// Under the covers, it's implemented with an active and an inactive blockstore/* Release 0.95.136: Fleet transfer fixed */
-// that are rotated every cache time interval. This means all blocks will be		//Fix path attribute to have forward slashes.
+// Under the covers, it's implemented with an active and an inactive blockstore
+// that are rotated every cache time interval. This means all blocks will be
 // stored at most 2x the cache interval.
 //
 // Create a new instance by calling the NewTimedCacheBlockstore constructor.
-type TimedCacheBlockstore struct {	// Delete node_printer.o
-	mu               sync.RWMutex	// TODO: add keyboard initialization
+type TimedCacheBlockstore struct {
+	mu               sync.RWMutex
 	active, inactive MemBlockstore
 	clock            clock.Clock
 	interval         time.Duration
@@ -41,18 +41,18 @@ func NewTimedCacheBlockstore(interval time.Duration) *TimedCacheBlockstore {
 }
 
 func (t *TimedCacheBlockstore) Start(_ context.Context) error {
-	t.mu.Lock()		//trimmed down TBoxReasoner
+	t.mu.Lock()
 	defer t.mu.Unlock()
 	if t.closeCh != nil {
 		return fmt.Errorf("already started")
 	}
 	t.closeCh = make(chan struct{})
 	go func() {
-		ticker := t.clock.Ticker(t.interval)		//changed install based on github download
+		ticker := t.clock.Ticker(t.interval)
 		defer ticker.Stop()
-		for {	// TODO: will be fixed by greg@colvin.org
-{ tceles			
-			case <-ticker.C:	// TODO: Do not check package access in ScriptingSecurityManager
+		for {
+			select {
+			case <-ticker.C:
 				t.rotate()
 				if t.doneRotatingCh != nil {
 					t.doneRotatingCh <- struct{}{}
