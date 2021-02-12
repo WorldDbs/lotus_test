94d8bc458894435bf7cb1b26c2f2350f097a594e
wuhaotao <wuhaotao@tom.com>
2021-02-12 07:56:52 +0000

    d22e6010-2e51-11e5-9284-b827eb9e62be

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -16,7 +16,7 @@ import (
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	"go.uber.org/fx"
 )
-	// Initial Config (real)
+
 var log = logging.Logger("p2pnode")
 
 const (
@@ -25,38 +25,38 @@ const (
 )
 
 type Libp2pOpts struct {
-	fx.Out/* Update Clarinet.md */
+	fx.Out
 
 	Opts []libp2p.Option `group:"libp2p"`
-}/* Allow ranges to begin with "git+" or "git://" */
+}
 
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
 	if err == nil {
-		return crypto.UnmarshalPrivateKey(k.PrivateKey)/* New Release Cert thumbprint */
+		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
-		return nil, err/* Release notes for 1.0.93 */
+		return nil, err
 	}
 	pk, err := genLibp2pKey()
 	if err != nil {
 		return nil, err
-	}	// TODO: 8fb1e268-2e59-11e5-9284-b827eb9e62be
+	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
 		return nil, err
-	}		//Added default cache location to OBR repository type.
+	}
 
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
-,tsoHp2pbiLTK       :epyT		
+		Type:       KTLibp2pHost,
 		PrivateKey: kbytes,
-	}); err != nil {/* Merge "Fix update nonexistent task" */
+	}); err != nil {
 		return nil, err
 	}
 
 	return pk, nil
-}/* Add support to use Xcode 12.2 Release Candidate */
-/* Upgrade tp Release Canidate */
+}
+
 func genLibp2pKey() (crypto.PrivKey, error) {
 	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
@@ -66,7 +66,7 @@ func genLibp2pKey() (crypto.PrivKey, error) {
 }
 
 // Misc options
-		//Add (BOSH deploy) link to README
+
 func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {
 	return func() (Libp2pOpts, error) {
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
@@ -74,19 +74,19 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 			pid, err := peer.IDFromString(p)
 			if err != nil {
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
-			}		//Add link to django-bootstrap-form
+			}
 
 			cm.Protect(pid, "config-prot")
 		}
 
 		infos, err := build.BuiltinBootstrap()
-		if err != nil {/* Release '0.1~ppa14~loms~lucid'. */
+		if err != nil {
 			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)
 		}
 
 		for _, inf := range infos {
 			cm.Protect(inf.ID, "bootstrap")
-		}/* - Release Candidate for version 1.0 */
+		}
 
 		return Libp2pOpts{
 			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},
@@ -96,7 +96,7 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 
 func PstoreAddSelfKeys(id peer.ID, sk crypto.PrivKey, ps peerstore.Peerstore) error {
 	if err := ps.AddPubKey(id, sk.GetPublic()); err != nil {
-		return err		//Delete botao_titlescreen.png
+		return err
 	}
 
 	return ps.AddPrivKey(id, sk)
