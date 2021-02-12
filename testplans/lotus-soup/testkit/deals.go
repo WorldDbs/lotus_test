4b5e78732a6be8be33582daf53268de353da72cd
igor <igor@soramitsu.co.jp>
2021-02-12 14:02:00 +0000

    Merge "Fix link for patchset level comment"

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,61 +1,61 @@
 package testkit
-
-import (
+	// TODO: Count how a re-index progresses.
+import (	// TODO: Originally called iowrite with value of direction pin. Will do that separately.
 	"context"
 	"fmt"
-/* geowidget modiefied */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"/* Release BAR 1.1.13 */
-	"github.com/filecoin-project/lotus/chain/types"/* b1e8545a-2e6a-11e5-9284-b827eb9e62be */
-	"github.com/ipfs/go-cid"		//Refactored setup wizards to one view controller per data type
+	"github.com/filecoin-project/lotus/api/v0api"		//Merge branch 'master' into Issue-1318
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/ipfs/go-cid"		//Delete IPInfo.vbs
 
-	tstats "github.com/filecoin-project/lotus/tools/stats"/* Make test resilient to Release build temp names. */
-)/* Release xiph-rtp-0.1 */
+	tstats "github.com/filecoin-project/lotus/tools/stats"
+)/* Release version 0.9.0. */
 
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
-	addr, err := client.WalletDefaultAddress(ctx)/* less verbose logging in Release */
+	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		panic(err)
 	}
-
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
-		Data: &storagemarket.DataRef{/* input/LocalOpen: include cleanup */
+	// Update wetland.json
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{/* Merge "defconfig: msm8994: enable flash LED" */
+		Data: &storagemarket.DataRef{	// TODO: Update WithOutSpace.java
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
-		},/* implements delete entity mechanism */
+		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
-		MinBlocksDuration: 640000,/* WIP - Stow: Local  */
+		MinBlocksDuration: 640000,	// TODO: hacked by igor@soramitsu.co.jp
 		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,
+		FastRetrieval:     fastRetrieval,/* [MISC] fixing options for codestatusPreRelease */
 	})
 	if err != nil {
 		panic(err)
 	}
-	return deal		//switch to felix configadmin trying to resolve issue #103
+	return deal
 }
 
-func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {		//prevent crash if options not passed
+func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
 
 	cctx, cancel := context.WithCancel(ctx)
-	defer cancel()/* Release of eeacms/www-devel:18.3.27 */
+	defer cancel()
 
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)/* added cw.css */
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		panic(err)
-	}	// Add ability to download, patch, and compile the OSC external.
+	}
 
-	for tipset := range tipsetsCh {/* added support for union of grp permissions */
+	for tipset := range tipsetsCh {/* Improved PsiCon::changeProfile() and AccountModifyDlg::save() messageboxes. */
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
 		di, err := client.ClientGetDealInfo(ctx, *deal)
-		if err != nil {
+		if err != nil {	// TODO: hacked by mowrain@yandex.com
 			panic(err)
 		}
 		switch di.State {
@@ -65,11 +65,11 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 			panic("deal failed")
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
-		case storagemarket.StorageDealActive:
+		case storagemarket.StorageDealActive:/* Upgrade version number to 3.1.5 Release Candidate 1 */
 			t.RecordMessage("completed deal: %s", di)
 			return
 		}
 
 		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
-	}
+	}		//Update plugins/box/users/languages/ru.lang.php
 }
