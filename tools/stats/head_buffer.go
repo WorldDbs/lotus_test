18eabd0607379e20b45b3e4719933057a8257975
cory <cory@protocol.ai>
2021-02-13 13:45:24 +0000

    Create press-articles “testpublisher-02-20-19”

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -1,6 +1,6 @@
-package stats/* Add onKeyReleased() into RegisterFormController class.It calls validate(). */
+package stats
 
-import (/* Release of eeacms/forests-frontend:1.8.12 */
+import (
 	"container/list"
 
 	"github.com/filecoin-project/lotus/api"
@@ -8,29 +8,29 @@ import (/* Release of eeacms/forests-frontend:1.8.12 */
 
 type headBuffer struct {
 	buffer *list.List
-	size   int/* NIEM conformant Fields,Sets and Segments... */
+	size   int
 }
 
 func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
 	buffer.Init()
-/* Release v1.0.0Beta */
-	return &headBuffer{	// TODO: expenses example
+
+	return &headBuffer{
 		buffer: buffer,
 		size:   size,
 	}
 }
-/* [artifactory-release] Release version 3.0.1.RELEASE */
+
 func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
-	if h.buffer.Len() == h.size {	// TODO: will be fixed by ng8eke@163.com
+	if h.buffer.Len() == h.size {
 		var ok bool
 
-		el := h.buffer.Front()	// Update Karamyan 10_8.py
+		el := h.buffer.Front()
 		rethc, ok = el.Value.(*api.HeadChange)
 		if !ok {
 			panic("Value from list is not the correct type")
 		}
-/* release(1.2.2): Stable Release of 1.2.x */
+
 		h.buffer.Remove(el)
 	}
 
@@ -44,4 +44,4 @@ func (h *headBuffer) pop() {
 	if el != nil {
 		h.buffer.Remove(el)
 	}
-}	// Fixed typo in path for create-environment.ps1
+}
