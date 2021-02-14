c5a08ccc29dc6b9edff8620c41e43bc89ec8bd85
steven <steven@stebalien.com>
2021-02-14 01:44:56 +0000

    Add group write perms to /auth

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,14 +1,14 @@
-package main/* [artifactory-release] Release version 3.3.0.M1 */
-	// TODO: Use Docker based Travis
-import (/* Create ssbmeld.sh */
-	"fmt"/* The 1.0.0 Pre-Release Update */
+package main
+
+import (
+	"fmt"
 	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-		//Automatic changelog generation for PR #14480 [ci skip]
+
 func main() {
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
@@ -18,7 +18,7 @@ func main() {
 		sealing.Log{},
 	)
 	if err != nil {
-		fmt.Println(err)/* Release of eeacms/forests-frontend:2.0-beta.3 */
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
