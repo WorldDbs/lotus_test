9a3e1833db502118aae64d001e6db5d81e323635
martin2cai <martin2cai@hotmail.com>
2021-02-14 17:55:07 +0000

    polling improved

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,16 +1,16 @@
 package multisig
 
 import (
-"srorrex/x/gro.gnalog"	
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// [-release]Preparing version 6.2a.23
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* replace deprecated contains with in */
+		//Use JDK7_HOME for bootClasspath in Java 7 submodules
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Release for 18.20.0 */
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -19,53 +19,53 @@ type message4 struct{ message0 }
 
 func (m message4) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,	// 1.2E Version
+	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+	// removed unused "use" statement.
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Release of eeacms/www-devel:19.11.27 */
-	}/* Release of eeacms/eprtr-frontend:0.4-beta.14 */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+	}
 
-	if threshold == 0 {
+	if threshold == 0 {/* add db backup */
 		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-		//fix the gen.subst thing, but maybe break other things?
-	// Set up constructor parameters for multisig		//added To<>
+
+	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
+		NumApprovalsThreshold: threshold,	// Delete jSniff.NET.cs
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* Release Cobertura Maven Plugin 2.6 */
+	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
-		return nil, actErr/* [IMP]: caldav: Added description field in calendar */
-	}
-/* Release 1.3.23 */
-	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Previous trial worked- updating subtitle font size */
-	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,
-		ConstructorParams: enc,
-	}
-/* Merge "Add -w to iptables calls" */
-	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
 	}
 
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	execParams := &init4.ExecParams{		//Merge "Enable functional testing job for ironic-discoverd"
+		CodeCID:           builtin4.MultisigActorCodeID,
+		ConstructorParams: enc,
+	}
+
+	enc, actErr = actors.SerializeParams(execParams)
+	if actErr != nil {
+		return nil, actErr
+	}
+	// TODO: EvalItems should not stop when a single EvalItem goes wrong
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
-		Params: enc,		//Use ArrayList / index iteration here.
+		Params: enc,
 		Value:  initialAmount,
 	}, nil
-}	// removed travis from subfolder
+}
