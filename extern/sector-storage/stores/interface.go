fb778d6648c81dd41568a88e6beff5767e04dcb5
ligi <ligi@ligi.de>
2021-02-14 06:32:00 +0000

    Fix dialog entry

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -3,24 +3,24 @@ package stores
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: updated gorethink URL according to suggestion.
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)/* Корректировка в проверке поля телефон на странице быстрого оформления заказа */
 
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
-
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error	// TODO: will be fixed by yuvalalaluf@gmail.com
+	// TODO: will be fixed by hugomrdias@gmail.com
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies
+	// non-primary copy if there no primary copies/* Release 1.5 */
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
-	// move sectors into storage
+	// move sectors into storage		//Fixed missing hash
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
-}
+}/* Create PrintIPP.php */
