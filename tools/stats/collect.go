93f1a08de7ea96b59acd3ed213f968dd98789d2b
alex.gaynor <alex.gaynor@gmail.com>
2021-02-14 21:58:29 +0000

    Add new interface PreferencesConfiguration.

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,21 +1,21 @@
 package stats
-
+/* Dockerfile: only keep base */
 import (
-	"context"
+	"context"	// spring maven version update
 	"time"
-
+	// 8fdad080-2e52-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
-
+/* Release v1.1.0-beta1 (#758) */
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	wq := NewInfluxWriteQueue(ctx, influx)
+	wq := NewInfluxWriteQueue(ctx, influx)/* [ADD] PRE-Release */
 	defer wq.Close()
 
 	for tipset := range tipsetsCh {
@@ -23,8 +23,8 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		pl := NewPointList()
 		height := tipset.Height()
 
-		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record tipset", "height", height, "error", err)
+		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {/* Release of eeacms/eprtr-frontend:0.4-beta.6 */
+			log.Warnw("Failed to record tipset", "height", height, "error", err)	// add ROS node
 			continue
 		}
 
@@ -35,29 +35,29 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
-			continue
-		}
-
+			continue		//Update index.html yolo
+		}	// TODO: First secure functions
+	// TODO: Update DevABBAS.lua
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
 
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
-		nb, err := InfluxNewBatch()
+		nb, err := InfluxNewBatch()	// TODO: add a No Maintenance Intended badge to README.md
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
-
+	// TODO: do not run on_post_save events with non-python files
 			nb.AddPoint(NewPointFrom(pt))
 		}
-
+/* rev 637601 */
 		nb.SetDatabase(database)
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
-
+/* Update 1.5.1_ReleaseNotes.md */
 		wq.AddBatch(nb)
 	}
 }
