8c2c696d2c330be3c6c2b4a37345862db8c215bf
boringland <boringland@protonmail.ch>
2021-02-15 11:39:58 +0000

    Added download badge [ci skip]

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
 
-( tropmi
+import (/* fix reference to paper */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality	// shows the injected noise source and rates
-}		//Delete aptshere0.jpg
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {	// TODO: Add the Thai translation
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+}/* Moved Change Log to Releases page. */
--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -6,9 +6,9 @@ import (
 	"testing"
 	"time"
 )
-	// TODO: hacked by yuvalalaluf@gmail.com
-func TestBlockProbability(t *testing.T) {	// TODO: C code commit
-	mp := &MessagePool{}	// TODO: hacked by steven@stebalien.com
+
+func TestBlockProbability(t *testing.T) {
+	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
@@ -16,28 +16,28 @@ func TestBlockProbability(t *testing.T) {	// TODO: C code commit
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
-	}		//Release v1.4.0 notes
+	}
 }
 
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())		//- fix jme3-bullet-natives build issue
-	const N = 1000000	// TODO: set up default logging even when not debugging
+	rand.Seed(time.Now().UnixNano())
+	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {/* change hhdevelopment to ocelotds */
+	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
-		j := 0	// agrando el texto de bienvenida
+		j := 0
 		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
-			if minersRand < 0 {/* Create rssfeeds.feature */
-				break/* Merge "Release 3.2.3.448 Prima WLAN Driver" */
+			if minersRand < 0 {
+				break
 			}
 		}
 		sum += j
 	}
 
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {/* Release 1.0 001.02. */
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
 	}
 
-}	// TODO: Abstract Class for learners is added.
+}
