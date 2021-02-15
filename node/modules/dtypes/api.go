ce59c098b25e45965b03b7fc63c2eb7cdb270e51
nick <nick@perfectabstractions.com>
2021-02-15 18:50:18 +0000

    Create minimal-aws.yml

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+type APIAlg jwt.HMACSHA/* Release: 5.8.1 changelog */
 
-type APIAlg jwt.HMACSHA
-/* Release version 0.9.2 */
-type APIEndpoint multiaddr.Multiaddr/* removed bogus .gitignore */
+type APIEndpoint multiaddr.Multiaddr
