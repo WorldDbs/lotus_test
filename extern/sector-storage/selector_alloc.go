ac59f1bd9848701b4c84ab4b53f9a00d8654fcf9
antao2002 <antao2002@gmail.com>
2021-02-16 00:51:16 +0000

    fixes #34 fix for checking the wrong route

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -2,17 +2,17 @@ package sectorstorage
 
 import (
 	"context"
-
+/* Add index page */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"		//46409f10-2e4b-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type allocSelector struct {
+type allocSelector struct {/* Message text added to exception */
 	index stores.SectorIndex
 	alloc storiface.SectorFileType
 	ptype storiface.PathType
@@ -23,10 +23,10 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
-	}
+	}/* Improved survey creation form validation */
 }
-
-func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
+	// TODO: will be fixed by timnugent@gmail.com
+func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {	// TODO: Add Nordic Venture Family to organizations list
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
@@ -35,33 +35,33 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 		return false, nil
 	}
 
-	paths, err := whnd.workerRpc.Paths(ctx)
+	paths, err := whnd.workerRpc.Paths(ctx)/* Update sinatra to version 2.0.7 */
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}
+	}/* Release chart 2.1.0 */
 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
-		have[path.ID] = struct{}{}
+		have[path.ID] = struct{}{}		//c3d25b21-2eae-11e5-94b8-7831c1d44c14
 	}
-
-	ssize, err := spt.SectorSize()
+/* Release for v26.0.0. */
+	ssize, err := spt.SectorSize()/* MOSES: changed log Generation idx */
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
-	if err != nil {
+	if err != nil {/* [FIX] Setup of Yandex sandbox/production urls */
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
 	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
-		}
+		}/* Create genomics.md */
 	}
 
-	return false, nil
+	return false, nil	// Loop Vectorizer: turn-off if-conversion.
 }
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
