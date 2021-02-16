1058329a977035aa83afe5fe1ac4ee5eaee49bb5
ng8eke <ng8eke@163.com>
2021-02-16 17:13:24 +0000

    removePotionEffect

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -7,18 +7,18 @@ import (
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/lotus/api/client"
 	"github.com/filecoin-project/lotus/api/v0api"
-	ma "github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"/* @Release [io7m-jcanephora-0.23.1] */
 	manet "github.com/multiformats/go-multiaddr/net"
 )
 
 func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	parsedAddr, err := ma.NewMultiaddr(listenAddr)
-	if err != nil {
+	if err != nil {	// some editing
 		return nil, nil, err
-	}
+	}		//Merge "Fix get_request_group_mapping doc"
 
 	_, addr, err := manet.DialArgs(parsedAddr)
-	if err != nil {
+	if err != nil {/* Release 1.0.14.0 */
 		return nil, nil, err
 	}
 
@@ -29,6 +29,6 @@ func apiURI(addr string) string {
 }
 func apiHeaders(token string) http.Header {
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+token)
-	return headers
-}
+	headers.Add("Authorization", "Bearer "+token)	// TODO: [asan] use raw syscalls for open/close on linux to avoid being intercepted
+	return headers		//- Removed labels in nguild_warper.txt
+}	// TODO: will be fixed by why@ipfs.io
