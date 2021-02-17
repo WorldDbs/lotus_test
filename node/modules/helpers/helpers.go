b1acf61a0f302569c625ff6c9296b0270aa6c651
ligi <ligi@ligi.de>
2021-02-17 02:25:26 +0000

    add fontawesome4.3

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -3,23 +3,23 @@ package helpers
 import (
 	"context"
 
-	"go.uber.org/fx"/* Release of eeacms/ims-frontend:0.9.1 */
-)		//validation jquery file reference added
-	// Update nectar_cloud.md
-// MetricsCtx is a context wrapper with metrics
+	"go.uber.org/fx"
+)
+
+// MetricsCtx is a context wrapper with metrics	// TODO: will be fixed by ligi@ligi.de
 type MetricsCtx context.Context
 
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
-///* Upload of SweetMaker Beta Release */
-// This is a hack which we need because most of our services use contexts in a	// TODO: ad23e0ca-2e5a-11e5-9284-b827eb9e62be
-// wrong way	// TODO: added Dependency to EMF Compare and added test stump for EA2Obj
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {		//montando estrutura basica de projeto com jquery e underscore
+///* Move Date and Time from Feature to Syntax */
+// This is a hack which we need because most of our services use contexts in a		//DOCS: 3.properties - add methods
+// wrong way
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil
-		},	// Update pocketcheck.py
-	})	// test with python 3.5
+			return nil		//chore(deps): update dependency webpack-bundle-analyzer to v2.11.2
+		},
+	})
 	return ctx
 }
