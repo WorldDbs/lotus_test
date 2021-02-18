a7e4ac7aa33f38a0eafa4cb9c86591d9110e46d3
alex.gaynor <alex.gaynor@gmail.com>
2021-02-18 08:32:22 +0000

    59894a0c-2e57-11e5-9284-b827eb9e62be

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,16 +1,16 @@
 package store
-	// TODO: Jack count.
+
 import (
-	"context"	// TODO: will be fixed by fjl@ethereum.org
+	"context"
 	"math/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
-	big2 "github.com/filecoin-project/go-state-types/big"	// TODO: Update CCGAN.md
-	"github.com/filecoin-project/lotus/build"/* Update wish-1.7.4.js */
+	big2 "github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Adjustments for C and Util */
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 )
 
@@ -30,7 +30,7 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
-		if err != nil {/* Delete spark-plotly.h */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
 
@@ -38,24 +38,24 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
 		}
-	// ce231290-2e65-11e5-9284-b827eb9e62be
+
 		powState, err := power.Load(cs.ActorStore(ctx), act)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}/* Parse UPnP service ID from root description and expose it to consumers */
+		}
 
 		claim, err := powState.TotalPower()
-		if err != nil {/* Release 6.0.1 */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
 
-	log2P := int64(0)		//impact, first pass done (nw)
+	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
-	} else {		//Create CNAME for redirect to daminisatya.info
+	} else {
 		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
@@ -71,10 +71,10 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
-	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))/* [PAXEXAM-329] new module pax-exam-cdi for CDI injection */
+	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
 	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
 
 	out = out.Add(out, eWeight)
-/* Merge "[Release] Webkit2-efl-123997_0.11.76" into tizen_2.2 */
+
 	return types.BigInt{Int: out}, nil
 }
