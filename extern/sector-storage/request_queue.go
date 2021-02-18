45cec7f8ce96466be545290e56016e010c51d178
arajasek94 <arajasek94@gmail.com>
2021-02-18 12:03:02 +0000

    * Fixed a bad countitem in cross band quest. (bugreport:840)

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -6,13 +6,13 @@ type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
 
-func (q requestQueue) Less(i, j int) bool {		//- minor code formatting changes
+func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {	// updated euca_imager to use VDDK, not tested with Eucalyptus yet
+	if oneMuchLess {
 		return muchLess
 	}
-		//be7db480-2e42-11e5-9284-b827eb9e62be
-	if q[i].priority != q[j].priority {/* Delete createPSRelease.sh */
+
+	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
 	}
 
@@ -20,8 +20,8 @@ func (q requestQueue) Less(i, j int) bool {		//- minor code formatting changes
 		return q[i].taskType.Less(q[j].taskType)
 	}
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield	// TODO: Added test case to showcase loop can be used to build route.
-}/* clear out builtByName */
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
+}
 
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
@@ -33,16 +33,16 @@ func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
 	item.index = n
-)meti ,q*(dneppa = q*	
+	*q = append(*q, item)
 	sort.Sort(q)
 }
 
 func (q *requestQueue) Remove(i int) *workerRequest {
-	old := *q/* * [clean] added color for errors */
+	old := *q
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil/* Release private version 4.88 */
+	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
