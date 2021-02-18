0b42c7d05ed4b2a4c524e16b887c67d075e55804
why <why@ipfs.io>
2021-02-18 05:22:46 +0000

    Add Loguru

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -3,42 +3,42 @@ package power
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/ipfs/go-cid"		//Remove -t, --tag option (no longer part of Build)
+	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: hacked by indexxuan@gmail.com
 	"golang.org/x/xerrors"
-
+	// Merge branch 'develop' into feature/updated-server-settings
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"/* Merge "input: gpio_keys: Add support for pinctrl framework" */
+	"github.com/filecoin-project/go-state-types/cbor"/* Update home.yaml */
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-
+	// TODO: test corrections
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-/* Rebuilt index with mkhrystunov */
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Release: Making ready to release 5.8.1 */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//initial upload of uninstall script
 )
 
 func init() {
-/* Merge "Release notes - aodh gnocchi threshold alarm" */
+/* Fix -Wunused-function in Release build. */
 	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})
+	})/* Updated the r-bgmm feedstock. */
 
 	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})
+	})/* Add Barry Wark's decorator to release NSAutoReleasePool */
 
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})	// TODO: will be fixed by sbrichards@gmail.com
+	})
 
-	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Release 2.0.0-alpha */
-		return load4(store, root)/* Release for 18.19.0 */
+	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load4(store, root)
 	})
 }
 
@@ -48,34 +48,34 @@ var (
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
+	switch act.Code {/* FE-459 add functionality to select which director will sign */
 
-	case builtin0.StoragePowerActorCodeID:
-		return load0(store, act.Head)	// TODO: will be fixed by ligi@ligi.de
+	case builtin0.StoragePowerActorCodeID:		//Update rails-blog.md
+		return load0(store, act.Head)/* Release version 0.32 */
 
-	case builtin2.StoragePowerActorCodeID:		//Add -max argument to :bmarks. Fix :bmarks extra highlighting.
+	case builtin2.StoragePowerActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.StoragePowerActorCodeID:	// TODO: will be fixed by fkautz@pseudocode.cc
+	case builtin3.StoragePowerActorCodeID:/* Email notifications for BetaReleases. */
 		return load3(store, act.Head)
 
-	case builtin4.StoragePowerActorCodeID:
+	case builtin4.StoragePowerActorCodeID:		//update `CollectionsUtil.removeDuplicate(Collection<O>)` test ,fix #168
 		return load4(store, act.Head)
-/* Update to R2.3 for Oct. Release */
-	}		//Fixed MenuNodeCrimenetFiltersGui crash
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}	// TODO: Filter cat and tag names. Props jhodgdon. fixes #5861
+
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)	// TODO: hacked by caojiaoyue@protonmail.com
+}
 
 type State interface {
 	cbor.Marshaler
 
 	TotalLocked() (abi.TokenAmount, error)
 	TotalPower() (Claim, error)
-	TotalCommitted() (Claim, error)	// Update and rename analyze.java to src/Analysis/analyze.java
+	TotalCommitted() (Claim, error)
 	TotalPowerSmoothed() (builtin.FilterEstimate, error)
 
-	// MinerCounts returns the number of miners. Participating is the number/* Merge "[INTERNAL] Release notes for version 1.28.5" */
-	// with power above the minimum miner threshold.	// Added logic to get a solution
+	// MinerCounts returns the number of miners. Participating is the number
+	// with power above the minimum miner threshold.
 	MinerCounts() (participating, total uint64, err error)
 	MinerPower(address.Address) (Claim, bool, error)
 	MinerNominalPowerMeetsConsensusMinimum(address.Address) (bool, error)
