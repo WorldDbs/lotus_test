8473b15098843b5d6256f3275247c64ee7df508b
witek <witek@enjin.io>
2021-02-19 17:23:25 +0000

    Update AskMeActivity.java

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
 package lp2p
-		//269bcb1a-2e5c-11e5-9284-b827eb9e62be
+
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	tls "github.com/libp2p/go-libp2p-tls"/* actualizada la ventanaMatricula con las tablas  */
+	tls "github.com/libp2p/go-libp2p-tls"
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {		//Update rubertooth.rb
+	if !enabled {
 		return func() (opts Libp2pOpts) {
-			// TODO: shouldn't this be Errorf to guarantee visibility?	// TODO: Удалён неиспользуемый файл robox.txt
+			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)		//tunneling setting
+		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
-			return opts
+			return opts	// TODO: Upgrade escodegen to version 1.9.1
 		}
-	}
+	}/* Started new Release 0.7.7-SNAPSHOT */
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
-		return opts		//Exporting all necessary packages
+		return opts
 	}
-}		//Update elastic.co.md
-
+}
+		//Update settings sample to add register_device var
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()		//logic should be &&
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))/* add a readme of sorts */
+	reporter = metrics.NewBandwidthCounter()	// TODO: will be fixed by ng8eke@163.com
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	return opts, reporter
 }
