ceb70be2d10ab7a4ef1736ce5b6c92aabafc8339
ng8eke <ng8eke@163.com>
2021-02-19 10:24:10 +0000

    Merge pull request #74 from knoxHuang/dev

--- storage/sealing.go
+++ storage/sealing.go
@@ -4,62 +4,62 @@ import (
 	"context"
 	"io"
 
-	"github.com/ipfs/go-cid"
-	// TODO: kkex fetchOHLCV parseInteger → parseInt
-	"github.com/filecoin-project/go-address"
+	"github.com/ipfs/go-cid"	// Delete Portfolio_09.jpg
+
+	"github.com/filecoin-project/go-address"/* #4 added two more character to clean (‘’) */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"/* Released 0.9.4 */
-
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Release v4.1.10 [ci skip] */
+	"github.com/filecoin-project/specs-storage/storage"
+/* MessageListener Initial Release */
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-
-// TODO: refactor this to be direct somehow/* Release 1-119. */
-
-func (m *Miner) Address() address.Address {/* #208 - Release version 0.15.0.RELEASE. */
-	return m.sealing.Address()/* removed networking options */
+/* ReleasesCreateOpts. */
+// TODO: refactor this to be direct somehow
+	// TODO: hacked by zaq1tomo@gmail.com
+func (m *Miner) Address() address.Address {/* Release 2.0.10 - LongArray param type */
+	return m.sealing.Address()
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
+	return m.sealing.AddPieceToAnySector(ctx, size, r, d)	// TODO: will be fixed by ligi@ligi.de
 }
-		//Merge branch 'master' into DisplaySSr
+
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
-	return m.sealing.StartPacking(sectorNum)/* Update TCEFORM.txt */
-}	// Add drag and drop functionality to first item in lists
+	return m.sealing.StartPacking(sectorNum)/* Fix for MT #2072 (Robbert) */
+}
 
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
 
-func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {		//use GPXZOOMLEVEL constant in ImageCollector
+func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {		//Use only integration-test phase since it already includes test phase
 	return m.sealing.GetSectorInfo(sid)
 }
 
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
-}		//Merge "Allow hostname set by a plugin in dimensions to be used in a metric."
+}
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
-	return m.sealing.ForceSectorState(ctx, id, state)	// TODO: Fix bug with rotation angle 
-}/* #108327# handling of paper tray for printing */
+	return m.sealing.ForceSectorState(ctx, id, state)
+}
 
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
-}		//Prevent players without permission from seeing bubbles and using givers.
-	// TODO: Added TODO section
+}
+
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Terminate(ctx, id)
 }
 
-func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {/* Ring session store which is based on shared map #98 */
-	return m.sealing.TerminateFlush(ctx)
+func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
+	return m.sealing.TerminateFlush(ctx)	// TODO: Merge "bump to 0.4.0.beta.52"
 }
-
+/* Improve efficiency of javascript channels. */
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
-	return m.sealing.TerminatePending(ctx)
+	return m.sealing.TerminatePending(ctx)		//Delete tgl-@08b6340
 }
 
-func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
+func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {	// 0.9.3.pre4 prerelease!
 	return m.sealing.MarkForUpgrade(id)
 }
 
