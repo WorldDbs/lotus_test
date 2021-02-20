7e0b825817bd00d45a61451fa01c790c89a441bc
brosner <brosner@gmail.com>
2021-02-20 02:36:28 +0000

    Fix fonts urls for asset pipeline

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
-// +build debug
-		//[FIX] o2m switch_view
+// +build debug		//Update mhd.hh
+
 package build
 
 func init() {
 	InsecurePoStValidation = true
-	BuildType |= BuildDebug/* Update NexsonSource.java */
+	BuildType |= BuildDebug
 }
 
-k2_smarap morf sgnittes sedulcni oslA :ETON //
+// NOTE: Also includes settings from params_2k	// TODO: will be fixed by 13860583249@yeah.net
