cff5b0afdc9ab9f9e669d2379b936e15314d9da9
why <why@ipfs.io>
2021-02-20 18:40:14 +0000

    New font for p1

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,39 +1,39 @@
 package testkit
-	// TODO: Count how a re-index progresses.
-import (	// TODO: Originally called iowrite with value of direction pin. Will do that separately.
+
+import (
 	"context"
-	"fmt"
+	"fmt"		//Allow specification of sync options, and alias sync as s
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"		//Merge branch 'master' into Issue-1318
+	"github.com/filecoin-project/lotus/api"/* Create filter.cpp */
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"		//Delete IPInfo.vbs
+	"github.com/ipfs/go-cid"		//Make formatting more consistent
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-)/* Release version 0.9.0. */
+)/* Merge "ARM: dts: msm: remove unused 8952 context banks" */
 
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		panic(err)
 	}
-	// Update wetland.json
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{/* Merge "defconfig: msm8994: enable flash LED" */
-		Data: &storagemarket.DataRef{	// TODO: Update WithOutSpace.java
+
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{/* FIX: exporting non-rectangular puzzles as a Loopy string doesn't make sense */
+		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
-		},
-		Wallet:            addr,
+		},		//Update fresh-osx.md: Fix typo
+		Wallet:            addr,/* Released 1.0.2. */
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),
-		MinBlocksDuration: 640000,	// TODO: hacked by igor@soramitsu.co.jp
+		EpochPrice:        types.NewInt(4000000),/* Add saving of a rating to back end */
+		MinBlocksDuration: 640000,/* Release final 1.0.0  */
 		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,/* [MISC] fixing options for codestatusPreRelease */
-	})
-	if err != nil {
+		FastRetrieval:     fastRetrieval,/* Prepare Release 2.0.11 */
+	})		//Merge branch 'master' into issue-2196
+	if err != nil {	// TODO: Merge branch 'master' into fix-test-execution-in-forks
 		panic(err)
 	}
 	return deal
@@ -44,20 +44,20 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 	headlag := 3
 
 	cctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	defer cancel()		//added stage blocks to the modular layout model
 
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		panic(err)
 	}
-
-	for tipset := range tipsetsCh {/* Improved PsiCon::changeProfile() and AccountModifyDlg::save() messageboxes. */
+	// TODO: will be fixed by steven@stebalien.com
+	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
-
+/* Fix insertion on files_commits table. */
 		di, err := client.ClientGetDealInfo(ctx, *deal)
-		if err != nil {	// TODO: hacked by mowrain@yandex.com
+		if err != nil {
 			panic(err)
-		}
+		}		//a bare-bones dataLogger
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
@@ -65,11 +65,11 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 			panic("deal failed")
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
-		case storagemarket.StorageDealActive:/* Upgrade version number to 3.1.5 Release Candidate 1 */
+		case storagemarket.StorageDealActive:
 			t.RecordMessage("completed deal: %s", di)
 			return
 		}
 
 		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
-	}		//Update plugins/box/users/languages/ru.lang.php
+	}
 }
