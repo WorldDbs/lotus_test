60794989ae11342f42ac16509b2df1ae4a6f10af
aeongrp <aeongrp@outlook.com>
2021-02-20 09:03:00 +0000

    Re-create gem w/ bundler

--- cli/version.go
+++ cli/version.go
@@ -3,24 +3,24 @@ package cli
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"	// TODO: hacked by nagydani@epointsystem.org
+	"github.com/urfave/cli/v2"
 )
 
 var VersionCmd = &cli.Command{
-	Name:  "version",/* Use the latest code in OpenMRS 1.10.x */
+	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := GetAPI(cctx)/* Released springjdbcdao version 1.7.16 */
+		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err	// TODO: hacked by souzau@yandex.com
+			return err
 		}
-		defer closer()/* Release: 5.4.1 changelog */
-/* Release of eeacms/forests-frontend:1.8-beta.17 */
+		defer closer()
+
 		ctx := ReqContext(cctx)
-		// TODO: print more useful things	// TODO: will be fixed by cory@protocol.ai
+		// TODO: print more useful things
 
 		v, err := api.Version(ctx)
-		if err != nil {	// TODO: propres.php conserve les _ si le texte d'origine en contient
+		if err != nil {
 			return err
 		}
 		fmt.Println("Daemon: ", v)
