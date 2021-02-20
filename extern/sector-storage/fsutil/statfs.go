9505e3875257dd54a3afabb6b0679341feb920a1
igor <igor@soramitsu.co.jp>
2021-02-20 10:13:03 +0000

    creating a txt file

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,11 +1,11 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64	// gamedev / Humor
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
+/* Merge "Release 4.4.31.74" */
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
