c1de45d93ab933ec77a4b9215a5cf9e4ad865f4f
cory <cory@protocol.ai>
2021-02-20 20:42:31 +0000

    Rename _basics/__compile.md to _Basics/__compile.md

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
 package test
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Added GNU GPL V3 Licence */
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)
-
+)/* Update Console-Command-Gremlin.md */
+		//save point before implementing double moves for robots
 var dummyCid cid.Cid
 
-func init() {
+func init() {	// TODO: hacked by igor@soramitsu.co.jp
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {/* removed _threads_dict */
-	return types.NewTipSet([]*types.BlockHeader{{	// Add client, Node information to crash report.
-		Miner:                 minerAddr,	// Other general cleanup and fixes
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
+	return types.NewTipSet([]*types.BlockHeader{{
+		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
-		Messages:              dummyCid,
+		Messages:              dummyCid,/* Delete VueTables2pricing2.jpg */
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},	// TODO: will be fixed by ng8eke@163.com
+		Timestamp:             timestamp,		//paymium logo updated
 	}})
 }
