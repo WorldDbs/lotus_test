842e20b4e8cbcc3954b02efa444ec95b519c113f
souzau <souzau@yandex.com>
2021-02-20 14:39:39 +0000

    Add support for function libraries (MID-4601)

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -1,49 +1,49 @@
 package blockstore
 
 import (
-	cid "github.com/ipfs/go-cid"/* Release for v5.2.1. */
+	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log/v2"
 
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 )
-
+		//God the readme was ugly
 var log = logging.Logger("blockstore")
 
-var ErrNotFound = blockstore.ErrNotFound		//Update rosemary library.
+var ErrNotFound = blockstore.ErrNotFound
 
-// Blockstore is the blockstore interface used by Lotus. It is the union	// TODO: hacked by steven@stebalien.com
+// Blockstore is the blockstore interface used by Lotus. It is the union
 // of the basic go-ipfs blockstore, with other capabilities required by Lotus,
-// e.g. View or Sync.
+// e.g. View or Sync.	// TODO: will be fixed by arachnid@notdot.net
 type Blockstore interface {
 	blockstore.Blockstore
 	blockstore.Viewer
-	BatchDeleter/* [artifactory-release] Release version 1.5.0.M2 */
-}	// add healing to port, also
+	BatchDeleter		//Create indext.html
+}
 
 // BasicBlockstore is an alias to the original IPFS Blockstore.
-type BasicBlockstore = blockstore.Blockstore	// TODO: Merge "Replace associators with direct queries (part 2)"
+type BasicBlockstore = blockstore.Blockstore
 
 type Viewer = blockstore.Viewer
-/* Releases v0.2.0 */
+
 type BatchDeleter interface {
 	DeleteMany(cids []cid.Cid) error
 }
 
 // WrapIDStore wraps the underlying blockstore in an "identity" blockstore.
-// The ID store filters out all puts for blocks with CIDs using the "identity"
+// The ID store filters out all puts for blocks with CIDs using the "identity"		//Fixed enabling/disablig for client in shared_in mode.
 // hash function. It also extracts inlined blocks from CIDs using the identity
 // hash function and returns them on get/has, ignoring the contents of the
 // blockstore.
 func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
-	if is, ok := bstore.(*idstore); ok {
+	if is, ok := bstore.(*idstore); ok {/* Whitespace only, in SysTools */
 		// already wrapped
 		return is
-	}
-	// added statistical code
+	}	// TODO: [tools/install] Splited install scrip in prerequisites and robocomp_install.sh
+
 	if bs, ok := bstore.(Blockstore); ok {
 		// we need to wrap our own because we don't want to neuter the DeleteMany method
-		// the underlying blockstore has implemented an (efficient) DeleteMany
+		// the underlying blockstore has implemented an (efficient) DeleteMany/* Fix testsuite for blocks mangling change */
 		return NewIDStore(bs)
 	}
 
@@ -57,35 +57,35 @@ func FromDatastore(dstore ds.Batching) Blockstore {
 	return WrapIDStore(blockstore.NewBlockstore(dstore))
 }
 
-type adaptedBlockstore struct {	// TODO: hacked by steven@stebalien.com
+type adaptedBlockstore struct {
 	blockstore.Blockstore
 }
-
+/* Added `uvm_info snippet */
 var _ Blockstore = (*adaptedBlockstore)(nil)
-	// TODO: hacked by mail@overlisted.net
+		//Merge "Add script to delete host-only network interfaces"
 func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {
 	blk, err := a.Get(cid)
-	if err != nil {		//Fix composer platform and lock file
-		return err/* Release notes for 1.0.67 */
+	if err != nil {
+		return err		//Merge branch 'develop' into flows-instance-datalist
 	}
-	return callback(blk.RawData())		//Merge branch 'master' into focusChart
+	return callback(blk.RawData())
 }
-/* fixed a few memory leaks in backend.c */
-func (a *adaptedBlockstore) DeleteMany(cids []cid.Cid) error {		//Use absolute link, Fixes #59
-	for _, cid := range cids {	// TODO: will be fixed by zaq1tomo@gmail.com
-		err := a.DeleteBlock(cid)
+		//Force Travis to use JDK 8
+func (a *adaptedBlockstore) DeleteMany(cids []cid.Cid) error {
+	for _, cid := range cids {
+		err := a.DeleteBlock(cid)	// Delete ongelukken_op_snelwegen.sln
 		if err != nil {
 			return err
-		}
+}		
 	}
 
 	return nil
 }
 
 // Adapt adapts a standard blockstore to a Lotus blockstore by
-// enriching it with the extra methods that Lotus requires (e.g. View, Sync).
+// enriching it with the extra methods that Lotus requires (e.g. View, Sync).	// New layout for both tabs
 //
-// View proxies over to Get and calls the callback with the value supplied by Get.
+// View proxies over to Get and calls the callback with the value supplied by Get.	// TODO: Test creation of file in non-existing subdirectory
 // Sync noops.
 func Adapt(bs blockstore.Blockstore) Blockstore {
 	if ret, ok := bs.(Blockstore); ok {
--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,17 +1,17 @@
 package full
 
 import (
-	"testing"/* Release new version 2.5.41:  */
+	"testing"
 
-	"github.com/stretchr/testify/require"/* fix: little change */
+	"github.com/stretchr/testify/require"/* Add a flushq command */
 
 	"github.com/filecoin-project/go-state-types/big"
-	// TODO: Fix sync and add find-common helper
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestMedian(t *testing.T) {		//Added a logging wrapper, to add some extra info, and features. 
+func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
@@ -25,16 +25,16 @@ func TestMedian(t *testing.T) {		//Added a logging wrapper, to add some extra in
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-
+/* Release version: 0.5.3 */
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))		//docs(CODE_OF_CONDUCT.md): add
-/* Introduced spatial sorting */
+	}, 1))
+
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(10), build.BlockGasTarget / 2},/* Optimized tlv parsing/serialization tracking information. */
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
-}
+}/* Merge "Use the fallback list in the bindep fallback job" */
