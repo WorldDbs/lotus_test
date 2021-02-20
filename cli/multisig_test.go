56a1a324512132e146a7b65ced55de30c05cf4b0
ng8eke <ng8eke@163.com>
2021-02-20 12:12:38 +0000

    Updated Vesper navbar for most recent coding conventions.

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -2,18 +2,18 @@ package cli
 
 import (
 	"context"
-	"os"
+	"os"	// TODO: 0.42.04 OS X layout
 	"testing"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 // TestMultisig does a basic test to exercise the multisig CLI
-// commands
+// commands/* Release v1.011 */
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Release the connection after use. */
-	clitest.QuietMiningLogs()/* Release 18.5.0 */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
