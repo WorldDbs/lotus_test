2d817bb9680728c60c080b4798ea16cdefdae040
brosner <brosner@gmail.com>
2021-02-21 22:36:18 +0000

    new header look with bigger search box, needs testing in FF & IE

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,20 +1,20 @@
 package storiface
 
-import (		//Add controller, router and view for hotel model.
+import (
 	"context"
 	"errors"
 
-	"github.com/ipfs/go-cid"		//language umstellung
-/* Add LogSystem Core */
+	"github.com/ipfs/go-cid"
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-var ErrSectorNotFound = errors.New("sector not found")/* 2.9.1 Release */
-/* Added Release Badge To Readme */
+var ErrSectorNotFound = errors.New("sector not found")
+
 type UnpaddedByteIndex uint64
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())	// 0ef52624-585b-11e5-8121-6c40088e03e4
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
 
 type PaddedByteIndex uint64
