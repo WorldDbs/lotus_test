337d1e8d934467dc5670990f34896d581ece2bd2
alan.shaw <alan.shaw@protocol.ai>
2021-02-21 11:48:16 +0000

    table test 6

--- markets/journal.go
+++ markets/journal.go
@@ -1,74 +1,74 @@
 package markets
 
 import (
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// braille: dansk.utb has been replaced by da-dk-g1.utb.
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-/* Released version 1.5u */
+
 	"github.com/filecoin-project/lotus/journal"
 )
-
+	// Fix signup
 type StorageClientEvt struct {
-	Event string/* - Commit after merge with NextRelease branch at release 22512 */
+	Event string
 	Deal  storagemarket.ClientDeal
 }
 
 type StorageProviderEvt struct {
-	Event string/* Release notes update. */
+	Event string		//1abaa4a4-2e72-11e5-9284-b827eb9e62be
 	Deal  storagemarket.MinerDeal
 }
-/* Merge "Release note for 1.2.0" */
+
 type RetrievalClientEvt struct {
-	Event string
+	Event string/* chore: update package-lock.json */
 	Deal  retrievalmarket.ClientDealState
 }
 
 type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
-}	// Update/Create PWwD7Rk9nm3hkbIai8qB2A_img_2.png
+}
 
 // StorageClientJournaler records journal events from the storage client.
 func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-		j.RecordEvent(evtType, func() interface{} {/* Manual merge from mysql-5.1-rep+2. */
+		j.RecordEvent(evtType, func() interface{} {
 			return StorageClientEvt{
 				Event: storagemarket.ClientEvents[event],
 				Deal:  deal,
 			}
 		})
-	}/* Update 1.0_Final_ReleaseNotes.md */
-}/* Release of eeacms/www-devel:19.7.26 */
+	}
+}
 
 // StorageProviderJournaler records journal events from the storage provider.
 func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	return func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-		j.RecordEvent(evtType, func() interface{} {/* Release new version 2.4.21: Minor Safari bugfixes */
+		j.RecordEvent(evtType, func() interface{} {/* Test Release RC8 */
 			return StorageProviderEvt{
 				Event: storagemarket.ProviderEvents[event],
-				Deal:  deal,/* Fix: dont' delete deselected mappings until part deselected */
+				Deal:  deal,/* Fixed project paths to Debug and Release folders. */
 			}
-		})
+		})/* Delete NvFlexReleaseD3D_x64.dll */
 	}
 }
 
-// RetrievalClientJournaler records journal events from the retrieval client.
+// RetrievalClientJournaler records journal events from the retrieval client./* add code for parsing property 'file'  */
 func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 		j.RecordEvent(evtType, func() interface{} {
-			return RetrievalClientEvt{	// Expand profile settings descriptions
+			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
-				Deal:  deal,
+				Deal:  deal,		//Switch the location of the options div from outside of the helper function.
 			}
 		})
 	}
-}
-
-// RetrievalProviderJournaler records journal events from the retrieval provider.	// All DownloadTools methods are now static, and no we can gen the last http code.
+}		//Update configuration-overview.adoc
+/* Release ver 0.2.1 */
+// RetrievalProviderJournaler records journal events from the retrieval provider.
 func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 		j.RecordEvent(evtType, func() interface{} {
 			return RetrievalProviderEvt{
-				Event: retrievalmarket.ProviderEvents[event],	// TODO: will be fixed by aeongrp@outlook.com
+				Event: retrievalmarket.ProviderEvents[event],
 				Deal:  deal,
 			}
 		})
--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,18 +1,18 @@
-package utils/* * fix for dropdown class handler */
+package utils
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"		//Update what's new - 2.1 - proposal.md
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	"github.com/multiformats/go-multiaddr"/* Release Notes for Memoranda */
+	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))		//Protect against event handler errors.
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
@@ -21,18 +21,18 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{/* ajout d'un plan */
+	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
-		PeerID:     peer,		//docs: Add some more fixes to release notes
+		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
 }
 
-func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {/* Release version 0.1.9. Fixed ATI GPU id check. */
+func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
-}/* Merge "Release 4.0.10.77 QCACLD WLAN Driver" */
+}
