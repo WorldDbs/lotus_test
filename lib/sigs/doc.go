af15ad6258a5fae15193d599fb205c6ed4c9f254
alan.shaw <alan.shaw@protocol.ai>
2021-02-21 21:45:51 +0000

    Renamed Shape to Shape2D

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
+// Package sigs allows for signing, verifying signatures and key generation	// TODO: Changed server ports back to 8080.
 // using key types selected by package user.
-//	// TODO: remove sleep , accelerate the seeed-voicecard service startup
+//
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Release FPCM 3.5.0 */
+package sigs	// Delete register.blade.php
