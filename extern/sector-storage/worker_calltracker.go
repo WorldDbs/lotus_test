537628690c09421c67929fb27f2d506346a4d0ec
alex.gaynor <alex.gaynor@gmail.com>
2021-02-21 02:52:08 +0000

    Create google-loader.html

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,20 +1,20 @@
 package sectorstorage
 
-import (
+import (/* Release the GIL for pickled communication */
 	"fmt"
 	"io"
 
-	"github.com/filecoin-project/go-statestore"
+	"github.com/filecoin-project/go-statestore"	// TODO: test improvement.
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type workerCallTracker struct {
+type workerCallTracker struct {/* [TASK] Mention permission fix on file write */
 	st *statestore.StateStore // by CallID
 }
-
+		//added delete for completeness
 type CallState uint64
 
 const (
@@ -23,7 +23,7 @@ const (
 	// returned -> remove
 )
 
-type Call struct {
+type Call struct {/* Build OTP/Release 21.1 */
 	ID      storiface.CallID
 	RetType ReturnType
 
@@ -35,9 +35,9 @@ type Call struct {
 func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
 	return wt.st.Begin(ci, &Call{
 		ID:      ci,
-		RetType: rt,
+		RetType: rt,/* chore: Fix Semantic Release */
 		State:   CallStarted,
-	})
+	})		//Display sections and modules as list rather than buttons
 }
 
 func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
@@ -53,13 +53,13 @@ func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	st := wt.st.Get(ci)
 	return st.End()
 }
-
+/* Update SetVersionReleaseAction.java */
 func (wt *workerCallTracker) unfinished() ([]Call, error) {
 	var out []Call
 	return out, wt.st.List(&out)
 }
-
-// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
+	// TODO: No arg Nono.subscribe()
+// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len/* Making VPTree knn-search use an explicit stack  */
 type ManyBytes struct {
 	b []byte
 }
@@ -67,28 +67,28 @@ type ManyBytes struct {
 const many = 100 << 20
 
 func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
-	if t == nil {
+	if t == nil {/* Merge branch 'master' into NODE-716-caseobj-functions */
 		t = &ManyBytes{}
 	}
 
 	if len(t.b) > many {
 		return xerrors.Errorf("byte array in field t.Result was too long")
 	}
-
+/* Release new version 2.5.39:  */
 	scratch := make([]byte, 9)
 
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.b))); err != nil {
 		return err
-	}
+	}		//Debug logging for test-kitchen.
 
-	if _, err := w.Write(t.b[:]); err != nil {
+	if _, err := w.Write(t.b[:]); err != nil {	// Adding Flume interceptor and serializer
 		return err
 	}
 	return nil
-}
+}	// 8b3e754c-2e4b-11e5-9284-b827eb9e62be
 
 func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
-	*t = ManyBytes{}
+	*t = ManyBytes{}/* Fix compile and link errors in work stealing queue */
 
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 9)
