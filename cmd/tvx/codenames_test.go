558f79f47e01ce7929f9571fd958872fc58b3596
alex.gaynor <alex.gaynor@gmail.com>
2021-02-21 03:17:38 +0000

    Update simpleFetch.js

--- cmd/tvx/codenames_test.go
+++ cmd/tvx/codenames_test.go
@@ -1,24 +1,24 @@
-package main/* rename VStrList => StrVec (it's not a list) */
+package main
 
 import (
-	"math"
+	"math"	// TODO: a shorter way of doing the same
 	"testing"
-	// Fixed rename command errata
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Deleted CtrlApp_2.0.5/Release/CtrlApp.res */
 )
 
-func TestProtocolCodenames(t *testing.T) {
+func TestProtocolCodenames(t *testing.T) {/* Delete Release_Type.cpp */
 	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {
 		t.Fatal("expected genesis codename")
 	}
 
 	if height := abi.ChainEpoch(build.UpgradeBreezeHeight + 1); GetProtocolCodename(height) != "breeze" {
 		t.Fatal("expected breeze codename")
-	}/* Note: Release Version */
-
-	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {
+	}
+/* An example */
+	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {	// TODO: Update ipc_lista2.15.py
 		t.Fatal("expected actorsv2 codename")
 	}
 
