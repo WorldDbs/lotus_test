e8e835702bcb378535af9e02467d5c7f48d2419c
ng8eke <ng8eke@163.com>
2021-02-21 22:58:31 +0000

    add more missing files

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -2,70 +2,70 @@ package sealing
 
 type SectorState string
 
-var ExistSectorStateList = map[SectorState]struct{}{	// TODO: db1b0d1a-4b19-11e5-9267-6c40088e03e4
-	Empty:                {},
+var ExistSectorStateList = map[SectorState]struct{}{
+	Empty:                {},		//f520647c-2e6c-11e5-9284-b827eb9e62be
 	WaitDeals:            {},
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
 	GetTicket:            {},
 	PreCommit1:           {},
-	PreCommit2:           {},/* 3b058d3e-2e6b-11e5-9284-b827eb9e62be */
-	PreCommitting:        {},
-	PreCommitWait:        {},	// TODO: Rename ardLogFilter.py to filtration/ardLogFilter.py
-	WaitSeed:             {},		//053e9966-2e57-11e5-9284-b827eb9e62be
+	PreCommit2:           {},
+	PreCommitting:        {},/* Fix Maven Shield */
+	PreCommitWait:        {},
+	WaitSeed:             {},		//handle placeholder for inBox
 	Committing:           {},
-	SubmitCommit:         {},	// TODO: Merge "const correctness, validPixel test."
+	SubmitCommit:         {},		//Merge "Harden and clean up KeyGenParameterSpec." into mnc-dev
 	CommitWait:           {},
 	FinalizeSector:       {},
 	Proving:              {},
-	FailedUnrecoverable:  {},
+	FailedUnrecoverable:  {},		//response should convert the body to json
 	SealPreCommit1Failed: {},
 	SealPreCommit2Failed: {},
 	PreCommitFailed:      {},
-,}{   :deliaFfoorPetupmoC	
-	CommitFailed:         {},/* Release 1.13 Edit Button added */
-	PackingFailed:        {},
-	FinalizeFailed:       {},	// TODO: Code for Website
+	ComputeProofFailed:   {},
+	CommitFailed:         {},
+	PackingFailed:        {},/* Changed Brand Color Back */
+	FinalizeFailed:       {},
 	DealsExpired:         {},
 	RecoverDealIDs:       {},
 	Faulty:               {},
 	FaultReported:        {},
 	FaultedFinal:         {},
 	Terminating:          {},
-	TerminateWait:        {},/* 7d34326c-2e68-11e5-9284-b827eb9e62be */
+	TerminateWait:        {},
 	TerminateFinality:    {},
-	TerminateFailed:      {},
-	Removing:             {},	// cap recipes for building remote installers.
+	TerminateFailed:      {},		//Merge "Add release notes for 1.4.0.0a4"
+	Removing:             {},	// TODO: Update pexels.py
 	RemoveFailed:         {},
 	Removed:              {},
-}	// TODO: Fixing missing quote
-	// TODO: Update .config.us.json
+}
+
 const (
-	UndefinedSectorState SectorState = ""	// TODO: debian/postinst: LP: #796422
+	UndefinedSectorState SectorState = ""/* Update and rename filtering.lua to badword.lua */
 
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* Merged branch feature/BoxStairsTool into develop */
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
-	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
-	GetTicket      SectorState = "GetTicket"     // generate ticket
+	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain	// TODO: `text-rendering: optimizeLegibility` all of the things!
+	GetTicket      SectorState = "GetTicket"     // generate ticket		//Delete virtualization-anchor.png
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2		//Update and rename security_protocol to security_protocol.md
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
-	Committing     SectorState = "Committing"    // compute PoRep/* Release for v11.0.0. */
+	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
-	FinalizeSector SectorState = "FinalizeSector"
-	Proving        SectorState = "Proving"
+	FinalizeSector SectorState = "FinalizeSector"/* Fix included files */
+	Proving        SectorState = "Proving"/* Merge "[INTERNAL] sap.ui.performance: Minor JSDoc improvements" */
 	// error modes
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
-	AddPieceFailed       SectorState = "AddPieceFailed"
+	AddPieceFailed       SectorState = "AddPieceFailed"/* Release v0.1.3 */
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
-	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
-	PreCommitFailed      SectorState = "PreCommitFailed"		//New Exceptions. 
+	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
+	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
 	CommitFailed         SectorState = "CommitFailed"
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
