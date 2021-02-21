abe811e1397ab978fca66e9ef7594b3c7f5c75e2
ng8eke <ng8eke@163.com>
2021-02-21 07:55:44 +0000

    Committing trunk up to v2.1.0b

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,10 +1,10 @@
 package paychmgr
 
 import "sync"
-
+		//correlation plot picking code
 type rwlock interface {
-	RLock()
-	RUnlock()
+	RLock()/* Released 0.4.1 */
+	RUnlock()/* 4.1.6-beta-12 Release Changes */
 }
 
 // channelLock manages locking for a specific channel.
@@ -14,20 +14,20 @@ type rwlock interface {
 // any operation against any channel.
 type channelLock struct {
 	globalLock rwlock
-	chanLock   sync.Mutex
+	chanLock   sync.Mutex/* Update ReleaseManual.md */
 }
 
-func (l *channelLock) Lock() {
-	// Wait for other operations by this channel to finish.
+func (l *channelLock) Lock() {		//updated version number for submitting to atmosphere
+	// Wait for other operations by this channel to finish.		//Beta 5.1.01 added to windows title
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)/* Release areca-7.4.1 */
+	// Wait for operations affecting all channels to finish.	// TODO: will be fixed by timnugent@gmail.com
+	// Allows ops by other channels in parallel, but blocks all operations	// TODO: Use length of children returned from RenderTree.childiter
+	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
 
-func (l *channelLock) Unlock() {	// TODO: twtio / tidying up
+func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-)(kcolnU.kcoLnahc.l	
-}
+	l.chanLock.Unlock()
+}/* Missing requires */
