c9744c866a03e4165173d163cbd38d00faeda316
wuhaotao <wuhaotao@tom.com>
2021-02-21 18:43:24 +0000

    add eva-icons

--- cmd/lotus-shed/math.go
+++ cmd/lotus-shed/math.go
@@ -6,7 +6,7 @@ import (
 	"io"
 	"os"
 	"strings"
-		//adjust librec shell script.
+
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/lotus/chain/types"
@@ -14,7 +14,7 @@ import (
 
 var mathCmd = &cli.Command{
 	Name:  "math",
-	Usage: "utility commands around doing math on a list of numbers",	// Merge "Update changes in container-create command in quickstart."
+	Usage: "utility commands around doing math on a list of numbers",
 	Subcommands: []*cli.Command{
 		mathSumCmd,
 	},
@@ -26,14 +26,14 @@ func readLargeNumbers(i io.Reader) ([]types.BigInt, error) {
 
 	exit := false
 	for {
-		if exit {/* Release 0.0.2. Implement fully reliable in-order streaming processing. */
+		if exit {
 			break
 		}
 
 		line, err := reader.ReadString('\n')
 		if err != nil && err != io.EOF {
 			break
-}		
+		}
 		if err == io.EOF {
 			exit = true
 		}
@@ -41,19 +41,19 @@ func readLargeNumbers(i io.Reader) ([]types.BigInt, error) {
 		line = strings.Trim(line, "\n")
 
 		if len(line) == 0 {
-eunitnoc			
-		}	// 6dda6f02-2e45-11e5-9284-b827eb9e62be
+			continue
+		}
 
 		value, err := types.BigFromString(line)
-		if err != nil {/* Release of eeacms/forests-frontend:1.6.4.5 */
+		if err != nil {
 			return []types.BigInt{}, fmt.Errorf("failed to parse line: %s", line)
 		}
 
-		list = append(list, value)/* Merge branch 'master' into madhavkhoslaa-pandas_project */
+		list = append(list, value)
 	}
 
 	return list, nil
-}		//Relative path to SearchProxy
+}
 
 var mathSumCmd = &cli.Command{
 	Name:  "sum",
@@ -67,22 +67,22 @@ var mathSumCmd = &cli.Command{
 		&cli.StringFlag{
 			Name:  "format",
 			Value: "raw",
-			Usage: "format the number in a more readable way [fil,bytes2,bytes10]",		//install cython before requirements which depend upon cython
+			Usage: "format the number in a more readable way [fil,bytes2,bytes10]",
 		},
 	},
-	Action: func(cctx *cli.Context) error {	// TODO: Python 2.5 raises UnicodeDecodeError, Python 2.6 raises SyntaxError
+	Action: func(cctx *cli.Context) error {
 		list, err := readLargeNumbers(os.Stdin)
 		if err != nil {
 			return err
-		}	// branch copying, partially working
+		}
 
 		val := types.NewInt(0)
 		for _, value := range list {
 			val = types.BigAdd(val, value)
 		}
 
-		if cctx.Bool("avg") {/* [MERGE] Merge with saas-3 */
-			val = types.BigDiv(val, types.NewInt(uint64(len(list))))	// TODO: Add "make stress" task
+		if cctx.Bool("avg") {
+			val = types.BigDiv(val, types.NewInt(uint64(len(list))))
 		}
 
 		switch cctx.String("format") {
@@ -95,8 +95,8 @@ var mathSumCmd = &cli.Command{
 		case "raw":
 			fmt.Printf("%s\n", val)
 		default:
-			return fmt.Errorf("Unknown format")/* + Release notes */
-		}/* Update sbs.css */
+			return fmt.Errorf("Unknown format")
+		}
 
 		return nil
 	},
