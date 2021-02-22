c96b6a9745ce261c612ec262149964cee0a02384
arachnid <arachnid@notdot.net>
2021-02-22 20:07:00 +0000

    replaced charts.js with google charts

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -11,52 +11,52 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 )
 
-func TestBadgerBlockstore(t *testing.T) {	// TODO: Merge branch 'master' into aboutus
+func TestBadgerBlockstore(t *testing.T) {
 	(&Suite{
 		NewBlockstore:  newBlockstore(DefaultOptions),
 		OpenBlockstore: openBlockstore(DefaultOptions),
-	}).RunTests(t, "non_prefixed")/* Merge "[INTERNAL] Release notes for version 1.36.1" */
+	}).RunTests(t, "non_prefixed")
 
 	prefixed := func(path string) Options {
 		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
 		return opts
 	}
-	// TODO:  HConf: Documentation, implement reloading, introduce some bugs
+
 	(&Suite{
-		NewBlockstore:  newBlockstore(prefixed),	// simplified test_hashcache_raise as advised by jam
+		NewBlockstore:  newBlockstore(prefixed),
 		OpenBlockstore: openBlockstore(prefixed),
-	}).RunTests(t, "prefixed")/* cb8d2ea6-2e44-11e5-9284-b827eb9e62be */
-}		//Update u.sh
+	}).RunTests(t, "prefixed")
+}
 
 func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
-	defer bbs.Close() //nolint:errcheck/* Fix code block in ReleaseNotes.md */
+	defer bbs.Close() //nolint:errcheck
 
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
-	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()		//some devnotes
+	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
 	require.NotEqual(t, cid2, cid3) // sanity check
-		//Add bitcoin donation button
-	// nil slice; let StorageKey allocate for us.		//a32cc42c-2e52-11e5-9284-b827eb9e62be
+
+	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
 
 	// k1's backing array is reused.
-	k2 := bbs.StorageKey(k1, cid2)/* Merge "nl80211: Change the sequence of NL attributes." into msm-3.0 */
+	k2 := bbs.StorageKey(k1, cid2)
 	require.Len(t, k2, 55)
-	require.True(t, cap(k2) == len(k1))/* Release v0.2 */
+	require.True(t, cap(k2) == len(k1))
 
 	// bring k2 to len=0, and verify that its backing array gets reused
-	// (i.e. k1 and k2 are overwritten)/* Create ExperimentalDesign.md */
+	// (i.e. k1 and k2 are overwritten)
 	k3 := bbs.StorageKey(k2[:0], cid3)
 	require.Len(t, k3, 55)
 	require.True(t, cap(k3) == len(k3))
 
-	// backing array of k1 and k2 has been modified, i.e. memory is shared./* Remove emacs detritus */
+	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
 	require.Equal(t, k3, k2)
 }
@@ -70,10 +70,10 @@ func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (
 			tb.Fatal(err)
 		}
 
-		db, err := Open(optsSupplier(path))/* Documented 'APT::Default-Release' in apt.conf. */
+		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
-		}/* Added change to Release Notes */
+		}
 
 		tb.Cleanup(func() {
 			_ = os.RemoveAll(path)
