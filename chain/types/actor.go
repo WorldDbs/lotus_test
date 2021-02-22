d18ec5b08564fb4fd4be5b9c43f8a89d5ed8e912
alan.shaw <alan.shaw@protocol.ai>
2021-02-22 23:14:45 +0000

    Added generic parameter to mapReduce function

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
 
 import (
-	"errors"
+	"errors"/* Merge branch 'master' into bugfix/cutter_unit_test */
 
 	"github.com/ipfs/go-cid"
 )
 
 var ErrActorNotFound = errors.New("actor not found")
 
-type Actor struct {
+type Actor struct {/* First commit to add file */
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64
+	Nonce   uint64	// TODO: hacked by nicksavers@gmail.com
 	Balance BigInt
 }
