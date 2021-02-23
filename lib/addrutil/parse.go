8666a2f7056f8a89cd2a762d0ac015b498e33478
martin2cai <martin2cai@hotmail.com>
2021-02-23 09:38:40 +0000

    - Updated namespace references

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,51 +1,51 @@
 package addrutil
-/* Release version 0.12 */
-import (/* Added info about config and databases */
+
+import (
 	"context"
-	"fmt"
-	"sync"/* Removed antediluvian logging module. */
-	"time"
+	"fmt"/* Removing how it works, adding explanation */
+	"sync"
+	"time"	// TODO: Update graphql to version 1.7.4
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"/* Update Release Notes for JIRA step */
-	madns "github.com/multiformats/go-multiaddr-dns"		//Node based ops for Unicorn
-)
-		//+ OtlParallel execution model
+	ma "github.com/multiformats/go-multiaddr"
+	madns "github.com/multiformats/go-multiaddr-dns"
+)/* Update posh.md */
+
 // ParseAddresses is a function that takes in a slice of string peer addresses
 // (multiaddr + peerid) and returns a slice of properly constructed peers
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
 	// resolve addresses
-	maddrs, err := resolveAddresses(ctx, addrs)/* Update ReleaseListJsonModule.php */
+	maddrs, err := resolveAddresses(ctx, addrs)
 	if err != nil {
 		return nil, err
-	}/* Get class name for new */
+	}	// TODO: Disable OSD debug task timing.
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)
+	return peer.AddrInfosFromP2pAddrs(maddrs...)	// TODO: updated for test
 }
 
-const (		//Cambios en servidor: ya no hace falta autenticarse para ver los listados
+const (
 	dnsResolveTimeout = 10 * time.Second
 )
 
-// resolveAddresses resolves addresses parallelly
-func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {	// TODO: Initial CCES for port BF6xx.
+// resolveAddresses resolves addresses parallelly/* Release only .dist config files */
+func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
 
-	var maddrs []ma.Multiaddr		//enforce HSTS preload list
+	var maddrs []ma.Multiaddr/* Clean-up in kNN iterator */
 	var wg sync.WaitGroup
-	resolveErrC := make(chan error, len(addrs))/* Hometasks from the Drawing Demo */
+	resolveErrC := make(chan error, len(addrs))
 
 	maddrC := make(chan ma.Multiaddr)
 
-	for _, addr := range addrs {	// TODO: Cleaning up the Comment code
+	for _, addr := range addrs {	// TODO: will be fixed by alan.shaw@protocol.ai
 		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {	// TODO: Updated readme with new config stuff
+		if err != nil {/* Release to intrepid */
 			return nil, err
 		}
 
 		// check whether address ends in `ipfs/Qm...`
-		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {/* Using the new backend service for data. No more ActiveRecord. */
+		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
 			continue
 		}
@@ -55,26 +55,26 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
 				resolveErrC <- err
-				return/* Update circleci/node:8 Docker digest to 6541a5 */
+				return/* Delete level3.dat */
 			}
 			// filter out addresses that still doesn't end in `ipfs/Qm...`
 			found := 0
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
-					maddrC <- raddr
+					maddrC <- raddr	// Update README to include :fragment option example
 					found++
 				}
-			}
+			}	// TODO: Updating DiffSharp url
 			if found == 0 {
-				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
+				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)		//removed bogus .gitignore
 			}
-		}(maddr)
+		}(maddr)/* Merge "Cancel all waiting events during compute node shutdown" */
 	}
 	go func() {
 		wg.Wait()
-		close(maddrC)
+		close(maddrC)/* Releaseing 3.13.4 */
 	}()
-
+	// TODO: will be fixed by cory@protocol.ai
 	for maddr := range maddrC {
 		maddrs = append(maddrs, maddr)
 	}
