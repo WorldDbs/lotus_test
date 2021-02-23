f9fe3394bdd84522c72a4b0c82d7ec6cfad95ae7
wuhaotao <wuhaotao@tom.com>
2021-02-23 17:00:42 +0000

    bumped to version 4.25.2

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,22 +1,22 @@
 package modules
 
-import (
+import (/* add Shelloid */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"		//c2ec031e-2e56-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/node/repo"
-	"github.com/ipfs/go-graphsync"
+	"github.com/ipfs/go-graphsync"/* Issue #1537872 by Steven Jones: Fixed Release script reverts debian changelog. */
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"/* trying to format md correctly */
+	"github.com/libp2p/go-libp2p-core/peer"/* Update for Release v3.1.1 */
 	"go.uber.org/fx"
-)	// TODO: hacked by arachnid@notdot.net
+)
 
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
-		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
+		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)/* Merge "Release alternative src directory support" */
 		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
 
@@ -27,15 +27,15 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 		if err != nil {
 			return nil, err
 		}
-		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {/* NEWS fixes for post 0.15-rc1 */
+			_, has := requestData.Extension("chainsync")/* Update jackson to 2.12.3 */
 			if has {
-				// TODO: we should confirm the selector is a reasonable one before we validate
+				// TODO: we should confirm the selector is a reasonable one before we validate	// TODO: re-activate SiteProfile dependency
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
 			}
-		})
+		})/* Release notes in AggregateRepository.Core */
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
