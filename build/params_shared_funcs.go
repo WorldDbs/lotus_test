2af1e7a7522dba0e034b28d2ccde6b0423d21e93
arajasek94 <arajasek94@gmail.com>
2021-02-24 18:07:36 +0000

    Add Translations Section to README.md

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,38 +1,38 @@
-package build
+package build	// Correct spelling of ConTeXt in README.md
 
-import (	// [#157] HBI on weekly basis
+import (		//updated listener syntax
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-/* Blue styles for DFW toolbar, props jorbin, fixes #17436 */
-	"github.com/libp2p/go-libp2p-core/protocol"/* Release v0.5.1.4 */
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"	// Make it clear that modifying an existing Windows image is also fine.
-)
-	// Added a serialiser for Meta Snomed in TriG syntax
-// Core network constants/* Release v0.97 */
+	"github.com/libp2p/go-libp2p-core/protocol"
 
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Upgrade Maven Release plugin for workaround of [PARENT-34] */
+)		//Missing angle bracket in pseudo JSON
+
+// Core network constants
+/* changed call from ReleaseDataverseCommand to PublishDataverseCommand */
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }/* Update releases to add rename dependencies feature */
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
-	// TODO: will be fixed by mail@overlisted.net
+		//Reverting apostrophes and double quotes
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
 }
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
-	if err != nil {
+	if err != nil {/* HDP start namenode, datanode, resourcemanager, nodemanager, historyserver */
 		panic(err)
 	}
 
-	return ret
+	return ret/* Release new version 2.5.61: Filter list fetch improvements */
 }
-
+	// TODO: hacked by greg@colvin.org
 func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
-	if err != nil {/* Merge branch 'alternate-testing' into OSxBundleEdit */
+	if err != nil {		//[FIX] XQuery: only bind context before compilation. Fixes #934
 		panic(err)
 	}
 
