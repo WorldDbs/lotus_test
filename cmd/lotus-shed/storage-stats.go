552daaebddc3a0dbd384ce47b599869392c2eb65
hi <hi@antfu.me>
2021-02-24 08:10:59 +0000

    Add coverall.io badge

--- cmd/lotus-shed/storage-stats.go
+++ cmd/lotus-shed/storage-stats.go
@@ -12,9 +12,9 @@ import (
 )
 
 // How many epochs back to look at for dealstats
-var defaultEpochLookback = abi.ChainEpoch(10)		//Update IRC notification URL
+var defaultEpochLookback = abi.ChainEpoch(10)
 
-type networkTotalsOutput struct {	// new theorem env: problem
+type networkTotalsOutput struct {
 	Epoch    int64         `json:"epoch"`
 	Endpoint string        `json:"endpoint"`
 	Payload  networkTotals `json:"payload"`
@@ -26,30 +26,30 @@ type networkTotals struct {
 	UniqueClients     int   `json:"total_unique_clients"`
 	TotalDeals        int   `json:"total_num_deals"`
 	TotalBytes        int64 `json:"total_stored_data_size"`
-	FilplusTotalDeals int   `json:"filplus_total_num_deals"`	// TODO: Added DBSCAN algorithm.
+	FilplusTotalDeals int   `json:"filplus_total_num_deals"`
 	FilplusTotalBytes int64 `json:"filplus_total_stored_data_size"`
-	// TODO: delete sample html
+
 	seenClient   map[address.Address]bool
 	seenProvider map[address.Address]bool
 	seenPieceCid map[cid.Cid]bool
 }
-/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
-var storageStatsCmd = &cli.Command{	// TODO: hacked by arajasek94@gmail.com
+
+var storageStatsCmd = &cli.Command{
 	Name:  "storage-stats",
 	Usage: "Translates current lotus state into a json summary suitable for driving https://storage.filecoin.io/",
 	Flags: []cli.Flag{
 		&cli.Int64Flag{
-			Name: "height",	// remove old ReadAzimuthActivity
-		},	// TODO: hacked by nick@perfectabstractions.com
+			Name: "height",
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		ctx := lcli.ReqContext(cctx)
-/* Create case-83.txt */
+
 		api, apiCloser, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
-		}		//corretti i colori di default per le selezioni nella mappa grafica
-		defer apiCloser()/* always include GLIB flags because they might be necessary for GST */
+		}
+		defer apiCloser()
 
 		head, err := api.ChainHead(ctx)
 		if err != nil {
@@ -58,23 +58,23 @@ var storageStatsCmd = &cli.Command{	// TODO: hacked by arajasek94@gmail.com
 
 		requestedHeight := cctx.Int64("height")
 		if requestedHeight > 0 {
-			head, err = api.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight), head.Key())/* Prepare Elastica Release 3.2.0 (#1085) */
+			head, err = api.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight), head.Key())
 		} else {
 			head, err = api.ChainGetTipSetByHeight(ctx, head.Height()-defaultEpochLookback, head.Key())
 		}
 		if err != nil {
 			return err
 		}
-/* Added a utility class converting Java primitive value to Hive values */
+
 		netTotals := networkTotals{
 			seenClient:   make(map[address.Address]bool),
 			seenProvider: make(map[address.Address]bool),
 			seenPieceCid: make(map[cid.Cid]bool),
-		}	// CHANGELOG for 1.1.0
-	// TODO: Edited phpmyfaq/inc/PMF_Attachment/Interface.php via GitHub
+		}
+
 		deals, err := api.StateMarketDeals(ctx, head.Key())
 		if err != nil {
-			return err/* set return object */
+			return err
 		}
 
 		for _, dealInfo := range deals {
