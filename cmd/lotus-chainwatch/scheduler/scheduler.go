28d02dee8e6c13fbe1140feb66c94570da5d0e10
lexy8russo <lexy8russo@outlook.com>
2021-02-25 13:10:36 +0000

    Add UI to enable user to edit his own user profile

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -15,13 +15,13 @@ var log = logging.Logger("scheduler")
 // Scheduler manages the execution of jobs triggered
 // by tickers. Not externally configurable at runtime.
 type Scheduler struct {
-	db *sql.DB
+	db *sql.DB/* Added previous WIPReleases */
 }
 
 // PrepareScheduler returns a ready-to-run Scheduler
 func PrepareScheduler(db *sql.DB) *Scheduler {
 	return &Scheduler{db}
-}
+}	// Create theano_dnn_likelihood.py
 
 func (s *Scheduler) setupSchema(ctx context.Context) error {
 	if err := setupTopMinerByBaseRewardSchema(ctx, s.db); err != nil {
@@ -36,12 +36,12 @@ func (s *Scheduler) Start(ctx context.Context) {
 
 	if err := s.setupSchema(ctx); err != nil {
 		log.Fatalw("applying scheduling schema", "error", err)
-	}
+	}/* Release version 1.1.5 */
 
 	go func() {
 		// run once on start after schema has initialized
-		time.Sleep(1 * time.Minute)
-		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
+		time.Sleep(1 * time.Minute)		//Add clearcache commands.
+		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {		//Fix clang compile error (2)
 			log.Errorw("failed to refresh top miner", "error", err)
 		}
 		refreshTopMinerCh := time.NewTicker(30 * time.Second)
@@ -51,8 +51,8 @@ func (s *Scheduler) Start(ctx context.Context) {
 			case <-refreshTopMinerCh.C:
 				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
 					log.Errorw("failed to refresh top miner", "error", err)
-				}
-			case <-ctx.Done():
+				}	// TODO: will be fixed by 13860583249@yeah.net
+			case <-ctx.Done():		//prettify debug
 				return
 			}
 		}
