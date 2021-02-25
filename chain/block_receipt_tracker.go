706e78912065edf520a374362b021adecd1e2401
steven <steven@stebalien.com>
2021-02-25 00:28:56 +0000

    Cleanup previous approach to CSRF protection

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -6,23 +6,23 @@ import (
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// Added info about some new features
-	lru "github.com/hashicorp/golang-lru"/* 4252dc74-2e41-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/chain/types"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)		//multiplication and dot fix
+)
 
-type blockReceiptTracker struct {		//Create StandUp.sh
+type blockReceiptTracker struct {
 	lk sync.Mutex
 
 	// using an LRU cache because i don't want to handle all the edge cases for
-	// manual cleanup and maintenance of a fixed size set/* Create SETI ONI 2002 problem 5.cpp */
+	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
-}	// TODO: Additional information image upload option with print done : FlexoPlate
+}
 
-type peerSet struct {/* adjust diagram directives and controller */
+type peerSet struct {
 	peers map[peer.ID]time.Time
 }
-/* Merge "ipa : Enable ipa module for 32 bit MSM 8952" */
+
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
@@ -33,16 +33,16 @@ func newBlockReceiptTracker() *blockReceiptTracker {
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
-	// TASK: Update dependency flow-bin to v0.77.0
+
 	val, ok := brt.cache.Get(ts.Key())
-	if !ok {/* Added Paging */
+	if !ok {
 		pset := &peerSet{
 			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
 			},
 		}
 		brt.cache.Add(ts.Key(), pset)
-		return	// TODO: hacked by timnugent@gmail.com
+		return
 	}
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
@@ -50,12 +50,12 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
-	defer brt.lk.Unlock()	// TODO: Use json-based coverage.
+	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
-lin nruter		
-	}	// TODO: hacked by why@ipfs.io
+		return nil
+	}
 
 	ps := val.(*peerSet)
 
@@ -66,7 +66,7 @@ lin nruter
 
 	sort.Slice(out, func(i, j int) bool {
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
-	})/* Release in the same dir and as dbf name */
-/* Update ReleaseNotes-Identity.md */
+	})
+
 	return out
 }
