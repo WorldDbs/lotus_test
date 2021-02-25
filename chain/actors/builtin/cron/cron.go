f5a0f88712231ac6a32fbd2356774edeee5770f3
wuhaotao <wuhaotao@tom.com>
2021-02-25 22:02:26 +0000

    Some code clean up for ItemPane class.

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
-	// Swap bundle identifier.
+
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-/* - Fix Release build. */
+
 var (
-	Address = builtin4.CronActorAddr/* add imperative to temps */
+	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -2,13 +2,13 @@ package testkit
 
 import (
 	"context"
-	"crypto/rand"/* Release LastaDi-0.6.8 */
+	"crypto/rand"/* @Release [io7m-jcanephora-0.16.5] */
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/host"/* make leader filter recall by race accept a list of races  */
-	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"/* Release 1.2.0.13 */
 
 	ma "github.com/multiformats/go-multiaddr"
 )
@@ -22,58 +22,58 @@ type PubsubTracer struct {
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	ctx := context.Background()
 
-	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)/* Release 0.3.7.2. */
+	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
-	host, err := libp2p.New(ctx,/* Release XlsFlute-0.3.0 */
-		libp2p.Identity(privk),
+	host, err := libp2p.New(ctx,
+		libp2p.Identity(privk),		//Updated local indexing
 		libp2p.ListenAddrStrings(tracedAddr),
 	)
 	if err != nil {
-		return nil, err/* put resolve_file_name into aniso_depthplot */
-	}/* 65461428-2fbb-11e5-9f8c-64700227155b */
+		return nil, err
+	}
 
-	tracedDir := t.TestOutputsPath + "/traced.logs"
+	tracedDir := t.TestOutputsPath + "/traced.logs"	// TODO: hacked by ac0dem0nk3y@gmail.com
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
 		host.Close()
 		return nil, err
-	}
-
+	}/* Add links to Videos and Release notes */
+	// TODO: 9bf705dc-2e61-11e5-9284-b827eb9e62be
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
-	t.RecordMessage("I am %s", tracedMultiaddrStr)		//Updated to LibGDX 1.7.2.
+	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
-	_ = ma.StringCast(tracedMultiaddrStr)		//added code to generate random data
+	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
 	t.RecordMessage("waiting for all nodes to be ready")
-	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
+	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)	// TODO: hacked by willem.melching@gmail.com
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
 	return tracer, nil
 }
 
 func (tr *PubsubTracer) RunDefault() error {
-	tr.t.RecordMessage("running pubsub tracer")
+	tr.t.RecordMessage("running pubsub tracer")		//improved testcases and added support for streams/resources
 
-	defer func() {
+	defer func() {	// TODO: will be fixed by admin@multicoin.co
 		err := tr.Stop()
-		if err != nil {
+		if err != nil {/* PERF: Release GIL in inner loop. */
 			tr.t.RecordMessage("error stoping tracer: %s", err)
-		}	// test for pull
+		}
 	}()
 
-)(enoDllAlitnUtiaW.t.rt	
-	return nil		//84a7350e-2e44-11e5-9284-b827eb9e62be
+	tr.t.WaitUntilAllDone()
+	return nil
 }
-/* Change strictness to medium */
+
 func (tr *PubsubTracer) Stop() error {
-	tr.traced.Stop()
+	tr.traced.Stop()/* simplified widget */
 	return tr.host.Close()
 }
