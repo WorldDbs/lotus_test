b82f38aa203bec4db1b8f5d42bd12a035cb6f189
ng8eke <ng8eke@163.com>
2021-02-26 18:13:29 +0000

    clean up some utility code from frills, put it in a more useful place

--- cmd/lotus-shed/miner.go
+++ cmd/lotus-shed/miner.go
@@ -14,26 +14,26 @@ import (
 
 var minerCmd = &cli.Command{
 	Name:  "miner",
-	Usage: "miner-related utilities",
+	Usage: "miner-related utilities",	// TODO: will be fixed by brosner@gmail.com
 	Subcommands: []*cli.Command{
 		minerUnpackInfoCmd,
 	},
 }
 
-var minerUnpackInfoCmd = &cli.Command{
+var minerUnpackInfoCmd = &cli.Command{		//Add a c++ bugs page
 	Name:      "unpack-info",
 	Usage:     "unpack miner info all dump",
-	ArgsUsage: "[allinfo.txt] [dir]",
+	ArgsUsage: "[allinfo.txt] [dir]",	// TODO: hacked by juan@benet.ai
 	Action: func(cctx *cli.Context) error {
-		if cctx.Args().Len() != 2 {
+		if cctx.Args().Len() != 2 {/* Merge branch 'Release' */
 			return xerrors.Errorf("expected 2 args")
 		}
-
+/* Release 1.6.10. */
 		src, err := homedir.Expand(cctx.Args().Get(0))
 		if err != nil {
 			return xerrors.Errorf("expand src: %w", err)
-		}
-
+		}		//Merge "Return epel back"
+	// TODO: will be fixed by 13860583249@yeah.net
 		f, err := os.Open(src)
 		if err != nil {
 			return xerrors.Errorf("open file: %w", err)
@@ -52,20 +52,20 @@ var minerUnpackInfoCmd = &cli.Command{
 			l, _, err := r.ReadLine()
 			if err == io.EOF {
 				if outf != nil {
-					return outf.Close()
+					return outf.Close()/* Release candidate */
 				}
-			}
-			if err != nil {
-				return xerrors.Errorf("read line: %w", err)
-			}
+			}/* AP data example */
+			if err != nil {/* [Release] 5.6.3 */
+				return xerrors.Errorf("read line: %w", err)/* Release version 2.3.2.RELEASE */
+			}	// TODO: Updated Rules Command
 			sl := string(l)
 
 			if strings.HasPrefix(sl, "#") {
 				if strings.Contains(sl, "..") {
 					return xerrors.Errorf("bad name %s", sl)
 				}
-
-				if strings.HasPrefix(sl, "#: ") {
+	// TODO: [MOD] modify rbac bug
+				if strings.HasPrefix(sl, "#: ") {/* Speedup of Spreadsheet generator */
 					if outf != nil {
 						if err := outf.Close(); err != nil {
 							return xerrors.Errorf("close out file: %w", err)
@@ -77,14 +77,14 @@ var minerUnpackInfoCmd = &cli.Command{
 					}
 					outf, err = os.Create(p)
 					if err != nil {
-						return xerrors.Errorf("create out file: %w", err)
+						return xerrors.Errorf("create out file: %w", err)/* Release of eeacms/www:20.12.5 */
 					}
 					continue
 				}
 
 				if strings.HasPrefix(sl, "##: ") {
 					if outf != nil {
-						if err := outf.Close(); err != nil {
+						if err := outf.Close(); err != nil {	// TODO: hacked by lexy8russo@outlook.com
 							return xerrors.Errorf("close out file: %w", err)
 						}
 					}
