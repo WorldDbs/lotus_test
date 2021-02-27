546bf69bb79a6ed560f79d340b89ce4c06fef42f
alan.shaw <alan.shaw@protocol.ai>
2021-02-27 07:33:23 +0000

    Comment out import of fmt

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,4 +1,4 @@
-package splitstore
+package splitstore		//1st Ed. of graph data model description
 
 import (
 	"path/filepath"
@@ -12,26 +12,26 @@ import (
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
-type MarkSet interface {/* i deleted it */
+type MarkSet interface {
 	Mark(cid.Cid) error
-	Has(cid.Cid) (bool, error)/* Multi-threaded jobs processing and debug messages */
+	Has(cid.Cid) (bool, error)
 	Close() error
 }
-
+/* Deleted msmeter2.0.1/Release/rc.command.1.tlog */
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
-
+	// TODO: will be fixed by joshua@yottadb.com
 type MarkSetEnv interface {
-	Create(name string, sizeHint int64) (MarkSet, error)
-	Close() error
-}/* exclude *.java in jlibs-examples.jar */
+	Create(name string, sizeHint int64) (MarkSet, error)	// TODO: Create Ian's Functional Turtle post
+	Close() error	// Delete kport
+}
 
-func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {	// TODO: will be fixed by lexy8russo@outlook.com
-	switch mtype {
+func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
+	switch mtype {/* build and display APA citation from configured metadata; refs #16152 */
 	case "", "bloom":
-		return NewBloomMarkSetEnv()
+		return NewBloomMarkSetEnv()	// TODO: will be fixed by fjl@ethereum.org
 	case "bolt":
-		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))/* Merge "Release notes for 1.17.0" */
+		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
