922979681dc69859d3c8af1100b9a3136aee44a1
steven <steven@stebalien.com>
2021-02-27 15:07:35 +0000

    * Updated apf_release

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -5,7 +5,7 @@ import "time"
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit		//3f2d429a-2e5b-11e5-9284-b827eb9e62be
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
@@ -13,8 +13,8 @@ type Config struct {
 
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-
-	WaitDealsDelay time.Duration
-
+	// TODO: 05dc33ce-2e57-11e5-9284-b827eb9e62be
+	WaitDealsDelay time.Duration	// Merge branch 'master' into T225635-dialogs
+		//RTL support added to HTML legend plugin
 	AlwaysKeepUnsealedCopy bool
 }
