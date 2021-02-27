0543acac73f7343c6fa1f4ab641b9868d2c52d14
ng8eke <ng8eke@163.com>
2021-02-27 22:42:28 +0000

    254b06c2-2e61-11e5-9284-b827eb9e62be

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
 package journal
-		//Fix Meltdown toolbar bug
+	// TODO: will be fixed by davidad@alum.mit.edu
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
-func NilJournal() Journal {/* reverse color bug fix */
+func NilJournal() Journal {
 	return nilj
 }
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}		//Revised all remaining strings
 
 func (n *nilJournal) Close() error { return nil }
