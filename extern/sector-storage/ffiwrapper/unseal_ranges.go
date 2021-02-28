d7a3936b401ef192df001f857de021fd945c422c
why <why@ipfs.io>
2021-02-28 20:45:10 +0000

    Create Pool.pm6

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,11 +1,11 @@
-package ffiwrapper
+package ffiwrapper		//Merge branch 'master' into dependabot/bundler/tilt-2.0.9
 
-import (
+import (/* launch inverse search relative to application directory */
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by mail@overlisted.net
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
@@ -14,14 +14,14 @@ import (
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Added Initial Release (TrainingTracker v1.0) Database\Sqlite File. */
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
-	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {
+	todo, err := rlepluslazy.Subtract(todo, unsealed)	// TODO: hacked by fjl@ethereum.org
+	if err != nil {		//CHANGE: debug statements and commons jar.
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
+	}	// bf2a79d2-2e73-11e5-9284-b827eb9e62be
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}
+}/* Adding Release Notes for 1.12.2 and 1.13.0 */
