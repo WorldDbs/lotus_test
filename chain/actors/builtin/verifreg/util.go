ae1fd32ac25badab58a3842a064aabdef7a87e6e
juan <juan@benet.ai>
2021-03-01 13:57:49 +0000

    Bypass Ubiquity bug

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,19 +1,19 @@
 package verifreg
-
-import (
+	// TODO: Initial revision to github
+import (/* Remove class name from default component */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//...all changes.
 	"golang.org/x/xerrors"
 )
 
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
-// checking boilerplate.
+// checking boilerplate./* Delete .~lock.sensorsemfronteiras-apresentacao.pptx# */
 //
 // "go made me do it"
-type rootFunc func() (adt.Map, error)
+type rootFunc func() (adt.Map, error)	// TODO: hacked by mail@bitpshr.net
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
@@ -27,7 +27,7 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
-		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
+		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)		//use much more let
 	} else if !found {
 		return false, big.Zero(), nil
 	}
@@ -43,10 +43,10 @@ func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr
 	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
-		a, err := address.NewFromBytes([]byte(key))
+		a, err := address.NewFromBytes([]byte(key))	// TODO: hacked by ligi@ligi.de
 		if err != nil {
 			return err
-		}
-		return cb(a, dcap)
+		}/* database: script adaptation for H2 */
+		return cb(a, dcap)	// TODO: Fix bug #791788: Store search not able to handle unicode characters.
 	})
 }
