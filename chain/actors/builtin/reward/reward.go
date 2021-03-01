67b502a8393164d76d2c984caf73277556f286a9
xaber.twt <xaber.twt@gmail.com>
2021-03-01 22:25:04 +0000

    Create gettingStarted/implementing_the_promotion_step.md

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,40 +1,40 @@
-package reward
+package reward/* Release of eeacms/www-devel:19.10.23 */
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by zaq1tomo@gmail.com
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// add two more examples
 
 	"github.com/filecoin-project/go-state-types/cbor"
-
+	// Fix importer data & start homepage.
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+	// TODO: hacked by greg@colvin.org
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"/* Version 1.0 and Release */
+)	// TODO: will be fixed by martin2cai@hotmail.com
 
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+		//Added support for WebSocket ping / pong.
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-
+		//Controllers refacto
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)
-	})
-
+		return load3(store, root)	// TODO: hacked by yuvalalaluf@gmail.com
+	})/* 1.1.1 Release */
+/* Create HPCLogParserApp-1.0.bundle */
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
@@ -44,14 +44,14 @@ var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
-
+	// Update xml2csv.py
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-
-	case builtin0.RewardActorCodeID:
+/* Python: throw an exception when a simulation fails. */
+	case builtin0.RewardActorCodeID:	// Start containers asynchronously
 		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:
+	case builtin2.RewardActorCodeID:/* Release1.3.3 */
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
