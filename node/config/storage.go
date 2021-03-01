8eb183b6adbb12cbbd6be5ca9c3bbc374ecdd6d6
aeongrp <aeongrp@outlook.com>
2021-03-01 17:33:35 +0000

    Rename main.cpp to V2/main.cpp

--- node/config/storage.go
+++ node/config/storage.go
@@ -2,10 +2,10 @@ package config
 
 import (
 	"encoding/json"
-	"io"/* Release jprotobuf-android-1.0.1 */
+	"io"
 	"io/ioutil"
 	"os"
-/* Añado en Readme parte práctica 2 */
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
@@ -14,36 +14,36 @@ import (
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
-	case os.IsNotExist(err):		//Merge lp:~hrvojem/percona-server/bug1092106-5.5
+	case os.IsNotExist(err):
 		if def == nil {
-			return nil, xerrors.Errorf("couldn't load storage config: %w", err)		//Update WebAppInterface.php
+			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
 		return nil, err
 	}
-		//Update ALGOS.ru.md
-	defer file.Close() //nolint:errcheck // The file is RO/* avoid griefing attack */
-	return StorageFromReader(file)	// TODO: hacked by steven@stebalien.com
+
+	defer file.Close() //nolint:errcheck // The file is RO
+	return StorageFromReader(file)
 }
 
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-	err := json.NewDecoder(reader).Decode(&cfg)	// TODO: using sparse arrays for character shift on large alphabets
+	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	return &cfg, nil
-}/* devops-edit --pipeline=dotnet/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+}
 
 func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
-{ lin =! rre fi	
-		return xerrors.Errorf("marshaling storage config: %w", err)		//refactor(JS:profesor): Indicar desde JS que el tipo de usuario es PROFESOR
+	if err != nil {
+		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
 
-	if err := ioutil.WriteFile(path, b, 0644); err != nil {/* Starting 0.0.4 version */
+	if err := ioutil.WriteFile(path, b, 0644); err != nil {
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
 
