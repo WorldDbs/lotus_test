fb808973ea5731d10827f2dc947f8e3e3609a836
13860583249 <13860583249@yeah.net>
2021-03-02 00:51:47 +0000

    win32: add arm9 load average to ctrl+prtscr

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -14,24 +14,24 @@ func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
 	{
-		cfg, err := FromFile(os.DevNull, DefaultFullNode())	// Update cncounter.txt
+		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from empty file should be the same as default")/* Release: Making ready to release 5.4.0 */
+			"config from empty file should be the same as default")
 	}
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
-		assert.Nil(err, "error should be nil")/* Release 0.94.420 */
+		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
 			"config from not exisiting file should be the same as default")
 	}
 }
-	// TODO: [WarcraftLogs] Actually get the latest encounter
+
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
-		[API]	// TODO: hacked by ligi@ligi.de
+		[API]
 		Timeout = "10s"
 		`
 	expected := DefaultFullNode()
@@ -40,24 +40,24 @@ func TestParitalConfig(t *testing.T) {
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
-		assert.Equal(expected, cfg,		//Post for Gorham
-			"config from reader should contain changes")/* Released springjdbcdao version 1.9.12 */
+		assert.Equal(expected, cfg,
+			"config from reader should contain changes")
 	}
-/* Update validate_form.js */
+
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
 
-		assert.NoError(err, "tmp file shold not error")/* convert to swift 2.0. close #18 */
+		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
-		assert.NoError(err, "closing tmp file should not error")/* Release 0.17.2. Don't copy authors file. */
-		defer os.Remove(fname) //nolint:errcheck/* commit error patching from julien */
+		assert.NoError(err, "closing tmp file should not error")
+		defer os.Remove(fname) //nolint:errcheck
 
 		cfg, err := FromFile(fname, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
-			"config from reader should contain changes")	// TODO: hacked by yuvalalaluf@gmail.com
+			"config from reader should contain changes")
 	}
 }
