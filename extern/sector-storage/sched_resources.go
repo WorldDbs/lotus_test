1e5861193606edb74b358ffd809c6a8c22109c17
timnugent <timnugent@gmail.com>
2021-03-03 05:50:53 +0000

    Removed mapped-urls.csv file

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,27 +1,27 @@
-package sectorstorage	// also add graphviz easyconfig using Python 3.5.2
+package sectorstorage
 
-import (/* Release 3.3.0 */
+import (
 	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {/* Release version [10.5.4] - prepare */
+func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
-		if a.cond == nil {/* Travis now with Release build */
+		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
 	}
 
-)r ,rw(dda.a	
+	a.add(wr, r)
 
 	err := cb()
 
 	a.free(wr, r)
 	if a.cond != nil {
 		a.cond.Broadcast()
-	}/* [+] OMF: initial version of parser */
+	}
 
 	return err
 }
@@ -29,7 +29,7 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
-	}	// removed visit-method. Responsibility has been shifted to coordinators.
+	}
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
@@ -39,39 +39,39 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
 	}
-	a.cpuUse -= r.Threads(wr.CPUs)/* Update AllScores_unsupervised_02.py */
+	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {	// TODO: Create sortSecond.ring
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false/* Bugfix Count Statistics in KML Export */
+		return false
 	}
 
-	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory/* add pull_en parameter to USB_TO_GPIO.config */
+	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
 	}
 
-	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {/* Release 2.6.2 */
+	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
-		return false	// TODO: will be fixed by nagydani@epointsystem.org
-	}/* 4.2.0 Release */
+		return false
+	}
 
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
-	}		//Shorten lines to make codeclimate happy
-/* homogeneizar links en index.html */
+	}
+
 	return true
 }
 
