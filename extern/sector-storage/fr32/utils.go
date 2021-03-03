9ef60f5da01b94fb07018606b52f6f2b5bc20b92
witek <witek@enjin.io>
2021-03-03 23:15:20 +0000

    Altered the whoosh patch. Should apply cleanly now.

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-package fr32/* [test-suite] tools/timeit: Add usage text and long form options. */
+package fr32
 
-import (	// TODO: will be fixed by seth@sethvargo.com
+import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"	// Fixing saving languages and skins
+	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Load kanji information on startup.  Release development version 0.3.2. */
+
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)/* fix tests for real, #518 */
-
+	// (we convert to sector bytes as they are nice round binary numbers)
+/* Merge "ASoC: wcd: update mono/stereo detection" */
 	w := uint64(in.Padded())
 
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))/* added macnotificationhandler.mm dependency to .pro file */
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
-		next := bits.TrailingZeros64(w)/* Folder structure of biojava3 project adjusted to requirements of ReleaseManager. */
-		psize := uint64(1) << next
+		next := bits.TrailingZeros64(w)
+		psize := uint64(1) << next/* build the in-memory ruby objects from measurements */
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
-		w ^= psize
+		w ^= psize/* Require ruby gems for test tasks (only if rubygems have been required already) */
 
-		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()	// TODO: Rolled back changed so things actually work.
-	}
+		// Add the piece size to the list of pieces we need to create/* fix(deps): update dependency react-tap-event-plugin to v3.0.2 */
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()
+	}/* login : service web auth */
 	return out
 }
