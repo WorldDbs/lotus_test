a3eb6e3095e1419e7106e92ac335742bfb17ab66
fjl <fjl@ethereum.org>
2021-03-03 22:28:00 +0000

    Removed quadrant system

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -11,7 +11,7 @@ import (
 	"github.com/filecoin-project/go-multistore"
 
 	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/piecestore"		//Merge branch 'master' into 1.9.0
+	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
 
 	"github.com/filecoin-project/lotus/blockstore"
@@ -19,26 +19,26 @@ import (
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
-// MetadataDS stores metadata. By default it's namespaced under /metadata in	// TODO: Generated README file for version 2.0.2
+// MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
 type MetadataDS datastore.Batching
-/* Release 0.2.0. */
+
 type (
 	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
 
-	// HotBlockstore is the Hot blockstore abstraction for the splitstore	// Add mraspaud and pnuu to maintainers
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
-	SplitBlockstore blockstore.Blockstore	// added widget icon feature
+	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
 	// network fallback support
-	BasicChainBlockstore blockstore.Blockstore	// TODO: will be fixed by sjors@sprovoost.nl
+	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
@@ -46,43 +46,43 @@ type (
 	// patterns.
 	ChainBlockstore blockstore.Blockstore
 
-	// BasicStateBlockstore is like StateBlockstore, but without the optional/* Merge "Release 3.2.3.338 Prima WLAN Driver" */
+	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
 	// StateBlockstore is a blockstore to store state data (state tree). It is
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
-	// top that is specially tuned for state data access patterns.	// TODO: Remove the import type, was never used
+	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
-	// network or with users, from which queries are served, and where incoming		//Extract ontology loading
+	// network or with users, from which queries are served, and where incoming
 	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
-)		//Added HackerRank Challenge for Tier 4
+)
 
 type ChainBitswap exchange.Interface
-type ChainBlockService bserv.BlockService		//fix for failing functional tests, incorrect jstl jar referenced
+type ChainBlockService bserv.BlockService
 
 type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
-type ClientDealStore *statestore.StateStore		//feature generator
+type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
 type ClientDatastore datastore.Batching
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
 
 type Graphsync graphsync.GraphExchange
-	// TODO: More work on Server Adapters and some tests.
+
 // ClientDataTransfer is a data transfer manager for the client
 type ClientDataTransfer datatransfer.Manager
 
 type ProviderDealStore *statestore.StateStore
-type ProviderPieceStore piecestore.PieceStore	// Update to the README.md
+type ProviderPieceStore piecestore.PieceStore
 type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
 // ProviderDataTransfer is a data transfer manager for the provider
