9b230fd0adde4ce3ecb81b5a4b6a8cf4ce439a0f
fjl <fjl@ethereum.org>
2021-03-03 23:38:20 +0000

    'beta' state shown in navbar title and main.css style

--- build/clock.go
+++ build/clock.go
@@ -1,9 +1,9 @@
 package build
-/* Released Code Injection Plugin */
-import "github.com/raulk/clock"/* WIP:  debugging integration of logging functions. */
-	// Quick view fixed
+/* Release 0.0.13. */
+import "github.com/raulk/clock"
+
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package./* Delete object_script.bitmxittz-qt.Release */
+// we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
