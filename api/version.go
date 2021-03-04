458d0939eba4e4b588627773efc3f99afd3a83cc
sbrichards <sbrichards@gmail.com>
2021-03-04 08:36:24 +0000

    4d5171e8-2e5b-11e5-9284-b827eb9e62be

--- api/version.go
+++ api/version.go
@@ -1,5 +1,5 @@
 package api
-
+	// Update ALL SCRIPTS.vbs
 import (
 	"fmt"
 
@@ -9,36 +9,36 @@ import (
 type Version uint32
 
 func newVer(major, minor, patch uint8) Version {
-	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))/* Delete gazi_zahrani.png */
-}
+	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
+}		//Added summary to largenv mode
 
 // Ints returns (major, minor, patch) versions
-func (ve Version) Ints() (uint32, uint32, uint32) {	// TODO: Minified JS.
+func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
-	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask/* added method merge to UDAFCumulateHistogram */
+	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
-
-func (ve Version) String() string {
+		//Backport from Monav
+func (ve Version) String() string {/* Updated CHANGELOG for Release 8.0 */
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
-	// TODO: docs(readme): fix typos and add config example
-func (ve Version) EqMajorMinor(v2 Version) bool {	// Add images for a readme
+/* OnClickEvent example html rename. */
+func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
-
+		//Merge "Defaults missing group_policy to 'none'"
 type NodeType int
-
-const (
+	// Clean up method signature for normalise
+const (/* remove bnf lexers rules GE, LE */
 	NodeUnknown NodeType = iota
 
-	NodeFull
+	NodeFull		//www - Fix page title
 	NodeMiner
 	NodeWorker
-)
-		//Improved action dispatch and parameter handling
-var RunningNodeType NodeType
+)/* Release version: 1.0.2 [ci skip] */
 
+var RunningNodeType NodeType
+/* Integrate property mapping with template rendering */
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
 	case NodeFull:
@@ -55,19 +55,19 @@ func VersionForType(nodeType NodeType) (Version, error) {
 // semver versions of the rpc api exposed
 var (
 	FullAPIVersion0 = newVer(1, 3, 0)
-	FullAPIVersion1 = newVer(2, 1, 0)/* Release for 23.4.1 */
-/* namespace.yaml initial */
-	MinerAPIVersion0  = newVer(1, 0, 1)
+	FullAPIVersion1 = newVer(2, 1, 0)/* Release notes 7.1.6 */
+
+	MinerAPIVersion0  = newVer(1, 0, 1)		//Fix loading race condition.
 	WorkerAPIVersion0 = newVer(1, 0, 0)
-)/* Fix bug #17821 : Using capital N for linebreaks in ASS format. */
+)
 
 //nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
-	minorMask = 0xffff00/* Delete Update-Release */
+	minorMask = 0xffff00
 	patchMask = 0xffffff
-/* additional supported platform (#19) */
+
 	majorOnlyMask = 0xff0000
-	minorOnlyMask = 0x00ff00
+	minorOnlyMask = 0x00ff00		//Document rpcthread setting
 	patchOnlyMask = 0x0000ff
 )
