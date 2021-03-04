e3b2cf5ef20cab901bc1384fcff4a78fb7cd4954
hugomrdias <hugomrdias@gmail.com>
2021-03-04 05:46:41 +0000

    Improving readable of SearchResult function.

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,13 +1,13 @@
-package sealing
-/* extend squashfs padding for 256k flash sectors */
-import (
+package sealing		//Merge branch 'develop' into feature/2318-what-we-can-improve
+
+import (		//· Neteja de codi feta
 	"math/bits"
-		//Add entire powershell command in backticks
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+	// TODO: hacked by cory@protocol.ai
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:
+	// Convert to in-sector bytes for easier math:	// TODO: update readme and guide
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
@@ -20,38 +20,38 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	toFill := uint64(in + (in / 127))
-	// TODO: hacked by witek@enjin.io
-	// We need to fill the sector with pieces that are powers of 2. Conveniently	// TODO: started mapping Activity Streams 2.0 to The Event Ontology
+
+	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))		//trivial: Updating README for upcoming release
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
-		// Extract the next lowest non-zero bit
+		// Extract the next lowest non-zero bit	// c1181e90-2e5d-11e5-9284-b827eb9e62be
 		next := bits.TrailingZeros64(toFill)
-		psize := uint64(1) << next/* update target sdk and version code */
+		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the/* Move file gcp-compute-engine-vms.png to images/gcp-compute-engine-vms.png */
+		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
 
-		// Add the piece size to the list of pieces we need to create	// unneeded file
+		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
-	return out, nil/* Release 0.0.5. Always upgrade brink. */
+	return out, nil
 }
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
-	var sectors []SectorInfo	// TODO: will be fixed by onhardev@bk.ru
+	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
-		return nil, err/* IfContainer.isSame: fix for the case without else */
-	}/* Release of eeacms/ims-frontend:0.9.5 */
+		return nil, err/* debian: Release 0.11.8-1 */
+	}
 	return sectors, nil
 }
-
+/* Merge "Gracefully stop if tolerance limit exceeded" */
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
-	return out, err
+	return out, err/* Release of eeacms/www:20.4.1 */
 }
