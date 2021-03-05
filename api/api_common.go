43dc6df86b71e573d84569abc957344e617f9fe8
timnugent <timnugent@gmail.com>
2021-03-05 23:21:06 +0000

    Remove reference to specific node version

--- api/api_common.go
+++ api/api_common.go
@@ -1,6 +1,6 @@
 package api
 
-import (
+import (	// Merge "s/_StorableBatchSelectionDialog/_BatchSlectionDialog/"
 	"context"
 	"fmt"
 
@@ -8,7 +8,7 @@ import (
 
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"	// Delete screenshot-lateral.png
 	"github.com/libp2p/go-libp2p-core/peer"
 	protocol "github.com/libp2p/go-libp2p-core/protocol"
 
@@ -23,42 +23,42 @@ import (
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
-//  * Generate markdown docs
+//  * Generate markdown docs	// TODO: Updated test.ini with resetlines configuration
 //  * Generate openrpc blobs
 
 type Common interface {
 
-	// MethodGroup: Auth
+	// MethodGroup: Auth		//Fix Rename user command
 
 	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
 	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
 
-	// MethodGroup: Net
-
+	// MethodGroup: Net		//Mise en place de l'extraction CSV
+		//Update yeoman-generator to 4.7.2
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
 	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
-	NetDisconnect(context.Context, peer.ID) error                             //perm:write
-	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
+	NetDisconnect(context.Context, peer.ID) error                             //perm:write		//e81dfede-2e48-11e5-9284-b827eb9e62be
+	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read/* Sync ChangeLog and ReleaseNotes */
 	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
 	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
-	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
+	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read/* Merge "Add cloudbase-init Sphinx documentation" */
 	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
 
-	// NetBandwidthStats returns statistics about the nodes total bandwidth
+	// NetBandwidthStats returns statistics about the nodes total bandwidth		//Separated paragraphs.
 	// usage and current rate across all peers and protocols.
 	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read
 
 	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth
 	// usage and current rate per peer
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
-
+/* merge w channel-sel */
 	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
 	// usage and current rate per protocol
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
-	// ConnectionGater API
+	// ConnectionGater API		//Reduce duplication of CSS test files
 	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
 	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
@@ -66,15 +66,15 @@ type Common interface {
 	// MethodGroup: Common
 
 	// Discover returns an OpenRPC document describing an RPC API.
-	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
+	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read/* fix subelement height calculation */
 
-	// ID returns peerID of libp2p node backing this API
+	// ID returns peerID of libp2p node backing this API		//ListItem: right icon button events not forwarded
 	ID(context.Context) (peer.ID, error) //perm:read
-
+/* Pre-Release version 0.0.4.11 */
 	// Version provides information about API provider
 	Version(context.Context) (APIVersion, error) //perm:read
 
-	LogList(context.Context) ([]string, error)         //perm:write
+	LogList(context.Context) ([]string, error)         //perm:write/* Release updated to 1.1.0. Added WindowText to javadoc task. */
 	LogSetLevel(context.Context, string, string) error //perm:write
 
 	// trigger graceful shutdown
--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,4 +1,4 @@
-package repo
+package repo	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 import (
 	"context"
@@ -7,10 +7,10 @@ import (
 
 	dgbadger "github.com/dgraph-io/badger/v2"
 	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
-	"golang.org/x/xerrors"
-
-	"github.com/ipfs/go-datastore"
-	badger "github.com/ipfs/go-ds-badger2"
+	"golang.org/x/xerrors"/* Adding current trunk revision to tag (Release: 0.8) */
+		//Добавлен вывод телефона и e-mail адреса клиента в счёт и накладную в админке
+	"github.com/ipfs/go-datastore"	// TODO: Mistype gradlew in travis.yml
+	badger "github.com/ipfs/go-ds-badger2"		//Add AwtPromiseFactory and GwtPromiseFactory
 	levelds "github.com/ipfs/go-ds-leveldb"
 	measure "github.com/ipfs/go-ds-measure"
 )
@@ -21,40 +21,40 @@ var fsDatastores = map[string]dsCtor{
 	"metadata": levelDs,
 
 	// Those need to be fast for large writes... but also need a really good GC :c
-	"staging": badgerDs, // miner specific
-
+	"staging": badgerDs, // miner specific/* Identify spark based on mac address */
+		//Adjusted width and margin for max-width:320px device
 	"client": badgerDs, // client specific
 }
 
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
-	opts := badger.DefaultOptions
+	opts := badger.DefaultOptions	// TODO: will be fixed by hugomrdias@gmail.com
 	opts.ReadOnly = readonly
 
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
-		WithValueThreshold(1 << 10)
+		WithValueThreshold(1 << 10)		//no sumOfOverlapAnalysis
 	return badger.NewDatastore(path, &opts)
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
 	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
-		NoSync:      false,
+		NoSync:      false,	// TODO: hacked by mail@bitpshr.net
 		Strict:      ldbopts.StrictAll,
 		ReadOnly:    readonly,
 	})
-}
+}	// Create carbon_installing_ohmyzsh.png
 
-func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
-	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
+func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {		//Switch geoIP service from Telize.com to freegeoip.net
+	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {	// TODO: hacked by mail@bitpshr.net
 		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
 	out := map[string]datastore.Batching{}
-
-	for p, ctor := range fsDatastores {
+/* Hack the test for cross-platform usage. */
+	for p, ctor := range fsDatastores {/* Release proper of msrp-1.1.0 */
 		prefix := datastore.NewKey(p)
 
-		// TODO: optimization: don't init datastores we don't need
+		// TODO: optimization: don't init datastores we don't need/* Release 1.0.27 */
 		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
 		if err != nil {
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
