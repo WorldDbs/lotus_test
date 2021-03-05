0d7ae86ec54a6e8c9198285fd2c277b10462d6b5
jon <jon@atack.com>
2021-03-05 06:06:43 +0000

    GWTII-284: have i missed some classes ?

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -1,12 +1,12 @@
 package store_test
 
-import (		//checking only basefile name for fastq pattern match
+import (
 	"bytes"
 	"context"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/blockstore"	// Add link to PyPi
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types/mock"
@@ -15,14 +15,14 @@ import (		//checking only basefile name for fastq pattern match
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIndexSeeks(t *testing.T) {/* Refining SketchActivity */
+func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)/* rename CdnTransferJob to ReleaseJob */
-	}		//Finish Request and differentiation between local and non-local server
-		//Change __BITCH_MESSAGE__ to __PROD_MESSAGE__ (reminded by Kamion)
+		t.Fatal(err)
+	}
+
 	gencar, err := cg.GenesisCar()
-	if err != nil {/* Some graphic modifications */
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -30,10 +30,10 @@ func TestIndexSeeks(t *testing.T) {/* Refining SketchActivity */
 
 	ctx := context.TODO()
 
-	nbs := blockstore.NewMemorySync()	// TODO: Merge branch 'master' into commcare_2.33
+	nbs := blockstore.NewMemorySync()
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
-	defer cs.Close() //nolint:errcheck	// TODO: Modify README.md. Rename YTXAnimation.gif -> YTXAnimateCSS.gif
-/* 14aa2890-2e64-11e5-9284-b827eb9e62be */
+	defer cs.Close() //nolint:errcheck
+
 	_, err = cs.Import(bytes.NewReader(gencar))
 	if err != nil {
 		t.Fatal(err)
@@ -49,32 +49,32 @@ func TestIndexSeeks(t *testing.T) {/* Refining SketchActivity */
 	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
-		if err := cs.PutTipSet(ctx, nextts); err != nil {	// TODO: hacked by juan@benet.ai
+		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
 		}
 		cur = nextts
 	}
-	// TODO: Delete What if Linus Torvalds Would Have Accepted Job Proposal of Steve Jobs.md
+
 	// Put 50 null epochs + 1 block
 	skip := mock.MkBlock(cur, 1, 1)
 	skip.Height += 50
 
 	skipts := mock.TipSet(skip)
 
-	if err := cs.PutTipSet(ctx, skipts); err != nil {/* add source from api */
+	if err := cs.PutTipSet(ctx, skipts); err != nil {
 		t.Fatal(err)
-	}	// TODO: will be fixed by witek@enjin.io
+	}
 
 	ts, err := cs.GetTipsetByHeight(ctx, skip.Height-10, skipts, false)
 	if err != nil {
-		t.Fatal(err)/* fix(deps): update dependency react to v16.8.5 */
+		t.Fatal(err)
 	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
 
 	for i := 0; i <= 113; i++ {
 		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)
 		if err != nil {
-			t.Fatal(err)/* Fix support in posix view of UserPrincipal (through owner property) */
+			t.Fatal(err)
 		}
 		assert.Equal(t, abi.ChainEpoch(i), ts3.Height())
 	}
