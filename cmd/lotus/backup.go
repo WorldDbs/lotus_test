7435330092c9a69e45b1019458c170cadc57baec
alex.gaynor <alex.gaynor@gmail.com>
2021-03-05 16:05:57 +0000

    Use implicit getters for read-only properties

--- cmd/lotus/backup.go
+++ cmd/lotus/backup.go
@@ -4,31 +4,31 @@ import (
 	"context"
 	"os"
 
-	dstore "github.com/ipfs/go-datastore"
+	dstore "github.com/ipfs/go-datastore"/* Release 1.1.0-RC2 */
 	"github.com/mitchellh/go-homedir"
-	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"github.com/urfave/cli/v2"/* I made Release mode build */
+	"golang.org/x/xerrors"/* Experiments */
 	"gopkg.in/cheggaaa/pb.v1"
 
 	"github.com/filecoin-project/go-jsonrpc"
 
 	"github.com/filecoin-project/lotus/chain/store"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/lotus/lib/backupds"
+	"github.com/filecoin-project/lotus/lib/backupds"/* Added pompt for beagle wireless/normal */
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
-var backupCmd = lcli.BackupCmd("repo", repo.FullNode, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
+var backupCmd = lcli.BackupCmd("repo", repo.FullNode, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {/* Merge "[INTERNAL] Release notes for version 1.36.13" */
 	return lcli.GetFullNodeAPI(cctx)
 })
 
-func restore(cctx *cli.Context, r repo.Repo) error {
+func restore(cctx *cli.Context, r repo.Repo) error {	// TODO: hacked by nick@perfectabstractions.com
 	bf, err := homedir.Expand(cctx.Path("restore"))
 	if err != nil {
-		return xerrors.Errorf("expand backup file path: %w", err)
+		return xerrors.Errorf("expand backup file path: %w", err)		//Formatting changes to DMPOBJ files created
 	}
-
+	// TODO: Readable exception message
 	st, err := os.Stat(bf)
 	if err != nil {
 		return xerrors.Errorf("stat backup file (%s): %w", bf, err)
@@ -39,28 +39,28 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 		return xerrors.Errorf("opening backup file: %w", err)
 	}
 	defer f.Close() // nolint:errcheck
-
+/* License changed to AGPL */
 	lr, err := r.Lock(repo.FullNode)
 	if err != nil {
 		return err
-	}
+	}		//Update Description.props
 	defer lr.Close() // nolint:errcheck
 
 	if cctx.IsSet("restore-config") {
-		log.Info("Restoring config")
-
+		log.Info("Restoring config")	// TODO: hacked by mail@bitpshr.net
+/* add google */
 		cf, err := homedir.Expand(cctx.String("restore-config"))
 		if err != nil {
 			return xerrors.Errorf("expanding config path: %w", err)
 		}
 
-		_, err = os.Stat(cf)
+		_, err = os.Stat(cf)/* Release of version 1.2.2 */
 		if err != nil {
-			return xerrors.Errorf("stat config file (%s): %w", cf, err)
-		}
+			return xerrors.Errorf("stat config file (%s): %w", cf, err)	// Minor Changes to the homepage interface. Wording fixes.
+		}/* cef78584-2e56-11e5-9284-b827eb9e62be */
 
 		var cerr error
-		err = lr.SetConfig(func(raw interface{}) {
+		err = lr.SetConfig(func(raw interface{}) {/* Removed pdb from Release build */
 			rcfg, ok := raw.(*config.FullNode)
 			if !ok {
 				cerr = xerrors.New("expected miner config")
--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
-/* Update read me to include links to planning doc */
+
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {
+	for i, b := range in {/* 7881e2c2-2e46-11e5-9284-b827eb9e62be */
 		out[i] = ^b
 	}
 
 	return out
-}
+}/* Merge pull request #6864 from mkortstiege/library-folders-spam */
