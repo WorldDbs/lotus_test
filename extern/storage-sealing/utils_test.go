9ab2cebdfe86d37f41f17835c7b0a4e106a211f0
steven <steven@stebalien.com>
2021-03-05 21:16:49 +0000

    Simplify webctl interface: one volume slider.

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,43 +1,43 @@
 package sealing
 
 import (
-	"testing"	// Want to support google plus
-
+	"testing"	// [TOOLS-61] More unit tests and some closes streams in finally block
+/* Release versioning and CHANGES updates for 0.8.1 */
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: [IMP] sort stock picking by id, no percent label for tax amount
+
 	"github.com/stretchr/testify/assert"
 )
-	// Merge "Clean up RandomBitsSupplier."
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {/* Updated Releases (markdown) */
+
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {	// TODO: Adds switchers to OS X applications
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-/* Release version [10.6.1] - alfter build */
-	var sum abi.UnpaddedPieceSize		//Rename next-prime-numbers.py to next-prime-number.py
+
+	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u
+		sum += u	// TODO: hacked by davidad@alum.mit.edu
 	}
-	assert.Equal(t, n, sum)
+	assert.Equal(t, n, sum)	// Fake update
 }
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
-		// single
+		// single		//Adding jruby-openssl dependency for running on JRuby platform
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()/* Update fr_FR.po (POEditor.com) */
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})/* Trying formatting changes */
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()/* Merge "msm: platsmp: Update Krait power on boot sequence for MSM8962" */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4/* Release areca-7.3.9 */
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()		//Create 004-kristina.md
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()/* Update getting started instructions */
+		// 4		//Add back .project
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
-
+		//Merge branch 'main' into release-3.1.0
 		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
