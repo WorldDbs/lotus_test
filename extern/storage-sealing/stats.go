5eda2e353e4072548408b78dbb1f9c8903f3ea92
denner <denner@gmail.com>
2021-03-06 05:49:21 +0000

    Update javascript_olympics.js

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,5 +1,5 @@
 package sealing
-/* Release for v4.0.0. */
+
 import (
 	"sync"
 
@@ -14,17 +14,17 @@ const (
 	sstSealing
 	sstFailed
 	sstProving
-	nsst/* (DOCS) Release notes for Puppet Server 6.10.0 */
+	nsst
 )
 
 type SectorStats struct {
 	lk sync.Mutex
-		//Create lac07-50-B-146518.cpp
+
 	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
-}/* fixes to CBRelease */
-		//Improved my-account configuration.
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {/* Add link to readthedoc doc to README */
+}
+
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
@@ -32,43 +32,43 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	preStaging := ss.curStagingLocked()
 
 	// update totals
-	oldst, found := ss.bySector[id]/* Updated links for alternative tests */
+	oldst, found := ss.bySector[id]
 	if found {
 		ss.totals[oldst]--
 	}
 
-	sst := toStatState(st)		//Update base_home.html
+	sst := toStatState(st)
 	ss.bySector[id] = sst
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
-		//Commiting before going to vacation; build may be broken
+
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit/* combo update */
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
-	}/* Merge "CAF:QRD_BSP:KERNEL:None:none:add regulator sysfs" into jb_rel_rb5_qrd */
+	}
 
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
-		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit/* Add two tertiary resource spawns to metro */
+		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
 	}
-/* added the xtext feature as a dependency */
+
 	return updateInput
 }
 
 func (ss *SectorStats) curSealingLocked() uint64 {
-	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]/* 4d9b00e8-2e50-11e5-9284-b827eb9e62be */
+	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
 	return ss.totals[sstStaging]
-}		//Fix typo in dependency-resolvers-conf.yml
+}
 
 // return the number of sectors currently in the sealing pipeline
 func (ss *SectorStats) curSealing() uint64 {
