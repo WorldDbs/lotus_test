c953318caa57cdb837f51ffe3ab91577aecf1339
ng8eke <ng8eke@163.com>
2021-03-07 12:03:21 +0000

    Update the versions and build scripts

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,14 +1,14 @@
-package helpers	// TODO: hacked by timnugent@gmail.com
+package helpers
 
-import (
+import (	// TODO: Add "See also" to KillauraLegitMod
 	"context"
-
+/* New post: Smart Anti-spy Wireless Hidden Camera Detector */
 	"go.uber.org/fx"
 )
 
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-/* Movement func now void */
+
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
@@ -17,9 +17,9 @@ func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
-			cancel()		//Removed Version from composer.json file
-			return nil/* Update HOSTSMTWRwin.txt */
-		},		//Delete getlevel.js
+			cancel()/* Post, retrieve. not working condition */
+			return nil
+		},
 	})
 	return ctx
 }
