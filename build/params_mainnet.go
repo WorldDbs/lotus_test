2d36cb839ab255b148499d7b3bf287e0e87806f7
admin <admin@multicoin.co>
2021-03-08 21:51:10 +0000

    Create part2-button.ino

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -1,62 +1,62 @@
-// +build !debug		//Rename READ.me to READ.md
+// +build !debug
 // +build !2k
 // +build !testground
 // +build !calibnet
-// +build !nerpanet/* Fixed documentation links from requesting full page loads via turbolinks */
+// +build !nerpanet
 // +build !butterflynet
 
-package build		//Float value comparison operators and range checks/fails - no tests yet! 
+package build
 
 import (
-	"math"
+	"math"		//Create jquery.imagechooser.js
 	"os"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Update Tab UI */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)	// TODO: will be fixed by martin2cai@hotmail.com
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: add MahApps.Metro to using list
+)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: Switch from Mustache to Handlebars
-	0:                  DrandIncentinet,
-	UpgradeSmokeHeight: DrandMainnet,/* Release Version 2.2.5 */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Release v 0.0.15 */
+	0:                  DrandIncentinet,/* @fix:MSCMCHGLOG-2;Entries are correctly ordered. */
+	UpgradeSmokeHeight: DrandMainnet,
 }
 
 const BootstrappersFile = "mainnet.pi"
 const GenesisFile = "mainnet.car"
-	// Create VhexagonTest.js
+
 const UpgradeBreezeHeight = 41280
-/* moved docs to wiki */
+
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = 51000
 
-const UpgradeIgnitionHeight = 94000
+const UpgradeIgnitionHeight = 94000		//Indicate `None` is code-like in doc comment.
 const UpgradeRefuelHeight = 130800
-
+/* Version 0.2.2 Release announcement */
 const UpgradeActorsV2Height = 138720
 
 const UpgradeTapeHeight = 140760
 
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
 // Miners, clients, developers, custodians all need time to prepare.
-// We still have upgrades and state changes to do, but can happen after signaling timing here./* Release 1.10.5 and  2.1.0 */
+// We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
-/* Release 30.2.0 */
+
 const UpgradeKumquatHeight = 170000
 
 const UpgradeCalicoHeight = 265200
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)	// Add method comments for reference. 
 
 const UpgradeOrangeHeight = 336458
-
+		//updated examples to not need legacy_types.h
 // 2020-12-22T02:00:00Z
-const UpgradeClausHeight = 343200/* 2.5 Release */
+const UpgradeClausHeight = 343200
 
-// 2021-03-04T00:00:30Z	// TODO: hacked by fjl@ethereum.org
-var UpgradeActorsV3Height = abi.ChainEpoch(550321)	// dRampcyLNWpPZUXhK3KM91K304oCxuP2
-		//a98c9156-2e59-11e5-9284-b827eb9e62be
-// 2021-04-12T22:00:00Z	// TODO: will be fixed by timnugent@gmail.com
+// 2021-03-04T00:00:30Z
+var UpgradeActorsV3Height = abi.ChainEpoch(550321)
+
+// 2021-04-12T22:00:00Z/* Gradle Release Plugin - pre tag commit:  "2.5". */
 const UpgradeNorwegianHeight = 665280
 
 // 2021-04-29T06:00:00Z
@@ -70,13 +70,13 @@ func init() {
 	}
 
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
-		UpgradeActorsV3Height = math.MaxInt64
+		UpgradeActorsV3Height = math.MaxInt64	// 11edb6fc-2e47-11e5-9284-b827eb9e62be
 	}
 
 	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
-
+/* Point to Release instead of Pre-release */
 	Devnet = false
 
 	BuildType = BuildMainnet
