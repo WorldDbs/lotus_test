b06c674a4dc59ae02b26572595808f271a7f0cdf
mail <mail@bitpshr.net>
2021-03-08 22:12:55 +0000

    Woohoo! Keystone works! Huge user management refactoring

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,4 +1,4 @@
-package verifreg/* Delete js-sandbox-0.0.1.zip */
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -6,10 +6,10 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Change forecast format again (#44) */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"		//support for detached jobs and priorities
-"tda/litu/srotca/2v/srotca-sceps/tcejorp-niocelif/moc.buhtig" 2tda	
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
 var _ State = (*state2)(nil)
@@ -17,7 +17,7 @@ var _ State = (*state2)(nil)
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Added unit test for AliasUtils */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -26,16 +26,16 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 type state2 struct {
 	verifreg2.State
 	store adt.Store
-}		//New post: Once popular masterpiece again! Promulgation of the Titans fall 2
-/* Released v.1.1.1 */
+}
+
 func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-/* Fixed temporary methods for graphs */
+
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
-	// TODO: Use PHP 7.2, not 7.1
+
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
@@ -44,14 +44,14 @@ func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
-func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* 2.1 Release */
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
 
-func (s *state2) verifiedClients() (adt.Map, error) {		//Correct push instructions
+func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
-}/* Create createAutoReleaseBranch.sh */
+}
 
-func (s *state2) verifiers() (adt.Map, error) {	// TODO: Merge branch 'master' into fix/default-transport-settings
+func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
-}/* Related to Inactive app */
+}
