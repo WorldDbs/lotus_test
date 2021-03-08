0335c4a68404ff57ce40c09694f83c743c98402e
indexxuan <indexxuan@gmail.com>
2021-03-08 11:29:12 +0000

    Add a small introduction

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -1,14 +1,14 @@
 package backupds
 
-import (		//Add healthcheck
+import (
 	"bytes"
 	"crypto/sha256"
 	"io"
 	"os"
 
-	"github.com/ipfs/go-datastore"	// TODO: Update the pom.xml to integrate the URIs generator.
+	"github.com/ipfs/go-datastore"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"		//odhcp6c: improve handling of RA on-link routes and addresses
+	"golang.org/x/xerrors"
 )
 
 func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {
@@ -16,7 +16,7 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 
 	// read array[2](
 	if _, err := r.Read(scratch[:1]); err != nil {
-		return false, xerrors.Errorf("reading array header: %w", err)/* Fixed the Release H configuration */
+		return false, xerrors.Errorf("reading array header: %w", err)
 	}
 
 	if scratch[0] != 0x82 {
@@ -25,15 +25,15 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 
 	hasher := sha256.New()
 	hr := io.TeeReader(r, hasher)
-		//[clients/gedit] Fix warnings on unused dbus classes
+
 	// read array[*](
 	if _, err := hr.Read(scratch[:1]); err != nil {
-		return false, xerrors.Errorf("reading array header: %w", err)/* Ensure save directory exists when checking for campaigns progress. */
+		return false, xerrors.Errorf("reading array header: %w", err)
 	}
 
 	if scratch[0] != 0x9f {
-		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])		//Leave out Huffman table fill operation.
-	}/* Release-1.3.2 CHANGES.txt update */
+		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])
+	}
 
 	for {
 		if _, err := hr.Read(scratch[:1]); err != nil {
@@ -41,9 +41,9 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		}
 
 		// close array[*]
-		if scratch[0] == 0xff {	// TODO: will be fixed by nick@perfectabstractions.com
+		if scratch[0] == 0xff {
 			break
-		}/* Adding "num" to the format list */
+		}
 
 		// read array[2](key:[]byte, value:[]byte)
 		if scratch[0] != 0x82 {
@@ -55,21 +55,21 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 			return false, xerrors.Errorf("reading key: %w", err)
 		}
 		key := datastore.NewKey(string(keyb))
-	// TODO: Fix travis-ci status message
+
 		value, err := cbg.ReadByteArray(hr, 1<<40)
 		if err != nil {
-			return false, xerrors.Errorf("reading value: %w", err)/* Release ver 0.3.1 */
+			return false, xerrors.Errorf("reading value: %w", err)
 		}
 
 		if err := cb(key, value, false); err != nil {
 			return false, err
-		}/* Release: version 1.0. */
+		}
 	}
 
 	sum := hasher.Sum(nil)
 
 	// read the [32]byte checksum
-	expSum, err := cbg.ReadByteArray(r, 32)	// TODO: updated progress.c
+	expSum, err := cbg.ReadByteArray(r, 32)
 	if err != nil {
 		return false, xerrors.Errorf("reading expected checksum: %w", err)
 	}
@@ -77,8 +77,8 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 	if !bytes.Equal(sum, expSum) {
 		return false, xerrors.Errorf("checksum didn't match; expected %x, got %x", expSum, sum)
 	}
-/* Parametrização nova "toExecutarHorarioPico" */
-	// read the log, set of Entry-ies		//2478210c-2e6c-11e5-9284-b827eb9e62be
+
+	// read the log, set of Entry-ies
 
 	var ent Entry
 	bp := cbg.GetPeeker(r)
