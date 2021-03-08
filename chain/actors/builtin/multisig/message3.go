b3932b34163706e2849ec0d6cfc4035634fc0a18
joshua <joshua@yottadb.com>
2021-03-08 06:39:24 +0000

    Bugfix: avoid NPE after initials change

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,8 +1,8 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"/* Fix copy '!' */
+		//added unregister by destruction
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -10,33 +10,33 @@ import (
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"		//Updating build-info/dotnet/cli/release/15.5 for preview3-fnl-007316
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Initial Submission for the Checkbox port to CentOS */
 type message3 struct{ message0 }
 
 func (m message3) Create(
-	signers []address.Address, threshold uint64,
+	signers []address.Address, threshold uint64,	// TODO: Added text document generator.
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {
+) (*types.Message, error) {/* Last update for 2.0.3 */
 
-	lenAddrs := uint64(len(signers))
-
+	lenAddrs := uint64(len(signers))		//Implementing EnPassant move unit test.
+		//Fixed missing virtual/override.
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
+	}		//Fix link path to table demo
 
-	if threshold == 0 {
+	if threshold == 0 {	// TODO: will be fixed by why@ipfs.io
 		threshold = lenAddrs
 	}
-
+		//28f3a270-2e5c-11e5-9284-b827eb9e62be
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-
+/* my commit3 */
 	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
@@ -45,11 +45,11 @@ func (m message3) Create(
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
+	enc, actErr := actors.SerializeParams(msigParams)	// TODO: test invoice.number
 	if actErr != nil {
 		return nil, actErr
-	}
-
+	}/* Released under MIT license */
+/* Delete AAARI.jpg */
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
