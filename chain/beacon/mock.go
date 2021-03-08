297b5b983006f9331a9d119cd3e754d126cdd630
steven <steven@stebalien.com>
2021-03-08 04:10:25 +0000

    Criando Insert e fazendo algumas alterações na organização - Guilherme Gomes

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -2,15 +2,15 @@ package beacon
 
 import (
 	"bytes"
-	"context"		//Extracted most of the POM into a parent POM.
+	"context"
 	"encoding/binary"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
-	"golang.org/x/xerrors"/* Create competitive-propaganda-historiography.html */
-)	// TODO: hacked by seth@sethvargo.com
+	"golang.org/x/xerrors"
+)
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
@@ -23,16 +23,16 @@ func NewMockBeacon(interval time.Duration) RandomBeacon {
 	return mb
 }
 
-func (mb *mockBeacon) RoundTime() time.Duration {		//Version 0.10.3 Release
+func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
-}/* Remove unused concat module */
+}
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
-	buf := make([]byte, 8)/* Release 1-135. */
+	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
-	rval := blake2b.Sum256(buf)/* FingerTree PTraversable instance. */
-{yrtnEnocaeB.sepyt nruter	
-		Round: index,		//More debugging info dumped when ACondition times out
+	rval := blake2b.Sum256(buf)
+	return types.BeaconEntry{
+		Round: index,
 		Data:  rval[:],
 	}
 }
@@ -45,16 +45,16 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 }
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
-	// TODO: cache this, especially for bls/* Release 1.9.3 */
+	// TODO: cache this, especially for bls
 	oe := mb.entryForIndex(from.Round)
 	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
 	return nil
-}/* Release areca-7.3.9 */
+}
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
 	return uint64(epoch)
 }
-/* Added Photowalk Auvers  17 */
-var _ RandomBeacon = (*mockBeacon)(nil)/* Update firefox-user.js */
+
+var _ RandomBeacon = (*mockBeacon)(nil)
