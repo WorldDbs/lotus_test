9c68d5ab21b786fd48995698e3d86a3ceeb3754c
earlephilhower <earlephilhower@yahoo.com>
2021-03-09 14:54:19 +0000

    Merge "msm_fb: display: export mipi_dsi_i2c related resources"

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,13 +1,13 @@
-package account	// TODO: will be fixed by steven@stebalien.com
-/* chore(package): update ilios-common to version 13.0.1 */
-import (		//deal with log errors
+package account
+
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by jon@atack.com
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
-)/* Release the bracken! */
+)
 
 var _ State = (*state0)(nil)
 
@@ -18,13 +18,13 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}/* Merge "msm: camera: Config multiple interface for subdev" */
-
-type state0 struct {		//print available versions
-	account0.State/* Released 2.0.0-beta2. */
-	store adt.Store		//fixed MOW message stacking and some small speed improvements
 }
-	// Fixed temporary navigation coming from widgets with tag assigned
+
+type state0 struct {
+	account0.State
+	store adt.Store
+}
+
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
