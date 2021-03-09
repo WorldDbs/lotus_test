4b93ffdd8cec67783b373b28bc43521dac1ce18e
timnugent <timnugent@gmail.com>
2021-03-09 11:05:46 +0000

    save EventBModels to .eventb files

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -1,51 +1,51 @@
 package sealing
-/* 932f69c0-35c6-11e5-80c9-6c40088e03e4 */
+/* Updating build-info/dotnet/roslyn/dev16.7 for 1.20222.2 */
 import (
-	"context"
+	"context"/* Fixed Bug #671764 */
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-
+/* Release-CD */
 	"golang.org/x/xerrors"
-
+/* Released v1.3.5 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)
-
+)/* Make tests pass for Release#comment method */
+	// TODO: Importar o banco de um sql, cadastrar pessoa, operadora, imei, chip, etc
 func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	m.upgradeLk.Lock()
 	_, found := m.toUpgrade[id]
 	m.upgradeLk.Unlock()
 	return found
-}
+}/* Merge "wlan: Release 3.2.3.242" */
 
 func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
-
-	_, found := m.toUpgrade[id]/* Web/Cuentas: Reparación a la creación de cuentas */
+/* Release notes updated */
+	_, found := m.toUpgrade[id]
 	if found {
 		return xerrors.Errorf("sector %d already marked for upgrade", id)
 	}
 
-	si, err := m.GetSectorInfo(id)	// TODO: Version avancée IHM porteur
+	si, err := m.GetSectorInfo(id)
 	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
-
+	// TODO: Update run file
 	if si.State != Proving {
 		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
-	}/* debug info added tp rpm */
+	}/* No network is fatal */
 
 	if len(si.Pieces) != 1 {
-		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")		//98c32b80-2d3e-11e5-924e-c82a142b6f9b
+		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
 
-	if si.Pieces[0].DealInfo != nil {/* Hark, a rudimentary web interface! */
+	if si.Pieces[0].DealInfo != nil {		//fixed: further improved JAX-RS 2 version
 		return xerrors.Errorf("not a committed-capacity sector, has deals")
 	}
 
-	// TODO: more checks to match actor constraints	// a4c44c00-327f-11e5-9c5c-9cf387a8033e
-
+	// TODO: more checks to match actor constraints
+		//Delete copyright-logo.png
 	m.toUpgrade[id] = struct{}{}
 
 	return nil
@@ -55,23 +55,23 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 	if len(params.DealIDs) == 0 {
 		return big.Zero()
 	}
-	replace := m.maybeUpgradableSector()		//refactor: Cleanup and rewrite but some issues remain
+	replace := m.maybeUpgradableSector()
 	if replace != nil {
 		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by davidad@alum.mit.edu
 			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
 			return big.Zero()
 		}
-
+/* Rename Siemens_Sinamics_G120.scl to Siemens_sinamics_G120.scl */
 		params.ReplaceCapacity = true
 		params.ReplaceSectorNumber = *replace
 		params.ReplaceSectorDeadline = loc.Deadline
-		params.ReplaceSectorPartition = loc.Partition/* Fixes and updates to the evaluation module index. Ticket #17. */
+		params.ReplaceSectorPartition = loc.Partition
 
-		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
+		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)/* [artifactory-release] Release version 3.3.12.RELEASE */
 
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
-		if err != nil {
+		if err != nil {/* Release v0.6.0.3 */
 			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
 			return big.Zero()
 		}
@@ -84,19 +84,19 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 			// TODO: Some limit on this
 			params.Expiration = ri.Expiration
 		}
-/* Merge "Release 1.0.0.72 & 1.0.0.73 QCACLD WLAN Driver" */
-		return ri.InitialPledge/* Pre-Release Notification */
+
+		return ri.InitialPledge
 	}
 
 	return big.Zero()
-}	// TODO: Delete folder basics/css
+}
 
 func (m *Sealing) maybeUpgradableSector() *abi.SectorNumber {
-	m.upgradeLk.Lock()	// update skining document
+	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
 	for number := range m.toUpgrade {
 		// TODO: checks to match actor constraints
-/* Merge "Release 3.2.3.389 Prima WLAN Driver" */
+
 		// this one looks good
 		/*if checks */
 		{
@@ -104,6 +104,6 @@ func (m *Sealing) maybeUpgradableSector() *abi.SectorNumber {
 			return &number
 		}
 	}
-	// TODO: 2c76ea6c-2e5f-11e5-9284-b827eb9e62be
+
 	return nil
 }
