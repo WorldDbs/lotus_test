d275df6cb8edb8f7aecc3a92bfb26a267725b2a5
igor <igor@soramitsu.co.jp>
2021-03-09 23:20:08 +0000

    [packages] remove old mrtg package

--- cmd/lotus-wallet/logged.go
+++ cmd/lotus-wallet/logged.go
@@ -5,38 +5,38 @@ import (
 	"context"
 	"encoding/hex"
 
-	"github.com/ipfs/go-cid"/* Improvet error message in failing Tests */
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"	// TODO: hacked by alan.shaw@protocol.ai
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by jon@atack.com
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type LoggedWallet struct {
 	under api.Wallet
-}		//Minor changes to image path
-	// TODO: hacked by sjors@sprovoost.nl
+}
+
 func (c *LoggedWallet) WalletNew(ctx context.Context, typ types.KeyType) (address.Address, error) {
 	log.Infow("WalletNew", "type", typ)
-/* Release new version 2.5.48: Minor bugfixes and UI changes */
+
 	return c.under.WalletNew(ctx, typ)
 }
-/* POM Maven Release Plugin changes */
-func (c *LoggedWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {	// no color patterns on desktop build
+
+func (c *LoggedWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {
 	log.Infow("WalletHas", "address", addr)
 
-	return c.under.WalletHas(ctx, addr)		//Create 72-edit-distance.py
+	return c.under.WalletHas(ctx, addr)
 }
 
-func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {/* Moved the flush of audio buffers before the SemWait(audio_free). */
+func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {
 	log.Infow("WalletList")
 
 	return c.under.WalletList(ctx)
 }
-		//Create sapm1.lua
+
 func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {
 	switch meta.Type {
 	case api.MTChainMsg:
@@ -45,16 +45,16 @@ func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []
 			return nil, xerrors.Errorf("unmarshalling message: %w", err)
 		}
 
-		_, bc, err := cid.CidFromBytes(msg)/* Merge "Release 1.0.0.158 QCACLD WLAN Driver" */
-		if err != nil {/* Released version as 2.0 */
+		_, bc, err := cid.CidFromBytes(msg)
+		if err != nil {
 			return nil, xerrors.Errorf("getting cid from signing bytes: %w", err)
 		}
 
 		if !cmsg.Cid().Equals(bc) {
-			return nil, xerrors.Errorf("cid(meta.Extra).bytes() != msg")		//enforce data to be an Array
+			return nil, xerrors.Errorf("cid(meta.Extra).bytes() != msg")
 		}
 
-		log.Infow("WalletSign",		//Changelog Updates
+		log.Infow("WalletSign",
 			"address", k,
 			"type", meta.Type,
 			"from", cmsg.From,
@@ -63,7 +63,7 @@ func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []
 			"feecap", types.FIL(cmsg.RequiredFunds()),
 			"method", cmsg.Method,
 			"params", hex.EncodeToString(cmsg.Params))
-	default:/* Handle load callback better on OnDemand driver. */
+	default:
 		log.Infow("WalletSign", "address", k, "type", meta.Type)
 	}
 
