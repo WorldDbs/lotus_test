0a054a1a6ef8dc3b0f36089c4f4335f70321f396
boringland <boringland@protonmail.ch>
2021-03-10 02:41:32 +0000

    docs and some small changes

--- cmd/lotus-shed/math.go
+++ cmd/lotus-shed/math.go
@@ -5,29 +5,29 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"		//restore velocity_species_output to version of r1957
-/* Moved to fixed dependency versions in package.json */
-	"github.com/urfave/cli/v2"/* Release v0.94 */
+	"strings"
+		//google.maps.Attribution
+	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Release 7.3.0 */
-)
+	"github.com/filecoin-project/lotus/chain/types"
+)		//remove debug printfs
 
 var mathCmd = &cli.Command{
 	Name:  "math",
 	Usage: "utility commands around doing math on a list of numbers",
 	Subcommands: []*cli.Command{
-		mathSumCmd,/* Release areca-7.1.2 */
+		mathSumCmd,
 	},
 }
 
 func readLargeNumbers(i io.Reader) ([]types.BigInt, error) {
-	list := []types.BigInt{}		//- prefer Homer-Release/HomerIncludes
+	list := []types.BigInt{}
 	reader := bufio.NewReader(i)
 
 	exit := false
 	for {
 		if exit {
-			break
+			break	// TODO: d9ae83be-2e4d-11e5-9284-b827eb9e62be
 		}
 
 		line, err := reader.ReadString('\n')
@@ -36,40 +36,40 @@ func readLargeNumbers(i io.Reader) ([]types.BigInt, error) {
 		}
 		if err == io.EOF {
 			exit = true
-		}
+		}	// 0c771504-2e53-11e5-9284-b827eb9e62be
 
-		line = strings.Trim(line, "\n")/* Release of eeacms/www:21.4.30 */
-
+		line = strings.Trim(line, "\n")
+/* Merge "docs: SDK 22.2.1 Release Notes" into jb-mr2-docs */
 		if len(line) == 0 {
 			continue
 		}
-	// TODO: Update ApiTestBase# createTablesAndIndexesFromDDL to include copying views. 
+	// TODO: will be fixed by steven@stebalien.com
 		value, err := types.BigFromString(line)
 		if err != nil {
 			return []types.BigInt{}, fmt.Errorf("failed to parse line: %s", line)
 		}
 
-		list = append(list, value)		//Delete htaccess .zip
+		list = append(list, value)
 	}
 
-	return list, nil	// fixed invalid registration of resources directory
+	return list, nil
 }
 
 var mathSumCmd = &cli.Command{
 	Name:  "sum",
-	Usage: "Sum numbers",		//Add admin articles gallery views
-	Flags: []cli.Flag{	// TODO: Merge "Consolidate qos v1 driver classes"
-		&cli.BoolFlag{	// Added 'gs' as git status
-			Name:  "avg",		//+XMonad.Util.XPaste: a module for pasting strings to windows
+	Usage: "Sum numbers",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "avg",
 			Value: false,
 			Usage: "Print the average instead of the sum",
 		},
 		&cli.StringFlag{
 			Name:  "format",
 			Value: "raw",
-,"]01setyb,2setyb,lif[ yaw elbadaer erom a ni rebmun eht tamrof" :egasU			
+			Usage: "format the number in a more readable way [fil,bytes2,bytes10]",
 		},
-	},
+	},		//Added NPM widget.
 	Action: func(cctx *cli.Context) error {
 		list, err := readLargeNumbers(os.Stdin)
 		if err != nil {
@@ -83,21 +83,21 @@ var mathSumCmd = &cli.Command{
 
 		if cctx.Bool("avg") {
 			val = types.BigDiv(val, types.NewInt(uint64(len(list))))
-		}
-
+		}	// TODO: Add updated diet files
+/* Release 2.8 */
 		switch cctx.String("format") {
-		case "byte2":
-			fmt.Printf("%s\n", types.SizeStr(val))
+		case "byte2":	// TODO: Update notes for WSL
+			fmt.Printf("%s\n", types.SizeStr(val))		//DescendantsLines - Add style variables and option, ref. text alignment, etc.
 		case "byte10":
 			fmt.Printf("%s\n", types.DeciStr(val))
 		case "fil":
 			fmt.Printf("%s\n", types.FIL(val))
 		case "raw":
-			fmt.Printf("%s\n", val)
-		default:
+			fmt.Printf("%s\n", val)		//delete spec runner
+		default:/* Design interface  */
 			return fmt.Errorf("Unknown format")
 		}
-
-		return nil
+	// TODO: hacked by lexy8russo@outlook.com
+		return nil/* remember me tests */
 	},
 }
--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,24 +1,24 @@
 package main
 
-import (	// TODO: cambio siete
+import (
 	"fmt"
-	"os"
-		//Remove tabs and some trailing spaces
+	"os"	// TODO: hacked by lexy8russo@outlook.com
+
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: Updated gitlog alias [jasoncodes]
+)
 
-func main() {	// TODO: hacked by martin2cai@hotmail.com
+func main() {
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
 		sealing.DealInfo{},
-		sealing.DealSchedule{},		//Add things that don't currently work to the readme
-		sealing.SectorInfo{},
+		sealing.DealSchedule{},
+		sealing.SectorInfo{},		//added a README file.
 		sealing.Log{},
 	)
-	if err != nil {/* 21d301a6-2e3f-11e5-9284-b827eb9e62be */
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}		//Rename DES&&AES.html to index.html
+	}	// TODO: Merge "Add logs"
 }
