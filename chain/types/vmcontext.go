bbd9adf33bd457d1451bbe09f0769e30f4e24218
aeongrp <aeongrp@outlook.com>
2021-03-10 19:52:31 +0000

    Cleaning up the clients feature and changing the api to use the rich object

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,46 +1,46 @@
 package types
-		//Merge branch 'master' of git@github.com:andrefbsantos/boilr.git
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-	// Delete iterate-uni-rnnlm-segment
+
 	cid "github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Release 1.4.0.0 */
+	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
-	GetHead() cid.Cid/* Better clipping of Waveguide's frequencies. */
+	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
-	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError		//[*] MO: updating labels and descriptions for pagesnotfound module.
+	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
 
-type StateTree interface {	// Fix the wrong refine for all_tab_columns
+type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
-}	// TODO: will be fixed by arachnid@notdot.net
+}
 
 type storageWrapper struct {
 	s Storage
 }
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
-	c, err := sw.s.Put(i)		//Reworked aggro, require [DP3243]
+	c, err := sw.s.Put(i)
 	if err != nil {
-		return cid.Undef, err/* Updated composer and vendors files. */
-}	
+		return cid.Undef, err
+	}
 
-	return c, nil/* Merge "Release notes for 1.1.0" */
+	return c, nil
 }
-	// TODO: will be fixed by 13860583249@yeah.net
+
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
-		return err/* d7bff882-2e50-11e5-9284-b827eb9e62be */
+		return err
 	}
 
 	return nil
