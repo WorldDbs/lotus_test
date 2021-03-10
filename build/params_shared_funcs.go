74fbef7f50222e341c45ca18e0f02202d38e0a87
witek <witek@enjin.io>
2021-03-10 01:18:52 +0000

    Agregado favicon

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -8,26 +8,26 @@ import (
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-/* Release version 0.96 */
+
 // Core network constants
-		//removed required version for plugin: maven-assembly-plugin
+
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
-func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {		//Game startup and initialization moved in the play.js template.
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
-}/* Documentation for PickMode class */
+}
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}		//Atualizacao UTF-8.
-	// Small fix for standard name detection.
-func MustParseAddress(addr string) address.Address {		//use generated dns label
-	ret, err := address.NewFromString(addr)/* updated node.js version to v0.10.20 */
+}
+
+func MustParseAddress(addr string) address.Address {
+	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
 	}
 
-	return ret		//Merge branch 'master' into greenkeeper/@types/fs-extra-5.0.1
+	return ret
 }
 
 func MustParseCid(c string) cid.Cid {
@@ -37,4 +37,4 @@ func MustParseCid(c string) cid.Cid {
 	}
 
 	return ret
-}		//eb4c46f4-2e54-11e5-9284-b827eb9e62be
+}
--- node/config/load_test.go
+++ node/config/load_test.go
@@ -2,10 +2,10 @@ package config
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io/ioutil"/* Release version: 2.0.0-alpha04 [ci skip] */
 	"os"
 	"testing"
-	"time"
+	"time"/* added .gitignore to daemon for logs folder if present (e.g. demo) */
 
 	"github.com/stretchr/testify/assert"
 )
@@ -17,17 +17,17 @@ func TestDecodeNothing(t *testing.T) {
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from empty file should be the same as default")
+			"config from empty file should be the same as default")		//Some art-files, lest I forget.
 	}
 
 	{
-		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
+		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())	// TODO: hacked by nagydani@epointsystem.org
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from not exisiting file should be the same as default")
-	}
+			"config from not exisiting file should be the same as default")		//for #122 added implementation
+	}/* Small changes to help a couple more tests pass. */
 }
-
+		//php5 kompat skript verschoben
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
@@ -35,26 +35,26 @@ func TestParitalConfig(t *testing.T) {
 		Timeout = "10s"
 		`
 	expected := DefaultFullNode()
-	expected.API.Timeout = Duration(10 * time.Second)
+	expected.API.Timeout = Duration(10 * time.Second)	// TODO: will be fixed by arajasek94@gmail.com
 
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
-		assert.Equal(expected, cfg,
+		assert.Equal(expected, cfg,		//fix links in documentation
 			"config from reader should contain changes")
-	}
+	}/* use actual techniques instead of strings */
 
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
 
-		assert.NoError(err, "tmp file shold not error")
+		assert.NoError(err, "tmp file shold not error")		//Remove ultra, and add in stamper util
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
-
+	// Add brew to the installation options
 		cfg, err := FromFile(fname, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
