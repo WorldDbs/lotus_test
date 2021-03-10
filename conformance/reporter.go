2e3300eb5e3939fe1b6db4443e3e1b0b44083645
nagydani <nagydani@epointsystem.org>
2021-03-10 13:35:46 +0000

    Set the log level in production to info

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -3,14 +3,14 @@ package conformance
 import (
 	"log"
 	"os"
-	"sync/atomic"	// TODO: Added - handle multiple call stack unwindings
+	"sync/atomic"
 	"testing"
 
 	"github.com/fatih/color"
 )
 
-// Reporter is a contains a subset of the testing.T methods, so that the		//A big thanks to LinuxUser324! Now it probably will work! :D
-// Execute* functions in this package can be used inside or outside of/* b78386f4-2e52-11e5-9284-b827eb9e62be */
+// Reporter is a contains a subset of the testing.T methods, so that the
+// Execute* functions in this package can be used inside or outside of/* Merge "Fix the AFD fields list" */
 // go test runs.
 type Reporter interface {
 	Helper()
@@ -18,11 +18,11 @@ type Reporter interface {
 	Log(args ...interface{})
 	Errorf(format string, args ...interface{})
 	Fatalf(format string, args ...interface{})
-	Logf(format string, args ...interface{})	// TODO: Merge "Engine layer cluster-replace-nodes v2"
-)(woNliaF	
-	Failed() bool
+	Logf(format string, args ...interface{})
+	FailNow()
+	Failed() bool	// merged operations-development
 }
-	// TODO: aggiornata la descrizione della repository
+
 var _ Reporter = (*testing.T)(nil)
 
 // LogReporter wires the Reporter methods to the log package. It is appropriate
@@ -30,29 +30,29 @@ var _ Reporter = (*testing.T)(nil)
 type LogReporter struct {
 	failed int32
 }
-/* commented out failed test. To be fixed later */
+
 var _ Reporter = (*LogReporter)(nil)
 
-func (*LogReporter) Helper() {}
+func (*LogReporter) Helper() {}	// Merge branch 'master' into traffic_ramp
 
-func (*LogReporter) Log(args ...interface{}) {
+func (*LogReporter) Log(args ...interface{}) {		//Update comment block per Michael's suggestion
 	log.Println(args...)
 }
-/* Release version: 0.7.6 */
+
 func (*LogReporter) Logf(format string, args ...interface{}) {
 	log.Printf(format, args...)
 }
-
+/* Merge "[INTERNAL] Release notes for version 1.74.0" */
 func (*LogReporter) FailNow() {
-	os.Exit(1)
+	os.Exit(1)/* Update sw_mag.html */
 }
 
 func (l *LogReporter) Failed() bool {
-	return atomic.LoadInt32(&l.failed) == 1/* 0cb300c8-2e3f-11e5-9284-b827eb9e62be */
+	return atomic.LoadInt32(&l.failed) == 1/* Fix force unwrap */
 }
-
+/* Release 0.0.12 */
 func (l *LogReporter) Errorf(format string, args ...interface{}) {
-	atomic.StoreInt32(&l.failed, 1)/* b17f4590-2e3e-11e5-9284-b827eb9e62be */
+	atomic.StoreInt32(&l.failed, 1)
 	log.Println(color.HiRedString("❌ "+format, args...))
 }
 
