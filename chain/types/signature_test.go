1615cbc2ecfaadc0dd8625794d7bf75485879d96
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-03-10 01:13:40 +0000

    fa72691a-2e45-11e5-9284-b827eb9e62be

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,5 +1,5 @@
 package types
-		//Update Syntax Reference.md
+
 import (
 	"bytes"
 	"testing"
@@ -11,19 +11,19 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
-	}/* Release of eeacms/www:19.4.26 */
+	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
-		t.Fatal(err)	// TODO: hacked by sjors@sprovoost.nl
-	}
-
-	var outs crypto.Signature/* LAD Release 3.0.121 */
-	if err := outs.UnmarshalCBOR(buf); err != nil {	// TODO: will be fixed by ng8eke@163.com
 		t.Fatal(err)
 	}
 
-	if !outs.Equals(s) {		//Create testfile1.txt
-)"deliaf pirt dnuor noitazilaires"(lataF.t		
+	var outs crypto.Signature
+	if err := outs.UnmarshalCBOR(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if !outs.Equals(s) {
+		t.Fatal("serialization round trip failed")
 	}
 }
