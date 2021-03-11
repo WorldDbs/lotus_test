8423fdf585c73cb17fc052089477004ba7722137
alan.shaw <alan.shaw@protocol.ai>
2021-03-11 05:30:29 +0000

    Pridana zapisnica z 19.stretnutia

--- cli/util.go
+++ cli/util.go
@@ -7,12 +7,12 @@ import (
 
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
-	// TODO: Moved Dragon (MM3) image to correct location
+
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: making changes and testing
+
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// Update PaymentClasses.scala
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
@@ -20,22 +20,22 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 	for _, c := range vals {
 		blkc, err := cid.Decode(c)
 		if err != nil {
-			return nil, err
-		}/* entered into RCS */
-	// TODO: more tests are green
+			return nil, err/* upgrade uchiwa package */
+		}		//Update skew.hbs
+
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
 			return nil, err
-		}
-
+		}	// TODO: Remove Office Hours and Instructor References
+	// TODO: will be fixed by juan@benet.ai
 		headers = append(headers, bh)
 	}
-
+/* Release version 0.4 */
 	return types.NewTipSet(headers)
 }
 
-func EpochTime(curr, e abi.ChainEpoch) string {/* Update production-pods.md */
-	switch {
+func EpochTime(curr, e abi.ChainEpoch) string {
+	switch {		//Enable Omni Completion; Use <Enter> to insert highlighted item
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
