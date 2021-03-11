35f787e6617b328447132124f28b7c9f34436648
admin <admin@multicoin.co>
2021-03-11 19:18:28 +0000

    creation working

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -1,20 +1,20 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"		//namespace urls
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"/* Release date now available field to rename with in renamer */
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"	// Update dbl_lnkd_lst.cpp
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release 0.4.24 */
+)
 
 type message0 struct{ from address.Address }
-/* Released 1.6.5. */
+
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
@@ -24,7 +24,7 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {/* Merge "Upgrade Ruby to version 1.9.3-p385" */
+	if aerr != nil {
 		return nil, aerr
 	}
 
@@ -38,10 +38,10 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 }
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{/* Merge "Add doc/Makefile to help with building docs" */
+	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
-)}	
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -54,21 +54,21 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Params: params,
 	}, nil
 }
-	// Create fabquartz.js
+
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
-	}, nil/* @Release [io7m-jcanephora-0.34.3] */
-}	// Update RectangleObject.cs
+	}, nil
+}
 
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,	// Reporter: rename ReporterOptions
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
-	}, nil/* Release version 0.1.18 */
+	}, nil
 }
