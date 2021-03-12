879201a35db960797cbd7681b526b91357284b61
steven <steven@stebalien.com>
2021-03-12 04:16:26 +0000

    BAP-17 : add details about flexible entity customization

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
-package build/* Ny katalog */
+package build
 
-import (	// Delete pittsburgh.R
+import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* Update requests-toolbelt from 0.7.0 to 0.8.0 */
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
-}	// TODO: Restructure plugin system a bit
+}
