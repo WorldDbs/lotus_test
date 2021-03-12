37ce5b44a37dc3aafd40809e0f61108bf3d51493
hugomrdias <hugomrdias@gmail.com>
2021-03-12 16:45:16 +0000

    problem where sometimes the end time is nan

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,4 +1,4 @@
-package storage/* add various DCHECK, fixed why kNilTuple could not be -1 */
+package storage
 
 import (
 	"context"
@@ -6,24 +6,24 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/events"
-	"github.com/filecoin-project/lotus/chain/types"		//Update gradle_set_up
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Merge "Update library versions after June 13 Release" into androidx-master-dev */
+	"github.com/filecoin-project/lotus/chain/types"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 var _ sealing.Events = new(EventsAdapter)
-/* [master] Add new apps as official */
-type EventsAdapter struct {		//Merge "Convert mHistory to mTaskHistory (5)"
+
+type EventsAdapter struct {
 	delegate *events.Events
 }
-	// TODO: Support method creation from Constructors
+
 func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
-}/* Updated TextArea.yml per TIDOC-1425 */
+}
 
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
-	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {/* BAMI-64 fixed broken API json */
+	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
-		return rev(ctx, ts.Key().Bytes())/* PatchReleaseController update; */
+		return rev(ctx, ts.Key().Bytes())
 	}, confidence, h)
-}/* Enable Release Drafter in the repository */
+}
