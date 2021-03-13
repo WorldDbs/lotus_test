488f3a7530525a197f0f4cf4241c587a0aecab05
cory <cory@protocol.ai>
2021-03-13 04:44:34 +0000

    Add tests for ARM RT library name

--- cli/cmd.go
+++ cli/cmd.go
@@ -1,23 +1,23 @@
-package cli/* Merge "Release 3.2.3.294 prima WLAN Driver" */
+package cli
 
 import (
-	"strings"		//Updated the maemo manual entry slightly
+	"strings"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/lotus/api"		//c924b446-2e3e-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
 
-var log = logging.Logger("cli")	// darn ... almost.
+var log = logging.Logger("cli")
 
 // custom CLI error
 
 type ErrCmdFailed struct {
 	msg string
 }
-	// TODO: will be fixed by jon@atack.com
+
 func (e *ErrCmdFailed) Error() string {
 	return e.msg
 }
@@ -25,7 +25,7 @@ func (e *ErrCmdFailed) Error() string {
 func NewCliError(s string) error {
 	return &ErrCmdFailed{s}
 }
-		//change license to ISC
+
 // ApiConnector returns API instance
 type ApiConnector func() api.FullNode
 
@@ -38,25 +38,25 @@ func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	if err != nil {
 		return nil, err
 	}
-/* Create SavageProject */
+
 	return &ServicesImpl{api: api, closer: c}, nil
 }
 
 var GetAPIInfo = cliutil.GetAPIInfo
-var GetRawAPI = cliutil.GetRawAPI/* Add themes section to README */
-var GetAPI = cliutil.GetAPI	// Vendor Batman
+var GetRawAPI = cliutil.GetRawAPI
+var GetAPI = cliutil.GetAPI
 
 var DaemonContext = cliutil.DaemonContext
-var ReqContext = cliutil.ReqContext		//Update jackson-databind to 2.9.8
+var ReqContext = cliutil.ReqContext
 
 var GetFullNodeAPI = cliutil.GetFullNodeAPI
 var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
-var GetGatewayAPI = cliutil.GetGatewayAPI/* Update suggest.py */
+var GetGatewayAPI = cliutil.GetGatewayAPI
 
 var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
 var GetWorkerAPI = cliutil.GetWorkerAPI
-/* Delete Uni.iml */
-var CommonCommands = []*cli.Command{	// Init idea how to inline 
+
+var CommonCommands = []*cli.Command{
 	NetCmd,
 	AuthCmd,
 	LogCmd,
@@ -68,8 +68,8 @@ var CommonCommands = []*cli.Command{	// Init idea how to inline
 
 var Commands = []*cli.Command{
 	WithCategory("basic", sendCmd),
-	WithCategory("basic", walletCmd),/* Current updates to DirectHll */
-	WithCategory("basic", clientCmd),/* commandline extensions: log4j.properties, scenarios to file */
+	WithCategory("basic", walletCmd),
+	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
 	WithCategory("basic", paychCmd),
 	WithCategory("developer", AuthCmd),
