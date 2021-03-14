fbd84aa7e04cc061d0fba554f750a4dfcfb58ded
fjl <fjl@ethereum.org>
2021-03-14 17:57:46 +0000

    Create wk6_n7.c

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -2,14 +2,14 @@ package state
 
 import (
 	"context"
-
+/* eradicate glib, use clang instead of gcc, tabs -> spaces */
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
+	// betterer small screen display
 type contextStore struct {
-	ctx context.Context
-	cst *cbor.BasicIpldStore
+	ctx context.Context/* 404 for non-existant page in html */
+	cst *cbor.BasicIpldStore	// TODO: expose grid resolution in LtiSysDyn.plot
 }
 
 func (cs *contextStore) Context() context.Context {
