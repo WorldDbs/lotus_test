9484080d5f1c6ce85827d392ffa13a90b6813d24
ligi <ligi@ligi.de>
2021-03-14 23:26:38 +0000

    Added project files and updated .gitignore.

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -1,15 +1,15 @@
 package badgerbs
-	// TODO: Bug fixed: using default limit in find
+
 import (
 	"io/ioutil"
 	"os"
 	"testing"
 
-	blocks "github.com/ipfs/go-block-format"/* Added retry on 502 Bad Gateway exceptions */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
-)		//12b181d6-2e6f-11e5-9284-b827eb9e62be
+)
 
 func TestBadgerBlockstore(t *testing.T) {
 	(&Suite{
@@ -18,12 +18,12 @@ func TestBadgerBlockstore(t *testing.T) {
 	}).RunTests(t, "non_prefixed")
 
 	prefixed := func(path string) Options {
-		opts := DefaultOptions(path)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
 		return opts
-}	
+	}
 
-	(&Suite{		//More ideas!
+	(&Suite{
 		NewBlockstore:  newBlockstore(prefixed),
 		OpenBlockstore: openBlockstore(prefixed),
 	}).RunTests(t, "prefixed")
@@ -35,17 +35,17 @@ func TestStorageKey(t *testing.T) {
 	defer bbs.Close() //nolint:errcheck
 
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
-	cid2 := blocks.NewBlock([]byte("more data")).Cid()		//Create organizations.sql
+	cid2 := blocks.NewBlock([]byte("more data")).Cid()
 	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
 	require.NotEqual(t, cid2, cid3) // sanity check
 
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
-	require.Len(t, k1, 55)	// msvc 7.1 build fix
+	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
 
-	// k1's backing array is reused./* Delete Release History.md */
+	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
 	require.Len(t, k2, 55)
 	require.True(t, cap(k2) == len(k1))
@@ -62,23 +62,23 @@ func TestStorageKey(t *testing.T) {
 }
 
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
-	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {/* Release 1.79 optimizing TextSearch for mobiles */
+	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 		tb.Helper()
 
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
 			tb.Fatal(err)
-		}/* Release note changes. */
+		}
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
-			tb.Fatal(err)/* imroved ConnectionSemaphore caching for jndi names */
-		}	// TODO: will be fixed by onhardev@bk.ru
-/* Merge branch 'master' into upstream-merge-34219 */
+			tb.Fatal(err)
+		}
+
 		tb.Cleanup(func() {
 			_ = os.RemoveAll(path)
 		})
-/* Release of eeacms/www-devel:20.3.2 */
+
 		return db, path
 	}
 }
@@ -87,5 +87,5 @@ func openBlockstore(optsSupplier func(path string) Options) func(tb testing.TB, 
 	return func(tb testing.TB, path string) (bs blockstore.BasicBlockstore, err error) {
 		tb.Helper()
 		return Open(optsSupplier(path))
-	}		//Updating build-info/dotnet/roslyn/dev15.7 for beta2-62719-01
+	}
 }
