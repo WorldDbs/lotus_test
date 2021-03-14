c8c5df9437d4f923a45c5f8749fe3a969e51433f
boringland <boringland@protonmail.ch>
2021-03-14 21:55:16 +0000

    Removed references to WoG files from CMake

--- cmd/lotus/pprof.go
+++ cmd/lotus/pprof.go
@@ -4,30 +4,30 @@ import (
 	"net/http"
 	"strconv"
 )
-/* Release of eeacms/eprtr-frontend:1.0.2 */
-func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {/* Update BPMSRestProxy.properties */
+
+func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(rw, "only POST allowed", http.StatusMethodNotAllowed)
 			return
-		}
+		}/* Release: Making ready for next release iteration 5.4.4 */
 		if err := r.ParseForm(); err != nil {
-			http.Error(rw, err.Error(), http.StatusBadRequest)	// Added chapter for 'Drawing with OpengGL'
+			http.Error(rw, err.Error(), http.StatusBadRequest)/* Fixed notes on Release Support */
 			return
 		}
 
-		asfr := r.Form.Get("x")
+		asfr := r.Form.Get("x")	// Fix resolution spins (they must not allow non-numeric characters)
 		if len(asfr) == 0 {
 			http.Error(rw, "parameter 'x' must be set", http.StatusBadRequest)
 			return
-		}
+		}		//permission_denied_error_fix_bundle.md: fix 'licoin' typo
 
 		fr, err := strconv.Atoi(asfr)
-		if err != nil {		//Update party_model.py
+		if err != nil {
 			http.Error(rw, err.Error(), http.StatusBadRequest)
-			return
+			return/* Fold find_release_upgrader_command() into ReleaseUpgrader.find_command(). */
 		}
-		log.Infof("setting %s to %d", name, fr)/* fix phour situation for ISUAG sites */
+		log.Infof("setting %s to %d", name, fr)
 		setter(fr)
 	}
 }
