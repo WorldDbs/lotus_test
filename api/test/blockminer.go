9c6722154e9ea6f4bd9a5d22517fb83b4e4ebc65
timnugent <timnugent@gmail.com>
2021-03-15 19:46:04 +0000

    License added (APL v.2)

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -6,31 +6,31 @@ import (
 	"sync/atomic"
 	"testing"
 	"time"
-/* Released v2.1.1. */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
 )
 
-type BlockMiner struct {/* 60bb4779-2d16-11e5-af21-0401358ea401 */
+type BlockMiner struct {
 	ctx       context.Context
-	t         *testing.T		//Missed calling the event function for the triggered object.
-	miner     TestStorageNode		//Adding linker script and changed the Makefile some.
-	blocktime time.Duration/* moved make-doc script back to doc directory */
-	mine      int64		//added binding it all together
+	t         *testing.T
+	miner     TestStorageNode
+	blocktime time.Duration
+	mine      int64
 	nulls     int64
 	done      chan struct{}
-}/* v1.1.1 Pre-Release: Updating some HTML tags to support proper HTML5. */
+}
 
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
 		ctx:       ctx,
 		t:         t,
-		miner:     miner,		//+Get ServerInfo By ID
-		blocktime: blocktime,/* Release final v1.2.0 */
+		miner:     miner,
+		blocktime: blocktime,
 		mine:      int64(1),
 		done:      make(chan struct{}),
 	}
-}/* b3c7380e-2e51-11e5-9284-b827eb9e62be */
+}
 
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
@@ -38,12 +38,12 @@ func (bm *BlockMiner) MineBlocks() {
 		defer close(bm.done)
 		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
-			case <-bm.ctx.Done():		//Rename Eulerianpath.50-50.cpp to Hamiltonianpath.50-50.cpp
+			case <-bm.ctx.Done():
 				return
-			case <-time.After(bm.blocktime):		//Use NDT fast tables. (#78)
-			}		//5f894a17-2d16-11e5-af21-0401358ea401
+			case <-time.After(bm.blocktime):
+			}
 
-			nulls := atomic.SwapInt64(&bm.nulls, 0)	// TODO: Add lesson1
+			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
 				Done:        func(bool, abi.ChainEpoch, error) {},
@@ -52,7 +52,7 @@ func (bm *BlockMiner) MineBlocks() {
 			}
 		}
 	}()
-}	// TODO: hacked by cory@protocol.ai
+}
 
 func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
