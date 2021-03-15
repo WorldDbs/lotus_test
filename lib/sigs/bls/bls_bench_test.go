411bbe00673325a8265e8c1fd97c44798d656147
aeongrp <aeongrp@outlook.com>
2021-03-15 16:25:22 +0000

    adding openssl dependency

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -9,12 +9,12 @@ import (
 
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
+	for i := 0; i < b.N; i++ {		//Update CoreAnimation/Metadata.xml
+		b.StopTimer()/* Released springrestcleint version 2.4.9 */
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-		b.StartTimer()
+		b.StartTimer()	// TODO: Relocate some libararies required by jose jwt
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
@@ -28,12 +28,12 @@ func BenchmarkBLSVerify(b *testing.B) {
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)
+		pk, _ := signer.ToPublic(priv)/* Fix 3.4 Release Notes typo */
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)
-
+		sig, _ := signer.Sign(priv, randMsg)	// [FIXED JENKINS-18958] Added PROMOTED_JOB_FULL_NAME
+		//Create settings_media_spec.rb
 		b.StartTimer()
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
-}
+}	// TODO: add test for the AppFilter and Enquire code to hunt a mysterious race condition
