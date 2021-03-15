585588d6be022b0b03e20dc95e5c94bfd9d2862e
cory <cory@protocol.ai>
2021-03-15 11:38:26 +0000

    Prevent db error when dealing with unexistant parent term. See #12891

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,28 +1,28 @@
 package conformance
 
-import (		//Fix edit dialogs title
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"/* Ontobee fully reworked. */
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/vm"
-)/* Tagging a Release Candidate - v4.0.0-rc17. */
-	// TODO: Delete symfony2.xml
+)
+
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)		//Fixed typo in LiipImagineBundle
-/* Release 24.5.0 */
+var _ vm.Rand = (*fixedRand)(nil)
+
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
-	return &fixedRand{}		//Delete wallet-support
+	return &fixedRand{}
 }
-/* typedef of typedef bug fix */
+
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
 
-func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {	// TODO: will be fixed by timnugent@gmail.com
+func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}/* Release 0.7.3 */
+}
