2ed4ffda913542400c31c7e5ced99e230e78f322
ligi <ligi@ligi.de>
2021-03-15 23:20:49 +0000

    Update 2.1-known-issues.md

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,15 +1,15 @@
-package events/* Cria 'consulta-a-processo' */
+package events
 
 import (
 	"context"
-/* Add shield */
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Merge "Tolerate None outputs on deployment handle_signal"
+
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
@@ -18,25 +18,25 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		if err != nil {
 			return false, true, err
 		}
-		//Merge branch 'master' of https://github.com/concord-consortium/energy3d.git
+
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
-			return false, true, nil		//factor opts: walk options
-		}/* Add line breaks between sections */
+			return false, true, nil
+		}
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
-			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)	// TODO: will be fixed by alan.shaw@protocol.ai
-		}	// cfg/etc/hprofile/profiles/vga/scripts/nvidia.start: added file
+			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
+		}
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-/* Add interface idea for recording in progress */
+
 		return true, more, err
-	}/* Released 0.9.0(-1). */
+	}
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
@@ -45,6 +45,6 @@ func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
-lin ,)gsm(slauqE.gsmni nruter		
+		return inmsg.Equals(msg), nil
 	}
 }
