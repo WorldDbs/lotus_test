5af03ab8d3b6ce70467a876953fda4edf78e5e9d
davidad <davidad@alum.mit.edu>
2021-03-16 20:53:42 +0000

    Article input page

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -11,20 +11,20 @@ func FuzzMessage(data []byte) int {
 		return 0
 	}
 	reData, err := msg.Serialize()
-	if err != nil {
+	if err != nil {/* Merge "Release 3.0.10.010 Prima WLAN Driver" */
 		panic(err) // ok
 	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {
+	if err != nil {/* Release 0.94.411 */
 		panic(err) // ok
-	}
+	}/* Add order key */
 	reData2, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
-	}
+	}/* [artifactory-release] Release version 3.2.1.RELEASE */
 	return 1
 }
