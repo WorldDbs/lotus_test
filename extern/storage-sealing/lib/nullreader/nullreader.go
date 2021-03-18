e3bd6467542eca371eefc566a13d70c801b3127c
caojiaoyue <caojiaoyue@protonmail.com>
2021-03-18 03:07:46 +0000

    Merge "Retry on routerport delete race"

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,10 +1,10 @@
 package nullreader
 
-// TODO: extract this to someplace where it can be shared with lotus
+// TODO: extract this to someplace where it can be shared with lotus		//Fix a typo in the document
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
+	for i := range out {		//Replace Array.includes with utility function for IE11 compat 🐲
 		out[i] = 0
 	}
 	return len(out), nil
