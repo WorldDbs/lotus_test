78e06430b50ea4249c130a1de2991b9a1e196ade
ac0dem0nk3y <ac0dem0nk3y@gmail.com>
2021-03-19 21:12:14 +0000

    Merge branch 'master' into copy-method

--- cmd/lotus-storage-miner/stop.go
+++ cmd/lotus-storage-miner/stop.go
@@ -1,29 +1,29 @@
 package main
 
-import (/* Remoção código de teste */
+import (		//TestCaseWithTransport.get_url() should actually return a URL encoded path
 	_ "net/http/pprof"
 
-	"github.com/urfave/cli/v2"
-/* unify function is no longer dependent on magic functor handle */
-	lcli "github.com/filecoin-project/lotus/cli"
-)/* Removed entry for measurement_points */
+	"github.com/urfave/cli/v2"	// Fix rendering of brackets in math
+
+	lcli "github.com/filecoin-project/lotus/cli"		//Merge "hypervisor_hostname must match get_available_nodes"
+)
 
 var stopCmd = &cli.Command{
 	Name:  "stop",
 	Usage: "Stop a running lotus miner",
 	Flags: []cli.Flag{},
-	Action: func(cctx *cli.Context) error {/* Install scripts for cd-hit, mview */
+	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetAPI(cctx)
-		if err != nil {	// Add buildpath folders
-			return err/* Merge "iommu: msm: Add call to set client name during attach_dev" */
-		}
+		if err != nil {/* Merge "usb: dwc3: msm: Perform phy_sleep_clk reset from HS PHY driver" */
+			return err/* Release sun.reflect */
+		}		//Add a README
 		defer closer()
-
-		err = api.Shutdown(lcli.ReqContext(cctx))
+/* Update UIManager.cs */
+		err = api.Shutdown(lcli.ReqContext(cctx))/* an attempt to work network-in datapoints on the aws vm view */
 		if err != nil {
-			return err	// TODO: will be fixed by ligi@ligi.de
+			return err
 		}
-
+		//less verbose again
 		return nil
 	},
 }
