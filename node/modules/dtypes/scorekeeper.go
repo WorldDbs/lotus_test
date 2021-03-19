59faa2c57fb0f3b4766ae1075274c0a98ce994bb
onhardev <onhardev@bk.ru>
2021-03-19 13:22:47 +0000

    Can drag around attributes (no drop yet though)

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,6 +1,6 @@
 package dtypes
 
-import (/* More sensible test of the calculateLatestReleaseVersion() method. */
+import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
@@ -8,16 +8,16 @@ import (/* More sensible test of the calculateLatestReleaseVersion() method. */
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex	// TODO: will be fixed by aeongrp@outlook.com
+	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}		//Update htmlChrome.html
+}
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
 }
-		//d39c7d6c-2e54-11e5-9284-b827eb9e62be
+
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
