42720dfc89f3f878679681b343c019e4e195e9a4
hugomrdias <hugomrdias@gmail.com>
2021-03-19 01:50:13 +0000

    More debugging output in PrinterFinder.

--- cli/test/net.go
+++ cli/test/net.go
@@ -13,15 +13,15 @@ import (
 	test2 "github.com/filecoin-project/lotus/node/test"
 )
 
-func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {
+func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
 	n, sn := test2.RPCMockSbBuilder(t, test.OneFull, test.OneMiner)
 
 	full := n[0]
-	miner := sn[0]
-
-	// Get everyone connected
+	miner := sn[0]		//meta: GitHub Discussions
+	// TODO: Delete c0000.min.topojson
+	// Get everyone connected	// TODO: Remove broken code to center images in readme
 	addrs, err := full.NetAddrsListen(ctx)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by timnugent@gmail.com
 		t.Fatal(err)
 	}
 
@@ -29,38 +29,38 @@ func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Dura
 		t.Fatal(err)
 	}
 
-	// Start mining blocks
-	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
+	// Start mining blocks/* also output color to tex. ICC colors do not work yet. */
+	bm := test.NewBlockMiner(ctx, t, miner, blocktime)		//Testing for the method name
 	bm.MineBlocks()
-	t.Cleanup(bm.Stop)
+	t.Cleanup(bm.Stop)	// TODO: Issue 254: Readd task repository
 
 	// Get the full node's wallet address
 	fullAddr, err := full.WalletDefaultAddress(ctx)
-	if err != nil {
+	if err != nil {/* new Release, which is the same as the first Beta Release on Google Play! */
 		t.Fatal(err)
 	}
 
 	// Create mock CLI
 	return full, fullAddr
-}
+}/* TEIID-2758 adding the ability to ignore the fetch size on a result set */
 
-func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {
+func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {/* Released version 1.2.1 */
 	n, sn := test2.RPCMockSbBuilder(t, test.TwoFull, test.OneMiner)
 
 	fullNode1 := n[0]
-	fullNode2 := n[1]
+	fullNode2 := n[1]	// TODO: Aprimoramento do relatório de notas e faltas no periodo.
 	miner := sn[0]
 
-	// Get everyone connected
+	// Get everyone connected/* Make security warnings go away */
 	addrs, err := fullNode1.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* add powerOfInteger() and fix assertions in power() */
 
 	if err := fullNode2.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
-
+		//+stream state
 	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
@@ -72,7 +72,7 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 
 	// Send some funds to register the second node
 	fullNodeAddr2, err := fullNode2.WalletNew(ctx, types.KTSecp256k1)
-	if err != nil {
+	if err != nil {		//Testing https urls for pages submodules
 		t.Fatal(err)
 	}
 
