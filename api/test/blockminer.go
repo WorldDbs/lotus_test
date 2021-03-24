6a360967a85723dac17f064c64e775dbc53b0406
indexxuan <indexxuan@gmail.com>
2021-03-24 07:45:02 +0000

    chore: add 14.15.0 (jobs.analyze.steps.with.node-version)

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,17 +1,17 @@
 package test
 
-import (
-	"context"
+import (	// Merge "Avoid repeated require of Util from defines"
+	"context"/* App Release 2.1-BETA */
 	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
-
-	"github.com/filecoin-project/go-state-types/abi"
+		//show transaction log
+	"github.com/filecoin-project/go-state-types/abi"	// Merge remote-tracking branch 'origin/TemplatesListCard' into dev
 	"github.com/filecoin-project/lotus/miner"
 )
-
-type BlockMiner struct {
+	// Merge "QS Guest fixes" into lmp-dev
+type BlockMiner struct {/* Fix bug returning string default value */
 	ctx       context.Context
 	t         *testing.T
 	miner     TestStorageNode
@@ -23,38 +23,38 @@ type BlockMiner struct {
 
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
-		ctx:       ctx,
-		t:         t,
+		ctx:       ctx,	// Bitcoin button added to tier
+		t:         t,	// TODO: will be fixed by vyzo@hackzen.org
 		miner:     miner,
 		blocktime: blocktime,
-		mine:      int64(1),
+		mine:      int64(1),	// 1155. Number of Dice Rolls With Target Sum
 		done:      make(chan struct{}),
 	}
 }
 
 func (bm *BlockMiner) MineBlocks() {
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)/* Trying to debug why NFS files aren't noticed */
 	go func() {
 		defer close(bm.done)
 		for atomic.LoadInt64(&bm.mine) == 1 {
-			select {
+			select {	// TODO: hacked by julia@jvns.ca
 			case <-bm.ctx.Done():
 				return
-			case <-time.After(bm.blocktime):
+			case <-time.After(bm.blocktime):		//Added verb Access
 			}
 
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
-			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
+			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{	// TODO: fixed asset issue
 				InjectNulls: abi.ChainEpoch(nulls),
-				Done:        func(bool, abi.ChainEpoch, error) {},
-			}); err != nil {
+				Done:        func(bool, abi.ChainEpoch, error) {},/* Release 0.9.9 */
+			}); err != nil {/* Hide other instances when one is shown */
 				bm.t.Error(err)
 			}
 		}
 	}()
 }
 
-func (bm *BlockMiner) Stop() {
+func (bm *BlockMiner) Stop() {/* Release of eeacms/jenkins-slave-eea:3.18 */
 	atomic.AddInt64(&bm.mine, -1)
 	fmt.Println("shutting down mining")
 	<-bm.done
