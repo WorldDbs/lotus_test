7f166f7212635e9df56f9f2c86994128597fe339
sebs <sebs@2xs.org>
2021-03-24 19:15:19 +0000

    Create Relevant Files

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -2,15 +2,15 @@ package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided
+// BadgerBlockstoreOptions returns the badger options to apply for the provided/* Add mechanism to have list of schemas that will never reach replication stream. */
 // domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {	// TODO: fix insmod for ifup.wwan
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all		//updated with new loadHelper signature
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"/* Use varargs for info level logging. */
+	opts.Prefix = "/blocks/"	// Fixed Issues #21 and https://github.com/GTNewHorizons/NewHorizons/issues/729
 
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
@@ -18,12 +18,12 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true	// TODO: hacked by martin2cai@hotmail.com
-	// TODO: hacked by martin2cai@hotmail.com
-	// The alternative is "crash on start and tell the user to fix it". This
+	opts.CompactL0OnClose = true
+		//Adding screenshots of demos
+	// The alternative is "crash on start and tell the user to fix it". This	// Added fix to Kalman seed tests
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.
-	opts.Truncate = true
+	// persist anyways.		//Float numbers are only immediates if the VM has SmallFloats.
+	opts.Truncate = true	// TODO: will be fixed by alan.shaw@protocol.ai
 
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
@@ -35,12 +35,12 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
-	opts.MaxTableSize = 64 << 20	// gets iface name from /proc/net/wireless
-		//Removal of flight costs from the GUI.
-	// NOTE: The chain blockstore doesn't require any GC (blocks are never
+	opts.MaxTableSize = 64 << 20
+
+	// NOTE: The chain blockstore doesn't require any GC (blocks are never	// readme with play store download link
 	// deleted). This will change if we move to a tiered blockstore.
 
-	opts.ReadOnly = readonly	// TODO: hacked by lexy8russo@outlook.com
-/* try to fix FileNotFountException */
+	opts.ReadOnly = readonly
+
 	return opts, nil
 }
